c6f16fd198183ba1515c14fe4231b8e21f721a29
Foxfire <ajdobbs.sm@gmail.com>
2018-12-18 17:39:25 -0500

    updated the script with some feedback from JB

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-
+set -e
 # $1 the first argument after the command is the relative or absolute path to the client
 # directory, where you intend to run npm link on installed UI-Kit modules.
 # To run, navigate to your leafygreen-ui folder root, then in the shell run
 # bash scripts/link.sh ${PATH_TO_APPLICATION}
 
 if [ "$1" != "" ]; then
-    npm run bootstrap && \
-    npm run link && \
-    cd ./packages/lib && \
-    npm run build && \
-    cd ../theme && \
-    npm run build && \
-    cd ../../ && \
-    npm run build && \
-    cd $1 && \
+    npm run bootstrap
+    npm run link
+    cd ./packages/lib
+    npm run build
+    cd ../theme
+    npm run build
+    cd ../../
+    npm run build
+    cd $1
     cd ./node_modules/@leafygreen-ui
     INSTALLED_PACKAGES_ARRAY=()
     for d in *; do
