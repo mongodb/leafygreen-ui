1df6487238f4dbf4c6f4e6497adef01f1313923d
Foxfire <ajdobbs.sm@gmail.com>
2018-12-18 12:47:43 -0500

    parameterized the client folder for npm

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-npm run bootstrap && \
-npm run link && \
-{
-    npm run build
-    } || {
-        npm run bootstrap
-        } && \
-npm run build && \
-cd ../mms/client && \
-npm link @leafygreen-ui/Button
+if [ "$1" != "" ]; then
+    npm run bootstrap && \
+    npm run link && \
+    {
+        npm run build
+        } || {
+            npm run bootstrap
+            } && \
+    npm run build && \
+    cd ../$1 && \
+    npm link @leafygreen-ui/Button
+else
+    echo "Client Folder Path is empty"
+fi
