75cefdd5a4023473ac996554992430e9575c6e24
Foxfire <ajdobbs.sm@gmail.com>
2018-12-21 11:32:16 -0500

    first pass at moving around the shell script to pwd

--- scripts/link.sh
+++ scripts/link.sh
@@ -5,33 +5,40 @@ set -e
 # To run, navigate to your leafygreen-ui folder root, then in the shell run
 # npm run link -- ${PATH_TO_APPLICATION}
 
-if [ "$1" != "" ]; then
-    cd `dirname $0`/../
-    npm run bootstrap
-    npm run lerna-link
-    cd `dirname $0`/../packages/lib
-    npm run build
-    cd `dirname $0`/../packages/theme
-    npm run build
-    cd `dirname $0`/../
-    npm run build
-    cd $1
-    if cd ./node_modules/@leafygreen-ui; then
-        echo "leafygreen modules successfully found"
-    else
-        echo "The application either does not have it's node_modules installed or does not have leafygreen-ui components installed"
-        exit 1
-    fi
-    INSTALLED_PACKAGES_ARRAY=()
-    for d in *; do
-        echo $d
-        INSTALLED_PACKAGES_ARRAY+=($d)
-    done
-    cd ../../
-    for f in "${INSTALLED_PACKAGES_ARRAY[@]}"; do
-        npm link @leafygreen-ui/$f
-    done
-else
+if [ "$1" == "" ]; then
     echo "This script requires a path to the target application from the root folder of this repository."
     echo "After the bash command, please add a relative or absolute path to the repository of your application where you run your node_modules are located"
+    exit 1
 fi
+echo `dirname $1`
+LEAFYGREEN_HOME=$(pwd)/`dirname $0`/../
+echo $LEAFYGREEN_HOME
+cd $LEAFYGREEN_HOME
+npm run bootstrap
+npm run lerna-link
+cd $LEAFYGREEN_HOME/packages/lib
+npm run build
+cd $LEAFYGREEN_HOME/packages/theme
+npm run build
+cd $LEAFYGREEN_HOME
+npm run build
+APPLICATION_HOME=$1
+if cd $APPLICATION_HOME/node_modules/@leafygreen-ui; then
+    echo "leafygreen modules successfully found"
+else
+    echo "The application either does not have it's node_modules installed or does not have leafygreen-ui components installed"
+    exit 1
+fi
+INSTALLED_PACKAGES_ARRAY=()
+for d in *; do
+    if [ "$d" == "lib" ] || [ "$d" == "theme" ]; then
+        echo "skipping dependecy"
+    else
+        INSTALLED_PACKAGES_ARRAY+=($d)
+    fi
+done
+cd ../../
+for f in "${INSTALLED_PACKAGES_ARRAY[@]}"; do
+    npm link @leafygreen-ui/$f
+done
+
