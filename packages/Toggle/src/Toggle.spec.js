df13ffa1d3d150a7900daff332b20befd2f2fb72
Dave McCarthy <dave.mccarthy@mongodb.com>
2019-02-06 12:13:43 -0800

    implement dark-mode toggles

--- packages/Toggle/src/Toggle.spec.js
+++ packages/Toggle/src/Toggle.spec.js
@@ -1,37 +1,89 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from 'react-testing-library';
-import Checkbox from '.';
+import Toggle from '.';
 
 afterAll(cleanup);
 
-describe('packages/Checkbox', () => {
-  const onClick = jest.fn();
+describe('packages/Toggle', () => {
   const className = 'test-checkbox-class';
   const { container } = render(
-    <Checkbox className={className} checked={false} />,
+    <Toggle className={className} checked={false} />,
   );
-  const renderedContainer = container.firstChild;
-  const renderedCheckbox = renderedContainer.children[0];
+  const controlledContainer = container.firstChild;
+  const controlledCheckbox = controlledContainer.children[0];
 
   test(`renders "${className}" in the Checkbox label's classList`, () => {
-    expect(renderedContainer.classList.contains(className)).toBe(true);
+    expect(controlledContainer.classList.contains(className)).toBe(true);
   });
 
   test('Checkbox is checked when checked prop is set', () => {
-    render(<Checkbox className={className} checked={true} />, { container });
-    expect(renderedCheckbox.checked).toBe(true);
-    expect(renderedCheckbox.getAttribute('aria-checked')).toBe('true');
+    expect(controlledCheckbox.checked).toBe(false);
+    expect(controlledCheckbox.getAttribute('aria-checked')).toBe('false');
+
+    render(<Toggle checked={true} />, { container });
+
+    expect(controlledCheckbox.checked).toBe(true);
+    expect(controlledCheckbox.getAttribute('aria-checked')).toBe('true');
   });
 
   test('Checkbox is disabled when disabled prop is set', () => {
-    render(<Checkbox className={className} disabled={true} />, { container });
-    expect(renderedCheckbox.disabled).toBe(true);
-    expect(renderedCheckbox.getAttribute('aria-disabled')).toBe('true');
+    render(<Toggle disabled={true} checked={false} />, { container });
+
+    expect(controlledCheckbox.disabled).toBe(true);
+    expect(controlledCheckbox.getAttribute('aria-disabled')).toBe('true');
   });
 
-  test('onClick fires once when the label is clicked', () => {
-    render(<Checkbox className={className} onClick={onClick} />, { container });
-    fireEvent.click(renderedContainer);
-    expect(onClick.mock.calls.length).toBe(1);
+  // Test controlled checkbox events and checked state
+  describe('when controlled', () => {
+    const controlledOnClick = jest.fn();
+    const controlledOnChange = jest.fn();
+    render(
+      <Toggle
+        className={className}
+        onClick={controlledOnClick}
+        onChange={controlledOnChange}
+        checked={false}
+      />,
+      { container },
+    );
+    fireEvent.click(controlledContainer);
+
+    test('onClick fires once when the label is clicked', () => {
+      expect(controlledOnClick.mock.calls.length).toBe(1);
+    });
+
+    test('onChange fires once when the label is clicked', () => {
+      expect(controlledOnChange.mock.calls.length).toBe(1);
+    });
+
+    test('checkbox does not become checked when clicked', () => {
+      expect(controlledCheckbox.checked).toBe(false);
+    });
+  });
+
+  describe('when uncontrolled', () => {
+    const uncontrolledOnClick = jest.fn();
+    const uncontrolledOnChange = jest.fn();
+    const uncontrolledContainer = render(
+      <Toggle
+        onClick={uncontrolledOnClick}
+        onChange={uncontrolledOnChange}
+      />,
+    ).container.firstChild;
+    const uncontrolledCheckbox = uncontrolledContainer.children[0];
+
+    fireEvent.click(uncontrolledContainer.firstChild);
+
+    test('onClick fires once when the label is clicked', () => {
+      expect(uncontrolledOnClick.mock.calls.length).toBe(1);
+    });
+
+    test('onChange fires once when the label is clicked', () => {
+      expect(uncontrolledOnChange.mock.calls.length).toBe(1);
+    });
+
+    test('checkbox becomes checked when clicked', () => {
+      expect(uncontrolledCheckbox.checked).toBe(true);
+    });
   });
 });
--- packages/Toggle/src/Toggle.story.js
+++ packages/Toggle/src/Toggle.story.js
@@ -1,69 +1,37 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, boolean } from '@storybook/addon-knobs';
-import { emotion } from '@leafygreen-ui/lib';
-import { colors } from '@leafygreen-ui/theme';
-import Checkbox from '.';
+import { boolean, select } from '@storybook/addon-knobs';
+import { ccClassName, emotion } from '@leafygreen-ui/lib';
+import Toggle from '.';
 
-class Control extends PureComponent {
-  static propTypes = {
-    variant: PropTypes.oneOf(['default', 'light']),
-    checked: PropTypes.bool,
-    disabled: PropTypes.bool,
-    indeterminate: PropTypes.bool,
-    label: PropTypes.string,
-  };
-
-  state = { checked: false };
-
-  onChange = e => {
-    this.setState({ checked: e.target.checked });
-  };
-
-  render() {
-    const { indeterminate, label, disabled, variant = 'default' } = this.props;
-    const { checked } = this.state;
-
-    return (
-      <div
-        className={emotion.css`
-        background-color: ${
-          variant === 'light' ? colors.gray[1] : colors.gray[8]
-        };
-        padding: 20px;
-      `}
-      >
-        <Checkbox
-          checked={checked}
-          disabled={disabled}
-          indeterminate={indeterminate}
-          onChange={this.onChange}
-          label={label}
-          variant={variant}
-        />
-      </div>
-    );
-  }
-}
-
-storiesOf('Checkbox', module)
+storiesOf('Toggle', module)
   .add('Default', () => (
     <section className="storybook-container">
-      <Control
-        disabled={boolean('Disabled', false)}
-        indeterminate={boolean('Indeterminate', false)}
-        label={text('Label', 'I agree to this thing.')}
-      />
+      <Toggle
+        size={select(
+          'Size',
+          ['default', 'small', 'xsmall'],
+          'default',
+        )}
+        disabled={boolean('Disabled', false)} />
     </section>
   ))
-  .add('Light', () => (
-    <section className="storybook-container">
-      <Control
-        disabled={boolean('Disabled', false)}
-        indeterminate={boolean('Indeterminate', false)}
-        label={text('Label', 'I agree to this thing.')}
-        variant="light"
-      />
+  .add('Dark', () => (
+    <section
+      className={ccClassName(
+        'storybook-container',
+        emotion.css`
+          background-color: #464C4F;
+          padding: 2rem;
+        `,
+      )}>
+      <Toggle
+        size={select(
+          'Size',
+          ['default', 'small', 'xsmall'],
+          'default',
+        )}
+        variant='dark'
+        disabled={boolean('Disabled', false)} />
     </section>
   ));
