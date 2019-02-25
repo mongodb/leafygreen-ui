1495ff4d8685ae491b5c396d02beed3ea22e5970
brookescarlett <brookeyalof2017@u.northwestern.edu>
2019-02-25 16:12:11 -0500

    added light variant

--- packages/RadioGroup/src/RadioGroup.story.js
+++ packages/RadioGroup/src/RadioGroup.story.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, select, boolean } from '@storybook/addon-knobs';
-import { css } from 'react-emotion';
+import { text, boolean } from '@storybook/addon-knobs';
+import { emotion } from '@leafygreen-ui/lib';
+import { colors } from '@leafygreen-ui/theme';
 import RadioGroup from '.';
 import RadioButton from '../../RadioButton/src/index';
 
-storiesOf('RadioGroup', module).add('Default', () => (
-  <section className="storybook-container">
-    <RadioGroup>
-      <RadioButton value="option-1">Option One</RadioButton>
-      <RadioButton value="option-2">Option Two</RadioButton>
-      <RadioButton value="option-3">Option Three</RadioButton>
-      <RadioButton disabled value="option-4">
-        Disabled Option
-      </RadioButton>
-    </RadioGroup>
-  </section>
-));
+storiesOf('RadioGroup', module)
+    .add('Default', () => (
+        <section className="storybook-container">
+            <RadioGroup>
+                <RadioButton value="option-1">Option One</RadioButton>
+                <RadioButton value="option-2">
+                    {text('Label text', 'Option Two')}
+                </RadioButton>
+                <RadioButton value="option-3">Option Three</RadioButton>
+                <RadioButton disabled={boolean('Disabled', true)} value="option-4">
+                    Disabled Option
+                </RadioButton>
+            </RadioGroup>
+        </section>
+    ))
+    .add('Light', () => (
+        <section className="storybook-container">
+            <RadioGroup variant="light" className={emotion.css`
+                background-color: ${colors.gray[1]}
+            `}>
+                <RadioButton value="option-1">Option One</RadioButton>
+                <RadioButton value="option-2">
+                        {text('Label text', 'Option Two')}
+                </RadioButton>
+                <RadioButton value="option-3">Option Three</RadioButton>
+                <RadioButton disabled={boolean('Disabled', true)} value="option-4">
+                        Disabled Option
+                </RadioButton>
+            </RadioGroup>
+        </section>
+    ));
