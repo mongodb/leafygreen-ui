604f77d59d508c88cef8feae389567d3b66ea8fd
brookescarlett <brookeyalof2017@u.northwestern.edu>
2019-02-26 18:34:18 -0500

    pretty much working version of radio group

--- packages/RadioGroup/src/RadioGroup.js
+++ packages/RadioGroup/src/RadioGroup.js
@@ -48,22 +48,6 @@ export default class RadioGroup extends Component {
     updatedChildren: [],
   };
 
-  componentDidMount = () => {
-    const { children } = this.props;
-
-    const updatedChildren = React.Children.map(children, child => {
-      if (child.type !== RadioButton) {
-        return child;
-      }
-
-      return React.cloneElement(child, {
-        value: child.props.value || Math.floor(Math.random() * 10000000),
-      });
-    });
-
-    this.setState({ updatedChildren });
-  };
-
   handleChange = e => {
     const { onChange, value } = this.props;
 
@@ -80,13 +64,11 @@ export default class RadioGroup extends Component {
     const { children, name, className, variant } = this.props;
 
     const renderChildren = React.Children.map(
-      this.state.updatedChildren,
+      children,
       (child, index) => {
         if (child.type !== RadioButton) {
           return child;
         }
-
-        console.log(child.props.value, this.state.value);
 
         return React.cloneElement(child, {
           checked: this.state.value == child.props.value,
