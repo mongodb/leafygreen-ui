2fef57163a55df29a8ec8dd2896f5fff8dd6474b
brookescarlett <brookeyalof2017@u.northwestern.edu>
2019-02-27 14:52:14 -0500

    removed local dependencies, works in mms

--- packages/RadioGroup/src/RadioGroup.story.js
+++ packages/RadioGroup/src/RadioGroup.story.js
@@ -4,12 +4,13 @@ import { text, boolean } from '@storybook/addon-knobs';
 import { emotion } from '@leafygreen-ui/lib';
 import { colors } from '@leafygreen-ui/theme';
 import RadioGroup from '.';
-import RadioButton from '../../RadioButton/src/index';
+import RadioButton from '@leafygreen-ui/RadioButton'
+
 
 storiesOf('RadioGroup', module)
   .add('Default', () => (
     <section className="storybook-container">
-      <RadioGroup>
+      <RadioGroup name="radio-group-default">
         <RadioButton value="option-1">Option One</RadioButton>
         <RadioButton value="option-2">
           {text('Label text', 'Option Two')}
@@ -24,6 +25,7 @@ storiesOf('RadioGroup', module)
   .add('Light', () => (
     <section className="storybook-container">
       <RadioGroup
+        name="radio-group-light"
         variant="light"
         className={emotion.css`
                 background-color: ${colors.gray[1]}
