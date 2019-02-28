505acd7eb4a4da563d2c39a07af87f8344d7be87
brookescarlett <brookeyalof2017@u.northwestern.edu>
2019-02-28 10:08:12 -0500

    created rich radio button group package

---
+++ packages/RichRadioGroup/src/RichRadioGroup.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { colors } from '@leafygreen-ui/theme';
+import { ccClassName, emotion } from '@leafygreen-ui/lib';
+
+const { css } = emotion;
+
+export default class RichRadioGroup extends Component {
+  static displayName = 'RichRadioGroup';
+
+
+  render() {
+    
+    return (
+      <div>
+        HELLLOO
+      </div>
+    );
+  }
+}
