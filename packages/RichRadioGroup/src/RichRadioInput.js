ef2394da281ee08f615b558440b7d6268f9536f0
brookescarlett <brookeyalof2017@u.northwestern.edu>
2019-03-01 17:25:06 -0500

    process on rich radio group thus far, functionality and basic styling. working on sibling selectors

---
+++ packages/RichRadioGroup/src/RichRadioInput.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import * as style from './style.js';
+
+import { ccClassName, emotion } from '@leafygreen-ui/lib';
+const { css } = emotion;
+
+export default class RichRadioInput extends Component {
+  static displayName = 'RichRadioInput'
+
+  static defaultProps = {
+    onChange: () => {}, 
+    checked: false, 
+    disabled: false, 
+    className: '', 
+    variant: 'default', 
+  }
+
+  static propTypes = {
+    label: PropTypes.node.isRequired,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    onChange: PropTypes.func, 
+    checked: PropTypes.bool, 
+    disabled: PropTypes.bool, 
+    className: PropTypes.string, 
+    variant: PropTypes.oneOf(['default']), 
+    id: PropTypes.string,
+    name: PropTypes.string
+  }
+
+  render() {
+
+    const { 
+      label, 
+      className, 
+      onChange, 
+      value, 
+      checked, 
+      disabled, 
+      id, 
+      name, 
+      variant, 
+      ...rest
+    } = this.props
+
+    checked === true ? console.log('here') : null 
+
+    return(
+      <label
+        htmlFor={id} 
+        className={ccClassName(css`${style.baseLabelStyle}`)}>
+
+        <input 
+          {...rest}
+          type="radio"
+          id={id}
+          name={name}
+          onChange={onChange}
+          value={value}
+          checked={checked}
+          aria-checked={checked}
+          disabled={disabled}
+          aria-disabled={disabled}
+          className={ccClassName(css`${style.baseInputStyle}`)}
+        />
+
+        <div className={ccClassName(css`${style.baseTextStyle}`)}>
+          {label}
+        </div>
+      </label>
+    )
+  }
+}
