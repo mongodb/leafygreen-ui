867ad4df52cc8ad2079098196e624ccd26f4b549
brookescarlett <brookeyalof2017@u.northwestern.edu>
2019-03-04 16:47:05 -0500

    removed tight/full for now

--- packages/RichRadioGroup/src/style.js
+++ packages/RichRadioGroup/src/style.js
@@ -19,6 +19,8 @@ const smallWidth = 175;
 export const container = css`
   display: flex;
   margin-right: ${externalSpacing}px;
+  box-sizing: content-box !important;
+
 `;
 
 export const wrapperStyle = css`
@@ -28,7 +30,8 @@ export const wrapperStyle = css`
   border-radius: ${borderRadius}px;
   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
   cursor: pointer;
-  height: 48px;
+  height: 100%;
+  margin-right: ${externalSpacing}px;
 
   &:hover {
     border-color: ${colors.gray[3]};
@@ -44,7 +47,7 @@ export const baseInputStyle = css`
   &:checked + .${wrapperStyle} {
     border-radius: ${borderRadiusChecked}px;
     border: ${selectedBorderSize}px solid ${colors.green[2]};
-    transition: border-color 150ms ease-in-out;
+    transition: border-color 300ms ease-out;
     z-index: -1;
   }
 `;
@@ -57,123 +60,24 @@ export const baseTextStyle = css`
   font-weight: bold;
   padding: 16px 10px;
   margin-left: 5px;
+  box-sizing: content-box;
 `;
-// export const baseTextStyle = css`
-//   cursor: pointer;
-//   display: block;
-//   margin: 0;
-// text-align: center;
-// padding: 16px 10px;
-// font-size: 14px;
-// font-weight: bold;
-// text-align: center;
-//   position: relative;
-// `;
 
-// export const baseInputStyle = css`
-//   display: block;
-//   left: 0;
-//   top: 0;
-//   position: absolute;
-//   visibility: hidden;
-//   width: 100%;
-// `;
+export const richRadioVariants = {
+  green: css`
+    &:checked + .${wrapperStyle} {
+      border: 1px solid ${colors.green[2]};
+      background-color: rgba(196, 219, 179, 0.3); 
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1) inset;
 
-// export const wrapperStyle = css`
-//   padding: ${internalSpacing}px;
-//   border: ${defaultBorderSize}px solid ${colors.gray[5]};
-//   margin-left: ${borderDifference}px;
-//   margin-right: 12px;
-//   font-weight: normal;
-//   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
+      &:hover {
+        border-color: ${colors.green[2]};
+      }
+    }
+  `
+}
 
-//   position: relative;
-//   width: 60px;
-//   height: 60px;
-//   z-index: -1;
-// `;
-
-// export const baseLabelStyle = css`
-//   padding: 0;
-//   display: block;
-//   box-sizing: content-box;
-//   padding: ${internalSpacing}px;
-//   border: ${defaultBorderSize}px solid ${colors.gray[5]};
-//   margin-left: ${borderDifference}px;
-//   margin-right: 12px;
-//   font-weight: normal;
-//   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
-//   cursor: pointer;
-//   position: relative;
-//   transition: border-color 300ms ease-in;
-
-//   &:hover {
-//     border-color: ${colors.gray[3]};
-//     transition: border-color 150ms ease-in-out;
-
-//     .rich-radio-illustration-is-image {
-//       filter: none;
-//     }
-//   }
-// `;
-
-// export const checkedVariant = {
-//   checked: css`
-//     padding: ${internalSpacing}px;
-//     border-radius: ${borderRadiusChecked}px;
-//     border: ${selectedBorderSize}px solid ${colors.green[2]};
-//     margin-top: 0;
-//     margin-left: 0;
-//     box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.15);
-//     transition: border-color 300ms ease-out;
-
-//     &:hover {
-//       border-color: ${colors.green[2]};
-//     }
-//   `,
-// };
-
-// export const richRadioInputStyleVariants = {
-//   green: css`
-//     visibility: hidden;
-
-//     &:before {
-//       background: white;
-//       border-radius: 100%;
-//       border: 1px solid ${colors.green[2]};
-//       content: '';
-//       cursor: pointer;
-//       display: inline-block;
-//       height: 16px;
-//       left: 0;
-//       top: 0;
-//       vertical-align: top;
-//       visibility: visible;
-//       width: 16px;
-//     }
-
-//     &:checked {
-//       &:before {
-//         background-color: ${colors.green[2]};
-//         box-shadow: inset 0 0 0 2px white;
-//         border-width: 2px;
-//       }
-
-//       .${baseLabelStyle} {
-//         background-color: rgba(196, 219, 179, 0.3);
-//       }
-//     }
-
-//     &:focus {
-//       &:before {
-//         outline: none;
-//         border-color: white;
-//       }
-//     }
-//   `,
-// };
-
-export const richRadioInputSizeVaraints = {
+export const richSizedVariants = {
   tight: css`
     padding-right: 4px;
     padding-left: 4px;
@@ -182,7 +86,9 @@ export const richRadioInputSizeVaraints = {
   full: css`
     width: calc(100% - 36px);
   `,
+}
 
+export const richRadioInputSizeVaraints = {
   small: css`
     width: ${smallWidth - 2 * selectedBorderSize - 2 * internalSpacing}px;
     padding: ${internalSpacing - 5 * internalSpacing}px;
