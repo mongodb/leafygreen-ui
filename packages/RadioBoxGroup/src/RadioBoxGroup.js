97c3ff96877d910f78bb182d530e2aebd1e02dd4
brookescarlett <brookeyalof2017@u.northwestern.edu>
2019-03-06 17:05:46 -0500

    renamed component -> Radio Box Group and Radio Box

--- packages/RadioBoxGroup/src/RadioBoxGroup.js
+++ packages/RadioBoxGroup/src/RadioBoxGroup.js
@@ -46,13 +46,6 @@ export default class RadioBoxGroup extends Component {
       children,
       name = this.defaultName,
       className,
-<<<<<<< HEAD:packages/RadioBoxGroup/src/RadioBoxGroup.js
-<<<<<<< HEAD:packages/RadioBoxGroup/src/RadioBoxGroup.js
-=======
-      variant,
->>>>>>> passing tests, readME completed:packages/RichRadioGroup/src/RichRadioGroup.js
-=======
->>>>>>> removed large size; removed variant:packages/RichRadioGroup/src/RichRadioGroup.js
       size,
       value = this.state.value,
     } = this.props;
