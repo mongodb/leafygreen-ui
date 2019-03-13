8872360c495250f82ea37c68de42d5b81a7be2cf
brookescarlett <brookeyalof2017@u.northwestern.edu>
2019-03-13 14:01:49 -0400

    RadioBoxGroup component

---
+++ packages/RadioBoxGroup/src/RadioBoxGroup.js
@@ -0,0 +1,74 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import * as style from './style.js';
+import { ccClassName } from '@leafygreen-ui/lib';
+
+export default class RadioBoxGroup extends PureComponent {
+  static displayName = 'RadioBoxGroup';
+
+  static propTypes = {
+    children: PropTypes.node,
+    onChange: PropTypes.func,
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    size: PropTypes.oneOf(['tightContentBox', 'default', 'full']),
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+    onChange: () => {},
+    size: 'default',
+  };
+
+  state = {
+    value: '',
+  };
+
+  defaultName = `radio-box-group-${Math.floor(Math.random() * 1000000)}`;
+
+  handleChange = e => {
+    const { onChange, value } = this.props;
+
+    // Exposing both event and event.target.value, rather than just one or the other
+    // Stopped propagation to prevent event from bubbling with new target, and thus value coming back as undefined
+    if (onChange) {
+      e.stopPropagation();
+      onChange(e, e.target.value);
+    }
+
+    if (!value) {
+      this.setState({ value: e.target.value });
+    }
+  };
+
+  render() {
+    const {
+      children,
+      name = this.defaultName,
+      className,
+      size,
+      value = this.state.value,
+      ...rest
+    } = this.props;
+
+    const renderedChildren = React.Children.map(children, (child, index) => {
+      if (child.type.displayName !== 'RadioBox') {
+        return child;
+      }
+
+      return React.cloneElement(child, {
+        onChange: this.handleChange,
+        checked: value === child.props.value,
+        id: child.props.id || `${this.defaultName}-button-${index}`,
+        size,
+        name,
+      });
+    });
+
+    return (
+      <div {...rest} className={ccClassName(style.baseGroupStyle, className)}>
+        {renderedChildren}
+      </div>
+    );
+  }
+}
---
+++ packages/RadioBoxGroup/src/index.js
@@ -0,0 +1,4 @@
+import RadioBoxGroup from './RadioBoxGroup';
+import RadioBox from './RadioBox';
+
+export default { RadioBoxGroup, RadioBox };
