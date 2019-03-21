1e61a94145295a127467ea467487a24592895a78
Brooke Yalof <brooke.yalof@mongodb.com>
2019-03-21 11:30:34 -0400

    Portal component basics

---
+++ packages/Portal/src/Portal.spec.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import Portal from '.';
+
+afterAll(cleanup);
+
+describe('packages/Portal', () => {
+  const { container } = render(
+    <div>
+      Existing content on the DOM
+      <Portal>
+        <div>Content portaled to the end of the DOM</div>
+      </Portal>
+    </div>,
+  );
+
+  test(`appends portal to document body`, () => {
+    expect(document.body.firstChild.outerHTML).toBe(
+      '<div><div>Existing content on the DOM</div></div>',
+    );
+
+    expect(document.body.lastChild.outerHTML).toBe(
+      '<div><div>Content portaled to the end of the DOM</div></div>',
+    );
+  });
+
+  test(`appends portal to a custom node`, () => {
+    render(<div id="custom-container" />, { container });
+
+    render(
+      <Portal node={document.getElementById('custom-container')}>
+        Portaled to a custom node
+      </Portal>,
+      { container },
+    );
+
+    expect(document.getElementById('custom-container').outerHTML).toBe(
+      `<div id="custom-container">Portaled to a custom node</div>`,
+    );
+  });
+});
