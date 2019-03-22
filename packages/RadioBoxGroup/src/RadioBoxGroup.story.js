e9fb15c07a7b524f6021942934acc8b48f862cce
Brooke Yalof <brooke.yalof@mongodb.com>
2019-03-22 10:22:37 -0400

    fixed a zindex issue

--- packages/RadioBoxGroup/src/RadioBoxGroup.story.js
+++ packages/RadioBoxGroup/src/RadioBoxGroup.story.js
@@ -7,17 +7,14 @@ import RadioBox from './RadioBox';
 
 storiesOf('RadioBoxGroup', module).add('Default', () => (
   <section className="storybook-container">
-
-
     <RadioBoxGroup
       size={select('Size', ['tightContentBox', 'default', 'full'], 'default')}
-      >
+    >
       <RadioBox value="option-1">{text('Label text', 'Radio Box 1')}</RadioBox>
       <RadioBox value="option-2">Radio Box 2</RadioBox>
       <RadioBox value="option-3" disabled={boolean('Disabled', true)}>
         Radio Box 3
       </RadioBox>
     </RadioBoxGroup>
-  
   </section>
 ));
