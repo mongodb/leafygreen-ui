ee8ab0e68a31d36985f8ae546333b19461b46df1
Brooke Yalof <brooke.yalof@mongodb.com>
2019-03-25 16:07:15 -0400

    Responding to PR feedback

--- packages/Portal/src/Portal.story.js
+++ packages/Portal/src/Portal.story.js
@@ -6,45 +6,16 @@ import { colors } from '@leafygreen-ui/theme';
 
 const { css } = emotion;
 
-const triggeredStyle = css`
-  position: absolute;
-  top: 10px;
-  right: 20px;
-  padding: 5px;
-  background-color: ${colors.green[2]};
-  color: ${colors.mongodb.white};
-  text-align: right;
+const portalChildrenStyle = css`
+  text-align: center;
 `;
-
-class Control extends Component {
-  state = { on: false };
-
-  render() {
-    return (
-      <div>
-        <button onClick={() => this.setState({ on: !this.state.on })}>
-          Toggle Portal
-        </button>
-        {this.state.on && (
-          <Portal container={document.getElementById('root')}>
-            <div className={triggeredStyle}>
-              I have been portaled to the end of the DOM!
-            </div>
-          </Portal>
-        )}
-        <p>
-          Open the inspector to watch the DOM change when the Portal is toggled
-          on and off. When the Portal is active, a <code>div</code> is appended
-          to the end of the <code>documet.body</code> containing the Portal
-          components children.
-        </p>
-      </div>
-    );
-  }
-}
 
 storiesOf('Portal', module).add('Default', () => (
   <section className="storybook-container">
-    <Control />
+    <Portal>
+      <div className={portalChildrenStyle}>
+        Portals transport their children to a <code>div</code> that is appended to the end of the <code>documet.body</code> to or a <code>node</code> that can be specified with a <code>container</code> prop.
+      </div>
+    </Portal>
   </section>
 ));
