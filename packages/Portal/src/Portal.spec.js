9d49e442493e6118c22dea0d7bcd86374044a448
Brooke Yalof <brooke.yalof@mongodb.com>
2019-04-03 13:31:06 -0400

    minor fix

--- packages/Portal/src/Portal.spec.js
+++ packages/Portal/src/Portal.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, rerender, cleanup } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
 import Portal from '.';
 
 afterAll(cleanup);
