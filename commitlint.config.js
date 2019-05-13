5507c1770191adf2d98d44f886b8a96d6cd18f5a
Brooke Yalof <brooke.yalof@mongodb.com>
2019-05-13 15:48:04 -0400

    chore: added scope-linter

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,6 @@
-const lernaScopesConfig = require('@commitlint/config-lerna-scopes');
-
-const scopes = ['tooling', 'XX'];
-
-const getScopes = (initialEnum = []) => (ctx) =>
-  lernaScopesConfig.utils
-    .getPackages()
-    .then((packageList) => initialEnum.concat(packageList))
-    .then((scopeList) => [2, 'always', scopeList]);
-
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': (ctx) => getScopes(scopes)(ctx),
-  },
+  extends: [
+    '@commitlint/config-lerna-scopes',
+    '@commitlint/config-conventional',
+  ],
 };
-
