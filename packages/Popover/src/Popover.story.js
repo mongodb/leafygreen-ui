b10359e60dd6c061788a6cfa6c8bebbebfd5e2b7
Brooke Yalof <brooke.yalof@mongodb.com>
2019-05-20 13:33:27 -0400

    refactor: rebase to update popover component)

--- packages/Popover/src/Popover.story.js
+++ packages/Popover/src/Popover.story.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useRef } from 'react';
 import Popover from './Popover';
 import { storiesOf } from '@storybook/react';
@@ -10,19 +9,6 @@ const { css, cx } = emotion;
 
 const containerStyle = css`
   position: absolute;
-=======
-import React, { Component } from 'react';
-import Popover from './Popover.tsx';
-import { storiesOf } from '@storybook/react';
-import { select, boolean } from '@storybook/addon-knobs';
-import { emotion } from '@leafygreen-ui/lib';
-import { colors } from '@leafygreen-ui/theme';
-
-const { css } = emotion;
-
-const containerStyle = css`
-  position: relative;
->>>>>>> groundwork
 `;
 
 const popoverStyle = css`
@@ -32,7 +18,6 @@ const popoverStyle = css`
   background-color: ${colors.mongodb.white};
 `;
 
-<<<<<<< HEAD
 const referenceElPositions = {
   centered: css`
     position: relative;
@@ -123,34 +108,3 @@ function AdvancedExample() {
 storiesOf('Popover', module)
   .add('Default', () => <DefaultExample />)
   .add('Advanced', () => <AdvancedExample />);
-=======
-class Testing extends Component {
-  state = { active: false };
-  popoverTest = React.createRef();
-
-  render() {
-    return (
-      <div className={containerStyle}>
-        <button
-          ref={this.popoverTest}
-          onClick={() => this.setState({ active: !this.state.active })}
-        >
-          Popover
-        </button>
-
-        <Popover
-          align={select('Align', ['top', 'bottom', 'left', 'right'], 'bottom')}
-          justify={select('justify', ['start', 'middle', 'end'], 'start')}
-          refEl={this.popoverTest}
-          active={this.state.active}
-          withoutPortal={boolean('Without Portal', false)}
-        >
-          <div className={popoverStyle}>Popover content</div>
-        </Popover>
-      </div>
-    );
-  }
-}
-
-storiesOf('Popover', module).add('Default', () => <Testing />);
->>>>>>> groundwork
