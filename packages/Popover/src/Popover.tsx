423718fa4b1e8f8097bfb72d38370d0bdf2a753c
Brooke Yalof <brooke.yalof@mongodb.com>
2019-05-28 17:24:31 -0400

    chore: rebased with master

--- packages/Popover/src/Popover.tsx
+++ packages/Popover/src/Popover.tsx
@@ -1,7 +1,5 @@
 import React, {
   useMemo,
-  useLayoutEffect,
-  useState,
   Fragment,
   ReactNode,
   RefObject,
@@ -183,26 +181,9 @@ function Popover({
     adjustOnMutation,
   );
 
-  const memoedRefPosition = useMemo(
-    () => getElementPosition(referenceElement),
-    [referenceElement, viewportSize, lastTimeRefElMutated],
-  );
-
-  const memoedContentPosition = useMemo(
-    () => getElementPosition(contentRef.current),
-    [contentRef.current, viewportSize, lastTimeContentElMutated],
-  );
-
-  const [referenceElPos, setReferenceElPos] = useState();
-  const [contentElPos, setContentElPos] = useState();
-
-  useLayoutEffect(() => {
-    setReferenceElPos(memoedRefPosition);
-  }, [
+  const referenceElPos = useMemo(() => getElementPosition(referenceElement), [
     referenceElement,
-    contentRef.current,
     viewportSize,
-    lastTimeContentElMutated,
     lastTimeRefElMutated,
   ]);
 
@@ -210,22 +191,7 @@ function Popover({
     contentNode,
     viewportSize,
     lastTimeContentElMutated,
-    lastTimeRefElMutated,
   ]);
-
-  // function useReferenceElPos() {
-  //   const getReferenceElPos = useMemo(() => getElementPosition(referenceElement), [
-  //     referenceElement,
-  //     viewportSize,
-  //     lastTimeRefElMutated,
-  //   ]);
-
-  //   useLayoutEffect(() => {
-  //     setReferenceElPos(getReferenceElPos)
-  //   }, [referenceElement]);
-
-  //   return referenceElPos
-  // }
 
   const position = css(
     calculatePosition({
