c9043d9b709048a5d0534c40c5b261d47c2e1666
Brooke Yalof <brooke.yalof@mongodb.com>
2019-06-07 10:37:42 -0400

    chore: uppercasing letters in TSDocs

--- packages/hooks/src/useEventListener.ts
+++ packages/hooks/src/useEventListener.ts
@@ -12,10 +12,10 @@ interface UseEventOptions {
  * @param type Represents the event type to listen for.
  * @param eventCallback Event listener callback function.
  * @param optional Optional third argument passed to function with implementation specifications
- * @param optional.options parameter to specify options passed to the eventListener.
- * @param optional.dependencies array to be passed to useEffect hook, such that the hook will only run if the array's values have changed.
- * @param optional.enabled determines whether or not the useEffect hook should run.
- * @param optional.element value to be passed as target of event handler, will default to document.
+ * @param optional.options Parameter to specify options passed to the eventListener.
+ * @param optional.dependencies Array to be passed to useEffect hook, such that the hook will only run if the array's values have changed.
+ * @param optional.enabled Determines whether or not the useEffect hook should run.
+ * @param optional.element Value to be passed as target of event handler, will default to document.
  */
 export default function useEventListener(
   type: string,
