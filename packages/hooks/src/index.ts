f6e7539a4a33cedfc3036978e869d92650e30b41
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2019-06-10 16:18:55 -0400

    feat(hooks): implement custom hooks package (#75)
    * feat(hooks): created hooks package, to make custom hooks accesible
    
    * chore: split hooks into individual files
    
    * fix: cleaned useDocumentListener
    
    * chore: add tsdocs to each hook
    
    * chore: more tsdoc annotation
    
    * chore: working through test suite
    
    * chore: updated testing language
    
    * chore: updated TSDocs and useDoc API
    
    * chore: updated useElementNodeTests
    
    * fix: fixed broken tests
    
    * fix: readded debounce and updated tests
    
    * fix: added ts:emit script
    
    * fix: fixed useElementNode tests
    
    * fix: memoized eventCallback
    
    * fix: added enabled to dependencies
    
    * fix: responding to PR feedback
    
    * fix: added element option to useDocEventListener
    
    * docs: updated docs for useDoc
    
    * fix: responding to PR feedback
    
    * fix: fix useDocumentOptions
    
    * fix: rebased with master
    
    * fix: rebased with master
    
    * fix: almost fixed useEventListener hook
    
    * fix: a little cleaner
    
    * fix: fixed useEvent
    
    * chore: uppercasing letters in TSDocs
    
    * fix: responding to PR feedback
    
    * fix: added import pattern to Menu readme
    
    * fix: fix recent fix
    
    * chore: rebase with master

---
+++ packages/hooks/src/index.ts
@@ -0,0 +1,11 @@
+import useEventListener from './useEventListener';
+import useElementNode from './useElementNode';
+import useMutationObserver from './useMutationObserver';
+import useViewportSize from './useViewportSize';
+
+export {
+  useEventListener,
+  useElementNode,
+  useMutationObserver,
+  useViewportSize,
+};
---
+++ packages/hooks/src/useEventListener.ts
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from 'react';
+
+interface UseEventOptions {
+  options?: object;
+  dependencies?: Array<any>;
+  enabled?: boolean;
+  element?: Document | HTMLElement;
+}
+
+/**
+ * Hook to subscribe to an event listener.
+ * @param type Represents the event type to listen for.
+ * @param eventCallback Event listener callback function.
+ * @param optional Optional third argument passed to function with implementation specifications
+ * @param optional.options Parameter to specify options passed to the eventListener.
+ * @param optional.enabled Determines whether or not the useEffect hook should run.
+ * @param optional.dependencies Array to be passed to useEffect hook, such that the hook will only run if the array's values have changed.
+ * @param optional.element Value to be passed as target of event handler, will default to document.
+ */
+export default function useEventListener(
+  type: keyof GlobalEventHandlersEventMap,
+  eventCallback: (e) => void,
+  {
+    options = undefined,
+    enabled = true,
+    dependencies = [enabled, type],
+    element = document,
+  }: UseEventOptions = {},
+) {
+  const memoizedEventCallback = useRef(e => {}); //eslint-disable-line @typescript-eslint/no-unused-vars
+
+  useEffect(() => {
+    memoizedEventCallback.current = eventCallback;
+  }, [eventCallback]);
+
+  useEffect(() => {
+    if (!enabled) {
+      return;
+    }
+
+    const callback = e => memoizedEventCallback.current(e);
+
+    element.addEventListener(type, callback, options);
+
+    return () => {
+      document.removeEventListener(type, eventCallback, options);
+    };
+  }, dependencies);
+}
