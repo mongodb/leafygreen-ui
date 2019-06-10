c44dc79dd2f12507e2a6cda1f45fbed8a5d4b467
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2019-06-10 16:18:55 -0400

    feat(hooks): implement custom hooks package (#75)
    * feat(hooks): created hooks package, to make custom hooks accesible
    
    * chore: split hooks into individual files
    
    * fix: cleaned useDocumentListener
    
    * chore: add tsdocs to each hook
    
    * chore: more tsdoc annotation
    
    * chore: working through test suite
    
    * chore: updated testing language
    
    * chore: updated TSDocs and useDoc API
    
    * chore: updated useElementNodeTests
    
    * fix: fixed broken tests
    
    * fix: readded debounce and updated tests
    
    * fix: added ts:emit script
    
    * fix: fixed useElementNode tests
    
    * fix: memoized eventCallback
    
    * fix: added enabled to dependencies
    
    * fix: responding to PR feedback
    
    * fix: added element option to useDocEventListener
    
    * docs: updated docs for useDoc
    
    * fix: responding to PR feedback
    
    * fix: fix useDocumentOptions
    
    * fix: rebased with master
    
    * fix: rebased with master
    
    * fix: almost fixed useEventListener hook
    
    * fix: a little cleaner
    
    * fix: fixed useEvent
    
    * chore: uppercasing letters in TSDocs
    
    * fix: responding to PR feedback
    
    * fix: added import pattern to Menu readme
    
    * fix: fix recent fix
    
    * chore: rebase with master

---
+++ packages/hooks/src/useViewportSize.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import debounce from 'lodash/debounce';
+
+function getViewportSize() {
+  return {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+}
+
+export default function useViewportSize() {
+  const [viewportSize, setViewportUpdateVal] = useState(getViewportSize);
+
+  useEffect(() => {
+    const calcResize = debounce(
+      () => setViewportUpdateVal(getViewportSize()),
+      100,
+    );
+
+    window.addEventListener('resize', calcResize);
+
+    return () => window.removeEventListener('resize', calcResize);
+  }, []);
+
+  return viewportSize;
+}
