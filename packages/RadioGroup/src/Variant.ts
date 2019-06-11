751e7992815a444401e1af9b13518ab189e9ef4c
Jeremy Tice <jeremy.tice@10gen.com>
2019-06-11 16:29:23 -0400

    chore: convert enum to object

--- packages/RadioGroup/src/Variant.ts
+++ packages/RadioGroup/src/Variant.ts
@@ -1,6 +1,8 @@
-enum Variant {
-  Default = 'default',
-  Light = 'light',
-}
+const Variant = {
+  Default: 'default',
+  Light: 'light',
+} as const;
+
+type Variant = typeof Variant[keyof typeof Variant];
 
 export default Variant;
