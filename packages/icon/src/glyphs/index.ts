60fa40ee8d7c60d40675f24ce002b72a7ae6fc09
Dave McCarthy <dave.mccarthy@mongodb.com>
2019-06-21 16:01:37 -0400

    PD-319: Add new glyphs, and update existing glyphs with new styles (#101)
    * feat(icon): add new glyphs, and update existing glyphs  with new styles
    
    * chore(icon): run prettier
    
    * docs(icon): add new glyphs to readme

--- packages/icon/src/glyphs/index.ts
+++ packages/icon/src/glyphs/index.ts
@@ -5,6 +5,8 @@ import CaretUp from './CaretUp.svg';
 import CaretDown from './CaretDown.svg';
 import CaretRight from './CaretRight.svg';
 import CaretLeft from './CaretLeft.svg';
+import Checkmark from './Checkmark.svg';
+import CheckmarkWithCircle from './CheckmarkWithCircle.svg';
 import ChevronUp from './ChevronUp.svg';
 import ChevronDown from './ChevronDown.svg';
 import ChevronRight from './ChevronRight.svg';
@@ -14,19 +16,24 @@ import Cloud from './Cloud.svg';
 import CreditCard from './CreditCard.svg';
 import Edit from './Edit.svg';
 import Ellipsis from './Ellipsis.svg';
+import InfoWithCircle from './InfoWithCircle.svg';
 import Laptop from './Laptop.svg';
 import Lock from './Lock.svg';
+import MagnifyingGlass from './MagnifyingGlass.svg';
 import Menu from './Menu.svg';
+import NotAllowed from './NotAllowed.svg';
 import Person from './Person.svg';
 import PersonWithLock from './PersonWithLock.svg';
 import Plus from './Plus.svg';
 import PlusWithCircle from './PlusWithCircle.svg';
+import QuestionMarkWithCircle from './QuestionMarkWithCircle.svg';
 import Refresh from './Refresh.svg';
 import Save from './Save.svg';
 import Settings from './Settings.svg';
 import Stitch from './Stitch.svg';
 import Support from './Support.svg';
 import Trash from './Trash.svg';
+import Warning from './Warning.svg';
 import X from './X.svg';
 import XWithCircle from './XWithCircle.svg';
 
@@ -38,6 +45,8 @@ export default {
   CaretDown,
   CaretRight,
   CaretLeft,
+  Checkmark,
+  CheckmarkWithCircle,
   ChevronUp,
   ChevronDown,
   ChevronRight,
@@ -47,19 +56,24 @@ export default {
   CreditCard,
   Edit,
   Ellipsis,
+  InfoWithCircle,
   Laptop,
   Lock,
+  MagnifyingGlass,
   Menu,
+  NotAllowed,
   Person,
   PersonWithLock,
   Plus,
   PlusWithCircle,
+  QuestionMarkWithCircle,
   Refresh,
   Save,
   Settings,
   Stitch,
   Support,
   Trash,
+  Warning,
   X,
   XWithCircle,
 };
