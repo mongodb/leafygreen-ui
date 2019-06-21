6e83ee94a098808e284e69768266a0e1fc866ce6
Dave McCarthy <dave.mccarthy@mongodb.com>
2019-06-21 15:27:20 -0400

    feat(icon): add new glyphs, and update existing glyphs  with new styles

--- packages/icon/src/Icon.story.tsx
+++ packages/icon/src/Icon.story.tsx
@@ -5,7 +5,7 @@ import { css } from '@leafygreen-ui/emotion';
 import Icon, { glyphs, Size } from '.';
 
 const containerStyle = css`
-  width: 100px;
+  width: 150px;
   height: 70px;
   flex-shrink: 0;
   text-align: center;
@@ -24,15 +24,15 @@ const textStyle = css`
   margin-top: 0.5rem;
 `;
 
-storiesOf('Icons', module).add('Icon', () =>
-  (Object.keys(glyphs) as Array<keyof typeof glyphs>).map(glyph => (
-    <div key={glyph} className={containerStyle}>
-      <Icon
-        glyph={glyph}
-        fill={color('Fill', '#000000')}
-        size={select('size', Object.values(Size) as Array<Size>, Size.Default)}
-      />
-      <div className={textStyle}>{glyph}</div>
-    </div>
-  )),
-);
+const glyphList = Object.keys(glyphs) as Array<keyof typeof glyphs>
+
+storiesOf('Icons', module).add('Icon', () => glyphList.map(glyph => (
+  <div key={glyph} className={containerStyle}>
+    <Icon
+      glyph={glyph}
+      fill={color('Fill', '#000000')}
+      size={select('size', Object.values(Size) as Array<Size>, Size.Default)}
+    />
+    <div className={textStyle}>{glyph}</div>
+  </div>
+)));
