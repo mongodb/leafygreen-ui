60fbcf7e310e20ae5f83b531d315ac8ba0831b44
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2019-07-01 10:12:03 -0400

    feat(card): implement card component (#102)
    * chore: renamed packages
    
    * feat(card): implement card component
    
    * fix(card): respondign to PR feedback
    
    * fix(card): render in section html element
    
    * feat: addded border-radius
    
    * feat: added yarn error log to gitignore

---
+++ packages/card/src/Card.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css, cx } from '@leafygreen-ui/emotion';
+import { uiColors } from '@leafygreen-ui/palette';
+
+const containerStyle = css`
+  background-color: white;
+  border: 1px solid ${uiColors.gray.light2};
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px -4px ${uiColors.gray.light1};
+  transition: border 300ms ease-in-out, box-shadow 300ms ease-in-out;
+
+  &:hover {
+    border: 1px solid ${uiColors.gray.light2};
+    box-shadow: 0px 3px 6px -2px ${uiColors.gray.base};
+  }
+`;
+
+interface CardProps {
+  children: React.ReactNode;
+  as?: React.ElementType<any>;
+  className?: string;
+}
+
+function Card({ children, as = 'section', className, ...rest }: CardProps) {
+  const Root = as;
+
+  return (
+    <Root {...rest} className={cx(containerStyle, className)}>
+      {children}
+    </Root>
+  );
+}
+
+Card.displayName = 'Card';
+
+Card.propTypes = {
+  children: PropTypes.node,
+  as: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+  className: PropTypes.string,
+};
+
+export default Card;
