c86edfc5ed77ea21b918fc21bbc729bd176db8a7
Dave McCarthy <dave.mccarthy@mongodb.com>
2019-07-01 15:22:31 -0700

    test(icon): verify svg files are prepared for svgr

--- packages/icon/src/Icon.spec.tsx
+++ packages/icon/src/Icon.spec.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import path from 'path';
+import fs from 'fs';
+import { toJson } from 'xml2json';
 import { render, cleanup } from '@testing-library/react';
 import { typeIs } from '@leafygreen-ui/lib';
 import { createIconComponent } from '.';
 
 afterAll(cleanup);
+
+describe('packages/Icon/glyphs/', () => {
+  const glyphPaths = fs
+    .readdirSync(path.resolve(__dirname, './glyphs'))
+    /**
+     * './glyphs/' contains an index.ts file, so we filter out
+     * anything that's not an svg.
+     */
+    .filter(path => /.*\.svg$/.test(path));
+
+  glyphPaths.forEach(glyphPath => {
+    describe(`${glyphPath}`, () => {
+      const { svg } = require(path.resolve(__dirname, `./glyphs/${glyphPath}`));
+
+      if (!svg) {
+        return;
+      }
+
+      type SVGNodeObject = { readonly [K in string]: string | SVGNodeObject };
+
+      const rootGlyphObject: SVGNodeObject = toJson(svg, { object: true });
+
+      let isValid = true;
+
+      function processGlyphObject(obj: SVGNodeObject) {
+        Object.keys(obj).forEach(key => {
+          const currentValue = obj[key];
+
+          if (typeof currentValue === 'object') {
+            processGlyphObject(currentValue);
+          } else {
+            if (key === 'fill') {
+              const validFills = ['#000', '#000000', 'none'];
+
+              if (!validFills.includes(currentValue)) {
+                isValid = false;
+              }
+            }
+          }
+        });
+      }
+
+      processGlyphObject(rootGlyphObject);
+
+      test('all fills used in SVG files are "none", "#000", or "#000000"', () => {
+        expect(isValid).toBe(true);
+      });
+    });
+  });
+});
 
 const text = 'Hello world';
 
@@ -12,9 +65,7 @@ const MyGlyph: React.FC<React.SVGProps<SVGSVGElement>> = () => (
   <div>{text}</div>
 );
 
-const glyphs = {
-  MyGlyph,
-};
+const glyphs = { MyGlyph };
 
 describe('packages/Icon/createIconComponent', () => {
   const IconComponent = createIconComponent(glyphs);
