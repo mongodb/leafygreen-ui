dc63a74f0d87ab1ac5773ce249b719ad5f33e36f
Brooke Yalof <brooke.yalof@mongodb.com>
2019-08-07 10:01:25 -0400

    chore: updated examples in modal storybook

--- packages/modal/src/Modal.story.tsx
+++ packages/modal/src/Modal.story.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { select } from '@storybook/addon-knobs';
+import { css } from '@leafygreen-ui/emotion';
 import Modal, { ModalSize } from '.';
 
-function Example() {
+const scroll = css`
+  height: 200vh;
+`;
+
+function Default() {
   const [open, setOpen] = useState(false);
 
   return (
@@ -20,4 +25,44 @@ function Example() {
   );
 }
 
-storiesOf('Modal', module).add('Default', () => <Example />);
+function Scroll() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setOpen(!open)}>Open Modal</button>
+      <Modal
+        open={open}
+        setOpen={setOpen}
+        size={select('size', Object.values(ModalSize), ModalSize.Default)}
+      >
+        <div className={scroll}>Modal Content goes here.</div>
+      </Modal>
+    </>
+  );
+}
+
+function Interactive() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setOpen(!open)}>Open Modal</button>
+      <Modal
+        open={open}
+        setOpen={setOpen}
+        size={select('size', Object.values(ModalSize), ModalSize.Default)}
+      >
+        <div>
+          <p>Modal Content goes here.</p>
+          <button>Click me, I will not close the modal!</button>
+        </div>
+      </Modal>
+    </>
+  );
+}
+
+storiesOf('Modal', module)
+  .add('Default', () => <Default />)
+  .add('Scroll', () => <Scroll />)
+  .add('Interactive', () => <Interactive />);
