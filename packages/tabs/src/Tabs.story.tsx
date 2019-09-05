70ee2abceac17dc00f5cd3a268393f7ba879bc8c
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2019-09-05 16:08:24 -0400

    PD-311: Implement Tabs Component (#109)
    * chore: renamed packages
    
    * feat: implement tabs component
    
    * feat(tabs): implement tabs component
    
    * chore(tabs): fix controlled vs uncontrolled API
    
    * feat(tabs): fixed ts errors
    
    * fix: remove change to accidently edited file
    
    * docs: added docs
    
    * feat: added keyboard nav
    
    * feat(tabs): more tabs
    
    * feat: added focus state entering
    
    * feat: pulling out TabListitem
    
    * feat: working on tabs component
    
    * feat: starting tests
    
    * feat: focus state working, type errors exist
    
    * feat: tabs component working
    
    * chore: improving tests
    
    * feat(tabs): proofreading
    
    * feat(tabs): adding tsdocs
    
    * chore: cleaning
    
    * docs: updated docs
    
    * chore: fixed tests
    
    * chore: fixing tests
    
    * chore: responding to initial PR feedback
    
    * chore: responding to PR feedback
    
    * chore: responding to PR feedback
    
    * chore: fix where as prop lives
    
    * chore: update docs
    
    * chore: added test for as prop
    
    * chore: reformat docs
    
    * chore: proofreading
    
    * chore: reabsed with master
    
    * chore: updated sb and animation
    
    * chore: warn when value is used twice
    
    * chore: clean up TS docs
    
    * chore: responding to PR feedback
    
    * chore: responding to PR feedback
    
    * chore: update ux for border clicking
    
    * chore: working through links
    
    * chore: cleaning
    
    * chore: experimental
    
    * chore: removed commented code
    
    * docs: fixed docs
    
    * chore: cleaning up tabs
    
    * chore: cleaning docs
    
    * get tabs working with ts projects
    
    * yeah
    
    * chore: responding to PR feedback
    
    * chore: responding to PR feedback
    
    * chore: responding to PR feedback
    
    * chore: base refactor completed, slide broken
    
    * chore: fixed tests
    
    * chore: cleaning up and proofreading
    
    * chore: more proofreading
    
    * chore: responding to PR feedback
    
    * rebased with master
    
    * responding to PR feedback
    
    * minor bug fix
    
    * respondded to PR feedback
    
    * keyboard movement now works with semi controlled
    
    * copy changes
    
    * responding to PR feedback
    
    * rename active to selected in Tab and TabTitle
    
    * aliased selected and setselected
    
    * updated readme
    
    * adding tests
    
    * strengthened tests
    
    * cleaning readme
    
    * removing set timeout
    
    * rebased with master

---
+++ packages/tabs/src/Tabs.story.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean, text } from '@storybook/addon-knobs';
+import { Tab, Tabs } from './index';
+
+function ControlledTabs() {
+  const [selected, setSelected] = useState(0);
+
+  return (
+    <div>
+      <Tabs setSelected={setSelected} selected={selected}>
+        <Tab name={text('name', 'Harry Spencer Wolff')}>
+          {text('Tab Content', 'Hello 1')}
+        </Tab>
+        <Tab name="Jeremy Steven Tice">Hello 2</Tab>
+        <Tab name="Mark Frederick Truman">Hello 3</Tab>
+      </Tabs>
+    </div>
+  );
+}
+
+storiesOf('Tabs', module)
+  .add('Uncontrolled', () => (
+    <Tabs>
+      <Tab
+        default={boolean('default', true)}
+        name={text('name', 'Brooke Scarlett Yalof')}
+      >
+        {text('Tab Content', 'Hello 1')}
+      </Tab>
+      <Tab name="Robert Arnold Audroue">Hello 2</Tab>
+      <Tab disabled={boolean('disabled', true)} name="David Scott McCarthy">
+        Hello 3
+      </Tab>
+    </Tabs>
+  ))
+  .add('Controlled', () => <ControlledTabs />);
