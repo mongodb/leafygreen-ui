8f568bf9b06243b94bc24d0f1d2ad38da012ab2e
Dave McCarthy <dave.mccarthy@mongodb.com>
2019-09-24 11:59:38 -0400

    PD-325 : Add disabled + checked state to Checkbox component (#154)
    * add disabled / checked state to checkbox component
    
    * add changeset for checkbox update
    
    losslessly compress disabled+checked PNGs

--- packages/checkbox/src/img/index.ts
+++ packages/checkbox/src/img/index.ts
@@ -2,6 +2,8 @@ import spritesheetLight from './spritesheet-light.png';
 import spritesheetDark from './spritesheet-dark.png';
 import disabledLight from './disabled-light.png';
 import disabledDark from './disabled-dark.png';
+import disabledDarkChecked from './disabled-dark-checked.png';
+import disabledLightChecked from './disabled-light-checked.png';
 import indeterminateLight from './indeterminate-light.png';
 import indeterminateDark from './indeterminate-dark.png';
 
@@ -9,7 +11,9 @@ export {
   spritesheetLight,
   spritesheetDark,
   disabledLight,
+  disabledLightChecked,
   disabledDark,
+  disabledDarkChecked,
   indeterminateLight,
   indeterminateDark,
 };
