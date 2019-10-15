12c5b357331217791133ef755bb5f8d400bf1066
Brooke Yalof <brooke.yalof@mongodb.com>
2019-10-15 15:47:41 -0400

    nav item component created

--- packages/menu/src/Menu.spec.js
+++ packages/menu/src/Menu.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import 'jest-dom/extend-expect';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import { Menu, MenuGroup, MenuItem } from './index';
@@ -22,9 +21,13 @@ describe('packages/Menu', () => {
     </Menu>,
   );
 
+<<<<<<< HEAD
   const menu = getByTestId('test-menu');
 
   test('appears on DOM when open prop is set', () => {
+=======
+  test('Appears on DOM when open prop is set', () => {
+>>>>>>> nav item component created
     const menu = getByTestId('test-menu');
     expect(menu).toBeInTheDocument();
   });
@@ -60,29 +63,5 @@ describe('packages/Menu', () => {
 
       expect(menuItem).not.toBeVisible();
     });
-  });
-
-  describe('packages/MenuItem', () => {
-    test('fires onClick callback, when clicked', () => {
-      const menuItem = menu.firstChild.firstChild.firstChild;
-
-      act(() => {
-        fireEvent.click(menuItem);
-      });
-
-      setTimeout(() => {
-        expect(onClick).toHaveBeenCalledTimes(1);
-      }, 100);
-    });
-  });
-
-  test(`renders "${className}" in the DropdownItem container's classList`, () => {
-    const itemA = menu.firstChild.firstChild;
-    expect(itemA.classList.contains(className)).toBe(true);
-  });
-
-  test('Renders inside of an `a` instead of a `span` tag, when `href` prop is supplied', () => {
-    const itemB = menu.firstChild.children[1];
-    expect(itemB.tagName.toLowerCase()).toBe('a');
   });
 });
