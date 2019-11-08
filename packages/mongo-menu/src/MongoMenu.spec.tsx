234f9c444fcfc6b11d5973ef7c873214723fadf9
Matt Fairbrass <matt.fairbrass@gmail.com>
2019-11-08 13:39:54 +1100

    chore: Upgrade jest-dom to latest version (@testing-library/jest-dom). Updated import references across packages.

--- packages/mongo-menu/src/MongoMenu.spec.tsx
+++ packages/mongo-menu/src/MongoMenu.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import MongoMenu from '.';
 
