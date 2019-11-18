0a22b2bf1ce7d72e17bd1461805d0e88d9e2eb13
Brooke Yalof <brooke.yalof@mongodb.com>
2019-11-18 13:23:40 -0500

    Updates

--- vendors~main.ba15565f83b961a9e270.bundle.js
+++ vendors~main.ba15565f83b961a9e270.bundle.js
@@ -95,4 +95,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",function(){return isPlainObject})}]]);
-//# sourceMappingURL=vendors~main.406fd9ed4436dc4f7e5b.bundle.js.map
+//# sourceMappingURL=vendors~main.ba15565f83b961a9e270.bundle.js.map
