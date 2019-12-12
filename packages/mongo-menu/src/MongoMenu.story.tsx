0bfadcbd1ed692e16a49f41740621d45ab8ef4ef
Sean Hanson <smh@seanmhanson.com>
2019-12-12 15:25:50 -0500

    PD-473: Revert MongoMenu story

--- packages/mongo-menu/src/MongoMenu.story.tsx
+++ packages/mongo-menu/src/MongoMenu.story.tsx
@@ -3,19 +3,16 @@ import { storiesOf } from '@storybook/react';
 import { text, select } from '@storybook/addon-knobs';
 import MongoMenu, { Product } from '.';
 
-const user = {
-  name: text('name', 'Alex Smith'),
-  email: text('email', 'alex.smith@mongodb.com'),
-};
-
-const activeProduct = select(
-  'activeProduct',
-  Object.values(Product) as Array<Product>,
-  Product.Atlas,
-);
-
-storiesOf('MongoMenu', module)
-  .add('Default', () => <MongoMenu user={user} activeProduct={activeProduct} />)
-  .add('Disabled MongoDB Account link', () => (
-    <MongoMenu user={user} activeProduct={activeProduct} accountURL={''} />
-  ));
+storiesOf('MongoMenu', module).add('Default', () => (
+  <MongoMenu
+    user={{
+      name: text('name', 'Alex Smith'),
+      email: text('email', 'alex.smith@mongodb.com'),
+    }}
+    activeProduct={select(
+      'activeProduct',
+      Object.values(Product) as Array<Size>,
+      Product.Atlas,
+    )}
+  />
+));
