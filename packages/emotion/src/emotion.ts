655046b74b2bca61170425132cd2f7cdabb164bd
Dave McCarthy <dave.mccarthy@mongodb.com>
2019-12-31 16:04:01 -0500

    fix build to make components compatible in SSR applications

--- packages/emotion/src/emotion.ts
+++ packages/emotion/src/emotion.ts
@@ -1,20 +1,38 @@
 import createEmotion from 'create-emotion';
 
+// We're doing this here because doing it globally was proving problematic.
+// We should solve for this if we need to use __TARGET__ elsewhere in the future.
+declare const __TARGET__:
+  | 'web'
+  | 'node'
+  | 'async-node'
+  | 'electron-main'
+  | 'electron-renderer'
+  | 'electron-preload'
+  | 'node-webkit'
+  | 'webworker';
+
 // In case the original emotion, and create-emotion packages become unsupported,
 // we should consider implementing our own wrapper around createCache like what's
 // being done here:
 //
 // https://github.com/emotion-js/emotion/blob/master/packages/create-emotion/src/index.js
 function createEmotionInstance() {
-  const head = document.head;
-  const config = {
-    container: document.createElement('div'),
-    key: 'leafygreen-ui',
-  };
+  const key = 'leafygreen-ui';
 
-  head.insertBefore(config.container, head.firstChild);
+  if (__TARGET__ === 'web') {
+    const head = document.head;
+    const config = {
+      container: document.createElement('div'),
+      key,
+    };
 
-  return createEmotion(config);
+    head.insertBefore(config.container, head.firstChild);
+
+    return createEmotion(config);
+  }
+
+  return createEmotion({ key });
 }
 
 export default createEmotionInstance();
--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = ({ config, mode }) => {
-  const webpackConfig = require('../webpack.config.js')(mode);
+  const webpackConfig = require('../webpack.config.js')[0](mode);
   config.module.rules = webpackConfig.module.rules;
   config.resolve.extensions = webpackConfig.resolve.extensions;
 
