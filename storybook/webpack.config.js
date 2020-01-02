b8ce49d5af2e881c3c034ebe23b9b12c7e22a901
Dave McCarter <dave.mccarthy@mongodb.com>
2020-01-02 16:22:22 -0500

    Update storybook/webpack.config.js
    Co-Authored-By: Harry Wolff <hswolff@users.noreply.github.com>

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = ({ config, mode }) => {
+  // Use web version of webpack config
   const webpackConfig = require('../webpack.config.js')[0](mode);
   config.module.rules = webpackConfig.module.rules;
   config.resolve.extensions = webpackConfig.resolve.extensions;
