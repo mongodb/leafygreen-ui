05357d67c253de1f9b10bc93457e16b3f95f5c88
Dave McCarthy <dave.mccarthy@mongodb.com>
2020-01-02 12:37:14 -0500

    SSR improvements

--- packages/emotion/src/emotion.ts
+++ packages/emotion/src/emotion.ts
@@ -12,27 +12,29 @@ declare const __TARGET__:
   | 'node-webkit'
   | 'webworker';
 
+interface CreateEmotionConfig {
+  key: string;
+  container?: HTMLElement;
+}
+
 // In case the original emotion, and create-emotion packages become unsupported,
 // we should consider implementing our own wrapper around createCache like what's
 // being done here:
 //
 // https://github.com/emotion-js/emotion/blob/master/packages/create-emotion/src/index.js
 function createEmotionInstance() {
-  const key = 'leafygreen-ui';
+  const config: CreateEmotionConfig = {
+    key: 'leafygreen-ui',
+  };
 
   if (__TARGET__ === 'web') {
+    config.container = document.createElement('div');
+
     const head = document.head;
-    const config = {
-      container: document.createElement('div'),
-      key,
-    };
-
     head.insertBefore(config.container, head.firstChild);
-
-    return createEmotion(config);
   }
 
-  return createEmotion({ key });
+  return createEmotion(config);
 }
 
 export default createEmotionInstance();
