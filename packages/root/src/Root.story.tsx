35cc91d892915758d80bcade8008d8304a1b959b
Brooke Yalof <brooke.yalof@mongodb.com>
2020-01-23 18:34:14 -0500

    made storybook more dynamic

--- packages/root/src/Root.story.tsx
+++ packages/root/src/Root.story.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { text, select } from '@storybook/addon-knobs';
 import Root from '.';
 
-storiesOf('Root', module).add('Default', () => (
-  <Root href="https://google.com">test me out</Root>
-));
+storiesOf('Root', module)
+  .add('Anchor', () => (
+    <Root href="https://mongodb.design">
+      {text('Children', 'I am an anchor tag')}
+    </Root>
+  ))
+  .add('Custom', () => {
+    const CustomRoot = select(
+      'div',
+      { div: 'div', span: 'span', button: 'button' },
+      'div',
+    );
+
+    return (
+      <Root as={CustomRoot}>{text('Children', 'I am a custom element')}</Root>
+    );
+  });
