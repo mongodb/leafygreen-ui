2632743e739db96fb2a5c632a7770cf0b577a674
Brooke Yalof <brooke.yalof@mongodb.com>
2020-01-24 12:02:39 -0500

    added tsdocs

--- packages/root/src/Root.tsx
+++ packages/root/src/Root.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { HTMLElementProps } from '@leafygreen-ui/lib';
 
 type ButtonProps<T> = HTMLElementProps<'button'> &
@@ -28,7 +29,19 @@ function usesLinkElement<T>(props: RootProps<T>): props is Anchor<T> {
   return props.href != null;
 }
 
-export default function Root<T>(props: RootProps<T>) {
+/**
+ * # Root
+ *
+ * Root component
+ *
+ * ```
+<Root href="https://mongodb.design">Anchors Away!</Root>
+```
+ * @param props.children Takes priority in determining what tag the `<Root />` element will render.
+ * @param props.href Determines the location that an anchor tag will point to. If prop is set without the `as` prop, `<Root />` will render `a` tags.
+ * @param props.as Callback to change the open state of the Menu.
+ */
+function Root<T>(props: RootProps<T>) {
   const { children, ...rest } = props;
   let Root: React.ElementType<any> = 'button';
 
@@ -40,3 +53,13 @@ export default function Root<T>(props: RootProps<T>) {
 
   return <Root {...rest}>{children}</Root>;
 }
+
+Root.displayName = 'Root';
+
+Root.propTypes = {
+  children: PropTypes.node,
+  href: PropTypes.string,
+  as: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+};
+
+export default Root;
