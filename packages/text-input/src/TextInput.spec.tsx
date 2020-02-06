4c1593bb75dacf41207efb3007ed2d2bfe633b52
Domino Weir <domino.weir@mongodb.com>
2020-02-06 17:00:23 -0500

    structure TextInput in same style as Checkbox

--- packages/text-input/src/TextInput.spec.tsx
+++ packages/text-input/src/TextInput.spec.tsx
@@ -1,9 +1,43 @@
-// import React from 'react';
-import { /*render,*/ cleanup } from '@testing-library/react';
-// import TextInput from '.';
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import TextInput from './TextInput';
+import { typeIs } from '@leafygreen-ui/lib';
 
 afterAll(cleanup);
 
 describe('packages/text-input', () => {
-  test('condition', () => {});
+  const className = 'test-checkbox-class';
+  const { container } = render(<TextInput className={className} />);
+  const controlledContainer = container.firstChild;
+
+  if (!typeIs.element(controlledContainer)) {
+    throw new Error('Could not find controlled container component');
+  }
+
+  const controlledCheckbox = controlledContainer.children[0];
+
+  if (!typeIs.input(controlledCheckbox)) {
+    throw new Error('Could not find checkbox input element');
+  }
+
+  test(`renders "${className}" in the Checkbox label's classList`, () => {
+    expect(controlledContainer.classList.contains(className)).toBe(true);
+  });
+
+  test('Input is optional when prop is set', () => {
+    expect(controlledCheckbox.checked).toBe(false);
+    expect(controlledCheckbox.getAttribute('aria-checked')).toBe('false');
+
+    render(<TextInput optional={true} />, { container });
+
+    expect(controlledCheckbox.checked).toBe(true);
+    expect(controlledCheckbox.getAttribute('aria-checked')).toBe('true');
+  });
+
+  test('Input is disabled when disabled prop is set', () => {
+    render(<TextInput disabled={true} />, { container });
+
+    expect(controlledCheckbox.disabled).toBe(true);
+    expect(controlledCheckbox.getAttribute('aria-disabled')).toBe('true');
+  });
 });
--- packages/text-input/src/TextInput.tsx
+++ packages/text-input/src/TextInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@leafygreen-ui/emotion';
 // import Icon from '@leafygreen-ui/icon';
@@ -12,75 +12,85 @@ interface TextInputProps {
   errorMessage?: string;
 }
 
-const TextInput = React.forwardRef((props: TextInputProps, forwardRef) => {
-  const {
-    label = '',
-    description = '',
-    optional = false,
-    disabled = false,
-    placeholder = '',
-    errorMessage = '',
-  } = props;
+export default class TextInput extends PureComponent<
+  TextInputProps & React.InputHTMLAttributes<HTMLInputElement>
+> {
+  static displayName = 'TextInput';
 
-  const textInputStyle = css`
-    display: grid;
-  `;
+  static propTypes = {
+    label: PropTypes.string,
+    description: PropTypes.string,
+    optional: PropTypes.bool,
+    disabled: PropTypes.bool,
+    placeholder: PropTypes.string,
+    errorMessage: PropTypes.string,
+  };
 
-  const labelStyle = css`
-    color: #3d4f58;
-    font-size: 14px;
-    font-weight: bold;
-    height: 20px;
-  `;
+  static defaultProps = {
+    label: '',
+    description: '',
+    optional: false,
+    disabled: false,
+    placeholder: '',
+    errorMessage: '',
+  };
 
-  const descriptionStyle = css`
-    color: #5d6c74;
-    font-size: 14px;
-    height: ${description === '' ? '0' : '20'}px;
-  `;
+  render() {
+    const {
+      label,
+      description,
+      optional,
+      disabled,
+      placeholder,
+      errorMessage,
+    } = this.props;
 
-  const inputStyle = css`
-    width: 300px;
-    height: 36px;
-    border-radius: 4px;
-    border: 1px solid #ccc;
-    padding-left: 12px;
-  `;
+    const textInputStyle = css`
+      display: grid;
+    `;
 
-  const errorStyle = css`
-    color: #cf4a22;
-    font-size: 14px;
-    height: ${errorMessage === '' ? '0' : '20'}px;
-    padding-top: 6px;
-  `;
+    const labelStyle = css`
+      color: #3d4f58;
+      font-size: 14px;
+      font-weight: bold;
+      height: 20px;
+    `;
 
-  return (
-    <div className={textInputStyle}>
-      <label className={labelStyle}>{label}</label>
-      <label className={descriptionStyle}>{description}</label>
-      <input
-        className={inputStyle}
-        type="text"
-        required={!optional}
-        disabled={disabled}
-        placeholder={placeholder}
-      />
-      {/* <Icon glyph='Error'/> */}
-      <label className={errorStyle}>{errorMessage}</label>
-    </div>
-  );
-});
+    const descriptionStyle = css`
+      color: #5d6c74;
+      font-size: 14px;
+      height: ${description === '' ? '0' : '20'}px;
+    `;
 
-TextInput.displayName = 'TextInput';
+    const inputStyle = css`
+      width: 300px;
+      height: 36px;
+      border-radius: 4px;
+      border: 1px solid #ccc;
+      padding-left: 12px;
+    `;
 
-// @ts-ignore: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/37660
-TextInput.propTypes = {
-  label: PropTypes.string,
-  description: PropTypes.string,
-  optional: PropTypes.bool,
-  disabled: PropTypes.bool,
-  placeholder: PropTypes.string,
-  errorMessage: PropTypes.string,
-};
+    const errorStyle = css`
+      color: #cf4a22;
+      font-size: 14px;
+      height: ${errorMessage === '' ? '0' : '20'}px;
+      padding-top: 6px;
+    `;
 
-export default TextInput;
+    return (
+      <div className={textInputStyle}>
+        <label className={labelStyle}>{label}</label>
+        <label className={descriptionStyle}>{description}</label>
+        <input
+          className={inputStyle}
+          type="text"
+          required={!optional}
+          disabled={disabled}
+          placeholder={placeholder}
+        />
+        {/* <Icon glyph='Error'/> */}
+        <label className={errorStyle}>{errorMessage}</label>
+      </div>
+    );
+  }
+}
