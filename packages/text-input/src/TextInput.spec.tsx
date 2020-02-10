0725d2d0f3bd8ef10c2e63c035e364758ed8575f
Domino Weir <domino.weir@mongodb.com>
2020-02-10 17:34:11 -0500

    add key press test

--- packages/text-input/src/TextInput.spec.tsx
+++ packages/text-input/src/TextInput.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent, getByLabelText } from '@testing-library/react';
 import TextInput from './TextInput';
 import { typeIs } from '@leafygreen-ui/lib';
 
@@ -9,9 +9,11 @@ describe('packages/text-input', () => {
   const className = 'test-text-input-class';
   const label = 'Test Input Label';
   const description = 'This is the description';
+  const onFocus = jest.fn();
+  const onBlur = jest.fn();
 
   const renderedTextInput = render(
-    <TextInput className={className} label={label} description={description} />,
+    <TextInput className={className} label={label} description={description} onFocus={onFocus} onBlur={onBlur}/>,
   );
   const renderedChildren = renderedTextInput.container.firstChild;
 
@@ -24,5 +26,19 @@ describe('packages/text-input', () => {
     expect(renderedChildren.innerHTML).toContain(description);
   });
 
-  test('key presses are reflected in component state', () => {});
+  test('key presses are reflected in component state', () => {
+    const renderedInputElement = renderedChildren.children[2].children[0];
+    if (!typeIs.input(renderedInputElement)) {
+      throw new Error('Could not find input element');
+    }
+    expect(renderedInputElement.value).toBe('');
+    // fireEvent.click(renderedInputElement);
+    // fireEvent.keyDown(renderedInputElement, {key: 'a', code: 65, charCode: 65});
+    fireEvent.change(getByLabelText(renderedChildren, description), { target: { value: 'a' } })
+    expect(renderedInputElement.value).toBe('a');
+  });
+
+  test('key press is not recorded when field is disabled', () => {
+
+  }); 
 });
