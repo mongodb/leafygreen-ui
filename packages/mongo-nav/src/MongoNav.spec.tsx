4c16d51d398bd41d4422d617b12b1f58f2662c1b
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2020-02-11 15:44:39 -0500

    PD-497: API Request in MongoNav (#250)
    * add dev mode
    
    * bump menu deps
    
    * add docs
    
    * wip
    
    * dynamically working fetch requests
    
    * loading.tsx
    
    * doc update
    
    * removed console.log
    
    * make devMode data async too
    
    * PD-497
    
    * responding to PR feedback
    
    * responding to PR feedback
    
    * fixed tests, made them async, there is definitely a better way to do this but for now...
    
    * responding to PR feedback
    
    * responding to PR feedback, made fetch more robust
    
    * cleaned
    
    * even cleaner
    
    * update storybook
    
    * style changes
    
    * more style changes
    
    * fix spacing btw org/proj select trigger and menu
    
    * reorg
    
    * cleaned up mongoselect tests
    
    * responding to PR feedback

--- packages/mongo-nav/src/MongoNav.spec.tsx
+++ packages/mongo-nav/src/MongoNav.spec.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, wait } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import MongoNav from './MongoNav';
+
+const timeout = 200;
 
 afterAll(cleanup);
 
@@ -26,25 +28,37 @@ describe('packages/MongoNav', () => {
   );
 
   describe('by default', () => {
-    test('it renders the ProjectNav', () => {
+    test('it renders the ProjectNav', async () => {
+      await wait(() => getByTestId('project-nav'), {
+        timeout,
+      });
       const projectNav = getByTestId('project-nav');
       expect(projectNav).toBeInTheDocument();
     });
 
-    test('it renders admin as false', () => {
+    test('it renders admin as false', async () => {
+      await wait(() => getByTestId('project-nav'), {
+        timeout,
+      });
       const orgNav = getByTestId('organization-nav');
       expect(orgNav.innerHTML.includes('Admin')).toBe(false);
     });
   });
 
   describe('it successfully constructs urls based on hosts and urls props', () => {
-    test('specific url overrides take precedence over hosts, when the prop is set', () => {
+    test('specific url overrides take precedence over hosts, when the prop is set', async () => {
+      await wait(() => getByTestId('project-nav'), {
+        timeout,
+      });
       const accessManager = getByText('Access Manager');
       expect((accessManager as HTMLAnchorElement).href).toBe(
         'https://cloud.mongodb.com/access-manager-test',
       );
     });
-    test('host string changes default host, when the prop is set', () => {
+    test('host string changes default host, when the prop is set', async () => {
+      await wait(() => getByTestId('project-nav'), {
+        timeout,
+      });
       const support = getByText('Support');
       const billing = getByText('Billing');
 
@@ -57,8 +71,10 @@ describe('packages/MongoNav', () => {
       );
     });
   });
-
-  test('when mode prop is set to `dev`, fixture data is rendered inside of MongoNav', () => {
+  test('when mode prop is set to `dev`, fixture data is rendered inside of MongoNav', async () => {
+    await wait(() => getByTestId('project-nav'), {
+      timeout,
+    });
     const firstName = getByText('DevMode');
     expect(firstName).toBeInTheDocument();
   });
