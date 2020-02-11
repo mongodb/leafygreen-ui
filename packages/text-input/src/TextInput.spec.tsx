ec057ccd6d6d15ef61e1651abce31306c5d7d829
Domino Weir <domino.weir@mongodb.com>
2020-02-11 10:29:32 -0500

    add disabled input test

--- packages/text-input/src/TextInput.spec.tsx
+++ packages/text-input/src/TextInput.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup, fireEvent, getByLabelText } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import TextInput from './TextInput';
 import { typeIs } from '@leafygreen-ui/lib';
 
@@ -13,12 +13,23 @@ describe('packages/text-input', () => {
   const onBlur = jest.fn();
 
   const renderedTextInput = render(
-    <TextInput className={className} label={label} description={description} onFocus={onFocus} onBlur={onBlur}/>,
+    <TextInput
+      className={className}
+      label={label}
+      description={description}
+      onFocus={onFocus}
+      onBlur={onBlur}
+    />,
   );
   const renderedChildren = renderedTextInput.container.firstChild;
 
   if (!typeIs.element(renderedChildren)) {
     throw new Error('TextInput component failed to render');
+  }
+
+  const renderedInputElement = renderedChildren.children[2].children[0];
+  if (!typeIs.input(renderedInputElement)) {
+    throw new Error('Could not find input element');
   }
 
   test(`renders "${label}" as the input's label and "${description}" as the description`, () => {
@@ -27,18 +38,19 @@ describe('packages/text-input', () => {
   });
 
   test('key presses are reflected in component state', () => {
-    const renderedInputElement = renderedChildren.children[2].children[0];
-    if (!typeIs.input(renderedInputElement)) {
-      throw new Error('Could not find input element');
-    }
     expect(renderedInputElement.value).toBe('');
-    // fireEvent.click(renderedInputElement);
-    // fireEvent.keyDown(renderedInputElement, {key: 'a', code: 65, charCode: 65});
-    fireEvent.change(getByLabelText(renderedChildren, description), { target: { value: 'a' } })
+    fireEvent.change(renderedInputElement, {
+      target: { value: 'a' },
+    });
     expect(renderedInputElement.value).toBe('a');
   });
 
   test('key press is not recorded when field is disabled', () => {
-
-  }); 
+    renderedInputElement.setAttribute('disabled', 'true');
+    expect(renderedInputElement.value).toBe('a');
+    fireEvent.change(renderedInputElement, {
+      target: { value: 'a' },
+    });
+    expect(renderedInputElement.value).toBe('a');
+  });
 });
