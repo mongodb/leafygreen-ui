bc527ea1019961f219078c78feb2985f34846b17
Dave McCarthy <dave.mccarthy@mongodb.com>
2020-02-19 13:54:09 -0500

    remove webpack config

--- packages/syntax/src/types.ts
+++ packages/syntax/src/types.ts
@@ -5,8 +5,6 @@ export const Variant = {
 
 export type Variant = typeof Variant[keyof typeof Variant];
 
-// When adding additional language support, be sure to also add
-// the language in webpack.config.js
 export const SupportedLanguages = {
   JavaScript: 'javascript',
   TypeScript: 'typescript',
