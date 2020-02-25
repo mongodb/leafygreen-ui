d3a215f87f382ef597005e9aa9f9fb7d35b39ae8
Domino Weir <domino.weir@mongodb.com>
2020-02-25 14:49:20 -0500

    add wrapper styling to storybook example

--- packages/text-input/src/TextInput.story.tsx
+++ packages/text-input/src/TextInput.story.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, text, select } from '@storybook/addon-knobs';
+import { css } from '@leafygreen-ui/emotion';
 import TextInput from '.';
 
+const wrapperStyle = css`
+  width:400px;
+`;
+
 storiesOf('TextInput', module).add('Default', () => (
-  <TextInput
-    label={text('Label', 'Input Label')}
-    description={text('Description', 'This is a description for the input')}
-    optional={boolean('Optional', false)}
-    disabled={boolean('Disabled', false)}
-    placeholder={text('Placeholder Text', 'This is some placeholder text')}
-    state={select('State', ['none', 'valid', 'error'], 'none')}
-    errorMessage={text('Error Message', 'This is an error message')}
-  />
+  <div className={wrapperStyle}>
+    <TextInput
+      label={text('Label', 'Input Label')}
+      description={text('Description', 'This is a description for the input')}
+      optional={boolean('Optional', false)}
+      disabled={boolean('Disabled', false)}
+      placeholder={text('Placeholder Text', 'This is some placeholder text')}
+      state={select('State', ['none', 'valid', 'error'], 'none')}
+      errorMessage={text('Error Message', 'This is an error message')}
+    />
+  </div>
 ));
