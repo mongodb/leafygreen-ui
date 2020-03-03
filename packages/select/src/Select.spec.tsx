c6e4ca712d9be3ca188f08b30e6ea6bd90b393e3
Jeremy Tice <jeremy.tice@10gen.com>
2020-03-03 16:02:14 -0500

    Temporarily remove Select package to prepare for PR review

--- packages/select/src/Select.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import Select from '.';
-
-afterAll(cleanup);
-
-describe('packages/select', () => {
-  test('condition', () => {});
-});
--- packages/select/src/index.ts
+++
@@ -1,5 +0,0 @@
-import Select from './Select';
-import Option from './Option';
-
-export default Select;
-export { Option };
