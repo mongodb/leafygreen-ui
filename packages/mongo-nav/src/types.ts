ebb5102fdbf2c7a646f447f2f7686211e1899382
melissarudofsky <43755359+melissarudofsky@users.noreply.github.com>
2020-03-06 15:38:24 -0500

    Update university preferences link in UserMenu (#303)

--- packages/mongo-nav/src/types.ts
+++ packages/mongo-nav/src/types.ts
@@ -143,7 +143,7 @@ export interface URLSInterface {
       mfa: string;
     };
     university?: {
-      videoPreferences: string;
+      universityPreferences: string;
     };
     support?: {
       userPreferences: string;
