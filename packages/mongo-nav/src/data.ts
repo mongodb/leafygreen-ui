392f366e98cd34bcef74d192fe50941f3fdcaf6c
Brooke Yalof <brooke.yalof@mongodb.com>
2020-03-09 09:56:29 -0400

    revert data

--- packages/mongo-nav/src/data.ts
+++ packages/mongo-nav/src/data.ts
@@ -33,26 +33,6 @@ export const dataFixtures = {
       orgName: 'Demo Organization 2',
       planType: 'cloud',
     },
-    {
-      orgId: '5d729a93',
-      orgName: 'Demo Organization',
-      planType: 'atlas',
-    },
-    {
-      orgId: '5e0fa79',
-      orgName: 'Demo Organization 2',
-      planType: 'cloud',
-    },
-    {
-      orgId: '5d729a93',
-      orgName: 'Demo Organization',
-      planType: 'atlas',
-    },
-    {
-      orgId: '5e0fa79',
-      orgName: 'Demo Organization 2',
-      planType: 'cloud',
-    },
   ],
   projects: [
     {
