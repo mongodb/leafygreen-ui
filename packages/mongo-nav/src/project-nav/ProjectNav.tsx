3f7719e5d5e307925472060e902d1abb8d366c0c
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2020-03-09 15:05:58 -0400

    PD-561: Logic to determine when to display Realm/Stitch (#302)
    * realm or stitch logic
    
    * changeset
    
    * second tab name
    
    * another little fix
    
    * fix tests
    
    * fix tests

--- packages/mongo-nav/src/project-nav/ProjectNav.tsx
+++ packages/mongo-nav/src/project-nav/ProjectNav.tsx
@@ -198,6 +198,18 @@ const tooltipStyles = css`
   text-align: center;
 `;
 
+export function displayProductName(today = new Date(Date.now())) {
+  const mdbworld = new Date('May 4, 2020 0:00:00');
+
+  if (today >= mdbworld) {
+    return 'Realm';
+  }
+
+  return 'Stitch';
+}
+
+const secondTabName = displayProductName();
+
 interface ProjectNavInterface {
   current?: CurrentProjectInterface;
   data?: Array<ProjectInterface>;
@@ -358,7 +370,7 @@ export default function ProjectNav({
                   glyph="Stitch"
                 />
               )}
-              Realm
+              {secondTabName}
             </a>
           </li>
         )}
