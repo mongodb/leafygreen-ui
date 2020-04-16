211bc9742ea43cf2962e4d49038e01f30bdf601f
Dave McCarthy <dave.mccarthy@mongodb.com>
2020-04-16 15:45:09 -0400

    build script allows specifying paths to individual icons and a flag for the output directory

--- packages/icon/scripts/build.js
+++ packages/icon/scripts/build.js
@@ -2,69 +2,108 @@ const svgr = require('@svgr/core').default;
 const template = require('../src/template');
 const fs = require('fs');
 const path = require('path');
+const meow = require('meow')
 
-const glyphsDir = path.resolve(__dirname, '../src/glyphs');
-const svgFilePaths = fs
-  .readdirSync(glyphsDir)
-  .filter(fileName => fileName.includes('.svg'))
-  .map(fileName => ({
-    name: fileName.replace('.svg', ''),
-    path: path.resolve(glyphsDir, fileName),
-  }));
+const cli = meow(`
+	Usage
+		$ node ./build.js <filename(s)>
 
-svgFilePaths.forEach(file => {
-  try {
-    const svgCode = fs.readFileSync(file.path, { encoding: 'utf8' });
+	Options
+		--outDir, -o  Output directory for built SVG components
+`, {
+	flags: {
+		outDir: {
+			type: 'string',
+			alias: 'o',
+		},
+	},
+})
 
-    svgr(
-      svgCode,
-      {
-        titleProp: true,
-        svgProps: {
-          role: 'img',
-          viewBox: '0 0 16 16',
-        },
-        jsx: {
-          babelConfig: {
-            plugins: [
-              [
-                // This plugin lets us transform the JSX output to change instances of
-                // #000000 and #000 (the fill for our SVG glyphs) to use `this.props.fill` instead.
-                '@svgr/babel-plugin-replace-jsx-attribute-value',
-                {
-                  values: [
-                    {
-                      value: '#000',
-                      newValue: "'currentColor'",
-                      literal: true,
-                    },
-                    {
-                      value: '#000000',
-                      newValue: "'currentColor'",
-                      literal: true,
-                    },
-                  ],
-                },
-              ],
-            ],
-          },
-        },
-        template,
-      },
-      {
-        componentName: file.name,
-      },
-    ).then(moduleCode => {
-      const glyphsDistDir = path.resolve(__dirname, '../dist/test');
+function filterSvgFiles(str) {
+	return str.includes('.svg')
+}
 
-      if (!fs.existsSync(glyphsDistDir)) {
-        fs.mkdirSync(glyphsDistDir, { recursive: true });
-      }
+function buildSvgFiles(input, flags) {
+	let svgFiles;
 
-      const distFilePath = path.resolve(glyphsDistDir, `${file.name}.js`);
-      fs.writeFileSync(distFilePath, moduleCode);
-    });
-  } catch (err) {
-    console.error(err);
-  }
-});
+	if (input && input.length) {
+		svgFiles = input
+			.filter(filterSvgFiles)
+		  .map(filePath => {
+				const splitPath = filePath.split('/')
+				const fileName = splitPath[splitPath.length - 1].replace('.svg', '')
+
+				return {
+					name: fileName,
+					path: path.resolve(process.cwd(), filePath),
+				}
+			})
+	} else {
+		const glyphsDir = path.resolve(__dirname, '../src/glyphs');
+
+		svgFiles = fs
+			.readdirSync(glyphsDir)
+			.filter(filterSvgFiles)
+			.map(fileName => ({
+				name: fileName.replace('.svg', ''),
+				path: path.resolve(glyphsDir, fileName),
+			}));
+	}
+
+	svgFiles.forEach(file => {
+		svgr(
+			fs.readFileSync(file.path, { encoding: 'utf8' }),
+			{
+				titleProp: true,
+				svgProps: {
+					role: 'img',
+					viewBox: '0 0 16 16',
+				},
+				jsx: {
+					babelConfig: {
+						plugins: [
+							[
+								// This plugin lets us transform the JSX output to change instances of
+								// #000000 and #000 (the fill for our SVG glyphs) to use `this.props.fill` instead.
+								'@svgr/babel-plugin-replace-jsx-attribute-value',
+								{
+									values: [
+										{
+											value: '#000',
+											newValue: "'currentColor'",
+											literal: true,
+										},
+										{
+											value: '#000000',
+											newValue: "'currentColor'",
+											literal: true,
+										},
+									],
+								},
+							],
+						],
+					},
+				},
+				template,
+			},
+			{
+				componentName: file.name,
+			},
+		).then(moduleCode => {
+			let outputDir = path.resolve(__dirname, '../dist/test');
+
+			if (flags.outDir) {
+				outputDir = path.resolve(process.cwd(), flags.outDir)
+			}
+
+			if (!fs.existsSync(outputDir)) {
+				fs.mkdirSync(outputDir, { recursive: true });
+			}
+
+			fs.writeFileSync(path.resolve(outputDir, `${file.name}.js`), moduleCode);
+		})
+		.catch(console.error);
+	});
+}
+
+buildSvgFiles(cli.input, cli.flags)
