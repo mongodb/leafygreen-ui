24df09a1746faac51800dcad11431f767b726b0b
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2020-04-21 11:20:37 -0400

    Adds performance knob to storybook (#383)

--- storybook/config.js
+++ storybook/config.js
@@ -1,6 +1,7 @@
 import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
+import { withPerformance } from 'storybook-addon-performance';
 import ComponentPreview from './decorators/ComponentPreview';
 import ReactStrictMode from './decorators/ReactStrictMode';
 import theme from './theme';
@@ -17,6 +18,7 @@ addDecorator(ReactStrictMode);
 addDecorator(ComponentPreview);
 addDecorator(withKnobs);
 addDecorator(withA11y);
+addDecorator(withPerformance);
 
 // Dynamically load all stories found in the packages sub-directories (excluding node_modules) that
 // match the ".stories.js" extension
