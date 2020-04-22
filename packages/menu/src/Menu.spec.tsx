9b56e18f41696ad0019f7c0387dd457fa55ce144
Brooke Yalof <brooke.yalof@mongodb.com>
2020-04-22 16:54:23 -0400

    menu tests

--- packages/menu/src/Menu.spec.tsx
+++ packages/menu/src/Menu.spec.tsx
@@ -1,8 +1,138 @@
-// import '@testing-library/jest-dom/extend-expect'
-// import React from 'react'
-// import { render, fireEvent} from '@testing-library/react'
-// import { Menu, MenuSeparator, MenuItem, FocusableMenuItem, SubMenu };
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render, fireEvent, getByTestId } from '@testing-library/react';
+import { Menu, MenuSeparator, MenuItem, FocusableMenuItem, SubMenu } from '.';
 
-// function renderMenu(props) {
-//   const utils = render(<Menu></Menu>)
-// }
+const menuTestId = 'menu-test-id';
+const className = 'menu-item-class-name';
+const onClick = jest.fn();
+
+function renderMenu(props = {}) {
+  const utils = render(
+    <Menu {...props} data-testid={menuTestId}>
+      <MenuItem>Item A</MenuItem>
+      <MenuSeparator />
+      <MenuItem href="http://mongodb.design">Item B</MenuItem>
+    </Menu>,
+  );
+  return utils;
+}
+
+function renderMenuItem(props = {}) {
+  const utils = render(
+    <MenuItem {...props} data-testid="menu-item-test-id">
+      Item 1
+    </MenuItem>,
+  );
+  const menuItem = utils.getByTestId('menu-item-test-id');
+  return { ...utils, menuItem };
+}
+
+function renderSubMenuItem(props = {}) {
+  const utils = render(
+    <Menu open={true} setOpen={jest.fn()}>
+      <SubMenu title="First SubMenu" {...props}>
+        <MenuItem data-testid="sub-menu-item-a">SubMenu Item One</MenuItem>
+      </SubMenu>
+      <SubMenu title="Second SubMenu" data-testid="sub-menu-b">
+        <MenuItem data-testid="sub-menu-item-b">SubMenu Item Two</MenuItem>
+      </SubMenu>
+    </Menu>,
+  );
+  const subMenu = utils.getByText('First SubMenu');
+  return { subMenu, ...utils };
+}
+
+describe('packages/menu', () => {
+  test('menu appears on DOM when the "open" prop is set', () => {
+    const { getByTestId } = renderMenu({ open: true });
+    const menu = getByTestId(menuTestId);
+    expect(menu).toBeInTheDocument();
+  });
+
+  test('renders children to the DOM', () => {
+    const { getByText } = renderMenu({ open: true });
+    const menuItem = getByText('Item A');
+    expect(menuItem).toBeInTheDocument();
+  });
+
+  describe('when uncontrolled', () => {
+    const uncontrolledSetOpen = jest.fn();
+    const trigger = <button>trigger</button>;
+
+    test('and "setOpen" is provided, but "open" prop is not set', () => {
+      const { getByText } = renderMenu({
+        setOpen: uncontrolledSetOpen,
+        trigger,
+      });
+
+      const button = getByText('trigger');
+      fireEvent.click(button);
+
+      const menuItem = getByText('Item B');
+      expect(menuItem).toBeInTheDocument();
+
+      fireEvent.click(button);
+
+      expect(menuItem).not.toBeVisible();
+    });
+  });
+});
+
+describe('packages/menu/menu-item', () => {
+  test('fires onClick callback when clicked', () => {
+    const { menuItem } = renderMenuItem({ onClick });
+    fireEvent.click(menuItem);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  test(`renders "${className}" in the MenuItem container's classList`, () => {
+    const { menuItem } = renderMenuItem({ className });
+    expect(menuItem.classList.contains(className)).toBe(true);
+  });
+
+  test('renders inside of an `a` instead of a `button` tag, when `href` prop is supplied', () => {
+    const { menuItem } = renderMenuItem({ href: 'https://mongodb.design' });
+    expect(menuItem.tagName.toLowerCase()).toBe('a');
+  });
+
+  test('renders with correct target and rel values when set', () => {
+    const { menuItem } = renderMenuItem({
+      href: 'https://mongodb.design',
+      target: '_blank',
+      rel: 'help',
+    });
+
+    expect((menuItem as HTMLAnchorElement).target).toBe('_blank');
+    expect((menuItem as HTMLAnchorElement).rel).toBe('help');
+  });
+
+  test('renders as `div` tag when the as prop is set', () => {
+    const { menuItem } = renderMenuItem({ as: 'div' });
+    expect(menuItem.tagName.toLowerCase()).toBe('div');
+  });
+});
+
+describe('packages/menu/sub-menu', () => {
+  test('renders a SubMenu open by default, when the SubMenu is active', () => {
+    const { getByTestId } = renderSubMenuItem({ active: true });
+    const subMenuItem = getByTestId('sub-menu-item-a');
+    expect(subMenuItem).toBeInTheDocument();
+  });
+
+  test('when a SubMenu is clicked, it opens and closes the previously opened SubMenu', () => {
+    const { getByTestId } = renderSubMenuItem({ active: true });
+    const subMenuB = getByTestId('sub-menu-b');
+    const subMenuBArrow = subMenuB?.parentNode?.querySelectorAll('button')[1];
+
+    fireEvent.click(subMenuBArrow as HTMLElement);
+    const subMenuItemB = getByTestId('sub-menu-item-b');
+    expect(subMenuItemB).toBeVisible();
+  });
+
+  test('onClick is fired when SubMenu is clicked', () => {
+    const { subMenu } = renderSubMenuItem({ onClick });
+    fireEvent.click(subMenu);
+    expect(onClick).toHaveBeenCalled();
+  });
+});
