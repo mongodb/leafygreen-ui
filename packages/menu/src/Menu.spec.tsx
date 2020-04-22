a617c244d20af46a84b31bb18ecaf8a65348ce4b
Brooke Yalof <brooke.yalof@mongodb.com>
2020-04-22 13:37:55 -0400

    fixing tests p1

---
+++ packages/menu/src/Menu.spec.tsx
@@ -0,0 +1,8 @@
+// import '@testing-library/jest-dom/extend-expect'
+// import React from 'react'
+// import { render, fireEvent} from '@testing-library/react'
+// import { Menu, MenuSeparator, MenuItem, FocusableMenuItem, SubMenu };
+
+// function renderMenu(props) {
+//   const utils = render(<Menu></Menu>)
+// }
