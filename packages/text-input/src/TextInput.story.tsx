f0663039b849dff9e68f2118326369f07a2f9cb7
Brooke Yalof <brooke.yalof@mongodb.com>
2020-05-05 17:25:41 -0400

    tests

--- packages/text-input/src/TextInput.story.tsx
+++ packages/text-input/src/TextInput.story.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, text, select } from '@storybook/addon-knobs';
 import { css } from '@leafygreen-ui/emotion';
-import TextInput from '.';
+import TextInput, { TextInputType } from '.';
 
 const wrapperStyle = css`
   width: 400px;
@@ -17,6 +17,7 @@ storiesOf('TextInput', module).add('Default', () => (
       disabled={boolean('Disabled', false)}
       placeholder={text('Placeholder Text', 'This is some placeholder text')}
       state={select('State', ['none', 'valid', 'error'], 'none')}
+      type={select('Type', Object.values(TextInputType), TextInputType.Text)}
       errorMessage={text('Error Message', 'This is an error message')}
     />
   </div>
