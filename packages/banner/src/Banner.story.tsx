931448d305f2c5c200b33e50ce1725fdb7db6110
Brooke Yalof <brooke.yalof@mongodb.com>
2020-05-20 14:33:38 -0400

    add story styling

--- packages/banner/src/Banner.story.tsx
+++ packages/banner/src/Banner.story.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, text, select } from '@storybook/addon-knobs';
+import { css } from '@leafygreen-ui/emotion';
 import Banner, { Variant } from '.';
 
+const margin = css`
+  margin-left: 100px;
+  margin-right: 100px;
+`;
+
 storiesOf('Banner', module).add('Default', () => (
-  <Banner
-    variant={select('variant', Object.values(Variant), 'info')}
-    dismissable={boolean('dismissable', false)}
-  >
-    {text(
-      'Children',
-      'To avoid disrupting majority writes, new members are now added to replica sets as priority=0, votes=0 until they reach secondary state, after which Cloud Manager automatically updates the configuration to match the priority    and votes value specified in the deployment.',
-    )}
-  </Banner>
+  <div className={margin}>
+    <Banner
+      variant={select('variant', Object.values(Variant), 'info')}
+      dismissable={boolean('dismissable', false)}
+    >
+      {text(
+        'Children',
+        'To avoid disrupting majority writes, new members are now added to replica sets as priority=0, votes=0 until they reach secondary state, after which Cloud Manager automatically updates the configuration to match the priority    and votes value specified in the deployment.',
+      )}
+    </Banner>
+  </div>
 ));
