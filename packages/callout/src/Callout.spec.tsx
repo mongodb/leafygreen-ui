da8666e4026cf5d0e8db925a5b950fd2217d3aea
Michael Mitchell <michael.mitchell@mongodb.com>
2020-05-28 15:17:59 -0400

    [Callout] Add a new Callout package

---
+++ packages/callout/src/Callout.spec.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Callout from '.';
+
+describe('packages/callout', () => {
+  test('condition', () => {});
+});
