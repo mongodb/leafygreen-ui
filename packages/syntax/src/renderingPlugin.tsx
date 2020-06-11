d9c3af350f4096c0d8b271af493813f66d911008
Dave McCarthy <dave.mccarthy@mongodb.com>
2020-06-11 12:29:56 -0400

    checkin

--- packages/syntax/src/renderingPlugin.tsx
+++ packages/syntax/src/renderingPlugin.tsx
@@ -1,4 +1,4 @@
- import React from 'react';
+import React from 'react';
 import { css, cx } from '@leafygreen-ui/emotion';
 import { uiColors } from '@leafygreen-ui/palette';
 import { useVariant } from './CodeWrapper';
@@ -10,7 +10,9 @@ interface TokenProps {
 }
 
 function Token({ kind, children }: TokenProps) {
-  return <span className={kind ? `lg-highlight-${kind}` : ''}>{children}</span>;
+  const className = kind ? `lg-highlight-${kind}` : '';
+
+  return <span className={className}>{children}</span>;
 }
 
 type TreeItem =
@@ -67,79 +69,72 @@ interface LineTableRowProps {
 
 function LineTableRow({ lineNumber, children }: LineTableRowProps) {
   const variant = useVariant();
-
-  let numberColor: string = uiColors.gray.light1
-
-  if (variant === Variant.Dark) {
-    numberColor = uiColors.gray.dark1;
-  }
+  const numberColor =
+    uiColors.gray[variant === Variant.Dark ? 'dark1' : 'light1'];
 
   const numberCellStyle = css`
     padding-right: 24px;
     color: ${numberColor};
-  `
+  `;
+
   return (
     <tr>
-      <td
-        className={cx(
-          cellStyle,
-          numberCellStyle,
-        )}
-      >
-        {lineNumber}
-      </td>
+      <td className={cx(cellStyle, numberCellStyle)}>{lineNumber}</td>
       <td className={cellStyle}>{children}</td>
     </tr>
   );
+}
+
+function treeToLines(child: Array<string | TokenObject>): Array<Array<TreeItem>> {
+  const lines: Array<Array<TreeItem>> = [];
+  let currentLineIndex = 0;
+
+  // Create a new line, if no lines exist yet
+  if (lines[currentLineIndex] == null) {
+    lines[currentLineIndex] = [];
+  }
+
+  if (isString(child)) {
+    // If the current element is a string that includes a line break, we need to handle it differently
+    if (child.includes('\n')) {
+      child.split('').forEach(fragment => {
+        if (fragment === '\n') {
+          // If the fragment is a new line character, we create a new line
+          currentLineIndex++;
+          lines[currentLineIndex] = [];
+        } else {
+          const currentIndexInLine = lines[currentLineIndex].length - 1;
+
+          if (isString(lines[currentLineIndex][currentIndexInLine])) {
+            // If the last element in the line is a string, we append this string to it
+            lines[currentLineIndex][currentIndexInLine] += fragment;
+          } else {
+            // Otherwise, we push the string fragment on its own
+            lines[currentLineIndex].push(fragment);
+          }
+        }
+      });
+    } else {
+      // We don't need to do anything special in the case where the string doesn't contain a line break
+      lines[currentLineIndex].push(child);
+    }
+  }
+
+  // Line breaks aren't a part of token objects, so we can assume those objects go on the current line
+  if (isObject(child)) {
+    lines[currentLineIndex].push(child);
+  }
+
+  return lines
 }
 
 function renderTokenTreeToReact(
   { rootNode }: TokenTreeEmitter,
   numbered = false,
 ) {
-  const lines: Array<Array<TreeItem>> = [];
+  const lines = treeToLines(rootNode.children);
 
-  let currentLine = 0;
-
-  function treeToLines(child: string | TokenObject) {
-    // Create a new line, if no lines exist yet
-    if (lines[currentLine] == null) {
-      lines[currentLine] = [];
-    }
-
-    if (isString(child)) {
-      // If the current element is a string that includes a line break, we need to handle it differently
-      if (child.includes('\n')) {
-        child.split('').forEach(fragment => {
-          if (fragment === '\n') {
-            // If the fragment is a new line character, we create a new line
-            currentLine++;
-            lines[currentLine] = [];
-          } else {
-            const currentIndexInLine = lines[currentLine].length - 1;
-
-            if (isString(lines[currentLine][currentIndexInLine])) {
-              // If the last element in the line is a string, we append this string to it
-              lines[currentLine][currentIndexInLine] += fragment;
-            } else {
-              // Otherwise, we push the string fragment on its own
-              lines[currentLine].push(fragment);
-            }
-          }
-        });
-      } else {
-        // We don't need to do anything special in the case where the string doesn't contain a line break
-        lines[currentLine].push(child);
-      }
-    }
-
-    // Line breaks aren't a part of token objects, so we can assume those objects go on the current line
-    if (isObject(child)) {
-      lines[currentLine].push(child);
-    }
-  }
-
-  rootNode.children.forEach(treeToLines);
+  console.log(lines) // Returns Array with only one element - the desired array
 
   return lines.map((line, index) => {
     const content = line.map(processToken);
