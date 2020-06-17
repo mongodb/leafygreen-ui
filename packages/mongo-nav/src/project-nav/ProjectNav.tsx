98255fc71425983bfbfd486c71b121a1ceff81d8
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2020-06-17 16:03:06 -0400

    PD-677: Convert instances of Icon imports to import individual icons from dist (#440)
    * wip
    
    * wip
    
    * wip
    
    * remove console
    
    * small cleanuip
    
    * last instance of direct icon import
    
    * build
    
    * fix bug
    
    * fix broken test
    
    * responding to PR feedback
    
    * responding to PR feedback
    
    * responding to PR feedback
    
    * responding to PR feedback
    
    * responding to pr feedback
    
    * removed size imports
    
    * responding to PR feewdback
    
    * changesets

--- packages/mongo-nav/src/project-nav/ProjectNav.tsx
+++ packages/mongo-nav/src/project-nav/ProjectNav.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import Tooltip from '@leafygreen-ui/tooltip';
 import IconButton from '@leafygreen-ui/icon-button';
-import Icon from '@leafygreen-ui/icon';
+import EllipsisIcon from '@leafygreen-ui/icon/dist/Ellipsis';
+import InviteUserIcon from '@leafygreen-ui/icon/dist/InviteUser';
+import ActivityFeedIcon from '@leafygreen-ui/icon/dist/ActivityFeed';
+import BellIcon from '@leafygreen-ui/icon/dist/Bell';
 import { Menu, MenuItem } from '@leafygreen-ui/menu';
 import { createDataProp } from '@leafygreen-ui/lib';
 import { css, cx } from '@leafygreen-ui/emotion';
@@ -353,7 +356,7 @@ export default function ProjectNav({
             data-testid="project-nav-project-menu"
             onClick={onElementClick(ProjectNavProjectDropdown)}
           >
-            <Icon glyph="Ellipsis" className={menuIconStyle} />
+            <EllipsisIcon className={menuIconStyle} />
           </IconButton>
         }
       >
@@ -493,7 +496,7 @@ export default function ProjectNav({
                 data-testid="project-nav-invite"
                 onClick={onElementClick(ProjectNavInvite)}
               >
-                <Icon glyph="InviteUser" size="large" />
+                <InviteUserIcon size="large" />
               </IconButton>
             }
           >
@@ -518,7 +521,7 @@ export default function ProjectNav({
                 data-testid="project-nav-activity-feed"
                 onClick={onElementClick(ProjectNavActivityFeed)}
               >
-                <Icon glyph="ActivityFeed" size="large" />
+                <ActivityFeedIcon size="large" />
               </IconButton>
             }
           >
@@ -550,7 +553,7 @@ export default function ProjectNav({
                   </div>
                 )}
 
-                <Icon glyph="Bell" size="large" />
+                <BellIcon size="large" />
               </IconButton>
             }
           >
