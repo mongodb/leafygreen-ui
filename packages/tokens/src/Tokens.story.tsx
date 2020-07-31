2028ddb83e2850ff98ea90ed1bfd153306817608
Brooke Yalof <brooke.yalof@mongodb.com>
2020-07-31 15:33:02 -0400

    storybook updaet

--- packages/tokens/src/Tokens.story.tsx
+++ packages/tokens/src/Tokens.story.tsx
@@ -1,51 +1,80 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { css, cx } from '@leafygreen-ui/emotion';
+import { css } from '@leafygreen-ui/emotion';
 import { spacing } from '.';
 
 const gutter = css`
   margin-left: ${spacing[3]};
 `;
 
-const spacing1 = css`
-  background-color: #fb4949;
-  padding: ${spacing[1]};
+const textWrapper = css`
+  margin-bottom: ${spacing[2]};
+  font-size: 10px;
 `;
 
-const spacing2 = css`
-  background-color: #497ffb;
-  padding: ${spacing[2]};
-`;
+const spacingBlockVariants = {
+  1: css`
+    background-color: #fb4949;
+    width: ${spacing[1]};
+    height: ${spacing[1]};
+  `,
 
-const spacing3 = css`
-  background-color: #62e3fd;
-  padding: ${spacing[3]};
-`;
-const spacing4 = css`
-  background-color: #52c825;
-  padding: ${spacing[4]};
-`;
-const spacing5 = css`
-  background-color: #fdd063;
-  padding: ${spacing[5]};
-`;
-const spacing6 = css`
-  background-color: #fd7fec;
-  padding: ${spacing[6]};
-`;
-const spacing7 = css`
-  background-color: #a5fd8b;
-  padding: ${spacing[7]};
-`;
+  2: css`
+    background-color: #497ffb;
+    width: ${spacing[2]};
+    height: ${spacing[2]};
+  `,
+
+  3: css`
+    background-color: #62e3fd;
+    width: ${spacing[3]};
+    height: ${spacing[3]};
+  `,
+
+  4: css`
+    background-color: #52c825;
+    width: ${spacing[4]};
+    height: ${spacing[4]};
+  `,
+
+  5: css`
+    background-color: #fdd063;
+    width: ${spacing[5]};
+    height: ${spacing[5]};
+  `,
+
+  6: css`
+    background-color: #fd7fec;
+    width: ${spacing[6]};
+    height: ${spacing[6]};
+  `,
+
+  7: css`
+    background-color: #a5fd8b;
+    width: ${spacing[7]};
+    height: ${spacing[7]};
+  `,
+};
+
+function SpacingBlock({ space }: { space: string }) {
+  return (
+    <div className={gutter}>
+      <div
+        className={textWrapper}
+      >{`spacing[${space}]: ${spacing[space]}`}</div>
+      <div className={spacingBlockVariants[space]}></div>
+    </div>
+  );
+}
 
 storiesOf('Tokens', module).add('Spacing', () => (
-  <>
-    <div className={cx(spacing1, gutter)}>{spacing[1]}</div>
-    <div className={cx(spacing2, gutter)}>{spacing[2]}</div>
-    <div className={cx(spacing3, gutter)}>{spacing[3]}</div>
-    <div className={cx(spacing4, gutter)}>{spacing[4]}</div>
-    <div className={cx(spacing5, gutter)}>{spacing[5]}</div>
-    <div className={cx(spacing6, gutter)}>{spacing[6]}</div>
-    <div className={cx(spacing7, gutter)}>{spacing[7]}</div>
-  </>
+  <div
+    className={css`
+      display: flex;
+    `}
+  >
+    {Object.keys(spacing).map(space => (
+      <SpacingBlock space={space} key={space} />
+    ))}
+  </div>
 ));
