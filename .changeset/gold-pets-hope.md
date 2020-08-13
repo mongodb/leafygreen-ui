db9ed5aaf707954e5413d9dd2b82092bf1b7c4dc
Michael Mitchell <michael.mitchell@mongodb.com>
2020-08-13 12:35:22 -0400

    Make Card box shadow dark

---
+++ .changeset/gold-pets-hope.md
@@ -0,0 +1,7 @@
+---
+'@leafygreen-ui/card': patch
+'@leafygreen-ui/tokens': patch
+'@leafygreen-ui/tooltip': patch
+---
+
+Make Card box shadow darker
--- packages/card/src/Card.tsx
+++ packages/card/src/Card.tsx
@@ -8,7 +8,7 @@ const containerStyle = css`
   background-color: white;
   border: 1px solid ${uiColors.gray.light2};
   border-radius: 4px;
-  box-shadow: 0px 4px 10px -4px ${uiColors.gray.light1};
+  box-shadow: 0px 4px 10px -4px ${uiColors.gray.dark1};
   transition: border 300ms ease-in-out, box-shadow 300ms ease-in-out;
 
   &:hover {
