463a338e306667a9b6afbf42ed5d215bf0e3f362
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2020-08-20 11:34:55 -0400

    0.1.0 Table Release (#522)
    * before sort
    
    * basic table working
    
    * wip
    
    * wip
    
    * useReducer and useContext working
    
    * reducer now properly types
    
    * reducers now have types
    
    * progress with selectable
    
    * small cleanup
    
    * border height fixed, dataTypes added
    
    * known bugs worked out
    
    * more cleanup
    
    * cleanup
    
    * responding to PR feedback
    
    * wip responding to pr feedback
    
    * responding
    
    * responding to PR feedback
    
    * change child arg interface name
    
    * smarter accessor prop default
    
    * starting the test suite...
    
    * more cleanup, more tests
    
    * more tests
    
    * working through tests some more
    
    * add more test coverage
    
    * brb
    
    * test suite
    
    * test cleanup
    
    * responding to PR feedback
    
    * everything working except checkbox
    
    * serious cleanup, but needs mroe
    
    * fix infinite render issue
    
    * checkmark fix and selectable table tests
    
    * responding to more pr feedback
    
    * cleanup
    
    * trying to figure out nodeRef
    
    * remove sticky logic
    
    * infer data shape
    
    * remove types from storybook
    
    * remove demo data interface
    
    * responding to PR feedback
    
    * fix another bgu
    
    * more pr feedback
    
    * wip
    
    * closes nested rows when parent is open
    
    * trying to clean up
    
    * trying to fix accessibility issues
    
    * responding to PR feedback
    
    * fix ts error in submenu
    
    * responding to initial PR feedback
    
    * responding to more PR feedback
    
    * clean a little
    
    * refactor sort
    
    * light cleanup
    
    * solve for keys
    
    * clean a map
    
    * clean
    
    * huge checkbox logic refactor
    
    * add disabled useEffect
    
    * step closer
    
    * more cleanup of prop rename converging
    
    * wip
    
    * test cleanup
    
    * fix table checkmark bug
    
    * clean up messy border surrounding disabled
    
    * rm unnecc var
    
    * fix failing test
    
    * responding to PR feedback
    
    * responding to PR feedback
    
    * pr feedback
    
    * removes centered component
    
    * more pr feedback
    
    * clean up selectable logic
    
    * responding to PR feedback
    
    * wip
    
    * tiny bug
    
    * fixed bug, but its hacky
    
    * clean
    
    * necessary accessibility changes to tab that were preventing linting changes from passing
    
    * add back disabled prop
    
    * add iconbutton changeset
    
    * responded ot michaels pr feedback
    
    * in progress, but don't want to lose this state
    
    * tiny perf updates
    
    * perf updaets
    
    * midway pause
    
    * latest
    
    * more performant, but remind to remove linter
    
    * wip
    
    * fix tests
    
    * remove whyd you render
    
    * beginning of a refactor, why does table-head need to subscribe to state
    
    * quick rename
    
    * cleanup
    
    * table without selectable API
    
    * remove junit
    
    * remove why did you render
    
    * clean up table context
    
    * add readme
    
    * more config stuf
    
    * smaller fixes
    
    * cleanup
    
    * fix ts errors
    
    * update
    
    * merged and resolved
    
    * resolve ts error
    
    * upgrade to idallocator
    
    * Delete {
    
    * responding to initial PR feedback
    
    * responding to more pr feedback
    
    * rm console
    
    * fix introduced ts error
    
    * wip
    
    * greatfix
    
    * icon button weird
    
    * remove comments
    
    * responding to Pr feedback
    
    * make sure table updates when data does
    
    * simplify column API
    
    * change icons and push chevron left
    
    * re-add support for fragments

---
+++ packages/table/src/styles.tsx
@@ -0,0 +1,11 @@
+import { css } from '@leafygreen-ui/emotion';
+
+export const commonCellStyles = css`
+  min-width: 40px;
+  max-width: 144px;
+  padding-left: 8px;
+  padding-right: 8px;
+  box-style: border-box;
+  vertical-align: middle;
+  font-size: 14px;
+`;
