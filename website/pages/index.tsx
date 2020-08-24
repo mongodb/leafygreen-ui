873900aef1fcc4f9fda276a3e21045b23b46ac0c
Brooke Yalof <brooke.yalof@mongodb.com>
2020-08-24 17:56:56 -0400

    got this working i think with yarn workspaces

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Body } from '@leafygreen-ui/typography';
+import Banner from '@leafygreen-ui/banner';
 
 export default function Home() {
   return (
-    <Body>
+    <Banner>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry&#39;s standard dummy text ever since the
       1500s, when an unknown printer took a galley of type and scrambled it to
       make a type specimen book.
-    </Body>
+    </Banner>
   );
 }
