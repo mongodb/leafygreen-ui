e9707e60b732ee907e68323e6c89fc127cffccc3
Brooke Yalof <brooke.yalof@mongodb.com>
2020-08-24 13:04:06 -0400

    wip

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,12 +1,12 @@
-import Code from '@leafygreen-ui/code';
+import { Body } from '@leafygreen-ui/typography';
 
 export default function Home() {
   return (
-    <Code language="javascript">
+    <Body>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry&#39;s standard dummy text ever since the
       1500s, when an unknown printer took a galley of type and scrambled it to
       make a type specimen book.
-    </Code>
+    </Body>
   );
 }
