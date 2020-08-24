9400d7483d203399a74fe2bd2bbbdc30c69b76b5
Brooke Yalof <brooke.yalof@mongodb.com>
2020-08-24 18:36:36 -0400

    further cleanup

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,12 +1,11 @@
-import Banner from '@leafygreen-ui/banner';
+import { Body } from '@leafygreen-ui/typography';
+import MegaphoneIcon from '@leafygreen-ui/icon/dist/Megaphone';
 
 export default function Home() {
   return (
-    <Banner>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry&#39;s standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book.
-    </Banner>
+    <Body>
+      <MegaphoneIcon />
+      Coming Soon!
+    </Body>
   );
 }
