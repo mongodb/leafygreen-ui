87930bd008ceb778b5ed9090a46cf409bedc6943
Brooke Yalof <brooke.yalof@mongodb.com>
2020-08-25 08:05:40 -0400

    ignore

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,11 +1,5 @@
 import { Body } from '@leafygreen-ui/typography';
-import MegaphoneIcon from '@leafygreen-ui/icon/dist/Megaphone';
 
 export default function Home() {
-  return (
-    <Body>
-      <MegaphoneIcon />
-      Coming Soon!
-    </Body>
-  );
+  return <Body>Coming Soon!</Body>;
 }
