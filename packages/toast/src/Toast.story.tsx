3518f8147bbdfd6945b29c150c143afc5eedeaee
Dave McCarthy <dave.mccarthy@mongodb.com>
2020-08-26 14:16:14 -0400

    finished variants?

--- packages/toast/src/Toast.story.tsx
+++ packages/toast/src/Toast.story.tsx
@@ -1,9 +1,27 @@
-
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { number, select, text, boolean } from '@storybook/addon-knobs';
 import Toast from '.';
 
-storiesOf('Toast', module)
-  .add('Default', () => (
-    <Toast variant='success' body='hello' open={true} />
-  ))
+storiesOf('Toast', module).add('Default', () => {
+  const variant = select('Variant', ['progress', 'success'], 'success');
+  const knobs = {
+    variant,
+    progress:
+      variant === 'progress'
+        ? number('Progress', 1, { range: true, min: 0, max: 1 })
+        : undefined,
+    title: text(
+      'Title',
+      'Exercitation incididunt ea proident velit mollit',
+    ),
+    body: text(
+      'Body',
+      'Velit ea exercitation qui aute dolor proident.',
+    ),
+    open: boolean('Open', true),
+    close: boolean('Close', false) ? () => {} : undefined,
+  };
+
+  return <Toast {...knobs} />;
+});
--- packages/toast/src/Toast.tsx
+++ packages/toast/src/Toast.tsx
@@ -1,22 +1,86 @@
 import React, { useRef } from 'react';
 import { Transition } from 'react-transition-group';
 import { transparentize } from 'polished';
-import { cx, css } from '@leafygreen-ui/emotion';
+import { cx, css, keyframes } from '@leafygreen-ui/emotion';
 import Portal from '@leafygreen-ui/portal';
 import { uiColors } from '@leafygreen-ui/palette';
 import { spacing } from '@leafygreen-ui/tokens';
 import { Body } from '@leafygreen-ui/typography';
 import IconButton from '@leafygreen-ui/icon-button';
 import CheckmarkWithCircleIcon from '@leafygreen-ui/icon/dist/CheckmarkWithCircle';
+import RefreshIcon from '@leafygreen-ui/icon/dist/Refresh';
 import XIcon from '@leafygreen-ui/icon/dist/X';
+
+const progressBackgroundBase = '#22B7EB';
+const toastWidth = 400;
+
+const progressBarBackgroundStyle = css`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 6px;
+  background-color: ${uiColors.gray.light2};
+  border-radius: 0 0 4px 4px;
+  overflow: hidden;
+`;
+
+const backgroundShimmer = keyframes`
+  0% {
+    background-position: ${-toastWidth}px;
+  }
+
+  100% {
+    background-position: ${toastWidth * 2}px;
+  }
+`;
+
+const progressBarStyle = css`
+  overflow: hidden;
+  height: 6px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  background-color: ${progressBackgroundBase};
+  background-image: linear-gradient(
+    90deg,
+    ${progressBackgroundBase} 0px,
+    #cce8f4 ${toastWidth / 2}px,
+    ${progressBackgroundBase} ${toastWidth}px
+  );
+  background-size: 600px;
+  animation: ${backgroundShimmer} 4s infinite linear;
+  transition: width 0.3s ease-in-out;
+`;
 
 interface ProgressBarProps {
   progress: number;
 }
 
 function ProgressBar({ progress }: ProgressBarProps) {
-  return <div>test</div>;
+  return (
+    <div className={progressBarBackgroundStyle}>
+      <div
+        className={cx(
+          progressBarStyle,
+          css`
+            width: ${progress * 100}%;
+          `,
+        )}
+      />
+    </div>
+  );
 }
+
+const RTGStates = {
+  Entering: 'entering',
+  Entered: 'entered',
+  Exiting: 'exiting',
+  Exited: 'exited',
+} as const;
+
+type RTGStates = typeof RTGStates[keyof typeof RTGStates];
 
 const ToastVariants = {
   success: 'success',
@@ -25,29 +89,41 @@ const ToastVariants = {
 
 type ToastVariants = typeof ToastVariants[keyof typeof ToastVariants];
 
+const transitionDuration = 150;
+
 const baseToastStyle = css`
   position: fixed;
   bottom: ${spacing[6]};
   left: ${spacing[4]};
-  display: flex;
-  align-items: center;
-  width: 400px;
+  width: ${toastWidth}px;
   max-width: calc(100vw - (${spacing[4]} * 2));
-  padding: ${spacing[3]};
   border-radius: 4px;
   box-shadow: 0 18px 18px -15px ${transparentize(0.7, uiColors.black)};
+  overflow: hidden;
+  transform: translate3d(0, ${spacing[3]}, 0) scale(0.95);
+  transform-origin: bottom center;
+  opacity: 0;
+  transition: all ${transitionDuration}ms ease-in-out;
 `;
 
 const toastVariantStyles: Record<ToastVariants, string> = {
   [ToastVariants.success]: css`
     background-color: ${uiColors.green.light3};
-    border: 1px solid ${uiColors.green.light2};
   `,
 
   [ToastVariants.progress]: css`
-    background-color: ${uiColors.green.light3};
-    border: 1px solid ${uiColors.green.light2};
+    background-color: ${uiColors.white};
+    padding-bottom: 6px;
   `,
+};
+
+const toastVisibleStateStyle = css`
+  transform: translate3d(0, 0, 0) scale(1);
+  opacity: 1;
+`;
+
+const toastTransitionStateStyles: Partial<Record<RTGStates, string>> = {
+  [RTGStates.Entered]: toastVisibleStateStyle,
 };
 
 const baseIconStyle = css`
@@ -65,6 +141,25 @@ const iconVariantStyles: Record<ToastVariants, string> = {
   `,
 };
 
+const contentWrapperStyles = css`
+  display: flex;
+  align-items: center;
+  padding: ${spacing[3]};
+`;
+
+const contentWrapperVariantStyles: Record<ToastVariants, string> = {
+  [ToastVariants.success]: css`
+    border-radius: 4px;
+    border: 1px solid ${uiColors.green.light2};
+  `,
+
+  [ToastVariants.progress]: css`
+    border-radius: 4px 4px 0 0;
+    border: 1px solid ${uiColors.blue.light2};
+    border-bottom: 0;
+  `,
+};
+
 const bodyVariantStyles: Record<ToastVariants, string> = {
   [ToastVariants.success]: css`
     color: ${uiColors.green.dark2};
@@ -73,6 +168,29 @@ const bodyVariantStyles: Record<ToastVariants, string> = {
   [ToastVariants.progress]: css`
     color: ${uiColors.gray.dark2};
   `,
+};
+
+const dismissButtonStyle = css`
+  position: absolute;
+  top: ${spacing[2]};
+  right: ${spacing[2]};
+  transition: color 0.15s ease-in-out;
+`;
+
+const dismissButtonVariantStyle = {
+  [ToastVariants.success]: css`
+    color: ${uiColors.green.base};
+
+    &:hover {
+      color: ${uiColors.green.dark2};
+
+      &:before {
+        background-color: ${uiColors.green.light2};
+      }
+    }
+  `,
+
+  [ToastVariants.progress]: '',
 };
 
 interface ToastProps {
@@ -97,27 +215,67 @@ export default function Toast({
   const nodeRef = useRef(null);
   const dismissible = !!close;
 
+  let VariantIcon: React.ComponentType<any>;
+
+  if (variant === ToastVariants.progress) {
+    VariantIcon = RefreshIcon;
+  } else if (variant === ToastVariants.success) {
+    VariantIcon = CheckmarkWithCircleIcon;
+  }
+
   return (
     <Transition
       in={open}
-      timeout={150}
+      timeout={transitionDuration}
       mountOnEnter
       unmountOnExit
       nodeRef={nodeRef}
     >
-      {(state: string) => (
+      {(state: RTGStates) => (
         <Portal>
           <div
             role="status"
             ref={nodeRef}
-            className={cx(baseToastStyle, toastVariantStyles[variant], className)}
+            className={cx(
+              baseToastStyle,
+              toastVariantStyles[variant],
+              toastTransitionStateStyles[state],
+              className,
+            )}
           >
-            <CheckmarkWithCircleIcon className={cx(baseIconStyle, iconVariantStyles[variant])} size={30} />
+            <div
+              className={cx(
+                contentWrapperStyles,
+                contentWrapperVariantStyles[variant],
+                {
+                  [css`
+                    padding-right: calc(${spacing[3]} + ${spacing[2]});
+                  `]: dismissible,
+                },
+              )}
+            >
+              <VariantIcon
+                className={cx(baseIconStyle, iconVariantStyles[variant])}
+                size={30}
+              />
 
-            <Body className={bodyVariantStyles[variant]}>{body}</Body>
+              <div>
+                {title && (
+                  <Body className={cx(bodyVariantStyles[variant], css`font-weight: bold;`)}>{title}</Body>
+                )}
+
+                <Body className={bodyVariantStyles[variant]}>{body}</Body>
+              </div>
+            </div>
 
             {dismissible && (
-              <IconButton aria-label="Close">
+              <IconButton
+                className={cx(
+                  dismissButtonStyle,
+                  dismissButtonVariantStyle[variant],
+                )}
+                aria-label="Close Message"
+              >
                 <XIcon />
               </IconButton>
             )}
