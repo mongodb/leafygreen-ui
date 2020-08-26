9dc20dac345c4c92d72eb4013a9b00e93d2e7503
Brooke Yalof <brooke.yalof@mongodb.com>
2020-08-26 09:16:53 -0400

    changeset

---
+++ .changeset/long-zebras-brake.md
@@ -0,0 +1,5 @@
+---
+'@leafygreen-ui/syntax': patch
+---
+
+Explicitly sets font-family for syntax component to Source Code Pro
---
+++ packages/tokens/src/fonts.ts
@@ -0,0 +1,6 @@
+const fonts = {
+  default: `Akzidenz, 'Helvetica Neue', Helvetica, Arial, sans-serif`,
+  code: `'Source Code Pro', monospace`,
+};
+
+export default fonts;
