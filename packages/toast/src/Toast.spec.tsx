402600b930ca29acbb6dbb9410ce6adf899275a2
Dave McCarthy <dave.mccarthy@mongodb.com>
2020-08-31 14:29:25 -0400

    check-in

--- packages/toast/src/Toast.spec.tsx
+++ packages/toast/src/Toast.spec.tsx
@@ -2,6 +2,70 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Toast from '.';
 
+// interface ToastProps extends Omit<React.ComponentProps<'div'>, 'title'> {
+//   /**
+//    * Optional text shown in bold above the body text.
+//    */
+//   title?: React.ReactNode;
+
+//   /**
+//    * Required main text for the Toast.
+//    */
+//   body: React.ReactNode;
+
+//   /**
+//    * Optional className passed to the wrapping <div /> for the toast.
+//    */
+//   className?: string;
+
+//   /**
+//    * Required style variant to render the Toast as.
+//    */
+//   variant: ToastVariants;
+
+//   /**
+//    * Optional number between 0 and 1 that sets the progress bar's progress. Note that the progress bar is only rendered when the Toast variant is set to `'progress'`.
+//    *
+//    * **Default:** `1`
+//    */
+//   progress?: number;
+
+//   /**
+//    * Optional boolean that renders the Toast and makes it visible when true.
+//    *
+//    * **Default:** `false`
+//    */
+//   open?: boolean;
+
+//   /**
+//    * Optional click event handler that, when set, renders a close button that receives the passed handler.
+//    */
+//   close?: React.MouseEventHandler;
+// }
+
 describe('packages/toast', () => {
+
+  // open prop
+  // it renders when true
+  // it doesn't when false
+  // it doesn't by default
+
+  // close prop
+  // it renders close button when passed a function
+  // close button doesn't render when undefined
+  // close button doesn't render when incorrect type is passed
+
+  // progress prop
+  // when progress variant
+  //  it renders when value is 0
+  //  it renders when value is 0.5
+  // when other variant
+  //   it doesn't render when value is 0
+  //   it doesn't render when value is 0.5
+
+  //
+
+
+
   test('condition', () => {});
 });
