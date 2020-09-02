aa22bce4571d11ca6a1d483561813c442b91b06c
Brooke Yalof <brooke.yalof@mongodb.com>
2020-09-02 13:24:01 -0400

    should auto deploy with Realm

---
+++ website/hosting/files/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/next/dist/pages/_error":["static\u002Fchunks\u002Fpages\u002Fnext\u002Fdist\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ website/hosting/files/_next/static/wws33eNjTNd381mqs6N0Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-620bfa076c4004f74e31.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-981104e12fae00297e61.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
