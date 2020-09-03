f1b0fff2fd724bbbd1d4edc423434338deccd099
Michael Mitchell <michael.mitchell@mongodb.com>
2020-09-02 22:56:32 -0400

    Include SVG change

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import svgr from '@svgr/rollup';
 import { terser } from 'rollup-plugin-terser';
 import urlPlugin from '@rollup/plugin-url';
+
+const extensions = ['.ts', '.tsx'];
+
+const { name } = require(path.resolve(process.cwd(), 'package.json'));
 
 function getAllPackages(dir) {
   const dirList = fs.readdirSync(dir);
@@ -14,6 +17,18 @@ function getAllPackages(dir) {
     .map(subDir => `${path.resolve(dir, subDir)}/package.json`)
     .filter(packageJsonPath => fs.existsSync(packageJsonPath))
     .map(packageJsonPath => require(packageJsonPath).name);
+}
+
+function getLodashExternals() {
+  return fs
+    .readdirSync(path.resolve(__dirname, 'node_modules', 'lodash'))
+    .filter(fileName => {
+      const splitName = fileName.split('.');
+      const extension = splitName[splitName.length - 1];
+
+      return extension === 'js';
+    })
+    .map(fileName => 'lodash/' + fileName.split('.')[0]);
 }
 
 function getDirectGlyphImports() {
@@ -27,9 +42,18 @@ function getDirectGlyphImports() {
     );
 }
 
-const extensions = ['.ts', '.tsx'];
+function getGeneratedFiles() {
+  const directory = path.resolve(process.cwd(), 'src/generated');
 
-const { name } = require(path.resolve(process.cwd(), 'package.json'));
+  if (!fs.existsSync(directory)) {
+    return [];
+  }
+
+  return fs
+    .readdirSync(directory)
+    .filter(file => /\.tsx?$/.test(file))
+    .map(file => path.resolve(directory, file));
+}
 
 const allPackages = getAllPackages('../../packages');
 const directGlyphImports = getDirectGlyphImports();
@@ -65,70 +89,85 @@ directGlyphImports.forEach(glyphImport => {
   globals[glyphImport] = /[^/]+$/.exec(glyphImport)[0];
 });
 
-const config = ['esm', 'umd'].map(format => ({
-  input: 'src/index.ts',
-  output: {
-    dir: `dist/${format}`,
-    name,
-    // exports: 'named',
+const moduleFormatToDirectory = {
+  esm: 'dist/esm/',
+  umd: 'dist',
+};
 
-    format,
-    sourcemap: true,
-    globals,
-  },
-  plugins: [
-    resolve({ extensions }),
+const config = ['esm', 'umd'].flatMap(format => {
+  const baseConfig = {
+    input: 'src/index.ts',
+    output: {
+      dir: moduleFormatToDirectory[format],
+      name,
 
-    babel({
-      babelrc: false,
-      babelHelpers: 'bundled',
-      extensions,
-      configFile: path.resolve(__dirname, 'babel.config.js'),
-      sourceMaps: 'inline',
-      envName: 'production',
-    }),
+      format,
+      sourcemap: true,
+      globals,
+    },
+    plugins: [
+      resolve({ extensions }),
 
-    commonjs({
-      include: /node_modules/,
-    }),
+      babel({
+        babelrc: false,
+        babelHelpers: 'bundled',
+        extensions,
+        configFile: path.resolve(__dirname, 'babel.config.js'),
+        sourceMaps: 'inline',
+        envName: 'production',
+      }),
 
-    urlPlugin({
-      limit: 50000,
-      include: ['**/*.png'],
-    }),
+      urlPlugin({
+        limit: 50000,
+        include: ['**/*.png'],
+      }),
 
-    urlPlugin({
-      limit: 0,
-      include: ['**/*.less'],
-      fileName: '[name][extname]',
-    }),
+      urlPlugin({
+        limit: 0,
+        include: ['**/*.less'],
+        fileName: '[name][extname]',
+      }),
 
-    svgr(),
+      svgr(),
 
-    terser(),
-  ],
-  external: id =>
-    [
-      'clipboard',
-      'highlight.js',
-      'highlightjs-graphql',
-      'react',
-      'react-dom',
-      'emotion',
-      'lodash',
-      'create-emotion',
-      'create-emotion-server',
-      'polished',
-      'prop-types',
-      'react-transition-group',
-      '@testing-library/react',
-      ...allPackages,
-      ...directGlyphImports,
-    ].includes(id) ||
-    // We test if an import includes lodash to avoid having
-    // to whitelist every nested lodash module individually
-    /^lodash\//.test(id) ||
-    /^highlight\.js\//.test(id),
-}));
+      terser(),
+    ],
+    external: id =>
+      [
+        'clipboard',
+        'highlight.js',
+        'highlightjs-graphql',
+        'react',
+        'react-dom',
+        'emotion',
+        'create-emotion',
+        'create-emotion-server',
+        'polished',
+        'prop-types',
+        'react-transition-group',
+        '@testing-library/react',
+        'lodash',
+        ...getLodashExternals(),
+        ...allPackages,
+        ...directGlyphImports,
+      ].includes(id) ||
+      // We test if an import includes lodash to avoid having
+      // to whitelist every nested lodash module individually
+      /^lodash\//.test(id) ||
+      /^highlight\.js\//.test(id),
+  };
+
+  return [
+    baseConfig,
+    ...getGeneratedFiles().map(input => ({
+      ...baseConfig,
+      input: `src/generated/${path.basename(input)}`,
+      output: {
+        ...baseConfig.output,
+        name: `${path.basename(input, path.extname(input))}.js`,
+      },
+    })),
+  ];
+});
 
 export default config;
