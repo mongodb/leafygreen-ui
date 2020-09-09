0a6441910a89b6f8281996dad97e080a91fe0be3
Michael Mitchell <michael.mitchell@mongodb.com>
2020-09-09 03:26:01 -0400

    actually fix

--- packages/tooltip/src/index.ts
+++ packages/tooltip/src/index.ts
@@ -1,5 +1,5 @@
 import Tooltip, { TriggerEvent, Variant, Align, Justify } from './Tooltip';
 export type { TooltipProps } from './Tooltip';
 
-export { TooltipProps, TriggerEvent, Variant, Align, Justify };
+export { TriggerEvent, Variant, Align, Justify };
 export default Tooltip;
