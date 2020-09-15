729c79f88974d00978236f1b76aab19ae1b9100d
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2020-09-15 10:53:25 -0400

    PD-866/867: Adds darkMode props to Syntax and Code components (#610)
    * add darkMode props to syntax and code components
    
    * responding to PR feedback
    
    * format
    
    * localize mode

--- packages/syntax/src/index.ts
+++ packages/syntax/src/index.ts
@@ -1,5 +1,4 @@
 import Syntax from './Syntax';
-
-export { Variant, Language, SyntaxProps } from './types';
+export { Language, SyntaxProps } from './types';
 export { variantColors } from './globalStyles';
 export default Syntax;
