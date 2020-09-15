4cf2cb927daa27c4842b039b62b5749845cd13a1
Michael Mitchell <michael.mitchell@mongodb.com>
2020-09-15 09:56:22 -0400

    pr feedback

--- packages/table/src/TableBody.tsx
+++ packages/table/src/TableBody.tsx
@@ -8,29 +8,29 @@ type TableBodyProps<Shape> = Pick<TableProps<Shape>, 'children'>;
  * Hook that avoids re-rendering children when the data used to
  * render them hasn't changed, even if their ordering has changed.
  */
-function useRenderedChildren<Props>(
-  propList: Array<Props>,
-  renderFunction: React.FunctionComponent<Props>,
-  compareFn?: (a: Props, b: Props) => number,
+function useRenderedChildren<Datum>(
+  data: Array<Datum>,
+  renderFunction: React.FunctionComponent<Datum>,
+  compareFn?: (a: Datum, b: Datum) => number,
 ): Array<React.ReactNode> {
   const resultMap = useMemo(() => {
-    const resultMap = new Map<Props, React.ReactNode>();
-    propList.forEach((props, index) =>
+    const resultMap = new Map<Datum, React.ReactNode>();
+    data.forEach((datum, index) =>
       resultMap.set(
-        props,
-        <React.Fragment key={index}>{renderFunction(props)}</React.Fragment>,
+        datum,
+        <React.Fragment key={index}>{renderFunction(datum)}</React.Fragment>,
       ),
     );
     return resultMap;
-  }, [propList, renderFunction]);
+  }, [data, renderFunction]);
 
   return useMemo(() => {
     if (!compareFn) {
       return Array.from(resultMap.values());
     }
 
-    return [...propList].sort(compareFn).map(props => resultMap.get(props));
-  }, [propList, resultMap, compareFn]);
+    return [...data].sort(compareFn).map(datum => resultMap.get(datum));
+  }, [data, resultMap, compareFn]);
 }
 
 function TableBody<Shape>({ children }: TableBodyProps<Shape>) {
