6af99fb919b9c8c64eb048964069ca7b605e8f53
Brooke Yalof <brooke.yalof@mongodb.com>
2020-09-16 09:19:19 -0400

    wip

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,39 +1,98 @@
 /** @jsx jsx */
-import { css, jsx } from '@emotion/core';
-import { GridContainer, Grid } from '../components/grid/Grid';
+import { jsx } from '@emotion/core';
+import styled from '@emotion/styled';
+import { GridContainer, GridItem } from '../components/grid/Grid';
 
-const Layout = () => {
-  return (
-    <div
-      css={css`
-        height: 100px;
-        background-color: orange;
-      `}
-    />
-  );
-};
+const PinkDiv = styled.div`
+  height: 100px;
+  background-color: #ff66d6;
+`;
+
+const GreenDiv = styled.div`
+  height: 100px;
+  background-color: #65fd44;
+`;
+
+const BlueDiv = styled.div`
+  height: 100px;
+  background-color: #65c7ff;
+`;
+
+const SalmonDiv = styled.div`
+  height: 100px;
+  background-color: #ff9191;
+`;
+
+const GoldDiv = styled.div`
+  height: 100px;
+  background-color: #d9c276;
+`;
 
 export default function Home() {
   return (
-    <GridContainer>
-      <Grid>
-        <Layout />
-      </Grid>
-      <Grid>
-        <Layout />
-      </Grid>
-      <Grid>
-        <Layout />
-      </Grid>
-      <Grid>
-        <Layout />
-      </Grid>
-      <Grid>
-        <Layout />
-      </Grid>
-      <Grid>
-        <Layout />
-      </Grid>
-    </GridContainer>
+    <div>
+      <GridContainer>
+        <GridItem lg={12}>
+          <PinkDiv />
+        </GridItem>
+      </GridContainer>
+
+      <GridContainer>
+        <GridItem lg={6}>
+          <GreenDiv />
+        </GridItem>
+        <GridItem lg={6}>
+          <GreenDiv />
+        </GridItem>
+      </GridContainer>
+
+      <GridContainer>
+        <GridItem md={6}>
+          <BlueDiv />
+        </GridItem>
+        <GridItem md={6}>
+          <BlueDiv />
+        </GridItem>
+        <GridItem md={6}>
+          <BlueDiv />
+        </GridItem>
+      </GridContainer>
+
+      <GridContainer>
+        <GridItem md={3}>
+          <SalmonDiv />
+        </GridItem>
+        <GridItem md={3}>
+          <SalmonDiv />
+        </GridItem>
+        <GridItem md={3}>
+          <SalmonDiv />
+        </GridItem>
+        <GridItem md={3}>
+          <SalmonDiv />
+        </GridItem>
+      </GridContainer>
+
+      <GridContainer>
+        <GridItem sm={6}>
+          <GoldDiv />
+        </GridItem>
+        <GridItem sm={6}>
+          <GoldDiv />
+        </GridItem>
+        <GridItem sm={6}>
+          <GoldDiv />
+        </GridItem>
+        <GridItem sm={6}>
+          <GoldDiv />
+        </GridItem>
+        <GridItem sm={6}>
+          <GoldDiv />
+        </GridItem>
+        <GridItem sm={6}>
+          <GoldDiv />
+        </GridItem>
+      </GridContainer>
+    </div>
   );
 }
