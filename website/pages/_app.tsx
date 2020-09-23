e00b67df953b19d82b1894dab3691173ceaf0cbc
Brooke Yalof <brooke.yalof@mongodb.com>
2020-09-23 13:29:27 -0400

    wip

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
