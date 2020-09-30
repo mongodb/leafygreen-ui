a182ca76978e536110c6f17dd36a1ca9a0d07885
Brooke Yalof <brooke.yalof@mongodb.com>
2020-09-30 14:48:45 -0400

    make table responsive

--- packages/table/src/Table.tsx
+++ packages/table/src/Table.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import debounce from 'lodash/debounce';
 import { cx, css } from '@leafygreen-ui/emotion';
 import { uiColors } from '@leafygreen-ui/palette';
+import {
+  useIsomorphicLayoutEffect,
+  useViewportSize,
+} from '@leafygreen-ui/hooks';
 import { HeaderRowProps } from './HeaderRow';
 import { TableHeaderProps } from './TableHeader';
 import { TableProvider } from './TableContext';
@@ -13,6 +18,42 @@ const tableStyles = css`
   box-sizing: border-box;
   border-bottom: 1px solid ${uiColors.gray.light2};
 `;
+
+const ScrollState = {
+  None: 'none',
+  Left: 'left',
+  Right: 'right',
+  Both: 'both',
+} as const;
+
+type ScrollState = typeof ScrollState[keyof typeof ScrollState];
+
+function getScrollShadowStyle(scrollState: ScrollState): string {
+  const shadowColor = 'rgba(93,108,116,0.3)';
+
+  if (scrollState === ScrollState.Both) {
+    return css`
+      box-shadow: inset 6px 0 6px -6px ${shadowColor},
+        inset -6px 0 6px -6px ${shadowColor},
+        inset 0 6px 6px -6px ${uiColors.gray.light3},
+        inset 0 -6px 6px -6px ${uiColors.gray.light3};
+    `;
+  }
+
+  if (scrollState === ScrollState.Left) {
+    return css`
+      box-shadow: inset 6px 0 6px -6px ${shadowColor};
+    `;
+  }
+
+  if (scrollState === ScrollState.Right) {
+    return css`
+      box-shadow: inset -6px 0 6px -6px ${shadowColor};
+    `;
+  }
+
+  return '';
+}
 
 interface TableRowInterface<Shape = {}> {
   datum: Shape;
@@ -35,19 +76,77 @@ export default function Table<Shape>({
   className,
   ...rest
 }: TableProps<Shape>) {
+  const [scrollState, setScrollState] = React.useState<ScrollState>(
+    ScrollState.None,
+  );
+  const divRef = React.useRef<HTMLDivElement>(null);
+  const viewportSize = useViewportSize();
+  console.log(viewportSize);
+
+  useIsomorphicLayoutEffect(() => {
+    const ref = divRef.current;
+
+    if (ref != null && ref.scrollWidth > ref.clientWidth) {
+      setScrollState(ScrollState.Right);
+    } else if (
+      ref != null &&
+      viewportSize != null &&
+      ref.getBoundingClientRect().width < viewportSize.width
+    ) {
+      setScrollState(ScrollState.None);
+    }
+  }, [viewportSize]);
+
+  const handleScroll = (e: React.UIEvent) => {
+    const { scrollWidth, clientWidth: elementWidth } = e.target as HTMLElement;
+    const isScrollable = scrollWidth > elementWidth;
+
+    if (isScrollable) {
+      const scrollPosition = (e.target as HTMLElement).scrollLeft;
+      const maxPosition = scrollWidth - elementWidth;
+
+      if (scrollPosition > 0 && scrollPosition < maxPosition) {
+        setScrollState(ScrollState.Both);
+      } else if (scrollPosition > 0) {
+        setScrollState(ScrollState.Left);
+      } else if (scrollPosition < maxPosition) {
+        setScrollState(ScrollState.Right);
+      }
+    }
+  };
+
+  const debounceScroll = debounce(handleScroll, 50, { leading: true });
+
+  const onScroll: React.UIEventHandler<HTMLDivElement | HTMLPreElement> = e => {
+    e.persist();
+    debounceScroll(e);
+  };
+
   return (
-    <table
-      cellSpacing="0"
-      cellPadding="0"
-      className={cx(tableStyles, className)}
-      {...rest}
+    <div
+      ref={divRef}
+      onScroll={onScroll}
+      className={cx(
+        css`
+          max-width: calc(100% - 100px);
+          overflow-x: auto;
+        `,
+        getScrollShadowStyle(scrollState),
+      )}
     >
-      <TableProvider data={dataProp}>
-        <SortProvider>
-          <TableHead columns={columns} />
-          <TableBody>{children}</TableBody>
-        </SortProvider>
-      </TableProvider>
-    </table>
+      <table
+        cellSpacing="0"
+        cellPadding="0"
+        className={cx(tableStyles, className)}
+        {...rest}
+      >
+        <TableProvider data={dataProp}>
+          <SortProvider>
+            <TableHead columns={columns} />
+            <TableBody>{children}</TableBody>
+          </SortProvider>
+        </TableProvider>
+      </table>
+    </div>
   );
 }
