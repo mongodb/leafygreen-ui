d1bd9da10f7be6f6be3230afdff316efe3643fa2
Brooke Yalof <brooke.yalof@mongodb.com>
2020-10-06 15:52:33 -0400

    working now

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,7 +1,7 @@
-/** @jsx jsx */
+import React from 'react';
 import type { AppProps } from 'next/app';
 import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
-import { Global, jsx } from '@emotion/core';
+import { Global } from '@emotion/core';
 import { globalStyles } from '../styles/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
