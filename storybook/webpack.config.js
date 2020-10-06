50a637aa3e429860bd97c25bd53baa6e1ea0bc2c
lazytype <lazytype@users.noreply.github.com>
2020-10-06 13:38:24 -0400

    Use Rollup to generate UMD and ESM files (#573)
    * Use Rollup to generate UMD and ESM files
    
    * change dist directory structure
    
    * Refactor package configs
    
    * Generate SVG components and include them as part of the build
    
    * Fix direct glyph import reference
    
    * Refactor shared code
    
    * Include SVG change
    
    * Include build command in test message
    
    * recursive mkdir
    
    * update inline definition
    
    * update marketing modal
    
    * update toast
    
    * fix tooltip import
    
    * actually fix
    
    * update yarn lock
    
    * path feedback
    
    * generate changeset
    
    Co-authored-by: Michael Mitchell <michael.mitchell@mongodb.com>
    Co-authored-by: Dave McCarter <dave.mccarthy@mongodb.com>

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,8 +1,39 @@
-module.exports = ({ config, mode }) => {
-  // Use web version of webpack config
-  const webpackConfig = require('../webpack.config.js')(mode)[0];
-  config.module.rules = webpackConfig.module.rules;
-  config.resolve.extensions = webpackConfig.resolve.extensions;
+module.exports = ({ config }) => {
+  config.module.rules = [
+    {
+      test: /\.(t|j)sx?$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          // Makes Babel treat the directory containing babel.config.js as the project root
+          rootMode: 'upward',
+        },
+      },
+      exclude: /node_modules/,
+    },
+
+    {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: '@svgr/webpack',
+    },
+
+    {
+      test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url-loader',
+      query: {
+        limit: 50000,
+      },
+    },
+    {
+      test: /\.less(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+      },
+    },
+  ];
+
+  config.resolve.extensions = ['.js', '.json', '.less', '.css', '.tsx', '.ts'];
 
   return config;
 };
