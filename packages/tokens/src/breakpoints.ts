1b91d811c9cb9bee942d886528a806e7fc0d09dc
Brooke Yalof <brooke.yalof@mongodb.com>
2020-10-08 14:37:39 -0400

    lowercase

--- packages/tokens/src/breakpoints.ts
+++ packages/tokens/src/breakpoints.ts
@@ -1,8 +1,8 @@
-const Breakpoints = {
+const breakpoints = {
   Mobile: 320,
   Tablet: 768,
   Desktop: 1024,
   XLDesktop: 1440,
 } as const;
 
-export default Breakpoints;
+export default breakpoints;
