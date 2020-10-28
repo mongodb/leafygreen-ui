bcd9a16592a374795386a341c4dfc580316b2752
Brooke Yalof <brooke.yalof@mongodb.com>
2020-10-28 09:30:34 -0400

    text area component

---
+++ packages/text-area/src/TextArea.story.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean, text, select } from '@storybook/addon-knobs';
+import { css } from '@leafygreen-ui/emotion';
+import { uiColors } from '@leafygreen-ui/palette';
+import TextArea, { State } from './TextArea';
+
+storiesOf('TextArea', module).add('Default', () => {
+  const darkMode = boolean('darkMode', false);
+  return (
+    <div
+      className={css`
+        padding: 30px;
+        background-color: ${darkMode ? uiColors.gray.dark3 : 'white'};
+      `}
+    >
+      <TextArea
+        label={text('Label', 'Input Label')}
+        description={text('Description', 'This is a description for the input')}
+        disabled={boolean('Disabled', false)}
+        state={select('State', ['none', 'valid', 'error'], 'none')}
+        darkMode={darkMode}
+        errorMessage={text('Error Message', 'This is an error message')}
+      />
+    </div>
+  );
+});
---
+++ packages/text-area/src/TextArea.tsx
@@ -0,0 +1,284 @@
+import React, { useState, useMemo } from 'react';
+import { IdAllocator, Either, createDataProp } from '@leafygreen-ui/lib';
+import { css, cx } from '@leafygreen-ui/emotion';
+import { uiColors } from '@leafygreen-ui/palette';
+import { spacing, fontFamilies } from '@leafygreen-ui/tokens';
+
+const idAllocator = IdAllocator.create('textarea');
+const textAreaProp = createDataProp('area-selector');
+
+export const State = {
+  None: 'none',
+  Valid: 'valid',
+  Error: 'error',
+} as const;
+
+export type State = typeof State[keyof typeof State];
+
+const Mode = {
+  Light: 'light',
+  Dark: 'dark',
+} as const;
+
+type Mode = typeof Mode[keyof typeof Mode];
+
+const containerStyles = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+const labelStyle = css`
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 16px;
+  padding-bottom: 4px;
+`;
+
+const descriptionStyle = css`
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: normal;
+  padding-bottom: 4px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`;
+
+const textAreaContainer = css`
+  margin-top: 4px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  z-index: 0;
+`;
+
+const textAreaStyle = css`
+  width: 100%;
+  min-height: ${spacing[6]}px;
+  resize: none;
+  border-radius: 4px;
+  padding-left: 12px;
+  font-size: 14px;
+  font-weight: normal;
+  font-family: ${fontFamilies.default};
+  z-index: 1;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+`;
+
+const interactionRingStyles = css`
+  transition: all 150ms ease-in-out;
+  transform: scale(0.9, 0.8);
+  border-radius: 7px;
+  position: absolute;
+  top: -3px;
+  bottom: -3px;
+  left: -3px;
+  right: -3px;
+  pointer-events: none;
+  background-color: pink;
+
+  ${textAreaProp.selector}:hover ~ & {
+    transform: scale(1);
+  }
+
+  ${textAreaProp.selector}:focus ~ & {
+    transform: scale(1);
+  }
+`;
+
+const errorMessageStyle = css`
+  font-size: 14px;
+  height: 20px;
+  padding-top: 4px;
+  font-weight: normal;
+`;
+
+interface ColorSets {
+  labelColor: string;
+  descriptionColor: string;
+  textAreaBackgroundColor: string;
+  textAreaColor: string;
+  disabledBackgroundColor: string;
+  disabledColor: string;
+  interactionRing: string;
+  interactionRingFocus: string;
+  validBorder: string;
+  errorBorder: string;
+  errorMessage: string;
+}
+
+const colorSets: Record<Mode, ColorSets> = {
+  [Mode.Light]: {
+    labelColor: uiColors.gray.dark2,
+    descriptionColor: uiColors.gray.dark1,
+    textAreaColor: uiColors.gray.dark3,
+    textAreaBackgroundColor: uiColors.white,
+    disabledColor: uiColors.gray.base,
+    disabledBackgroundColor: uiColors.gray.light2,
+    interactionRing: uiColors.gray.light2,
+    interactionRingFocus: '#9dd0e7',
+    validBorder: uiColors.green.base,
+    errorBorder: uiColors.red.base,
+    errorMessage: uiColors.red.base,
+  },
+  [Mode.Dark]: {
+    labelColor: uiColors.white,
+    descriptionColor: uiColors.gray.light1,
+    textAreaColor: uiColors.white,
+    textAreaBackgroundColor: '#394F5A',
+    disabledColor: uiColors.gray.dark1,
+    disabledBackgroundColor: '#263843',
+    interactionRing: uiColors.gray.dark1,
+    interactionRingFocus: uiColors.blue.base,
+    validBorder: uiColors.green.base,
+    errorBorder: '#EF8D6F',
+    errorMessage: '#EF8D6F',
+  },
+};
+
+type BaseTextAreaProps = JSX.IntrinsicElements['textarea'] & {
+  id?: string;
+  darkMode?: boolean;
+  label: string;
+  description?: string;
+  state?: State;
+  errorMessage?: string;
+};
+
+type AriaLabels = 'label' | 'aria-labelledby';
+type TextAreaProps = Either<BaseTextAreaProps, AriaLabels>;
+
+export default function TextArea({
+  label,
+  description,
+  className,
+  errorMessage,
+  darkMode = false,
+  disabled = false,
+  state = State.None,
+  id: idProp,
+  value: controlledValue,
+  onChange,
+  'aria-labelledby': ariaLabelledBy,
+  ...rest
+}: TextAreaProps) {
+  const id = useMemo(() => idProp ?? idAllocator.generate(), [idProp]);
+  const mode = darkMode ? Mode.Dark : Mode.Light;
+
+  const isControlled = typeof controlledValue === 'string';
+  const [uncontrolledValue, setValue] = useState('');
+  const value = isControlled ? controlledValue : uncontrolledValue;
+
+  const onValueChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (onChange) {
+      onChange(e);
+    }
+
+    if (!isControlled) {
+      setValue(e.target.value);
+    }
+  };
+
+  if (!label && !ariaLabelledBy) {
+    console.error(
+      'For screen-reader accessibility, label or aria-labelledby must be provided to IconButton.',
+    );
+  }
+
+  const stateBorderColor =
+    state === State.Valid
+      ? colorSets[mode].validBorder
+      : colorSets[mode].errorBorder;
+
+  return (
+    <div className={containerStyles}>
+      {label && (
+        <label
+          htmlFor={id}
+          className={cx(
+            labelStyle,
+            css`
+              color: ${colorSets[mode].labelColor};
+            `,
+          )}
+        >
+          {label}
+        </label>
+      )}
+      {description && (
+        <p
+          className={cx(
+            descriptionStyle,
+            css`
+              color: ${colorSets[mode].labelColor};
+            `,
+          )}
+        >
+          {description}
+        </p>
+      )}
+      <div className={textAreaContainer}>
+        <textarea
+          {...textAreaProp.prop}
+          {...rest}
+          title={label}
+          id={id}
+          className={cx(
+            textAreaStyle,
+            css`
+              color: ${colorSets[mode].textAreaColor};
+              background-color: ${colorSets[mode].textAreaBackgroundColor};
+
+              &:disabled {
+                color: ${colorSets[mode].disabledColor};
+                background-color: ${colorSets[mode].disabledBackgroundColor};
+              }
+            `,
+            {
+              [css`
+                border: 1px solid ${stateBorderColor};
+              `]: state !== State.None,
+            },
+            className,
+          )}
+          disabled={disabled}
+          onChange={onValueChange}
+          value={value}
+        />
+
+        {!disabled && (
+          <div
+            className={cx(
+              interactionRingStyles,
+              css`
+                background-color: ${colorSets[mode].interactionRing};
+
+                ${textAreaProp.selector}:focus ~ & {
+                  background-color: ${colorSets[mode].interactionRingFocus};
+                }
+              `,
+            )}
+          />
+        )}
+      </div>
+      {!disabled && state === State.Error && errorMessage && (
+        <div
+          className={cx(
+            errorMessageStyle,
+            css`
+              color: ${colorSets[mode].errorMessage};
+            `,
+          )}
+        >
+          <label>{errorMessage}</label>
+        </div>
+      )}
+    </div>
+  );
+}
