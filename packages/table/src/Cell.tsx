3bf125b7b5c6d604a42bb7c0e39e8888a5788509
Dave McCarter <dave.mccarthy@mongodb.com>
2020-12-21 12:35:25 -0500

    PD-942: Add isHeader prop to table cell component (#747)
    * add isHeader prop
    
    * fix row accessibility issue
    add tests for isHeader
    
    * changeset
    
    * format
    
    * pr feedback
    
    * make TH part of union require isHeader

--- packages/table/src/Cell.tsx
+++ packages/table/src/Cell.tsx
@@ -5,11 +5,24 @@ import { commonCellStyles } from './styles';
 
 export const tdInnerDiv = createDataProp('td-inner-div');
 
-type CellProps = HTMLElementProps<'td', HTMLTableCellElement>;
+interface HeaderCellProps
+  extends HTMLElementProps<'th', HTMLTableHeaderCellElement> {
+  isHeader: true;
+}
 
-const tdStyles = css`
+interface TableCellProps extends HTMLElementProps<'td', HTMLTableCellElement> {
+  isHeader?: false;
+}
+
+type CellProps = HeaderCellProps | TableCellProps;
+
+const baseStyles = css`
   line-height: 20px;
   position: relative;
+`;
+
+const thStyles = css`
+  font-weight: bold;
 `;
 
 const innerDivStyles = css`
@@ -18,17 +31,32 @@ const innerDivStyles = css`
 `;
 
 const Cell = React.forwardRef(
-  ({ children, className, ...rest }: CellProps, ref: React.Ref<any>) => {
+  (
+    { children, className, isHeader = false, ...rest }: CellProps,
+    ref: React.Ref<any>,
+  ) => {
+    const Root = isHeader ? 'th' : 'td';
+
+    const props: Partial<CellProps> = {
+      ref,
+      className: cx(
+        commonCellStyles,
+        baseStyles,
+        { [thStyles]: isHeader },
+        className,
+      ),
+    };
+
+    if (isHeader) {
+      props.scope = 'row';
+    }
+
     return (
-      <td
-        ref={ref}
-        className={cx(commonCellStyles, tdStyles, className)}
-        {...rest}
-      >
+      <Root {...props} {...rest}>
         <div className={innerDivStyles} {...tdInnerDiv.prop}>
           {children}
         </div>
-      </td>
+      </Root>
     );
   },
 );
