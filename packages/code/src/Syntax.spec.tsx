31cfb3bb34ab4bbdafe3fb0b97d11fae6a4d7e34
Dave McCarthy <dave.mccarthy@mongodb.com>
2020-12-29 16:47:42 -0500

    remove unused tests

--- packages/code/src/Syntax.spec.tsx
+++ packages/code/src/Syntax.spec.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Syntax, {
-  expandRangeTuple,
-  normalizeLineHighlightingDefinition,
-} from './Syntax';
+import Syntax from './Syntax';
 
 const codeSnippet = 'const greeting = "Hello, world!";';
 const className = 'test-class';
@@ -49,54 +46,6 @@ describe('packages/Syntax', () => {
       // We test for more than one node rather than a specific number here and below to ensure
       // we're testing this component rather than the underlying library's implementation.
       expect(child.children.length).toBeGreaterThan(1);
-    });
-  });
-
-  describe('expandRangeTuple()', () => {
-    test('when passed two numbers, returns an inclusive array of that range', () => {
-      expect(expandRangeTuple([0, 5])).toEqual([0, 1, 2, 3, 4, 5]);
-      expect(expandRangeTuple([2, 5])).toEqual([2, 3, 4, 5]);
-      expect(expandRangeTuple([5, 2])).toEqual([2, 3, 4, 5]);
-      expect(expandRangeTuple([0, 0])).toEqual([0]);
-      expect(expandRangeTuple([2, 2])).toEqual([2]);
-      expect(expandRangeTuple([-1, 1])).toEqual([0, 1]);
-
-      // Caps maximum range to 500 lines
-      expect(expandRangeTuple([1, 1000]).length).toEqual(500);
-    });
-  });
-
-  describe('normalizeLineHighlightingDefinition()', () => {
-    test('when passed an empty array, returns an empty array', () => {
-      expect(normalizeLineHighlightingDefinition([])).toEqual([]);
-    });
-
-    test('when passed an array of numbers, returns an array of those numbers', () => {
-      expect(normalizeLineHighlightingDefinition([0, 1])).toEqual([0, 1]);
-      expect(normalizeLineHighlightingDefinition([1, 2])).toEqual([1, 2]);
-    });
-
-    test('when passed an array of tuples, returns an array of numbers', () => {
-      expect(normalizeLineHighlightingDefinition([[0, 1]])).toEqual([0, 1]);
-      expect(
-        normalizeLineHighlightingDefinition([
-          [1, 3],
-          [4, 4],
-          [7, 5],
-        ]),
-      ).toEqual([1, 2, 3, 4, 5, 6, 7]);
-    });
-
-    test('when passed an array of tuples and numbers, returns an array of numbers', () => {
-      expect(normalizeLineHighlightingDefinition([[1, 3], 4, [7, 5]])).toEqual([
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-      ]);
     });
   });
 });
