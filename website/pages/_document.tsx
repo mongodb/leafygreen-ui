083af8afb7d4737abeab0dd6e9f6423e644df432
Brooke Yalof <brooke.yalof@mongodb.com>
2021-01-07 16:21:57 -0500

    responding to PR feedback

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -2,6 +2,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import * as React from 'react';
 import { renderStatic } from 'utils/renderer';
 
+/**
+ * We create a custom _document.tsx in order to support vanilla emotion with Next.js
+ * https://github.com/vercel/next.js/pull/20228/files
+ */
 export default class AppDocument extends Document {
   static async getInitialProps(ctx) {
     const page = await ctx.renderPage();
@@ -10,13 +14,13 @@ export default class AppDocument extends Document {
     return {
       ...initialProps,
       styles: (
-        <React.Fragment>
+        <>
           {initialProps.styles}
           <style
             data-emotion={`css ${ids.join(' ')}`}
             dangerouslySetInnerHTML={{ __html: css }}
           />
-        </React.Fragment>
+        </>
       ),
     };
   }
--- website/utils/renderer.ts
+++ website/utils/renderer.ts
@@ -1,7 +1,7 @@
 import createEmotionServer from '@emotion/server/create-instance';
 import { cache } from '@emotion/css';
 
-export const renderStatic = async (callback: Function) => {
+export const renderStatic = async (callback: () => string) => {
   const html = callback();
 
   if (html === undefined) {
