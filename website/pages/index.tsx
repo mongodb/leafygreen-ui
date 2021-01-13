3d4f7feedcf85545ca69a138696302d304983572
Brooke Yalof <brooke.yalof@mongodb.com>
2021-01-13 13:14:55 -0500

    yarn fix

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -29,10 +29,10 @@ const backdrop = css`
 
 const layoutProperties = css`
   ${mq({
-  width: ['calc(100% + 48px)', '100%', '100%', '1077px'],
-  paddingRight: [0, `${spacing[4]}px`, `${spacing[4]}px`, `${spacing[4]}px`],
-  marginLeft: ['-24px', 'unset', 'unset', 'unset'],
-})}
+    width: ['calc(100% + 48px)', '100%', '100%', '1077px'],
+    paddingRight: [0, `${spacing[4]}px`, `${spacing[4]}px`, `${spacing[4]}px`],
+    marginLeft: ['-24px', 'unset', 'unset', 'unset'],
+  })}
 `;
 
 const boxShadow = css`
@@ -85,13 +85,13 @@ const overlineContainer = css`
   transition: all 300ms ease-in-out;
 
   ${mq({
-  opacity: [1, 1, 0],
-  transform: [
-    'none',
-    'none',
-    `translate3d(0, ${spacing[3]}px, 0) scale(0.95)`,
-  ],
-})}
+    opacity: [1, 1, 0],
+    transform: [
+      'none',
+      'none',
+      `translate3d(0, ${spacing[3]}px, 0) scale(0.95)`,
+    ],
+  })}
 `;
 
 const overlineColor = css`
@@ -120,20 +120,20 @@ const textWrapper = css`
   overflow: hidden;
 
   ${mq({
-  fontSize: ['24px', '60px', '60px', '60px'],
-})}
+    fontSize: ['24px', '60px', '60px', '60px'],
+  })}
 `;
 
 const largeHeight = css`
   ${mq({
-  height: ['50vw', '350px', '350px', '350px'],
-})}
+    height: ['50vw', '350px', '350px', '350px'],
+  })}
 `;
 
 const smallHeight = css`
   ${mq({
-  height: ['50vw', '175px', '175px', '175px'],
-})}
+    height: ['50vw', '175px', '175px', '175px'],
+  })}
 `;
 
 const halfWidth = css`
