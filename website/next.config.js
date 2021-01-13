e9aab0da945a792adeb43cb52b8e132e6207b438
Brooke Yalof <brooke.yalof@mongodb.com>
2021-01-13 12:35:02 -0500

    remove nextconfig

--- website/next.config.js
+++ website/next.config.js
@@ -5,7 +5,4 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx', 'tsx', 'ts'],
   trailingSlash: true,
-  images: {
-    domains: ['d2va9gm4j17fy9.cloudfront.net'],
-  },
 });
