3f72422cc843cf72b627291f6ac057c35a901067
Brooke Yalof <brooke.yalof@mongodb.com>
2021-02-02 14:46:09 -0500

    add metaKey file

--- website/layouts/ComponentLayout.tsx
+++ website/layouts/ComponentLayout.tsx
@@ -14,6 +14,7 @@ import { mq } from 'utils/mediaQuery';
 import { pageContainerWidth } from 'styles/constants';
 import figmaLinks from 'utils/figmaLinks';
 import { Component } from 'utils/types';
+import { metaTagKey } from 'utils/metaTagKey';
 
 const layout = css`
   ${mq({
@@ -105,7 +106,7 @@ function ComponentLayout({ children }: { children: React.ReactNode }) {
       <Head>
         <title>{pageTitle}</title>
 
-        <meta property="og:title" content={pageTitle} key="title" />
+        <meta property="og:title" content={pageTitle} key={metaTagKey.Title} />
       </Head>
 
       <div className={margin4}>
