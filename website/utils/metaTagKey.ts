9912da2c3aae50cbd6a16008421e4c29b7e62e70
Brooke Yalof <brooke.yalof@mongodb.com>
2021-02-02 14:56:51 -0500

    add comment

--- website/utils/metaTagKey.ts
+++ website/utils/metaTagKey.ts
@@ -1,3 +1,5 @@
+// Add keys to meta tags in order to help Next.js prioritize when duplicates appear on a page
+// https://nextjs.org/docs/api-reference/next/head
 const metaTagKey = {
   Title: 'title',
 } as const;
