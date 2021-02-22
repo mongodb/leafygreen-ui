17af7ce59bf42c76e378adf4dbe62049c03d97b3
Brooke Yalof <brooke.yalof@mongodb.com>
2021-02-22 11:35:57 -0500

    fix type

--- packages/logo/src/Logo.story.tsx
+++ packages/logo/src/Logo.story.tsx
@@ -87,7 +87,7 @@ storiesOf('Logo', module)
           darkMode={darkMode}
           knockout={boolean('Knockout', false)}
           height={number('Height', 40)}
-          product={select('Product', Object.values(Product), Product.None)}
+          product={select('Product', [...Object.values(Product), 'none'], 'none')}
           lockup={select('Lockup', Object.values(Lockup), Lockup.Default)}
         />
       </div>
