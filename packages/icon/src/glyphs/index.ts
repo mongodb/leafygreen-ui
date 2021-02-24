005c8c8b1765f650cd5e6b8f30afb3c3a6407fb3
Dave McCarthy <dave.mccarthy@mongodb.com>
2021-02-24 13:58:54 -0500

    add apps glyph

--- packages/icon/src/glyphs/index.ts
+++ packages/icon/src/glyphs/index.ts
@@ -4,6 +4,7 @@ import { LGGlyph } from '../types';
 // Glyphs
 import ActivityFeed from './ActivityFeed.svg';
 import AddFile from './AddFile.svg';
+import Apps from './Apps.svg';
 import ArrowDown from './ArrowDown.svg';
 import ArrowLeft from './ArrowLeft.svg';
 import ArrowRight from './ArrowRight.svg';
@@ -71,6 +72,7 @@ import XWithCircle from './XWithCircle.svg';
 const glyphs = {
   ActivityFeed,
   AddFile,
+  Apps,
   ArrowDown,
   ArrowLeft,
   ArrowRight,
