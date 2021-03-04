337738f111b30fd49db3e895e8f35ca6dcd51539
Dave McCarthy <dave.mccarthy@mongodb.com>
2021-03-04 17:57:08 -0500

    format

--- packages/a11y/src/A11y.spec.tsx
+++ packages/a11y/src/A11y.spec.tsx
@@ -79,10 +79,10 @@ describe('packages/a11y', () => {
     test('when passed an invalid input, returns a string', () => {
       // @ts-expect-error
       expect(prefersReducedMotion(null)).toEqual('');
-      
+
       // @ts-expect-error
       expect(prefersReducedMotion(undefined)).toEqual('');
-      
+
       // @ts-expect-error
       expect(prefersReducedMotion(24)).toEqual('');
 
--- packages/mongo-nav/src/mongo-select/selectHelpers.ts
+++ packages/mongo-nav/src/mongo-select/selectHelpers.ts
@@ -2,7 +2,9 @@ import { keyMap } from '@leafygreen-ui/lib';
 
 export const onKeyDown = (e: React.KeyboardEvent) => {
   // Stops default browser behavior from automatically scrolling the component
-  if (([keyMap.ArrowUp, keyMap.ArrowDown] as Array<number>).includes(e.keyCode)) {
+  if (
+    ([keyMap.ArrowUp, keyMap.ArrowDown] as Array<number>).includes(e.keyCode)
+  ) {
     e.preventDefault();
   }
 };
