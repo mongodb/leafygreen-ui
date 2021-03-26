2eb8f868ff03d7cc135a0114bd4af37851252396
Brooke Yalof <brooke.yalof@mongodb.com>
2021-03-26 11:40:11 -0400

    update example

--- website/pages/component/tabs/example.tsx
+++ website/pages/component/tabs/example.tsx
@@ -1,86 +1,9 @@
-// import React from 'react';
-// import { css } from 'emotion';
-// import { Tab, Tabs } from '@leafygreen-ui/tabs';
-// import LiveExample, { KnobsConfigInterface } from 'components/live-example';
-// import { uiColors } from '@leafygreen-ui/palette/dist';
-
-// const knobsConfig: KnobsConfigInterface<{
-//   darkMode: boolean;
-//   disabled: boolean;
-//   name: string;
-//   children: string;
-// }> = {
-//   darkMode: {
-//     type: 'boolean',
-//     default: false,
-//     label: 'Dark Mode',
-//   },
-//   disabled: {
-//     type: 'boolean',
-//     default: false,
-//     label: 'Disabled',
-//   },
-//   name: {
-//     type: 'text',
-//     default: 'Users',
-//     label: 'Name',
-//   },
-//   children: {
-//     type: 'text',
-//     default: 'Find a user',
-//     label: 'Children',
-//   },
-// };
-
-// export default function TabsLiveExample() {
-//   return (
-//     <LiveExample knobsConfig={knobsConfig}>
-//       {({ children, name, disabled, darkMode }) => (
-//         <div
-//           className={css`
-//             min-width: 400px;
-//           `}
-//         >
-//           <Tabs darkMode={darkMode} aria-label="Example usage of Tab component">
-//             <Tab default name={name}>
-//               <div
-//                 className={css`
-//                   color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
-//                 `}
-//               >
-//                 {children}
-//               </div>
-//             </Tab>
-//             <Tab name="Teams">
-//               <div
-//                 className={css`
-//                   color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
-//                 `}
-//               >
-//                 Grant teams of users access to projects
-//               </div>
-//             </Tab>
-//             <Tab name="API Keys" disabled={disabled}>
-//               <div
-//                 className={css`
-//                   color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
-//                 `}
-//               >
-//                 Manage your infrastructure in code
-//               </div>
-//             </Tab>
-//           </Tabs>
-//         </div>
-//       )}
-//     </LiveExample>
-//   );
-// }
-
-import React, { useState } from 'react';
+import React from 'react';
 import { css } from 'emotion';
 import { Tab, Tabs } from '@leafygreen-ui/tabs';
 import LiveExample, { KnobsConfigInterface } from 'components/live-example';
 import { uiColors } from '@leafygreen-ui/palette/dist';
+
 const knobsConfig: KnobsConfigInterface<{
   darkMode: boolean;
   disabled: boolean;
@@ -110,27 +33,22 @@ const knobsConfig: KnobsConfigInterface<{
 };
 
 export default function TabsLiveExample() {
-  const [selected, setSelected] = useState(0);
-  const disabled = false;
-  const darkMode = false;
-  const name = 'test';
   return (
     <LiveExample knobsConfig={knobsConfig}>
-      {props => (
-        <div>
-          <Tabs
-            darkMode={darkMode}
-            aria-label="Example usage of Tab component"
-            setSelected={setSelected}
-            selected={selected}
-          >
+      {({ children, name, disabled, darkMode }) => (
+        <div
+          className={css`
+            min-width: 400px;
+          `}
+        >
+          <Tabs darkMode={darkMode} aria-label="Example usage of Tab component">
             <Tab default name={name}>
               <div
                 className={css`
                   color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
                 `}
               >
-                testing
+                {children}
               </div>
             </Tab>
             <Tab name="Teams">
@@ -152,250 +70,6 @@ export default function TabsLiveExample() {
               </div>
             </Tab>
           </Tabs>
-          <ul>
-            <li>
-              Morbi in sem quis dui placerat ornare. Pellentesque odio nisi,
-              euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras
-              consequat.
-            </li>
-            <li>
-              Praesent dapibus, neque id cursus faucibus, tortor neque egestas
-              augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam
-              dui mi, tincidunt quis, accumsan porttitor, facilisis luctus,
-              metus.
-            </li>
-            <li>
-              Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
-              consectetuer ligula vulputate sem tristique cursus. Nam nulla
-              quam, gravida non, commodo a, sodales sit amet, nisi.
-            </li>
-            <li>
-              Pellentesque fermentum dolor. Aliquam quam lectus, facilisis
-              auctor, ultrices ut, elementum vulputate, nunc.
-            </li>
-            <Tabs
-              darkMode={darkMode}
-              aria-label="Example usage of Tab component"
-              setSelected={setSelected}
-              selected={selected}
-            >
-              <Tab default name={name}>
-                <div
-                  className={css`
-                    color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
-                  `}
-                >
-                  testing
-                </div>
-              </Tab>
-              <Tab name="Teams">
-                <div
-                  className={css`
-                    color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
-                  `}
-                >
-                  Grant teams of users access to projects
-                </div>
-              </Tab>
-              <Tab name="API Keys" disabled={disabled}>
-                <div
-                  className={css`
-                    color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
-                  `}
-                >
-                  Manage your infrastructure in code
-                </div>
-              </Tab>
-            </Tabs>
-            <ul>
-              <li>
-                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi,
-                euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras
-                consequat.
-              </li>
-              <li>
-                Praesent dapibus, neque id cursus faucibus, tortor neque egestas
-                augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
-                Nam dui mi, tincidunt quis, accumsan porttitor, facilisis
-                luctus, metus.
-              </li>
-              <li>
-                Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
-                consectetuer ligula vulputate sem tristique cursus. Nam nulla
-                quam, gravida non, commodo a, sodales sit amet, nisi.
-              </li>
-              <li>
-                Pellentesque fermentum dolor. Aliquam quam lectus, facilisis
-                auctor, ultrices ut, elementum vulputate, nunc.
-              </li>
-            </ul>
-            <ul>
-              <li>
-                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi,
-                euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras
-                consequat.
-              </li>
-              <li>
-                Praesent dapibus, neque id cursus faucibus, tortor neque egestas
-                augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
-                Nam dui mi, tincidunt quis, accumsan porttitor, facilisis
-                luctus, metus.
-              </li>
-              <li>
-                Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
-                consectetuer ligula vulputate sem tristique cursus. Nam nulla
-                quam, gravida non, commodo a, sodales sit amet, nisi.
-              </li>
-              <li>
-                Pellentesque fermentum dolor. Aliquam quam lectus, facilisis
-                auctor, ultrices ut, elementum vulputate, nunc.
-              </li>
-            </ul>
-            <ul>
-              <li>
-                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi,
-                euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras
-                consequat.
-              </li>
-              <li>
-                Praesent dapibus, neque id cursus faucibus, tortor neque egestas
-                augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
-                Nam dui mi, tincidunt quis, accumsan porttitor, facilisis
-                luctus, metus.
-              </li>
-              <li>
-                Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
-                consectetuer ligula vulputate sem tristique cursus. Nam nulla
-                quam, gravida non, commodo a, sodales sit amet, nisi.
-              </li>
-              <li>
-                Pellentesque fermentum dolor. Aliquam quam lectus, facilisis
-                auctor, ultrices ut, elementum vulputate, nunc.
-              </li>
-            </ul>
-            <ul>
-              <li>
-                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi,
-                euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras
-                consequat.
-              </li>
-              <li>
-                Praesent dapibus, neque id cursus faucibus, tortor neque egestas
-                augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
-                Nam dui mi, tincidunt quis, accumsan porttitor, facilisis
-                luctus, metus.
-              </li>
-              <li>
-                Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
-                consectetuer ligula vulputate sem tristique cursus. Nam nulla
-                quam, gravida non, commodo a, sodales sit amet, nisi.
-              </li>
-              <li>
-                Pellentesque fermentum dolor. Aliquam quam lectus, facilisis
-                auctor, ultrices ut, elementum vulputate, nunc.
-              </li>
-            </ul>
-            <Tabs
-              darkMode={darkMode}
-              aria-label="Example usage of Tab component"
-              setSelected={setSelected}
-              selected={selected}
-            >
-              <Tab default name={name}>
-                <div
-                  className={css`
-                    color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
-                  `}
-                >
-                  testing
-                </div>
-              </Tab>
-              <Tab name="Teams">
-                <div
-                  className={css`
-                    color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
-                  `}
-                >
-                  Grant teams of users access to projects
-                </div>
-              </Tab>
-              <Tab name="API Keys" disabled={disabled}>
-                <div
-                  className={css`
-                    color: ${darkMode ? uiColors.white : uiColors.gray.dark3};
-                  `}
-                >
-                  Manage your infrastructure in code
-                </div>
-              </Tab>
-            </Tabs>
-            <ul>
-              <li>
-                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi,
-                euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras
-                consequat.
-              </li>
-              <li>
-                Praesent dapibus, neque id cursus faucibus, tortor neque egestas
-                augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
-                Nam dui mi, tincidunt quis, accumsan porttitor, facilisis
-                luctus, metus.
-              </li>
-              <li>
-                Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
-                consectetuer ligula vulputate sem tristique cursus. Nam nulla
-                quam, gravida non, commodo a, sodales sit amet, nisi.
-              </li>
-              <li>
-                Pellentesque fermentum dolor. Aliquam quam lectus, facilisis
-                auctor, ultrices ut, elementum vulputate, nunc.
-              </li>
-            </ul>
-            <ul>
-              <li>
-                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi,
-                euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras
-                consequat.
-              </li>
-              <li>
-                Praesent dapibus, neque id cursus faucibus, tortor neque egestas
-                augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
-                Nam dui mi, tincidunt quis, accumsan porttitor, facilisis
-                luctus, metus.
-              </li>
-              <li>
-                Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
-                consectetuer ligula vulputate sem tristique cursus. Nam nulla
-                quam, gravida non, commodo a, sodales sit amet, nisi.
-              </li>
-              <li>
-                Pellentesque fermentum dolor. Aliquam quam lectus, facilisis
-                auctor, ultrices ut, elementum vulputate, nunc.
-              </li>
-            </ul>
-            <ul>
-              <li>
-                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi,
-                euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras
-                consequat.
-              </li>
-              <li>
-                Praesent dapibus, neque id cursus faucibus, tortor neque egestas
-                augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
-                Nam dui mi, tincidunt quis, accumsan porttitor, facilisis
-                luctus, metus.
-              </li>
-              <li>
-                Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec
-                consectetuer ligula vulputate sem tristique cursus. Nam nulla
-                quam, gravida non, commodo a, sodales sit amet, nisi.
-              </li>
-              <li>
-                Pellentesque fermentum dolor. Aliquam quam lectus, facilisis
-                auctor, ultrices ut, elementum vulputate, nunc.
-              </li>
-            </ul>
-          </ul>
         </div>
       )}
     </LiveExample>
