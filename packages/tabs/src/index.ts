3fbcaf7f3c66f4695217d72ffd1242b01184a85a
Hai-Kinh Hoang <haikinhhoang@gmail.com>
2021-03-26 16:57:22 -0400

    lint file

--- packages/tabs/src/index.ts
+++ packages/tabs/src/index.ts
@@ -1,4 +1,4 @@
 export { default as Tabs } from './Tabs';
-export type { AccessibleTabsProps as TabsProps } from './Tabs'
+export type { AccessibleTabsProps as TabsProps } from './Tabs';
 export { default as Tab } from './Tab';
 export type { TabProps } from './Tab';
