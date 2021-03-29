b9a8df1e8e6ef8e5c4fb09fd3e5c2b5f6e25ae95
Hai-Kinh Hoang <chickenbug@users.noreply.github.com>
2021-03-29 11:07:04 -0400

    export props used by Tabs (#875)

--- packages/tabs/src/index.ts
+++ packages/tabs/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Tabs } from './Tabs';
+export type { AccessibleTabsProps as TabsProps } from './Tabs';
 export { default as Tab } from './Tab';
 export type { TabProps } from './Tab';
