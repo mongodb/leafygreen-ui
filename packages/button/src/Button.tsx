a5131014da3ca9d3667f213e3320c8f8e67e4f27
Brooke Yalof <brooke.yalof@mongodb.com>
2021-04-26 15:54:50 -0400

    rm space

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -29,7 +29,6 @@ const containerChildStyles = css`
   position: relative;
   z-index: 0;
   font-family: ${fontFamilies.default};
-
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
