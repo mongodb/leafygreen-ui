6c329f24bba405db3e078231f69388e75c621887
Jeremy Tice <jeremy.tice@10gen.com>
2021-05-25 12:23:27 -0400

    It helps to include the new file

---
+++ packages/radio-box-group/src/context.ts
@@ -0,0 +1,19 @@
+import { createContext, ChangeEventHandler, useContext } from 'react';
+import Size from './Size';
+
+export interface RadioBoxGroupContext {
+  value: string | number | undefined;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  getNextId: () => string;
+  size: Size | undefined;
+  name: string | undefined;
+}
+
+const context = createContext<RadioBoxGroupContext | null>(null);
+
+export const Provider = context.Provider;
+export const Consumer = context.Consumer;
+
+export function useRadioBoxGroupContext() {
+  return useContext(context);
+}
