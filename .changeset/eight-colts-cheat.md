4b6a264335b130f37d65e6c0f3b92e9ec9eff2be
Jeremy Tice <jeremy.tice@10gen.com>
2021-05-25 16:36:12 -0400

    Use context instead of cloneElement in RadioBox (#953)

---
+++ .changeset/eight-colts-cheat.md
@@ -0,0 +1,5 @@
+---
+'@leafygreen-ui/radio-box-group': minor
+---
+
+Allow for RadioBox elements to be rendered as non-direct descendants of a RadioBoxGroup (either inside another element, or rendered by a wrapper component)
---
+++ packages/radio-box-group/src/context.ts
@@ -0,0 +1,18 @@
+import { createContext, ChangeEventHandler, useContext } from 'react';
+import Size from './Size';
+
+export interface RadioBoxGroupContext {
+  value: string | number | undefined;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  getNextId: () => string;
+  size: Size | undefined;
+  name: string | undefined;
+}
+
+const context = createContext<RadioBoxGroupContext | null>(null);
+
+export const Provider = context.Provider;
+
+export function useRadioBoxGroupContext() {
+  return useContext(context);
+}
