e88d523e58f76023fe8cf0a0ae1038a29ccb1c4d
Brooke Yalof <brooke.yalof@mongodb.com>
2021-06-03 14:51:26 -0400

    clean up

--- website/components/GuidelineImage.tsx
+++ website/components/GuidelineImage.tsx
@@ -2,24 +2,16 @@ import React from 'react';
 import { css } from 'emotion';
 
 function GuidelineImage({
-  width,
-  height,
   imageURL,
   className,
 }: {
-  width: string;
-  height: string;
+  className: string;
   imageURL: string;
 }) {
-  console.log(width);
-
   return (
     <div
       className={css`
         margin: 40px 0px;
-        // width: ${width};
-        ${typeof width === 'string' ? 'width: auto;' : width}
-        height: ${height};
         background-image: url('${imageURL}');
         background-position: center center;
         background-repeat: no-repeat;
