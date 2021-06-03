c11b22597293e8871cbc8a356a8028436a7be585
Brooke Yalof <brooke.yalof@mongodb.com>
2021-06-03 11:41:11 -0400

    wip

--- website/components/GuidelineImage.tsx
+++ website/components/GuidelineImage.tsx
@@ -5,21 +5,26 @@ function GuidelineImage({
   width,
   height,
   imageURL,
+  className,
 }: {
   width: string;
   height: string;
   imageURL: string;
 }) {
+  console.log(width);
+
   return (
     <div
       className={css`
         margin: 40px 0px;
-        width: ${width};
+        // width: ${width};
+        ${typeof width === 'string' ? 'width: auto;' : width}
         height: ${height};
         background-image: url('${imageURL}');
         background-position: center center;
         background-repeat: no-repeat;
         background-size: contain;
+        ${className}
       `}
     />
   );
