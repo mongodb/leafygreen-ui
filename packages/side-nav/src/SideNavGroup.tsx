08e9be428443a3d3420a700fdf84a9e3a70ecbd9
Brooke Yalof <brooke.yalof@mongodb.com>
2021-06-16 13:23:36 -0700

    final real fix

--- packages/side-nav/src/SideNavGroup.tsx
+++ packages/side-nav/src/SideNavGroup.tsx
@@ -40,10 +40,10 @@ const labelStyle = css`
   min-height: ${spacing[5]}px;
   margin-top: 0;
   margin-bottom: 0;
-  padding: 12px ${sideNavItemSidePadding}px 4px ${sideNavItemSidePadding}px;
+  padding: 16px 8px;
 
   &:not(:first-of-type) {
-    margin-top: ${spacing[1]}px;
+    padding-top: ${spacing[2]}px;
   }
 `;
 
