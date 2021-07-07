29f133f9e0e46a100c9de2c345fd1601b020115b
Adam Thompson <adam.thompson@mongodb.com>
2021-07-07 17:17:10 -0400

    Fixes generated icon

--- packages/icon/src/generated/Clone.tsx
+++ packages/icon/src/generated/Clone.tsx
@@ -2,7 +2,7 @@
  * This is a generated file. Do not modify it manually.
  *
  * @script ./node_modules/.bin/ts-node packages/icon/scripts/build.ts
- * @checksum 623992e0bbbed8a281b40a4e779779d7
+ * @checksum 21048c83272122cbe944a5b09ed3a090
  */
 import * as React from 'react';
 import PropTypes from 'prop-types';
@@ -50,11 +50,11 @@ const Clone = ({
     >
       <path
         d="M5.5 12C5.5 13.1046 6.39543 14 7.5 14H12.5C13.6046 14 14.5 13.1046 14.5 12V8C14.5 6.89543 13.6046 6 12.5 6H7.5C6.39543 6 5.5 6.89543 5.5 8V12Z"
-        fill="#000"
+        fill={'currentColor'}
       />
       <path
         d="M4.25 10H3.5C2.39543 10 1.5 9.10457 1.5 8V4C1.5 2.89543 2.39543 2 3.5 2H8.5C9.60457 2 10.5 2.89543 10.5 4V4.75H8.5V4H3.5L3.5 8H4.25V10Z"
-        fill="#000"
+        fill={'currentColor'}
       />
     </svg>
   );
