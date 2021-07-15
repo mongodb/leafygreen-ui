9309e76e7193ec9876a2f71c7d914793bc1eab6b
Adam Thompson <adam.thompson@mongodb.com>
2021-07-15 17:03:32 -0400

    Set pre-build to only run when needed

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,22 @@
 const { spawn, spawnSync } = require('child_process');
 
-console.log('Running pre-build...');
-spawnSync('yarn', ['pre-build'], { stdio: 'inherit' });
-
-const args = process.argv.slice(2);
-
 const cmdArgs = ['--parallel', 'build'];
+const args = process.argv.slice(2);
 
 // check if we should be watching
 if (args.includes('--watch')) {
   args.splice(args.indexOf('--watch'), 1);
   cmdArgs.push('--', '--watch');
+}
+
+// Only run pre-build for packages that need it
+if (
+  args.length === 0 ||
+  args.includes('icon') ||
+  args.includes('icon-button')
+) {
+  console.log('Running pre-build...');
+  spawnSync('yarn', ['pre-build'], { stdio: 'inherit' });
 }
 
 if (args.length > 0) {
