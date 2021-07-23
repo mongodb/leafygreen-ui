ce2868986f4158d7ebe4ed37190909504272525d
Adam Thompson <adam.thompson@mongodb.com>
2021-07-23 14:18:29 -0400

    Update glob string

--- storybook/main.js
+++ storybook/main.js
@@ -1,7 +1,7 @@
 const customWebpack = require('./webpack.config.js');
 
 module.exports = {
-  stories: ['../**/*.story.(t|j)s(x)'],
+  stories: ['../**/*.story.@(ts|js)?(x)'],
   addons: [
     '@storybook/addon-knobs',
     '@storybook/addon-a11y',
