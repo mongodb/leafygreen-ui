d131d3fc906a034cd26ccafae35f192cbde45c1f
Adam Thompson <adam.thompson@mongodb.com>
2021-09-16 11:33:03 -0400

    Updates checksum

--- packages/icon/src/generated/Table.tsx
+++ packages/icon/src/generated/Table.tsx
@@ -2,7 +2,7 @@
  * This is a generated file. Do not modify it manually.
  *
  * @script ./node_modules/.bin/ts-node packages/icon/scripts/build.ts
- * @checksum 1abe3cd160b8e484365d149d8e4ae073
+ * @checksum da08be11d1c484ae1e9839b23ef87760
  */
 import * as React from 'react';
 import PropTypes from 'prop-types';
