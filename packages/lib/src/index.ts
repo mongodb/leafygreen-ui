3625b6363f3dbdfab435639bf34902bc43bb91b7
Adam Thompson <adam.thompson@mongodb.com>
2021-11-01 11:49:38 -0400

    Adds console once functions

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,3 +1,4 @@
+import { once } from 'lodash';
 import * as typeIs from './typeIs';
 
 export { typeIs };
@@ -179,3 +180,7 @@ export type RecursivePartial<T> = {
 export function enforceExhaustive(value: never): never {
   throw Error(`Received unhandled value: ${value}`);
 }
+
+export const errorOnce = once(console.error);
+export const warnOnce = once(console.warn);
+export const logOnce = once(console.log);
