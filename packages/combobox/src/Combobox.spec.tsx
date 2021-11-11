e9b6ca8849f32c929f72b85526c20461ad9ba093
Adam Thompson <adam.thompson@mongodb.com>
2021-11-11 11:39:46 -0500

    Adds todo tests

--- packages/combobox/src/Combobox.spec.tsx
+++ packages/combobox/src/Combobox.spec.tsx
@@ -1,7 +1,77 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Combobox from '.';
+import { Combobox, ComboboxOption } from '.';
+import { axe } from 'jest-axe';
+
+function renderBasicCheckbox(props = {}) {
+  const utils = render(
+    <Combobox data-testid="combobox" label="Some Label" {...props}>
+      <ComboboxOption value="Apple" />
+      <ComboboxOption value="Banana" />
+      <ComboboxOption value="Carrot" />
+    </Combobox>,
+  );
+  const combobox = utils.getByTestId('combobox');
+  return { ...utils, combobox };
+}
 
 describe('packages/combobox', () => {
-  test('condition', () => {});
+  describe('A11y', () => {
+    test('does not have basic accessibility violations', async () => {
+      const { combobox } = renderBasicCheckbox();
+      const results = await axe(combobox);
+      expect(results).toHaveNoViolations();
+    });
+  });
+
+  describe('Rendering', () => {
+    const { combobox } = renderBasicCheckbox();
+
+    test.todo('Label is rendered');
+    test.todo('Description is rendered');
+
+    describe('on error', () => {
+      test.todo('Error message is rendered');
+      test.todo('Error Icon is rendered');
+    });
+
+    describe('Single select', () => {
+      test.todo('Text input renders with initial value');
+      test.todo('Menu renders with checkmarks');
+    });
+
+    describe('Multi select', () => {
+      test.todo('Chips render with initial value');
+      test.todo('Menu renders with checkboxes');
+    });
+  });
+
+  describe('Interaction', () => {
+    test.todo('Menu appears when box is clicked');
+    test.todo('Menu appears when input is focused');
+    test.todo('Menu closes on click-away');
+    test.todo('Menu does not close on interaction with the menu'); // ensure no close-on-blur
+    test.todo('Menu options list narrows when text is entered');
+
+    test.todo('Up & Down arrow keys focus menu options');
+    test.todo('Tab key closes menu');
+    test.todo('Escape key closes menu');
+    test.todo('Enter key selects focused option');
+    test.todo('Space key selects focused option');
+
+    test.todo('Clicking an option sets selection');
+
+    describe('Single select', () => {
+      test.todo('Input value changes when a selection is made');
+      test.todo('Input value is set to selection value when menu closes');
+    });
+
+    describe('Multi select', () => {
+      test.todo('Left & Right arrow keys highlight chips');
+    });
+  });
+
+  describe('Chips', () => {
+    test.todo('Chips truncate properly...');
+  });
 });
