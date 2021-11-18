037389d9fa4c25729b5899d7cfcfb4bce0531f8c
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-11-18 16:11:48 -0500

    export sizeMap from glyphCommon

--- packages/icon/src/index.ts
+++ packages/icon/src/index.ts
@@ -3,6 +3,6 @@ import glyphs from './glyphs';
 
 export { default as createIconComponent } from './createIconComponent';
 export { default as glyphs } from './glyphs';
-export { Size } from './glyphCommon';
+export { Size, sizeMap } from './glyphCommon';
 export { isComponentGlyph } from './isComponentGlyph';
 export default createIconComponent(glyphs);
