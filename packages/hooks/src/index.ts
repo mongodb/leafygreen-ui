500df1c6f5bd5a426e135812f1205d1f9ee836f5
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-01-19 14:35:54 -0500

    [PD-1629 PD-1699] Combobox (#1059)
    * Scaffolds combobox
    
    * Basic combobox functionality
    
    * Adds focus management
    
    * Rough single select
    
    * Adds multiselect fuctionality
    
    * Improves focus & hover states
    
    * Adds empty, loading & error state handling
    
    * Fixes multiselect with icon style
    
    * Builds Chip component
    
    * Adds chip array
    
    * Enables overflow = scroll-x
    
    * Adds expand-x behavior
    
    * Handles expand-y
    
    * Handles expand-x
    
    * Adds todos
    
    * Adde error checking on initial value
    
    * Hide input in expand-x version
    
    * Scroll-x chips full bleed
    
    * fix typo
    
    * Moves styles
    
    * Add consoleonce
    
    * Improves selection state
    
    * Improves dynamic width
    
    * Adds clearable button
    
    * Improves clear behavior
    
    * Adds caret icon
    
    * Improve enum types
    
    * Adds error message & improves types
    
    * Improves typing
    
    * Fixes button styling and onChange callback value
    
    * More generic TS improvements
    
    * Gets default multiselect value working
    
    * Restores checkbox functionality
    
    * Adds truncation to Chips
    
    * Remove console.once
    
    * WIP - Scaffolds ComboboxGroup
    
    * Converts to using value based indexing
    
    * Implements groups
    
    * Enables filtering
    
    * Enables filtering & match highlighting
    
    * Adds keyMap & keyframes
    
    * Cleanup
    
    * Limits menu height & autoscrolls menu
    
    * Adds todo tests
    
    * when the menu closes, update the value
    
    * WIP - chip focus management
    
    * Gets Chip focus state working
    
    * minor cleanuo
    
    * Improves "No results found" logic
    
    * Convert enums to const
    
    * Allows controlled combobox
    
    * Moves flattenChildren to utils
    
    * Removes global event listener
    
    * fix style types
    
    * Removes "hasBeenOpened" flag
    
    * remove unised code
    
    * Opens menu on down arrow
    
    * Enables external filtering
    
    * Ensure invalid options passed as value aren't rendered
    
    * Better test organization
    
    * Improves test architecture & adds more tests
    
    * Adds more tests
    
    * Adds more tests
    
    * Convert tests to use expectSelection
    
    * Moves combobox utils to separate file
    
    * Adds more tests
    
    * Fix types
    
    * Adds keyboard tests
    
    * Fix types
    
    * Update ComboboxTestUtils.tsx
    
    * Adds more todo tests
    
    * Adds more tests
    
    * Adds chip tests
    
    * Resolves test
    
    * Fixes broken tests
    
    * Adds `disabled` tests
    
    * Resolves quick feedback
    
    * Updates the focused chip on remove
    
    * Removes `expectSelection`
    
    * Removing a chip sets focus to prev chip
    
    * tsc
    
    * Fix bold height error
    
    * Calls onChange on every update
    
    * Ensures left & right arrows move cursor in the text input
    
    * tsc fix
    
    * Improves arrow key tests
    
    * tsc
    
    * update icon import
    
    * Update Combobox.spec.tsx
    
    * Adds option wedge animation
    
    * Adds behavior: click on a chip (outside of the "x") to select the chip
    
    * Backspace focuses last chip
    
    * tsc
    
    * Adds combobox group tests
    
    * Adds new test (should fail CI)
    
    * Resolves failing test
    
    * Adds disabled clear button test (should fail)
    
    * Resolves clear button test
    
    * Changes default displayName to be `value`
    
    * Fix tsc
    
    * PD-1699 document combobox (#1076)
    
    * Adds Live example
    
    * Updates Readme
    
    * Adds inline type documentation
    
    * Adds tests in response to PR feedback
    
    * Refactors handleArrowKey
    
    * Adds todo test
    
    * Cleanup tests
    
    * Resolves tests re:PR feedback
    
    * remove uiColors import
    
    * Fixes test "Deletes text after making a single selection"
    
    * Update justification of popover
    
    * Removes isControlled variable
    
    * Add TS assertions
    
    * Open menu on keydown after making a selection

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -8,3 +8,5 @@ export { default as useObjectDependency } from './useObjectDependency';
 export { default as useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 export { default as useIdAllocator } from './useIdAllocator';
 export { default as useValidation } from './useValidation';
+export { default as useDynamicRefs } from './useDynamicRefs';
+export { useObservedRef, useForwardedRef } from './useForwardedRef';
