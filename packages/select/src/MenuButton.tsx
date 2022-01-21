93131df2b98817fe9bd5e72f9c028f3843930f89
Adam Thompson <adam.thompson@mongodb.com>
2022-01-21 16:33:58 -0500

    Fix focus states

--- packages/select/src/MenuButton.tsx
+++ packages/select/src/MenuButton.tsx
@@ -1,20 +1,16 @@
 import React, { useCallback, useContext } from 'react';
 import Button, { Variant } from '@leafygreen-ui/button';
 import { css, cx } from '@leafygreen-ui/emotion';
-import InteractionRing from '@leafygreen-ui/interaction-ring';
 import CaretDownIcon from '@leafygreen-ui/icon/dist/CaretDown';
 import { breakpoints, spacing } from '@leafygreen-ui/tokens';
 import { palette, uiColors } from '@leafygreen-ui/palette';
 import WarningIcon from '@leafygreen-ui/icon/dist/Warning';
+import { HTMLElementProps } from '@leafygreen-ui/lib';
+import { useUsingKeyboardContext } from '@leafygreen-ui/leafygreen-provider';
 import { colorSets, mobileSizeSet, Mode, sizeSets } from './styleSets';
 import SelectContext from './SelectContext';
 import { useForwardedRef } from './utils';
 import { State } from '.';
-import { HTMLElementProps } from '@leafygreen-ui/lib';
-
-const interactionRingStyle = css`
-  width: 100%;
-`;
 
 const menuButtonStyle = css`
   // reset default Button padding
@@ -38,8 +34,9 @@ const errorColor: Record<Mode, string> = {
 const menuButtonFocusStyle: Record<Mode, string> = {
   [Mode.Light]: css`
     &:focus {
-      box-shadow: none;
       border-color: transparent;
+      background-color: ${palette.white};
+      box-shadow: 0 0 0 3px ${palette.blue.light1};
     }
   `,
   [Mode.Dark]: css``,
@@ -105,6 +102,7 @@ const MenuButton = React.forwardRef<HTMLElement, Props>(function MenuButton(
   }: Props,
   forwardedRef,
 ) {
+  const { usingKeyboard: showFocus } = useUsingKeyboardContext();
   const { mode, open, size, disabled } = useContext(SelectContext);
 
   const ref = useForwardedRef(forwardedRef, null);
@@ -125,72 +123,62 @@ const MenuButton = React.forwardRef<HTMLElement, Props>(function MenuButton(
     ? __INTERNAL__menuButtonSlot__
     : Button;
 
-  const Wrapper = mode === Mode.Dark ? React.Fragment : InteractionRing;
-
   return (
-    <Wrapper
-      borderRadius={mode === Mode.Dark ? '4px' : '6px'}
-      darkMode={mode === Mode.Dark}
+    <Component
+      {...rest}
+      ref={ref}
+      name={name}
+      value={value}
       disabled={disabled}
-      className={interactionRingStyle}
+      onClick={onClick}
+      variant={Variant.Default}
+      darkMode={mode === Mode.Dark}
+      rightGlyph={<CaretDownIcon />}
+      data-testid="leafygreen-ui-select-menubutton"
+      className={cx(
+        menuButtonStyle,
+        css`
+          width: 100%;
+          @media only screen and (max-width: ${breakpoints.Desktop}px) {
+            height: ${mobileSizeSet.height}px;
+            font-size: ${mobileSizeSet.text}px;
+          }
+        `,
+        {
+          [css`
+            border-color: transparent;
+          `]: mode === Mode.Dark,
+          [css`
+            color: ${colorSet.text.deselected};
+          `]: deselected,
+          [menuButtonErrorStyle[mode]]: state === State.Error && !!errorMessage,
+          [menuButtonFocusStyle[mode]]: showFocus,
+        },
+      )}
     >
-      <Component
-        {...rest}
-        ref={ref}
-        name={name}
-        value={value}
-        disabled={disabled}
-        onClick={onClick}
-        variant={Variant.Default}
-        darkMode={mode === Mode.Dark}
-        rightGlyph={<CaretDownIcon />}
-        data-testid="leafygreen-ui-select-menubutton"
-        className={cx(
-          menuButtonStyle,
-          css`
-            width: 100%;
-            @media only screen and (max-width: ${breakpoints.Desktop}px) {
-              height: ${mobileSizeSet.height}px;
-              font-size: ${mobileSizeSet.text}px;
-            }
-          `,
-          {
-            [css`
-              border-color: transparent;
-            `]: mode === Mode.Dark,
-            [css`
-              color: ${colorSet.text.deselected};
-            `]: deselected,
-            [menuButtonErrorStyle[mode]]:
-              state === State.Error && !!errorMessage,
-          },
-          menuButtonFocusStyle[mode],
-        )}
+      <div
+        className={css`
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          flex-grow: 1;
+          width: 90%;
+        `}
       >
-        <div
-          className={css`
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            flex-grow: 1;
-            width: 90%;
-          `}
-        >
-          <div className={menuButtonTextStyle}>{text}</div>
-          {state === State.Error && errorMessage && (
-            <WarningIcon
-              role="presentation"
-              className={css`
-                color: ${errorColor[mode]};
-                margin-left: ${spacing[1]}px;
-              `}
-              size={sizeSet.warningIcon}
-            />
-          )}
-        </div>
-        {children}
-      </Component>
-    </Wrapper>
+        <div className={menuButtonTextStyle}>{text}</div>
+        {state === State.Error && errorMessage && (
+          <WarningIcon
+            role="presentation"
+            className={css`
+              color: ${errorColor[mode]};
+              margin-left: ${spacing[1]}px;
+            `}
+            size={sizeSet.warningIcon}
+          />
+        )}
+      </div>
+      {children}
+    </Component>
   );
 });
 
