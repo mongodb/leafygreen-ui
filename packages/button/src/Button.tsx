bd1342c59940148a59d59b5fe1f3f280dda91e00
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-01-25 16:20:21 -0500

    PD-1707 Refresh button (#1103)
    * Updates button styles
    
    * Create sixty-windows-listen.md
    
    * Updates ripple border radius
    
    * remove comment
    
    * Fixes button padding & spacing
    
    * Updates bg color for primary outline variant
    
    * Updates icon only hover/active color
    
    * Fix dark mode disabled icon color

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -2,16 +2,14 @@ import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Box, { ExtendableBox } from '@leafygreen-ui/box';
 import { css, cx } from '@leafygreen-ui/emotion';
-import { fontFamilies, spacing } from '@leafygreen-ui/tokens';
 import { registerRipple } from '@leafygreen-ui/ripple';
 import { useUsingKeyboardContext } from '@leafygreen-ui/leafygreen-provider';
 import { Variant, Size, ButtonProps, Mode } from './types';
-import { getClassName, colorMap } from './styles';
+import { getClassName, colorMap, ButtonDataProp } from './styles';
 import ButtonIcon from './ButtonIcon';
 
 const rippleStyle = css`
   overflow: hidden;
-  border-radius: 3px;
   position: absolute;
   top: 0;
   left: 0;
@@ -20,35 +18,31 @@ const rippleStyle = css`
 `;
 
 const containerChildStyles = css`
-  display: flex;
+  display: grid;
+  grid-auto-flow: column;
   align-items: center;
   height: 100%;
   width: 100%;
   pointer-events: none;
   position: relative;
   z-index: 0;
-  font-family: ${fontFamilies.default};
 `;
 
 const padding: Record<Size, string> = {
   [Size.XSmall]: css`
-    padding-left: 6px;
-    padding-right: 6px;
+    padding: 0 6px; // 8px - 2px (border)
   `,
 
   [Size.Small]: css`
-    padding-left: 12px;
-    padding-right: 12px;
+    padding: 0 10px; // 12px - 2px (border)
   `,
 
   [Size.Default]: css`
-    padding-left: 12px;
-    padding-right: 12px;
+    padding: 0 10px; // 12px - 2px (border)
   `,
 
   [Size.Large]: css`
-    padding-left: ${spacing[3]}px;
-    padding-right: ${spacing[3]}px;
+    padding: 0 14px; // 16px - 2px (border)
   `,
 };
 
@@ -126,6 +120,7 @@ const Button: ExtendableBox<
     // only add a disabled prop if not an anchor
     ...(typeof rest.href !== 'string' && { disabled }),
     'aria-disabled': disabled,
+    ...ButtonDataProp.prop,
     ...rest,
   } as const;
 
@@ -136,31 +131,30 @@ const Button: ExtendableBox<
   const content = (
     <>
       {/* Ripple cannot wrap children, otherwise components that rely on children to render dropdowns will not be rendered due to the overflow:hidden rule. */}
-      <div className={rippleStyle} ref={rippleRef} />
+      <div
+        className={cx(
+          rippleStyle,
+          css`
+            // TODO: Refresh - remove darkMode logic
+            border-radius: ${darkMode ? 3 : 6}px;
+          `,
+        )}
+        ref={rippleRef}
+      />
 
       <div
         className={cx(
           containerChildStyles,
-          {
-            [css`
-              justify-content: space-between;
-            `]: !!rightGlyph,
-            [css`
-              justify-content: center;
-            `]: !rightGlyph,
-          },
           padding[size],
+          css`
+            gap: ${iconSpacing};
+          `,
         )}
       >
         {leftGlyph && (
           <ButtonIcon
             glyph={leftGlyph}
-            className={cx(
-              { [css`margin-right: ${iconSpacing};}`]: !isIconOnlyButton },
-              css`
-                vertical-align: text-top;
-              `,
-            )}
+            className={''} // Prevents TS error
             {...iconProps}
           />
         )}
@@ -168,22 +162,11 @@ const Button: ExtendableBox<
         {children}
 
         {rightGlyph && (
-          <span
-            className={css`
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              margin-left: auto;
-            `}
-          >
-            <ButtonIcon
-              glyph={rightGlyph}
-              className={
-                !isIconOnlyButton ? css`margin-left: ${iconSpacing};}` : ''
-              }
-              {...iconProps}
-            />
-          </span>
+          <ButtonIcon
+            glyph={rightGlyph}
+            className={''} // Prevents TS error
+            {...iconProps}
+          />
         )}
       </div>
     </>
