abff5da5ef1feb034022cba39842932042f6b06a
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-01-26 14:27:27 -0500

    PD-1707 Refresh: Menu (#1099)
    * Updates basic typography elements
    
    * Update Link.tsx
    
    * Updates inline code
    
    * Create mean-rice-exercise.md
    
    * Test em and strong
    
    * Update inlineKeyCode
    
    * Update typography context values
    
    * Updates fonts & sizes
    
    * Updates base font size
    
    * Remove changes to Context
    
    * bumpVersionsWithWorkspaceProtocolOnly
    
    * Update TODO comments
    
    * update disabled styles
    
    * Updates menu colors
    
    * Updates sizing
    
    * Updates description wrapping
    
    * Updates submenu divider and focus styles
    
    * cleanup
    
    * remove uiColors
    
    * Fix sub menu padding
    
    * Fixes Title text styles
    
    * Fixes menu item height
    
    * remove console.log
    
    * Remove comment
    
    * Remove bold sub item on hover
    
    * Remove green glow on active focus
    
    * Updates pseudo-bold style to remove !important

--- packages/menu/src/styles.tsx
+++ packages/menu/src/styles.tsx
@@ -1,30 +1,37 @@
 import { css } from '@leafygreen-ui/emotion';
-import { uiColors } from '@leafygreen-ui/palette';
+import { fontFamilies } from '@leafygreen-ui/tokens';
+import { palette } from '@leafygreen-ui/palette';
 
-const indentation = 15;
-const leftBar = 5;
+const indentation = 16;
+const leftBar = 4;
 export const svgWidth = 24;
 export const menuItemPadding = 15;
 export const paddingLeft = 52;
 
+/**
+ * Base styles
+ */
 export const menuItemContainerStyle = css`
+  // Using vars to maintain proper specificity for sub-items
+  --lg-menu-item-text-color: ${palette.white};
   display: flex;
+  position: relative;
+  box-sizing: border-box;
   flex-direction: row;
   align-items: center;
+  width: 100%;
+  margin: unset;
   padding-left: ${indentation}px;
   padding-right: ${indentation}px;
-  text-decoration: none;
-  cursor: pointer;
-  text-decoration: none;
-  position: relative;
-  transition: background-color 150ms ease-in-out;
-  border: none;
-  margin: unset;
-  width: 100%;
-  font-family: Akzidenz, ‘Helvetica Neue’, Helvetica, Arial, sans-serif;
-  box-sizing: border-box;
-  background: ${uiColors.white};
+  font-family: ${fontFamilies.default};
+  font-size: 13px;
   text-align: left;
+  text-decoration: none;
+  color: ${palette.white};
+  cursor: pointer;
+  border: none;
+  background-color: ${palette.black};
+  transition: background-color 150ms ease-in-out;
 
   &:focus {
     outline: none;
@@ -35,61 +42,140 @@ export const menuItemContainerStyle = css`
     content: '';
     position: absolute;
     width: ${leftBar}px;
-    top: 0;
-    bottom: 0;
-    left: -1px;
+    height: 22px;
+    left: 0px;
+    border-radius: 0 ${leftBar}px ${leftBar}px 0;
     background-color: transparent;
     transition: background-color 150ms ease-in-out;
   }
 
   &:hover {
     text-decoration: none;
-    background-color: ${uiColors.gray.light2};
+    background-color: ${palette.gray.dark3};
     &:before {
-      background-color: ${uiColors.gray.light1};
     }
   }
 
   &:active {
-    background-color: ${uiColors.gray.light2};
+    background-color: ${palette.black};
     &:before {
-      background-color: ${uiColors.gray.light1};
+      background-color: ${palette.green.base};
     }
   }
 `;
 
+export const textContainer = css`
+  width: 100%;
+  overflow: hidden;
+  padding: 2px 0;
+`;
+
+export const mainIconStyle = css`
+  color: ${palette.gray.dark1};
+  margin-right: ${paddingLeft - svgWidth - menuItemPadding}px;
+  flex-shrink: 0;
+`;
+
+export const titleTextStyle = css`
+  width: 100%;
+  font-size: 13px;
+  font-weight: 500;
+  color: var(--lg-menu-item-text-color, ${palette.white});
+  // Define the default pseudo-bold styles
+  --lg-menu-pseudo-bold: 0.125px 0 var(--lg-menu-item-text-color),
+    -0.125px 0 var(--lg-menu-item-text-color),
+    0 0.125px var(--lg-menu-item-text-color),
+    0 -0.125px var(--lg-menu-item-text-color);
+`;
+
+export const descriptionTextStyle = css`
+  font-size: 13px;
+  font-weight: normal;
+  line-height: 16px;
+  color: ${palette.gray.light1};
+`;
+
+export const linkDescriptionTextStyle = css`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+/**
+ * Hover Styles
+ */
+
+export const getHoverStyles = (container: string) => ({
+  text: css`
+    ${container}:hover & {
+      // Using vars to maintain proper specificity for sub-items
+      --lg-menu-item-text-color: ${palette.green.base};
+      // Pseudo-bold so the text doesn't overflow on hover
+      // var is defined in \`*TextStyles\` for specificity
+      text-shadow: var(--lg-menu-pseudo-bold);
+    }
+  `,
+});
+
+/**
+ * Active styles
+ */
 export const activeMenuItemContainerStyle = css`
+  background-color: ${palette.black};
+
   &:before {
-    background-color: ${uiColors.green.base};
+    background-color: ${palette.green.base};
   }
 
   &:hover {
-    background-color: ${uiColors.gray.light3};
-    color: ${uiColors.green.dark3};
+    color: ${palette.green.base};
+
     &:before {
-      background-color: ${uiColors.green.base};
+      background-color: ${palette.green.base};
     }
   }
 `;
 
+export const activeTitleTextStyle = css`
+  font-weight: bold;
+  color: ${palette.white};
+  // no pseudo bold text when active */
+  --lg-menu-pseudo-bold: unset;
+`;
+export const activeDescriptionTextStyle = css`
+  color: ${palette.gray.light1};
+`;
+export const activeIconStyle = css`
+  color: ${palette.green.base};
+`;
+
+/**
+ * Disabled styles
+ */
 export const disabledMenuItemContainerStyle = css`
   cursor: not-allowed;
   pointer-events: none;
-  background-color: ${uiColors.gray.light3};
 
   &:hover:before {
     background-color: unset;
   }
 `;
 
+export const disabledTextStyle = css`
+  color: ${palette.gray.dark2};
+`;
+
+/**
+ * Focused styles
+ */
 export const focusedMenuItemContainerStyle = css`
   &:focus {
     text-decoration: none;
-    background-color: ${uiColors.blue.light3};
-    color: ${uiColors.blue.dark3};
+    background-color: ${palette.blue.dark3};
+    color: ${palette.white};
 
     &:before {
-      background-color: #63b0d0;
+      background-color: ${palette.blue.light1};
     }
   }
 
@@ -98,11 +184,27 @@ export const focusedMenuItemContainerStyle = css`
   }
 `;
 
+export const getFocusedStyles = (selector: string) => {
+  return {
+    textStyle: css`
+      ${selector}:focus & {
+        color: ${palette.white};
+      }
+    `,
+    descriptionStyle: css`
+      ${selector}:focus & {
+        color: ${palette.blue.light3};
+      }
+    `,
+    iconStyle: css`
+      ${selector}:focus > & {
+        color: ${palette.blue.light3};
+      }
+    `,
+  };
+};
+
 export const linkStyle = css`
   text-decoration: none;
   color: inherit;
-`;
-
-export const disabledTextStyle = css`
-  color: ${uiColors.gray.light1};
 `;
