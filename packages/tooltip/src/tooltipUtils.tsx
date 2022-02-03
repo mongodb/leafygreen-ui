fd5186bdb19af71d7bf7190e21086b3ed97caa0b
Adam Thompson <adam.thompson@mongodb.com>
2022-02-03 12:06:51 -0500

    Update comments to jsdoc

--- packages/tooltip/src/tooltipUtils.tsx
+++ packages/tooltip/src/tooltipUtils.tsx
@@ -23,29 +23,42 @@ export function notchPositionStyles(
   const notchStyleObj: Partial<Record<Styles, string>> = {};
   const containerStyleObj: Partial<Record<Styles, string>> = {};
 
-  // The bounds used to clamp the notchOffset value
+  /**
+   * The bounds used to clamp the notchOffset value
+   */
   const notchOffsetLowerBound = 5;
 
-  // This number is somewhat "magical", but adjusted for the Tooltip alignment.
-  // Calculating the exact value needed here requires setting a ref on the Tooltip content wrapper, and getting the height / width of it. The problem was that the height / width changes when the open prop is set, causing the notch to lose its positioning before the tooltip transitions out in some cases.
+  /**
+   * This number is somewhat "magical", but adjusted for the Tooltip alignment.
+   * Calculating the exact value needed here requires setting a ref on the Tooltip content wrapper, and getting the height / width of it.
+   * The problem was that the height / width changes when the open prop is set, causing the notch to lose its positioning before the tooltip transitions out in some cases.
+   */
   let notchOffsetUpperBound = notchOffsetLowerBound * 2;
 
-  // The un-clamped value that would exactly center the tooltip notch relative to the trigger.
+  /**
+   * The un-clamped value that would exactly center the tooltip notch relative to the trigger.
+   */
   let notchOffsetActual: number;
 
-  // The clamped value that makes a best-attempt to center the notch relative to the trigger,
-  // while also ensuring that the notch is positioned within the bounds of the tooltip itself,
-  // and still has the appearance of an alignment.
+  /**
+   * The clamped value that makes a best-attempt to center the notch relative to the trigger,
+   * while also ensuring that the notch is positioned within the bounds of the tooltip itself,
+   * and still has the appearance of an alignment.
+   */
   let notchOffset = 0;
 
-  // Boolean derived from the notchOffsetActual and notchOffsetLowerBound that determines if the trigger
-  // is small enough to make a transformation of the tooltip itself necessary.
+  /**
+   * Boolean derived from the notchOffsetActual and notchOffsetLowerBound that determines if the trigger
+   * is small enough to make a transformation of the tooltip itself necessary.
+   */
   let transformPosition: boolean;
 
-  // When the trigger is smaller than the minimum offset we require to position the notch over the trigger,
-  // we calculate a transformation to apply to the entire tooltip so that the notch centers on that element.
-  // This is particularly important for things like icons, and icon buttons where without this transformation,
-  // the tooltip's notch could be positioned entirely off of the trigger.
+  /**
+   * When the trigger is smaller than the minimum offset we require to position the notch over the trigger,
+   * we calculate a transformation to apply to the entire tooltip so that the notch centers on that element.
+   * This is particularly important for things like icons, and icon buttons where without this transformation,
+   * the tooltip's notch could be positioned entirely off of the trigger.
+   */
   let tooltipOffsetTransform = '';
 
   switch (align) {
