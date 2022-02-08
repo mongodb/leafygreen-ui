bf4e6d71cea515fc1e60ae321df459bee6a04052
Adam Thompson <adam.thompson@mongodb.com>
2022-02-08 13:02:43 -0500

    Removes "X" button from search input

--- packages/text-input/src/TextInput.tsx
+++ packages/text-input/src/TextInput.tsx
@@ -168,6 +168,22 @@ const inputStyle = css`
   &:disabled {
     cursor: not-allowed;
   }
+
+  /* clears the ‘X’ from Internet Explorer & Chrome */
+  &[type='search'] {
+    &::-ms-clear,
+    &::-ms-reveal {
+      display: none;
+      width: 0;
+      height: 0;
+    }
+    &::-webkit-search-decoration,
+    &::-webkit-search-cancel-button,
+    &::-webkit-search-results-button,
+    &::-webkit-search-results-decoration {
+      display: none;
+    }
+  }
 `;
 
 const inputIconStyle = css`
