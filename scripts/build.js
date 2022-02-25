b347155f7012684d6fa7b8860f34fee426490750
Adam Thompson <adam.thompson@mongodb.com>
2022-02-25 15:19:08 -0500

    Adds exclude package functionality

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,11 @@
 const { spawn, spawnSync } = require('child_process');
+const fs = require('fs');
+
+const getAllPackageNames = () =>
+  fs
+    .readdirSync(`${__dirname}/../packages`, { withFileTypes: true })
+    .filter(dir => dir.isDirectory())
+    .map(dir => dir.name);
 
 const cmdArgs = ['--parallel', 'build'];
 const args = process.argv.slice(2);
@@ -9,15 +16,24 @@ if (args.includes('--watch')) {
   cmdArgs.push('--', '--watch');
 }
 
+if (args.length > 0) {
+  let packageArgs;
+
+  // If we need to ignore some packages
+  if (args.some(arg => arg.startsWith('^'))) {
+    packageArgs = getAllPackageNames()
+      .filter(pkg => !args.includes(`^${pkg}`))
+      .flatMap(pkg => ['--scope', `@leafygreen-ui/${pkg}`]);
+  } else {
+    // Add only the package names passed in
+    packageArgs = args.flatMap(pkg => ['--scope', `@leafygreen-ui/${pkg}`]);
+  }
+  cmdArgs.unshift(...packageArgs);
+}
+
 // eslint-disable-next-line no-console
 console.log('Running pre-build...');
 spawnSync('yarn', ['pre-build', ...args], { stdio: 'inherit' });
-
-if (args.length > 0) {
-  // Add only the package names passed in
-  const packageArgs = args.flatMap(arg => ['--scope', `@leafygreen-ui/${arg}`]);
-  cmdArgs.unshift(...packageArgs);
-}
 
 // Run lerna
 const cmd = spawn('npx', ['lerna', 'run', ...cmdArgs], { stdio: 'inherit' });
