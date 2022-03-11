165b46a935411c55626a96fb9149c9c6f4e626a0
Anemy <rhysh@live.com>
2022-03-11 14:49:15 -0500

    Remove old closeOnBackdropClick prop docs

--- packages/modal/src/Modal.tsx
+++ packages/modal/src/Modal.tsx
@@ -252,7 +252,6 @@ interface ModalProps {
  * @param props.shouldClose Callback to determine whether or not Modal should close when user tries to close it.
  * @param props.className className applied to container div.
  * @param props.contentClassName className applied to overlay div.
- * @param props.closeOnBackdropClick Determines whether or not a Modal should close when a user clicks outside the modal.
  * @param props.initialFocus By default, when a focus trap is activated the first element in the focus trap's tab order will receive focus. With this option you can specify a different element to receive that initial focus. Selector string (which will be passed to document.querySelector() to find the DOM node).
  * @param props.closeIconColor Choose between dark or light close icon. Default is dark.
  */
