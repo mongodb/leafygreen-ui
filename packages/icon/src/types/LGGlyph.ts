109531d7e3385347f92d05cef2c5172800b3c85c
Adam Thompson <adam.thompson@mongodb.com>
2022-03-28 14:29:39 -0400

    Update LGGlyph.ts

--- packages/icon/src/types/LGGlyph.ts
+++ packages/icon/src/types/LGGlyph.ts
@@ -1,5 +1,5 @@
 import { ComponentType } from 'react';
-import * as SVGR from './svgr';
+import * as SVGR from './SVGR';
 
 export interface ComponentProps extends SVGR.ComponentProps {
   size?: number | 'small' | 'default' | 'large' | 'xlarge';
