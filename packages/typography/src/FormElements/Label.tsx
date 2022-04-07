d81146998e7bcc85a04b0ca920558d5b2e53896d
Adam Thompson <adam.thompson@mongodb.com>
2022-04-07 13:48:44 -0400

    Updates Form Elements for DarkMode

--- packages/typography/src/FormElements/Label.tsx
+++ packages/typography/src/FormElements/Label.tsx
@@ -1,73 +1,44 @@
 import React from 'react';
-import { HTMLElementProps } from '@leafygreen-ui/lib';
 import { css, cx } from '@leafygreen-ui/emotion';
-import { palette, uiColors } from '@leafygreen-ui/palette';
-
-import { fontFamilies } from '@leafygreen-ui/tokens';
-
-const Mode = {
-  Light: 'light',
-  Dark: 'dark',
-} as const;
-
-type Mode = typeof Mode[keyof typeof Mode];
-
-interface ColorSets {
-  labelStyle: string;
-  disabledLabelStyle: string;
-  descriptionStyle: string;
-  disabledDescriptionStyle: string;
-}
-
-// TODO: Refresh - move font-size & line-height back to common styles when darkMode gets redesigned
-const colorSets: Record<Mode, ColorSets> = {
-  [Mode.Light]: {
-    labelStyle: css`
-      font-family: ${fontFamilies.default};
-      color: ${palette.black};
-      font-size: 13px;
-      line-height: 20px;
-    `,
-    disabledLabelStyle: css`
-      color: ${palette.gray.base};
-    `,
-    descriptionStyle: css`
-      font-family: ${fontFamilies.default};
-      color: ${palette.gray.dark1};
-      font-size: 13px;
-      line-height: 20px;
-    `,
-    disabledDescriptionStyle: css`
-      color: ${palette.gray.base};
-    `,
-  },
-  [Mode.Dark]: {
-    labelStyle: css`
-      font-family: ${fontFamilies.legacy};
-      color: ${uiColors.white};
-      font-size: 14px;
-      line-height: 16px;
-      padding-bottom: 4px;
-    `,
-    disabledLabelStyle: css`
-      color: ${uiColors.gray.light1};
-    `,
-    descriptionStyle: css`
-      font-family: ${fontFamilies.legacy};
-      color: ${uiColors.gray.light1};
-      font-size: 14px;
-      line-height: 16px;
-      padding-bottom: 4px;
-    `,
-    disabledDescriptionStyle: css`
-      color: ${uiColors.gray.light1};
-    `,
-  },
-};
+import { HTMLElementProps } from '@leafygreen-ui/lib';
+import { BaseFontSize, fontFamilies, typeScales } from '@leafygreen-ui/tokens';
+import { Mode } from '../types';
+import { palette } from '@leafygreen-ui/palette';
+import { useBaseFontSize } from '@leafygreen-ui/leafygreen-provider';
 
 const labelStyle = css`
+  font-family: ${fontFamilies.default};
   font-weight: bold;
 `;
+
+const labelTypeScale: Record<BaseFontSize, string> = {
+  [BaseFontSize.Body1]: css`
+    font-size: ${typeScales.body1.fontSize};
+    line-height: ${typeScales.body1.lineHeight};
+  `,
+  [BaseFontSize.Body2]: css`
+    font-size: ${typeScales.body2.fontSize};
+    line-height: ${typeScales.body2.lineHeight};
+  `,
+};
+
+const labelColorStyle: Record<Mode, string> = {
+  [Mode.Light]: css`
+    color: ${palette.black};
+  `,
+  [Mode.Dark]: css`
+    color: ${palette.white};
+  `,
+};
+
+const disabledLabelColorStyle: Record<Mode, string> = {
+  [Mode.Light]: css`
+    color: ${palette.gray.base};
+  `,
+  [Mode.Dark]: css`
+    color: ${palette.gray.light1};
+  `,
+};
 
 type LabelProps = HTMLElementProps<'label', never> & {
   darkMode?: boolean;
@@ -75,21 +46,23 @@ type LabelProps = HTMLElementProps<'label', never> & {
   disabled?: boolean;
 };
 
-const Label = ({
+export const Label = ({
   darkMode = false,
   className,
   children,
   disabled = false,
   ...rest
 }: LabelProps) => {
+  const baseFontSize = useBaseFontSize() as BaseFontSize;
   const mode = darkMode ? Mode.Dark : Mode.Light;
 
   return (
     <label
       className={cx(
         labelStyle,
-        colorSets[mode].labelStyle,
-        { [colorSets[mode].disabledLabelStyle]: disabled },
+        labelColorStyle[mode],
+        labelTypeScale[baseFontSize],
+        { [disabledLabelColorStyle[mode]]: disabled },
         className,
       )}
       {...rest}
@@ -100,44 +73,3 @@ const Label = ({
 };
 
 Label.displayName = 'Label';
-
-const descriptionStyle = css`
-  font-weight: normal;
-  margin-top: 0;
-  margin-bottom: 0;
-`;
-
-type DescriptionProps = HTMLElementProps<'p', never> & {
-  darkMode?: boolean;
-  disabled?: boolean;
-};
-
-const Description = ({
-  darkMode = false,
-  disabled = false,
-  children,
-  className,
-  ...rest
-}: DescriptionProps) => {
-  const mode = darkMode ? Mode.Dark : Mode.Light;
-
-  return (
-    <p
-      className={cx(
-        descriptionStyle,
-        colorSets[mode].descriptionStyle,
-        {
-          [colorSets[mode].disabledDescriptionStyle]: disabled,
-        },
-        className,
-      )}
-      {...rest}
-    >
-      {children}
-    </p>
-  );
-};
-
-Description.displayName = 'Description';
-
-export { Label, Description };
