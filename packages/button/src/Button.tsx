be4c8e272a0dae8c2dfb43db28b90eaf5284f847
Sean Park <99368875+spark3-mdb@users.noreply.github.com>
2022-04-11 14:54:10 -0400

    Bump yarn lock (#1195)
    * new prettier fixes
    
    * ts updates
    
    * updating tsc and fixing errors
    
    Co-authored-by: Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -53,123 +53,121 @@ const containerChildSizeStyles: Record<Size, string> = {
   `,
 };
 
-const Button: ExtendableBox<
-  ButtonProps & { ref?: React.Ref<any> },
-  'button'
-> = React.forwardRef(function Button(
-  {
-    variant = Variant.Default,
-    size = Size.Default,
-    darkMode = false,
-    baseFontSize = 14,
-    disabled = false,
-    leftGlyph,
-    rightGlyph,
-    children,
-    className,
-    as,
-    type,
-    ...rest
-  }: ButtonProps,
-  forwardRef,
-) {
-  const { usingKeyboard: showFocus } = useUsingKeyboardContext();
-  const rippleRef = useRef<HTMLDivElement | null>(null);
+const Button: ExtendableBox<ButtonProps & { ref?: React.Ref<any> }, 'button'> =
+  React.forwardRef(function Button(
+    {
+      variant = Variant.Default,
+      size = Size.Default,
+      darkMode = false,
+      baseFontSize = 14,
+      disabled = false,
+      leftGlyph,
+      rightGlyph,
+      children,
+      className,
+      as,
+      type,
+      ...rest
+    }: ButtonProps,
+    forwardRef,
+  ) {
+    const { usingKeyboard: showFocus } = useUsingKeyboardContext();
+    const rippleRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    let unregisterRipple: (() => void) | undefined;
-    const backgroundColor =
-      rippleColors[darkMode ? Mode.Dark : Mode.Light][variant];
+    useEffect(() => {
+      let unregisterRipple: (() => void) | undefined;
+      const backgroundColor =
+        rippleColors[darkMode ? Mode.Dark : Mode.Light][variant];
 
-    if (rippleRef.current != null) {
-      unregisterRipple = registerRipple(rippleRef.current, {
-        backgroundColor,
-      });
-    }
+      if (rippleRef.current != null) {
+        unregisterRipple = registerRipple(rippleRef.current, {
+          backgroundColor,
+        });
+      }
 
-    return unregisterRipple;
-  }, [rippleRef, variant, darkMode]);
+      return unregisterRipple;
+    }, [rippleRef, variant, darkMode]);
 
-  const isIconOnlyButton = ((leftGlyph || rightGlyph) && !children) ?? false;
+    const isIconOnlyButton = ((leftGlyph || rightGlyph) && !children) ?? false;
 
-  const buttonClassName = getClassName({
-    variant,
-    size,
-    darkMode,
-    baseFontSize,
-    disabled,
-    showFocus,
+    const buttonClassName = getClassName({
+      variant,
+      size,
+      darkMode,
+      baseFontSize,
+      disabled,
+      showFocus,
+    });
+
+    const isAnchor: boolean = (!!rest.href || as === 'a') && !disabled;
+
+    const buttonProps = {
+      type: isAnchor ? undefined : type || 'button',
+      className: cx(buttonClassName, className),
+      ref: forwardRef,
+      // Provide a default value for the as prop
+      // If consumping application passes a value for as, it will override the default set here
+      as: (isAnchor ? 'a' : 'button') as keyof JSX.IntrinsicElements,
+      // only add a disabled prop if not an anchor
+      ...(typeof rest.href !== 'string' && { disabled }),
+      'aria-disabled': disabled,
+      ...ButtonDataProp.prop,
+      ...rest,
+    } as const;
+
+    const iconProps = { variant, size, darkMode, disabled, isIconOnlyButton };
+
+    const content = (
+      <>
+        {/* Ripple cannot wrap children, otherwise components that rely on children to render dropdowns will not be rendered due to the overflow:hidden rule. */}
+        <div
+          className={cx(rippleStyle, {
+            // TODO: Refresh - remove darkMode logic
+            [css`
+              border-radius: 3px;
+            `]: darkMode,
+          })}
+          ref={rippleRef}
+        />
+
+        <div
+          className={cx(containerChildStyles, containerChildSizeStyles[size], {
+            // TODO: Refresh - remove darkMode logic
+            [css`
+              justify-content: space-between;
+            `]: !!rightGlyph && darkMode,
+            [css`
+              font-family: ${fontFamilies.legacy};
+            `]: darkMode,
+          })}
+        >
+          {leftGlyph && (
+            <ButtonIcon
+              glyph={leftGlyph}
+              className={css`
+                justify-self: right;
+              `}
+              {...iconProps}
+            />
+          )}
+
+          {children}
+
+          {rightGlyph && (
+            <ButtonIcon
+              glyph={rightGlyph}
+              className={css`
+                justify-self: left;
+              `}
+              {...iconProps}
+            />
+          )}
+        </div>
+      </>
+    );
+
+    return <Box {...buttonProps}>{content}</Box>;
   });
-
-  const isAnchor: boolean = (!!rest.href || as === 'a') && !disabled;
-
-  const buttonProps = {
-    type: isAnchor ? undefined : type || 'button',
-    className: cx(buttonClassName, className),
-    ref: forwardRef,
-    // Provide a default value for the as prop
-    // If consumping application passes a value for as, it will override the default set here
-    as: (isAnchor ? 'a' : 'button') as keyof JSX.IntrinsicElements,
-    // only add a disabled prop if not an anchor
-    ...(typeof rest.href !== 'string' && { disabled }),
-    'aria-disabled': disabled,
-    ...ButtonDataProp.prop,
-    ...rest,
-  } as const;
-
-  const iconProps = { variant, size, darkMode, disabled, isIconOnlyButton };
-
-  const content = (
-    <>
-      {/* Ripple cannot wrap children, otherwise components that rely on children to render dropdowns will not be rendered due to the overflow:hidden rule. */}
-      <div
-        className={cx(rippleStyle, {
-          // TODO: Refresh - remove darkMode logic
-          [css`
-            border-radius: 3px;
-          `]: darkMode,
-        })}
-        ref={rippleRef}
-      />
-
-      <div
-        className={cx(containerChildStyles, containerChildSizeStyles[size], {
-          // TODO: Refresh - remove darkMode logic
-          [css`
-            justify-content: space-between;
-          `]: !!rightGlyph && darkMode,
-          [css`
-            font-family: ${fontFamilies.legacy};
-          `]: darkMode,
-        })}
-      >
-        {leftGlyph && (
-          <ButtonIcon
-            glyph={leftGlyph}
-            className={css`
-              justify-self: right;
-            `}
-            {...iconProps}
-          />
-        )}
-
-        {children}
-
-        {rightGlyph && (
-          <ButtonIcon
-            glyph={rightGlyph}
-            className={css`
-              justify-self: left;
-            `}
-            {...iconProps}
-          />
-        )}
-      </div>
-    </>
-  );
-
-  return <Box {...buttonProps}>{content}</Box>;
-});
 
 Button.displayName = 'Button';
 
