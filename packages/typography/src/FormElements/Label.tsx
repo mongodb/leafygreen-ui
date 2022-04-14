8a93bd4d1a712a5887c92ca9d685ca07be330a0e
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-04-14 13:44:47 -0400

    PD-1941 form elements font size fix (#1205)
    * Fixes bad type casting and adds a temp hook
    
    * Update docs
    
    * Update useUpdatedBaseFontSize.ts
    
    * Updates import paths
    
    * Adds docs for useUpdatedBaseFontSize

--- packages/typography/src/FormElements/Label.tsx
+++ packages/typography/src/FormElements/Label.tsx
@@ -4,7 +4,7 @@ import { HTMLElementProps } from '@leafygreen-ui/lib';
 import { BaseFontSize, fontFamilies, typeScales } from '@leafygreen-ui/tokens';
 import { Mode } from '../types';
 import { palette } from '@leafygreen-ui/palette';
-import { useBaseFontSize } from '@leafygreen-ui/leafygreen-provider';
+import { useUpdatedBaseFontSize } from '../useUpdatedBaseFontSize';
 
 const labelStyle = css`
   font-family: ${fontFamilies.default};
@@ -53,7 +53,7 @@ export const Label = ({
   disabled = false,
   ...rest
 }: LabelProps) => {
-  const baseFontSize = useBaseFontSize() as BaseFontSize;
+  const baseFontSize = useUpdatedBaseFontSize();
   const mode = darkMode ? Mode.Dark : Mode.Light;
 
   return (
