a648409d1cc3cde99c922738a5ea1a1fb858d6b4
Adam Thompson <adam.thompson@mongodb.com>
2022-04-14 12:13:54 -0400

    Adds docs for useUpdatedBaseFontSize

--- .changeset/dull-countries-greet.md
+++ .changeset/dull-countries-greet.md
@@ -3,3 +3,5 @@
 ---
 
 Updates `Label` and `Description` for Dark Mode Refresh
+
+Exports a temporary `useUpdatedBaseFontSize` hook to convert the values returned from `useBaseFontSize` to the new `tokens`
--- packages/typography/src/useUpdatedBaseFontSize.ts
+++ packages/typography/src/useUpdatedBaseFontSize.ts
@@ -2,12 +2,19 @@ import { useBaseFontSize } from '@leafygreen-ui/leafygreen-provider';
 import { BaseFontSize } from '@leafygreen-ui/tokens';
 
 /**
- * A wrapper around the hook useBaseFontSize that returns
- * a value that is consistent with the updated base font sizes.
+ * A temporary wrapper around {@link useBaseFontSize}
  *
- * Prefer using {@link useBaseFontSize} when LeafygreenProvider is updated to support the new values.
+ * Returns a font size token consistent with the visual brand refresh.
+ *
+ * Once all components have been updated for the refresh,
+ * this hook will be removed, and {@link useBaseFontSize} updated
+ * to return the updated tokens.
+ *
+ * @deprecated
+ *
+ * @returns - {@link BaseFontSize}
  */
 export const useUpdatedBaseFontSize = (): BaseFontSize => {
   const baseFontSize = useBaseFontSize();
-  return baseFontSize === 16 ? baseFontSize : 13;
+  return baseFontSize === 16 ? BaseFontSize.Body2 : BaseFontSize.Body1;
 };
