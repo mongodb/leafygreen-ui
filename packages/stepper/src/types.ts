43dd8e1aa979abc754d56c7ac0012992c1830f4d
Sean Park <s.park@mongodb.com>
2022-04-18 14:53:09 -0400

    type Mode

--- packages/stepper/src/types.ts
+++ packages/stepper/src/types.ts
@@ -12,6 +12,7 @@ export const Mode = {
   Light: 'light',
   Dark: 'dark',
 } as const;
+export type Mode = typeof Mode[keyof typeof Mode];
 
 export enum StepStates {
   CompletedMultiple = 'completed-multiple',
