2c6db19dcdb20baf2d7962d4aa58550a78956dbe
Sean Park <s.park@mongodb.com>
2022-04-18 18:35:25 -0400

    fixing svg build bug

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,4 +11,17 @@ module.exports = {
   ],
   framework: '@storybook/react',
   staticDirs: ['./static'],
+  webpackFinal: config => { 
+    // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;  
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    });
+
+    return config;
+  } 
 };
--- packages/icon/src/Icon.story.tsx
+++ packages/icon/src/Icon.story.tsx
@@ -29,12 +29,16 @@ storiesOf('Packages/Icons', module).add('Icon', () => {
   const fill = color('Fill', '#000000');
   const size = select('size', Object.values(Size), Size.Default);
 
-  const renderGlyph = (glyph: string) => (
-    <div key={glyph} className={containerStyle}>
-      <Icon glyph={glyph} fill={fill} size={size} />
-      <div className={textStyle}>{glyph}</div>
-    </div>
-  );
+  console.log(glyphs)
 
-  return <>{Object.keys(glyphs).map(renderGlyph)}</>;
+  return (
+    <>
+      {Object.keys(glyphs).map((glyph) => (
+        <div key={glyph} className={containerStyle}>
+          <Icon glyph={glyph} fill={fill} size={size} />
+          <div className={textStyle}>{glyph}</div>
+        </div>
+      ))}
+    </>
+  );
 });
