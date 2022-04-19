02db62a43df87705ba18cc9eb3b73bdbe11833b0
Shaneeza <shaneeza.ali@mongodb.com>
2022-04-19 14:22:35 -0400

    remove comments

--- packages/leafygreen-provider/src/PortalContext.tsx
+++ packages/leafygreen-provider/src/PortalContext.tsx
@@ -33,8 +33,6 @@ export default function PortalContextProvider({
   popover = defaultPortalContextValues.popover,
   children,
 }: PortalContext) {
-  // eslint-disable-next-line no-console
-  console.log(popover);
   return (
     <PortalContext.Provider value={{ popover }}>
       {children}
