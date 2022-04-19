cad898fb9d3939b3f0614d0852cd1c282d1a62de
Sean Park <s.park@mongodb.com>
2022-04-19 10:44:23 -0400

    moving styles out of component definition

--- packages/stepper/src/StepIcon.tsx
+++ packages/stepper/src/StepIcon.tsx
@@ -33,8 +33,64 @@ const StepIconGlyph = ({ state, content }: StepIconProps) => {
   }
 };
 
+// Light Mode Styles
+const completedStyles = css`
+  color: ${palette.white};
+  border-color: ${palette.green.dark1};
+  background-color: ${palette.green.dark1};
+`;
+
+const currentStyles = css`
+  color: ${palette.green.dark2};
+  background-color: ${palette.white};
+  border-color: ${palette.green.dark1};
+`;
+
+const upcomingStyles = css`
+  color: ${palette.gray.dark1};
+  background-color: ${palette.white};
+  border-color: ${palette.gray.dark1};
+`;
+
+// Dark Mode Styles
+const darkCompletedStyles = css`
+  color: ${palette.black};
+  border-color: ${palette.green.base};
+  background-color: ${palette.green.base};
+`;
+
+const darkCurrentStyles = css`
+  color: ${palette.green.base};
+  background-color: ${palette.black};
+  border-color: ${palette.green.base};
+`;
+
+const darkUpcomingStyles = css`
+  color: ${palette.gray.light1};
+  background-color: ${palette.black};
+  border-color: ${palette.gray.light1};
+`;
+
+const styles = {
+  [Mode.Dark]: {
+    [StepStates.CompletedMultiple]: darkCompletedStyles,
+    [StepStates.Completed]: darkCompletedStyles,
+    [StepStates.Current]: darkCurrentStyles,
+    [StepStates.Upcoming]: darkUpcomingStyles,
+    [StepStates.UpcomingMultiple]: darkUpcomingStyles,
+  },
+  [Mode.Light]: {
+    [StepStates.CompletedMultiple]: completedStyles,
+    [StepStates.Completed]: completedStyles,
+    [StepStates.Current]: currentStyles,
+    [StepStates.Upcoming]: upcomingStyles,
+    [StepStates.UpcomingMultiple]: upcomingStyles,
+  },
+};
+
 const StepIcon = ({ state, size, ...rest }: StepIconProps) => {
   const { isDarkMode } = useStepperContext();
+
   const baseStyles = css`
     width: ${size}px;
     height: ${size}px;
@@ -53,61 +109,6 @@ const StepIcon = ({ state, size, ...rest }: StepIconProps) => {
       width: 100%;
     }
   `;
-
-  // Light Mode Styles
-  const completedStyles = css`
-    color: ${palette.white};
-    border-color: ${palette.green.dark1};
-    background-color: ${palette.green.dark1};
-  `;
-
-  const currentStyles = css`
-    color: ${palette.green.dark2};
-    background-color: ${palette.white};
-    border-color: ${palette.green.dark1};
-  `;
-
-  const upcomingStyles = css`
-    color: ${palette.gray.dark1};
-    background-color: ${palette.white};
-    border-color: ${palette.gray.dark1};
-  `;
-
-  // Dark Mode Styles
-  const darkCompletedStyles = css`
-    color: ${palette.black};
-    border-color: ${palette.green.base};
-    background-color: ${palette.green.base};
-  `;
-
-  const darkCurrentStyles = css`
-    color: ${palette.green.base};
-    background-color: ${palette.black};
-    border-color: ${palette.green.base};
-  `;
-
-  const darkUpcomingStyles = css`
-    color: ${palette.gray.light1};
-    background-color: ${palette.black};
-    border-color: ${palette.gray.light1};
-  `;
-
-  const styles = {
-    [Mode.Dark]: {
-      [StepStates.CompletedMultiple]: darkCompletedStyles,
-      [StepStates.Completed]: darkCompletedStyles,
-      [StepStates.Current]: darkCurrentStyles,
-      [StepStates.Upcoming]: darkUpcomingStyles,
-      [StepStates.UpcomingMultiple]: darkUpcomingStyles,
-    },
-    [Mode.Light]: {
-      [StepStates.CompletedMultiple]: completedStyles,
-      [StepStates.Completed]: completedStyles,
-      [StepStates.Current]: currentStyles,
-      [StepStates.Upcoming]: upcomingStyles,
-      [StepStates.UpcomingMultiple]: upcomingStyles,
-    },
-  };
 
   return (
     <div
