15ba485f21415f8384464c452ee01b130177550e
Shaneeza <shaneeza.ali@mongodb.com>
2022-04-20 17:11:22 -0400

    add context to modal

--- packages/leafygreen-provider/src/index.ts
+++ packages/leafygreen-provider/src/index.ts
@@ -3,6 +3,10 @@ export {
   default as PortalContextProvider,
   usePopoverPortalContainer,
 } from './PortalContext';
+export {
+  usePopoverContext,
+  default as PopoverProvider,
+} from './PopoverContext';
 export { useBaseFontSize } from './TypographyContext';
 
 export { default } from './LeafyGreenContext';
