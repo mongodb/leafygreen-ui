bcef15785be88888e8c4edc693d097831488fc5f
Sean Park <99368875+spark3-mdb@users.noreply.github.com>
2022-04-25 12:18:30 -0400

    Updating stories for Badge, Button, Banner, Callout (#1226)
    * updating stories for Badge, Button, Banner, Callout
    
    * adding variants and changing default args

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,6 +13,7 @@ import {
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
