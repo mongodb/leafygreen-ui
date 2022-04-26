182a70d7fe17dacb20840329e376c9f313568809
Sean Park <99368875+spark3-mdb@users.noreply.github.com>
2022-04-26 16:52:15 -0400

    PD-1993: Stories for Box, Card, Checkbox, Code (#1230)
    * updating stories for Badge, Button, Banner, Callout
    
    * adding variants and changing default args
    
    * Box story
    
    * Card story
    
    * Checkbox story
    
    * Code story

--- packages/checkbox/src/Checkbox.story.tsx
+++ packages/checkbox/src/Checkbox.story.tsx
@@ -1,74 +1,26 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text, boolean } from '@storybook/addon-knobs';
 import { css } from '@leafygreen-ui/emotion';
-import { uiColors } from '@leafygreen-ui/palette';
-import LeafygreenProvider from '@leafygreen-ui/leafygreen-provider';
+import { Story } from '@storybook/react';
+import React from 'react';
 import Checkbox from '.';
+import { CheckboxProps } from './types';
 
-function Control() {
-  const [checked, setChecked] = React.useState(false);
-  const darkMode = boolean('darkMode', false);
+export default {
+  title: 'Packages/Checkbox',
+  component: Checkbox,
+  parameters: {
+    controls: { exclude: ['children'] },
+  },
+};
 
-  return (
-    <Checkbox
-      darkMode={darkMode}
-      checked={checked}
-      disabled={boolean('Disabled', false)}
-      indeterminate={boolean('Indeterminate', false)}
-      animate={boolean('Animate', true)}
-      onChange={() => setChecked(curr => !curr)}
-      label={text('Label', 'I agree to this thing.')}
-      className={css`
-        padding: 20px;
-        background-color: ${darkMode ? uiColors.gray.dark3 : uiColors.white};
-      `}
-    />
-  );
-}
+const Template: Story<CheckboxProps> = args => <Checkbox {...args} />;
 
-storiesOf('Packages/Checkbox', module)
-  .add('Uncontrolled', () => {
-    const darkMode = boolean('darkMode', false);
-
-    return (
-      <LeafygreenProvider>
-        <Checkbox
-          darkMode={darkMode}
-          disabled={boolean('Disabled', false)}
-          indeterminate={boolean('Indeterminate', false)}
-          animate={boolean('Animate', true)}
-          label={text('Label', 'I agree to this thing.')}
-          description={text(
-            'Description',
-            'Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
-          )}
-          className={css`
-            padding: 20px;
-            background-color: ${darkMode
-              ? uiColors.gray.dark3
-              : uiColors.white};
-            max-width: 512px;
-          `}
-        />
-      </LeafygreenProvider>
-    );
-  })
-  .add('Controlled', () => (
-    <LeafygreenProvider>
-      <Control />
-    </LeafygreenProvider>
-  ))
-  .add('No Label', () => {
-    return (
-      <LeafygreenProvider>
-        <Checkbox
-          aria-label="Some Check"
-          animate={true}
-          className={css`
-            padding: 20px;
-          `}
-        />
-      </LeafygreenProvider>
-    );
-  });
+export const Basic = Template.bind({});
+Basic.args = {
+  animate: true,
+  label: 'I agree to this thing.',
+  description:
+    'Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
+  className: css`
+    max-width: 700px;
+  `,
+};
