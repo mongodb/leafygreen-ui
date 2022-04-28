b5a32c72617ab0c74a8df38bb34a12e7b1c1a05d
Sean Park <s.park@mongodb.com>
2022-04-28 16:31:10 -0400

    Copyable story

--- packages/copyable/src/Copyable.story.tsx
+++ packages/copyable/src/Copyable.story.tsx
@@ -1,36 +1,19 @@
 import React from 'react';
-import { boolean, select, text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react';
-import LeafygreenProvider from '@leafygreen-ui/leafygreen-provider';
-import Copyable, { Size } from '.';
+import Copyable from '.';
 
-storiesOf('Packages/Copyable', module).add('Default', () => {
-  const darkMode = boolean('Dark mode', false);
-  const label = text('Label', 'Label');
-  const description = text('Description', 'Description') || undefined;
-  const children = text('Text', 'npm install @leafygreen-ui/copyable');
-  const isCopyable = boolean('Copyable', true);
-  const size = select('Size', Object.values(Size), 'default');
-  const baseFontSize = select('Base Font Size', [14, 16], 14);
+export default {
+  title: 'Packages/Copyable',
+  component: Copyable,
+  args: {
+    isCopyable: true,
+  }
+};
 
-  return (
-    <LeafygreenProvider baseFontSize={baseFontSize}>
-      <div
-        style={{
-          backgroundColor: darkMode ? '#21313C' : undefined,
-          padding: 48,
-        }}
-      >
-        <Copyable
-          label={label}
-          description={description}
-          darkMode={darkMode}
-          copyable={isCopyable}
-          size={size}
-        >
-          {children}
-        </Copyable>
-      </div>
-    </LeafygreenProvider>
-  );
-});
+const Template = args => <div><Copyable {...args} /></div>;
+
+export const Basic = Template.bind({})
+Basic.args = {
+  label: 'Label',
+  description: 'Description',
+  children: 'npm install @leafygreen-ui/copyable',
+}
