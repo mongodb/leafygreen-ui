b751b6418c0e8cb35e2bd414cee2208e4ce48591
Adam Thompson <adam.thompson@mongodb.com>
2022-04-28 16:13:48 -0400

    Update Tokens.story.tsx

--- packages/tokens/src/Tokens.story.tsx
+++ packages/tokens/src/Tokens.story.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { css } from '@leafygreen-ui/emotion';
 import { palette } from '@leafygreen-ui/palette';
-import { H2 } from '@leafygreen-ui/typography';
+import { H2, InlineCode } from '@leafygreen-ui/typography';
 import { spacing } from '.';
 import hoverRing from './hoverRing';
 import { Mode } from './mode';
@@ -14,11 +14,6 @@ import { focusRing } from '../dist';
 
 const gutter = css`
   margin-left: ${spacing[3]}px;
-`;
-
-const textWrapper = css`
-  margin-bottom: ${spacing[2]}px;
-  font-size: 10px;
 `;
 
 const colors = [
@@ -47,9 +42,9 @@ const spacingBlockVariants = Object.keys(spacing).reduce(
 function SpacingBlock({ space }: { space: keyof typeof spacing }) {
   return (
     <div className={gutter}>
-      <div
-        className={textWrapper}
-      >{`spacing[${space}]: ${spacing[space]}`}</div>
+      <InlineCode>
+        spacing[{space}]: {spacing[space]}
+      </InlineCode>
       <div className={spacingBlockVariants[space]}></div>
     </div>
   );
@@ -57,16 +52,63 @@ function SpacingBlock({ space }: { space: keyof typeof spacing }) {
 
 storiesOf('Packages/Tokens', module)
   .add('Spacing', () => (
-    <div
-      className={css`
-        display: flex;
-      `}
-    >
-      {Object.keys(spacing).map(space => (
-        <SpacingBlock
-          space={space as PropertyKey as keyof typeof spacing}
-          key={space}
-        />
+    <div>
+      <H2>Spacing</H2>
+      <div
+        className={css`
+          display: flex;
+        `}
+      >
+        {Object.keys(spacing).map(space => (
+          <SpacingBlock
+            space={space as PropertyKey as keyof typeof spacing}
+            key={space}
+          />
+        ))}
+      </div>
+    </div>
+  ))
+  .add('Typescales', () => {
+    return (
+      <div>
+        <H2>Typescales</H2>
+        {Object.keys(typeScales).map(scale => (
+          <div
+            key={scale}
+            className={css`
+              font-family: ${scale.includes('code')
+                ? fontFamilies.code
+                : fontFamilies.default};
+              font-size: ${typeScales[scale].fontSize}px;
+              line-height: ${typeScales[scale].lineHeight}px;
+              margin: ${spacing[3]}px;
+            `}
+          >
+            <InlineCode>
+              typeScales.{scale}: {JSON.stringify(typeScales[scale])}
+            </InlineCode>{' '}
+            <div>{scale}</div>
+          </div>
+        ))}
+      </div>
+    );
+  })
+  .add('Font Families', () => (
+    <div>
+      <H2>Font Families</H2>
+      {Object.keys(fontFamilies).map(family => (
+        <div
+          key={family}
+          className={css`
+            font-family: ${fontFamilies[family]};
+            margin: ${spacing[3]}px;
+          `}
+        >
+          <InlineCode>
+            fontFamilies.{family}: {fontFamilies[family]}
+          </InlineCode>
+          <div>{family}</div>
+        </div>
       ))}
     </div>
   ))
@@ -76,14 +118,14 @@ storiesOf('Packages/Tokens', module)
 
     const modeWrapper = (mode: Mode) => css`
       display: flex;
-      gap: 8px;
+      gap: ${spacing[2]}px;
       color: ${mode === 'dark' ? palette.white : palette.black};
       background-color: ${mode === 'dark' ? palette.black : palette.white};
       border: 1px solid
         ${mode === 'dark' ? palette.gray.light3 : palette.gray.dark3};
-      border-radius: 16px;
-      padding: 32px;
-      margin: 8px;
+      border-radius: ${spacing[3]}px;
+      padding: ${spacing[4]}px;
+      margin: ${spacing[2]}px;
     `;
 
     const buttonBase = css`
@@ -92,8 +134,8 @@ storiesOf('Packages/Tokens', module)
       outline: none;
       background-color: unset;
       border: unset;
-      padding: 8px;
-      border-radius: 4px;
+      padding: ${spacing[2]}px;
+      border-radius: ${spacing[1]}px;
       cursor: pointer;
       transition: box-shadow 100ms ease-in-out;
     `;
