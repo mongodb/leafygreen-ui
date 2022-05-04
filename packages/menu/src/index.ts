6ac28ffa257b8842c5b7f3812c44ac1242890d98
Sean Park <s.park@mongodb.com>
2022-05-04 17:56:04 -0400

    Menu story

--- packages/menu/src/index.ts
+++ packages/menu/src/index.ts
@@ -1,7 +1,8 @@
-import Menu from './Menu';
+import Menu, { MenuProps } from './Menu';
 import MenuSeparator from './MenuSeparator';
 import MenuItem from './MenuItem';
 import FocusableMenuItem from './FocusableMenuItem';
 import SubMenu from './SubMenu';
 
 export { Menu, MenuSeparator, MenuItem, FocusableMenuItem, SubMenu };
+export type { MenuProps };
