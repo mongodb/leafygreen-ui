0703c342828f67d388e9677fbd72c74c6f1a9f56
Adam Thompson <adam.thompson@mongodb.com>
2022-05-09 14:19:21 -0400

    Better fix to SSR tests

--- packages/combobox/src/Combobox.spec.tsx
+++ packages/combobox/src/Combobox.spec.tsx
@@ -21,10 +21,6 @@ import {
 } from './ComboboxTestUtils';
 import { OptionObject, wrapJSX } from './util';
 
-// Removes HTML comments added by SSR from `innerHTML`
-const getInnerHTML = (element: HTMLElement) =>
-  element.innerHTML.replace(/<!--.*?-->/g, '');
-
 /**
  * Tests
  */
@@ -33,70 +29,100 @@ describe('packages/combobox/utils', () => {
     test('Wraps the matched string in the provided element', () => {
       const JSX = wrapJSX('Apple', 'pp', 'em');
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`A<em>pp</em>le`);
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(1);
+      expect(ems[0]).toHaveTextContent('pp');
+      expect(container).toHaveTextContent('Apple');
     });
     test('Wraps the entire string when it matches', () => {
       const JSX = wrapJSX('Apple', 'Apple', 'em');
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`<em>Apple</em>`);
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(1);
+      expect(ems[0]).toHaveTextContent('Apple');
+      expect(container).toHaveTextContent('Apple');
     });
     test('Keeps case consistent with source', () => {
       const JSX = wrapJSX('Apple', 'aPPlE', 'em');
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`<em>Apple</em>`);
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(1);
+      expect(ems[0]).toHaveTextContent('Apple');
+      expect(container).toHaveTextContent('Apple');
     });
     // No match
     test('Wraps nothing when there is no match', () => {
       const JSX = wrapJSX('Apple', 'q', 'em');
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`Apple`);
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(0);
+      expect(container).toHaveTextContent('Apple');
     });
 
     // Multiple matches
     test('wraps all instances of a match', () => {
       const JSX = wrapJSX('Pepper', 'p', 'em');
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(
-        `<em>P</em>e<em>p</em><em>p</em>er`,
-      );
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(3);
+      expect(ems[0]).toHaveTextContent('P');
+      expect(ems[1]).toHaveTextContent('p');
+      expect(ems[2]).toHaveTextContent('p');
+      expect(container).toHaveTextContent('Pepper');
     });
     test('wraps all instances of longer match', () => {
       const JSX = wrapJSX('Pepper', 'pe', 'em');
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`<em>Pe</em>p<em>pe</em>r`);
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(2);
+      expect(ems[0]).toHaveTextContent('Pe');
+      expect(ems[1]).toHaveTextContent('pe');
+      expect(container).toHaveTextContent('Pepper');
     });
 
     // No `wrap`
     test('Returns the input string when "wrap" is empty', () => {
       const JSX = wrapJSX('Apple', '', 'em');
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`Apple`);
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(0);
+      expect(container).toHaveTextContent(`Apple`);
     });
     test('Returns the input string when "wrap" is `undefined`', () => {
       const JSX = wrapJSX('Apple', undefined, 'em');
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`Apple`);
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(0);
+      expect(container).toHaveTextContent(`Apple`);
     });
 
     // No `element`
     test('Returns the input string when "element" is empty', () => {
       const JSX = wrapJSX('Apple', 'ap', '' as keyof HTMLElementTagNameMap);
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`Apple`);
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(0);
+      expect(container).toHaveTextContent(`Apple`);
     });
     test('Returns the input string when "element" is undefined', () => {
       const JSX = wrapJSX('Apple', 'ap');
       const { container } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`Apple`);
+      const ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(0);
+      expect(container).toHaveTextContent(`Apple`);
     });
 
     test('Updates after a second call', () => {
       const JSX = wrapJSX('Apple', 'p', 'em');
       const { container, rerender } = render(JSX);
-      expect(getInnerHTML(container)).toEqual(`A<em>p</em><em>p</em>le`);
+      let ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(2);
       const JSX2 = wrapJSX('Apple', 'pp', 'em');
       rerender(JSX2);
-      expect(getInnerHTML(container)).toEqual(`A<em>pp</em>le`);
+      ems = container.querySelectorAll('em');
+      expect(ems).toHaveLength(1);
+      expect(ems[0]).toHaveTextContent('pp');
+      expect(container).toHaveTextContent(`Apple`);
     });
   });
 });
