2b478f7124a94d80971a4fbec4fd0eaca16c63b5
spark33 <seanpark1107@gmail.com>
2022-05-10 14:29:04 -0400

    Portal + RadioBoxGroup

--- packages/popover/src/Popover.story.tsx
+++ packages/popover/src/Popover.story.tsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { Meta } from '@storybook/react';
 import { css } from '@leafygreen-ui/emotion';
+import { uiColors } from '@leafygreen-ui/palette';
 import Popover, { Align, Justify } from '.';
+
+const popoverStyle = css`
+  border: 1px solid ${uiColors.gray.light1};
+  text-align: center;
+  padding: 20px;
+  max-height: 100%;
+  overflow: hidden;
+  // Reset these properties since they'll be inherited
+  // from the container element when not using a portal.
+  font-size: initial;
+  color: initial;
+  background-color: initial;
+`;
 
 const scrollableStyle = css`
   height: 200vh;
@@ -34,152 +48,17 @@ const Template = ({ buttonText, ...args }) => {
   return (
     <button onClick={() => setActive(active => !active)}>
       {buttonText}
-      <Popover
-          {...args}
-          active={active}
-        >
-          <div className={popoverStyle}>Popover content</div>
-        </Popover>
+      <Popover {...args} active={active}>
+        <div className={popoverStyle}>Popover content</div>
+      </Popover>
     </button>
-  )
-}
+  );
+};
 
-export const Basic = Template.bind({})
+export const Basic = Template.bind({});
 
-export const ScrollableContainer = (args) => (
-  <div className={scrollableStyle}><Template {...args} /></div>
-)
-
-// export function DefaultExample() {
-//   const [active, setActive] = useState(false);
-//   const exampleIsScrollable = boolean('Example page scrolling', false);
-
-//   return (
-//     <div className={cx({ [scrollableStyle]: exampleIsScrollable })}>
-//       <button
-//         onClick={() => setActive(!active)}
-//         className={cx(
-//           containerStyle,
-//           referenceElPositions[
-//             select(
-//               'Reference Element Position',
-//               ['centered', 'top', 'right', 'bottom', 'left'],
-//               'centered',
-//             )
-//           ],
-//         )}
-//       >
-//         {text('Button Content', 'Popover')}
-//         <Popover
-//           align={select('Align', Object.values(Align), 'top')}
-//           justify={select('justify', Object.values(Justify), 'start')}
-//           active={active}
-//           usePortal={boolean('usePortal', true)}
-//           spacing={number('spacing', 10)}
-//           adjustOnMutation={boolean('adjustOnMutation', false)}
-//         >
-//           <div className={popoverStyle}>Popover content</div>
-//         </Popover>
-//       </button>
-//     </div>
-//   );
-// }
-
-// function AdvancedExample() {
-//   const [active, setActive] = useState(false);
-//   const refEl = useRef(null);
-
-//   return (
-//     <>
-//       <button
-//         ref={refEl}
-//         onClick={() => setActive(!active)}
-//         className={cx(
-//           containerStyle,
-//           referenceElPositions[
-//             select(
-//               'Reference Element Position',
-//               ['centered', 'top', 'right', 'bottom', 'left'],
-//               'centered',
-//             )
-//           ],
-//         )}
-//       >
-//         {text('Button Content', 'Popover')}
-//       </button>
-
-//       <Popover
-//         align={select('Align', Object.values(Align), 'top')}
-//         justify={select('justify', Object.values(Justify), 'start')}
-//         active={active}
-//         usePortal={boolean('usePortal', true)}
-//         spacing={number('spacing', 10)}
-//         adjustOnMutation={boolean('adjustOnMutation', false)}
-//         popoverZIndex={number('popoverZIndex', 1)}
-//         refEl={refEl}
-//       >
-//         <div className={popoverStyle}>Popover content</div>
-//       </Popover>
-//     </>
-//   );
-// }
-
-// function ScrollExample() {
-//   const [portalContainer, setPortalContainer] = useState<HTMLDivElement | null>(
-//     null,
-//   );
-//   const [active, setActive] = useState(false);
-//   const buttonRef = useRef(null);
-
-//   return (
-//     <>
-//       <div
-//         ref={el => setPortalContainer(el)}
-//         className={css`
-//           margin: 150px;
-//           width: 100%;
-//           overflow-y: auto;
-//           background-color: #eaeaea;
-//           max-height: calc(100vh - 200px);
-//           padding: 100px;
-//           position: relative;
-//         `}
-//       >
-//         <button
-//           ref={buttonRef}
-//           onClick={() => setActive(curr => !curr)}
-//           className={cx(
-//             containerStyle,
-//             referenceElPositions[
-//               select(
-//                 'Reference Element Position',
-//                 ['centered', 'top', 'right', 'bottom', 'left'],
-//                 'centered',
-//               )
-//             ],
-//           )}
-//         >
-//           {text('Button Content', 'Popover')}
-//         </button>
-//       </div>
-
-//       <Popover
-//         align={select('Align', Object.values(Align), 'top')}
-//         justify={select('justify', Object.values(Justify), 'start')}
-//         spacing={number('spacing', 10)}
-//         adjustOnMutation={boolean('adjustOnMutation', false)}
-//         portalContainer={portalContainer}
-//         scrollContainer={portalContainer}
-//         active={active}
-//         refEl={buttonRef}
-//       >
-//         <div className={popoverStyle}>Popover content</div>
-//       </Popover>
-//     </>
-//   );
-// }
-
-// // storiesOf('Packages/Popover', module)
-// //   .add('Default', () => <DefaultExample />)
-// //   .add('Advanced', () => <AdvancedExample />)
-// //   .add('Scroll Container', () => <ScrollExample />);
+export const ScrollableContainer = args => (
+  <div className={scrollableStyle}>
+    <Template {...args} />
+  </div>
+);
