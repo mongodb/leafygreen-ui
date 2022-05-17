ac27e84c6f6c0b4bc64c6d593a61a07931b042b9
spark33 <seanpark1107@gmail.com>
2022-05-17 15:34:06 -0400

    use getAllPackages script from rollup

--- scripts/parse-tsdocs.js
+++ scripts/parse-tsdocs.js
@@ -1,16 +1,27 @@
 const docgen = require('react-docgen-typescript');
 const fs = require('fs');
+const path = require('path');
 
 const options = {
   savePropValueAsString: true,
 };
 
-const parsedPackages = ['radio-group'];
+function getAllPackages(dir) {
+  const dirList = fs.readdirSync(dir);
+
+  return dirList
+    .map(subDir => `${path.resolve(dir, subDir)}/package.json`)
+    .filter(packageJsonPath => fs.existsSync(packageJsonPath))
+    .map(packageJsonPath => require(packageJsonPath).name);
+}
+
+const dirPath = path.resolve(__dirname, '../packages');
+const packagePrefix = `@leafygreen-ui/`;
 
 async function getFilenames() {
   const parsedFileNames = [];
-  await parsedPackages.forEach(async pkg => {
-    const dirPath = `./packages/${pkg}/src`;
+  await getAllPackages(dirPath).forEach(async pkg => {
+    const dirPath = `./packages/${pkg.substring(packagePrefix.length)}/src`;
     const dir = await fs.readdirSync(dirPath);
     const regex = /^(?!.*\.(spec|d|story|stories)\.tsx?$).*\.tsx?$/;
     const filenames = dir.filter(filename => regex.test(filename));
@@ -20,6 +31,8 @@ async function getFilenames() {
 }
 
 getFilenames().then(filenames => {
+  // eslint-disable-next-line no-console
+  console.log(filenames);
   const docs = docgen.parse(filenames, options);
   // TODO: Replace log with push to CMS
   // eslint-disable-next-line no-console
