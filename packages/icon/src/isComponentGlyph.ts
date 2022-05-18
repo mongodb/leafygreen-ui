2670e4db71d500824b0fe9a2e7105c003eecb015
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-05-18 16:53:13 -0400

    PD-1620 Adds `isGlyph` property to generic Icon (#1243)
    * Moves template.ts
    
    * overloads isComponentGlyph
    
    * createX functions set appropriate isGlyph property
    
    * Update Select.story.tsx
    
    * Adds changeset
    
    * Revert to ternary
    
    * Creates LGGlyph.Element type
    
    * Update tsconfig.json
    
    * Create silent-snails-invent.md

--- packages/icon/src/isComponentGlyph.ts
+++ packages/icon/src/isComponentGlyph.ts
@@ -1,13 +1,34 @@
+import { ComponentType, isValidElement, ReactNode } from 'react';
 import { LGGlyph } from './types';
 
+type ExtendedComponentType = ComponentType<any> & {
+  [key: string]: any;
+};
 /** Helper type to check if element is a LeafyGreen UI Glyph  */
-export function isComponentGlyph(
-  child: React.ReactNode,
-): child is React.ReactElement<LGGlyph.ComponentProps> {
+function isComponentGlyph(node: ReactNode): node is LGGlyph.Element;
+function isComponentGlyph(
+  component: ExtendedComponentType,
+): component is LGGlyph.Component;
+function isComponentGlyph(
+  child: ReactNode | ExtendedComponentType,
+): child is LGGlyph.Element | LGGlyph.Component {
+  // If we're received a rendered component (i.e. ReactNode)
+  if (isValidElement(child)) {
+    return (
+      child != null &&
+      typeof child === 'object' &&
+      'type' in child &&
+      (child.type as any).isGlyph === true
+    );
+  }
+
+  // If we've recieved a component function
   return (
     child != null &&
-    typeof child === 'object' &&
-    'type' in child &&
-    (child.type as any).isGlyph === true
+    typeof child === 'function' &&
+    'isGlyph' in child &&
+    child.isGlyph === true
   );
 }
+
+export { isComponentGlyph };
