fd2f6de0160b9b303c989f773793cea8c79f7acf
Sean Park <seanpark1107@gmail.com>
2022-05-26 17:13:06 -0400

    Storybook config updates (#1285)
    * WIP
    
    * moving Button over to use BaseFontSize token
    
    * storybook and tsdoc updates
    
    * lint
    
    * banner description
    
    * updates
    
    * removing uncontrolled modal stories
    
    * prep for release

--- packages/marketing-modal/src/MarketingModal.story.tsx
+++ packages/marketing-modal/src/MarketingModal.story.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentStory, Meta } from '@storybook/react';
+import defaultArgTypes from '../../../stories/defaultArgTypes';
 import MarketingModal, { BlobPosition, GraphicStyle } from '.';
 import { CloseIconColor } from '@leafygreen-ui/modal';
+import Button from '@leafygreen-ui/button';
 
 export default {
   title: 'Packages/Modals/Marketing Modal',
@@ -16,40 +18,29 @@ export default {
       'This is some description text, and it is extra long so it fills up this modal. Another thing about the modals here.',
   },
   argTypes: {
-    open: {
-      control: false,
-    },
-    setOpen: {
-      control: false,
-    },
-    onClose: {
-      control: false,
-    },
-    children: {
-      control: 'string',
-    },
-    graphic: {
-      control: false,
-    },
+    open: { control: false },
+    setOpen: { control: false },
+    onClose: { control: false },
+    graphic: { control: false },
     graphicStyle: {
       control: 'radio',
-      options: [GraphicStyle.Center, GraphicStyle.Fill],
+      options: Object.values(GraphicStyle),
     },
     closeIconColor: {
       control: 'radio',
       options: Object.values(CloseIconColor),
     },
-    showBlob: {
-      control: 'boolean',
-    },
+    showBlob: { control: 'boolean' },
     blobPosition: {
       control: 'radio',
       options: Object.values(BlobPosition),
     },
+    children: defaultArgTypes.children,
+    darkMode: defaultArgTypes.darkMode,
   },
 } as Meta<typeof MarketingModal>;
 
-const UncontrolledTemplate: ComponentStory<typeof MarketingModal> = ({
+const ControlledTemplate: ComponentStory<typeof MarketingModal> = ({
   graphicStyle,
   darkMode,
   ...args
@@ -60,29 +51,35 @@ const UncontrolledTemplate: ComponentStory<typeof MarketingModal> = ({
   const graphicFillImage = darkMode
     ? 'Realm_Rebrand_Image.png'
     : 'marketing-fill-light.jpg';
-
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
   return (
-    <MarketingModal
-      {...args}
-      graphicStyle={graphicStyle}
-      darkMode={darkMode}
-      graphic={
-        graphicStyle === GraphicStyle.Center ? (
-          <img
-            alt=""
-            src={`examples/${graphicCenterImage}`}
-            width={darkMode ? 275 : 278}
-            height={darkMode ? 220 : 252.6}
-          />
-        ) : (
-          <img alt="Marketing Modal" src={`examples/${graphicFillImage}`} />
-        )
-      }
-    />
+    <>
+      <Button onClick={() => setOpen(!open)}>Open Modal</Button>
+      <MarketingModal
+        {...args}
+        graphicStyle={graphicStyle}
+        darkMode={darkMode}
+        graphic={
+          graphicStyle === GraphicStyle.Center ? (
+            <img
+              alt=""
+              src={`examples/${graphicCenterImage}`}
+              width={darkMode ? 275 : 278}
+              height={darkMode ? 220 : 252.6}
+            />
+          ) : (
+            <img alt="Marketing Modal" src={`examples/${graphicFillImage}`} />
+          )
+        }
+        open={open}
+        onClose={handleClose}
+      />
+    </>
   );
 };
 
-export const Uncontrolled = UncontrolledTemplate.bind({});
-Uncontrolled.args = {
+export const Basic = ControlledTemplate.bind({});
+Basic.args = {
   open: true,
 };
