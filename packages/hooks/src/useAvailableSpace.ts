e59ac899ad333a4b53e7b3f5fa70dac4c94a21c3
Adam Thompson <adam.thompson@mongodb.com>
2022-06-03 18:03:01 -0400

    Creates useAvailableSpace hook

---
+++ packages/hooks/src/useAvailableSpace.ts
@@ -0,0 +1,35 @@
+import { useMemo } from 'react';
+import useViewportSize from './useViewportSize';
+
+/**
+ * Returns the maximum space available above or below the a source trigger
+ * @param triggerRef The `ref` object attached to the source trigger
+ * @param margin The space around the trigger
+ */
+const useAvailableSpace = (
+  triggerRef: React.RefObject<HTMLElement>,
+  margin = 8,
+) => {
+  const viewportSize = useViewportSize();
+
+  return useMemo(() => {
+    if (viewportSize && triggerRef.current) {
+      // Get the top & bottom coordinates of the trigger
+      const { top: triggerTop, bottom: triggerBottom } =
+        triggerRef.current.getBoundingClientRect();
+
+      // Find out how much space is available above or below the trigger
+      const safeSpace = Math.max(
+        viewportSize.height - triggerBottom,
+        triggerTop,
+      );
+
+      // Return to fill the space available
+      return safeSpace - margin;
+    }
+
+    return 0;
+  }, [viewportSize, triggerRef, margin]);
+};
+
+export default useAvailableSpace;
