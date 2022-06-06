e5a61fe6a3824e27d54e82f6d959ce0423fd5e28
Adam Thompson <adam.thompson@mongodb.com>
2022-06-06 16:33:22 -0400

    refactor combobox group

--- packages/combobox/src/ComboboxGroup.tsx
+++ packages/combobox/src/ComboboxGroup.tsx
@@ -1,20 +1,21 @@
 import { css, cx } from '@leafygreen-ui/emotion';
 import { useIdAllocator } from '@leafygreen-ui/hooks';
 import { uiColors } from '@leafygreen-ui/palette';
+import { Mode } from '@leafygreen-ui/tokens';
 import React, { useContext } from 'react';
 import { ComboboxGroupProps } from './Combobox.types';
 import { ComboboxContext } from './ComboboxContext';
 
-const comboboxGroupStyle = (darkMode: boolean) => css`
-  --lg-combobox-group-label-color: ${darkMode
-    ? uiColors.gray.light1
-    : uiColors.gray.dark1};
-  --lg-combobox-group-border-color: ${darkMode
-    ? uiColors.gray.dark1
-    : uiColors.gray.light1};
-  padding-top: 8px;
-  border-bottom: 1px solid var(--lg-combobox-group-border-color);
-`;
+const comboboxGroupStyle: Record<Mode, string> = {
+  [Mode.Light]: css`
+    padding-top: 8px;
+    border-bottom: 1px solid ${uiColors.gray.light1};
+  `,
+  [Mode.Dark]: css`
+    padding-top: 8px;
+    border-bottom: 1px solid ${uiColors.gray.dark1};
+  `,
+};
 
 const comboboxGroupLabel = css`
   cursor: default;
@@ -27,8 +28,16 @@ const comboboxGroupLabel = css`
   font-weight: bold;
   text-transform: uppercase;
   letter-spacing: 0.4px;
-  color: var(--lg-combobox-group-label-color);
 `;
+
+const comboboxGroupLabelModeStyle: Record<Mode, string> = {
+  [Mode.Light]: css`
+    color: ${uiColors.gray.dark1};
+  `,
+  [Mode.Dark]: css`
+    color: ${uiColors.gray.light1};
+  `,
+};
 
 export function InternalComboboxGroup({
   label,
@@ -36,13 +45,17 @@ export function InternalComboboxGroup({
   children,
 }: ComboboxGroupProps): JSX.Element {
   const { darkMode } = useContext(ComboboxContext);
+  const mode = darkMode ? Mode.Dark : Mode.Light;
 
   const groupId = useIdAllocator({ prefix: 'combobox-group' });
   const childCount = React.Children.count(children);
 
   return childCount > 0 ? (
-    <div className={cx(comboboxGroupStyle(darkMode), className)}>
-      <div className={comboboxGroupLabel} id={groupId}>
+    <div className={cx(comboboxGroupStyle[mode], className)}>
+      <div
+        className={cx(comboboxGroupLabel, comboboxGroupLabelModeStyle[mode])}
+        id={groupId}
+      >
         {label}
       </div>
       <div role="group" aria-labelledby={groupId}>
