f6c1c80efc2350e7321837cd9e95d010b6a9b0a9
Shaneeza <shaneeza.ali@mongodb.com>
2022-06-21 13:08:02 -0400

    update card darkmode styles

--- packages/card/src/Card.tsx
+++ packages/card/src/Card.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { transparentize } from 'polished';
 import PropTypes from 'prop-types';
 import { css, cx } from '@leafygreen-ui/emotion';
-import { palette, uiColors } from '@leafygreen-ui/palette';
+import { palette } from '@leafygreen-ui/palette';
 import Box, { BoxProps, ExtendableBox } from '@leafygreen-ui/box';
-import { fontFamilies } from '@leafygreen-ui/tokens';
+import { fontFamilies, focusRing } from '@leafygreen-ui/tokens';
 
 export const ContentStyle = {
   None: 'none',
@@ -33,13 +33,10 @@ const lightHoverBoxShadow = `0 4px 20px -4px ${transparentize(
   0.8,
   palette.black,
 )}`;
-const lightFocusBoxShadow = `
-  0 0 0 2px ${palette.white},
-  0 0 0 4px ${palette.blue.light1}
-`;
-const darkBaseBoxShadow = `0 4px 20px -4px  ${transparentize(0.3, '#000')}`;
-const darkHoverBoxShadow = `0 2px 12px -2px ${transparentize(0.1, '#000')}`;
-const darkFocusBoxShadow = `0 0 0 3px ${uiColors.focus}`;
+const lightFocusBoxShadow = focusRing['light'].default;
+const darkBaseBoxShadow = `0 4px 10px -4px  ${transparentize(0.4, '#000D13')}`;
+const darkHoverBoxShadow = `0 4px 20px -4px ${transparentize(0.3, '#000000')}`;
+const darkFocusBoxShadow = focusRing['dark'].default;
 
 const colorSet: Record<Mode, ColorSet> = {
   [Mode.Light]: {
@@ -48,13 +45,6 @@ const colorSet: Record<Mode, ColorSet> = {
       box-shadow: ${lightBaseBoxShadow};
       background-color: white;
       color: ${palette.gray.dark3};
-      // TODO: Refresh - remove properties from mode logic
-      border-radius: 24px;
-      font-family: ${fontFamilies.default};
-      font-size: 13px;
-      line-height: 20px;
-      padding: 24px;
-      min-height: 68px; // 48px + 20px (padding + line-height)
     `,
     clickableStyle: css`
       cursor: pointer;
@@ -77,14 +67,10 @@ const colorSet: Record<Mode, ColorSet> = {
   },
   [Mode.Dark]: {
     containerStyle: css`
-      border: 1px solid ${uiColors.gray.dark2};
+      border: 1px solid ${palette.gray.dark2};
       box-shadow: ${darkBaseBoxShadow};
-      background-color: ${uiColors.gray.dark2};
-      color: ${uiColors.white};
-      // TODO: Refresh - remove properties from dark mode logic
-      border-radius: 7px;
-      font-family: ${fontFamilies.legacy};
-      padding: 16px;
+      background-color: ${palette.black};
+      color: ${palette.white};
     `,
     clickableStyle: css`
       cursor: pointer;
@@ -109,6 +95,12 @@ const containerStyle = css`
   position: relative;
   transition: 150ms ease-in-out;
   transition-property: border, box-shadow;
+  border-radius: 24px;
+  font-family: ${fontFamilies.default};
+  font-size: 13px;
+  line-height: 20px;
+  padding: 24px;
+  min-height: 68px; // 48px + 20px (padding + line-height)
 `;
 
 export interface CardProps {
