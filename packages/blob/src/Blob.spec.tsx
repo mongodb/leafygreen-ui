823238584273b3cf61ad0ac6e476737debdb5ace
Adam Thompson <adam.thompson@mongodb.com>
2022-07-19 12:11:53 -0400

    Move to separate function

--- packages/blob/src/Blob.spec.tsx
+++ packages/blob/src/Blob.spec.tsx
@@ -1,10 +1,7 @@
-
 import React from 'react';
 import { render } from '@testing-library/react';
 import Blob from '.';
 
 describe('packages/blob', () => {
-  test('condition', () => {
-
-  })
-})
+  test('condition', () => {});
+});
--- packages/blob/src/utils.ts
+++ packages/blob/src/utils.ts
@@ -1,38 +1,111 @@
-import { blobCode, Coordinate, Direction } from "./types";
+import { blobCode, Coordinate, ExtendedDirection } from './types';
 
-export const makeHasAdjacency = (shape: blobCode) => {
-  return ([row, col]: Coordinate, direction: Direction) => {
+export const makeClosureDependentUtils = (shape: blobCode) => {
+  function hasAdjacency([row, col]: Coordinate) {
+    return (direction: ExtendedDirection) => {
+      if (!shape) return;
 
-    if (!shape) return
+      // For small Dots
+      switch (direction) {
+        case 'top': {
+          const hasAdjacency = indexExists(row - 1, col)
+            ? shape[row - 1][col] !== '_'
+            : false;
+          return hasAdjacency;
+        }
 
-    // For small Dots
-    switch (direction) {
-      case 'top': {
-        const hasAdjacency = indexExists(row - 1, col) ? shape[row - 1][col] !== '_' : false
-        return hasAdjacency
+        case 'topRight': {
+          const hasAdjacency = indexExists(row - 1, col + 1)
+            ? shape[row - 1][col + 1] !== '_'
+            : false;
+          return hasAdjacency;
+        }
+
+        case 'right': {
+          const hasAdjacency = indexExists(row, col + 1)
+            ? shape[row][col + 1] !== '_'
+            : false;
+          return hasAdjacency;
+        }
+
+        case 'bottomRight': {
+          const hasAdjacency = indexExists(row + 1, col + 1)
+            ? shape[row + 1][col + 1] !== '_'
+            : false;
+          return hasAdjacency;
+        }
+
+        case 'bottom': {
+          const hasAdjacency = indexExists(row + 1, col)
+            ? shape[row + 1][col] !== '_'
+            : false;
+          return hasAdjacency;
+        }
+
+        case 'bottomLeft': {
+          const hasAdjacency = indexExists(row + 1, col - 1)
+            ? shape[row + 1][col - 1] !== '_'
+            : false;
+          return hasAdjacency;
+        }
+
+        case 'left': {
+          const hasAdjacency = indexExists(row, col - 1)
+            ? shape[row][col - 1] !== '_'
+            : false;
+          return hasAdjacency;
+        }
+
+        case 'topLeft': {
+          const hasAdjacency = indexExists(row - 1, col - 1)
+            ? shape[row - 1][col - 1] !== '_'
+            : false;
+          return hasAdjacency;
+        }
+
+        default:
+          break;
       }
+    };
+  }
 
-      case 'bottom': {
-        const hasAdjacency = indexExists(row + 1, col) ? shape[row + 1][col] !== '_' : false
-        return hasAdjacency
-      }
+  function indexExists(row: number, col: number) {
+    return shape && shape[row] && shape[row][col];
+  }
 
-      case 'left': {
-        const hasAdjacency = indexExists(row, col - 1) ? shape[row][col - 1] !== '_' : false
-        return hasAdjacency
-      }
+  function isEmpty(row: number, col: number) {
+    return indexExists(row, col) && shape[row][col] === '_';
+  }
 
-      case 'right': {
-        const hasAdjacency = indexExists(row, col + 1) ? shape[row][col + 1] !== '_' : false
-        return hasAdjacency
-      }
+  function isSmallDot(row: number, col: number) {
+    return indexExists(row, col) && shape[row][col] === 'o';
+  }
 
-      default:
-        break;
+  function isLargeDot(row: number, col: number) {
+    return indexExists(row, col) && shape[row][col] === 'O';
+  }
+
+  /**
+   * Finds the top-right-most dot
+   */
+  function findStart(row = 0, col = 0): [number, number] {
+    if (isSmallDot(row, col) || isLargeDot(row, col)) {
+      return [row, col];
+    } else {
+      return (
+        findStart(row, col + 1) ||
+        findStart(row + 1, col) ||
+        findStart(row + 1, col + 1)
+      );
     }
   }
 
-  function indexExists (x: number, y: number) {
-    return shape && shape[x] && shape[x][y]
-  }
-}
+  return {
+    hasAdjacency,
+    indexExists,
+    isEmpty,
+    isSmallDot,
+    isLargeDot,
+    findStart,
+  };
+};
