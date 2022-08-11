76372deb63950af0582c7e7b31b6ad70abe1a535
Adam Thompson <adam.thompson@mongodb.com>
2022-08-11 15:21:18 -0400

    lint

--- packages/lib/src/createUniqueClassName/index.ts
+++ packages/lib/src/createUniqueClassName/index.ts
@@ -6,8 +6,8 @@
  * @param prefix: a custom prefix that follows the `lg-ui` prefix.
  */
 const generateUniqueClassName = (prefix: string) => {
-  const count = classNameRegistry.get(prefix)?.length ?? 0
-  const postfix = count.toString().padStart(4, '0')
+  const count = classNameRegistry.get(prefix)?.length ?? 0;
+  const postfix = count.toString().padStart(4, '0');
   return `lg-ui${prefix ? `-${prefix}` : ''}-${postfix}`;
 };
 
@@ -15,14 +15,17 @@ const classNameRegistry: Map<string, Array<string>> = new Map();
 
 const createUniqueClassName = (prefix: string = ''): string => {
   const uniqueClassName = generateUniqueClassName(prefix);
-  
+
   if (classNameRegistry.has(prefix)) {
-    const prefixClasses = classNameRegistry.get(prefix)
-    classNameRegistry.set(prefix, [uniqueClassName, ...prefixClasses as Array<string>])
+    const prefixClasses = classNameRegistry.get(prefix);
+    classNameRegistry.set(prefix, [
+      uniqueClassName,
+      ...(prefixClasses as Array<string>),
+    ]);
   } else {
     classNameRegistry.set(prefix, [uniqueClassName]);
   }
-  return uniqueClassName
+  return uniqueClassName;
 };
 
 export default createUniqueClassName;
