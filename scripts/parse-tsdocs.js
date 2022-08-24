0dba1ef49bf787021f83cd32f473bbb7ef7ffe4b
Adam Thompson <adam.thompson@mongodb.com>
2022-08-24 11:08:42 -0400

    lint

--- scripts/parse-tsdocs.js
+++ scripts/parse-tsdocs.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-console */
 const docgen = require('react-docgen-typescript');
 const fs = require('fs');
 const path = require('path');
-const chalk = require('chalk')
-const { Command } = require('commander')
+const chalk = require('chalk');
+const { Command } = require('commander');
 
 const cli = new Command('parse-tsdoc')
   .arguments('[packages]')
   .option('-r, --root <path>', 'Source packages directory', '../packages')
   .parse(process.argv);
 
-const packagesRoot = cli.opts()['root']
+const packagesRoot = cli.opts()['root'];
 const skippedComponents = [];
 
 const TSDocOptions = {
@@ -21,7 +22,7 @@ const TSDocOptions = {
     if (skippedComponents.includes(component.name)) {
       return false;
     }
-    
+
     if (prop.declarations !== undefined && prop.declarations.length > 0) {
       const hasPropAdditionalDescription = prop.declarations.find(
         declaration => {
@@ -37,54 +38,62 @@ const TSDocOptions = {
 };
 
 if (cli.args.length) {
-  cli.args.forEach(parseDocs)
+  cli.args.forEach(parseDocs);
 } else {
   const packagesDir = path.resolve(__dirname, packagesRoot);
   const packages = fs.readdirSync(packagesDir);
-  packages.forEach(parseDocs)
-} 
+  packages.forEach(parseDocs);
+}
 
 function parseDocs(componentName) {
-  const componentDir = path.resolve(__dirname, `${packagesRoot}/${componentName}`)
-  
+  const componentDir = path.resolve(
+    __dirname,
+    `${packagesRoot}/${componentName}`,
+  );
+
   if (fs.existsSync(componentDir)) {
-    console.log(chalk.blueBright(`Parsing TSDoc for:`, chalk.blue.bold(`${componentName}`)))
-    const componentFileNames = parseFileNames(componentDir)
+    console.log(
+      chalk.blueBright(
+        `Parsing TSDoc for:`,
+        chalk.blue.bold(`${componentName}`),
+      ),
+    );
+    const componentFileNames = parseFileNames(componentDir);
     const docs = docgen.parse(componentFileNames, TSDocOptions);
-    const outFilePath = path.resolve(__dirname, componentDir + '/tsdoc.json')
+    const outFilePath = path.resolve(__dirname, componentDir + '/tsdoc.json');
     fs.writeFileSync(outFilePath, JSON.stringify(docs, null, 2), err => {
       if (err) console.error(err);
     });
   } else {
     console.warn(
-      chalk.yellow('Could not find component:'), 
+      chalk.yellow('Could not find component:'),
       chalk.bold.yellow(`\`${componentName}\``),
-      chalk.yellow('in'), 
+      chalk.yellow('in'),
       chalk.bold.yellow(packagesRoot),
-    )
+    );
   }
 }
 
 function parseFileNames(root) {
   const parsedFileNames = [];
   getFilesRecursively(root);
-  return parsedFileNames
-  
+  return parsedFileNames;
+
   function getFilesRecursively(directory) {
     const filesInDirectory = fs.readdirSync(directory);
-  
+
     for (const file of filesInDirectory) {
       const absolute = path.join(directory, file);
-  
+
       if (fs.statSync(absolute).isDirectory()) {
         getFilesRecursively(absolute);
       } else {
         const regex = /^(?!.*\.(spec|d|story|stories)\.tsx?$).*\.tsx?$/;
-  
+
         if (regex.test(absolute)) {
           parsedFileNames.push(absolute);
         }
       }
     }
-  };
+  }
 }
