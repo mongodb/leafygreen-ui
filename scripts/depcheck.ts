f564741300409fd88a1f0619d7fba03a75622ab9
Adam Thompson <adam.thompson@mongodb.com>
2022-09-15 16:12:55 -0400

    Adds fix script

--- scripts/depcheck.ts
+++ scripts/depcheck.ts
@@ -4,10 +4,18 @@ import { readdirSync } from 'fs';
 import { resolve } from 'path';
 import { exit } from 'process';
 import chalk from 'chalk'
+import { Command } from 'commander'
 import packageJson from '../package.json';
+import { exec } from 'child_process';
 const lgPackages = readdirSync('packages/')
-
 const devDependencies = Object.keys(packageJson.devDependencies)
+
+const cli = new Command('depcheck')
+  .option('-f, --fix', 'Option to fix any errors found', false)
+  .parse(process.argv);
+const fix: boolean = cli.opts()['fix']
+
+checkDependencies()
 
 async function checkDependencies() {
   let issuesFound = false;
@@ -19,10 +27,16 @@ async function checkDependencies() {
     const missing = Object.keys(missingLocal).filter(dep => !devDependencies.includes(dep))
 
     if (missing.length > 0 || unused.length > 0) {
-      issuesFound = true;
+
       (missing.length > 0) && console.log(`${chalk.green(`packages/${pkg}`)} is missing: ${chalk.redBright(missing.join(', '))}`);
       (unused.length > 0) && console.log(`${chalk.green(`packages/${pkg}`)} doesn't use ${chalk.blueBright(unused.join(''))}`);
       console.log('')
+
+      if (fix) {
+        await fixDependencies(pkg, missing, unused)
+      } else {
+        issuesFound = true;
+      }
     }
   }
 
@@ -33,5 +47,11 @@ async function checkDependencies() {
   }
 }
 
-checkDependencies()
+
+async function fixDependencies(pkg: string, missing: Array<string>, unused: Array<string>) {
+  console.log(`Fixing packages/${pkg}`)
+  const installCmd = missing.length > 0 ? `yarn add -W ${missing.join(' ')}` : '';
+  const removeCmd = unused.length > 0 ? `yarn remove -W ${unused.join(' ')}`: '';
+  exec(`cd packages/${pkg}; ${installCmd}; ${removeCmd}`)
+}
 
