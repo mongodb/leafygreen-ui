bc52269819f08dd38d0929502ff0f68947318100
Adam Thompson <adam.thompson@mongodb.com>
2022-09-15 16:48:14 -0400

    adds yarn policies line

--- scripts/depcheck.ts
+++ scripts/depcheck.ts
@@ -3,56 +3,70 @@ import depcheck from 'depcheck';
 import { readdirSync } from 'fs';
 import { resolve } from 'path';
 import { exit } from 'process';
-import chalk from 'chalk'
-import { Command } from 'commander'
+import chalk from 'chalk';
+import { Command } from 'commander';
 import packageJson from '../package.json';
-import {
-  SpawnOptions,
-  spawnSync
-} from 'child_process';
-const lgPackages = readdirSync('packages/')
-const devDependencies = Object.keys(packageJson.devDependencies)
+import { SpawnOptions, spawnSync } from 'child_process';
+const lgPackages = readdirSync('packages/');
+const devDependencies = Object.keys(packageJson.devDependencies);
 
 const cli = new Command('depcheck')
   .option('-f, --fix', 'Option to fix any errors found', false)
   .parse(process.argv);
-const fix: boolean = cli.opts()['fix']
+const fix: boolean = cli.opts()['fix'];
 
-checkDependencies()
+checkDependencies();
 
 async function checkDependencies() {
   let issuesFound = false;
 
   for (const pkg of lgPackages) {
-    const check = await depcheck(resolve(__dirname, `../packages/${pkg}`), {})
-    const {dependencies: unused, missing: missingLocal} = check
+    const check = await depcheck(resolve(__dirname, `../packages/${pkg}`), {});
+    const { dependencies: unused, missing: missingLocal } = check;
 
-    const missing = Object.keys(missingLocal).filter(dep => !devDependencies.includes(dep))
+    const missing = Object.keys(missingLocal).filter(
+      dep => !devDependencies.includes(dep),
+    );
 
     if (missing.length > 0 || unused.length > 0) {
-
-      (missing.length > 0) && console.log(`${chalk.green(`packages/${pkg}`)} is missing: ${chalk.redBright(missing.join(', '))}`);
-      (unused.length > 0) && console.log(`${chalk.green(`packages/${pkg}`)} doesn't use ${chalk.blueBright(unused.join(', '))}`);
+      missing.length > 0 &&
+        console.log(
+          `${chalk.green(`packages/${pkg}`)} is missing: ${chalk.redBright(
+            missing.join(', '),
+          )}`,
+        );
+      unused.length > 0 &&
+        console.log(
+          `${chalk.green(`packages/${pkg}`)} doesn't use ${chalk.blueBright(
+            unused.join(', '),
+          )}`,
+        );
 
       if (fix) {
-        fixDependencies(pkg, missing, unused)
+        fixDependencies(pkg, missing, unused);
       } else {
         issuesFound = true;
       }
 
-      console.log('')
+      console.log('');
     }
   }
 
   if (issuesFound) {
-    exit(1)
+    exit(1);
   } else {
-    exit(0)
+    exit(0);
   }
 }
 
-function fixDependencies(pkg: string, missing: Array<string>, unused: Array<string>) {
-  const cmdOpts:SpawnOptions = {stdio: 'inherit', cwd: `packages/${pkg}`};
-  (missing.length > 0 ) && spawnSync('yarn', ['add', '-W', ...missing], cmdOpts);
-  (unused.length > 0) && spawnSync('yarn', ['remove', ...unused], cmdOpts)
+function fixDependencies(
+  pkg: string,
+  missing: Array<string>,
+  unused: Array<string>,
+) {
+  const cmdOpts: SpawnOptions = { stdio: 'inherit', cwd: `packages/${pkg}` };
+  // Fix for https://stackoverflow.com/questions/62254089/expected-workspace-package-to-exist-for-sane
+  spawnSync('yarn', ['policies', 'set-version', '1.19.0'])
+  missing.length > 0 && spawnSync('yarn', ['add', '-W', ...missing], cmdOpts);
+  unused.length > 0 && spawnSync('yarn', ['remove', ...unused], cmdOpts);
 }
