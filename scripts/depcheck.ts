180ab15cef63ad53f95e01952569b7a02fcfd966
Adam Thompson <adam.thompson@mongodb.com>
2022-09-15 15:01:18 -0400

    Creates depcheck script

---
+++ scripts/depcheck.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+import depcheck from 'depcheck';
+import { readdirSync } from 'fs';
+import { resolve } from 'path';
+import { exit } from 'process';
+import chalk from 'chalk'
+import packageJson from '../package.json';
+const lgPackages = readdirSync('packages/')
+
+const devDependencies = Object.keys(packageJson.devDependencies)
+// interface DepcheckResult {
+//   missing?: Array<string>,
+//   unused?: Array<string>
+// }
+
+async function checkDependencies() {
+  let issuesFound = false;
+
+  for (const pkg of lgPackages) {
+    const check = await depcheck(resolve(__dirname, `../packages/${pkg}`), {})
+    const {dependencies: unused, missing: missingLocal} = check
+
+    const missing = Object.keys(missingLocal).filter(dep => !devDependencies.includes(dep))
+
+    if (missing.length > 0 || unused.length > 0) {
+      issuesFound = true;
+      (missing.length > 0) && console.log(`${chalk.green(pkg)} is missing: ${chalk.redBright(missing.join(', '))}`);
+      (unused.length > 0) && console.log(`${chalk.green(pkg)} doesn't use ${chalk.blueBright(unused.join(''))}`);
+      console.log('')
+    }
+  }
+
+  if (issuesFound) {
+    exit(1)
+  } else {
+    exit(0)
+  }
+}
+
+checkDependencies()
+
