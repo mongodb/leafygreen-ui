4c105ced03c4f56b3e6ff9cf6183ef91370bb3ac
Adam Thompson <adam.thompson@mongodb.com>
2022-09-15 13:25:49 -0400

    lint ssr

--- packages/table/src/useSSR.ts
+++ packages/table/src/useSSR.ts
@@ -6,15 +6,14 @@
  * and if it is, maintain our own useSSR hook instead within the @leafygreen-ui/hooks package.
  */
 
-
 interface UseSSRReturn {
-  isBrowser: boolean
-  isServer: boolean
-  isNative: boolean
-  device: Device
-  canUseWorkers: boolean
-  canUseEventListeners: boolean
-  canUseViewport: boolean
+  isBrowser: boolean;
+  isServer: boolean;
+  isNative: boolean;
+  device: Device;
+  canUseWorkers: boolean;
+  canUseEventListeners: boolean;
+  canUseViewport: boolean;
 }
 
 export enum Device {
@@ -23,17 +22,18 @@ export enum Device {
   Native = 'native',
 }
 
-const { Browser, Server, Native } = Device
+const { Browser, Server, Native } = Device;
 
 const canUseDOM = !!(
   typeof window !== 'undefined' &&
   window.document &&
   window.document.createElement
-)
+);
 
-const canUseNative: boolean = typeof navigator != 'undefined' && navigator.product == 'ReactNative'
+const canUseNative: boolean =
+  typeof navigator != 'undefined' && navigator.product == 'ReactNative';
 
-const device = canUseNative ? Native : canUseDOM ? Browser : Server
+const device = canUseNative ? Native : canUseDOM ? Browser : Server;
 
 const SSRObject = {
   isBrowser: device === Browser,
@@ -43,19 +43,21 @@ const SSRObject = {
   canUseWorkers: typeof Worker !== 'undefined',
   canUseEventListeners: device === Browser && !!window.addEventListener,
   canUseViewport: device === Browser && !!window.screen,
-}
+};
 
 // TODO: instead of this, do a polyfill for `Object.assign` https://www.npmjs.com/package/es6-object-assign
-const assign = (...args: Array<any>) => args.reduce((acc, obj) => ({ ...acc, ...obj }), {})
-const values = (obj: any) => Object.keys(obj).map(key => obj[key])
-const toArrayObject = (): UseSSRReturn => assign((values(SSRObject), SSRObject))
+const assign = (...args: Array<any>) =>
+  args.reduce((acc, obj) => ({ ...acc, ...obj }), {});
+const values = (obj: any) => Object.keys(obj).map(key => obj[key]);
+const toArrayObject = (): UseSSRReturn =>
+  assign((values(SSRObject), SSRObject));
 
-let useSSRObject = toArrayObject()
+let useSSRObject = toArrayObject();
 
 export const weAreServer = () => {
-  SSRObject.isServer = true
-  useSSRObject = toArrayObject()
-}
+  SSRObject.isServer = true;
+  useSSRObject = toArrayObject();
+};
 
-export const useSSR = (): UseSSRReturn => useSSRObject
-export default useSSR
+export const useSSR = (): UseSSRReturn => useSSRObject;
+export default useSSR;
