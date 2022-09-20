c6a09b2fd7254fbb177e182543eb17e0f389aa24
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-09-20 18:02:14 -0400

    PD-2445 Add dependency check (#1418)
    * Creates depcheck script
    
    * Add step to actions
    
    * Update depcheck.ts
    
    * Adds fix script
    
    * Update fix cmd
    
    * Fixes missing & unused packages
    
    * adds yarn policies line
    
    * Uses npx
    
    * Revert "Fixes missing & unused packages"
    
    This reverts commit 07214e566f13f30d80956381f9787a66859ea014.
    
    * Fixes missing & unused packages
    
    * update clean-script
    
    * bump to compressed-size-action@v2
    
    * Update peer deps
    
    * Cherrypick utils
    
    * Adds TSconfig fix option
    
    * fixes tsconfigs
    
    * Adds missing leafygreen-provider references
    
    * fix

---
+++ scripts/utils/getPackageDependencies.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from 'fs';
+
+export function getPackageLGDependencies(pkg: string) {
+  const pkgJson = JSON.parse(
+    readFileSync(`packages/${pkg}/package.json`, 'utf-8'),
+  );
+  const dependencies = Object.keys({
+    ...pkgJson.dependencies,
+    ...pkgJson.devDependencies,
+    ...pkgJson.peerDependencies,
+  })
+    .filter(pkg => pkg.includes('@leafygreen-ui'))
+    .map(pkg => pkg.replace(`@leafygreen-ui/`, ''));
+  return dependencies;
+}
