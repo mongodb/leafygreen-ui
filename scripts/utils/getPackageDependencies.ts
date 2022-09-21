1047a1abe38995c6b079098e3fdaffb673bd8609
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-09-21 11:14:41 -0400

    Build & Test with Git-diff option (#1422)
    * Creates custom tsnode script
    
    * Convert to ts
    
    * Refactor to use Commander
    
    * Improves git diff
    
    * test change
    
    * Update console logs
    
    * Revert "test change"
    
    This reverts commit ca0a7c7e2170a0f722e48a271429ccf92b1a5512.
    
    * Update nodejs.yml
    
    * disable watch
    
    * fix
    
    * Revert "Update nodejs.yml"
    
    This reverts commit d45aa10da7a51ce99a7d5defa2bd3b0834fefa65.
    
    * Mo=inor fixes
    
    * Restores descripotion
    
    * lint
    
    * Adds custom Test script (#1423)
    
    * Adds test script
    
    * Pass through args
    
    * Update nodejs.yml
    
    * Fix default package
    
    * Includes dependents of packages
    
    * fix

--- scripts/utils/getPackageDependencies.ts
+++ scripts/utils/getPackageDependencies.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from 'fs';
+import { getAllPackageNames } from './getAllPackageNames';
 
 export function getPackageLGDependencies(pkg: string) {
   const pkgJson = JSON.parse(
@@ -12,4 +13,15 @@ export function getPackageLGDependencies(pkg: string) {
     .filter(pkg => pkg.includes('@leafygreen-ui'))
     .map(pkg => pkg.replace(`@leafygreen-ui/`, ''));
   return dependencies;
+}
+
+export function getPackageDependants(pkg: string) {
+  const allPackages = getAllPackageNames();
+
+  return allPackages
+    .filter(otherPkg => {
+      const otherPkgDeps = getPackageLGDependencies(otherPkg);
+      return otherPkgDeps.includes(pkg);
+    })
+    .map(pkg => pkg.replace(`@leafygreen-ui/`, ''));
 }
