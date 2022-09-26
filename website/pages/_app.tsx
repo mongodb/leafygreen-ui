27952bc5fc2ca0b85bb52021636308335247281d
spark33 <seanpark1107@gmail.com>
2022-09-26 00:05:15 -0400

    lint

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -115,19 +115,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   }
 
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
+    const handleRouteChange = url => {
+      ga.pageview(url);
+    };
     //When the component is mounted, subscribe to router changes
     //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('routeChangeComplete', handleRouteChange);
 
     // If the component is unmounted, unsubscribe
     // from the event with the `off` method
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     /// @ts-expect-error
--- website/utils/googleAnalytics.ts
+++ website/utils/googleAnalytics.ts
@@ -1,13 +1,13 @@
 // log the pageview with their URL
-export const pageview = (url) => {
+export const pageview = url => {
   // @ts-expect-error
-  window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+  window.gtag('config', 'G-VFTH2BJVVK', {
     page_path: url,
-  })
-}
+  });
+};
 
 // log specific events happening.
 export const event = ({ action, params }) => {
   // @ts-expect-error
-  window.gtag('event', action, params)
-}
+  window.gtag('event', action, params);
+};
