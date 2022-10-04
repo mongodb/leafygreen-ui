089aaee1a2f6b243bc83ec772b4748cb71e6d099
Shaneeza <shaneeza.ali@mongodb.com>
2022-10-04 15:55:29 -0400

    PD-2243: Guidecue (#1424)
    * testing
    
    * testing
    
    * messy code, dont look
    
    * adding some more props
    
    * adding more props
    
    * hmm
    
    * update the story
    
    * props
    
    * add focus trap
    
    * default buttonText
    
    * prop descriptions
    
    * more props
    
    * add callback option to tooltip
    
    * fixing some props
    
    * rename to guidecue
    
    * added test
    
    * readme and break out some components
    
    * add story for scroll container
    
    * some more test
    
    * beacon styles
    
    * update readme
    
    * some cleanup
    
    * remove random changes
    
    * add tooltip test
    
    * changesets
    
    * css updates
    
    * lib bump
    
    * remove dup tooltip code
    
    * heading color update
    
    * add children as children
    
    * break out test
    
    * test scroll and portal container
    
    * add demo
    
    * update test
    
    * some conditional props
    
    * updates callback names, add conditional props
    
    * spelling mistakes, remove dup classname
    
    * update tests
    
    * shared props
    
    * negative numOfSteps test
    
    * update standalone currentStep to never
    
    * update beaconAlignment description
    
    * use useIdAllocator for fallback class
    
    * rob feedback: tooltip cursor auto
    
    * change name
    
    * rob feedback: close hover color
    
    * rename to guide-cue
    
    * figuring out error
    
    * add to website
    
    * remove focus trap when click outside
    
    * safari animation fixes, website knob names

---
+++ packages/guide-cue/README.md
@@ -0,0 +1,103 @@
+# GuideCue
+
+![npm (scoped)](https://img.shields.io/npm/v/@leafygreen-ui/guide-cue.svg)
+
+#### [View on MongoDB.design](https://www.mongodb.design/component/guide-cue/example/)
+
+## Installation
+
+### Yarn
+
+```shell
+yarn add @leafygreen-ui/guide-cue
+```
+
+### NPM
+
+```shell
+npm install @leafygreen-ui/guide-cue
+```
+
+## Example
+
+### Stand-alone example
+
+```js
+import { GuideCue } from '@leafygreen-ui/guide-cue';
+
+const [open, setOpen] = useState(true);
+
+<GuideCue
+  open={open}
+  setOpen={setOpen}
+  title="New feature"
+  refEl={triggerRef}
+  numberOfSteps={1}
+  currentStep={1}
+  onPrimaryButtonClick={() => {
+    // do something
+  }}
+>
+  This is a new feature. You should try it out
+</GuideCue>;
+```
+
+or
+
+### Multi-step example
+
+```js
+import GuideCue from '@leafygreen-ui/guide-cue';
+
+const [open, setOpen] = useState(true);
+
+<GuideCue
+  open={open}
+  setOpen={setOpen}
+  title="New feature"
+  refEl={triggerRef}
+  numberOfSteps={4}
+  currentStep={2}
+  onPrimaryButtonClick={() => {
+    // do something
+  }}
+  onDismiss={() => {
+    // do something
+  }}
+>
+  This is a new feature. You should try it out
+</GuideCue>;
+```
+
+An example of how to use the multi-step tooltip can be found in (storybook - MultistepDemo)[https://github.com/mongodb/leafygreen-ui/blob/main/packages/guide-cue/src/GuideCue.story.tsx]
+
+## Usage
+
+There are two variations of the tooltip -- stand-alone and multi-step. The multi-step tooltip shows the number of steps, has a dismiss and primary button, and displays a pulsing beacon while the stand-alone tooltip only shows the primary button and does not include the number of steps or the dismiss button.
+
+The variant that is shown depends on the number of steps. If `numberOfSteps > 1` the multi-step tooltip is shown else the stand-alone tooltip is shown.
+
+## Properties
+
+| Prop                   | Type                                                                                           | Description                                                                                                                                                                                                                                                                | Default                |
+| ---------------------- | ---------------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ---------------------- |
+| `open`                 | `boolean`                                                                                      | Determines if the `Tooltip` will appear as open or close.                                                                                                                                                                                                                  | `false`                |
+| `setOpen`              | `function`                                                                                     | Callback to change the open state of the Tooltip in consuming applications.                                                                                                                                                                                                | `(boolean) => boolean` |
+| `refEl`                | `HTMLElement`                                                                                  | Pass a reference to an element that the beacon/tooltip(if stand-alone) should be centered against.                                                                                                                                                                         | `null`                 |
+| `numberOfSteps`        | `number`                                                                                       | Used to determine which tooltip will be shown. If `numberOfSteps > 1` then the multi-step tooltip will be shown else the stand-alone tooltip will be shown. This number will only be displayed in the multi-step tooltip.                                                  | `1`                    |
+| `currentStep`          | `number`                                                                                       | Used to display the current step. If `numberOfSteps === 1` this number will not display.                                                                                                                                                                                   | `1`                    |
+| `darkMode`             | `boolean`                                                                                      | Determines whether the `Tooltip` will appear in dark mode.                                                                                                                                                                                                                 | `false`                |
+| `title`                | `string`                                                                                       | Title to appear inside of Tooltip.                                                                                                                                                                                                                                         | `''`                   |
+| `children`             | `React.ReactNode`                                                                              | Body content to appear inside the tooltip, under the title.                                                                                                                                                                                                                | `''`                   |
+| `buttonText`           | `string`                                                                                       | Text to appear inside the primary button. The default text for the stand-alone tooltip is `Got it`. The default text for the multi-step tooltip varies on the `numberOfSteps` and `currentStep`. If `numberOfSteps === currentStep` the text is `Got it` else it is `Next. | `'Next'`               |
+| `onDismiss`            | `function`                                                                                     | Callback fired when the dismiss(X) button is clicked or when the `Esc` key is pressed. This only applies to the multi-step tooltip.                                                                                                                                        | `() => {}`             |
+| `onPrimaryButtonClick` | `function`                                                                                     | Callback fired when the bottom primary button is clicked. This applies to both the stand-alone and multi-step tooltip. This is also fired when the `Esc` key is pressed in the stand-alone tooltip.                                                                        | `() => {}`             |
+| `tooltipClassName`     | `string`                                                                                       | ClassName to be applied to the tooltip element.                                                                                                                                                                                                                            | `''`                   |
+| `tooltipAlign`         | `'top'` \| `'bottom'` \| `'left'` \| `'right'`                                                 | Determines the alignment of the tooltip.                                                                                                                                                                                                                                   | `top`                  |
+| `tooltipJustify`       | `'start'` \| `'middle'` \| `'end'`                                                             | Determines the justification of the tooltip.                                                                                                                                                                                                                               | `middle`               |
+| `beaconAlign`          | `'top'` \| `'bottom'` \| `'left'` \| `'right'` \| `'center-horizontal'` \| `'center-vertical'` | Determines the alignment of the beacon(animated pulsing circle that appears on top of the trigger element). This only applies to the multi-step tooltip.                                                                                                                   | `center-horizontal`    |
+| `portalContainer`      | `HTMLElement` \| `null`                                                                        | Sets the container used for the popover's portal. NOTE: If using a `scrollContainer` make sure that the `portalContainer` is contained within the `scrollContainer`. E.g, passing the same refrence to `scrollContainer` and `portalContainer`.                            |                        |
+| `scrollContainer`      | `HTMLElement` \| `null`                                                                        | If the popover portal has a scrollable ancestor other than the window, this prop allows passing a reference to that element to allow the portal to position properly.                                                                                                      |                        |
+| `portalClassName`      | `string`                                                                                       | Passes the given className to the popover's portal container if the default portal container is being used.                                                                                                                                                                |                        |
+| `popoverZIndex`        | `number`                                                                                       | Sets the z-index CSS property for the popover.                                                                                                                                                                                                                             |                        |
+| ...                    | native `div` attributes                                                                        | Any other props will be spread on the tooltip `div` element                                                                                                                                                                                                                |                        |
---
+++ packages/guide-cue/src/GuideCue.spec.tsx
@@ -0,0 +1,406 @@
+import React, { useState } from 'react';
+import {
+  act,
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { axe } from 'jest-axe';
+import { GuideCue } from '.';
+import userEvent from '@testing-library/user-event';
+import { timeout1 } from './styles';
+
+// TODO: maybe add globally
+async function expectElementToNotBeRemoved(element: HTMLElement) {
+  try {
+    await waitForElementToBeRemoved(element);
+    throw new Error('Expected to catch error.');
+  } catch (error) {
+    // eslint-disable-next-line jest/no-try-expect
+    if (error instanceof Error) {
+      expect(error.toString()).toMatch(
+        'Timed out in waitForElementToBeRemoved.',
+      );
+    }
+  }
+}
+
+function waitForTimeout(timeout = 500) {
+  return new Promise(res => setTimeout(res, timeout));
+}
+
+const guideCueTestId = 'tooltip-test-id';
+const guideCueTitle = 'this is the title';
+const guideCueChildren = 'this is the body content';
+const buttonTextDefault = 'bottom button';
+
+const GuideCueWrapper = ({
+  open: initialOpen = false,
+  buttonText = buttonTextDefault,
+  currentStep = 1,
+  numberOfSteps = 1,
+  ...props
+}: Partial<React.ComponentProps<typeof GuideCue>>) => {
+  const [open, setOpen] = useState(initialOpen);
+
+  const elem = document.createElement('div');
+  const ref = { current: elem };
+
+  return (
+    <GuideCue
+      open={open}
+      setOpen={setOpen}
+      data-testid={guideCueTestId}
+      title={guideCueTitle}
+      refEl={ref}
+      buttonText={buttonText}
+      currentStep={currentStep}
+      numberOfSteps={numberOfSteps}
+      {...props}
+    >
+      <div>{guideCueChildren}</div>
+    </GuideCue>
+  );
+};
+
+function renderGuideCue(
+  props: Partial<React.ComponentProps<typeof GuideCue>> = {},
+) {
+  const utils = render(
+    <>
+      <div data-testid="backdrop" />
+      <GuideCueWrapper {...props} />
+    </>,
+  );
+
+  const backdrop = utils.getByTestId('backdrop');
+
+  return { ...utils, backdrop };
+}
+
+describe('packages/guide-cue', () => {
+  describe('A11y', () => {
+    test('does not have basic accessibility violations', async () => {
+      const { container } = renderGuideCue({ open: true });
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    });
+  });
+
+  describe('Stand-alone tooltip', () => {
+    test('is visible when open is "true"', async () => {
+      const { getByRole } = renderGuideCue({ open: true });
+      const guideCue = getByRole('dialog');
+      await waitFor(() => expect(guideCue).toBeVisible());
+    });
+
+    test('is not visible when open is "false"', async () => {
+      const { queryByRole } = renderGuideCue({ open: true });
+      const guideCue = queryByRole('dialog');
+      await waitFor(() => expect(guideCue).not.toBeVisible());
+    });
+
+    test('is rendered if numberOfSteps < 1', async () => {
+      const { getAllByRole } = renderGuideCue({
+        open: true,
+        numberOfSteps: -1,
+      });
+      const numOfButtons = getAllByRole('button').length;
+      await waitFor(() => expect(numOfButtons).toEqual(1));
+    });
+
+    test('closes when the primary button is clicked', async () => {
+      const onPrimaryButtonClick = jest.fn();
+      const { getByRole } = renderGuideCue({
+        open: true,
+        onPrimaryButtonClick,
+      });
+      const guideCue = getByRole('dialog');
+      const button = getByRole('button');
+      userEvent.click(button);
+      await waitForElementToBeRemoved(guideCue);
+      expect(guideCue).not.toBeInTheDocument();
+    });
+
+    test('fires the onPrimaryButtonClick handler once when the primary button is clicked', async () => {
+      const onPrimaryButtonClick = jest.fn();
+      const { getByRole } = renderGuideCue({
+        open: true,
+        onPrimaryButtonClick,
+      });
+      const button = getByRole('button');
+      userEvent.click(button);
+      await act(async () => {
+        await waitFor(() =>
+          expect(onPrimaryButtonClick).toHaveBeenCalledTimes(1),
+        );
+      });
+    });
+
+    test('a click outside of the tooltip should do nothing', async () => {
+      const { getByRole, backdrop } = renderGuideCue({
+        open: true,
+      });
+      const guideCue = getByRole('dialog');
+      userEvent.click(backdrop);
+      await expectElementToNotBeRemoved(guideCue);
+      expect(guideCue).toBeVisible();
+    });
+
+    test('closes guide-cue when escape key is pressed', async () => {
+      const { getByRole } = renderGuideCue({ open: true });
+      const guideCue = getByRole('dialog');
+      userEvent.type(guideCue, '{esc}');
+      await waitForElementToBeRemoved(guideCue);
+      expect(guideCue).not.toBeInTheDocument();
+    });
+
+    test('content should render in a portal', () => {
+      const { container, getByTestId } = renderGuideCue({
+        open: true,
+      });
+      expect(container).not.toContain(getByTestId(guideCueTestId));
+    });
+
+    test('number of steps should not be visible', () => {
+      const { queryByText } = renderGuideCue({
+        open: true,
+      });
+
+      const steps = queryByText('1 of 1');
+      expect(steps).not.toBeInTheDocument();
+    });
+
+    test('title should be visible', () => {
+      const { getByText } = renderGuideCue({
+        open: true,
+      });
+
+      const title = getByText(guideCueTitle);
+      expect(title).toBeInTheDocument();
+    });
+
+    test('body content should be visible', () => {
+      const { getByText } = renderGuideCue({
+        open: true,
+      });
+
+      const body = getByText(guideCueChildren);
+      expect(body).toBeInTheDocument();
+    });
+
+    test('will render inside portal and scroll container', async () => {
+      const elem = document.createElement('div');
+      document.body.appendChild(elem);
+      renderGuideCue({
+        open: true,
+        portalContainer: elem,
+        scrollContainer: elem,
+      });
+      await act(async () => {
+        expect(elem.innerHTML.includes(guideCueTitle)).toBe(true);
+      });
+    });
+  });
+
+  describe('Multi-step tooltip', () => {
+    test('is visible when open is "true"', async () => {
+      const { getByRole } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const guideCue = getByRole('dialog');
+      await waitFor(() => expect(guideCue).toBeVisible());
+    });
+
+    test('closes when the dismiss(X) button is clicked', async () => {
+      const onClose = jest.fn();
+      const { getByRole, getByLabelText } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+        onDismiss: onClose,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const guideCue = getByRole('dialog');
+      const button = getByLabelText('Close Tooltip', { selector: 'button' });
+      userEvent.click(button);
+      await waitForElementToBeRemoved(guideCue);
+      expect(guideCue).not.toBeInTheDocument();
+    });
+
+    test('fires the onClose handler once when the dismiss(X) button is clicked', async () => {
+      const onClose = jest.fn();
+      const { getByLabelText } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+        onDismiss: onClose,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const button = getByLabelText('Close Tooltip', { selector: 'button' });
+      userEvent.click(button);
+      await act(async () => {
+        await waitFor(() => expect(onClose).toHaveBeenCalledTimes(1));
+      });
+    });
+
+    test('closes when the primary button is clicked', async () => {
+      const onPrimaryButtonClick = jest.fn();
+      const { getByRole, getAllByRole } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+        onPrimaryButtonClick,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const guideCue = getByRole('dialog');
+      const button = getAllByRole('button')[1];
+      userEvent.click(button);
+      await waitForElementToBeRemoved(guideCue);
+      expect(guideCue).not.toBeInTheDocument();
+    });
+
+    test('fires the onPrimaryButtonClick handler once when the primary button is clicked', async () => {
+      const onPrimaryButtonClick = jest.fn();
+      const { getAllByRole } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+        onPrimaryButtonClick,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const button = getAllByRole('button')[1];
+      userEvent.click(button);
+      await act(async () => {
+        await waitFor(() =>
+          expect(onPrimaryButtonClick).toHaveBeenCalledTimes(1),
+        );
+      });
+    });
+
+    test('a click outside the tooltip should do nothing', async () => {
+      const { getByRole, backdrop } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const guideCue = getByRole('dialog');
+      userEvent.click(backdrop);
+      await expectElementToNotBeRemoved(guideCue);
+      expect(guideCue).toBeVisible();
+    });
+
+    test('closes guide-cue when escape key is pressed', async () => {
+      const { getByRole } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const modal = getByRole('dialog');
+      userEvent.type(modal, '{esc}');
+      await waitForElementToBeRemoved(modal);
+      expect(modal).not.toBeInTheDocument();
+    });
+
+    test('content should render in a portal', async () => {
+      const { container, getByTestId } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+      });
+      await act(async () => {
+        await waitFor(() =>
+          expect(container).not.toContain(getByTestId(guideCueTestId)),
+        );
+      });
+    });
+
+    test('number of steps should be visible', async () => {
+      const { getByText } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const steps = getByText('1 of 2');
+      expect(steps).toBeInTheDocument();
+    });
+
+    test('title should be visible', async () => {
+      const { getByText } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const title = getByText(guideCueTitle);
+      expect(title).toBeInTheDocument();
+    });
+
+    test('body content should be visible', async () => {
+      const { getByText } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const body = getByText(guideCueChildren);
+      expect(body).toBeInTheDocument();
+    });
+
+    test('dismiss button and primary button should be visible', async () => {
+      const { getAllByRole } = renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+      });
+      await act(async () => {
+        await waitForTimeout(timeout1);
+      });
+      const numOfButtons = getAllByRole('button').length;
+      await waitFor(() => expect(numOfButtons).toEqual(2));
+    });
+
+    test('will render inside portal and scroll container', async () => {
+      const elem = document.createElement('div');
+      document.body.appendChild(elem);
+      renderGuideCue({
+        open: true,
+        numberOfSteps: 2,
+        currentStep: 1,
+        portalContainer: elem,
+        scrollContainer: elem,
+      });
+      await act(async () => {
+        await waitFor(() =>
+          expect(elem.innerHTML.includes(guideCueTitle)).toBe(true),
+        );
+      });
+    });
+  });
+});
