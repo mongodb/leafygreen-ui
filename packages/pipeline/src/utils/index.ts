53df487cec94c60f7c2838ae7c149b5df0fed169
Shaneeza <shaneeza.ali@mongodb.com>
2022-10-05 16:42:12 -0400

    remove isStageElement util

--- packages/pipeline/src/utils/index.ts
+++ packages/pipeline/src/utils/index.ts
@@ -1,5 +1,3 @@
 import getPipelineCounterTooltip from './getPipelineCounterTooltip';
-import isElementOverflowed from './isElementOverflowed';
-import isStageElement from './isStageElement';
 
-export { getPipelineCounterTooltip, isElementOverflowed, isStageElement };
+export { getPipelineCounterTooltip };
