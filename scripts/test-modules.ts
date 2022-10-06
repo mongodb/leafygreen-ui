c36968aee6572371ec0a981bd15b13f53b29ad8e
Adam Thompson <adam.thompson@mongodb.com>
2022-10-06 18:03:40 -0400

    Update test-modules.ts

--- scripts/test-modules.ts
+++ scripts/test-modules.ts
@@ -9,7 +9,7 @@ import vm from 'vm';
 import { spawnSync } from 'child_process';
 import { Command } from 'commander';
 import chalk from 'chalk';
-import { getAllPackageNames } from './utils/getAllPackageNames';
+import { getRelevantPackages } from './utils/getRelevantPackages';
 
 const ModuleType = {
   esm: 'esm',
@@ -17,20 +17,27 @@ const ModuleType = {
   cjs: 'cjs',
   yui: 'yui',
   amd: 'amd',
-  steal: 'steal'
-} as const
-type ModuleType = typeof ModuleType[keyof typeof ModuleType]
+  steal: 'steal',
+} as const;
+type ModuleType = typeof ModuleType[keyof typeof ModuleType];
 
 const cli = new Command('test')
   .description('Tests leafygreen-ui build integrity')
+  .option(
+    '--diff',
+    'Builds packages that you have been working on, based on the current git diff',
+    false,
+  )
+  .option(
+    '--deps',
+    'Builds packages that you have been working on, and their leafygreen-ui dependencies.',
+    false,
+  )
   .argument('[packages...]')
   .parse(process.argv);
 
-const allPackages = getAllPackageNames();
-const packages =
-  cli.args.length > 0
-    ? cli.args.filter((arg: string) => allPackages.includes(arg))
-    : allPackages;
+const { diff, deps } = cli.opts();
+const packages = getRelevantPackages(cli.args, { diff, deps });
 
 // Check if each package is built. If not, build them
 if (
@@ -45,37 +52,46 @@ if (
 // Check that every package's /dist folder has a valid UMD, ESM & TS files
 for (const pkg of packages) {
   const rootPath = path.resolve(__dirname, `../packages/${pkg}/dist/index.js`);
-  const esmPath = path.resolve(__dirname, `../packages/${pkg}/dist/esm/index.js`);
+  const esmPath = path.resolve(
+    __dirname,
+    `../packages/${pkg}/dist/esm/index.js`,
+  );
+  const tsPath = path.resolve(__dirname, `../packages/${pkg}/dist/index.d.ts`);
 
-  console.log(getModuleTypes(path.resolve(__dirname, `../packages/${pkg}/dist`)))
+  const rootExists = fs.existsSync(rootPath);
+  const esmExists = fs.existsSync(esmPath);
+  const tsExists = fs.existsSync(tsPath);
+  const isCJSValid = getModuleTypes(rootPath).includes('cjs');
+  const isESMValid = getModuleTypes(esmPath).includes('esm');
 
-  const rootExists = fs.existsSync(rootPath)
-  const esmExists = fs.existsSync(esmPath)
-  const isCJS = getModuleTypes(rootPath) === 'cjs'
-  const isESM = getModuleTypes(esmPath) === 'esm'
+  if (
+    ![rootExists, esmExists, tsExists, isCJSValid, isESMValid].every(
+      x => x == true,
+    )
+  ) {
+    const errorMsg: Array<string> = [
+      chalk.red.bold(`Error in \`${pkg}\` build:`),
+    ];
+    if (!rootExists) errorMsg.push(chalk.red('`dist/index.js` not found'));
+    if (!esmExists) errorMsg.push(chalk.red('`dist/esm/index.js` not found'));
+    if (!tsExists) errorMsg.push(chalk.red('Typescript build not found'));
+    if (!isCJSValid)
+      errorMsg.push(
+        chalk.red(`UMD module not valid`),
+        chalk.gray(`(${rootPath})`),
+      );
+    if (!isESMValid)
+      errorMsg.push(
+        chalk.red(`ESM module not valid`),
+        chalk.gray(`(${esmPath})`),
+      );
 
-  console.log({
-    pkg,
-    rootExists,
-    esmExists,
-    isCJS,
-    isESM
-  });
-
-
-  if (![ rootExists, esmExists, isCJS, isESM].every(x => x == true)) {
-    const errorMsg: Array<string> = [chalk.red.bold(`Error in \`${pkg}\`:`)]
-    if (!rootExists) errorMsg.push(chalk.red('`dist/index.js` not found'))
-    if (!esmExists) errorMsg.push(chalk.red('`dist/esm/index.js` not found'))
-    if (!isCJS) errorMsg.push(chalk.red(`UMD module not valid`), chalk.gray(`(${rootPath})`))
-    if (!isESM) errorMsg.push(chalk.red(`ESM module not valid`), chalk.gray(`(${esmPath})`))
-
-    console.log(errorMsg.join(' '))
-    process.exit(1)
+    console.log(errorMsg.join(' '));
+    process.exit(1);
   }
 }
 
-process.exit(0)
+process.exit(0);
 
 /**
  * Adapted from https://github.com/formatjs/js-module-formats
@@ -86,9 +102,10 @@ process.exit(0)
  * @param {string} path The JavaScript source path to be analyzed
  * @return {object|array} an object or a collection of object with the info gathered from the analysis, it usually includes objects with `type` and `name` per module.
  **/
-function getModuleTypes(path: string): ModuleType | Array<ModuleType> | undefined {
+function getModuleTypes(path: string): Array<ModuleType> {
   // Essentially we're looking for import/export statements
-  const ES6ImportExportRegExp = /(?:^\s*|[}{();,\n]\s*)(import\s*['"][a-zA-Z]+['"]|(import|module)\s*[^"'()\n;]+\s*from\s*['"]|export\s+(\*|\{|default|function|var|const|let|[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*))/;
+  const ES6ImportExportRegExp =
+    /(?:^\s*|[}{();,\n]\s*)(import\s*['"][a-zA-Z]+['"]|(import|module)\s*[^"'()\n;]+\s*from\s*['"]|export\s*((\*|\{|default|function|var|const|let|[_$a-zA-Z\xA0-\uFFFF])[\s_$a-zA-Z0-9\xA0-\uFFFF]*))/;
   const ES6AliasRegExp =
     /(?:^\s*|[}{();,\n]\s*)(export\s*\*\s*from\s*(?:'([^']+)'|"([^"]+)"))/;
   const context = vm.createContext({});
@@ -109,7 +126,7 @@ function getModuleTypes(path: string): ModuleType | Array<ModuleType> | undefine
   - if `define()` is called
   **/
   context.define = function () {
-    mods.push(ModuleType.amd)
+    mods.push(ModuleType.amd);
   };
 
   /**
@@ -131,12 +148,12 @@ function getModuleTypes(path: string): ModuleType | Array<ModuleType> | undefine
   **/
   context.require = function () {
     mods.push(ModuleType.cjs);
-    throw new Error("CJS Package. Stop Execution");
+    throw new Error('Confirmed CJS Package. Stop Execution');
   };
   context.exports = Object.create(null);
   context.module = context;
 
-  const src = fs.readFileSync(path, 'utf-8')
+  const src = fs.readFileSync(path, 'utf-8');
 
   // executing the JavaScript source into a new context to avoid leaking
   // globals during the detection process.
@@ -147,7 +164,6 @@ function getModuleTypes(path: string): ModuleType | Array<ModuleType> | undefine
       mods.push(ModuleType.esm);
     }
   } finally {
-
     // very dummy detection process for CommonJS modules
     if (
       typeof context.exports === 'function' ||
@@ -160,6 +176,5 @@ function getModuleTypes(path: string): ModuleType | Array<ModuleType> | undefine
     }
   }
 
-  // returning an array when more than one module is defined in the source
-  return mods.length > 1 ? mods : mods[0];
+  return mods;
 }
