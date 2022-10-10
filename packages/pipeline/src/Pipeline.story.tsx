601912687b84fedb0431463e1880ee28a949accb
Shaneeza <shaneeza.ali@mongodb.com>
2022-10-10 16:10:04 -0400

    fix errors

--- packages/pipeline/src/Pipeline.story.tsx
+++ packages/pipeline/src/Pipeline.story.tsx
@@ -11,7 +11,7 @@ export default {
   args: {
     stages: ['$match', '$group', '$project', '$addFields', '$limit'].join(','),
     darkMode: false,
-    size: Size.Normal
+    size: Size.Normal,
   },
   argTypes: {
     className: {
