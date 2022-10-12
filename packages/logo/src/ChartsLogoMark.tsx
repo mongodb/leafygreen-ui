a855b5132b781139d68b29979338b6e9af226999
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-10-12 10:53:01 -0400

    LG-2480 Adds Atlas logo (#1455)

--- packages/logo/src/ChartsLogoMark.tsx
+++ packages/logo/src/ChartsLogoMark.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { css, cx } from '@leafygreen-ui/emotion';
 import { ProductLogoProps, getColor, getAccessibleProps } from './utils';
 
+/**
+ * @deprecated
+ */
 function ChartsLogoMark({
   knockout = false,
   darkMode = false,
