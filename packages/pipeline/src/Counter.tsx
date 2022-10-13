d4561faeea5bf5c9b3e6b65d6311aa0d9f96aee5
Shaneeza <shaneeza.ali@mongodb.com>
2022-10-13 10:02:42 -0400

    PD-1867:  Pipeline brand refresh (#1446)
    * attempt 1
    
    * attempt 2
    
    * messy for now but tooltip text is correct
    
    * stil messy but added in svgs
    
    * new svgs
    
    * updated svgs
    
    * forgot to save
    
    * gah
    
    * saving but svg is not there yet
    
    * first segment border
    
    * style updates
    
    * svg updates
    
    * styles cleanup
    
    * stuck on testing
    
    * fix circular dependency
    
    * fix test
    
    * remove clone element, hide counter with css
    
    * some cleanup
    
    * update readme
    
    * export svgs
    
    * use tooltip text for counter visibility
    
    * remove isStageElement util
    
    * add changeset
    
    * extend li to stage
    
    * comments from review
    
    * wip
    
    * move some props to context
    
    * some feedback
    
    * fix type error
    
    * add icon to tooltip text
    
    * fix errors
    
    * split tooltiptext and add test
    
    * clean up
    
    * feedback comments: switch to map, add comment
    
    * remove some files
    
    * update yarn

--- packages/pipeline/src/Counter.tsx
+++ packages/pipeline/src/Counter.tsx
@@ -1,93 +1,26 @@
-import React, { forwardRef, ReactElement, ReactNode, Ref } from 'react';
+import React, { forwardRef, ReactElement, Ref, useContext } from 'react';
 import PropTypes from 'prop-types';
-import { css, cx } from '@leafygreen-ui/emotion';
+import { cx } from '@leafygreen-ui/emotion';
+import { SegmentXs, SegmentS, SegmentM, SegmentL } from './svgs';
 
 import {
-  getRootStyle,
-  getChildStyle,
-  getChevronStyle,
-  Size,
-  layout,
-  colors,
-  counterAttr,
-  counterChevronAttr,
+  counterThemeStyles,
+  counterBaseStyles,
+  counterSizeStyles,
+  svgLayer1Styles,
+  svgLayer2Styles,
+  counterSvgBaseStyles,
+  counterSvgColStyles,
 } from './styles';
+import PipelineContext from './PipelineContext';
+import { CounterProps, Size } from './types';
 
-interface StateForStyles {
-  size?: Size;
-}
-
-interface CounterProps {
-  /**
-   * Content that will appear inside of the Counter component.
-   */
-  children?: ReactNode;
-
-  /**
-   * Classname applied to Counter content container.
-   **/
-  className?: string;
-
-  /**
-   * Alter the rendered size of the component. Inherited from the parent Pipeline component.
-   */
-  size: Size;
-}
-
-const getBaseStyle = ({ size = Size.XSmall }: StateForStyles): string => {
-  const { chevron, fontSize, fontWeight, gutter, height, lineHeight } =
-    layout[size];
-
-  const { color, secondary } = colors;
-  const outerSize = height / 2;
-  const offset = outerSize + chevron.size * 2;
-
-  return cx(
-    getRootStyle({ size }),
-    getChildStyle({ size }),
-    css`
-      background-color: ${secondary.backgroundColor};
-      color: ${color};
-      padding: ${gutter.vertical}px ${gutter.horizontal}px;
-      margin-right: ${offset}px;
-      font-size: ${fontSize}px;
-      font-weight: ${fontWeight};
-      line-height: ${lineHeight};
-      flex: 1 1 auto;
-      white-space: nowrap;
-      z-index: 2;
-
-      &::before {
-        white-space: nowrap;
-        content: '+' counter(hiddenCount);
-      }
-    `,
-  );
-};
-
-const getCounterChevronStyle = ({
-  size = Size.XSmall,
-}: StateForStyles): string => {
-  const { chevron, height } = layout[size];
-  const { secondary } = colors;
-  const outerSize = height / 2;
-
-  return cx(
-    getChevronStyle({ size }),
-    css`
-      &::before {
-        background-color: ${secondary.backgroundColor};
-        box-shadow: 0 0 0 ${chevron.size}px transparent,
-          0 0 0 ${outerSize}px transparent;
-      }
-    `,
-  );
-};
-
-const getStatefulStyles = (state: StateForStyles) => ({
-  base: getBaseStyle(state),
-  chevron: getCounterChevronStyle(state),
-});
+const segments: Record<Size, React.ComponentType<any>> = {
+  [Size.XSmall]: SegmentXs,
+  [Size.Small]: SegmentS,
+  [Size.Normal]: SegmentM,
+  [Size.Large]: SegmentL,
+} as const;
 
 /**
  * # Counter
@@ -97,33 +30,43 @@ const getStatefulStyles = (state: StateForStyles) => ({
  * ```
  * <Counter />
  * ```
+ * @internal
  * @param props.className Classname applied to Counter content container.
  */
 const Counter = forwardRef(
   (
-    { className = '', children, size, ...rest }: CounterProps,
+    { className = '', children, ...rest }: CounterProps,
     ref: Ref<HTMLDivElement>,
   ): ReactElement => {
-    const { base: baseStyle, chevron: chevronStyle } = getStatefulStyles({
-      size,
-    });
+    const { theme, size } = useContext(PipelineContext);
+    const Icon = segments[size];
 
     return (
       <div
         {...rest}
-        {...counterAttr.prop}
         data-testid="pipeline-counter"
-        className={cx(baseStyle, className)}
+        className={cx(
+          counterBaseStyles,
+          counterSizeStyles[size],
+          counterThemeStyles[theme],
+          className,
+        )}
         ref={ref}
+        // Adding this so on focus the tooltip will show up
+        // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+        tabIndex={0}
       >
+        {/* The counter is a unique shape with focus and hover state that are not easily accomplished with css alone so we are using an SVG. The reason we have 2 SVGs is to mimic the behavior of a div expanding horizontally when the text increases since an SVG does not stretch horizontally while maintaining its height.*/}
+        <div className={counterSvgBaseStyles}>
+          <div className={counterSvgColStyles}>
+            <Icon className={svgLayer1Styles} />
+          </div>
+          <div className={counterSvgColStyles}>
+            <Icon className={svgLayer2Styles} />
+          </div>
+        </div>
         {/* Children will be the tooltip provided by the Pipeline component */}
         {children}
-
-        <div
-          {...counterChevronAttr.prop}
-          data-testid="pipeline-counter-chevron"
-          className={chevronStyle}
-        />
       </div>
     );
   },
@@ -134,7 +77,6 @@ Counter.displayName = 'Counter';
 Counter.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  size: PropTypes.oneOf(Object.values(Size)).isRequired,
 };
 
 export default Counter;
