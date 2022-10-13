c6a7f42d56a754ca7ae850944a8cf0b8fe0effa4
Shaneeza <shaneeza.ali@mongodb.com>
2022-10-13 10:02:42 -0400

    PD-1867:  Pipeline brand refresh (#1446)
    * attempt 1
    
    * attempt 2
    
    * messy for now but tooltip text is correct
    
    * stil messy but added in svgs
    
    * new svgs
    
    * updated svgs
    
    * forgot to save
    
    * gah
    
    * saving but svg is not there yet
    
    * first segment border
    
    * style updates
    
    * svg updates
    
    * styles cleanup
    
    * stuck on testing
    
    * fix circular dependency
    
    * fix test
    
    * remove clone element, hide counter with css
    
    * some cleanup
    
    * update readme
    
    * export svgs
    
    * use tooltip text for counter visibility
    
    * remove isStageElement util
    
    * add changeset
    
    * extend li to stage
    
    * comments from review
    
    * wip
    
    * move some props to context
    
    * some feedback
    
    * fix type error
    
    * add icon to tooltip text
    
    * fix errors
    
    * split tooltiptext and add test
    
    * clean up
    
    * feedback comments: switch to map, add comment
    
    * remove some files
    
    * update yarn

--- packages/pipeline/src/styles.ts
+++ packages/pipeline/src/styles.ts
@@ -1,182 +1,381 @@
-import { uiColors } from '@leafygreen-ui/palette';
-import { css } from '@leafygreen-ui/emotion';
-import { createDataProp } from '@leafygreen-ui/lib';
+import { palette } from '@leafygreen-ui/palette';
+import { css, cx } from '@leafygreen-ui/emotion';
+import { createUniqueClassName, Theme } from '@leafygreen-ui/lib';
+import { typeScales } from '@leafygreen-ui/tokens';
+import { Size } from './types';
+import { edges } from './svgs/edges';
 
-export const Size = {
-  XSmall: 'xsmall',
-  Small: 'small',
-  Normal: 'normal',
-  Large: 'large',
-} as const;
+export const svgInnerOutlineClassName = createUniqueClassName('pipeline');
+export const svgOuterOutlineClassName = createUniqueClassName('pipeline');
+export const svgInnerClassName = createUniqueClassName('pipeline');
 
-export type Size = typeof Size[keyof typeof Size];
+// base styles
 
-interface Layout {
-  borderRadius: number;
-  chevron: {
-    size: number;
-  };
-  fontSize: number;
-  fontWeight: number;
-  gutter: {
-    vertical: number;
-    horizontal: number;
-  };
-  height: number;
-  lineHeight: number;
-  minWidth: number;
-}
+export const basePipelineStyles = css`
+  display: flex;
+  counter-reset: hiddenCount;
+  font-weight: 600;
+`;
 
-export const layout: { readonly [K in Size]: Layout } = {
-  [Size.XSmall]: {
-    borderRadius: 4,
-    chevron: {
-      size: 2,
-    },
-    fontSize: 12,
-    fontWeight: 600,
-    gutter: {
-      vertical: 2,
-      horizontal: 4,
-    },
-    height: 22,
-    lineHeight: 1,
-    minWidth: 74,
-  },
+export const basePipelineListStyles = css`
+  display: flex;
+  flex-grow: 1;
+  flex-wrap: wrap;
+  overflow: hidden;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  min-width: 74px;
+`;
 
-  [Size.Small]: {
-    borderRadius: 4,
-    chevron: {
-      size: 2,
-    },
-    fontSize: 14,
-    fontWeight: 600,
-    gutter: {
-      vertical: 2,
-      horizontal: 6,
-    },
-    height: 25,
-    lineHeight: 1,
-    minWidth: 74,
-  },
-
-  [Size.Normal]: {
-    borderRadius: 4,
-    chevron: {
-      size: 3,
-    },
-    fontSize: 14,
-    fontWeight: 600,
-    gutter: {
-      vertical: 2,
-      horizontal: 8,
-    },
-    height: 32,
-    lineHeight: 1,
-    minWidth: 74,
-  },
-
-  [Size.Large]: {
-    borderRadius: 4,
-    chevron: {
-      size: 4,
-    },
-    fontSize: 16,
-    fontWeight: 600,
-    gutter: {
-      vertical: 2,
-      horizontal: 10,
-    },
-    height: 45,
-    lineHeight: 1,
-    minWidth: 74,
-  },
-} as const;
-
-interface Color {
-  color: string;
-  primary: {
-    backgroundColor: string;
-  };
-  secondary: {
-    backgroundColor: string;
-  };
-}
-
-export const colors: Color = {
-  color: uiColors.blue.base,
-  primary: {
-    backgroundColor: uiColors.blue.light2,
-  },
-  secondary: {
-    backgroundColor: uiColors.blue.light3,
-  },
+export const baseSizeStyles: Record<Size, string> = {
+  [Size.XSmall]: css`
+    font-size: ${typeScales.body1.fontSize}px;
+    height: 22px;
+  `,
+  [Size.Small]: css`
+    font-size: ${typeScales.body1.fontSize}px;
+    height: 28px;
+  `,
+  [Size.Normal]: css`
+    font-size: ${typeScales.body1.fontSize}px;
+    height: 36px;
+  `,
+  [Size.Large]: css`
+    font-size: 18px;
+    height: 48px;
+  `,
 };
 
-interface StateForStyles {
-  size: Size;
-}
+// Counter Styles
 
-export const getRootStyle = ({ size }: StateForStyles): string => {
-  const { height, lineHeight } = layout[size];
+export const counterVisibleStyles = css`
+  display: flex;
+`;
 
-  return css`
-    display: flex;
-    height: ${height}px;
-    line-height: ${lineHeight};
-  `;
-};
+export const counterSvgBaseStyles = css`
+  position: absolute;
+  width: 100%;
+  height: calc(100% + 10px);
+  display: flex;
+`;
 
-export const getChildStyle = ({ size }: StateForStyles): string => {
-  const { lineHeight } = layout[size];
+export const counterSvgColStyles = css`
+  width: 50%;
+  position: relative;
+  overflow: hidden;
+`;
 
-  return css`
-    position: relative;
-    flex-direction: row;
-    line-height: ${lineHeight};
-    align-items: center;
-    justify-content: center;
-  `;
-};
-
-export const getChevronStyle = ({ size }: StateForStyles): string => {
-  const { gutter, height, chevron } = layout[size];
-  const width = height / 2 + chevron.size * 2;
-  const offset = width;
-
-  return css`
-    margin-left: ${gutter.horizontal * 2}px;
-    width: ${width}px;
-    height: 100%;
-    position: absolute;
-    right: -${offset}px;
-    top: 0;
-    overflow: hidden;
-    align-self: center;
+export const counterSizeStyles: Record<Size, string> = {
+  [Size.XSmall]: css`
+    min-width: 44px;
+    margin-left: -6px;
 
     &::before {
-      content: '';
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      transform-origin: 0 0;
-      transform: rotate(45deg);
+      padding: 0 18px;
     }
-  `;
+  `,
+  [Size.Small]: css`
+    min-width: 48px;
+    margin-left: -8px;
+
+    &::before {
+      padding: 0 21px;
+    }
+  `,
+  [Size.Normal]: css`
+    min-width: 54px;
+    margin-left: -9px;
+
+    &::before {
+      padding: 0 23px;
+    }
+  `,
+  [Size.Large]: css`
+    min-width: 66px;
+    margin-left: -11px;
+
+    &::before {
+      padding: 0px 28px;
+    }
+  `,
 };
 
-/* LeafyGreen Data attributes */
+export const counterBaseStyles = cx(
+  css`
+    display: none;
+    flex-grow: 1;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    margin-left: -11px;
+    &::before {
+      white-space: nowrap;
+      content: '+' counter(hiddenCount);
+      z-index: 1;
+    }
 
-// Pipeline
-export const pipelineAttr = createDataProp('pipeline');
-export const pipelineStages = createDataProp('pipeline-stages');
+    .${svgInnerClassName} {
+      transition: all 300ms ease-in-out;
+    }
 
-// Stage
-export const stageAttr = createDataProp('pipeline-stage');
-export const stageChevronAttr = createDataProp('pipeline-stage-chevron');
+    .${svgInnerOutlineClassName}, .${svgOuterOutlineClassName} {
+      fill: rgba(255, 255, 255, 0);
+      transition: all 300ms ease-in-out;
+    }
 
-// Counter
-export const counterAttr = createDataProp('pipeline-counter');
-export const counterChevronAttr = createDataProp('pipeline-counter-chevron');
+    &:focus-visible,
+    &:focus {
+      outline: none;
+    }
+  `,
+);
+
+export const svgStyles = css`
+  position: absolute;
+  top: 50%;
+  transform: translate(0%, -50%);
+`;
+
+export const svgLayer1Styles = cx(
+  svgStyles,
+  css`
+    left: 0;
+  `,
+);
+
+export const svgLayer2Styles = cx(
+  svgStyles,
+  css`
+    right: 0;
+  `,
+);
+
+export const counterThemeStyles: Record<Theme, string> = {
+  [Theme.Light]: css`
+    color: ${palette.blue.dark1};
+
+    .${svgInnerClassName} {
+      fill: ${palette.blue.light3};
+    }
+
+    &:focus-visible {
+      .${svgOuterOutlineClassName} {
+        fill: ${palette.blue.light1};
+      }
+    }
+
+    &:hover {
+      .${svgInnerOutlineClassName} {
+        fill: ${palette.blue.light3};
+      }
+      .${svgInnerClassName} {
+        fill: ${palette.blue.light2};
+      }
+    }
+  `,
+  [Theme.Dark]: css`
+    color: ${palette.blue.light2};
+
+    .${svgInnerClassName} {
+      fill: ${palette.blue.dark2};
+    }
+
+    &:focus-visible {
+      .${svgOuterOutlineClassName} {
+        fill: ${palette.blue.light1};
+      }
+    }
+
+    &:hover {
+      .${svgInnerOutlineClassName} {
+        fill: ${palette.blue.dark2};
+      }
+      .${svgInnerClassName} {
+        fill: ${palette.blue.dark1};
+      }
+    }
+  `,
+};
+
+// Stage Styles
+
+export const stageTextStyles = css`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const stageTextSizeStyles: Record<Size, string> = {
+  [Size.XSmall]: css`
+    padding: 0 6px;
+  `,
+  [Size.Small]: css`
+    padding: 0 6px;
+  `,
+  [Size.Normal]: css`
+    padding: 0 8px;
+  `,
+  [Size.Large]: css`
+    padding: 0 12px;
+  `,
+};
+
+export const stageTextThemeStyles: Record<Theme, string> = {
+  [Theme.Light]: css`
+    background-color: ${palette.blue.light2};
+    color: ${palette.blue.dark1};
+  `,
+  [Theme.Dark]: css`
+    background-color: ${palette.blue.dark1};
+    color: ${palette.blue.light2};
+  `,
+};
+
+export const stageBaseStyles = cx(
+  css`
+    display: flex;
+    flex-grow: 1;
+    height: 100%;
+
+    &[data-stage-visible='false'] {
+      counter-increment: hiddenCount;
+    }
+
+    &::before,
+    &::after {
+      content: '';
+      display: inline-block;
+      flex-shrink: 0;
+      position: relative;
+      height: 100%;
+    }
+
+    &:first-of-type {
+      span {
+        border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+      }
+    }
+  `,
+);
+
+export const stageSvgThemeStyles: Record<Theme, string> = {
+  [Theme.Dark]: css`
+    &:not(&:first-of-type) {
+      &::before {
+        background: ${palette.blue.dark1};
+        right: -1px;
+      }
+    }
+    &::after {
+      background: ${palette.blue.dark1};
+      left: -1px;
+    }
+  `,
+  [Theme.Light]: css`
+    &:not(&:first-of-type) {
+      &::before {
+        background: ${palette.blue.light2};
+        right: -1px;
+      }
+    }
+    &::after {
+      background: ${palette.blue.light2};
+      left: -1px;
+    }
+  `,
+};
+
+// Adding edges of segments as an SVG with CSS
+export const stageSvgSizeStyles: Record<Size, string> = {
+  [Size.XSmall]: css`
+    &:first-of-type {
+      span {
+        padding-left: 10px;
+      }
+    }
+    &:not(&:first-of-type) {
+      margin-left: -3px;
+      &::before {
+        mask: url('${edges[Size.XSmall].before}');
+        width: 7px;
+      }
+    }
+    &::after {
+      mask: url('${edges[Size.XSmall].after}');
+      width: 9px;
+    }
+  `,
+  [Size.Small]: css`
+    &:first-of-type {
+      span {
+        padding-left: 10px;
+      }
+    }
+    &:not(&:first-of-type) {
+      margin-left: -6px;
+      &::before {
+        mask: url('${edges[Size.Small].before}');
+        width: 9px;
+      }
+    }
+    &::after {
+      mask: url('${edges[Size.Small].after}');
+      width: 11px;
+    }
+  `,
+  [Size.Normal]: css`
+    &:first-of-type {
+      span {
+        padding-left: 12px;
+      }
+    }
+    &:not(&:first-of-type) {
+      margin-left: -7px;
+      &::before {
+        mask: url('${edges[Size.Normal].before}');
+        width: 13px;
+      }
+    }
+    &::after {
+      mask: url('${edges[Size.Normal].after}');
+      width: 13px;
+    }
+  `,
+  [Size.Large]: css`
+    &:first-of-type {
+      span {
+        padding-left: 15px;
+      }
+    }
+    &:not(&:first-of-type) {
+      margin-left: -8px;
+      &::before {
+        mask: url('${edges[Size.Large].before}');
+        width: 13px;
+      }
+    }
+    &::after {
+      mask: url('${edges[Size.Large].after}');
+      width: 15px;
+    }
+  `,
+};
+
+export const tooltipStyles = css`
+  max-width: 400px;
+`;
+
+export const tooltipTextStyles = css`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 2px;
+
+  span {
+    display: inline-flex;
+  }
+`;
