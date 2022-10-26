e399f1b9c78c4111dd36cdd22f3f66d5e6b28117
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-10-26 15:24:51 -0400

    LG-2530 LG-2531 Provider inherits unset values. Exposes darkMode setter (#1468)
    * Updates provider to inherit when nested
    
    * Updates tests to use data attributes
    
    * Dark Mode setter works
    
    With externally defined setter
    
    * Moves setter inside the provider
    
    * Update DarkModeContext.tsx
    
    * changesets
    
    * Update LeafyGreenContext.spec.tsx
    
    * fix types
    
    * Resolves minor comments

--- packages/leafygreen-provider/src/DarkModeContext.spec.tsx
+++ packages/leafygreen-provider/src/DarkModeContext.spec.tsx
@@ -1,64 +1,100 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import LeafyGreenProvider, { useDarkMode } from '.';
 
 afterAll(cleanup);
 
 describe('packages/leafygreen-provider/DarkModeContext', () => {
   const TestComponent = () => {
-    const { darkMode, theme } = useDarkMode();
+    const { darkMode, theme, setDarkMode } = useDarkMode();
+
     return (
-      <div>{`${darkMode?.toString() || 'undefined'} - theme: ${theme}`}</div>
+      <>
+        <div data-testid="darkMode" data-mode={darkMode} data-theme={theme} />
+        <button onClick={() => setDarkMode(!darkMode)}></button>
+      </>
     );
   };
 
   test('without a provider, by default useDarkMode returns false', () => {
-    const { container } = render(<TestComponent />);
-    expect(container.innerHTML.includes('false')).toBeTruthy();
+    const { getByTestId } = render(<TestComponent />);
+    expect(getByTestId('darkMode')).toHaveAttribute('data-mode', 'false');
   });
 
   test('without a provider, by default theme returns "light"', () => {
-    const { container } = render(<TestComponent />);
-    expect(container.innerHTML.includes('theme: light')).toBeTruthy();
+    const { getByTestId } = render(<TestComponent />);
+    expect(getByTestId('darkMode')).toHaveAttribute('data-theme', 'light');
   });
 
   test('with a provider, by default useDarkMode returns false', () => {
-    const { container } = render(
+    const { getByTestId } = render(
       <LeafyGreenProvider>
         <TestComponent />
       </LeafyGreenProvider>,
     );
-
-    expect(container.innerHTML.includes('false')).toBeTruthy();
+    expect(getByTestId('darkMode')).toHaveAttribute('data-mode', 'false');
   });
 
   test('with a provider, by default theme returns "light"', () => {
-    const { container } = render(
+    const { getByTestId } = render(
       <LeafyGreenProvider>
         <TestComponent />
       </LeafyGreenProvider>,
     );
-
-    expect(container.innerHTML.includes('theme: light')).toBeTruthy();
+    expect(getByTestId('darkMode')).toHaveAttribute('data-theme', 'light');
   });
 
   test(`when darkMode prop is set to true, useDarkMode returns true`, () => {
-    const { container } = render(
+    const { getByTestId } = render(
       <LeafyGreenProvider darkMode={true}>
         <TestComponent />
       </LeafyGreenProvider>,
     );
-
-    expect(container.innerHTML.includes('true')).toBeTruthy();
+    expect(getByTestId('darkMode')).toHaveAttribute('data-mode', 'true');
   });
 
   test(`when darkMode prop is set to true, theme returns dark`, () => {
-    const { container } = render(
+    const { getByTestId } = render(
+      <LeafyGreenProvider darkMode={true}>
+        <TestComponent />
+      </LeafyGreenProvider>,
+    );
+    expect(getByTestId('darkMode')).toHaveAttribute('data-theme', 'dark');
+  });
+
+  test('setter updates mode & theme', () => {
+    const { getByTestId, getByRole } = render(
+      <LeafyGreenProvider darkMode={true}>
+        <TestComponent />
+      </LeafyGreenProvider>,
+    );
+    expect(getByTestId('darkMode')).toHaveAttribute('data-mode', 'true');
+    expect(getByTestId('darkMode')).toHaveAttribute('data-theme', 'dark');
+
+    const button = getByRole('button');
+    userEvent.click(button);
+
+    expect(getByTestId('darkMode')).toHaveAttribute('data-mode', 'false');
+    expect(getByTestId('darkMode')).toHaveAttribute('data-theme', 'light');
+  });
+
+  test('dark mode changes if re-rendered with a different value', () => {
+    const { getByTestId, rerender } = render(
       <LeafyGreenProvider darkMode={true}>
         <TestComponent />
       </LeafyGreenProvider>,
     );
 
-    expect(container.innerHTML.includes('theme: dark')).toBeTruthy();
+    waitFor(() => {
+      rerender(
+        <LeafyGreenProvider darkMode={false}>
+          <TestComponent />
+        </LeafyGreenProvider>,
+      );
+
+      expect(getByTestId('darkMode')).toHaveAttribute('data-mode', 'false');
+      expect(getByTestId('darkMode')).toHaveAttribute('data-theme', 'light');
+    });
   });
 });
