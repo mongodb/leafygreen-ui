30207122a211f0c1b4d0111b76c6a87147ffe7df
Shaneeza <shaneeza.ali@mongodb.com>
2022-11-08 16:47:43 -0500

    LG-2201: Group 4 (#1456)
    * initial commit
    
    * LG-2501: Refresh toggle (#1458)
    
    * add darkmode
    
    * changeset
    
    * feedback
    
    * LG-2505: Refresh radio box group (#1462)
    
    * update for darkmode
    
    * changeset
    
    * fix box-shadow
    
    * use rgba instead of transparent
    
    * update test
    
    * add to website
    
    * feedback
    
    * LG-2503: Refresh radio group (#1460)
    
    * updating for darkmode
    
    * add changeset
    
    * remove width
    
    * font variable
    
    * fix errors
    
    * feedback
    
    * LG-2504, LG-2487, LG-2557: Refresh segmented control (#1464)
    
    * first round of changes
    
    * updating padding
    
    * remove some comments
    
    * var names
    
    * add proptypes
    
    * use max-content for wrapper width
    
    * changeset
    
    * export types
    
    * remove width change
    
    * update changeset
    
    * add ellipsis to label and individual options
    
    * remove comments and undefined from story
    
    * changeset
    
    * types and remove some css
    
    * add back display flex
    
    * update test
    
    * wrap text in span
    
    * feedback and add type button to option
    
    * rob feedback
    
    * add packages
    
    * switch order of check
    
    * forgot to save comments
    
    * missed a few
    
    * prettier
    
    * LG-2502, LG-2488: Refresh code (#1479)
    
    * starting to change colors
    
    * more changes
    
    * update code colors
    
    * box shadow update
    
    * more color changes
    
    * add context
    
    * switch to theme
    
    * some color fixes and font style changes
    
    * update context to include useContext
    
    * changeset
    
    * feedback: leafygreen provider, font size
    
    * type
    
    * fix yarn?
    
    * prettier
    
    * updates some colors
    
    * fix panel styles and refactor
    
    * updates colors
    
    * feedback
    
    * feedback: number padding and larger styles
    
    * fix type
    
    * remove ellipsis logic
    
    * prettier
    
    * remove unused stuff
    
    * temp fix for icon colors

---
+++ .changeset/olive-timers-film.md
@@ -0,0 +1,5 @@
+---
+'@leafygreen-ui/segmented-control': major
+---
+
+Updates `Segmented Control` for dark mode brand refresh.
--- packages/segmented-control/src/index.ts
+++ packages/segmented-control/src/index.ts
@@ -1,2 +1,6 @@
 export { SegmentedControl } from './SegmentedControl';
 export { SegmentedControlOption } from './SegmentedControlOption';
+export type {
+  SegmentedControlProps,
+  SegmentedControlOptionProps,
+} from './types';
