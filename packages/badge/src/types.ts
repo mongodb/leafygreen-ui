9dc6fee09fc9d5811584b7ee5360d2f40d74769e
spark33 <seanpark1107@gmail.com>
2022-11-17 16:55:47 -0500

    moving Badge props ts to types.ts

--- packages/badge/src/types.ts
+++ packages/badge/src/types.ts
@@ -1,3 +1,31 @@
+import { HTMLElementProps } from "@leafygreen-ui/lib";
+
+interface BadgeProps extends HTMLElementProps<'div'> {
+  /**
+   * An additional className to add to the component's classList
+   */
+  className?: string;
+
+  /**
+   * The content to render within the badge
+   */
+  children?: React.ReactNode;
+
+  /**
+   * The Badge's style variant
+   *
+   * @default 'lightgray'
+   */
+  variant?: Variant;
+
+  /**
+   * Determines whether or not the component will be rendered in dark theme.
+   *
+   * @default false
+   */
+  darkMode?: boolean;
+}
+
 export const Variant = {
   DarkGray: 'darkgray',
   LightGray: 'lightgray',
@@ -8,3 +36,5 @@ export const Variant = {
 } as const;
 
 export type Variant = typeof Variant[keyof typeof Variant];
+
+export default BadgeProps;
