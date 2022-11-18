6338647d4c9d266119b6515a370033abea6c49eb
brooke <bsyalof@gmail.com>
2022-11-18 15:51:17 -0500

    lints

--- packages/typography/src/Description/Description.types.ts
+++ packages/typography/src/Description/Description.types.ts
@@ -1,4 +1,5 @@
 import { HTMLElementProps } from '@leafygreen-ui/lib';
 import { CommonTypographyProps } from '../types';
 
-export type DescriptionProps = HTMLElementProps<'p'> & CommonTypographyProps & { disabled?: boolean };
+export type DescriptionProps = HTMLElementProps<'p'> &
+  CommonTypographyProps & { disabled?: boolean };
