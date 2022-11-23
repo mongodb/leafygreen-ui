f8285105c0960aa4f0bd0fcabb0bce93cc3eb37d
Adam Thompson <adam.thompson@mongodb.com>
2022-11-23 14:16:47 -0500

    fix

--- packages/typography/src/H1/H1.types.ts
+++ packages/typography/src/H1/H1.types.ts
@@ -1,4 +1,3 @@
 import { TypographyProps } from '../types';
 
 export type H1Props<T extends keyof JSX.IntrinsicElements> = TypographyProps<T>;
-
--- packages/typography/src/H2/H2.types.ts
+++ packages/typography/src/H2/H2.types.ts
@@ -1,4 +1,3 @@
 import { TypographyProps } from '../types';
 
 export type H2Props<T extends keyof JSX.IntrinsicElements> = TypographyProps<T>;
-
