457daf1c50ccaa1e85c66c650132fc8a3744a9db
Adam Thompson <adam.thompson@mongodb.com>
2022-11-28 15:00:10 -0500

    Adds usePolymorphic hook

--- packages/internal/src/Polymorphic/Polymorphic.hooks.tsx
+++ packages/internal/src/Polymorphic/Polymorphic.hooks.tsx
@@ -1,16 +1,31 @@
+import isUndefined from 'lodash/isUndefined';
 import React from 'react';
 
 /**
  * A wrapper around `React.useRef`
  * that provides an accurately typed Ref object
  */
-export const usePolymorphicRef = <
-  E extends keyof HTMLElementTagNameMap & React.ElementType,
->() => {
+export const usePolymorphicRef = <E extends React.ElementType>() => {
   return React.useRef<
     | (E extends keyof HTMLElementTagNameMap
         ? HTMLElementTagNameMap[E]
         : unknown)
     | null
   >(null);
+};
+
+export const usePolymorphicComponent = (
+  as?: React.ElementType,
+): React.ElementType => {
+  const Component = as || 'div';
+  return Component;
+};
+
+export const usePolymorphic = <T extends React.ElementType>(as?: T) => {
+  const Component = usePolymorphicComponent(as);
+  const ref = usePolymorphicRef<T>();
+  return {
+    Component,
+    ref,
+  };
 };
