214971ab651fd432cb0b0f85c7110da8f00b5515
Shaneeza <shaneeza.ali@mongodb.com>
2022-12-01 11:23:54 -0500

    remove old blob file

--- packages/marketing-modal/src/index.ts
+++ packages/marketing-modal/src/index.ts
@@ -1,4 +1,3 @@
 export { default } from './MarketingModal/MarketingModal';
 export { GraphicStyle, BlobPosition } from './MarketingModal/types';
 export type { MarketingModalProps } from './MarketingModal/types';
-export { default as svgBlobs } from './blobs';
