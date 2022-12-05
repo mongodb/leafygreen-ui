e557af1cd4be35e4497d76ce2d1c0e60a862fc48
Adam Thompson <adam.thompson@mongodb.com>
2022-12-05 14:04:05 -0500

    Remove example test

--- packages/internal-input-option/src/InputOption.spec.tsx
+++ packages/internal-input-option/src/InputOption.spec.tsx
@@ -13,16 +13,8 @@ describe('packages/internal/input-option', () => {
   describe.skip('Types behave as expected', () => {
     // eslint-disable-next-line jest/expect-expect
     test('types', () => {
-      const MyComponent = (props: InputOptionProps) => <li {...props} />;
-
-      /** @ts-expect-error */
-      render(<MyComponent />);
-      render(<MyComponent aria-label="asdf" />);
-      render(<MyComponent aria-labelledby="asdf" />);
-
       /** @ts-expect-error */
       render(<InputOption />);
-      render(<InputOption href="asdf" />);
       render(<InputOption aria-label="Some Label" />);
       render(<InputOption aria-labelledby="some-id" />);
     });
