d29dd0656ddf52f6f5b425e28cd2fb8a28c08117
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-12-05 12:10:43 -0500

    Update Package Dependencies & TSConfigs (#1511)
    * Update depcheck script
    
    * format tsoconfigs
    
    * rm palette from popover
    
    * fix
    
    * rm lib from ripple
    
    * Update tsconfig.json
    
    * Updates depcheck to keep devDependencies
    
    * Fixes missing dependencies
    
    * Removes unused devDeps
    
    * Lints
    
    * Comments
    
    * Update package.json
    
    * Fix icon
    
    * refactor depcheck
    
    * Adds polished. Removes mongo-nav
    
    * spread unusedDev
    
    * reverts tsconfig prettier
    
    * lint
    
    * add comments
    
    * Update yarn.lock
    
    * fix
    
    * Update depcheck to handle dev deps
    
    * update dev deps
    
    * fix
    
    * Fix tsconfigs
    
    * fix
    
    * Removes build-cache for check:build step
    
    * Ignore devDeps when updating tsconfig
    
    * Only write tsconfig if needed
    
    * Adds dev/dep inconsistency check
    
    * updates package.json and tesonfigs
    
    * Create fifty-coats-melt.md
    
    * Adds regular dependency check
    
    * update more tsconfigs
    
    * fix depcheck
    
    * Update yarn.lock
    
    * if there are no devdeps, remove the object
    
    * remove empty devdeps
    
    * fix
    
    * Update tsconfig.json

--- scripts/depcheck.ts
+++ scripts/depcheck.ts
@@ -5,27 +5,34 @@ import { resolve } from 'path';
 import { exit } from 'process';
 import chalk from 'chalk';
 import { Command } from 'commander';
-import packageJson from '../package.json';
 import { SpawnOptions, spawnSync } from 'child_process';
 import { getPackageLGDependencies } from './utils/getPackageDependencies';
+import globalPackageJson from '../package.json';
+import fetch from 'node-fetch';
+import { isEqual, pick } from 'lodash';
+
 const lgPackages = readdirSync('packages/');
-const devDependencies = Object.keys(packageJson.devDependencies);
+const globalDevDependencies = Object.keys(globalPackageJson.devDependencies);
 
 const cli = new Command('depcheck')
+  .arguments('[...packages]')
+  .option('-v, --verbose', 'Verbose mode', false)
   .option('-f, --fix', 'Option to fix any errors found', false)
   .option(
     '--fix-tsconfig',
     'Optionally overwrite the tsconfig.json based on package.json',
   )
   .parse(process.argv);
+
 const fix: boolean = cli.opts()['fix'];
 const fixTS = cli.opts()['fixTsconfig'];
+const verbose = cli.opts()['verbose'];
 
 const depcheckOptions: depcheck.Options = {
   ignorePatterns: [
     // files matching these patterns will be ignored
-    '*.spec.tsx',
-    '*.story.tsx',
+    // '*.spec.tsx',
+    // '*.story.tsx',
   ],
   ignoreMatches: [
     // ignore dependencies that matches these globs
@@ -38,33 +45,115 @@ checkDependencies();
 async function checkDependencies() {
   let issuesFound = false;
 
-  for (const pkg of lgPackages) {
+  const packages = cli.args.length > 0 ? cli.args : lgPackages;
+
+  for (const pkg of packages) {
     const check = await depcheck(
       resolve(__dirname, `../packages/${pkg}`),
       depcheckOptions,
     );
-    const { dependencies: unused, missing: missingLocal } = check;
+    const {
+      dependencies: unusedDeps,
+      devDependencies: unusedDev,
+      missing: missingLocal,
+      using,
+    } = check;
 
-    const missing = Object.keys(missingLocal).filter(
-      dep => !devDependencies.includes(dep),
-    );
+    const pkgJson = readPackageJson(pkg);
 
-    if (missing.length > 0 || unused.length > 0) {
-      missing.length > 0 &&
+    // Decide which missing dependencies should just be devDependencies
+    const missing = sortDependenciesByUsage(missingLocal, pkg);
+
+    // Ensure used dependencies are listed correctly per their usage
+    // i.e. every listed devDep should _only_ be used in .story or .spec files
+    // If it's used in other files, we remove it and re-install it as a regular dependency
+    {
+      const usedAsDev = sortDependenciesByUsage(using, pkg).devDependencies;
+      const listed = pick(pkgJson, ['devDependencies']).devDependencies;
+      const listedDev = listed ? Object.keys(listed) : [];
+
+      if (
+        listedDev.length &&
+        !listedDev.every(depName =>
+          using[depName].every(
+            file => file.includes('.story.tsx') || file.includes('.spec.tsx'),
+          ),
+        )
+      ) {
+        const notUsedAsDev = listedDev.filter(dep => !usedAsDev.includes(dep));
+        // add the dependencies that are listed as dev but not used as dev to the unused array to uninstall them
+        unusedDev.push(...notUsedAsDev);
+        missing.dependencies.push(...notUsedAsDev);
+      }
+    }
+
+    // Do the inverse of above
+    // Ensure dependencies listed as `dependencies` are used in files other than .story or .spec files
+    // i.e. Every listed dependency should be used in at leas one file that is not .story or .spec
+    {
+      const usedAsDependency = sortDependenciesByUsage(using, pkg).dependencies;
+      const listed = pick(pkgJson, ['dependencies']).dependencies;
+      const listedDeps = listed ? Object.keys(listed) : [];
+
+      if (
+        usedAsDependency &&
+        listedDeps &&
+        !listedDeps.every(depName =>
+          using[depName].some(
+            file => !file.includes('.story.tsx') && !file.includes('.spec.tsx'),
+          ),
+        )
+      ) {
+        const notUsedAsDep = listedDeps.filter(
+          dep => !usedAsDependency.includes(dep),
+        );
+        unusedDeps.push(...notUsedAsDep);
+      }
+    }
+
+    const countMissing = Object.keys(missing.dependencies).length;
+    const countMissingDev = Object.keys(missing.devDependencies).length;
+
+    if (
+      countMissing > 0 ||
+      countMissingDev > 0 ||
+      unusedDeps.length > 0 ||
+      unusedDev.length > 0
+    ) {
+      unusedDeps.length > 0 &&
         console.log(
-          `${chalk.green(`packages/${pkg}`)} is missing: ${chalk.redBright(
-            missing.join(', '),
+          `${chalk.green(`packages/${pkg}`)} does not use ${chalk.blueBright(
+            unusedDeps.join(', '),
           )}`,
         );
-      unused.length > 0 &&
+
+      unusedDev.length > 0 &&
         console.log(
-          `${chalk.green(`packages/${pkg}`)} doesn't use ${chalk.blueBright(
-            unused.join(', '),
+          `${chalk.green(`packages/${pkg}`)} does not use ${chalk.blueBright(
+            unusedDev.join(', '),
+          )} as devDependencies`,
+        );
+
+      countMissing > 0 &&
+        console.log(
+          `${chalk.green(
+            `packages/${pkg}`,
+          )} is missing dependencies: ${chalk.redBright(
+            missing.dependencies.join(', '),
+          )}`,
+        );
+
+      countMissingDev > 0 &&
+        console.log(
+          `${chalk.green(
+            `packages/${pkg}`,
+          )} is missing devDependencies: ${chalk.yellowBright(
+            missing.devDependencies.join(', '),
           )}`,
         );
 
       if (fix) {
-        fixDependencies(pkg, missing, unused);
+        await fixDependencies(pkg, missing, [...unusedDeps, ...unusedDev]);
       } else {
         issuesFound = true;
       }
@@ -84,17 +173,58 @@ async function checkDependencies() {
   }
 }
 
-function fixDependencies(
+async function fixDependencies(
   pkg: string,
-  missing: Array<string>,
+  missing: {
+    dependencies: Array<string>;
+    devDependencies: Array<string>;
+  },
   unused: Array<string>,
-): void {
+) {
   const cmdOpts: SpawnOptions = { stdio: 'inherit', cwd: `packages/${pkg}` };
   // Using yarn 1.19.0 https://stackoverflow.com/questions/62254089/expected-workspace-package-to-exist-for-sane
-  missing.length > 0 &&
-    spawnSync('npx', ['yarn@1.19.0', 'add', '-W', ...missing], cmdOpts);
   unused.length > 0 &&
     spawnSync('npx', ['yarn@1.19.0', 'remove', ...unused], cmdOpts);
+
+  missing.dependencies.length > 0 &&
+    spawnSync('npx', ['yarn@1.19.0', 'add', ...missing.dependencies], cmdOpts);
+
+  // There's a bug in yarn@1.19.0 where some packages don't install as devDependencies
+  // even if the `-D` flag is provided.
+  // So we have to install devDependencies manually
+  if (missing.devDependencies.length > 0) {
+    const pkgJson = readPackageJson(pkg);
+
+    const missingDevDepsObject = {} as { [key: string]: string };
+
+    for (const depName of missing.devDependencies) {
+      const searchUrl = `https://registry.npmjs.com/-/v1/search?text=${depName}`;
+      const { objects }: any = await fetch(searchUrl).then(data => data.json());
+      const pkgRef = objects
+        ? objects.find((obj: any) => obj.package.name === depName)
+        : null;
+
+      if (!pkgRef) {
+        console.error(chalk.red(`Could not find ${depName} on npm`));
+        break;
+      }
+
+      const { version } = pkgRef.package;
+      missingDevDepsObject[depName] = `^${version}`;
+    }
+    pkgJson.devDependencies = {
+      ...pkgJson?.devDependencies,
+      ...missingDevDepsObject,
+    };
+
+    if (pkgJson.devDependencies.length <= 0) {
+      delete pkgJson.devDependencies;
+    }
+
+    const pkgJsonPath = resolve(__dirname, `../packages/${pkg}/package.json`);
+    writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2) + '\n');
+    spawnSync(`yarn`, ['install'], cmdOpts);
+  }
 }
 
 /**
@@ -102,12 +232,66 @@ function fixDependencies(
  */
 function fixTSconfig(pkg: string) {
   const tsConfigFileName = `packages/${pkg}/tsconfig.json`;
-  const dependencies = getPackageLGDependencies(pkg);
-  const tsconfig = JSON.parse(readFileSync(tsConfigFileName, 'utf-8'));
-  tsconfig.references = dependencies
-    .filter(dep => dep !== 'mongo-nav')
-    .map(dep => ({
-      path: `../${dep}`,
-    }));
-  writeFileSync(tsConfigFileName, JSON.stringify(tsconfig, null, 2) + '\n');
+  // Including devDependencies in tsconfig can introduce circular dependencies
+  const dependencies = getPackageLGDependencies(pkg, { dev: false });
+
+  try {
+    const tsconfig = JSON.parse(readFileSync(tsConfigFileName, 'utf-8'));
+    const refs = dependencies
+      .filter(dep => dep !== 'mongo-nav')
+      .map(dep => ({
+        path: `../${dep}`,
+      }));
+
+    if (!isEqual(tsconfig.references, refs)) {
+      tsconfig.references = refs;
+      console.log(chalk.gray(`Fixing ${pkg}/tsconfig.json`));
+      writeFileSync(tsConfigFileName, JSON.stringify(tsconfig, null, 2) + '\n');
+    }
+  } catch (error) {
+    throw new Error(`Error in ${pkg}/tsconfig.json: ${error}`);
+  }
+}
+
+function sortDependenciesByUsage(
+  localDeps: Record<string, Array<string>>,
+  pkg: string,
+): {
+  dependencies: Array<string>;
+  devDependencies: Array<string>;
+} {
+  return (
+    Object.entries(localDeps)
+      // If the package in provided globally, ignore it
+      .filter(([dep]) => !globalDevDependencies.includes(dep))
+      // If a dependency is only used in tests or storybook,
+      // then we add it as a dev dependency
+      .reduce(
+        (_missing, [name, usedIn]) => {
+          if (
+            usedIn.every(
+              file => file.includes('.story.tsx') || file.includes('.spec.tsx'),
+            )
+          ) {
+            verbose && console.log(`${pkg} uses ${name} in a test file`);
+            _missing.devDependencies.push(name);
+          } else {
+            verbose && console.log(`${pkg} missing ${name}`);
+            _missing.dependencies.push(name);
+          }
+
+          return _missing;
+        },
+        {
+          dependencies: [] as Array<string>,
+          devDependencies: [] as Array<string>,
+        },
+      )
+  );
+}
+
+function readPackageJson(pkg: string): { [key: string]: any } {
+  const pkgJsonPath = resolve(__dirname, `../packages/${pkg}/package.json`);
+  const pkgJson = JSON.parse(readFileSync(pkgJsonPath, 'utf-8'));
+  return pkgJson;
 }
