be90d79efd6b90ba22f2b588fb7f26fe84ff53b4
Adam Thompson <adam.thompson@mongodb.com>
2022-12-06 13:49:41 -0500

    changeset

---
+++ .changeset/happy-rabbits-laugh.md
@@ -0,0 +1,5 @@
+---
+'@leafygreen-ui/tokens': minor
+---
+
+Exports `shadows` token with `menu`, `card` and `modal` values, mirroring the Figma library
--- packages/tokens/src/shadows.ts
+++ packages/tokens/src/shadows.ts
@@ -3,8 +3,8 @@ import { palette } from '@leafygreen-ui/palette';
 
 const shadows = {
   menu: `0px 4px 4px ${transparentize(0.75, '#000000')};`,
-  card: `0px 4px 10px -4px ${transparentize(0.70, palette.black)};`,
-  modal: `0px 8px 20px -8px ${transparentize(0.40, palette.black)};`,
-} as const
+  card: `0px 4px 10px -4px ${transparentize(0.7, palette.black)};`,
+  modal: `0px 8px 20px -8px ${transparentize(0.4, palette.black)};`,
+} as const;
 
-export default shadows
+export default shadows;
