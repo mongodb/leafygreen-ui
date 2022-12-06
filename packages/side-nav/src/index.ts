b92e9d391e39a8df73abc6b3f691ffe8e7069442
Shaneeza <shaneeza.ali@mongodb.com>
2022-12-06 15:31:30 -0500

    prettier

--- packages/side-nav/src/index.ts
+++ packages/side-nav/src/index.ts
@@ -1,7 +1,7 @@
 import SideNav, { sideNavSelector } from './SideNav';
 import { SideNavGroup } from './SideNavGroup';
-import { CollapsedSideNavItem,SideNavItem } from './SideNavItem';
-import { sideNavItemSidePadding,sideNavWidth } from './styles';
+import { CollapsedSideNavItem, SideNavItem } from './SideNavItem';
+import { sideNavItemSidePadding, sideNavWidth } from './styles';
 
 export {
   CollapsedSideNavItem,
