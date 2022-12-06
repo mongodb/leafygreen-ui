a1f9ba5aa8f54814fba77c577935409d6e3a5745
Shaneeza <shaneeza.ali@mongodb.com>
2022-12-06 13:29:04 -0500

    collasped icon styles

--- packages/side-nav/src/SideNavItem/CollapsedSideNavItem.tsx
+++ packages/side-nav/src/SideNavItem/CollapsedSideNavItem.tsx
@@ -3,23 +3,45 @@ import Portal from '@leafygreen-ui/portal';
 import { cx, css } from '@leafygreen-ui/emotion';
 import { palette } from '@leafygreen-ui/palette';
 import { useSideNavContext } from '../SideNav/SideNavContext';
+import { Theme } from '@leafygreen-ui/lib';
 
-const collapsedItemStyles = css`
+// TODO: break out
+
+const baseStyles = css`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 40px;
-  border-bottom: 1px solid ${palette.gray.light2};
-  color: ${palette.green.dark2};
-
-  &:first-of-type {
-    border-top: 1px solid ${palette.gray.light2};
-  }
 `;
 
-const collapsedItemActiveStyle = css`
-  background-color: ${palette.green.light3};
-`;
+const themeStyle: Record<Theme, string> = {
+  [Theme.Light]: css`
+    border-bottom: 1px solid ${palette.gray.light2};
+    color: ${palette.green.dark2};
+
+    &:first-of-type {
+      border-top: 1px solid ${palette.gray.light2};
+    }
+  `,
+  [Theme.Dark]: css`
+    border-bottom: 1px solid ${palette.gray.dark2};
+    color: ${palette.green.dark1};
+
+    &:first-of-type {
+      border-top: 1px solid ${palette.gray.dark2};
+    }
+  `,
+};
+
+const activeThemeStyle: Record<Theme, string> = {
+  [Theme.Light]: css`
+    background-color: ${palette.green.light3};
+  `,
+  [Theme.Dark]: css`
+    color: ${palette.white};
+    background-color: ${palette.green.dark3};
+  `,
+};
 
 interface CollapsedSideNavItemProps {
   /**
@@ -43,15 +65,16 @@ function CollapsedSideNavItem({
   active = false,
   className,
 }: CollapsedSideNavItemProps) {
-  const { portalContainer } = useSideNavContext();
+  const { portalContainer, theme } = useSideNavContext();
 
   return (
     <Portal container={portalContainer}>
       <li
         className={cx(
-          collapsedItemStyles,
+          baseStyles,
+          themeStyle[theme],
           {
-            [collapsedItemActiveStyle]: active,
+            [activeThemeStyle[theme]]: active,
           },
           className,
         )}
