8433a3f0414587bb3d377c02ebcbf2ec06971f79
Adam Thompson <adam.thompson@mongodb.com>
2022-12-06 13:48:31 -0500

    Update shadows.ts

--- packages/tokens/src/shadows.ts
+++ packages/tokens/src/shadows.ts
@@ -2,7 +2,7 @@ import { transparentize } from 'polished';
 import { palette } from '@leafygreen-ui/palette';
 
 const shadows = {
-  menu: `0px 4px 4px ${transparentize(0.75, palette.black)};`,
+  menu: `0px 4px 4px ${transparentize(0.75, '#000000')};`,
   card: `0px 4px 10px -4px ${transparentize(0.70, palette.black)};`,
   modal: `0px 8px 20px -8px ${transparentize(0.40, palette.black)};`,
 } as const
