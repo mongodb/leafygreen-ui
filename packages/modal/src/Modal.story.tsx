c65de3fa7a965c66472f548defc3c2312cda2d61
Shaneeza <shaneeza.ali@mongodb.com>
2022-12-06 14:46:41 -0500

    add spacing to story

--- packages/modal/src/Modal.story.tsx
+++ packages/modal/src/Modal.story.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Button from '@leafygreen-ui/button';
-import { css } from '@leafygreen-ui/emotion';
+import { css, cx } from '@leafygreen-ui/emotion';
 import Modal, { CloseIconColor, ModalProps, ModalSize } from '.';
 import { Select, Option, OptionGroup } from '@leafygreen-ui/select';
 import { Subtitle, Body, H3 } from '@leafygreen-ui/typography';
@@ -8,6 +8,7 @@ import Copyable from '@leafygreen-ui/copyable';
 import Code from '@leafygreen-ui/code';
 import { ComponentStory, Meta } from '@storybook/react';
 import { storybookArgTypes } from '@leafygreen-ui/lib';
+import { spacing } from '@leafygreen-ui/tokens';
 
 export default {
   title: 'Components/Modals/Modal',
@@ -39,6 +40,12 @@ export default {
   },
 } as Meta<typeof Modal>;
 
+const margin = css`
+  & > * + * {
+    margin-top: ${spacing[3]}px;
+  }
+`;
+
 const ControlledTemplate: ComponentStory<typeof Modal> = (args: ModalProps) => {
   const [open, setOpen] = useState(false);
   const { darkMode } = args;
@@ -55,10 +62,10 @@ const ControlledTemplate: ComponentStory<typeof Modal> = (args: ModalProps) => {
 export const Controlled = ControlledTemplate.bind({});
 Controlled.args = {
   children: (
-    <>
+    <div className={margin}>
       <H3>Base modal</H3>
       <Body>Modal Content goes here.</Body>
-    </>
+    </div>
   ),
 };
 
@@ -66,9 +73,12 @@ export const Scroll = ControlledTemplate.bind({});
 Scroll.args = {
   children: (
     <div
-      className={css`
-        height: 200vh;
-      `}
+      className={cx(
+        css`
+          height: 200vh;
+        `,
+        margin,
+      )}
     >
       <div>Modal Content goes here.</div>
     </div>
@@ -96,64 +106,66 @@ export const DefaultSelect = (args: ModalProps) => {
         Open Modal
       </Button>
       <Modal {...args} open={open} setOpen={setOpen}>
-        <Subtitle>Modal Content goes here.</Subtitle>
-        <Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          tristique risus sed est finibus pellentesque. Vestibulum feugiat,
-          libero in efficitur egestas, ipsum leo mattis purus, nec maximus nisl
-          lorem at orci. Nam nunc turpis, vehicula ac aliquam vitae, convallis
-          et turpis. Fusce fermentum laoreet gravida. Nam malesuada nisl eget
-          blandit auctor. Quisque quis posuere enim. Etiam non est sit amet diam
-          efficitur malesuada. In ut pretium risus. Etiam convallis rhoncus
-          tempor. Donec ullamcorper maximus enim sed dapibus. Duis ac vehicula
-          orci, et semper turpis.
-        </Body>
-        <Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          tristique risus sed est finibus pellentesque. Vestibulum feugiat,
-          libero in efficitur egestas, ipsum leo mattis purus, nec maximus nisl
-          lorem at orci. Nam nunc turpis, vehicula ac aliquam vitae, convallis
-          et turpis. Fusce fermentum laoreet gravida. Nam malesuada nisl eget
-          blandit auctor. Quisque quis posuere enim. Etiam non est sit amet diam
-          efficitur malesuada. In ut pretium risus. Etiam convallis rhoncus
-          tempor. Donec ullamcorper maximus enim sed dapibus. Duis ac vehicula
-          orci, et semper turpis.
-        </Body>
+        <div className={margin}>
+          <Subtitle>Modal Content goes here.</Subtitle>
+          <Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            tristique risus sed est finibus pellentesque. Vestibulum feugiat,
+            libero in efficitur egestas, ipsum leo mattis purus, nec maximus
+            nisl lorem at orci. Nam nunc turpis, vehicula ac aliquam vitae,
+            convallis et turpis. Fusce fermentum laoreet gravida. Nam malesuada
+            nisl eget blandit auctor. Quisque quis posuere enim. Etiam non est
+            sit amet diam efficitur malesuada. In ut pretium risus. Etiam
+            convallis rhoncus tempor. Donec ullamcorper maximus enim sed
+            dapibus. Duis ac vehicula orci, et semper turpis.
+          </Body>
+          <Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            tristique risus sed est finibus pellentesque. Vestibulum feugiat,
+            libero in efficitur egestas, ipsum leo mattis purus, nec maximus
+            nisl lorem at orci. Nam nunc turpis, vehicula ac aliquam vitae,
+            convallis et turpis. Fusce fermentum laoreet gravida. Nam malesuada
+            nisl eget blandit auctor. Quisque quis posuere enim. Etiam non est
+            sit amet diam efficitur malesuada. In ut pretium risus. Etiam
+            convallis rhoncus tempor. Donec ullamcorper maximus enim sed
+            dapibus. Duis ac vehicula orci, et semper turpis.
+          </Body>
 
-        <div>
-          <Select
-            label="label"
-            size="small"
-            placeholder="animals"
-            name="pets"
-            value={value}
-            onChange={setValue}
-            usePortal={true}
-          >
-            <OptionGroup label="Common">
-              <Option value="dog">Dog</Option>
-              <Option value="cat">Cat</Option>
-              <Option value="axolotl">Axolotl</Option>
-            </OptionGroup>
-          </Select>
-        </div>
+          <div>
+            <Select
+              label="label"
+              size="small"
+              placeholder="animals"
+              name="pets"
+              value={value}
+              onChange={setValue}
+              usePortal={true}
+            >
+              <OptionGroup label="Common">
+                <Option value="dog">Dog</Option>
+                <Option value="cat">Cat</Option>
+                <Option value="axolotl">Axolotl</Option>
+              </OptionGroup>
+            </Select>
+          </div>
 
-        <div>
-          <Select
-            label="label2"
-            size="small"
-            placeholder="types of cat"
-            name="cats"
-            value={valueB}
-            onChange={setValueB}
-            usePortal={true}
-          >
-            <OptionGroup label="Common">
-              <Option value="smallCat">smallCat</Option>
-              <Option value="largeCat">largeCat</Option>
-              <Option value="mediumCat">mediumCat</Option>
-            </OptionGroup>
-          </Select>
+          <div>
+            <Select
+              label="label2"
+              size="small"
+              placeholder="types of cat"
+              name="cats"
+              value={valueB}
+              onChange={setValueB}
+              usePortal={true}
+            >
+              <OptionGroup label="Common">
+                <Option value="smallCat">smallCat</Option>
+                <Option value="largeCat">largeCat</Option>
+                <Option value="mediumCat">mediumCat</Option>
+              </OptionGroup>
+            </Select>
+          </div>
         </div>
       </Modal>
     </>
@@ -191,12 +203,14 @@ console.log(greeting('World'));
     <>
       <Button onClick={() => setOpen(!open)}>Open Modal</Button>
       <Modal {...args} open={open} setOpen={setOpen}>
-        <div>Modal Content goes here.</div>
-        <Copyable>Hello world in a modal</Copyable>
+        <div className={margin}>
+          <div>Modal Content goes here.</div>
+          <Copyable>Hello world in a modal</Copyable>
 
-        <Code copyable={true} language="javascript">
-          {jsSnippet}
-        </Code>
+          <Code copyable={true} language="javascript">
+            {jsSnippet}
+          </Code>
+        </div>
       </Modal>
     </>
   );
