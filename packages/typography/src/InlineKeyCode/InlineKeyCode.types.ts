a0d6638c42dac5ffb74955fdcfcc18bd6264bd0e
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2022-12-09 15:34:06 -0500

    Adds baseFontSize prop to all Typography components that consume baseFontSize from LG provider (#1534)
    * adds baseFontSize prop to all typography elements that consume font size from LG provider
    
    * with changeset
    
    * run yarn fix
    
    * tooltip too
    
    * adds types to readmes
    
    * accidentlly commit workspace settings
    
    * rm comma
    
    * responding to PR feedback
    
    Co-authored-by: brooke <bsyalof@gmail.com>

--- packages/typography/src/InlineKeyCode/InlineKeyCode.types.ts
+++ packages/typography/src/InlineKeyCode/InlineKeyCode.types.ts
@@ -1,5 +1,6 @@
 import { HTMLElementProps } from '@leafygreen-ui/lib';
 
-import { CommonTypographyProps } from '../types';
+import { ResponsiveTypographyProps } from '../types';
 
-export type InlineKeyCodeProps = HTMLElementProps<'h1'> & CommonTypographyProps;
+export type InlineKeyCodeProps = HTMLElementProps<'h1'> &
+  ResponsiveTypographyProps;
