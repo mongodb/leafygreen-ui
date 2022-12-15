019f13d9c9a0d38516aa0256d4d677976ac7af1c
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2022-12-15 18:17:23 -0500

    LG-2161 Create `Polymorphic` component (#1515)
    * initial poly
    
    * Create Box.spec.tsx
    
    * Update Box.spec.tsx
    
    * First pass polymorphic
    
    * Adds tests
    
    * generates tsdoc
    
    * Restructures tsdoc script
    
    Restructure so we have an exported parseTSDoc method
    
    * fix
    
    * Adds tsdoc tests
    
    * creates usePolymorphicRef
    
    * Move types to separate file
    
    * Improves PolymorphicComponentType
    
    Moves usePolymorphicRef
    
    * Break out makeWrapperComponent
    
    * fix
    
    * Adds restricted tests
    
    * Fixes dependencies
    
    * Updates parser. Improves story
    
    * Adds TODO
    
    * limits lib/IIntrinsicElements
    
    * fix
    
    * Adds usePolymorphic hook
    
    * Adds example. Creates PolymorphicComponent factory
    
    * fixes
    
    * Adds Readme
    
    * Update README.md
    
    * lint
    
    * Move to independent package
    
    * Fixes polymorph tests
    
    * rename packages dir
    
    * Break up Polymorph and Polymorphic
    
    * updates readme
    
    * fix tests
    
    * Supports conditional `href` prop
    
    * Disables basic Polymorph test
    
    * Improves href API
    
    * Creates "ImplicitPolymorphic"
    
    * Update index.ts
    
    * Remove build cache for build check step
    
    * Fixes polymorphic build
    
    * Adds Styled tests
    
    * Supports Emotion Styled
    
    * fix
    
    * Update example syntax
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Fixes versions
    
    * Update to InferredXYZ. Update Readme.
    
    * Finish "inferred" refactor
    
    * Removes unused dependencies
    
    * Rename example to polymorphic.example
    
    * Update depcheck to ignore .example files
    
    * fix depcheck
    
    * Creates test utils. Creates expect.toBePolymorphic
    
    * Adds toBePolymorphic to global jest namespace
    
    * Update PolymorphicTest.utils.tsx
    
    * Update depcheck ignored file
    
    * Moves toBePolymorphic
    
    * fix
    
    * Update Polymorphic.testutils.tsx
    
    * Update package.json
    
    * Update re:Brooke comments
    
    * Moves HOC example tests to new file
    
    * Ensure Polymorphic Display name is always defined
    
    * Fix import order
    
    * fix
    
    * addressing basic feedback
    
    * Moves InferredPolymorphic to subdir
    
    * rename example tests
    
    * fix
    
    * Supports propTypes
    
    * Update Polymorphic.spec.tsx
    
    * Adds clarifying comment to hooks
    
    * Adds tests to ensure TS props using styled API
    
    * Update Polymorphic.types.ts
    
    * Renames examples.spec
    
    * Creates type InferredPolymorphicComponentType
    
    * Remove exports
    
    {BasePolymorph, getPolymorphicComponent }
    
    * Removes unused var
    
    * Clarify type assertion cases
    
    * Restructure polymorphic
    
    * Fix errors
    
    * Update InferredPolymorphic.types.ts
    
    * Update Polymorphic.ts
    
    * update testFilePatterns
    
    * Improves TSDoc annotations
    
    * fix

---
+++ .changeset/lemon-bats-yawn.md
@@ -0,0 +1,5 @@
+---
+'@leafygreen-ui/polymorphic': minor
+---
+
+Releases the Polymorphic component
---
+++ packages/polymorphic/src/Example/Polymorphic.example.tsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import {
+  type InferredPolymorphicProps,
+  Polymorph,
+  Polymorphic,
+  PolymorphicAs,
+  type PolymorphicComponentType,
+  type PolymorphicPropsWithRef,
+  type PolymorphicRef,
+  useInferredPolymorphic,
+  usePolymorphic,
+} from '..';
+
+interface ExampleProps {
+  /** An arbitrary title */
+  title?: string;
+  /** Flag for dark mode */
+  darkMode?: boolean;
+}
+
+/**
+ * Uses `usePolymorphic` hook
+ */
+
+export const ExamplePolymorphic = Polymorphic<ExampleProps>(
+  ({ as, title, ...rest }) => {
+    const { Component, ref } = usePolymorphic(as);
+    return (
+      <Component ref={ref} {...rest}>
+        {title}
+      </Component>
+    );
+  },
+  'ExamplePolymorphic',
+);
+
+export const ExamplePolymorphicWithRef = Polymorphic<ExampleProps>(
+  ({ as, title, ...rest }, ref) => {
+    const { Component } = usePolymorphic(as);
+    return (
+      <Component ref={ref} {...rest}>
+        {title}
+      </Component>
+    );
+  },
+  'ExamplePolymorphicWithRef',
+);
+
+export const ExampleInferred = Polymorphic<
+  InferredPolymorphicProps<ExampleProps>
+>(({ as, title, ...rest }) => {
+  const { Component, ref } = useInferredPolymorphic(as, rest);
+  return (
+    <Component ref={ref} {...rest}>
+      {title}
+    </Component>
+  );
+}, 'ExampleInferred');
+
+/**
+ * Advanced usage, not recommended
+ */
+type AdvancedProps<T extends PolymorphicAs> = PolymorphicPropsWithRef<
+  T,
+  ExampleProps
+>;
+/**
+ * Extends Polymorphic
+ * @test
+ */
+export const AdvancedPolymorphic = <T extends PolymorphicAs = 'div'>({
+  as,
+  title,
+  ...rest
+}: AdvancedProps<T>) => {
+  return (
+    <Polymorph as={as as React.ElementType} {...rest}>
+      {title}
+    </Polymorph>
+  );
+};
+AdvancedPolymorphic.displayName = 'AdvancedPolymorphic';
+
+/**
+ * Extends Polymorphic
+ * @test
+ */
+export const AdvancedPolymorphicWithRef = React.forwardRef(
+  <T extends PolymorphicAs = 'div'>(
+    { as, title, ...rest }: AdvancedProps<T>,
+    ref: PolymorphicRef<T>,
+  ) => {
+    return (
+      <Polymorph as={as as React.ElementType} {...rest} ref={ref}>
+        {title}
+      </Polymorph>
+    );
+  },
+);
+AdvancedPolymorphicWithRef.displayName = 'AdvancedPolymorphicWithRef';
+
+/**
+ * Ensure `as` types can be restricted
+ */
+type RestrictedType = 'a' | 'button' | React.ComponentType;
+type RestrictedProps<T extends RestrictedType> = PolymorphicPropsWithRef<
+  T,
+  {
+    title?: 'string';
+  }
+>;
+
+/**
+ *
+ * @test
+ */
+export const RestrictedExample = <T extends RestrictedType = 'button'>({
+  as,
+  ...rest
+}: RestrictedProps<T>) => {
+  return <Polymorph as={as as RestrictedType} {...rest} />;
+};
+
+/**
+ * Styled version of ExampleComponent
+ * @test
+ */
+export const StyledExample = styled(ExamplePolymorphic)`
+  color: hotpink;
+` as PolymorphicComponentType;
