8a36b037472323be9be4ea67aa15c756e3650e33
Adam Thompson <adam.thompson@mongodb.com>
2022-12-15 17:44:27 -0500

    fix

--- packages/polymorphic/src/Polymorphic/Polymorphic.ts
+++ packages/polymorphic/src/Polymorphic/Polymorphic.ts
@@ -7,14 +7,14 @@ import {
 
 /**
  * Factory function that returns a polymorphic component.
- * 
+ *
  * If you want to expose `as` as a prop of your component,
  * use this `Polymorphic` factory function and related hooks.
- * 
+ *
  * However, if the logic defining the `as` prop is defined internally within your component,
  * use the standalone `Polymorph` component.
  *
- * For more, see {@link https://github.com/mongodb/leafygreen-ui/blob/main/packages/polymorphic/README.md | README.md} 
+ * For more, see {@link https://github.com/mongodb/leafygreen-ui/blob/main/packages/polymorphic/README.md | README.md}
  */
 export const Polymorphic = <XP extends object = {}>(
   render: PolymorphicRenderFunction<XP>,
