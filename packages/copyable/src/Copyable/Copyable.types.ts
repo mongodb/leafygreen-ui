d13e66c8ed782dfa6b8927d09b067810bd5bc417
brooke <bsyalof@gmail.com>
2023-01-10 15:23:02 -0500

    migrate copyable

--- packages/copyable/src/Copyable/Copyable.types.ts
+++ packages/copyable/src/Copyable/Copyable.types.ts
@@ -13,11 +13,12 @@ export interface CopyableProps extends HTMLElementProps<'div'> {
    * @default false
    */
   darkMode?: boolean;
-  children: string;
+
   /**
    * Label text
    */
   label?: string;
+
   /**
    * Description text
    */
@@ -27,10 +28,13 @@ export interface CopyableProps extends HTMLElementProps<'div'> {
    * If `true`, there will be a copy button that will move the component's children to the user's clipboard
    */
   copyable?: boolean;
+
   /**
    * The display size of the label, description, and copyable children
    */
+
   size?: Size;
+
   /**
    * If `true`, the tooltip rendered as feedback when the user clicks the copy button will be rendered using a portal
    */
