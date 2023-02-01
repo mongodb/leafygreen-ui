668008d4f4adb0fab6cb09c38451777b3d8342e1
Adam Thompson <adam.thompson@mongodb.com>
2023-02-01 12:24:37 -0500

    Update TsTest.types.ts

--- packages/ts-test/src/TsTest/TsTest.types.ts
+++ packages/ts-test/src/TsTest/TsTest.types.ts
@@ -1,6 +1,8 @@
+import { HTMLElementProps } from '@leafygreen-ui/lib';
+
 type ExcludeFromTypeInference<T> = [T][T extends any ? 0 : never];
 
-export interface TsTestProps<T extends number> {
+export interface TsTestProps<T extends number> extends HTMLElementProps<'div'> {
   value: ExcludeFromTypeInference<T>;
   options: Array<T>;
 }
