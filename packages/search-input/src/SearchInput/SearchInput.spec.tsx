5198880b38a43e432e1e13cf88785f3ef3418f69
Adam Thompson <adam.thompson@mongodb.com>
2023-02-07 12:19:56 -0500

    enables skipped test

--- packages/search-input/src/SearchInput/SearchInput.spec.tsx
+++ packages/search-input/src/SearchInput/SearchInput.spec.tsx
@@ -294,8 +294,7 @@ describe('packages/search-input', () => {
           expect(inputEl).not.toHaveFocus();
         });
 
-        // eslint-disable-next-line jest/no-disabled-tests
-        test.skip('Closes menu when tabbing away', async () => {
+        test('Closes menu when tabbing away', () => {
           const { getMenuElements, inputEl } = renderSearchInput({
             ...defaultProps,
           });
@@ -304,8 +303,7 @@ describe('packages/search-input', () => {
           expect(menuContainerEl).toBeInTheDocument();
           userEvent.tab();
           expect(inputEl).not.toHaveFocus();
-          await waitForElementToBeRemoved(menuContainerEl);
-          expect(menuContainerEl).not.toBeInTheDocument();
+          waitForElementToBeRemoved(menuContainerEl);
         });
       });
 
