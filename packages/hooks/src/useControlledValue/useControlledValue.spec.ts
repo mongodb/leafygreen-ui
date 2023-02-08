8620ee99a97a4a9eeb0151fb449bd1777289ad22
Adam Thompson <adam.thompson@mongodb.com>
2023-02-08 18:09:17 -0500

    Create useControlledValue.spec.ts

---
+++ packages/hooks/src/useControlledValue/useControlledValue.spec.ts
@@ -0,0 +1,45 @@
+import { ChangeEvent } from 'react';
+// import TestRenderer from 'react-test-renderer';
+import { act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+
+// const { act } = TestRenderer;
+import { useControlledValue } from './useControlledValue';
+
+describe('packages/lib/useControlledValue', () => {
+  test('with controlled component', async () => {
+    const value = 'apple';
+    const handler = jest.fn();
+    const {
+      result: { current },
+    } = renderHook(() => useControlledValue(value as string, handler));
+    expect(current.isControlled).toBe(true);
+    expect(current.value).toBe(value);
+
+    act(() => {
+      current.handleChange({ target: { value: 'banana' } } as ChangeEvent<any>);
+      current.setInternalValue('banana');
+    });
+    expect(handler).toHaveBeenCalledWith(
+      expect.objectContaining({ target: { value: 'banana' } }),
+    );
+    expect(current.value).toBe('apple');
+  });
+
+  test('with uncontrolled component', async () => {
+    const value = undefined;
+    const handler = jest.fn();
+    const {
+      result: { current },
+    } = renderHook(() => useControlledValue(value, handler));
+    expect(current.isControlled).toBe(false);
+    expect(current.value).toBe('');
+
+    act(() => {
+      current.handleChange({ target: { value: 'apple' } } as ChangeEvent<any>);
+      expect(handler).toHaveBeenCalledWith(
+        expect.objectContaining({ target: { value: 'apple' } }),
+      );
+    });
+  });
+});
