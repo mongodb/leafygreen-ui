b37e3ca0b8a6f62075e3267ec8e10690740b0511
Adam Thompson <adam.thompson@mongodb.com>
2023-02-08 14:27:46 -0500

    useBackdropClick

--- packages/combobox/src/Combobox/Combobox.tsx
+++ packages/combobox/src/Combobox/Combobox.tsx
@@ -15,8 +15,8 @@ import PropTypes from 'prop-types';
 
 import { cx } from '@leafygreen-ui/emotion';
 import {
+  useBackdropClick,
   useDynamicRefs,
-  useEventListener,
   useIdAllocator,
   usePrevious,
 } from '@leafygreen-ui/hooks';
@@ -1138,44 +1138,7 @@ export function Combobox<M extends boolean>({
    *
    */
 
-  // TODO: Replace this with `useBackdropClick`
-  /**
-   * We add two event handlers to the document to handle the backdrop click behavior.
-   * Intended behavior is to close the menu, and keep focus on the Combobox.
-   * No other click event handlers should fire on backdrop click
-   *
-   * 1. Mousedown event fires
-   * 2. We prevent `mousedown`'s default behavior, to prevent focus from being applied to the body (or other target)
-   * 3. Click event fires
-   * 4. We handle this event on _capture_, and stop propagation before the `click` event propagates all the way to any other element.
-   *  This ensures that even if we click on a button, that handler is not fired
-   * 5. Then we call `closeMenu`, setting `isOpen = false`, and rerender the component
-   */
-  useEventListener(
-    'mousedown',
-    (mousedown: MouseEvent) => {
-      if (!doesComponentContainEventTarget(mousedown)) {
-        mousedown.preventDefault(); // Prevent focus from being applied to body
-        mousedown.stopPropagation(); // Stop any other mousedown events from firing
-      }
-    },
-    {
-      enabled: isOpen,
-    },
-  );
-  useEventListener(
-    'click',
-    (click: MouseEvent) => {
-      if (!doesComponentContainEventTarget(click)) {
-        click.stopPropagation(); // Stop any other click events from firing
-        closeMenu();
-      }
-    },
-    {
-      options: { capture: true },
-      enabled: isOpen,
-    },
-  );
+  useBackdropClick(closeMenu, [menuRef, comboboxRef], isOpen);
 
   const popoverProps = {
     popoverZIndex,
@@ -1316,17 +1279,6 @@ export function Combobox<M extends boolean>({
   );
 
   // Closure-dependant utils
-
-  /**
-   * Returns whether the event target is a Combobox element
-   */
-  function doesComponentContainEventTarget({ target }: MouseEvent): boolean {
-    return (
-      menuRef.current?.contains(target as Node) ||
-      comboboxRef.current?.contains(target as Node) ||
-      false
-    );
-  }
 
   /**
    * Scrolls the combobox to the far right.
