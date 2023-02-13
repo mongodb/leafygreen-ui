f0f742f782ca9dab0be5a5f40fb85417194cefaa
Adam Thompson <adam.thompson@mongodb.com>
2023-02-13 14:12:33 -0500

    adds storybookOnly key

--- packages/lib/src/storybook/StoryMeta.ts
+++ packages/lib/src/storybook/StoryMeta.ts
@@ -1,6 +1,7 @@
+import { InputType } from '@storybook/csf';
 import { ComponentMeta } from '@storybook/react';
 import { defaultsDeep } from 'lodash';
-import { JSXElementConstructor } from 'react';
+import { ComponentProps, JSXElementConstructor } from 'react';
 import { storybookArgTypes } from './storybookArgTypes';
 import { storybookExcludedControlParams } from './storybookExcludedControlParams';
 
@@ -9,10 +10,19 @@ export interface StoryMeta<
 > extends ComponentMeta<T> {
   parameters: ComponentMeta<T>['parameters'] & {
     /**
-     * The default story to be displayed on mongodb.design
+     * The default story to be displayed on `mongodb.design`
      */
     default: string;
   };
+  argTypes?: Partial<{
+    [name in keyof ComponentProps<T>]: InputType & {
+      /**
+       * Identify an arg to render a control on Storybook only,
+       * and not on `mongodb.design`
+       */
+      storybookOnly?: boolean;
+    };
+  }>;
 }
 
 const baseMeta = {
