eaae1862e014a86361d3d2d41096d49307804ff2
Shaneeza <shaneeza.ali@mongodb.com>
2023-02-28 15:26:04 -0500

    remove website

--- website/components/LayoutContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const LayoutContext = createContext<HTMLDivElement | null>(null);
-
-export function useBodyContainerRef() {
-  const element = useContext(LayoutContext);
-
-  return element;
-}
