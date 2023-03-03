45982d8f1985c42a90f7dfada51e4b6f9520b5c7
Adam Thompson <adam.thompson@mongodb.com>
2023-03-03 14:04:40 -0500

    Update ToastContext.story.tsx

--- packages/toast/src/ToastContext/ToastContext.story.tsx
+++ packages/toast/src/ToastContext/ToastContext.story.tsx
@@ -6,6 +6,7 @@ import Button from '@leafygreen-ui/button';
 import { css } from '@leafygreen-ui/emotion';
 import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
 import { DarkModeProps, StoryMeta } from '@leafygreen-ui/lib';
+import { Body, Link } from '@leafygreen-ui/typography';
 
 import { InternalToast, InternalToastProps } from '../InternalToast';
 import { Variant } from '../Toast.types';
@@ -38,33 +39,46 @@ export default StoryMeta<typeof InternalToast>({
         'Dismiss the Toast after `timeout` milliseconds. If timeout is `null | 0`, the Toast will never dismiss automatically.',
     },
   },
+  args: {
+    /// @ts-expect-error
+    darkMode: false,
+  },
 });
 
 const BasicChildren = (props: Partial<InternalToastProps>) => {
   const { pushToast, clearStack } = useToast();
 
   return (
-    <div
-      className={css`
-        display: flex;
-        gap: 8px;
-      `}
-    >
-      <Button
-        data-testid="toast-trigger"
-        onClick={() => {
-          const variant = sample(Variant);
-          pushToast({
-            title: `I'm a ${variant} toast`,
-            description: faker.lorem.lines(random(1, 2)),
-            variant,
-            ...props,
-          });
-        }}
+    <div>
+      <div
+        className={css`
+          display: flex;
+          gap: 8px;
+        `}
       >
-        Push toast
-      </Button>
-      <Button onClick={() => clearStack()}>Clear all</Button>
+        <Button
+          data-testid="toast-trigger"
+          onClick={() => {
+            const variant = sample(Variant);
+            pushToast({
+              title: `I'm a ${variant} toast`,
+              description: faker.lorem.lines(random(1, 2)),
+              variant,
+              ...props,
+            });
+          }}
+        >
+          Push toast
+        </Button>
+        <Button onClick={() => clearStack()}>Clear all</Button>
+      </div>
+      {/* TODO: Remove this */}
+      <Body>
+        Please provide any feedback in{' '}
+        <Link href="https://docs.google.com/document/d/1xgzfJhkWugpEeBlbomTpkrlbjedNxdwn7A0vg3jvDXo/edit#">
+          this doc
+        </Link>
+      </Body>
     </div>
   );
 };
