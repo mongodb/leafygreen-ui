78e79a0f303922cfcabc281f6f48ceb4a956d2f0
brooke <bsyalof@gmail.com>
2023-03-15 12:19:19 -0400

    Fix ts erros

--- packages/typography/src/InlineCode/InlineCode.spec.tsx
+++ packages/typography/src/InlineCode/InlineCode.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import InlineCode from './InlineCode';
 
@@ -11,19 +11,19 @@ describe('packages/typography', () => {
       test('it renders as code by default', () => {
         const utils = render(<InlineCode>{children}</InlineCode>);
         const component = utils.container.firstChild;
-        expect(component.tagName.toLowerCase()).toBe('code');
+        expect((component as HTMLElement).tagName.toLowerCase()).toBe('code');
       });
 
       test('it renders as an anchor component when the href prop is supplied', () => {
         const utils = render(<InlineCode href="string">{children}</InlineCode>);
         const component = utils.container.firstChild;
-        expect(component.tagName.toLowerCase()).toBe('a');
+        expect((component as HTMLElement).tagName.toLowerCase()).toBe('a');
       });
 
       test('it renders as component passed to "as" prop when set', () => {
         const utils = render(<InlineCode as="div">{children}</InlineCode>);
         const component = utils.container.firstChild;
-        expect(component.tagName.toLowerCase()).toBe('div');
+        expect((component as HTMLElement).tagName.toLowerCase()).toBe('div');
       });
     });
   });
