da2d1cca8c0443edda1f58c59a2361dc01cc79ba
Sean Park <seanpark1107@gmail.com>
2023-03-21 16:43:30 -0400

    Sean/table v10 release candidate (#1662)
    * Expandable content wrapper
    
    * nested row support in wrapper
    
    * custom children testing
    
    * wrapper working with sort functions
    
    * Update V11Adapter.tsx
    
    ---------
    
    Co-authored-by: Adam Thompson <adam.thompson@mongodb.com>

--- packages/table/src/V11Adapter/processData.tsx
+++ packages/table/src/V11Adapter/processData.tsx
@@ -15,36 +15,54 @@ const processData = (
     const evaluatedChildren = childrenFn({ datum: oldDatum, index });
     const childrenArray = React.Children.toArray(evaluatedChildren);
     const evaluatedRow = childrenArray[0] as ReactElement;
-    const rowChildren = React.Children.toArray(
-      evaluatedRow.props.children,
-    )
-    const evaluatedCells = rowChildren.filter(child => isComponentType(child, 'Cell'));
-    const newDatum: LGTableDataType<any> = evaluatedCells.reduce((acc, currVal, index) => {
-      return {
-        ...acc,
-        [processedColumns[index].accessorKey]: () =>
-          (currVal as ReactElement).props.children,
-      };
-    }, {});
+    const rowChildren = React.Children.toArray(evaluatedRow.props.children);
+    const evaluatedCells = rowChildren.filter(child =>
+      isComponentType(child, 'Cell'),
+    );
+    const newDatum: LGTableDataType<any> = evaluatedCells.reduce(
+      (acc: object, currVal, index) => {
+        return {
+          ...acc,
+          [processedColumns[index].accessorKey]: () =>
+            (currVal as ReactElement).props.children,
+        };
+      },
+      {},
+    );
 
-    const subRowChildren = rowChildren.filter(child => isComponentType(child, 'Row'));
+    const subRowChildren = rowChildren.filter(child =>
+      isComponentType(child, 'Row'),
+    );
     if (subRowChildren.length > 0) newDatum.subRows = [];
-    subRowChildren.forEach((subRow) => {
-      const subRowCells = React.Children.toArray((subRow as ReactElement).props.children);
+    subRowChildren.forEach(subRow => {
+      const subRowCells = React.Children.toArray(
+        (subRow as ReactElement).props.children,
+      );
       const firstSubRowCell = subRowCells[0];
-      const firstSubRowCellColSpan = (firstSubRowCell as ReactElement).props.colSpan;
-      if (firstSubRowCellColSpan && firstSubRowCellColSpan === processedColumns.length) {
-        newDatum.renderExpandedContent = () => <div style={{ padding: '10px 8px 10px 32px' }}>{(firstSubRowCell as ReactElement).props.children}</div>
+      const firstSubRowCellColSpan = (firstSubRowCell as ReactElement).props
+        .colSpan;
+
+      if (
+        firstSubRowCellColSpan &&
+        firstSubRowCellColSpan === processedColumns.length
+      ) {
+        // eslint-disable-next-line react/display-name
+        newDatum.renderExpandedContent = () => (
+          <div style={{ padding: '10px 8px 10px 32px' }}>
+            {(firstSubRowCell as ReactElement).props.children}
+          </div>
+        );
       } else {
-        const processedSubRow = subRowCells.reduce((acc, currVal, index) => {
+        const processedSubRow = subRowCells.reduce((acc: object, currVal, index) => {
           return {
             ...acc,
-            [processedColumns[index].accessorKey]: () => (currVal as ReactElement).props.children,
+            [processedColumns[index].accessorKey]: () =>
+              (currVal as ReactElement).props.children,
           };
         }, {});
         newDatum.subRows.push(processedSubRow);
       }
-    })
+    });
 
     return newDatum;
   });
