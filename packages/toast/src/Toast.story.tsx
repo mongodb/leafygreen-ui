e616b04559adb1843c40819f191104ea86fc77fc
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2023-04-05 11:14:27 -0400

    LG-2774 Multi-toast (#1635)
    * Updates Toast API
    
    * mv progress bar
    
    * mv fixes
    
    * Creates toast context
    
    * basic toast rendering and timeout
    
    * wraps all toasts in portal
    
    * moves transition to provider
    
    * adds hover transitions in toast container
    
    * adds notification bar
    
    * fix
    
    * Rename Toast => Internal toast
    
    * creates Toast component
    
    * Better timeout handling
    
    * Adds controlled toast component
    
    * expectElementToNotBeRemoved
    
    * Adds controlled toast tests
    
    * Updates default props
    
    * does not autofire pop
    
    * Fixes internal toast tests
    
    * fixe3s animation styles and default overrides
    
    * reorg context
    
    * stubs tests
    
    * rename ControlledToast
    
    * cleanup tests
    
    * Update ToastReducer.spec.ts
    
    * mv useToastReducer
    
    * rm other package changes
    
    * useToast & context tests
    
    * more context tests
    
    * moves container styles
    
    * adds clearStack function
    
    * Handles variable heights better!
    
    * rm unused
    
    * cleanup stories
    
    * darkmode story fix
    
    * mv ToastContainer
    
    * Update InternalToast.tsx
    
    * Update ToastContext.story.tsx
    
    * ensure bottom toast is 16 from edge
    
    * fixes container hover issues
    
    * move stateful css to .styles
    
    * cleanup
    
    * fix variant icon colors
    
    * Adds differentiating close events
    
    * adds hover tests
    
    * Don't set timer until progress is complete
    
    * Comments
    
    * Expand & scroll
    
    * rearch constants
    
    * Fixes transition callback stale state
    
    * Create silly-pets-jog.md
    
    * smoother expand collapse transition
    
    * cleanup styles
    
    * rm debug data
    
    * remove scrollbars
    
    * fix ts
    
    * restructures constants
    
    * Moves ToastContainer. Improves Story
    
    * Cleanup file structure
    
    * Moves story & spec files
    
    * changes updateToast signature
    
    * WIP update tests
    
    * adds initial value prop to provider
    
    * Create little-cheetahs-try.md
    
    * adds initial value tests
    
    * updates exports
    
    * toast reducer test covg
    
    * resolves backdrop click issue
    
    * add TODO
    
    * Update ToastContainer.spec.tsx
    
    * address SP comments
    
    * SA comments
    
    * Resolves scroll container height animation glitch
    
    * smoother transition out
    
    * comments
    
    * minor todo fixes
    
    * adds warning
    
    * Enables actionElement
    
    * Update ToastContainer.spec.tsx
    
    * ignore tests on ssr
    
    * mobile width support
    
    * wrap more things in `act`
    
    * fix ts
    
    * Resolves backdrop click jank
    
    * Fixes scroll container animation jitter
    
    * fix
    
    * fix deconstruction error
    
    * fixes test
    
    * SP comments

---
+++ packages/toast/src/Toast.story.tsx
@@ -0,0 +1,210 @@
+import React, { useEffect, useState } from 'react';
+import { faker } from '@faker-js/faker';
+import { ComponentStory } from '@storybook/react';
+import { random, range, sample, startCase } from 'lodash';
+
+import Button from '@leafygreen-ui/button';
+import { css } from '@leafygreen-ui/emotion';
+import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
+import { DarkModeProps, StoryMeta } from '@leafygreen-ui/lib';
+import { InlineCode, Label } from '@leafygreen-ui/typography';
+
+import { variantIcons } from './InternalToast/VariantIcon';
+import { makeToast, makeToastStack } from './ToastContext/utils/makeToast';
+import { InternalToast, InternalToastProps } from './InternalToast';
+import { ToastProvider, useToast, Variant } from '.';
+
+export default StoryMeta<typeof InternalToast>({
+  title: 'Components/Toast',
+  component: InternalToast,
+  decorators: [
+    (Story, meta) => (
+      <LeafyGreenProvider darkMode={!!meta.args.darkMode}>
+        <ToastProvider initialValue={meta.args.initialValue}>
+          <Story />
+        </ToastProvider>
+      </LeafyGreenProvider>
+    ),
+  ],
+  parameters: {
+    default: 'Basic',
+    controls: {
+      exclude: [
+        'as',
+        'title',
+        'description',
+        'dismissible',
+        'onClose',
+        'progress',
+        'variant',
+      ],
+    },
+  },
+  argTypes: {
+    timeout: {
+      control: 'number',
+      defaultValue: 6_000,
+      description:
+        'Dismiss the Toast after `timeout` milliseconds. If timeout is `null | 0`, the Toast will never dismiss automatically.',
+    },
+  },
+  args: {
+    /// @ts-expect-error
+    darkMode: false,
+    initialValue: undefined,
+  },
+});
+export const Basic: ComponentStory<typeof InternalToast> = (
+  props: Partial<InternalToastProps> & DarkModeProps,
+) => {
+  const { pushToast, clearStack } = useToast();
+
+  return (
+    <div>
+      <div
+        className={css`
+          display: flex;
+          gap: 8px;
+        `}
+      >
+        <Button
+          data-testid="toast-trigger"
+          onClick={() => {
+            const variant = props.variant || sample(Variant);
+            pushToast({
+              title: `I'm a ${variant} toast`,
+              description: faker.lorem.lines(random(1, 2)),
+              variant,
+              ...props,
+            });
+          }}
+        >
+          Push toast
+        </Button>
+        <Button onClick={() => clearStack()}>Clear all</Button>
+      </div>
+    </div>
+  );
+};
+
+export const Variants: ComponentStory<typeof InternalToast> = (
+  props: Partial<InternalToastProps>,
+) => {
+  const { pushToast, clearStack, getStack, updateToast } = useToast();
+
+  const stack = getStack();
+  const progressToasts =
+    stack && Array.from(stack).filter(([_, t]) => t.variant === 'progress');
+
+  const [progress, setProgress] = useState(0.1);
+
+  useEffect(() => {
+    if (!progressToasts || progressToasts.length === 0) {
+      setProgress(0.1);
+    }
+  }, [progressToasts]);
+
+  return (
+    <div>
+      <div
+        className={css`
+          display: grid;
+          grid-template-columns: repeat(3, 1fr);
+          gap: 8px;
+          margin: 1em 0;
+        `}
+      >
+        {Object.values(Variant).map(variant => {
+          const VariantIcon = variantIcons[variant];
+
+          return (
+            <Button
+              key={variant}
+              onClick={() => {
+                pushToast({
+                  title: `I'm a ${variant} toast`,
+                  description: faker.lorem.lines(random(1, 2)),
+                  variant,
+                  progress,
+                  ...props,
+                });
+              }}
+            >
+              <VariantIcon /> {startCase(variant)} toast
+            </Button>
+          );
+        })}
+        <Button onClick={() => clearStack()}>Clear all</Button>
+      </div>
+      {progressToasts && progressToasts.length > 0 && (
+        <>
+          <Label htmlFor="progress">Progress: </Label>
+          <InlineCode>{progress}</InlineCode>
+          <br />
+          <input
+            value={progress}
+            onChange={e => {
+              const _progress = Number(e.target.value);
+              setProgress(_progress);
+
+              progressToasts.forEach(([id]) => {
+                updateToast(id, {
+                  progress: _progress,
+                });
+              });
+            }}
+            type="range"
+            id="progress"
+            min={0}
+            max={1}
+            step={0.1}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export const WithInitialToasts: ComponentStory<typeof InternalToast> = (
+  props: Partial<InternalToastProps>,
+) => {
+  const { pushToast, clearStack } = useToast();
+
+  return (
+    <div
+      className={css`
+        display: flex;
+        gap: 8px;
+      `}
+    >
+      <Button
+        data-testid="toast-trigger"
+        onClick={() => {
+          const variant = props.variant || sample(Variant);
+          pushToast({
+            title: `I'm a ${variant} toast`,
+            description: faker.lorem.lines(random(1, 2)),
+            variant,
+            ...props,
+          });
+        }}
+      >
+        Push toast
+      </Button>
+      <Button onClick={() => clearStack()}>Clear all</Button>
+    </div>
+  );
+};
+
+WithInitialToasts.args = {
+  // @ts-expect-error
+  initialValue: makeToastStack(
+    range(6).map(_ =>
+      makeToast({
+        title: 'Initial toast',
+        description: faker.lorem.lines(2),
+        variant: sample(Variant),
+      }),
+    ),
+  ),
+};
---
+++ packages/toast/src/ToastContext/ToastReducer/useToastReducer.ts
@@ -0,0 +1,128 @@
+import { Reducer, useReducer } from 'react';
+
+import { defaultToastProps } from '../../InternalToast/defaultProps';
+import { ToastProps } from '../../Toast.types';
+import {
+  ToastContextProps,
+  ToastId,
+  ToastReducerAction,
+  ToastReducerActionType,
+  ToastReducerState,
+  ToastStack,
+} from '../ToastContext.types';
+import { makeToast } from '../utils/makeToast';
+
+/**
+ *
+ * The reducer function
+ *
+ * @param state The current reducer state
+ * @param action The action type and payload
+ */
+const toastReducer = (
+  state: ToastReducerState,
+  action: ToastReducerAction,
+): ToastReducerState => {
+  switch (action.type) {
+    case ToastReducerActionType.Push: {
+      const { stack } = state;
+      const { id, ...toast } = action.payload;
+      return {
+        stack: stack.set(id, { ...defaultToastProps, ...toast }),
+      };
+    }
+
+    case ToastReducerActionType.Pop: {
+      const { stack } = state;
+      const id = action.payload;
+      const poppedToast = stack.get(id);
+
+      if (poppedToast) {
+        stack.delete(id);
+      }
+
+      return { stack };
+    }
+
+    case ToastReducerActionType.Update: {
+      const { stack } = state;
+      const { id, props } = action.payload;
+      const updatedToast = stack.get(id);
+
+      if (updatedToast) {
+        stack.set(id, { ...updatedToast, ...props });
+      }
+
+      return { stack };
+    }
+
+    case ToastReducerActionType.Clear: {
+      const { stack } = state;
+      stack.clear();
+      return { stack };
+    }
+  }
+};
+
+/**
+ * An abstraction of `useReducer` for the toast context
+ */
+export const useToastReducer = (initialValue?: ToastStack) => {
+  const [{ stack }, dispatch] = useReducer<
+    Reducer<ToastReducerState, ToastReducerAction>
+  >(toastReducer, {
+    stack: initialValue ?? (new Map<ToastId, ToastProps>() as ToastStack),
+  });
+
+  const pushToast: ToastContextProps['pushToast'] = (props: ToastProps) => {
+    const toast = makeToast(props);
+
+    dispatch({
+      type: ToastReducerActionType.Push,
+      payload: toast,
+    });
+
+    return toast.id;
+  };
+
+  const getToast: ToastContextProps['getToast'] = (id: ToastId) =>
+    stack.get(id);
+
+  const popToast: ToastContextProps['popToast'] = (payload: ToastId) => {
+    dispatch({
+      type: ToastReducerActionType.Pop,
+      payload,
+    });
+
+    return getToast(payload);
+  };
+
+  const updateToast: ToastContextProps['updateToast'] = (
+    id: ToastId,
+    props: Partial<ToastProps>,
+  ) => {
+    dispatch({
+      type: ToastReducerActionType.Update,
+      payload: {
+        id,
+        props,
+      },
+    });
+    return getToast(id);
+  };
+
+  const clearStack: ToastContextProps['clearStack'] = () => {
+    dispatch({
+      type: ToastReducerActionType.Clear,
+    });
+  };
+
+  return {
+    stack,
+    pushToast,
+    popToast,
+    updateToast,
+    getToast,
+    clearStack,
+  };
+};
