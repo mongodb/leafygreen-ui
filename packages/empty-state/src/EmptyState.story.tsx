08c9fe3e90cf38f6642f175c7f14b70c6c41bcba
spark33 <seanpark1107@gmail.com>
2023-04-06 11:12:38 -0400

    live example variant knob

--- packages/empty-state/src/EmptyState.story.tsx
+++ packages/empty-state/src/EmptyState.story.tsx
@@ -1,10 +1,39 @@
-import { storybookArgTypes } from '@leafygreen-ui/lib';
+import React from 'react';
+import { ComponentStory } from '@storybook/react';
 
-import { ThreeFeaturesWithActionsAndLink } from './FeaturesEmptyState/FeaturesEmptyState.story';
+import { DarkModeProps, storybookArgTypes } from '@leafygreen-ui/lib';
+import { spacing } from '@leafygreen-ui/tokens';
+
+import BasicEmptyStateStory, {
+  WithActionsAndLink,
+} from './BasicEmptyState/BasicEmptyState.story';
+import FeaturesEmptyStateStory, {
+  ThreeFeaturesWithActionsAndLink,
+  TwoFeaturesWithActionsAndLink,
+} from './FeaturesEmptyState/FeaturesEmptyState.story';
+
+const StoryVariant = {
+  Basic: 'Basic',
+  TwoFeatures: 'Two Features',
+  ThreeFeatures: 'Three Features',
+} as const;
+
+type StoryVariant = typeof StoryVariant[keyof typeof StoryVariant];
+
+interface EmptyStateStoryProps extends DarkModeProps {
+  variant: StoryVariant;
+}
 
 export default {
   title: 'Components/EmptyState',
+  args: {
+    variant: StoryVariant.Basic,
+  },
   argTypes: {
+    variant: {
+      control: 'radio',
+      options: Object.values(StoryVariant),
+    },
     graphic: { control: 'none' },
     externalLink: { control: 'none' },
     primaryButton: { control: 'none' },
@@ -19,8 +48,40 @@ export default {
   },
 };
 
-export const LiveExample = ThreeFeaturesWithActionsAndLink;
-LiveExample.args = {
-  ...ThreeFeaturesWithActionsAndLink.args,
-  title: 'Sample title of features empty state',
+// @ts-expect-error Props for the LiveExample story do not correspond to documented component's props
+export const LiveExample: ComponentStory<EmptyStateStoryProps> = ({
+  // @ts-expect-error Props for the LiveExample story do not correspond to documented component's props
+  // eslint-disable-next-line react/prop-types
+  variant,
+  ...rest
+}) => {
+  return (
+    <>
+      <div style={{ marginBottom: spacing[4] }}>
+        * Note: Controls on LiveExample are designed specifically for the Live
+        Example and may not correspond to the React component&apos;s properties.
+      </div>
+      {variant === StoryVariant.Basic && (
+        <WithActionsAndLink
+          {...BasicEmptyStateStory.args}
+          {...WithActionsAndLink.args}
+          {...rest}
+        />
+      )}
+      {variant === StoryVariant.TwoFeatures && (
+        <TwoFeaturesWithActionsAndLink
+          {...FeaturesEmptyStateStory.args}
+          {...TwoFeaturesWithActionsAndLink.args}
+          {...rest}
+        />
+      )}
+      {variant === StoryVariant.ThreeFeatures && (
+        <ThreeFeaturesWithActionsAndLink
+          {...FeaturesEmptyStateStory.args}
+          {...ThreeFeaturesWithActionsAndLink.args}
+          {...rest}
+        />
+      )}
+    </>
+  );
 };
