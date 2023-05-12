1d4901c68f21983cd4e595a1cf0dba1f318d60ba
brooke <bsyalof@gmail.com>
2023-05-12 15:21:13 -0400

    with changeset

--- packages/hooks/src/useIdAllocator.ts
+++ packages/hooks/src/useIdAllocator.ts
@@ -1,6 +1,6 @@
 // Currently using Material UI useId hook until we can upgrade to React 18's useId
 // https://github.com/mui/material-ui/blob/master/packages/mui-utils/src/useId.ts
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface Params {
   prefix?: string;
@@ -28,17 +28,7 @@ function useGlobalId({ id: idOverride, prefix }: Params): string {
   return idOverride ? idOverride : `${prefix ?? 'lg'}-${defaultId}`;
 }
 
-// eslint-disable-next-line no-useless-concat -- Workaround for https://github.com/webpack/webpack/issues/14814
-const maybeReactUseId: undefined | (() => string) = (React as any)[
-  'useId' + ''
-];
-
 export default function useId({ prefix, id: idOverride }: Params): string {
-  if (maybeReactUseId !== undefined) {
-    const reactId = maybeReactUseId();
-    return idOverride ?? reactId;
-  }
-
   // eslint-disable-next-line react-hooks/rules-of-hooks -- `React.useId` is invariant at runtime.
   return useGlobalId({ id: idOverride, prefix });
 }
