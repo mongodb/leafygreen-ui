239fe112b65e7f818f4e3de5469a0c6914dea1fe
Adam Thompson <adam.thompson@mongodb.com>
2023-05-12 16:51:46 -0400

    revert main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,12 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
-
 const nodeModulesThatNeedToBeParsedBecauseTheyExposeES6 = [
   '@tanstack[\\\\/]react-table',
   '@tanstack[\\\\/]table-core',
 ];
 
-const config: StorybookConfig = {
+const config = {
   stories: [
-    '../packages/**/Combobox.story.tsx',
+    // FIXME: temporarily disabling other stories
+    '../packages/**/Button.story.tsx',
     // '../**/*.story.@(mdx|js|jsx|ts|tsx)',
     // '../**/*.stories.@(mdx|js|jsx|ts|tsx)',
   ],
@@ -15,9 +14,9 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    // '@storybook/addon-docs',
+    '@storybook/addon-docs',
     '@storybook/addon-a11y',
-    // '@storybook/addon-mdx-gfm',
+    '@storybook/addon-mdx-gfm',
   ],
   framework: {
     name: '@storybook/react-webpack5',
@@ -39,31 +38,31 @@ const config: StorybookConfig = {
 
   webpackFinal: config => {
     // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
-    // const fileLoaderRule = config.module.rules.find(
-    //   rule => rule.test && rule.test.test('.svg'),
-    // );
-    // fileLoaderRule.exclude = /\.svg$/;
+    const fileLoaderRule = config?.module?.rules.find(
+      rule => rule?.test && rule?.test.test('.svg'),
+    );
+    fileLoaderRule.exclude = /\.svg$/;
 
-    // config.module.rules.map(rule => {
-    //   if (rule.test?.toString() !== String(/\.js$/)) return rule;
-    //   const include = new RegExp(
-    //     `[\\\\/]node_modules[\\\\/](${nodeModulesThatNeedToBeParsedBecauseTheyExposeES6.join(
-    //       '|',
-    //     )})`,
-    //   );
-    //   if (Array.isArray(rule.include)) {
-    //     rule.include.push(include);
-    //   } else {
-    //     rule.include = [rule.include, include].filter(Boolean);
-    //   }
-    //   return rule;
-    // });
+    config.module.rules.map(rule => {
+      if (rule.test?.toString() !== String(/\.js$/)) return rule;
+      const include = new RegExp(
+        `[\\\\/]node_modules[\\\\/](${nodeModulesThatNeedToBeParsedBecauseTheyExposeES6.join(
+          '|',
+        )})`,
+      );
+      if (Array.isArray(rule.include)) {
+        rule.include.push(include);
+      } else {
+        rule.include = [rule.include, include].filter(Boolean);
+      }
+      return rule;
+    });
 
-    // config.module.rules.push({
-    //   test: /\.svg$/,
-    //   enforce: 'pre',
-    //   loader: require.resolve('@svgr/webpack'),
-    // });
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    });
     return config;
   },
   typescript: {
