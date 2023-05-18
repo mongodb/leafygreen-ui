ac467334754e3a014f621df839662a1ce624738d
Adam Thompson <adam.thompson@mongodb.com>
2023-05-18 14:27:04 -0400

    Updates Empty State stories

--- packages/empty-state/src/EmptyState.story.tsx
+++ packages/empty-state/src/EmptyState.story.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { ComponentStory } from '@storybook/react';
+import { StoryFn } from '@storybook/react';
 
 import { css } from '@leafygreen-ui/emotion';
-import { storybookArgTypes, StoryMeta } from '@leafygreen-ui/lib';
+import { storybookArgTypes, StoryMetaType } from '@leafygreen-ui/lib';
 import { spacing } from '@leafygreen-ui/tokens';
 
 import BasicEmptyStateStoryMeta, {
@@ -13,6 +13,12 @@ import FeaturesEmptyStateStoryMeta, {
   ThreeFeaturesWithSecondaryActionAndLink,
   TwoFeaturesWithSecondaryActionAndLink,
 } from './FeaturesEmptyState/FeaturesEmptyState.story';
+import {
+  BasicEmptyState,
+  type BasicEmptyStateProps,
+  FeaturesEmptyState,
+  type FeaturesEmptyStateProps,
+} from '.';
 
 const StoryVariant = {
   Basic: 'Basic',
@@ -21,27 +27,27 @@ const StoryVariant = {
   ThreeFeatures: 'Three Features',
 } as const;
 
-type StoryVariant = typeof StoryVariant[keyof typeof StoryVariant];
+type StoryVariant = (typeof StoryVariant)[keyof typeof StoryVariant];
 
-export default StoryMeta({
-  title: 'Components/EmptyState',
-  args: {
-    variant: StoryVariant.Basic,
-  },
-  argTypes: {
-    variant: {
-      control: 'radio',
-      options: Object.values(StoryVariant),
+const meta: StoryMetaType<typeof BasicEmptyState | typeof FeaturesEmptyState> =
+  {
+    title: 'Components/EmptyState',
+    args: {
+      variant: StoryVariant.Basic,
     },
-    darkMode: storybookArgTypes.darkMode,
-  },
-  parameters: {
-    default: 'LiveExample',
-    controls: {
-      exclude: ['className', 'as', 'baseFontSize'],
+    argTypes: {
+      variant: {
+        control: 'radio',
+        options: Object.values(StoryVariant),
+      },
+      darkMode: storybookArgTypes.darkMode,
     },
-  },
-});
+    parameters: {
+      default: 'LiveExample',
+    },
+  };
+
+export default meta;
 
 export const LiveExample = ({
   // @ts-expect-error Props for the LiveExample story do not correspond to documented component's props
@@ -49,33 +55,35 @@ export const LiveExample = ({
   variant,
   ...rest
 }) => {
-  let StoryComponent: ComponentStory<any> = WithActionsAndLink;
-  let storyComponentProps: unknown = {};
+  let StoryComponent: StoryFn<BasicEmptyStateProps | FeaturesEmptyStateProps> =
+    WithActionsAndLink;
+  let storyComponentProps: BasicEmptyStateProps | FeaturesEmptyStateProps =
+    {} as BasicEmptyStateProps | FeaturesEmptyStateProps;
 
   if (variant === StoryVariant.Basic) {
     StoryComponent = WithActionsAndLink;
     storyComponentProps = {
       ...BasicEmptyStateStoryMeta.args,
       ...WithActionsAndLink.args,
-    };
+    } as BasicEmptyStateProps;
   } else if (variant === StoryVariant.BasicWithSmallAsset) {
     StoryComponent = WithSmallGraphic;
     storyComponentProps = {
       ...BasicEmptyStateStoryMeta.args,
       ...WithSmallGraphic.args,
-    };
+    } as BasicEmptyStateProps;
   } else if (variant === StoryVariant.TwoFeatures) {
     StoryComponent = TwoFeaturesWithSecondaryActionAndLink;
     storyComponentProps = {
       ...FeaturesEmptyStateStoryMeta.args,
       ...TwoFeaturesWithSecondaryActionAndLink.args,
-    };
+    } as FeaturesEmptyStateProps;
   } else if (variant === StoryVariant.ThreeFeatures) {
     StoryComponent = ThreeFeaturesWithSecondaryActionAndLink;
     storyComponentProps = {
       ...FeaturesEmptyStateStoryMeta.args,
       ...ThreeFeaturesWithSecondaryActionAndLink.args,
-    };
+    } as FeaturesEmptyStateProps;
   }
 
   return (
@@ -86,6 +94,7 @@ export const LiveExample = ({
         align-items: center;
       `}
     >
+      {/* @ts-expect-error - props are not specific enough */}
       <StoryComponent {...storyComponentProps} {...rest} />
       <div style={{ marginTop: spacing[4] }}>
         * Note: Controls below are designed specifically for the Live Example
