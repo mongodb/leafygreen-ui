6ac1f43fa9d3f77a3bc50fc69c226788eb748b27
Adam Thompson <adam.thompson@mongodb.com>
2023-05-18 14:48:17 -0400

    Update IconButton.story.tsx

--- packages/icon-button/src/IconButton.story.tsx
+++ packages/icon-button/src/IconButton.story.tsx
@@ -1,29 +1,42 @@
 import React from 'react';
-import { ComponentStory } from '@storybook/react';
+import { StoryFn } from '@storybook/react';
 
 import CloudIcon from '@leafygreen-ui/icon/dist/Cloud';
-import { StoryMeta } from '@leafygreen-ui/lib';
+import {
+  DarkModeProps,
+  storybookArgTypes,
+  storybookExcludedControlParams as defaultExclude,
+  StoryMetaType,
+} from '@leafygreen-ui/lib';
 
-import IconButton from '.';
+import IconButton, { AccessibleIconButtonProps } from '.';
 
-export default StoryMeta({
+const meta: StoryMetaType<typeof IconButton> = {
   title: 'Components/IconButton',
   component: IconButton,
   parameters: {
     default: 'Basic',
     controls: {
-      exclude: ['children'],
+      exclude: [...defaultExclude, 'children'],
     },
   },
   argTypes: {
+    darkMode: storybookArgTypes.darkMode,
     href: { control: 'string' },
     active: { control: 'boolean' },
     disabled: { control: 'boolean' },
   },
-});
-// eslint-disable-next-line react/prop-types
-const Template: ComponentStory<typeof IconButton> = ({ darkMode, ...args }) => (
-  <IconButton darkMode={darkMode} {...args}>
+};
+
+export default meta;
+
+/// @ts-expect-error
+const Template: StoryFn<AccessibleIconButtonProps & DarkModeProps> = ({
+  // eslint-disable-next-line react/prop-types
+  darkMode,
+  ...args
+}) => (
+  <IconButton darkMode={darkMode} {...(args as AccessibleIconButtonProps)}>
     <CloudIcon />
   </IconButton>
 );
