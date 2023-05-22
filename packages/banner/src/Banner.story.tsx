40f1726583f73a51028713c9d5135f8915dc3cc9
Adam Thompson <adam.thompson@mongodb.com>
2023-05-22 18:16:50 -0400

    Update Banner.story.tsx

--- packages/banner/src/Banner.story.tsx
+++ packages/banner/src/Banner.story.tsx
@@ -1,34 +1,19 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/jsx-key */
 import React from 'react';
 import { StoryFn } from '@storybook/react';
 
-import { css } from '@leafygreen-ui/emotion';
 import Icon, { glyphs } from '@leafygreen-ui/icon';
 import { storybookArgTypes, StoryMetaType } from '@leafygreen-ui/lib';
-import { Link } from '@leafygreen-ui/typography';
 
 import Banner, { BannerProps, Variant } from '.';
 
 const meta: StoryMetaType<typeof Banner> = {
   title: 'Components/Banner',
   component: Banner,
-  decorators: [
-    StoryFn => (
-      <div
-        className={css`
-          max-width: 256px;
-        `}
-      >
-        <StoryFn />
-      </div>
-    ),
-  ],
   parameters: {
-    default: 'WithLink',
-    controls: {
-      exclude: ['image'],
-    },
+    default: 'LiveExample',
     generate: {
       variant: Object.values(Variant),
       image: [
@@ -56,60 +41,20 @@ const meta: StoryMetaType<typeof Banner> = {
       defaultValue: Variant.Info,
     },
     children: storybookArgTypes.children,
+    glyph: {
+      options: [undefined, ...Object.keys(glyphs)],
+      control: { type: 'select' },
+    },
   },
 };
 export default meta;
 
-// eslint-disable-next-line react/prop-types
-export const Basic: StoryFn<typeof Banner> = ({ ...args }) => (
-  <Banner {...args} />
-);
+type StoryProps = BannerProps & { glyph: keyof typeof glyphs };
 
-export const WithIcon: StoryFn<any> = ({
-  glyph,
-  ...args
-}: BannerProps & { glyph: keyof typeof glyphs }) => (
+export const LiveExample: StoryFn<StoryProps> = ({ glyph, ...args }) => (
   <Banner image={glyph ? <Icon glyph={glyph} /> : undefined} {...args} />
 );
-WithIcon.argTypes = {
-  glyph: {
-    options: Object.keys(glyphs),
-    control: { type: 'select' },
-  },
-};
-WithIcon.args = {
-  glyph: 'ActivityFeed',
-};
-
-export const WithLink: StoryFn<typeof Banner> = ({
-  // eslint-disable-next-line react/prop-types
-  image,
-  // eslint-disable-next-line react/prop-types
-  children,
-  ...args
-}) => {
-  return (
-    <Banner
-      // @ts-expect-error
-      image={image ? <Icon glyph={image} /> : undefined}
-      {...args}
-    >
-      {children}
-      &nbsp;
-      <Link href="http://localhost:9001">Link component</Link>
-      &nbsp;
-      <a href="http://localhost:9001">Anchor tag</a>
-    </Banner>
-  );
-};
-
-export const WithCustomImage: StoryFn<typeof Banner> = ({ ...args }) => (
-  <Banner image={<img src="favicon.ico" alt="logo" />} {...args} />
-);
-WithCustomImage.args = {
-  variant: Variant.Info,
-};
-WithCustomImage.parameters = {
+LiveExample.parameters = {
   chromatic: { disableSnapshot: true },
 };
 
