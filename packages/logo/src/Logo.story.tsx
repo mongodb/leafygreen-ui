4ca93699310507c4c9d5428f2a2e1cbf872794df
Adam Thompson <adam.thompson@mongodb.com>
2023-05-25 15:02:00 -0400

    Update Logo.story.tsx

--- packages/logo/src/Logo.story.tsx
+++ packages/logo/src/Logo.story.tsx
@@ -26,7 +26,32 @@ const meta: StoryMetaType<typeof Logo> = {
   title: 'Components/Logo',
   component: Logo,
   parameters: {
-    default: 'Default',
+    default: 'LiveExample',
+    generate: {
+      props: {
+        darkMode: [false, true],
+        name: Object.values(LogoNames),
+        color: Object.values(SupportedColors),
+      },
+      excludeCombinations: [
+        {
+          darkMode: false,
+          color: SupportedColors.White,
+        },
+        {
+          darkMode: false,
+          color: SupportedColors.GreenBase,
+        },
+        {
+          darkMode: true,
+          color: SupportedColors.Black,
+        },
+        {
+          darkMode: true,
+          color: SupportedColors.GreenDark2,
+        },
+      ],
+    },
   },
   argTypes: {
     color: {
@@ -38,6 +63,10 @@ const meta: StoryMetaType<typeof Logo> = {
       default: palette.white,
       control: 'radio',
       options: [palette.white, palette.gray.dark3],
+    },
+    name: {
+      control: 'select',
+      options: LogoNames,
     },
   },
 };
@@ -56,6 +85,25 @@ const divStyle = css`
   margin: 0.5rem;
 `;
 
+export const LiveExample: StoryFn<
+  LogoProps & { name: LogoName; background?: string }
+> = (args: LogoProps & { name: LogoName } & { background?: string }) => {
+  if (!args.name) {
+    args = { ...args, name: 'MongoDBLogo' };
+  }
+
+  return <Logo {...args} />;
+};
+LiveExample.argTypes = {
+  name: {
+    control: 'select',
+    options: LogoNames,
+  },
+};
+LiveExample.args = {
+  name: 'MongoDBLogoMark',
+};
+
 const Template = (
   LogoComponent: React.FunctionComponent<LogoProps>,
   args: LogoProps & { background?: string },
@@ -71,25 +119,7 @@ const Template = (
   );
 };
 
-export const Default: StoryFn<
-  LogoProps & { name: LogoName; background?: string }
-> = (args: LogoProps & { name: LogoName } & { background?: string }) => {
-  if (!args.name) {
-    args = { ...args, name: 'MongoDBLogo' };
-  }
-
-  return <Logo {...args} />;
-};
-Default.argTypes = {
-  name: {
-    control: 'select',
-    options: LogoNames,
-  },
-};
-Default.args = {
-  name: 'MongoDBLogoMark',
-};
-
+// Individual Components
 export const MongoDB = (args: LogoProps) => Template(MongoDBLogo, args);
 export const Atlas = (args: LogoProps) => Template(AtlasNavGraphic, args);
 export const MongoDBMark = (args: LogoProps) => Template(MongoDBLogoMark, args);
@@ -110,3 +140,5 @@ export const RealmMark = (args: LogoProps) => Template(RealmLogoMark, args);
 RealmMark.storyName = '[DEPRECATED] Realm Mark';
 export const ChartsMark = (args: LogoProps) => Template(ChartsLogoMark, args);
 ChartsMark.storyName = '[DEPRECATED] Charts Mark';
+
+export const Generated = () => {};
