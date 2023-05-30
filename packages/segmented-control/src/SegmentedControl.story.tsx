de7bf1618a32dd40a2951a26ae709a86de78031f
Adam Thompson <adam.thompson@mongodb.com>
2023-05-30 16:35:45 -0400

    Update SegmentedControl.story.tsx

--- packages/segmented-control/src/SegmentedControl.story.tsx
+++ packages/segmented-control/src/SegmentedControl.story.tsx
@@ -8,13 +8,16 @@ import {
 } from '@leafygreen-ui/lib';
 import {
   SegmentedControl,
-  SegmentedControlOption,
   SegmentedControlProps,
 } from '@leafygreen-ui/segmented-control';
 import { transitionDuration } from '@leafygreen-ui/tokens';
 
 import { Size } from './SegmentedControl/SegmentedControl.types';
 import { TestChildren } from './SegmentedControl.testutils';
+
+interface LiveExampleProps {
+  childrenOptions: keyof typeof TestChildren;
+}
 
 const meta: StoryMetaType<typeof SegmentedControl> = {
   title: 'Components/SegmentedControl',
@@ -45,7 +48,17 @@ const meta: StoryMetaType<typeof SegmentedControl> = {
       },
     },
   },
+  args: {
+    label: 'Fruit',
+    name: 'fruit',
+    childrenOptions: 'Basic',
+  },
   argTypes: {
+    childrenOptions: {
+      control: 'select',
+      options: Object.keys(TestChildren),
+      description: 'LiveExample toggle for children',
+    },
     label: { control: 'text' },
     name: { control: 'text' },
     defaultValue: { control: 'text' },
@@ -64,109 +77,10 @@ const meta: StoryMetaType<typeof SegmentedControl> = {
 };
 export default meta;
 
-export const LiveExample: StoryFn<SegmentedControlProps> = (
-  args: SegmentedControlProps,
-) => <SegmentedControl {...args} />;
-LiveExample.args = {
-  label: 'Fruit',
-  name: 'fruit',
-  children: [
-    <SegmentedControlOption key="dragonfruit" value="dragonfruit">
-      Dragonfruit
-    </SegmentedControlOption>,
-    <SegmentedControlOption key="eggplant" value="eggplant">
-      Eggplant
-    </SegmentedControlOption>,
-    <SegmentedControlOption key="fig" value="fig">
-      Fig
-    </SegmentedControlOption>,
-    <SegmentedControlOption disabled key="grape" value="grape">
-      Grape
-    </SegmentedControlOption>,
-  ],
-};
-
-/*
-export const Controlled: StoryFn<SegmentedControlProps> = (
-  args: SegmentedControlProps,
-) => {
-  const [selectedFruit, setSelectedFruit] = useState('eggplant');
-  return (
-    <LiveExample
-      {...args}
-      key="selectedFruit"
-      value={selectedFruit}
-      onChange={setSelectedFruit}
-    />
-  );
-};
-Controlled.args = {
-  label: 'Fruit overwhelmed',
-  name: 'fruit',
-  children: [
-    <SegmentedControlOption key="dragonfruit" value="dragonfruit">
-      Dragonfruit fruit
-    </SegmentedControlOption>,
-    <SegmentedControlOption key="eggplant" value="eggplant">
-      Eggplant bananana
-    </SegmentedControlOption>,
-    <SegmentedControlOption key="fig" value="fig">
-      Fig
-    </SegmentedControlOption>,
-    <SegmentedControlOption key="grape" value="grape">
-      Grape
-    </SegmentedControlOption>,
-  ],
-};*/
-
-// export const WithIcons = LiveExample.bind({});
-// WithIcons.args = {
-//   label: 'View as',
-//   name: 'language',
-//   children: [
-//     <SegmentedControlOption
-//       key="json"
-//       value="json"
-//       glyph={<Icon glyph="CurlyBraces" />}
-//     >
-//       JSON and more
-//     </SegmentedControlOption>,
-//     <SegmentedControlOption key="xml" value="xml" glyph={<Icon glyph="Code" />}>
-//       XML
-//     </SegmentedControlOption>,
-//     <SegmentedControlOption
-//       disabled
-//       key="shell"
-//       value="shell"
-//       glyph={<Icon glyph="Shell" />}
-//     >
-//       Shell
-//     </SegmentedControlOption>,
-//   ],
-// };
-
-// export const IconsOnly = LiveExample.bind({});
-// IconsOnly.args = {
-//   label: 'Location',
-//   name: 'location',
-//   children: [
-//     <SegmentedControlOption
-//       key="cloud"
-//       value="cloud"
-//       glyph={<Icon glyph="Cloud" />}
-//     />,
-//     <SegmentedControlOption
-//       key="globe"
-//       value="globe"
-//       glyph={<Icon glyph="GlobeAmericas" />}
-//     />,
-//     <SegmentedControlOption
-//       disabled
-//       key="government"
-//       value="government"
-//       glyph={<Icon glyph="GovernmentBuilding" />}
-//     />,
-//   ],
-// };
-
-export const Generated = () => {};
+export const LiveExample: StoryFn<SegmentedControlProps & LiveExampleProps> = (
+  args: SegmentedControlProps & LiveExampleProps,
+) => (
+  <SegmentedControl {...args}>
+    {TestChildren[args.childrenOptions]}
+  </SegmentedControl>
+);
