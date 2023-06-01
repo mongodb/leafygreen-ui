bad39a42eeaf2f80beec5c590a97d4a5b093366f
Sean Park <seanpark1107@gmail.com>
2023-06-01 16:32:03 -0400

    LG-3246: Add loading state to button (#1770)
    * spinner component
    
    * blob loader
    
    * live example
    
    * docs and changeset
    
    * changes for live example ux
    
    * add tests
    
    * revert unintended changes
    
    * update live example story
    
    * live example story update
    
    * storybook arg update
    
    * pr comments and change variant prop
    
    * update canvas-mock usage to pass depcheck
    
    * addressing PR comments
    
    * wip
    
    * loading button v1
    
    * update loading-indicator storybook
    
    * remove irrelevant changeset
    
    * changeset
    
    * fix bug with button sizing when loading text is present
    
    * add tests
    
    * fix dark mode argType
    
    * colorOverride prop in loading-indicator; style cleanup in Button
    
    * lint
    
    * moving variable definition position
    
    * code style changes
    
    * fixing bug with sizeOverride when it is smaller than 16
    
    * fix positioning issue with spinner
    
    * rename BlobLoader to PageLoader (#1780)

--- packages/button/src/Button.story.tsx
+++ packages/button/src/Button.story.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { StoryFn } from '@storybook/react';
 
 import Icon, { glyphs } from '@leafygreen-ui/icon';
-import { type StoryMetaType } from '@leafygreen-ui/lib';
+import { storybookArgTypes, type StoryMetaType } from '@leafygreen-ui/lib';
 
 import { Size } from './types';
 import Button, { ButtonProps, Variant } from '.';
@@ -22,6 +22,7 @@ const meta: StoryMetaType<typeof Button> = {
     disabled: {
       control: { type: 'boolean' },
     },
+    darkMode: storybookArgTypes.darkMode,
     leftGlyph: {
       options: Object.keys(glyphs),
       control: { type: 'select' },
@@ -41,6 +42,12 @@ const meta: StoryMetaType<typeof Button> = {
       defaultValue: Size.Default,
     },
     href: {
+      control: 'text',
+    },
+    isLoading: {
+      control: 'boolean',
+    },
+    loadingText: {
       control: 'text',
     },
   },
@@ -65,4 +72,15 @@ export const Playground: StoryFn<ButtonProps> = ({
 export const BaseGreen = Playground.bind({});
 BaseGreen.args = {
   variant: Variant.BaseGreen,
+};
+
+export const LoadingState = Playground.bind({});
+LoadingState.args = {
+  isLoading: true,
+};
+
+export const LoadingStateWithText = Playground.bind({});
+LoadingStateWithText.args = {
+  isLoading: true,
+  loadingText: 'Saving',
 };
