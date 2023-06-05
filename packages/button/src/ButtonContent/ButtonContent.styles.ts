2cf3e5b1a0184b85a965bc29298186fc53461c51
spark33 <seanpark1107@gmail.com>
2023-06-05 14:10:12 -0400

    lint

--- packages/button/src/ButtonContent/ButtonContent.styles.ts
+++ packages/button/src/ButtonContent/ButtonContent.styles.ts
@@ -3,9 +3,9 @@ import { transparentize } from 'polished';
 import { css } from '@leafygreen-ui/emotion';
 import { Theme } from '@leafygreen-ui/lib';
 import { palette } from '@leafygreen-ui/palette';
+import { transitionDuration } from '@leafygreen-ui/tokens';
 
 import { Size, Variant } from '../types';
-import { transitionDuration } from '@leafygreen-ui/tokens';
 
 const rippleOpacity = 0.76;
 
