8eb2b7dc0183518cb74f83a71a2e4473b13bb0a3
Sean Park <seanpark1107@gmail.com>
2023-06-06 14:38:40 -0400

    LG-3246: Add loading state to Button (#1788)
    * LG-3246 v2
    
    * changeset
    
    * update README
    
    * address pr comments
    
    * lint
    
    * Update Button.tsx
    
    * update types to not duplicate default values
    
    * adding DefaultContentProps alias

--- packages/button/src/ButtonContent/ButtonContent.tsx
+++ packages/button/src/ButtonContent/ButtonContent.tsx
@@ -1,30 +1,39 @@
 import React, { useEffect, useRef } from 'react';
 
-import { css, cx } from '@leafygreen-ui/emotion';
+import { cx } from '@leafygreen-ui/emotion';
 import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
+import { Spinner } from '@leafygreen-ui/loading-indicator';
 import { registerRipple } from '@leafygreen-ui/ripple';
-
-import ButtonIcon from '../ButtonIcon/ButtonIcon';
-import { ButtonProps, Size, Variant } from '../types';
 
 import {
   buttonContentSizeStyle,
   buttonContentStyle,
+  buttonSpinnerSize,
+  centeredSpinnerContainerStyles,
+  centeredSpinnerStyles,
+  hiddenContentStyles,
   rippleColors,
   rippleStyle,
+  spinnerColor,
 } from './ButtonContent.styles';
+import { ButtonContentProps } from './ButtonContent.types';
+import DefaultContent from './DefaultContent';
 
-type ButtonContentProps = Omit<ButtonProps, 'as'>;
+/**
+ * Internal contents of a Button
+ * @internal
+ */
+export const ButtonContent = (props: ButtonContentProps) => {
+  const {
+    darkMode: darkModeProp,
+    disabled,
+    variant,
+    size,
+    isLoading,
+    loadingText,
+    className,
+  } = props;
 
-export const ButtonContent = ({
-  rightGlyph,
-  leftGlyph,
-  darkMode: darkModeProp,
-  disabled = false,
-  variant = Variant.Default,
-  size = Size.Default,
-  children,
-}: ButtonContentProps) => {
   const { darkMode, theme } = useDarkMode(darkModeProp);
   const rippleRef = useRef<HTMLDivElement | null>(null);
 
@@ -41,43 +50,39 @@ export const ButtonContent = ({
     return unregisterRipple;
   }, [rippleRef, variant, darkMode, disabled, theme]);
 
-  const isIconOnlyButton = ((leftGlyph || rightGlyph) && !children) ?? false;
-  const iconProps = { variant, size, darkMode, disabled, isIconOnlyButton };
+  if (isLoading) {
+    return (
+      <>
+        <div
+          className={cx(buttonContentStyle, buttonContentSizeStyle[size], {
+            [centeredSpinnerContainerStyles]: !loadingText,
+          })}
+        >
+          <Spinner
+            className={cx({
+              [centeredSpinnerStyles]: !loadingText,
+            })}
+            sizeOverride={buttonSpinnerSize[size]}
+            colorOverride={spinnerColor[theme]}
+            data-testid="lg-button-spinner"
+          />
+          {loadingText}
+        </div>
+        {!loadingText && (
+          <DefaultContent
+            {...props}
+            className={cx(hiddenContentStyles, className)}
+          />
+        )}
+      </>
+    );
+  }
 
   return (
     <>
       {/* Ripple cannot wrap children, otherwise components that rely on children to render dropdowns will not be rendered due to the overflow:hidden rule. */}
-      <div className={cx(rippleStyle)} ref={rippleRef} />
-
-      <div
-        className={cx(buttonContentStyle, buttonContentSizeStyle[size], {
-          [css`
-            justify-content: space-between;
-          `]: !!rightGlyph && darkMode,
-        })}
-      >
-        {leftGlyph && (
-          <ButtonIcon
-            glyph={leftGlyph}
-            className={css`
-              justify-self: right;
-            `}
-            {...iconProps}
-          />
-        )}
-
-        {children}
-
-        {rightGlyph && (
-          <ButtonIcon
-            glyph={rightGlyph}
-            className={css`
-              justify-self: left;
-            `}
-            {...iconProps}
-          />
-        )}
-      </div>
+      <div className={rippleStyle} ref={rippleRef} />
+      <DefaultContent {...props} />
     </>
   );
 };
