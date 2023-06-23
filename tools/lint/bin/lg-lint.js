b4c939bef1e5c489406703b995beb3f6d0f076c4
Adam Thompson <adam.thompson@mongodb.com>
2023-06-23 17:35:52 -0400

    bump eslint to ^7

--- tools/lint/bin/lg-lint.js
+++ tools/lint/bin/lg-lint.js
@@ -1,30 +1,40 @@
 #! /usr/bin/env node
 const { spawnSync } = require('child_process');
 const path = require('path');
-// const { Command } = require('commander');
+const { Command } = require('commander');
 const rootDir = process.cwd();
 const eslintConfig = path.resolve(__dirname, '../eslint.config.js');
 const prettierConfig = path.resolve(__dirname, '../prettier.config.js');
 
-// spawnSync(
-//   'eslint',
-//   [
-//     "--config",
-//     eslintConfig,
-//     `${rootDir}/**/*.{js,ts,tsx}`
-//   ],
-//   {
-//     stdio: 'inherit',
-//   }
-// );
+const esLintExtensions = ['js', 'ts', 'tsx'];
+const prettierExtensions = [...esLintExtensions, 'mjs', 'json', 'md', 'yml']
+
+const cli = new Command()
+.option('-f, --fix', 'fix linting errors', false)
+.parse(process.argv);
+
+const { fix } = cli.opts();
+
+spawnSync(
+  'eslint',
+  [
+    "--config",
+    eslintConfig,
+    `${rootDir}/**/*.{${esLintExtensions.join(',')}}`,
+    fix ? '--fix' : '--no-fix',
+  ],
+  {
+    stdio: 'inherit',
+  }
+);
 
 spawnSync(
   'prettier',
   [
-    '--check',
+    fix ? '--write' : '--check',
     '--config',
     prettierConfig,
-    `${rootDir}/**/*.{js,ts,tsx,mjs,json,md,yml}`,
+    `${rootDir}/**/*.{${prettierExtensions.join(',')}}`,
   ],
   {
     stdio: 'inherit',
