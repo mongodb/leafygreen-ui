2a6f79be5750a780b3c45fd6c46c4e919ed40093
Adam Thompson <adam.thompson@mongodb.com>
2023-06-23 17:41:52 -0400

    fix

--- tools/lint/bin/lg-lint.js
+++ tools/lint/bin/lg-lint.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-const { spawnSync } = require('child_process');
+const { spawn } = require('child_process');
 const path = require('path');
 const { Command } = require('commander');
 const rootDir = process.cwd();
@@ -7,36 +7,40 @@ const eslintConfig = path.resolve(__dirname, '../eslint.config.js');
 const prettierConfig = path.resolve(__dirname, '../prettier.config.js');
 
 const esLintExtensions = ['js', 'ts', 'tsx'];
-const prettierExtensions = [...esLintExtensions, 'mjs', 'json', 'md', 'yml']
+const prettierExtensions = [...esLintExtensions, 'mjs', 'json', 'md', 'yml'];
 
 const cli = new Command()
-.option('-f, --fix', 'fix linting errors', false)
-.parse(process.argv);
+  .option('-f, --fix', 'fix linting errors', false)
+  .option('-p, --prettier', 'run prettier only', false)
+  .option('-e, --eslint', 'run eslint only', false)
+  .parse(process.argv);
 
-const { fix } = cli.opts();
+const { fix, prettier: prettierOnly, eslint: eslintOnly } = cli.opts();
 
-spawnSync(
-  'eslint',
-  [
-    "--config",
-    eslintConfig,
-    `${rootDir}/**/*.{${esLintExtensions.join(',')}}`,
-    fix ? '--fix' : '--no-fix',
-  ],
-  {
-    stdio: 'inherit',
-  }
-);
+!prettierOnly &&
+  spawn(
+    'eslint',
+    [
+      '--config',
+      eslintConfig,
+      `${rootDir}/**/*.{${esLintExtensions.join(',')}}`,
+      fix ? '--fix' : '--no-fix',
+    ],
+    {
+      stdio: 'inherit',
+    },
+  );
 
-spawnSync(
-  'prettier',
-  [
-    fix ? '--write' : '--check',
-    '--config',
-    prettierConfig,
-    `${rootDir}/**/*.{${prettierExtensions.join(',')}}`,
-  ],
-  {
-    stdio: 'inherit',
-  },
-);
+!eslintOnly &&
+  spawn(
+    'prettier',
+    [
+      fix ? '--write' : '--check',
+      '--config',
+      prettierConfig,
+      `${rootDir}/**/*.{${prettierExtensions.join(',')}}`,
+    ],
+    {
+      stdio: 'inherit',
+    },
+  );
