11f06e836022d8960aaa1cd9867148f4dc1e1e50
Shaneeza <shaneeza.ali@mongodb.com>
2023-06-27 17:09:59 -0400

    fix guide cue

--- packages/guide-cue/src/TooltipContent.tsx
+++ packages/guide-cue/src/TooltipContent.tsx
@@ -8,6 +8,7 @@ import IconButton from '@leafygreen-ui/icon-button';
 import { Theme } from '@leafygreen-ui/lib';
 import Tooltip from '@leafygreen-ui/tooltip';
 import { Body, Disclaimer } from '@leafygreen-ui/typography';
+import { useIdAllocator } from '@leafygreen-ui/hooks';
 
 import {
   bodyThemeStyles,
@@ -62,8 +63,26 @@ function TooltipContent({
   usePortal = true,
   ...tooltipProps
 }: TooltipContentProps) {
+  const focusId = useIdAllocator({ prefix: 'guide-cue' });
   const focusTrapOptions = {
     clickOutsideDeactivates: true,
+    // Without this check focus-trap cannot find a tabbable element. My guess is because of the tooltip animations focus-trap is looking for a tabbale item too early.
+    checkCanFocusTrap: (trapContainers: Array<HTMLElement | SVGElement>) => {
+      const results = trapContainers.map(() => {
+        return new Promise<void>(resolve => {
+          const interval = setInterval(() => {
+            if (
+              document.body.contains(document.getElementById(`#${focusId}`))
+            ) {
+              resolve();
+              clearInterval(interval);
+            }
+          }, 5);
+        });
+      });
+      // Return a promise that resolves when all the trap containers are able to receive focus
+      return Promise.all(results);
+    },
   };
 
   return (
@@ -90,6 +109,8 @@ function TooltipContent({
         usePortal={usePortal}
         {...tooltipProps}
       >
+        {/* https://github.com/focus-trap/focus-trap-react/issues/720 */}
+        {/* @ts-expect-error - checkCanFocusTrap throws a type error */}
         <FocusTrap focusTrapOptions={focusTrapOptions} active={open}>
           <div>
             {!isStandalone && (
@@ -130,6 +151,7 @@ function TooltipContent({
                 onClick={() => handleButtonClick()}
                 darkMode={!darkMode}
                 className={buttonStyles}
+                id={focusId}
               >
                 {buttonText}
               </Button>
