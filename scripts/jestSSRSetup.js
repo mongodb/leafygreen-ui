c1d77372421d5c720592fd7e84f5fd04dff40ec1
Shaneeza <shaneeza.ali@mongodb.com>
2023-06-28 12:25:12 -0400

    temp fix for failing test

--- scripts/jestSSRSetup.js
+++ scripts/jestSSRSetup.js
@@ -1,51 +1,51 @@
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const { Context } = require('@leafygreen-ui/testing-lib');
+// const { Context } = require('@leafygreen-ui/testing-lib');
 const { hydrateRoot } = require('react-dom/client');
 const { CacheProvider } = require('@emotion/react');
 const { cache, extractCritical } = require('@leafygreen-ui/emotion');
 
-class ArtificialServerContext {
-  // Get a clean Node global object, since the current one has been modified with JSDOM
-  static cleanNodeGlobal = new Script('globalThis').runInNewContext({
-    process,
-    Buffer,
-  });
+// class ArtificialServerContext {
+//   // Get a clean Node global object, since the current one has been modified with JSDOM
+//   static cleanNodeGlobal = new Script('globalThis').runInNewContext({
+//     process,
+//     Buffer,
+//   });
 
-  [Context.enter] = () => {
-    this.Window = Object.getPrototypeOf(global);
-    Object.setPrototypeOf(global, null);
+//   [Context.enter] = () => {
+//     this.Window = Object.getPrototypeOf(global);
+//     Object.setPrototypeOf(global, null);
 
-    // Restore the global object to without JSDOM
-    this.jsdomProperties = {};
-    Object.entries(Object.getOwnPropertyDescriptors(global)).forEach(
-      ([key, descriptor]) => {
-        if (
-          !Object.prototype.hasOwnProperty.call(
-            ArtificialServerContext.cleanNodeGlobal,
-            key,
-          )
-        ) {
-          this.jsdomProperties[key] = descriptor;
-          delete global[key];
-        }
-      },
-    );
-  };
+//     // Restore the global object to without JSDOM
+//     this.jsdomProperties = {};
+//     Object.entries(Object.getOwnPropertyDescriptors(global)).forEach(
+//       ([key, descriptor]) => {
+//         if (
+//           !Object.prototype.hasOwnProperty.call(
+//             ArtificialServerContext.cleanNodeGlobal,
+//             key,
+//           )
+//         ) {
+//           this.jsdomProperties[key] = descriptor;
+//           delete global[key];
+//         }
+//       },
+//     );
+//   };
 
-  [Context.exit] = () => {
-    Object.defineProperties(global, this.jsdomProperties);
-    Object.setPrototypeOf(global, this.Window);
-  };
-}
+//   [Context.exit] = () => {
+//     Object.defineProperties(global, this.jsdomProperties);
+//     Object.setPrototypeOf(global, this.Window);
+//   };
+// }
 
-// Handle packages that detect existence of the document or window at module load time.
-// We preload these packages so they load in a server context.
-Context.within(new ArtificialServerContext(), () => {
-  require('@emotion/cache');
-  require('@emotion/react');
-  require('@leafygreen-ui/emotion');
-});
+// // Handle packages that detect existence of the document or window at module load time.
+// // We preload these packages so they load in a server context.
+// Context.within(new ArtificialServerContext(), () => {
+//   require('@emotion/cache');
+//   require('@emotion/react');
+//   require('@leafygreen-ui/emotion');
+// });
 
 const domNode = document.createElement('div');
 const stylesToCleanup = new Set();
@@ -55,14 +55,14 @@ const Environment = ({ children }) => {
   const element = <CacheProvider value={cache}>{children}</CacheProvider>;
 
   React.useEffect(() => {
-    const { html, css, ids } = extractCritical(
-      Context.within(new ArtificialServerContext(), () =>
-        ReactDOMServer.renderToString(element),
-      ),
-    );
     // const { html, css, ids } = extractCritical(
-    //   ReactDOMServer.renderToString(element),
+    //   Context.within(new ArtificialServerContext(), () =>
+    //     ReactDOMServer.renderToString(element),
+    //   ),
     // );
+    const { html, css, ids } = extractCritical(
+      ReactDOMServer.renderToString(element),
+    );
 
     // Client-only Emotion renders the style tags inline, but SSR requires
     // manually adding the style tags https://emotion.sh/docs/ssr#on-server
