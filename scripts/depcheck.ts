5e1a065c8036e36ee1666ee7bc107c233aa6cdcd
Adam Thompson <adam.thompson@mongodb.com>
2023-06-28 12:48:32 -0400

    lint

--- scripts/depcheck.ts
+++ scripts/depcheck.ts
@@ -41,7 +41,6 @@ const ignoreFilePatterns: Array<RegExp> = [
   /.*\/dist\/.*/,
 ];
 
-
 // these dependencies will be ignored when listed in a package.json
 const ignoreMatches = [
   '@leafygreen-ui/mongo-nav',
