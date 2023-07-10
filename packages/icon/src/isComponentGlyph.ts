686d908b56dad00da47138c1aa0767486b6c0d22
brooke <brooke.yalof@mongodb.com>
2023-07-10 12:12:42 -0400

    fixes tooltip TS issues

--- packages/icon/src/isComponentGlyph.ts
+++ packages/icon/src/isComponentGlyph.ts
@@ -2,19 +2,23 @@ import { ComponentType, isValidElement, ReactNode } from 'react';
 
 import { LGGlyph } from './types';
 
-type ExtendedComponentType = ComponentType<React.PropsWithChildren<any>> & {
-  [key: string]: any;
-};
+type ExtendedComponentType =
+  | ComponentType<React.PropsWithChildren<any>> & {
+      [key: string]: any;
+    };
 /**
  * Helper type to check if element is a LeafyGreen UI Glyph
  * @internal
  */
+function isComponentGlyph<P>(
+  node: {} | null | undefined,
+): node is React.ReactElement<P>;
 function isComponentGlyph(node: ReactNode): node is LGGlyph.Element;
 function isComponentGlyph(
   component: ExtendedComponentType,
 ): component is LGGlyph.Component;
 function isComponentGlyph(
-  child: ReactNode | ExtendedComponentType,
+  child: ReactNode | ExtendedComponentType | unknown,
 ): child is LGGlyph.Element | LGGlyph.Component {
   // If we're received a rendered component (i.e. ReactNode)
   if (isValidElement(child)) {
@@ -26,12 +30,12 @@ function isComponentGlyph(
     );
   }
 
-  // If we've recieved a component function
+  // If we've received a component function
   return (
     child != null &&
     typeof child === 'function' &&
     'isGlyph' in child &&
-    child.isGlyph === true
+    (child as LGGlyph.Component).isGlyph === true
   );
 }
 
