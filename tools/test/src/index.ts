fe822bcec86370df45b6ca18e5ffc5dcb8013cde
Adam Thompson <adam.thompson@mongodb.com>
2023-07-12 18:05:38 -0400

    Creates test command

--- tools/test/src/index.ts
+++ tools/test/src/index.ts
@@ -1,31 +1,26 @@
 #! /usr/bin/env node
 import { spawn } from 'child_process';
-import { Command } from 'commander';
 import path from 'path';
 
 const rootDir = process.cwd();
 
-const cli = new Command('test')
-  .description('Tests leafygreen-ui packages with unified config.')
-  .option('--ssr', 'Runs tests on a simulated server', false)
-  .option('--watch', 'Watch all files you intend to test', false)
-  .allowUnknownOption()
-  .parse(process.argv);
+interface TestCommandOptions {
+  watch: boolean;
+}
 
-const { ssr, watch } = cli.opts();
+export const test = (options: TestCommandOptions) => {
+  const { watch } = options;
+  const configFile = path.resolve(__dirname, '../config/jest.config.js');
 
-const configFile = ssr
-  ? path.resolve(__dirname, '../config/ssr/jest.config.js')
-  : path.resolve(__dirname, '../config/jest.config.js');
-
-spawn(
-  'jest',
-  [`--config`, configFile, `--rootDir`, rootDir, watch ? '--watch' : ''],
-  {
-    env: {
-      ...process.env,
-      JEST_ENV: ssr ? 'ssr' : 'client',
+  spawn(
+    'jest',
+    [`--config`, configFile, `--rootDir`, rootDir, watch ? '--watch' : ''],
+    {
+      env: {
+        ...process.env,
+        JEST_ENV: 'client',
+      },
+      stdio: 'inherit',
     },
-    stdio: 'inherit',
-  },
-);
+  );
+};
