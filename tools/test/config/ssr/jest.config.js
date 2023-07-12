006bb466c3f87bf3ff0e49cafaf308e43b466c93
Adam Thompson <adam.thompson@mongodb.com>
2023-07-12 18:05:38 -0400

    Creates test command

--- tools/test/config/ssr/jest.config.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = require('../jest.config.js');
-
-module.exports = {
-  ...defaultConfig,
-  setupFiles: [
-    ...defaultConfig.setupFiles,
-    '<rootDir>/node_modules/@lg-tools/test/config/ssr/setup.js',
-  ],
-};
