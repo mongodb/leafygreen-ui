1294c0a0f9ca9a4a78789551b6446df5b2864ff2
Adam Thompson <adam.thompson@mongodb.com>
2023-07-13 10:28:19 -0400

    Creates validate command

--- tools/validate/rollup.config.mjs
+++ tools/validate/rollup.config.mjs
@@ -1,12 +1,2 @@
 import { umdConfig } from '@lg-tools/build/config/rollup.config.mjs';
-
-export default [
-  {
-    ...umdConfig,
-    input: 'src/builds.ts',
-  },
-  {
-    ...umdConfig,
-    input: 'src/deps.ts',
-  },
-];
+export default umdConfig;
