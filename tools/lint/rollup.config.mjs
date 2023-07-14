34d9107ea1fb6e9a3cb2b75e3716c26f9d6966d1
Adam Thompson <adam.thompson@mongodb.com>
2023-07-14 16:54:35 -0400

    build umd & cjs

--- tools/lint/rollup.config.mjs
+++ tools/lint/rollup.config.mjs
@@ -1,6 +1,2 @@
-import { umdConfig } from '@lg-tools/build/config/rollup.config.mjs';
-
-export default {
-  ...umdConfig,
-  input: 'src/index.ts',
-};
+import { esmConfig, umdConfig } from '@lg-tools/build/config/rollup.config.mjs';
+export default [esmConfig, umdConfig];
