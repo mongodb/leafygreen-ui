fc0ce9d9e535c620df49486cc2789c4e940bec57
Adam Thompson <adam.thompson@mongodb.com>
2023-07-18 13:03:14 -0400

    Adds lg-internal-build-package command

--- tools/build/src/rollup/build-package.ts
+++ tools/build/src/rollup/build-package.ts
@@ -3,10 +3,20 @@ import { spawn } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 
+interface BuildPackageOptions {
+  /**
+   * Pass this option if the function is called directly, and not via Commander.action.
+   * We use this option in this package's `bin/build-packages.js` command,
+   * in order to log a warning to consumers to use the `lg` command from @lg-tools/cli
+   */
+  direct?: boolean;
+  verbose?: boolean;
+}
+
 /**
  * Builds packages using rollup for the current directory
  */
-export function buildPackage() {
+export function buildPackage({ direct }: BuildPackageOptions) {
   const packageDir = process.cwd();
   const localRollupConfigPath = path.join(packageDir, 'rollup.config.mjs');
   const defaultRollupConfigPath = path.join(
@@ -14,14 +24,29 @@ export function buildPackage() {
     '../config/rollup.config.mjs',
   );
 
+  const splitPath = packageDir.split('/');
+  const packageName = splitPath[splitPath.length - 1];
+  const scopeName = splitPath[splitPath.length - 2];
+
+  if (direct && scopeName !== 'tools') {
+    console.warn(
+      'Building package using the `lg-build-package` command directly from `@lg-tools/build`.',
+      'Consider using the global `lg build-package` command from `@lg-tools/cli` instead.',
+    );
+  }
+
   // If there is a local rollup config defined, use that
   // Otherwise use the default one
   const rollupConfigPath = fs.existsSync(localRollupConfigPath)
     ? localRollupConfigPath
     : defaultRollupConfigPath;
 
-  console.log({ __dirname });
-  console.log(chalk.bgGray('Using Rollup config ', rollupConfigPath));
+  console.log(
+    chalk.bgGray(
+      `Building \`${packageName}\` using rollup config: `,
+      rollupConfigPath,
+    ),
+  );
 
   spawn('rollup', ['--config', rollupConfigPath], {
     cwd: packageDir,
