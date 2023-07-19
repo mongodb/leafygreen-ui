870eb0fdee422f646c944622eba45b65d82bb430
Adam Thompson <adam.thompson@mongodb.com>
2023-07-19 13:04:08 -0400

    build tests

---
+++ tools/build/src/tsdoc/test-package/index.tsx
@@ -0,0 +1,8 @@
+interface TestProps {
+  /** The title for a component */
+  title: string;
+  /** The component description */
+  description?: string;
+}
+
+export const TestComponent = (props: TestProps) => {};
