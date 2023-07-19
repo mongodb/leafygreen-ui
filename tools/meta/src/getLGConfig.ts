8c21cf748358a266723c7dbbc4f644464dcc0fcf
Adam Thompson <adam.thompson@mongodb.com>
2023-07-19 14:40:04 -0400

    Update getLGConfig.ts

--- tools/meta/src/getLGConfig.ts
+++ tools/meta/src/getLGConfig.ts
@@ -5,13 +5,15 @@ export interface LGConfig {
   scopes: Record<string, string>;
 }
 
+export const LGConfigFileName = 'lg.config.json';
+
 export const getLGConfig = (): LGConfig => {
   const rootDir = process.cwd();
-  const lgConfigPath = path.resolve(rootDir, 'lg.config.json');
+  const lgConfigPath = path.resolve(rootDir, LGConfigFileName);
 
   // Check if an lg.json exists
   if (!fs.existsSync(lgConfigPath)) {
-    throw new Error('`lg.json` config file not found');
+    throw new Error(`${LGConfigFileName} file not found`);
   }
 
   const lgConfig = JSON.parse(fs.readFileSync(lgConfigPath, 'utf-8'));
