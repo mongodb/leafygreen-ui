6a48a779da9c96be66d198de28e540cdc990e9e3
Adam Thompson <adam.thompson@mongodb.com>
2023-07-19 18:31:50 -0400

    test basic action

---
+++ tools/actions/src/index.ts
@@ -0,0 +1,16 @@
+import core from '@actions/core';
+import github from '@actions/github';
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = new Date().toTimeString();
+  core.setOutput('time', time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  // @ts-ignore
+  core.setFailed(error.message);
+}
