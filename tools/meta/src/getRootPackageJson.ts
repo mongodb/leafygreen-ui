45f25b272ca84a4d88ebdb5a7fffd84ee7e04966
Adam Thompson <adam.thompson@mongodb.com>
2023-08-08 14:50:40 -0400

    Use fs-extra

--- tools/meta/src/getRootPackageJson.ts
+++ tools/meta/src/getRootPackageJson.ts
@@ -1,10 +1,10 @@
-import fs from 'fs';
+import fse from 'fs-extra';
 import path from 'path';
 
 export function getRootPackageJson(): Record<string, any> | undefined {
   const rootDir = process.cwd();
   const pkgJsonPath = path.resolve(rootDir, 'package.json');
-  const pkgJsonStr = fs.readFileSync(pkgJsonPath, 'utf-8');
+  const pkgJsonStr = fse.readFileSync(pkgJsonPath, 'utf-8');
 
   if (isValidJSON(pkgJsonStr)) {
     return JSON.parse(pkgJsonStr);
