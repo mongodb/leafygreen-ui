9bef0dd694327c63069aa24a61cd7b0ee0d49f76
Adam Thompson <adam.thompson@mongodb.com>
2023-08-08 15:04:50 -0400

    Fixes slackbot tests

--- tools/slackbot/src/release-bot/utils/generateOutputStrings.spec.ts
+++ tools/slackbot/src/release-bot/utils/generateOutputStrings.spec.ts
@@ -8,63 +8,79 @@ describe('tools/slackbot/release', () => {
         version: '1.0.2',
       });
 
-      expect(updateObj).toContainEqual({
-        name: '@leafygreen-ui/example-patch',
-        fullName: '@leafygreen-ui/example-patch@1.0.2',
-        scope: '@leafygreen-ui',
-        shortName: 'example-patch',
-        changelogUrl:
-          'https://github.com/mongodb/leafygreen-ui/blob/main/packages/example-patch/CHANGELOG.md`;',
-        version: '1.0.2',
-      });
+      expect(updateObj).toHaveProperty('name', '@leafygreen-ui/example-patch');
+
+      expect(updateObj).toHaveProperty(
+        'fullName',
+        '@leafygreen-ui/example-patch@1.0.2',
+      );
+      expect(updateObj).toHaveProperty('scope', '@leafygreen-ui');
+      expect(updateObj).toHaveProperty('shortName', 'example-patch');
+      expect(updateObj).toHaveProperty(
+        'changelogUrl',
+        'https://github.com/mongodb/leafygreen-ui/blob/main/packages/example-patch/CHANGELOG.md',
+      );
+      expect(updateObj).toHaveProperty('version', '1.0.2');
     });
+
     test('minor', () => {
       const updateObj = generateOutputStrings({
         name: '@leafygreen-ui/example-minor',
         version: '1.2.0',
       });
 
-      expect(updateObj).toContainEqual({
-        name: '@leafygreen-ui/example-minor',
-        fullName: '@leafygreen-ui/example-minor@1.2.0',
-        scope: '@leafygreen-ui',
-        shortName: 'example-minor',
-        changelogUrl:
-          'https://github.com/mongodb/leafygreen-ui/blob/main/packages/example-minor/CHANGELOG.md`;',
-        version: '1.2.0',
-      });
+      expect(updateObj).toHaveProperty('name', '@leafygreen-ui/example-minor');
+      expect(updateObj).toHaveProperty(
+        'fullName',
+        '@leafygreen-ui/example-minor@1.2.0',
+      );
+      expect(updateObj).toHaveProperty('scope', '@leafygreen-ui');
+      expect(updateObj).toHaveProperty('shortName', 'example-minor');
+      expect(updateObj).toHaveProperty(
+        'changelogUrl',
+        'https://github.com/mongodb/leafygreen-ui/blob/main/packages/example-minor/CHANGELOG.md',
+      );
+      expect(updateObj).toHaveProperty('version', '1.2.0');
     });
+
     test('major', () => {
       const updateObj = generateOutputStrings({
         name: '@leafygreen-ui/example-major',
         version: '2.0.0',
       });
 
-      expect(updateObj).toContainEqual({
-        name: '@leafygreen-ui/example-major',
-        fullName: '@leafygreen-ui/example-major@2.0.0',
-        scope: '@leafygreen-ui',
-        shortName: 'example-major',
-        changelogUrl:
-          'https://github.com/mongodb/leafygreen-ui/blob/main/packages/example-major/CHANGELOG.md`;',
-        version: '2.0.0',
-      });
+      expect(updateObj).toHaveProperty('name', '@leafygreen-ui/example-major');
+      expect(updateObj).toHaveProperty(
+        'fullName',
+        '@leafygreen-ui/example-major@2.0.0',
+      );
+      expect(updateObj).toHaveProperty('scope', '@leafygreen-ui');
+      expect(updateObj).toHaveProperty('shortName', 'example-major');
+      expect(updateObj).toHaveProperty(
+        'changelogUrl',
+        'https://github.com/mongodb/leafygreen-ui/blob/main/packages/example-major/CHANGELOG.md',
+      );
+      expect(updateObj).toHaveProperty('version', '2.0.0');
     });
+
     test('@lg-tools', () => {
       const updateObj = generateOutputStrings({
         name: '@lg-tools/example-tool',
         version: '1.2.0',
       });
 
-      expect(updateObj).toContainEqual({
-        name: '@lg-tools/example-tool',
-        fullName: '@lg-tools/example-tool@1.2.0',
-        scope: '@lg-tools',
-        shortName: 'example-tool',
-        changelogUrl:
-          'https://github.com/mongodb/leafygreen-ui/blob/main/tools/example-tool/CHANGELOG.md`;',
-        version: '1.2.0',
-      });
+      expect(updateObj).toHaveProperty('name', '@lg-tools/example-tool');
+      expect(updateObj).toHaveProperty(
+        'fullName',
+        '@lg-tools/example-tool@1.2.0',
+      );
+      expect(updateObj).toHaveProperty('scope', '@lg-tools');
+      expect(updateObj).toHaveProperty('shortName', 'example-tool');
+      expect(updateObj).toHaveProperty(
+        'changelogUrl',
+        'https://github.com/mongodb/leafygreen-ui/blob/main/tools/example-tool/CHANGELOG.md',
+      );
+      expect(updateObj).toHaveProperty('version', '1.2.0');
     });
   });
 });
