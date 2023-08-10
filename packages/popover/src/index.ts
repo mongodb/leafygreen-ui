11f968ccd425f3e4fa6546c4c8decf64e6a2c76c
Shaneeza <shaneeza.ali@mongodb.com>
2023-08-10 16:02:20 -0400

    remove position utils exports

--- packages/popover/src/index.ts
+++ packages/popover/src/index.ts
@@ -1,10 +1,5 @@
 import { getAlign, getJustify } from './Popover.testutils';
 export { default } from './Popover';
-export {
-  calculatePosition,
-  getElementDocumentPosition,
-  getElementViewportPosition,
-} from './positionUtils';
 export type { PortalControlProps } from './types';
 export type { ElementPosition, PopoverProps } from './types';
 export { Align, Justify } from './types';
