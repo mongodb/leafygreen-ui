f125e6309923f7dfb66e019625139a5c5bf63f9e
Shaneeza <shaneeza.ali@mongodb.com>
2023-08-11 16:05:13 -0400

    remove PropsWithChildren from Logo

--- packages/logo/src/Logo.story.tsx
+++ packages/logo/src/Logo.story.tsx
@@ -130,7 +130,7 @@ LiveExample.parameters = {
 };
 
 const Template = (
-  LogoComponent: React.FunctionComponent<React.PropsWithChildren<LogoProps>>,
+  LogoComponent: React.FunctionComponent<LogoProps>,
   args: LogoProps & { background?: string },
 ) => {
   const containerStyle = css`
