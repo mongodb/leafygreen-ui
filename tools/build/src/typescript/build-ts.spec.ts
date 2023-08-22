8e06bb2029d51e36a8f827d0346b38ba67953062
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2023-08-22 16:01:07 -0400

    `tsc` exit code [LG-3561] (#1940)
    * Update build-ts.ts
    
    * Create gentle-masks-greet.md
    
    * Update build-ts.spec.ts

--- tools/build/src/typescript/build-ts.spec.ts
+++ tools/build/src/typescript/build-ts.spec.ts
@@ -2,9 +2,12 @@ import xSpawn from 'cross-spawn';
 
 import { buildTypescript } from './build-ts';
 type SpawnType = ReturnType<typeof xSpawn.spawn>;
+const onCb = (_e: string) => {};
 
 const spawnSpy = jest.spyOn(xSpawn, 'spawn');
-spawnSpy.mockImplementation((...args) => ({} as SpawnType));
+spawnSpy.mockImplementation(
+  (..._args) => ({ on: onCb } as unknown as SpawnType),
+);
 
 describe('tools/build/build-ts', () => {
   test('runs with no options', () => {
