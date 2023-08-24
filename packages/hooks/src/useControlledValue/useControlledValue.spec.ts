c565f2638902fabe592895c0dcb9a5c1bb61eed6
Adam Thompson <adam.thompson@mongodb.com>
2023-08-24 15:16:06 -0400

    Adds more explicit tests

--- packages/hooks/src/useControlledValue/useControlledValue.spec.ts
+++ packages/hooks/src/useControlledValue/useControlledValue.spec.ts
@@ -1,42 +1,96 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, ChangeEventHandler } from 'react';
 import { act } from 'react-test-renderer';
 import { renderHook } from '@testing-library/react-hooks';
 
 import { useControlledValue } from './useControlledValue';
 
-describe('packages/lib/useControlledValue', () => {
-  test('with controlled component', async () => {
-    const value = 'apple';
-    const handler = jest.fn();
-    const {
-      result: { current },
-    } = renderHook(() => useControlledValue(value as string, handler));
-    expect(current.isControlled).toBe(true);
-    expect(current.value).toBe(value);
+const changeEventMock = {
+  target: { value: 'banana' },
+} as ChangeEvent<any>;
 
-    act(() => {
-      current.handleChange({ target: { value: 'banana' } } as ChangeEvent<any>);
-      current.setUncontrolledValue('banana');
+describe('packages/lib/useControlledValue', () => {
+  describe('with controlled component', () => {
+    test('calling with a value sets value and isControlled', () => {
+      const handler = jest.fn();
+      const { result } = renderHook(v => useControlledValue(v, handler), {
+        initialProps: 'apple',
+      });
+      expect(result.current.isControlled).toBe(true);
+      expect(result.current.value).toBe('apple');
     });
-    expect(handler).toHaveBeenCalledWith(
-      expect.objectContaining({ target: { value: 'banana' } }),
-    );
-    expect(current.value).toBe('apple');
+
+    test('calling with a new value changes the value', () => {
+      const handler = jest.fn();
+      const { result, rerender } = renderHook(
+        v => useControlledValue(v, handler),
+        {
+          initialProps: 'apple',
+        },
+      );
+
+      expect(result.current.value).toBe('apple');
+
+      act(() => {
+        rerender('banana');
+      });
+
+      expect(result.current.value).toBe('banana');
+    });
+
+    test('provided handler should be called', () => {
+      const handler = jest.fn();
+      const { result } = renderHook(v => useControlledValue(v, handler), {
+        initialProps: 'apple',
+      });
+
+      // simulate responding to an event
+      act(() => {
+        result.current.handleChange(changeEventMock);
+      });
+      expect(handler).toHaveBeenCalledWith(
+        expect.objectContaining(changeEventMock),
+      );
+      // value doesn't change unless we explicitly change it
+      expect(result.current.value).toBe('apple');
+    });
   });
 
-  test('with uncontrolled component', async () => {
-    const value = undefined;
-    const handler = jest.fn();
-    const {
-      result: { current },
-    } = renderHook(() => useControlledValue(value, handler));
-    expect(current.isControlled).toBe(false);
-    expect(current.value).toBe('');
+  describe('with uncontrolled component', () => {
+    test('calling without a value sets value and isControlled', () => {
+      const handler = jest.fn();
+      const { result } = renderHook(v => useControlledValue(v, handler), {
+        initialProps: undefined,
+      });
+      expect(result.current.isControlled).toBe(false);
+      expect(result.current.value).toBe(undefined);
+    });
 
-    act(() => {
-      current.handleChange({ target: { value: 'apple' } } as ChangeEvent<any>);
+    test('calling setter updates value', () => {
+      const handler = jest.fn();
+      const { result } = renderHook(
+        v => useControlledValue<string>(v, handler),
+        {
+          initialProps: undefined,
+        },
+      );
+
+      act(() => {
+        result.current.setUncontrolledValue('apple');
+      });
+      expect(result.current.value).toBe('apple');
+    });
+
+    test('provided handler should be called', () => {
+      const handler = jest.fn();
+      const { result } = renderHook(v => useControlledValue(v, handler), {
+        initialProps: undefined,
+      });
+
+      act(() => {
+        result.current.handleChange(changeEventMock);
+      });
       expect(handler).toHaveBeenCalledWith(
-        expect.objectContaining({ target: { value: 'apple' } }),
+        expect.objectContaining(changeEventMock),
       );
     });
   });
