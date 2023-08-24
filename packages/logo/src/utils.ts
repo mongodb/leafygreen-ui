e62bccfff83d90ecbd651de5cb9fd8b5b0c44905
Shaneeza <shaneeza.ali@mongodb.com>
2023-08-24 11:27:42 -0400

    update color default

--- packages/logo/src/utils.ts
+++ packages/logo/src/utils.ts
@@ -9,7 +9,7 @@ const SupportedColors = {
   GreenBase: 'green-base',
 } as const;
 
-type SupportedColors = typeof SupportedColors[keyof typeof SupportedColors];
+type SupportedColors = (typeof SupportedColors)[keyof typeof SupportedColors];
 
 const SupportedColorsMap: Record<SupportedColors, string> = {
   [SupportedColors.White]: palette.white,
@@ -19,7 +19,7 @@ const SupportedColorsMap: Record<SupportedColors, string> = {
 } as const;
 
 type SupportedColorsMap =
-  typeof SupportedColorsMap[keyof typeof SupportedColorsMap];
+  (typeof SupportedColorsMap)[keyof typeof SupportedColorsMap];
 
 export { SupportedColors, SupportedColorsMap };
 
@@ -27,7 +27,7 @@ export interface LogoProps extends HTMLElementProps<'svg'> {
   /**
    * Determines Color of the Logo or LogoMark component.
    *
-   * @default 40
+   * @default 'green-dark-2'
    */
   color?: SupportedColors;
 
