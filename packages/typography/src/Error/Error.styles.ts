52efdbccd1c5f6075618bb6cf03fb0bf48e73c54
Adam Thompson <adam.thompson@mongodb.com>
2023-08-25 14:49:44 -0400

    updates error message

--- packages/typography/src/Error/Error.styles.ts
+++ packages/typography/src/Error/Error.styles.ts
@@ -1,7 +1,12 @@
 import { css } from '@leafygreen-ui/emotion';
 import { Theme } from '@leafygreen-ui/lib';
 import { palette } from '@leafygreen-ui/palette';
-import { fontFamilies, fontWeights } from '@leafygreen-ui/tokens';
+import {
+  BaseFontSize,
+  fontFamilies,
+  fontWeights,
+  typeScales,
+} from '@leafygreen-ui/tokens';
 
 export const errorMessageStyle = css`
   font-family: ${fontFamilies.default};
@@ -20,5 +25,16 @@ export const errorMessageModeStyle: Record<Theme, string> = {
   `,
   [Theme.Dark]: css`
     color: ${palette.red.light1};
+  `,
+};
+
+export const errorMessageTypeScaleStyles: Record<BaseFontSize, string> = {
+  [BaseFontSize.Body1]: css`
+    font-size: ${typeScales.body1.fontSize}px;
+    line-height: ${typeScales.body1.lineHeight}px;
+  `,
+  [BaseFontSize.Body2]: css`
+    font-size: ${typeScales.body2.fontSize}px;
+    line-height: 20px; // Hardcoding because it does not match body2 lineHeight
   `,
 };
