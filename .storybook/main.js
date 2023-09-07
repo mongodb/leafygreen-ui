2f912a489fdb2c8c929f54c4777658573dd2c51f
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-09-07 14:42:40 -0400

    adding singletons
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,72 +1,78 @@
 const path = require('path');
-const {withStorybookModuleFederation} = require('storybook-module-federation');
+const {
+  withStorybookModuleFederation,
+} = require('storybook-module-federation');
 const CopyPlugin = require('copy-webpack-plugin');
 const storybookConfig = {
-    framework: "@storybook/web-components-webpack5",
-    stories: [
-        "../stories/*.stories.mdx",
-        "../packages/**/*.stories.@(js|jsx|ts|tsx)"
-    ],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/preset-scss",
-        "@storybook/theming"
-    ],
-    core: {
-        builder: "webpack5",
-        options: {
-            fsCache: false,
-        }
+  framework: '@storybook/web-components-webpack5',
+  stories: [
+    '../stories/*.stories.mdx',
+    '../packages/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
+    '@storybook/theming',
+  ],
+  core: {
+    builder: 'webpack5',
+    options: {
+      fsCache: false,
     },
-    features: {
-        storyStoreV7: false,
-    },
-    webpackFinal: async (config, {configType}) => {
-        config.output.filename = "dtk/"+config.output.filename;
-        const webComponentsRule = config.module.rules.find(
-            (rule) => rule.use && rule.use.options && rule.use.options.babelrc === true
-        );
-        
-        config.module.rules.push({
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            use: [{loader: 'url-loader'}],
-            include: path.resolve(__dirname, '../', 'node_modules'),
-        })
+  },
+  features: {
+    storyStoreV7: false,
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.output.filename = 'dtk/' + config.output.filename;
+    const webComponentsRule = config.module.rules.find(
+      rule => rule.use && rule.use.options && rule.use.options.babelrc === true,
+    );
 
-        config.plugins.push(
-            new CopyPlugin({
-                patterns: [
-                    { 
-                        from: path.resolve(__dirname, '../src/assets'),
-                        to: path.resolve(__dirname, '../storybook-static/assets'),
-                    }
-                ],
-            })
-        );
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: [{ loader: 'url-loader' }],
+      include: path.resolve(__dirname, '../', 'node_modules'),
+    });
 
-        config.module.rules.push({
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader',
-            ],
-            include: path.resolve(__dirname, '../', 'node_modules'),
-        });
-        return config;
-    }
-}
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, '../src/assets'),
+            to: path.resolve(__dirname, '../storybook-static/assets'),
+          },
+        ],
+      }),
+    );
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../', 'node_modules'),
+    });
+    return config;
+  },
+};
 
 const storybookModuleFederationConfig = {
-    name: 'storybook',
-    filename: 'dtk/remoteEntry.js',
-    exposes: {
-        './BaseStyles': '/src/assets/global.css',
-        './Button': '/packages/button/src/Button/Button.tsx'
-    }
-}
+  name: 'storybook',
+  filename: 'dtk/remoteEntry.js',
+  exposes: {
+    './BaseStyles': '/src/assets/global.css',
+    './Button': '/packages/button/src/Button/Button.tsx',
+  },
+  shared: {
+    react: {
+      singleton: true,
+    },
+    'react-dom': {
+      singleton: true,
+    },
+  },
+};
 
-module.exports = withStorybookModuleFederation(storybookModuleFederationConfig) (
-    storybookConfig
+module.exports = withStorybookModuleFederation(storybookModuleFederationConfig)(
+  storybookConfig,
 );
