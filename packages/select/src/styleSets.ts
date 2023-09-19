2c34caf5ae389c2eed552de5fc4bfb68dd6fbdab
Shaneeza <shaneeza.ali@mongodb.com>
2023-09-19 13:47:05 -0400

    remove css

--- packages/select/src/styleSets.ts
+++ packages/select/src/styleSets.ts
@@ -1,6 +1,5 @@
 import { transparentize } from 'polished';
 
-import { css } from '@leafygreen-ui/emotion';
 import { Theme } from '@leafygreen-ui/lib';
 import { palette } from '@leafygreen-ui/palette';
 
