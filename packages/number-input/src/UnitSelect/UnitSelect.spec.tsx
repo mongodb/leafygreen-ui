c5d957949bdcbddd295760cc315d64404a36a1f5
Shaneeza <shaneeza.ali@mongodb.com>
2023-09-29 14:49:23 -0400

    LG-3551, LG-2654: select option descriptions + package reorganization (#1991)
    * reorganize select
    
    * refactor to use inputOption
    
    * more updates
    
    * fix failing test
    
    * fix failing tests
    
    * update test
    
    * comments
    
    * changesets
    
    * update story
    
    * add internal tag
    
    * fix generated story context
    
    * robs line-height comment
    
    * sean feedback
    
    * use leafygreen provider for darkmode
    
    * update readme
    
    * update README and update changeset

--- packages/number-input/src/UnitSelect/UnitSelect.spec.tsx
+++ packages/number-input/src/UnitSelect/UnitSelect.spec.tsx
@@ -46,19 +46,12 @@ describe('packages/number-input/select', () => {
     expect(getByRole('listbox')).toBeInTheDocument();
   });
 
-  test('options displayed in select are based on the languageOptions prop', () => {
+  test('options displayed in select are based on the unitOptions prop', () => {
     const { getByRole } = renderSelect({ ...selectProps });
     const trigger = getByRole('button', { name: selectProps.unit });
     fireEvent.click(trigger);
 
-    // First option has a checkmark next to it
-    selectProps.unitOptions.slice(0, 1).forEach(lang => {
-      expect(
-        getByRole('option', { name: `Checkmark Icon ${lang.displayName}` }),
-      ).toBeInTheDocument();
-    });
-
-    selectProps.unitOptions.slice(1).forEach(lang => {
+    selectProps.unitOptions.forEach(lang => {
       expect(
         getByRole('option', { name: lang.displayName }),
       ).toBeInTheDocument();
