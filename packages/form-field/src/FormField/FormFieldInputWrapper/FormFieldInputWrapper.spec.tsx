d3e543ac4858d3bb825da6d54709a091049ff4c3
Adam Thompson <adam.thompson@mongodb.com>
2023-09-29 18:54:12 -0400

    lint

--- packages/form-field/src/FormField/FormFieldInputWrapper/FormFieldInputWrapper.spec.tsx
+++ packages/form-field/src/FormField/FormFieldInputWrapper/FormFieldInputWrapper.spec.tsx
@@ -1,11 +1,8 @@
-
 import React from 'react';
 import { render } from '@testing-library/react';
 
 import { FormFieldInputWrapper } from '.';
 
 describe('packages/form-field-input-wrapper', () => {
-  test('condition', () => {
-
-  })
-})
+  test('condition', () => {});
+});
