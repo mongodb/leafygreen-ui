ddd9712f19be1edcf8a79aaa3495c9d8669681c2
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2023-10-11 12:59:15 -0400

    resolves console errors in test suite (#2017)

--- typings/images.d.ts
+++ typings/images.d.ts
@@ -12,3 +12,10 @@ declare module '*.gif' {
   const value: string;
   export = value;
 }
+
+declare module '*.svg' {
+  const content: {
+    svg: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  };
+  export default content;
+}
