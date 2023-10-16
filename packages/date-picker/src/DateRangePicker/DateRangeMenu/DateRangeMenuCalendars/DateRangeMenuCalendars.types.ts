a4267237f1b0617f2b481fe99da8da2651682a45
Adam Thompson <adam.thompson@mongodb.com>
2023-10-16 17:45:30 -0400

    Update DateRangeMenuCalendars.types.ts

--- packages/date-picker/src/DateRangePicker/DateRangeMenu/DateRangeMenuCalendars/DateRangeMenuCalendars.types.ts
+++ packages/date-picker/src/DateRangePicker/DateRangeMenu/DateRangeMenuCalendars/DateRangeMenuCalendars.types.ts
@@ -1,10 +1,12 @@
 import { DynamicRefGetter } from '@leafygreen-ui/hooks';
+import { HTMLElementProps } from '@leafygreen-ui/lib';
 
 import { DateType } from '../../../types';
 import { DateRangeMenuProps } from '../DateRangeMenu.types';
 
 export interface DateRangeMenuCalendarsProps
-  extends Pick<DateRangeMenuProps, 'value' | 'setValue' | 'handleValidation'> {
+  extends Pick<DateRangeMenuProps, 'value' | 'setValue' | 'handleValidation'>,
+    HTMLElementProps<'div'> {
   highlight: DateType;
   setHighlight: React.Dispatch<React.SetStateAction<DateType>>;
   cellRefs: DynamicRefGetter<HTMLTableCellElement>;
