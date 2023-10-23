72a1f7265cb5f1e90f211593c12c49e25c29b80d
Adam Thompson <adam.thompson@mongodb.com>
2023-10-23 12:45:03 -0400

    adds getPackageJson util

--- tools/meta/src/index.ts
+++ tools/meta/src/index.ts
@@ -1,5 +1,6 @@
 export { getAllPackageNames, getAllPackages } from './getAllPackages';
 export { getLGConfig, type LGConfig } from './getLGConfig';
+export { getPackageJson } from './getPackageJson';
 export { getPackageManager } from './getPackageManager';
 export { getPackageName } from './getPackageName';
 export { getRootPackageJson, isValidJSON } from './getRootPackageJson';
