ad0b5112764f2631dbf6bf4fe438a70a65de1d91
Adam Thompson <adam.thompson@mongodb.com>
2023-10-26 14:02:40 -0400

    Update createNewMatcher.ts

--- tools/jest-matchers/scripts/createNewMatcher.ts
+++ tools/jest-matchers/scripts/createNewMatcher.ts
@@ -22,25 +22,39 @@ function createNewMatcher(matcherName: string) {
   }
 
   const matchersDir = path.resolve(__dirname, '../src/matchers');
-  const matchersFilePath = path.resolve(matchersDir, matcherName + '.ts');
 
+  /** Create the matcher file */
+  const matchersFilePath = path.resolve(matchersDir, matcherName + '.ts');
   const matcherFileTemplate = `
 import { createMatcher } from '../utils/createMatcher';
 
-export const ${matcherName} = createMatcher(function _${matcherName}(element: Element) {
+export const ${matcherName} = createMatcher(function _${matcherName}() {
   return {
     pass: true,
     message: () => '',
   };
 });
 `;
-
   fse.writeFileSync(matchersFilePath, matcherFileTemplate);
 
-  const indexFilePath = path.resolve(matchersDir, 'index.ts');
+  /** Create the test file */
 
+  const testFilePath = path.resolve(
+    __dirname,
+    '../src/tests',
+    matcherName + '.spec.ts',
+  );
+
+  const testFileTemplate = `
+describe('tools/jest-matchers/.${matcherName}', () => {
+  test.todo('')
+})
+`;
+  fse.writeFileSync(testFilePath, testFileTemplate);
+
+  /** Update the matchers index file */
+  const indexFilePath = path.resolve(matchersDir, 'index.ts');
   let indexContents = fse.readFileSync(indexFilePath, 'utf-8');
   indexContents += `export { ${matcherName} } from './${matcherName}';\n`;
-
   fse.writeFileSync(indexFilePath, indexContents);
 }
