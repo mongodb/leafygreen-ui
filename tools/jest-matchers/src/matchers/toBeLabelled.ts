b2159ad69ec13618a73fc016c02915915f78bd84
Adam Thompson <adam.thompson@mongodb.com>
2023-10-26 13:51:42 -0400

    creates `toBeLabelled` matcher

--- tools/jest-matchers/src/matchers/toBeLabelled.ts
+++ tools/jest-matchers/src/matchers/toBeLabelled.ts
@@ -1,8 +1,52 @@
+import { isNull } from 'lodash';
 import { createMatcher } from '../utils/createMatcher';
 
-export const toBeLabelled = createMatcher((element: Element) => {
+const isValidString = (str: any): str is string =>
+  str && typeof str === 'string' && str.length > 0;
+
+/**
+ * Returns whether the provided element has any of the following:
+ * - a `label` attribute
+ * - an `aria-label` attribute
+ * - an `aria-labelledby` attribute with a valid associated element
+ * - an associated element `<label>` element with the appropriate `for` attribute
+ */
+export const toBeLabelled = createMatcher(function (element: Element) {
+  const label = element.getAttribute('label');
+  const ariaLabel = element.getAttribute('aria-label');
+  const ariaLabelledBy = element.getAttribute('aria-labelledby');
+  const elementId = element.getAttribute('id');
+
+  // If at least one...
+  const hasStaticLabel = [label, ariaLabel].some(isValidString);
+
+  const hasLabelReference = isValidString(ariaLabelledBy);
+  const doesAriaLabelledByReferenceExist =
+    hasLabelReference && !isNull(document.querySelector(`#${ariaLabelledBy}`));
+  const hasLabelForId =
+    elementId && !isNull(document.querySelector(`label[for="${elementId}"]`));
+
+  const pass = Boolean(
+    hasStaticLabel || doesAriaLabelledByReferenceExist || hasLabelForId,
+  );
+
+  const message = () => {
+    if (!pass && hasLabelReference) {
+      return this.isNot
+        ? `Expected to not find element with id ${ariaLabelledBy}`
+        : `Could not find element referenced by \`aria-labelledby\`` +
+            document.body.innerHTML;
+    }
+    if (!pass && elementId) {
+      return this.isNot
+        ? `Expected not to find label for ${elementId}`
+        : `Could not find a label for the element with id ${elementId}`;
+    }
+    return `Expected the element ${this.isNot ? 'not ' : ''}to have a label`;
+  };
+
   return {
-    pass: true,
-    message: () => '',
+    pass,
+    message,
   };
 });
---
+++ tools/jest-matchers/src/tests/toBeLabelled.spec.ts
@@ -0,0 +1,48 @@
+import { render } from './utils/test-utils';
+
+describe('tools/jest-matchers/.toBeLabelled', () => {
+  test('unlabelled', () => {
+    const { queryByTestId } = render(`
+      <input type="text" data-testid="unlabelled" />
+    `);
+    expect(queryByTestId('unlabelled')).not.toBeLabelled();
+  });
+
+  test('`label` attribute', () => {
+    const { queryByTestId } = render(`
+      <input label="label" type="text" data-testid="labelled" />
+    `);
+
+    expect(queryByTestId('labelled')).toBeLabelled();
+  });
+
+  test('`aria-label` attribute', () => {
+    const { queryByTestId } = render(`
+      <input aria-label="label" type="text" data-testid="labelled" />
+    `);
+
+    expect(queryByTestId('labelled')).toBeLabelled();
+  });
+
+  test('`aria-labelledby` attribute', () => {
+    const { queryByTestId } = render(`
+      <label id="label-id">Text</label>
+      <input aria-labelledby="label-id" type="text" data-testid="labelled" />
+      <input aria-labelledby="invalid" type="text" data-testid="unlabelled" />
+    `);
+
+    expect(queryByTestId('labelled')).toBeLabelled();
+    expect(queryByTestId('unlabelled')).not.toBeLabelled();
+  });
+
+  test('`for` attribute', () => {
+    const { queryByTestId } = render(`
+      <label for="input-id">Text</label>
+      <input id="input-id" type="text" data-testid="labelled" />
+      <input id="invalid" type="text" data-testid="unlabelled" />
+    `);
+
+    expect(queryByTestId('labelled')).toBeLabelled();
+    expect(queryByTestId('unlabelled')).not.toBeLabelled();
+  });
+});
