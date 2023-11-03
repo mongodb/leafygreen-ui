4f28a550de1bb90ae570222b9ed94d785242c66f
Shaneeza <shaneeza.ali@mongodb.com>
2023-11-03 13:50:30 -0400

    update types

--- packages/date-picker/src/shared/types.ts
+++ packages/date-picker/src/shared/types.ts
@@ -59,7 +59,7 @@ export interface BaseDatePickerProps extends DarkModeProps {
   baseFontSize?: BaseFontSize;
 
   /**
-   * Whether the input is disabled. Note: will not set the `disabled` attribute on an input
+   * Whether the input is disabled. The calendar menu will not open if disabled is true. Note: will not set the `disabled` attribute on an input
    */
   disabled?: boolean;
 
@@ -76,6 +76,6 @@ export interface BaseDatePickerProps extends DarkModeProps {
    */
   errorMessage?: string;
 
-  /** Whether the calendar menu is initially open */
+  /** Whether the calendar menu is initially open. Will not open if disabled is initially set to true.  */
   initialOpen?: boolean;
 }
