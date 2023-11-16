8d55f83880c5850b1a3b490ab21be5d9188451f3
spark33 <seanpark1107@gmail.com>
2023-11-16 15:10:24 -0500

    add console warn

--- packages/table/src/Row/InternalRowWithoutRT.tsx
+++ packages/table/src/Row/InternalRowWithoutRT.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { cx } from '@leafygreen-ui/emotion';
 import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
+import { consoleOnce, isComponentType } from '@leafygreen-ui/lib';
 
 import { useTableContext } from '../TableContext/TableContext';
 
@@ -19,6 +20,14 @@ const InternalRowWithoutRT = ({
 }: InternalRowBaseProps) => {
   const { shouldAlternateRowColor } = useTableContext();
   const { theme } = useDarkMode();
+
+  React.Children.forEach(children, child => {
+    if (!isComponentType(child, 'Cell'))
+      consoleOnce.warn(
+        'LG Row is rendering a custom cell element. Utilize the `Cell` component for standardized styles, correct HTML properties and additional functionalities when using `useLeafyGreenTable`.',
+      );
+  });
+
   return (
     <InternalRowBase
       className={cx(
