b5b8c9c5ecfe757d3cce8133c7762a7f51963a1e
Adam Thompson <adam.thompson@mongodb.com>
2023-11-21 11:26:03 -0800

    resolutions

--- tools/test/scripts/install-react17.ts
+++ tools/test/scripts/install-react17.ts
@@ -1,8 +1,10 @@
-import { isValidJSON } from '@lg-tools/meta';
+import { getRootPackageJson, isValidJSON } from '@lg-tools/meta';
 import { sync as spawnSync } from 'cross-spawn';
 import fse from 'fs-extra';
 import path from 'path';
+const rootDir = process.cwd();
 
+const pkgJson = getRootPackageJson();
 const r17packagesFile = path.resolve(
   __dirname,
   '../config/react17/r17-packages.json',
@@ -10,15 +12,32 @@ const r17packagesFile = path.resolve(
 
 const r17packagesString = fse.readFileSync(r17packagesFile, 'utf-8');
 
-if (isValidJSON(r17packagesString)) {
-  const r17packages = JSON.parse(r17packagesString);
-  const dependencies = r17packages.dependencies;
-
-  const installString = Object.entries(dependencies)
-    .map(([pkg, version]) => `${pkg}@${version}`)
-    .join(' ');
-
-  spawnSync('yarn', ['add', '-WD', installString], {
-    stdio: 'inherit',
-  });
+if (!pkgJson) {
+  throw new Error(`Could not find root package.json`);
 }
+
+if (!isValidJSON(r17packagesString)) {
+  throw new Error(`Invalid JSON found in ${r17packagesFile}`);
+}
+
+const r17packages = JSON.parse(r17packagesString);
+
+// Add resolutions
+pkgJson.resolutions = {
+  ...pkgJson.resolutions,
+  ...r17packages.resolutions,
+};
+
+const pkgJsonPath = path.resolve(rootDir, 'package.json');
+fse.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
+
+// Install dependencies
+const packagesToInstall = Object.entries(r17packages.dependencies).map(
+  ([pkg, version]) => `${pkg}@${version}`,
+);
+
+console.log(packagesToInstall);
+
+spawnSync('yarn', ['add', '-WD', ...packagesToInstall], {
+  stdio: 'inherit',
+});
