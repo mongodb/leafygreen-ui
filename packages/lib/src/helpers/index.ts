1b3e891fc6847cf423f9d601fc98baae9e7eefae
Adam Thompson <adam.thompson@mongodb.com>
2023-11-29 14:13:57 -0500

    truncateStart

--- packages/lib/src/helpers/index.ts
+++ packages/lib/src/helpers/index.ts
@@ -2,3 +2,4 @@ export { pickAndOmit } from './pickAndOmit';
 export { consoleOnce } from './consoleOnce';
 export { allEqual } from './allEqual';
 export { rollover } from './rollover';
+export { truncateStart } from './truncateStart';
