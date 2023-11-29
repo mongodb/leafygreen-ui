e5e90a8aef52cfdb15cee9ab6aac47c6388c3a6c
Shaneeza <shaneeza.ali@mongodb.com>
2023-11-29 16:16:32 -0500

    Date Picker [LG-3811] autocomplete (#2099)
    * add autoComplete prop
    
    * update types
    
    * add default
    
    * fix errors
    
    * add bday to autoComplete
    
    * use type
    
    * use type

---
+++ packages/date-picker/src/shared/utils/getAutoComplete/index.ts
@@ -0,0 +1,22 @@
+import { DateSegment } from '../../hooks';
+import { AutoComplete } from '../../types';
+
+export const getAutoComplete = (
+  autoComplete: AutoComplete,
+  segment: DateSegment,
+) => {
+  if (autoComplete === AutoComplete.Bday) {
+    switch (segment) {
+      case DateSegment.Day:
+        return 'bday-day';
+
+      case DateSegment.Month:
+        return 'bday-month';
+
+      case DateSegment.Year:
+        return 'bday-year';
+    }
+  }
+
+  return autoComplete;
+};
