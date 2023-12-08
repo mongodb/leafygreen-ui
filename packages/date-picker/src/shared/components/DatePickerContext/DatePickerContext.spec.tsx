c6038e3af325062b71e59bd8a666388eab732104
Adam Thompson <adam.thompson@mongodb.com>
2023-12-08 12:02:21 -0500

    adds sorting min/max tests

--- packages/date-picker/src/shared/components/DatePickerContext/DatePickerContext.spec.tsx
+++ packages/date-picker/src/shared/components/DatePickerContext/DatePickerContext.spec.tsx
@@ -2,19 +2,25 @@ import React, { PropsWithChildren } from 'react';
 import { act, waitFor } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
 
+import { MAX_DATE, MIN_DATE, Month } from '../../constants';
+import { newUTC } from '../../utils';
+
 import {
   DatePickerContextProps,
   DatePickerProvider,
+  DatePickerProviderProps,
   useDatePickerContext,
 } from '.';
 
-const renderDatePickerProvider = () => {
+const renderDatePickerProvider = (props?: Partial<DatePickerProviderProps>) => {
   const { result, rerender } = renderHook<
     PropsWithChildren<{}>,
     DatePickerContextProps
   >(useDatePickerContext, {
     wrapper: ({ children }) => (
-      <DatePickerProvider label="">{children}</DatePickerProvider>
+      <DatePickerProvider label="" {...props}>
+        {children}
+      </DatePickerProvider>
     ),
   });
 
@@ -23,6 +29,58 @@ const renderDatePickerProvider = () => {
 
 describe('packages/date-picker-context', () => {
   describe('useDatePickerContext', () => {
+    describe('min/max', () => {
+      test('uses default min/max values when not provided', () => {
+        const { result } = renderDatePickerProvider();
+        expect(result.current.min).toEqual(MIN_DATE);
+        expect(result.current.max).toEqual(MAX_DATE);
+      });
+
+      test('sets context to provided min/max values', () => {
+        const testMin = newUTC(1999, Month.September, 2);
+        const testMax = newUTC(2011, Month.June, 22);
+
+        const { result } = renderDatePickerProvider({
+          min: testMin,
+          max: testMax,
+        });
+        expect(result.current.min).toEqual(testMin);
+        expect(result.current.max).toEqual(testMax);
+      });
+
+      test('sorts provided min/max values', () => {
+        const testMin = newUTC(1999, Month.September, 2);
+        const testMax = newUTC(2011, Month.June, 22);
+
+        const { result } = renderDatePickerProvider({
+          min: testMax,
+          max: testMin,
+        });
+        expect(result.current.min).toEqual(testMin);
+        expect(result.current.max).toEqual(testMax);
+      });
+
+      test('sorts min/max if provided max is before default min', () => {
+        const testMax = newUTC(1967, Month.March, 10);
+
+        const { result } = renderDatePickerProvider({
+          max: testMax,
+        });
+        expect(result.current.min).toEqual(testMax);
+        expect(result.current.max).toEqual(MIN_DATE);
+      });
+
+      test('sorts min/max if provided min is after default max', () => {
+        const testMin = newUTC(2067, Month.March, 10);
+
+        const { result } = renderDatePickerProvider({
+          min: testMin,
+        });
+        expect(result.current.min).toEqual(MAX_DATE);
+        expect(result.current.max).toEqual(testMin);
+      });
+    });
+
     describe('isOpen', () => {
       test('is `false` by default', () => {
         const { result } = renderDatePickerProvider();
