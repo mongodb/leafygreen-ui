79020b7657516768906e63bab048a2768073c744
Adam Thompson <adam.thompson@mongodb.com>
2023-12-11 16:25:03 -0500

    updates mockTimeZone to reset properly

--- packages/date-picker/src/shared/testutils/mockTimeZone/index.ts
+++ packages/date-picker/src/shared/testutils/mockTimeZone/index.ts
@@ -1,3 +1,4 @@
+import { padStart } from 'lodash';
 import timezoneMock, { TimeZone } from 'timezone-mock';
 
 /**
@@ -14,49 +15,105 @@ export const mockTimeZone = (timeZone: string, UTCOffset: number) => {
     },
   );
 
-  const realDTFOptions = Intl.DateTimeFormat().resolvedOptions();
+  const DTFResolved = Intl.DateTimeFormat().resolvedOptions();
 
   /** DTF.resolvedOptions */
-  global.Intl.DateTimeFormat.prototype.resolvedOptions = jest
-    .fn()
+  jest
+    .spyOn(global.Intl.DateTimeFormat.prototype, 'resolvedOptions')
     .mockImplementation(() => ({
-      ...realDTFOptions,
+      ...DTFResolved,
       timeZone,
     }));
 
   /** getTimezoneOffset */
-  global.Date.prototype.getTimezoneOffset = jest
-    .fn()
+  jest
+    .spyOn(global.Date.prototype, 'getTimezoneOffset')
     .mockImplementation(() => UTCOffset * 60);
 
   /** getDate */
-  global.Date.prototype.getDate = jest
-    .fn()
-    .mockImplementation(function getDate() {
-      /// @ts-expect-error - typeof `this` is unknown
-      const utcDate: number = (this as Date).getUTCDate();
-      /// @ts-expect-error - typeof `this` is unknown
-      const utcHrs: number = (this as Date).getUTCHours();
-
-      const adjustedHr = utcHrs + UTCOffset;
-      const daysOffset = adjustedHr >= 24 ? 1 : adjustedHr < 0 ? -1 : 0;
-      return utcDate + daysOffset;
-    });
+  jest.spyOn(global.Date.prototype, 'getDate').mockImplementation(mockGetDate);
 
   /** getHours */
-  global.Date.prototype.getHours = jest
-    .fn()
-    .mockImplementation(function getHours() {
-      /// @ts-expect-error - typeof `this` is unknown
-      const utcHrs: number = (this as Date).getUTCHours();
-      const adjustedHrs = utcHrs + UTCOffset;
-      const hours =
-        adjustedHrs >= 24
-          ? adjustedHrs % 24
-          : adjustedHrs < 0
-          ? adjustedHrs + 24
-          : adjustedHrs;
+  jest
+    .spyOn(global.Date.prototype, 'getHours')
+    .mockImplementation(mockGetHours);
 
-      return hours;
-    });
+  jest
+    .spyOn(global.Date.prototype, 'toLocaleDateString')
+    .mockImplementation(mockToLocaleDateString);
+
+  jest
+    .spyOn(global.Date.prototype, 'toLocaleTimeString')
+    .mockImplementation(mockToLocaleTimeString);
+
+  jest
+    .spyOn(global.Date.prototype, 'toLocaleString')
+    .mockImplementation(mockToLocaleString);
+
+  /**
+   * MOCK FUNCTIONS
+   * */
+  function mockGetDate() {
+    /// @ts-expect-error - typeof `this` is unknown
+    const utcDate: number = (this as Date).getUTCDate();
+    /// @ts-expect-error - typeof `this` is unknown
+    const utcHrs: number = (this as Date).getUTCHours();
+
+    const adjustedHr = utcHrs + UTCOffset;
+    const daysOffset = adjustedHr >= 24 ? 1 : adjustedHr < 0 ? -1 : 0;
+    return utcDate + daysOffset;
+  }
+
+  function mockGetHours() {
+    /// @ts-expect-error - typeof `this` is unknown
+    const utcHrs: number = (this as Date).getUTCHours();
+    const adjustedHrs = utcHrs + UTCOffset;
+    const hours =
+      adjustedHrs >= 24
+        ? adjustedHrs % 24
+        : adjustedHrs < 0
+        ? adjustedHrs + 24
+        : adjustedHrs;
+
+    return hours;
+  }
+
+  function mockToLocaleDateString() {
+    /// @ts-expect-error - typeof `this` is unknown
+    const _this = this as Date;
+
+    return (
+      String(_this.getMonth() + 1) +
+      '/' +
+      String(mockGetDate.call(_this)) +
+      '/' +
+      String(_this.getFullYear())
+    );
+  }
+
+  function mockToLocaleTimeString() {
+    /// @ts-expect-error - typeof `this` is unknown
+    const _this = this as Date;
+    const hrs = mockGetHours.call(_this);
+    return (
+      String(hrs % 12) +
+      ':' +
+      padStart(String(_this.getMinutes()), 2, '0') +
+      ':' +
+      padStart(String(_this.getSeconds()), 2, '0') +
+      ' ' +
+      String(hrs > 12 ? 'PM' : 'AM')
+    );
+  }
+
+  function mockToLocaleString() {
+    /// @ts-expect-error - typeof `this` is unknown
+    const _this = this as Date;
+
+    return (
+      mockToLocaleDateString.call(_this) +
+      ', ' +
+      mockToLocaleTimeString.call(_this)
+    );
+  }
 };
