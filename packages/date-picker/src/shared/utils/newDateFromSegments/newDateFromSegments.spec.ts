64f42abeacdd6e8bee131a853b8ef8a32502f66e
Adam Thompson <adam.thompson@mongodb.com>
2023-12-20 17:06:38 -0500

    updates newDateFromSegments

--- packages/date-picker/src/shared/utils/newDateFromSegments/newDateFromSegments.spec.ts
+++ packages/date-picker/src/shared/utils/newDateFromSegments/newDateFromSegments.spec.ts
@@ -1,59 +1,85 @@
+import { isInvalidDateObject, Month, newUTC } from '@leafygreen-ui/date-utils';
+
 import { newDateFromSegments } from '.';
 
-describe('packages/date=picker/utils/newDateFromSegments', () => {
-  test('returns the date in UTC', () => {
-    const newDate = newDateFromSegments({
-      day: '1',
-      month: '1',
-      year: '2023',
+describe('packages/date-picker/utils/newDateFromSegments', () => {
+  describe('valid dates', () => {
+    test('returns the date in UTC', () => {
+      const newDate = newDateFromSegments({
+        day: '01',
+        month: '01',
+        year: '2023',
+      });
+      expect(newDate).toEqual(newUTC(2023, Month.January, 1));
     });
-    expect(newDate?.toISOString()).toEqual('2023-01-01T00:00:00.000Z');
+
+    test('returns a date outside the default range', () => {
+      const newDate = newDateFromSegments({
+        day: '01',
+        month: '01',
+        year: '2100',
+      });
+      expect(newDate).toEqual(newUTC(2100, Month.January, 1));
+    });
+
+    test('returns a date if day segment is truncated', () => {
+      const newDate = newDateFromSegments({
+        day: '1',
+        month: '01',
+        year: '2023',
+      });
+      expect(newDate).toEqual(newUTC(2023, Month.January, 1));
+    });
+
+    test('returns a date if month segment is truncated', () => {
+      const newDate = newDateFromSegments({
+        day: '01',
+        month: '1',
+        year: '2023',
+      });
+      expect(newDate).toEqual(newUTC(2023, Month.January, 1));
+    });
   });
 
-  test('returns a date outside the default range', () => {
-    const newDate = newDateFromSegments({
-      day: '1',
-      month: '1',
-      year: '2100',
+  describe('null', () => {
+    test('returns `null` if all segments are empty', () => {
+      const newDate = newDateFromSegments({
+        day: '',
+        month: '',
+        year: '',
+      });
+      expect(newDate).toBeNull();
     });
-    expect(newDate?.toISOString()).toEqual('2100-01-01T00:00:00.000Z');
   });
 
-  test('returns undefined if year is truncated (2-digits)', () => {
-    const newDate = newDateFromSegments({
-      day: '1',
-      month: '1',
-      year: '20',
-    });
-    expect(newDate).toBeUndefined();
-  });
-
-  test('returns undefined if year is truncated (3-digits)', () => {
-    const newDate = newDateFromSegments({
-      day: '1',
-      month: '1',
-      year: '199',
-    });
-    expect(newDate).toBeUndefined();
-  });
-
-  test('returns undefined if month/day combo is invalid', () => {
-    const newDate = newDateFromSegments({
-      day: '31',
-      month: '02',
-      year: '2024',
+  describe('Invalid', () => {
+    test('returns "Invalid Date" if year segment is truncated', () => {
+      const newDate = newDateFromSegments({
+        day: '01',
+        month: '01',
+        year: '20',
+      });
+      expect(isInvalidDateObject(newDate)).toBe(true);
     });
 
-    expect(newDate).toBeUndefined();
-  });
+    test('returns "Invalid Date" if month/day combo is invalid', () => {
+      const newDate = newDateFromSegments({
+        day: '31',
+        month: '02',
+        year: '2024',
+      });
 
-  test('returns undefined if any segment is empty', () => {
-    const newDate = newDateFromSegments({
-      day: '',
-      month: '1',
-      year: '2023',
+      expect(isInvalidDateObject(newDate)).toBe(true);
     });
 
-    expect(newDate).toBeUndefined();
+    test('returns "Invalid Date" if any segment is empty', () => {
+      const newDate = newDateFromSegments({
+        day: '',
+        month: '1',
+        year: '2023',
+      });
+
+      expect(isInvalidDateObject(newDate)).toBe(true);
+    });
   });
 });
