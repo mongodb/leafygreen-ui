5adb021569f006ae9f199a7c712cd51859cbf064
Adam Thompson <adam.thompson@mongodb.com>
2023-12-20 11:34:35 -0500

    updates getSegmentsFromDate

--- packages/date-picker/src/shared/utils/getSegmentsFromDate/index.ts
+++ packages/date-picker/src/shared/utils/getSegmentsFromDate/index.ts
@@ -1,24 +1,2 @@
-import { DateSegmentsState } from '../../types';
-import { getValueFormatter } from '../getValueFormatter';
-
-/** Returns a single object with day, month & year segments */
-export const getSegmentsFromDate = (date: Date | null): DateSegmentsState => {
-  return {
-    day: date ? date.getUTCDate() : '',
-    month: date ? date.getUTCMonth() + 1 : '',
-    year: date ? date.getUTCFullYear() : '',
-  } as DateSegmentsState;
-};
-
-/** Returns a single object with _formatted_ day, month & year segments */
-export const getFormattedSegmentsFromDate = (
-  date: Date | null,
-): DateSegmentsState => {
-  const segments = getSegmentsFromDate(date);
-
-  return {
-    day: getValueFormatter('day')(segments['day']),
-    month: getValueFormatter('month')(segments['month']),
-    year: getValueFormatter('year')(segments['year']),
-  };
-};
+export { getFormattedSegmentsFromDate } from './getFormattedSegmentsFromDate';
+export { getSegmentsFromDate } from './getSegmentsFromDate';
