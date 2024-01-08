f5f315f0062331eb99b821443a5dbfa52577d55c
Adam Thompson <adam.thompson@mongodb.com>
2024-01-08 14:00:49 -0800

    updates isSameUTCDay & isSameUTCMonth tests

--- packages/date-utils/src/newTZDate/newTZDate.ts
+++ packages/date-utils/src/newTZDate/newTZDate.ts
@@ -1,6 +1,10 @@
 /**
- * Creates a Date object for a given UTC offset
+ * Creates a Date object for a given UTC offset.
+ * @internal
  */
+// This API is less than perfect,
+// but we shouldn't be constructing many (if any)
+// TZ dependent dates other than for testing
 export const newTZDate = (
   UTCOffset = 0,
   ...args: Parameters<typeof Date.UTC>
