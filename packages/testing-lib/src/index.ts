232d059f4cc0aa611a2935cfd6edb8c15fa6d005
Adam Thompson <adam.thompson@mongodb.com>
2024-01-10 11:17:52 -0500

    Creates `waitForState `

--- packages/testing-lib/src/index.ts
+++ packages/testing-lib/src/index.ts
@@ -2,5 +2,6 @@ import * as Context from './context';
 import * as jest from './jest';
 import * as JestDOM from './jest-dom';
 export { act, renderHook } from './RTLOverrides';
+export { waitForState } from './waitForState';
 
 export { Context, jest, JestDOM };
