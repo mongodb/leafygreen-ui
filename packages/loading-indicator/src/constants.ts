d3fadc5ca7d32304146c82c61030afac672a646b
Adam Thompson <adam.thompson@mongodb.com>
2024-01-30 14:29:22 -0500

    rm ts-expect-error

--- packages/loading-indicator/src/constants.ts
+++ packages/loading-indicator/src/constants.ts
@@ -3,6 +3,5 @@ import type { LottieProps } from 'react-lottie-player';
 type RendererSettings = LottieProps['rendererSettings'];
 
 export const lottieRendererSettings: RendererSettings = {
-  // @ts-expect-error - waiting on https://github.com/airbnb/lottie-web/pull/3050
   runExpressions: false,
 };
