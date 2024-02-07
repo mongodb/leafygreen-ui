fd1d929ce1ce4d3e58432d0cc691b27ed4b7ddf4
Shaneeza <shaneeza.ali@mongodb.com>
2024-02-07 16:24:50 -0500

    forgot type

--- packages/typography/src/Link/index.ts
+++ packages/typography/src/Link/index.ts
@@ -1,3 +1,3 @@
 export { BackLink, type BackLinkProps } from './BackLink';
-export { Link, LinkProps } from './Link';
+export { Link, type LinkProps } from './Link';
 export { anchorClassName } from './shared.styles';
