41c0dc005a643f74f5518e1cf31650593536f110
Adam Thompson <adam.thompson@mongodb.com>
2024-02-12 11:15:16 -0500

    rm redundant plugins

--- tools/build/config/babel.config.js
+++ tools/build/config/babel.config.js
@@ -19,7 +19,17 @@ module.exports = function (api) {
 
   // For an explanation of `{loose: true}` see:
   // https://github.com/babel/babel/issues/11622#issuecomment-644141879
-  const plugins = ['@emotion/babel-plugin'];
+  const plugins = [
+    // ['@babel/plugin-proposal-class-properties', { loose: true }],
+    '@babel/plugin-proposal-export-default-from',
+    // '@babel/plugin-proposal-logical-assignment-operators',
+    // '@babel/plugin-proposal-nullish-coalescing-operator',
+    // '@babel/plugin-proposal-object-rest-spread',
+    // '@babel/plugin-proposal-optional-chaining',
+    // ['@babel/plugin-proposal-private-methods', { loose: true }],
+    // ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    '@emotion/babel-plugin',
+  ];
 
   return { presets, plugins };
 };
