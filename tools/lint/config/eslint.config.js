573eb8af819d716c45fe15444ce1089a7b542510
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2024-03-13 16:28:31 -0400

    Update eslint.config.js

--- tools/lint/config/eslint.config.js
+++ tools/lint/config/eslint.config.js
@@ -151,7 +151,7 @@ module.exports = {
       },
     },
     {
-      files: ['chat/**/*.spec.{ts,tsx}', 'packages/**/*.spec.{ts,tsx}'],
+      files: ['{chat,packages,tools}/**/*.spec.{ts,tsx}'],
       globals: {
         expect: true,
         should: true,
