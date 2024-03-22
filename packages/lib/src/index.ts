66df9ab86d4c71dd3403b67a1ca85f69ceb39f92
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2024-03-22 13:09:40 -0400

    Adds `Mutable` type (#2259)
    * creates Mutable type
    
    * Create twelve-comics-agree.md
    
    * Update index.ts
    
    * Update yarn.lock

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -6,6 +6,7 @@ import getTheme from './getTheme';
 export * from './helpers';
 export { validateChildren, isComponentType } from './validateChildren';
 export { createSyntheticEvent } from './createSyntheticEvent';
+export { type Mutable } from './types';
 
 export {
   type ExtendedComponentProps,
