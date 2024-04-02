b7be1c1e87cdc6720f7989f18a476b583be35441
Shaneeza <shaneeza.ali@mongodb.com>
2024-04-02 11:03:51 -0400

    forgot to save

--- packages/toggle/src/utils/getLGToggleTestUtils/getLGToggleTestUtils.types.ts
+++ packages/toggle/src/utils/getLGToggleTestUtils/getLGToggleTestUtils.types.ts
@@ -1,6 +1,6 @@
 import { type FormElements, type FormUtils } from '@lg-tools/test-harnesses';
 
 type ToggleElements = Pick<FormElements<HTMLButtonElement>, 'getInput'>;
-type ToggleUtils = Pick<FormUtils, 'getInputValue' | 'isDisabled'>;
+type ToggleUtils = Pick<FormUtils<Boolean>, 'getInputValue' | 'isDisabled'>;
 
 export type LGToggleTestUtilsReturnType = ToggleElements & ToggleUtils;
