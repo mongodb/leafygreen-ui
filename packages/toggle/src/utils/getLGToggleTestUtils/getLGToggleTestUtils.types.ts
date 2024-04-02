dcf044616d90a3ef6da35aba54df11640c358377
Shaneeza <shaneeza.ali@mongodb.com>
2024-04-02 11:04:23 -0400

    boolean

--- packages/toggle/src/utils/getLGToggleTestUtils/getLGToggleTestUtils.types.ts
+++ packages/toggle/src/utils/getLGToggleTestUtils/getLGToggleTestUtils.types.ts
@@ -1,6 +1,6 @@
 import { type FormElements, type FormUtils } from '@lg-tools/test-harnesses';
 
 type ToggleElements = Pick<FormElements<HTMLButtonElement>, 'getInput'>;
-type ToggleUtils = Pick<FormUtils<Boolean>, 'getInputValue' | 'isDisabled'>;
+type ToggleUtils = Pick<FormUtils<boolean>, 'getInputValue' | 'isDisabled'>;
 
 export type LGToggleTestUtilsReturnType = ToggleElements & ToggleUtils;
