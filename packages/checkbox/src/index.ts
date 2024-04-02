676dfb8a5b65d5fc2d538e8934e0900421f6ce7e
brooke <brooke.yalof@mongodb.com>
2024-04-02 10:00:31 -0400

    run fix

--- packages/checkbox/src/index.ts
+++ packages/checkbox/src/index.ts
@@ -1,3 +1,3 @@
 export { type CheckboxProps, default } from './Checkbox';
-export { getTestUtils } from './utils';
 export { LGIDS_CHECKBOX } from './constants';
+export { getTestUtils } from './utils';
