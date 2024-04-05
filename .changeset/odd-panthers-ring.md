24ee033c7353d8cd81056fee584a36a8daaee1d4
Nick Larew <nick.larew@10gen.com>
2024-04-05 14:14:38 -0400

    (EAI-267) [UI] Verified Answers in LG Chat (#2283)

---
+++ .changeset/odd-panthers-ring.md
@@ -0,0 +1,5 @@
+---
+'@leafygreen-ui/banner': major
+---
+
+Banner now forwards refs
--- chat/message/src/Message/Message.stories.tsx
+++ chat/message/src/Message/Message.stories.tsx
@@ -5,9 +5,9 @@ import { WithMessageRating as MessageFeedbackStory } from '@lg-chat/message-feed
 import { storybookArgTypes, StoryMetaType } from '@lg-tools/storybook-utils';
 import { StoryFn } from '@storybook/react';
 
-import { Message, MessageSourceType } from '.';
+import { Message, MessageSourceType } from '..';
 
-const StoryMarkdown = `
+const MarkdownText = `
 # Heading 1
 
 ## Heading 2
@@ -39,11 +39,32 @@ function helloWorld() {
 - [https://mongodb.github.io/leafygreen-ui/?path=/docs/overview-introduction--docs](https://mongodb.github.io/leafygreen-ui/?path=/docs/overview-introduction--docs)
 `;
 
+const UserText = `How can I delete a massive amount of documents from a collection?`;
+
+const MongoText = `
+To efficiently delete a large number of documents from a MongoDB collection, you
+can use the \`deleteMany()\` method. This method allows you to delete multiple
+documents that match a specified filter.
+
+Here's an example of how to use the \`deleteMany()\` method to delete a large
+number of documents:
+
+\`\`\`javascript
+db.collection.deleteMany({ <filter> });
+\`\`\`
+
+Keep in mind that deleting a large number of documents can be resource-intensive
+and may impact the performance of your MongoDB server. It's recommended to
+perform such operations during periods of low activity or to use techniques like
+sharding to distribute the load across multiple servers.
+
+Let me know if you need any further assistance!
+`;
+
 const meta: StoryMetaType<typeof Message> = {
   title: 'Chat/Message',
   component: Message,
   args: {
-    message: StoryMarkdown,
     avatar: <Avatar variant="user" name="Sean Park" />,
     sourceType: MessageSourceType.Markdown,
   },
@@ -68,22 +89,47 @@ const Template: StoryFn<typeof Message> = ({ darkMode, avatar, ...rest }) => {
 };
 
 export const Basic: StoryFn<typeof Message> = Template.bind({});
+Basic.args = {
+  messageBody: UserText,
+};
 
 export const Text: StoryFn<typeof Message> = Template.bind({});
 Text.args = {
-  sourceType: undefined,
+  messageBody: MarkdownText,
+  sourceType: MessageSourceType.Text,
+};
+
+export const Markdown: StoryFn<typeof Message> = Template.bind({});
+Markdown.args = {
+  messageBody: MarkdownText,
+  sourceType: MessageSourceType.Markdown,
 };
 
 export const Mongo: StoryFn<typeof Message> = Template.bind({});
 Mongo.args = {
   isSender: false,
+  messageBody: MongoText,
   avatar: <Avatar variant="mongo" />,
 };
 
 export const WithMessageRating: StoryFn<typeof Message> = Template.bind({});
 WithMessageRating.args = {
   isSender: false,
+  messageBody: MongoText,
   avatar: <Avatar variant="mongo" />,
+  // @ts-ignore onChange is passed in the story itself
+  children: <MessageFeedbackStory />,
+};
+
+export const VerifiedAnswer: StoryFn<typeof Message> = Template.bind({});
+VerifiedAnswer.args = {
+  isSender: false,
+  messageBody: 'The MongoDB Atlas free tier includes 512MB of storage.',
+  avatar: <Avatar variant="mongo" />,
+  verified: {
+    verifier: 'MongoDB Staff',
+    verifiedAt: new Date('2023-08-24T16:20:00Z'),
+  },
   // @ts-ignore onChange is passed in the story itself
   children: <MessageFeedbackStory />,
 };
@@ -94,7 +140,7 @@ export const MultipleUser = () => (
       {/* @ts-expect-error baseFontSize is not a number */}
       <Basic {...meta.args} {...Basic.args} />
       {/* @ts-expect-error baseFontSize is not a number */}
-      <Basic {...meta.args} {...Basic.args} />
+      <Basic {...meta.args} {...Basic.args} messageBody="Another message!" />
     </div>
   </LeafyGreenChatProvider>
 );
@@ -103,7 +149,11 @@ export const MultipleMongo = () => (
   <LeafyGreenChatProvider>
     <div>
       {/* @ts-expect-error baseFontSize is not a number */}
-      <Mongo {...meta.args} {...Mongo.args} />
+      <Mongo
+        {...meta.args}
+        {...Mongo.args}
+        messageBody="First message! Expect another from me right after this one."
+      />
       {/* @ts-expect-error baseFontSize is not a number */}
       <Mongo {...meta.args} {...Mongo.args} />
     </div>
