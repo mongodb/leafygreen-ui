c4190091e675662f78717d2853f4599df4f4641c
Adam Thompson <adam.thompson@mongodb.com>
2024-04-05 19:07:49 -0400

    fix test package

--- tools/test/src/index.ts
+++ tools/test/src/index.ts
@@ -54,6 +54,12 @@ export const test = (
     ...passThroughOptions,
   ].filter(v => v !== '');
 
+  console.log({
+    verbose, 
+    jestBinary,
+    commandArgs
+  })
+
   spawn(jestBinary, commandArgs, {
     env: {
       ...process.env,
