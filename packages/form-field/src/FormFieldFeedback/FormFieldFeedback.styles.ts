7155db11d4e5aa409e7975b5f731a7429d6c6f74
Stephen Lee <stephen.lee@mongodb.com>
2024-04-19 09:20:14 -0700

    Add and export DEFAULT_MESSAGES constant and FormFieldFeedback

---
+++ packages/form-field/src/FormFieldFeedback/FormFieldFeedback.styles.ts
@@ -0,0 +1,27 @@
+import { css } from '@leafygreen-ui/emotion';
+import { Size, spacing, typeScales } from '@leafygreen-ui/tokens';
+
+export const containerStyles = css`
+  display: flex;
+  gap: ${spacing[100]}px;
+`;
+
+export const spacingTop = css`
+  padding-top: ${spacing[100]}px;
+`;
+
+export const hideContainerStyle = css`
+  opacity: 0;
+`;
+
+export const iconWrapperStyles = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const getIconWrapperHeight = (size: Size) => {
+  return css`
+    height: ${size === Size.Large ? 24 : typeScales.body1.lineHeight}px;
+  `;
+};
