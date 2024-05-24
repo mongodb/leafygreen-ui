057d79572470a2bc4dfe1bdaaa7eb024e8adb8f8
Adam Thompson <adam.thompson@mongodb.com>
2024-05-24 14:06:00 -0400

    installs descendants in menu

--- packages/menu/src/MenuContext/MenuContext.tsx
+++ packages/menu/src/MenuContext/MenuContext.tsx
@@ -1,6 +1,12 @@
 import { createContext } from 'react';
 
+import { createDescendantsContext } from '@leafygreen-ui/descendants';
+
 import { MenuData } from './MenuContext.types';
+
+export const MenuDescendantsContext = createDescendantsContext(
+  'MenuDescendantsContext',
+);
 
 export const MenuContext = createContext<MenuData>({
   theme: 'light',
