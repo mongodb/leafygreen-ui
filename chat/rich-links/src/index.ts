fa263aecae08a7a45d1b6137b5b6ea56b4f17de0
Nick Larew <nick.larew@mongodb.com>
2024-05-24 14:20:37 -0400

    reorder imports

--- chat/rich-links/src/index.ts
+++ chat/rich-links/src/index.ts
@@ -1,8 +1,8 @@
 export {
-  RichLink,
-  type RichLinkProps,
   createRichLinkVariant,
   type CreateRichLinkVariantArgs,
+  RichLink,
+  type RichLinkProps,
   type RichLinkVariantMap,
   type RichLinkVariantName,
   richLinkVariants,
