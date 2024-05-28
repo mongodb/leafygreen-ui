e7687e7d251d8a292e16d6e215a42f982bbf2231
Adam Thompson <adam.thompson@mongodb.com>
2024-05-28 19:18:46 -0400

    fix menu item list style

--- packages/menu/src/MenuItem/MenuItem.styles.ts
+++ packages/menu/src/MenuItem/MenuItem.styles.ts
@@ -2,6 +2,10 @@ import { css } from '@leafygreen-ui/emotion';
 import { Theme } from '@leafygreen-ui/lib';
 import { palette } from '@leafygreen-ui/palette';
 
+export const menuItemContainerStyles = css`
+  list-style: none;
+`;
+
 export const disabledIconStyle: Record<Theme, string> = {
   [Theme.Light]: css`
     color: ${palette.gray.dark2};
