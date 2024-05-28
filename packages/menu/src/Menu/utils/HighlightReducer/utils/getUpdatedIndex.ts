44a67ebd71655f9fdc3b56f0f87d49359d56fc6d
Adam Thompson <adam.thompson@mongodb.com>
2024-05-28 13:58:56 -0400

    skip disabled elements

--- packages/menu/src/Menu/utils/HighlightReducer/utils/getUpdatedIndex.ts
+++ packages/menu/src/Menu/utils/HighlightReducer/utils/getUpdatedIndex.ts
@@ -5,18 +5,20 @@ import type { Direction, Index } from '../highlight';
 
 import { isDescendantsSet } from './isDescendantsSet';
 
-/** Computes the next index given a direction */
-export function getNewIndex(
+/**
+ * Computes the next index given a direction
+ */
+function getNewIndex(
   direction: Direction,
   currentIndex: number,
   totalItems: number,
 ): number;
-export function getNewIndex(
+function getNewIndex(
   direction: Direction,
   currentIndex: undefined,
   totalItems: number,
 ): undefined;
-export function getNewIndex(
+function getNewIndex(
   direction: Direction,
   currentIndex: Index,
   totalItems: number,
@@ -36,26 +38,60 @@ export function getNewIndex(
   }
 }
 
+/**
+ * Finds the index of the subsequent `enabled` descendant element
+ */
+function getEnabledIndex(
+  direction: Direction,
+  currentIndex: number,
+  descendants: DescendantsList<HTMLElement>,
+): Index {
+  // If all descendants are disabled, then we skip this step
+  if (descendants.every(d => d.props.disabled)) {
+    return undefined;
+  }
+
+  let updatedIndex = getNewIndex(direction, currentIndex, descendants.length);
+  let item = descendants[updatedIndex];
+
+  // If the subsequent item is disabled,
+  // keep searching in that direction for an enabled one
+  while (item.props.disabled) {
+    // If the first/last item is disabled
+    // start the search in the forward/backward direction
+    const nextDirection: Direction = (() => {
+      switch (direction) {
+        case 'first':
+          return 'next';
+        case 'last':
+          return 'prev';
+        default:
+          return direction;
+      }
+    })();
+    updatedIndex = getNewIndex(nextDirection, updatedIndex, descendants.length);
+    item = descendants[updatedIndex];
+  }
+
+  return updatedIndex;
+}
+
 export const getUpdatedIndex = (
   direction: Direction,
-  index: Index,
+  currentIndex: Index,
   descendants: DescendantsList<HTMLElement>,
 ): Index => {
   // If descendants is not set
   // then we don't mutate the index
   if (!isDescendantsSet(descendants)) {
-    return index;
+    return currentIndex;
   }
 
-  // If descendants is defined
-  // but `index` is not initialized
-  // initialize that here
-  if (!isDefined(index)) {
-    return 0;
-  }
-
-  // // TODO: keep incrementing until we find an enabled item
-  const updatedIndex = getNewIndex(direction, index, descendants.length);
+  const updatedIndex = getEnabledIndex(
+    direction,
+    currentIndex ?? 0,
+    descendants,
+  );
 
   return updatedIndex;
 };
