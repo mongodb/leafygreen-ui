02e1d77e5ed7d55f9b8402299eae0c6d540c53f8
Stephen Lee <stephen.lee@mongodb.com>
2024-06-04 08:47:03 -0700

    LG-3988: Expose portalRef in components using Popover (#2241)
    * Reclassify portalRef prop as part of PortalControlProps in Popover component
    
    * Popover changeset
    
    * Expose portalRef prop in Combobox
    
    * Expose portalRef prop in DatePicker
    
    * Expose portalRef prop in GuideCue
    
    * Expose portalRef prop in Menu
    
    * Expose portalRef prop in NumberInput
    
    * Expose portalRef prop in Select
    
    * Expose portalRef prop in SplitButton
    
    * Expose portalRef prop in Tooltip
    
    * Changeset for exposing portalRef in components
    
    * Expose some PortalControlProps in SearchInput
    
    * SearchInput changeset
    
    * Update tests

--- packages/number-input/src/UnitSelect/UnitSelect.types.ts
+++ packages/number-input/src/UnitSelect/UnitSelect.types.ts
@@ -1,36 +1,14 @@
+import {
+  PopoverProps as ImportedPopoverProps,
+  PortalControlProps,
+} from '@leafygreen-ui/popover';
+
 import { Size, UnitOption } from '../NumberInput/NumberInput.types';
 
-export interface PopoverProps {
-  /**
-   * Specifies that the popover content should be rendered at the end of the DOM,
-   * rather than in the DOM tree.
-   *
-   * default: `true`
-   */
-  usePortal?: boolean;
+export type PopoverProps = PortalControlProps &
+  Pick<ImportedPopoverProps, 'popoverZIndex'>;
 
-  /**
-   * When usePortal is `true`, specifies a class name to apply to the root element of the portal.
-   */
-  portalClassName?: string;
-
-  /**
-   * When usePortal is `true`, specifies an element to portal within. The default behavior is to generate a div at the end of the document to render within.
-   */
-  portalContainer?: HTMLElement | null;
-
-  /**
-   * When usePortal is `true`, specifies the scrollable element to position relative to.
-   */
-  scrollContainer?: HTMLElement | null;
-
-  /**
-   * Number that controls the z-index of the popover element directly.
-   */
-  popoverZIndex?: number;
-}
-
-export interface UnitSelectProps extends PopoverProps {
+export type UnitSelectProps = {
   /**
    * Id for the select component.
    */
@@ -73,4 +51,4 @@ export interface UnitSelectProps extends PopoverProps {
    * @internal
    */
   ['data-testid']?: string;
-}
+} & PopoverProps;
