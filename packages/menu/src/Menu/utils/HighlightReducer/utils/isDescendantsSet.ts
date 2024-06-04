96f11aeb9e4c802a1ac767cd9da7d4cdc339f07a
Adam Thompson <adam.thompson@mongodb.com>
2024-06-04 13:23:30 -0400

    rm old highlight reducer

--- packages/menu/src/Menu/utils/HighlightReducer/utils/isDescendantsSet.ts
+++
@@ -1,8 +0,0 @@
-import { DescendantsList } from '@leafygreen-ui/descendants';
-import { isDefined } from '@leafygreen-ui/lib';
-
-export const isDescendantsSet = (
-  descendants?: DescendantsList<any>,
-): boolean => {
-  return isDefined(descendants) && descendants.length > 0;
-};
