71419d3a55efcf2b8b287029536d4025c7bea897
brooke <brooke.yalof@mongodb.com>
2024-06-06 15:41:39 -0400

    add args to icon meta for .design

--- packages/icon/src/Icon.stories.tsx
+++ packages/icon/src/Icon.stories.tsx
@@ -22,6 +22,10 @@ const meta: StoryMetaType<typeof Icon> = {
       exclude: [...storybookExcludedControlParams, 'title', 'data-testid'],
     },
   },
+  args: {
+    fill: '#fff',
+    size: Size.Default,
+  },
   argTypes: {
     size: {
       control: 'select',
