7a3ef5970968b1e6acc25ce6dba71bea43bf7338
Adam Thompson <adam.thompson@mongodb.com>
2024-06-07 11:47:21 -0400

    Create SubMenu.stories.tsx

---
+++ packages/menu/src/SubMenu/SubMenu.stories.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { StoryMetaType } from '@lg-tools/storybook-utils';
+import { StoryObj } from '@storybook/react';
+
+import { css } from '@leafygreen-ui/emotion';
+
+import { MenuProps } from '../Menu';
+import { MenuItem } from '../MenuItem';
+
+import { SubMenu } from '.';
+
+export default {
+  title: 'Components/Menu/SubMenu',
+  component: SubMenu,
+  args: {
+    active: false,
+  },
+  parameters: {
+    default: null,
+    controls: {
+      exclude: ['open', 'setOpen'],
+    },
+  },
+} satisfies StoryMetaType<typeof SubMenu, Partial<MenuProps>>;
+
+export const LiveExample = {
+  render: args => {
+    const [open, setOpen] = useState(true);
+
+    return (
+      <div
+        className={css`
+          width: 256px;
+        `}
+      >
+        <SubMenu {...args} open={open} setOpen={setOpen}>
+          <MenuItem>Apple</MenuItem>
+          <MenuItem>Banana</MenuItem>
+        </SubMenu>
+      </div>
+    );
+  },
+  args: {
+    title: 'Sub menu',
+    description: 'This is a description',
+  },
+} satisfies StoryObj<typeof Submenu>;
