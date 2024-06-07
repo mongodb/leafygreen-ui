2c04b21d192205575260d9ea35a0e4e89d488b8b
Adam Thompson <adam.thompson@mongodb.com>
2024-06-07 13:02:06 -0400

    creates Optional and PartialRequired types

--- packages/lib/src/types/index.ts
+++ packages/lib/src/types/index.ts
@@ -1,2 +1,4 @@
 export type { Mutable } from './Mutable';
+export type { Optional } from './Optional';
+export type { PartialRequired } from './PartialRequired';
 export type { RecursiveRecord } from './RecursiveRecord';
