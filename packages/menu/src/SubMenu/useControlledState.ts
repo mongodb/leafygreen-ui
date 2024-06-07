d10067d3c7bf3b9dff8d7d859acefb217f7d09b5
Adam Thompson <adam.thompson@mongodb.com>
2024-06-07 11:47:11 -0400

    Update useControlledState.ts

--- packages/menu/src/SubMenu/useControlledState.ts
+++ packages/menu/src/SubMenu/useControlledState.ts
@@ -8,8 +8,7 @@ export const useControlledState = <T extends any>(
   controlledState?: T,
   setControlledState?: Dispatch<SetStateAction<T>>,
 ): [T, Dispatch<SetStateAction<T>>] => {
-  const isControlled =
-    !isUndefined(controlledState) && !isUndefined(setControlledState);
+  const isControlled = !isUndefined(controlledState);
   const [internalState, setInternalState] = useState(initialState);
 
   useEffect(() => {
