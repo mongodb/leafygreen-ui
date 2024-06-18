3e515b090f0e0c667639e94405ca273a3bce09b8
Adam Thompson <adam.thompson@mongodb.com>
2024-06-18 13:29:05 -0400

    add internal flags to descendants utils

--- packages/descendants/src/utils/isElementPreceding.ts
+++ packages/descendants/src/utils/isElementPreceding.ts
@@ -2,7 +2,10 @@
  * Returns whether ElementA precedes ElementAB in the DOM
  *
  * See: https://developer.mozilla.org/en-US/docs/Web/API/Node/compareDocumentPosition
- */ export function isElementPreceding(elemA: HTMLElement, elemB: HTMLElement) {
+ *
+ * @internal
+ */
+export function isElementPreceding(elemA: HTMLElement, elemB: HTMLElement) {
   return Boolean(
     elemB.compareDocumentPosition(elemA) & Node.DOCUMENT_POSITION_PRECEDING,
   );
