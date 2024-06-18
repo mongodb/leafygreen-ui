d8dda5c1c8c82c01cbef0d5c85d489d876f29124
Adam Thompson <adam.thompson@mongodb.com>
2024-06-18 18:41:12 -0400

    Updates Descendant index properties

--- packages/descendants/src/utils/index.ts
+++ packages/descendants/src/utils/index.ts
@@ -3,4 +3,5 @@ export { findDOMIndex } from './findDOMIndex';
 export { getDescendantById, getDescendantByIndex } from './getDescendant';
 export { insertDescendantAt } from './insertDescendantAt';
 export { isElementPreceding } from './isElementPreceding';
+export { refreshDescendantIndexes } from './refreshDescendantIndexes';
 export { removeIndex } from './removeIndex';
