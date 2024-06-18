6226cb567f8329cbd6bde1bfe0b36e327a8acba0
Adam Thompson <adam.thompson@mongodb.com>
2024-06-18 14:00:26 -0400

    add getByIndex/id to descendants pkg

--- packages/descendants/src/utils/index.ts
+++ packages/descendants/src/utils/index.ts
@@ -1,5 +1,6 @@
 export { findDescendantIndexWithId } from './findDescendantIndexWithId';
 export { findDOMIndex } from './findDOMIndex';
+export { getDescendantById, getDescendantByIndex } from './getDescendant';
 export { insertDescendantAt } from './insertDescendantAt';
 export { isElementPreceding } from './isElementPreceding';
 export { removeIndex } from './removeIndex';
