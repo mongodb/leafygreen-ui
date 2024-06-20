5e448b891782580a3651dc3464355e677c9dfeeb
Adam Thompson <adam.thompson@mongodb.com>
2024-06-20 14:15:10 -0400

    Creates MenuGroup with title & context

--- packages/menu/src/MenuGroup/MenuGroup.stories.tsx
+++ packages/menu/src/MenuGroup/MenuGroup.stories.tsx
@@ -3,8 +3,10 @@ import { StoryMetaType } from '@lg-tools/storybook-utils';
 import { StoryObj } from '@storybook/react';
 
 import { css } from '@leafygreen-ui/emotion';
+import Icon, { glyphs } from '@leafygreen-ui/icon';
 
 import { MenuItem } from '../MenuItem';
+import { SubMenu } from '../SubMenu';
 
 import MenuGroup from './MenuGroup';
 
@@ -14,18 +16,46 @@ export default {
   parameters: {
     default: null,
   },
+  args: {
+    title: 'Group',
+    glyph: 'AllProducts',
+  },
+  argTypes: {
+    glyph: {
+      control: 'select',
+      options: [undefined, ...Object.keys(glyphs)],
+    },
+  },
 } satisfies StoryMetaType<typeof MenuGroup>;
 
 export const LiveExample = {
-  render: () => (
-    <ul
+  render: ({ glyph, ...args }) => (
+    <div
       className={css`
         width: 256px;
+        outline: 1px dashed gray;
       `}
     >
-      <MenuGroup>
-        <MenuItem>Item</MenuItem>
+      <MenuGroup
+        {...args}
+        glyph={
+          glyph && <Icon glyph={glyph as unknown as keyof typeof glyphs} />
+        }
+      >
+        <MenuItem>Apple</MenuItem>
+        <MenuItem>Banana</MenuItem>
+        <MenuItem>Carrot</MenuItem>
+        <SubMenu title="Peppers">
+          <MenuItem>Jalapeño</MenuItem>
+          <MenuItem>Habanero</MenuItem>
+          <MenuItem>Ghost</MenuItem>
+        </SubMenu>
       </MenuGroup>
-    </ul>
+    </div>
   ),
+  parameters: {
+    chromatic: {
+      disableSnapshot: true,
+    },
+  },
 } satisfies StoryObj<typeof MenuGroup>;
--- packages/menu/src/MenuGroup/MenuGroup.tsx
+++ packages/menu/src/MenuGroup/MenuGroup.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
+import { css } from '@leafygreen-ui/emotion';
+import { InputOption, InputOptionContent } from '@leafygreen-ui/input-option';
+import { Overline } from '@leafygreen-ui/typography';
+
+import { MenuGroupProvider } from '../MenuContext';
+
 import { MenuGroupProps } from './MenuGroup.types';
 
 /**
@@ -14,10 +20,25 @@ import { MenuGroupProps } from './MenuGroup.types';
  * @param props.children Content to appear inside of the MenuGroup.
  *
  */
-export function MenuGroup({ children, className, ...rest }: MenuGroupProps) {
+export function MenuGroup({
+  children,
+  className,
+  title,
+  glyph,
+  ...rest
+}: MenuGroupProps) {
   return (
     <section {...rest} className={className}>
-      {children}
+      {title && (
+        <InputOption as="div" className={css``} isInteractive={false}>
+          <InputOptionContent leftGlyph={glyph}>
+            <Overline>{title}</Overline>
+          </InputOptionContent>
+        </InputOption>
+      )}
+      <MenuGroupProvider depth={1} hasIcon={!!glyph}>
+        {children}
+      </MenuGroupProvider>
     </section>
   );
 }
