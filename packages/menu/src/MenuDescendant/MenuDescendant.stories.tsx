c546de418a2fae0cba9cf35250679b8e8146b7eb
Adam Thompson <adam.thompson@mongodb.com>
2024-06-20 17:24:27 -0400

    creates MenuDescendant

---
+++ packages/menu/src/MenuDescendant/MenuDescendant.stories.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from 'react';
+import { StoryMetaType } from '@lg-tools/storybook-utils';
+import { StoryObj } from '@storybook/react';
+
+import Button from '@leafygreen-ui/button';
+import TextInput from '@leafygreen-ui/text-input';
+
+import { Menu } from '../Menu';
+
+import { MenuDescendant } from './MenuDescendant';
+
+export default {
+  title: 'Components/Menu/Menu Descendant',
+  component: MenuDescendant,
+  parameters: {
+    default: null,
+  },
+  args: {
+    darkMode: false,
+  },
+  argTypes: {
+    darkMode: {
+      control: 'boolean',
+    },
+  },
+} satisfies StoryMetaType<typeof MenuDescendant>;
+
+export const LiveExample = {
+  render: () => {
+    return (
+      <Menu initialOpen trigger={<Button>Menu</Button>} renderDarkMenu={false}>
+        <MenuDescendant>
+          <TextInput aria-label="Test" placeholder="Input" />
+        </MenuDescendant>
+      </Menu>
+    );
+  },
+} satisfies StoryObj<typeof MenuDescendant>;
