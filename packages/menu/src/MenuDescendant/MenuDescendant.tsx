21e3ee129cfd30d36d2a115f09c289598a5402b1
Adam Thompson <adam.thompson@mongodb.com>
2024-06-26 10:13:39 -0400

    adds documentation on single focusable element

--- packages/menu/src/MenuDescendant/MenuDescendant.tsx
+++ packages/menu/src/MenuDescendant/MenuDescendant.tsx
@@ -8,6 +8,17 @@ import { MenuDescendantsContext, useMenuContext } from '../MenuContext';
 
 import { getMenuDescendantWrapperStyles } from './MenuDescendant.styles';
 
+/**
+ * Renders arbitrary focusable components as descendants in a menu context.
+ *
+ * By wrapping a single focusable component in `MenuDescendant`,
+ * it will be registered as a menu item descendant,
+ * and will be focusable using standard menu arrow key interactions.
+ *
+ * Note: the single child of `MenuDescendant` must _itself_ be focusable.
+ * Wrapping a focusable element (e.g. `input` in a `div`)
+ * will not enable the menu descendant functionality.
+ */
 export const MenuDescendant = React.forwardRef<
   HTMLElement,
   ComponentPropsWithRef<'div'>
