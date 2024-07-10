15bedb36675ed47f00e817b7e80d1a51f5092094
Adam Thompson <adam.thompson@mongodb.com>
2024-07-10 18:06:05 -0400

    adds todo tests

--- packages/descendants/src/Highlight/Highlight.spec.ts
+++ packages/descendants/src/Highlight/Highlight.spec.ts
@@ -1,1 +1,17 @@
-describe('packages/descendants/highlight', () => {});
+describe('packages/descendants/highlight', () => {
+  describe('useHighlight', () => {
+    test.todo('returns the current highlight object, and setter function(s)');
+    test.todo('calling the setter updates the highlight object');
+    test.todo(
+      '`onInit` option is called once when descendants are initialized',
+    );
+    test.todo('`onChange` option is called when the highlight object changes');
+    test.todo(
+      '`filter` option prevents relevant descendants from being highlighted',
+    );
+  });
+
+  describe('useHighlightContext', () => {
+    test.todo('returns the current highlight object');
+  });
+});
