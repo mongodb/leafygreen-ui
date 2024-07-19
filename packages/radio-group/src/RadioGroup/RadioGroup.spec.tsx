5ea7a1187023e40d01aa518f3b00725ba7f6ad7c
brooke <brooke.yalof@mongodb.com>
2024-07-19 08:43:34 -0400

    disabled does not fire events

--- packages/radio-group/src/RadioGroup/RadioGroup.spec.tsx
+++ packages/radio-group/src/RadioGroup/RadioGroup.spec.tsx
@@ -185,6 +185,14 @@ describe('packages/radio-group', () => {
       expect(radio.getAttribute('disabled')).toBeFalsy();
     });
 
+    test('does not fire onChange events when disabled', () => {
+      const onChange = jest.fn();
+      const { radio } = renderRadio({ disabled: true, onChange });
+      expect(onChange).not.toHaveBeenCalled();
+      expect(radio.getAttribute('aria-disabled')).toBeTruthy();
+      expect(radio.getAttribute('disabled')).toBeFalsy();
+    });
+
     test(`radio is checked when value is set`, () => {
       const { radio } = renderRadio({ checked: true });
       expect(radio.getAttribute('aria-checked')).toBe('true');
