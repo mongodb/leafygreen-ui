eb80fd3cbbe3250b050d74479decbd9e8f6e735d
Brooke Scarlett Yalof <brooke.yalof@mongodb.com>
2024-08-01 11:57:25 -0400

    pending icon (#2445)

---
+++ packages/icon/src/generated/Pending.tsx
@@ -0,0 +1,45 @@
+/**
+* This is a generated file. Do not modify it manually.
+*
+* @script ./node_modules/.bin/ts-node packages/icon/scripts/build.ts
+* @checksum 095e6dc89a43f71d355a360fbc593cc3
+*/
+import * as React from "react";
+import PropTypes from 'prop-types';
+import { css, cx } from '@leafygreen-ui/emotion';
+import { generateAccessibleProps, sizeMap } from '../glyphCommon';
+import { LGGlyph } from '../types';
+export interface PendingProps extends LGGlyph.ComponentProps {}
+const Pending = ({
+  className,
+  size = 16,
+  title,
+  ['aria-label']: ariaLabel,
+  ['aria-labelledby']: ariaLabelledby,
+  fill,
+  role = 'img',
+  ...props
+}: PendingProps) => {
+  const fillStyle = css`
+        color: ${fill};
+      `;
+  const noFlexShrink = css`
+        flex-shrink: 0;
+      `;
+  const accessibleProps = generateAccessibleProps(role, 'Pending', {
+    title,
+    ['aria-label']: ariaLabel,
+    ['aria-labelledby']: ariaLabelledby
+  });
+  return <svg className={cx({
+    [fillStyle]: fill != null
+  }, noFlexShrink, className)} height={typeof size === 'number' ? size : sizeMap[size]} width={typeof size === 'number' ? size : sizeMap[size]} role={role} {...accessibleProps} {...props} viewBox="0 0 16 16"><path fillRule="evenodd" clipRule="evenodd" d="M8 11.75C10.0711 11.75 11.75 10.0711 11.75 8C11.75 5.92893 10.0711 4.25 8 4.25C5.92893 4.25 4.25 5.92893 4.25 8C4.25 10.0711 5.92893 11.75 8 11.75ZM8 13.25C10.8995 13.25 13.25 10.8995 13.25 8C13.25 5.10051 10.8995 2.75 8 2.75C5.10051 2.75 2.75 5.10051 2.75 8C2.75 10.8995 5.10051 13.25 8 13.25Z" fill={'currentColor'} /></svg>;
+};
+Pending.displayName = 'Pending';
+Pending.isGlyph = true;
+Pending.propTypes = {
+  fill: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  className: PropTypes.string
+};
+export default Pending;
