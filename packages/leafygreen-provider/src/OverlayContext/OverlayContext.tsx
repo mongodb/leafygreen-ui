39213875bf25a220f8b100e8f49efe5f4086d56d
Stephen Lee <stephen.lee@mongodb.com>
2024-08-13 09:12:45 -0700

    LG-4444: OverlayContext and useOverlay (#2454)
    * OverlayContext, useOverlayContext, and OverlayProvider
    
    * useOverlay to register, remove, and track isTopMostOverlay
    
    * Export overlay context related components and hooks and include provider in LG provider
    
    * README and changeset
    
    * Fix type and lint
    
    * Update registerOverlay logic and use more explicit generic type names

---
+++ packages/leafygreen-provider/src/OverlayContext/OverlayContext.tsx
@@ -0,0 +1,79 @@
+import React, {
+  createContext,
+  ReactNode,
+  RefObject,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from 'react';
+import PropTypes from 'prop-types';
+
+interface OverlayItem<TOverlayElement extends HTMLElement = HTMLElement> {
+  element: TOverlayElement;
+  ref: RefObject<TOverlayElement>;
+  id: string;
+}
+
+export interface OverlayContextProps {
+  getOverlays: () => Array<OverlayItem>;
+  registerOverlay: (overlay: OverlayItem) => void;
+  removeOverlay: (idToRemove: string) => void;
+  topMostOverlay: OverlayItem | undefined;
+}
+
+export const OverlayContext = createContext<OverlayContextProps | undefined>(
+  undefined,
+);
+
+export const useOverlayContext = (): OverlayContextProps => {
+  const context = useContext(OverlayContext);
+
+  if (!context) {
+    throw new Error('useOverlayContext must be used within an OverlayProvider');
+  }
+
+  return context;
+};
+
+/**
+ * Global provider tracking overlays that stack on z-axis
+ */
+const OverlayProvider = ({ children }: { children: ReactNode }) => {
+  const [overlays, setOverlays] = useState<Array<OverlayItem>>([]);
+
+  const topMostOverlay = useMemo(
+    () => overlays[overlays.length - 1],
+    [overlays],
+  );
+
+  const registerOverlay = useCallback((overlay: OverlayItem) => {
+    if (overlays.some(item => item.id === overlay.id)) return;
+    setOverlays(prev => [...prev, { ...overlay }]);
+  }, []);
+
+  const removeOverlay = useCallback((idToRemove: string) => {
+    setOverlays(prev => prev.filter(item => item.id !== idToRemove));
+  }, []);
+
+  return (
+    <OverlayContext.Provider
+      value={{
+        getOverlays: () => overlays,
+        registerOverlay,
+        removeOverlay,
+        topMostOverlay,
+      }}
+    >
+      {children}
+    </OverlayContext.Provider>
+  );
+};
+
+OverlayProvider.displayName = 'OverlayProvider';
+
+OverlayProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default OverlayProvider;
