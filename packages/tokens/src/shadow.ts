dd760c176318cd133d098245e5ca4ae3e9545a50
Adam Thompson <adam.thompson@mongodb.com>
2024-09-06 12:44:40 -0400

    unsets dark mode shadow

--- packages/tokens/src/shadow.ts
+++ packages/tokens/src/shadow.ts
@@ -3,8 +3,7 @@ import { transparentize } from 'polished';
 import { Theme } from '@leafygreen-ui/lib';
 import { palette } from '@leafygreen-ui/palette';
 
-const lightModeShadow = transparentize(0.75, palette.black);
-const darkModeShadow = transparentize(0.85, palette.white);
+const lightModeShadowColor = transparentize(0.75, palette.black);
 
 interface Value {
   readonly 100: string;
@@ -12,9 +11,9 @@ interface Value {
 
 export const shadow: Record<Theme, Value> = {
   [Theme.Light]: {
-    100: `0px 2px 4px 1px ${lightModeShadow}`,
+    100: `0px 2px 4px 1px ${lightModeShadowColor}`,
   },
   [Theme.Dark]: {
-    100: `0px 4px 20px -4px ${darkModeShadow}`,
+    100: `unset`, // no shadow in dark mode
   },
 } as const;
