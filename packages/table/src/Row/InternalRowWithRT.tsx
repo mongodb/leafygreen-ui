0cc9d050fa8c73535466e96155850989622e0aed
Shaneeza <shaneeza.ali@mongodb.com>
2024-10-09 16:53:58 -0400

    another version

--- packages/table/src/Row/InternalRowWithRT.tsx
+++ packages/table/src/Row/InternalRowWithRT.tsx
@@ -20,62 +20,67 @@ import { RowContextProvider } from './RowContext';
 /**
  * Renders row data provided by `useReactTable`
  */
-const InternalRowWithRT = forwardRef(
-  <T extends LGRowData>(
-    {
-      children,
-      className,
-      row,
-      virtualRow,
-      disabled = false,
-      ...rest
-    }: InternalRowWithRTProps<T>,
-    fwdRef: ForwardedRef<HTMLTableRowElement>,
-  ) => {
-    const { theme } = useDarkMode();
-    const { measureElement, shouldAlternateRowColor } = useTableContext();
-    const isOddVSRow = !!virtualRow && virtualRow.index % 2 !== 0;
+const InternalRowWithRTForwardRef = <T extends LGRowData>(
+  {
+    children,
+    className,
+    row,
+    virtualRow,
+    disabled = false,
+    ...rest
+  }: InternalRowWithRTProps<T>,
+  ref: ForwardedRef<HTMLTableRowElement>,
+) => {
+  const { theme } = useDarkMode();
+  const { measureElement, shouldAlternateRowColor } = useTableContext();
+  const isOddVSRow = !!virtualRow && virtualRow.index % 2 !== 0;
 
-    const isExpanded = row.getIsExpanded();
-    const isSelected = row.getIsSelected();
-    const isParentExpanded = row.getParentRow()
-      ? row.getParentRow()?.getIsExpanded()
-      : false;
+  const isExpanded = row.getIsExpanded();
+  const isSelected = row.getIsSelected();
+  const isParentExpanded = row.getParentRow()
+    ? row.getParentRow()?.getIsExpanded()
+    : false;
 
-    const contextValues = useMemo(() => {
-      return {
-        disabled,
-        isReactTable: true,
-      };
-    }, [disabled]);
+  const contextValues = useMemo(() => {
+    return {
+      disabled,
+      isReactTable: true,
+    };
+  }, [disabled]);
 
-    return (
-      <InternalRowBase
-        className={cx(
-          {
-            [grayZebraRowStyles[theme]]:
-              isOddVSRow && shouldAlternateRowColor && !isSelected,
-            [zebraStyles[theme]]:
-              !virtualRow && shouldAlternateRowColor && !isSelected,
-            [selectedRowStyles[theme]]: isSelected && !disabled,
-            [expandedContentParentStyles[theme]]:
-              isExpanded || isParentExpanded,
-          },
-          className,
-        )}
-        data-selected={isSelected}
-        data-expanded={isExpanded}
-        id={`lg-table-row-${row.id}`}
-        ref={useMergeRefs([fwdRef, measureElement])}
-        data-index={virtualRow ? virtualRow!.index : ''}
-        {...rest}
-      >
-        <RowContextProvider {...contextValues}>{children}</RowContextProvider>
-      </InternalRowBase>
-    );
+  return (
+    <InternalRowBase
+      className={cx(
+        {
+          [grayZebraRowStyles[theme]]:
+            isOddVSRow && shouldAlternateRowColor && !isSelected,
+          [zebraStyles[theme]]:
+            !virtualRow && shouldAlternateRowColor && !isSelected,
+          [selectedRowStyles[theme]]: isSelected && !disabled,
+          [expandedContentParentStyles[theme]]: isExpanded || isParentExpanded,
+        },
+        className,
+      )}
+      data-selected={isSelected}
+      data-expanded={isExpanded}
+      id={`lg-table-row-${row.id}`}
+      ref={useMergeRefs([ref, measureElement])}
+      data-index={virtualRow ? virtualRow!.index : ''}
+      {...rest}
+    >
+      <RowContextProvider {...contextValues}>{children}</RowContextProvider>
+    </InternalRowBase>
+  );
+};
+export const InternalRowWithRT = React.forwardRef(
+  InternalRowWithRTForwardRef,
+) as <T extends LGRowData>(
+  props: InternalRowWithRTProps<T> & {
+    ref?: React.ForwardedRef<HTMLTableRowElement>;
   },
-);
+) => ReturnType<typeof InternalRowWithRTForwardRef>;
 
 export default InternalRowWithRT;
 
+// @ts-ignore - InternalRowWithRT assertion type does not include displayName
 InternalRowWithRT.displayName = 'InternalRowWithRT';
--- packages/table/src/Row/Row.tsx
+++ packages/table/src/Row/Row.tsx
@@ -1,4 +1,4 @@
-import React, { ForwardedRef, forwardRef } from 'react';
+import React, { ForwardedRef } from 'react';
 import PropTypes from 'prop-types';
 
 import { LGRowData } from '../useLeafyGreenTable';
@@ -10,35 +10,63 @@ import { RowProps } from './Row.types';
 /**
  * Renders the provided cells
  */
-const Row = forwardRef(
-  <T extends LGRowData>(
-    { row, virtualRow, ...rest }: RowProps<T>,
-    fwdRef: ForwardedRef<HTMLTableRowElement>,
-  ) => {
-    return (
-      <>
-        {row ? (
-          <InternalRowWithRT
-            ref={fwdRef}
-            //@ts-ignore - FIXME: incompatible type 😭
-            row={row}
-            virtualRow={virtualRow}
-            {...rest}
-          />
-        ) : (
-          <InternalRowWithoutRT {...rest} />
-        )}
-      </>
-    );
-  },
-);
+const RowForwardRef = <T extends LGRowData>(
+  { row, virtualRow, ...rest }: RowProps<T>,
+  ref: ForwardedRef<HTMLTableRowElement>,
+) => {
+  return (
+    <>
+      {row ? (
+        <InternalRowWithRT
+          //@ts-expect-error - FIXME: types are weird
+          ref={ref}
+          row={row}
+          virtualRow={virtualRow}
+          {...rest}
+        />
+      ) : (
+        <InternalRowWithoutRT {...rest} />
+      )}
+    </>
+  );
+};
 
+// https://oida.dev/typescript-react-generic-forward-refs/#option-1%3A-type-assertion
+export const Row = React.forwardRef(RowForwardRef) as <T extends LGRowData>(
+  props: RowProps<T> & { ref?: React.ForwardedRef<HTMLTableRowElement> },
+) => ReturnType<typeof RowForwardRef>;
+
+// @ts-ignore - Row assertion does not include propTypes
 Row.propTypes = {
   virtualRow: PropTypes.any,
   row: PropTypes.any,
   disabled: PropTypes.bool,
 };
 
+// @ts-ignore - Row assertion type does not include displayName
 Row.displayName = 'Row';
 
 export default Row;
+
+// const Row = forwardRef(
+//   <T extends LGRowData>(
+//     { row, virtualRow, ...rest }: RowProps<T>,
+//     fwdRef: ForwardedRef<HTMLTableRowElement>,
+//   ) => {
+//     return (
+//       <>
+//         {row ? (
+//           <InternalRowWithRT
+//             ref={fwdRef}
+//             //@ts-ignore - FIXME: incompatible type 😭
+//             row={row}
+//             virtualRow={virtualRow}
+//             {...rest}
+//           />
+//         ) : (
+//           <InternalRowWithoutRT {...rest} />
+//         )}
+//       </>
+//     );
+//   },
+// );
