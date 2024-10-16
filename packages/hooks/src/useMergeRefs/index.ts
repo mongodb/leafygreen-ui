ef155bcd3b71c8b18a090d7036e109e73f8292b7
Shaneeza <shaneeza.ali@mongodb.com>
2024-10-16 16:24:11 -0400

    added mergerefs

---
+++ packages/hooks/src/useMergeRefs/index.ts
@@ -0,0 +1,21 @@
+/**
+ *
+ * @param refs
+ * @returns
+ */
+
+export default function mergeRefs<T = any>(
+  refs: Array<
+    React.MutableRefObject<T> | React.LegacyRef<T> | undefined | null
+  >,
+): React.RefCallback<T> {
+  return value => {
+    refs.forEach(ref => {
+      if (typeof ref === 'function') {
+        ref(value);
+      } else if (ref != null) {
+        (ref as React.MutableRefObject<T | null>).current = value;
+      }
+    });
+  };
+}
