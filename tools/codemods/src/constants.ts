25ba4539e76b6d6815856b743b6b57ad334dfb61
Stephen Lee <stephen.lee@mongodb.com>
2024-10-18 14:22:31 -0700

    Add removeJSXAttributes code mod util

--- tools/codemods/src/constants.ts
+++ tools/codemods/src/constants.ts
@@ -1,4 +1,5 @@
 export const MIGRATOR_ERROR = {
   manualAdd: 'Please add manually',
+  manualRemove: 'Please remove manually',
   manualUpdate: 'Please update manually',
 };
---
+++ tools/codemods/src/utils/transformations/removeJSXAttributes/removeJSXAttributes.ts
@@ -0,0 +1,66 @@
+import type { ASTNode, ASTPath, JSXAttribute, JSXElement } from 'jscodeshift';
+import type core from 'jscodeshift';
+
+import { MIGRATOR_ERROR } from '../../../constants';
+import { insertJSXComment } from '../../jsx';
+
+export interface RemoveJSXAttributesType {
+  /**
+   *  A reference to the jscodeshift library
+   */
+  j: core.JSCodeshift;
+
+  /**
+   *  The element(component) to transform
+   */
+  element: ASTPath<JSXElement>;
+
+  /**
+   *  The name of the attribute that will be removed from the element
+   */
+  propName: string;
+}
+
+/**
+ * `removeJSXAttributes` can remove a value of an attribute(prop).
+ *
+ * e.g:
+ * ```tsx
+ * propName: prop
+ *
+ * Before:
+ * <MyComponent prop={false}/>
+ * After:
+ * <MyComponent />
+ * ```
+ */
+export function removeJSXAttributes({
+  j,
+  element,
+  propName,
+}: RemoveJSXAttributesType) {
+  const allAttributes = element.value.openingElement?.attributes;
+
+  const hasSpreadOperator = allAttributes?.some(
+    (attr: ASTNode) => attr.type !== 'JSXAttribute',
+  );
+
+  if (hasSpreadOperator) {
+    insertJSXComment(j, element, MIGRATOR_ERROR.manualRemove);
+    return;
+  }
+
+  const jsxAttributes = allAttributes?.filter(
+    (attr: ASTNode) =>
+      attr.type === 'JSXAttribute' && attr.name.name === propName,
+  );
+
+  if (!jsxAttributes) return;
+
+  jsxAttributes.forEach(attr => {
+    const jsxAttribute = attr as JSXAttribute;
+
+    jsxAttribute.name.name = '';
+    jsxAttribute.value = null;
+  });
+}
