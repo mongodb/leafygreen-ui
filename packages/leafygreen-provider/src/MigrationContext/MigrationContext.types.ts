c741181a690d9ff19196036f97236a43f2165fbb
Stephen Lee <stephen.lee@mongodb.com>
2024-10-24 14:47:48 -0700

    Add MigrationContext for global props

---
+++ packages/leafygreen-provider/src/MigrationContext/MigrationContext.types.ts
@@ -0,0 +1,7 @@
+export interface MigrationContextType {
+  /**
+   * Determines globally if popover elements using `Popover` component from `@leafygreen-ui/popover` package should render in top layer
+   * @internal
+   */
+  forceUseTopLayer?: boolean;
+}
