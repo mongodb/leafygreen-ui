a373db4aadb308a8b6500ffecb96ee67206fc6d2
Terrence Keane <terrence.keane@mongodb.com>
2024-10-24 13:57:46 -0400

    Fix build

--- charts/line-chart/src/LineChart.stories.tsx
+++ charts/line-chart/src/LineChart.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { storybookArgTypes } from '@lg-tools/storybook-utils';
 import { StoryFn } from '@storybook/react';
 
-import { css } from '@leafygreen-ui/emotion';
 import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
 
 import { LineChart } from './LineChart';
