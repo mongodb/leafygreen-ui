0ea0aea495ecb3c056f22bbfd57a31f3457d1f44
Terrence Keane <terrence.keane@mongodb.com>
2024-10-30 21:23:34 -0400

    Implement default tooltip

--- charts/core/src/Line/config/defaultLineOptions.ts
+++ charts/core/src/Line/config/defaultLineOptions.ts
@@ -1,14 +1,24 @@
-import { SeriesOption } from '../../Chart/Chart.types';
+import { SeriesOption } from '../../Chart';
 
 export const defaultLineOptions: Omit<SeriesOption, 'name' | 'data'> = {
   type: 'line',
   showSymbol: false,
   symbol: 'circle',
   clip: false,
+  symbolSize: 8,
   emphasis: {
-    disabled: true,
+    focus: 'series',
+    lineStyle: {
+      opacity: 1,
+    },
   },
   lineStyle: {
     width: 1,
+    type: 'solid',
+  },
+  blur: {
+    lineStyle: {
+      opacity: 0.5,
+    },
   },
 };
