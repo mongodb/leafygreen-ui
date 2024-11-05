955648fed35ae17fc461609b651e94f0cc558660
Terrence Keane <terrence.keane@mongodb.com>
2024-11-05 15:51:08 -0500

    Remove emphasis for now

--- charts/core/src/Line/config/defaultLineOptions.ts
+++ charts/core/src/Line/config/defaultLineOptions.ts
@@ -6,20 +6,8 @@ export const defaultLineOptions: Omit<SeriesOption, 'name' | 'data'> = {
   symbol: 'circle',
   clip: false,
   symbolSize: 8,
-  // emphasis: {
-  //   focus: 'series',
-  //   // lineStyle: {
-  //   //   opacity: 1,
-  //   // },
-  // },
   lineStyle: {
     width: 1,
     type: 'solid',
   },
-  // TODO: (TK) Ask Sooa if she wants to keep this
-  // blur: {
-  //   lineStyle: {
-  //     opacity: 0.5,
-  //   },
-  // },
 };
