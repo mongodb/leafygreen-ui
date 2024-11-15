03c0ad788e8afe848ecfa47e83fea998f1bdaf2c
Stephen Lee <stephen.lee@mongodb.com>
2024-11-15 11:04:14 -0800

    LG-4525: Support filtering by package name in popover-v12 codemod and component aliases (#2533)
    * Clean up codemods package configs
    
    * Add --packages arg to codemod command
    
    * Add LGPackages enum
    
    * Add packages to MigrateOptions
    
    * Support --packages in popover-v12 codemod
    
    * README and changesets
    
    * Ignore *.input.* and *.output.* files from dependency validation
    
    * Check if adding necessary and handle justify prop val replacement
    
    * Feedback

--- tools/codemods/src/codemods/popover-v12/tests/transform.spec.ts
+++ tools/codemods/src/codemods/popover-v12/tests/transform.spec.ts
@@ -7,16 +7,27 @@ const tests = [
     name: 'add-usePortal-consolidate-renderMode',
   },
   {
+    name: 'filter-packages',
+    options: {
+      packages: ['@leafygreen-ui/popover', '@leafygreen-ui/select'],
+    },
+  },
+  {
     name: 'remove-legacy-props',
   },
   {
     name: 'remove-shouldTooltipUsePortal',
   },
+  {
+    name: 'replace-justify-fit',
+  },
 ];
 
 for (const test of tests) {
   transformTest(__dirname, {
+    extension: 'jsx',
     fixture: test.name,
     transform,
+    options: test.options,
   });
 }
