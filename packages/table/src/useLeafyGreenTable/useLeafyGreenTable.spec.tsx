ba443a7f01b66d93fb0585d7595345c651c32166
Shaneeza <shaneeza.ali@mongodb.com>
2024-11-21 18:03:29 -0500

    update hooks tests

--- packages/table/src/useLeafyGreenTable/useLeafyGreenTable.spec.tsx
+++ packages/table/src/useLeafyGreenTable/useLeafyGreenTable.spec.tsx
@@ -5,40 +5,13 @@ import useLeafyGreenTable from './useLeafyGreenTable';
 describe('packages/table/useLeafyGreenTable', () => {
   // eslint-disable-next-line jest/no-disabled-tests
   test.skip('Typescript', () => {
-    // @ts-expect-error - requires containerRef, columns, data
+    // @ts-expect-error - requires columns, data
     useLeafyGreenTable({});
 
     // containerRef, columns, data are necessary & sufficient options
     useLeafyGreenTable({
-      containerRef: React.createRef<HTMLDivElement>(),
       columns: [],
       data: [],
-    });
-
-    const necessaryOptions = {
-      containerRef: React.createRef<HTMLDivElement>(),
-      columns: [],
-      data: [],
-    };
-
-    // @ts-expect-error - virtualizerOptions is not valid when useVirtualScrolling is false/undefined
-    useLeafyGreenTable({
-      ...necessaryOptions,
-      virtualizerOptions: {},
-    });
-
-    // @ts-expect-error - virtualizerOptions is not valid when useVirtualScrolling is false/undefined
-    useLeafyGreenTable({
-      ...necessaryOptions,
-      useVirtualScrolling: false,
-      virtualizerOptions: {},
-    });
-
-    // virtualizerOptions is a valid prop when useVirtualScrolling is true
-    useLeafyGreenTable({
-      ...necessaryOptions,
-      useVirtualScrolling: true,
-      virtualizerOptions: {},
     });
   });
 });
--- packages/table/src/useLeafyGreenVirtualTable/useLeafyGreenVirtualTable.spec.tsx
+++ packages/table/src/useLeafyGreenVirtualTable/useLeafyGreenVirtualTable.spec.tsx
@@ -1,1 +1,31 @@
-// TODO: adding in a new PR
+import React from 'react';
+
+import useLeafyGreenVirtualTable from './useLeafyGreenVirtualTable';
+
+describe('packages/table/useLeafyGreenVirtualTable', () => {
+  // eslint-disable-next-line jest/no-disabled-tests
+  test.skip('Typescript', () => {
+    // @ts-expect-error - requires columns, data, containerRef
+    useLeafyGreenVirtualTable({});
+
+    // @ts-expect-error - requires containerRef
+    useLeafyGreenVirtualTable({
+      columns: [],
+      data: [],
+    });
+
+    // containerRef, columns, data are necessary & sufficient options
+    useLeafyGreenVirtualTable({
+      containerRef: React.createRef<HTMLDivElement>(),
+      columns: [],
+      data: [],
+    });
+
+    useLeafyGreenVirtualTable({
+      containerRef: React.createRef<HTMLDivElement>(),
+      columns: [],
+      data: [],
+      virtualizerOptions: {},
+    });
+  });
+});
