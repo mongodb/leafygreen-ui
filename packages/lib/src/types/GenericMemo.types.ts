ec9ab355b227fa51db9e2b0c8e4717cdecc14d08
Shaneeza <shaneeza.ali@mongodb.com>
2024-11-25 15:28:21 -0500

    comments

--- packages/lib/src/types/GenericMemo.types.ts
+++ packages/lib/src/types/GenericMemo.types.ts
@@ -4,13 +4,19 @@
 
 import { PropsWithoutRef, WeakValidationMap } from 'react';
 
+// The function signature
 export interface GenericMemo {
+  // <T> is a generic parameter that represents the type of the component being wrapped by GenericMemo
+  // keyof JSX.IntrinsicElements Covers intrinsic JSX elements
+  // React.JSXElementConstructor<any> Covers React components (e.g., functional components, class components). These are custom components.
   <T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>>(
     component: T,
     propsAreEqual?: (
       prevProps: React.ComponentPropsWithRef<T>,
       nextProps: React.ComponentPropsWithRef<T>,
     ) => boolean,
+    // Return type
+    // The returned component is of the same type as the input component T
   ): T & {
     displayName?: string;
     propTypes?:
@@ -18,3 +24,14 @@ export interface GenericMemo {
       | undefined;
   };
 }
+
+// export interface GenericMemo {
+// // This constraint only allows T to be custom React components
+//   <T extends React.ComponentType<any>>(
+//     component: T,
+//     propsAreEqual?: (
+//       prevProps: React.ComponentPropsWithRef<T>,
+//       nextProps: React.ComponentPropsWithRef<T>,
+//     ) => boolean,
+//   ): React.MemoExoticComponent<T>;
+// }
