911605643213829c661cc124d333751f6bb81348
Adam Thompson <adam.thompson@mongodb.com>
2024-12-13 03:30:27 +0900

    use newUTC for consistency

--- packages/date-utils/src/getDaysInUTCMonth/getDaysInUTCMonth.spec.ts
+++ packages/date-utils/src/getDaysInUTCMonth/getDaysInUTCMonth.spec.ts
@@ -1,23 +1,26 @@
+import { Month } from '../constants';
+import { newUTC } from '../newUTC';
+
 import { getDaysInUTCMonth } from '.';
 
 describe('packages/date-utils/getDaysInUTCMonth', () => {
   test('returns the number of days in the month', () => {
-    const result = getDaysInUTCMonth(new Date("2100-02-11"));
+    const result = getDaysInUTCMonth(newUTC(2100, Month.February, 11));
     expect(result).toEqual(28);
   });
 
   test('when given the 1st of the month', () => {
-    const result = getDaysInUTCMonth(new Date("2100-02-01"));
+    const result = getDaysInUTCMonth(newUTC(2100, Month.February, 1));
     expect(result).toEqual(28);
   });
 
   test('when given the last of the month', () => {
-    const result = getDaysInUTCMonth(new Date("2100-02-28"));
+    const result = getDaysInUTCMonth(newUTC(2100, Month.February, 28));
     expect(result).toEqual(28);
   });
 
   test('when given February of a leap year', () => {
-    const result = getDaysInUTCMonth(new Date("2000-02-01"));
+    const result = getDaysInUTCMonth(newUTC(2000, Month.February, 1));
     expect(result).toEqual(29);
   });
 
