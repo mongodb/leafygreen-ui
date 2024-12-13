f29ad550d0ca0c66195797a6390d9b2cf028d373
Terrence Keane <terrence.keane@mongodb.com>
2024-12-13 13:01:09 -0500

    Improve typing

---
+++ charts/core/src/Echarts/echarts.types.ts
@@ -0,0 +1,85 @@
+import { ChartOptions, SeriesOption } from '../Chart';
+
+type EChartsEvents =
+  // Mouse Events
+  | 'click' // Triggered when clicking on a series, data item, or other graphical element
+  | 'dblclick' // Triggered when double clicking
+  | 'mousedown' // Triggered when pressing down the mouse button
+  | 'mouseup' // Triggered when releasing the mouse button
+  | 'mouseover' // Triggered when mouse hovers over a component
+  | 'mouseout' // Triggered when mouse leaves a component
+  | 'mousemove' // Triggered when mouse moves within a component
+  | 'globalout' // Triggered when mouse leaves the chart area
+  | 'contextmenu' // Triggered when right clicking
+
+  // Drag Events
+  | 'dragstart' // Triggered when starting to drag a component
+  | 'drag' // Triggered while dragging
+  | 'dragend' // Triggered when ending a drag operation
+
+  // Brush Events
+  | 'brushselected' // Triggered when selecting an area using the brush tool
+  | 'brushEnd' // Triggered when brush selection ends
+  | 'brush' // Triggered during brush selection
+
+  // Legend Events
+  | 'legendselectchanged' // Triggered when legend selection changes
+  | 'legendselected' // Triggered when clicking legend item
+  | 'legendunselected' // Triggered when unselecting legend item
+  | 'legendselectall' // Triggered when selecting all legend items
+  | 'legendinverseselect' // Triggered when inversely selecting legend items
+  | 'legendscroll' // Triggered when scrolling legend
+
+  // Dataoom Events
+  | 'datazoom' // Triggered when data zoom area changes
+  | 'datarangeselected' // Triggered when selecting a range in continuous visual map
+  | 'timelinechanged' // Triggered when timeline changes
+  | 'timelineplaychanged' // Triggered when timeline play state changes
+  | 'restore' // Triggered when restoring chart to initial state
+
+  // Map Events
+  | 'georoam' // Triggered when roaming (zooming/panning) in geo coordinate system
+  | 'geoselected' // Triggered when selecting areas in geo coordinate system
+  | 'geounselected' // Triggered when unselecting areas in geo coordinate system
+
+  // Axis Events
+  | 'axisareaselected' // Triggered when selecting an axis area
+  | 'focusnodeadjacency' // Triggered when focusing on adjacent nodes (graph)
+  | 'unfocusnodeadjacency' // Triggered when unfocusing adjacent nodes (graph)
+
+  // Rendering Events
+  | 'rendered' // Triggered after chart rendering finished
+  | 'finished' // Triggered after chart animation finished
+
+  // Chart Action Events
+  | 'magictypechanged' // Triggered when changing chart type using magic type switcher
+  | 'tooltipshown' // Triggered when tooltip is shown
+  | 'tooltiphidden' // Triggered when tooltip is hidden
+  | 'selectchanged' // Triggered when selection state changes
+  | 'globalcursortaken'; // Triggered when global cursor is taken
+
+export interface setupZoomSelectProps {
+  xAxis?: boolean;
+  yAxis?: boolean;
+}
+
+export interface EChartsInstance {
+  _echartsInstance: any;
+  ready: boolean;
+  options: Partial<ChartOptions>;
+  updateOptions: (options: Omit<Partial<ChartOptions>, 'series'>) => void;
+  on: (
+    event: EChartsEvents | 'zoomselect',
+    callback: (params: any) => void,
+  ) => void;
+  off: (
+    event: EChartsEvents | 'zoomselect',
+    callback: (params: any) => void,
+  ) => void;
+  addSeries: (series: SeriesOption) => void;
+  removeSeries: (name: string) => void;
+  addToGroup: (groupId: string) => void;
+  removeFromGroup: () => void;
+  setupZoomSelect: (setupZoomSelectProps: setupZoomSelectProps) => void;
+  error: Error | null;
+}
