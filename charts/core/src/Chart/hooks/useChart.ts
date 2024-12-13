ab255d3ca51982b495cc8099b26a42e4103a30b2
Terrence Keane <terrence.keane@mongodb.com>
2024-12-13 10:47:08 -0500

    Working state

--- charts/core/src/Chart/hooks/useChart.ts
+++ charts/core/src/Chart/hooks/useChart.ts
@@ -7,24 +7,26 @@ import { getDefaultChartOptions } from '../config';
 
 import { addSeries, removeSeries, updateOptions } from './updateUtils';
 import { ChartHookProps, ZoomSelectionEvent } from './useChart.types';
-import { useEchart } from '../../Echarts/useEchart';
+import { useEchart } from '../../echarts/useEchart';
 
-/**
- * Creates a generic Apache ECharts options object with default values for those not set
- * that are in line with the designs and needs of the design system.
- */
 export function useChart({
-  theme,
   onChartReady = () => {},
   zoomSelect,
   onZoomSelect,
   groupId,
 }: ChartHookProps) {
   const chartRef = useRef(null);
-  const { chart, core, addToGroup } = useEchart(chartRef.current);
-  const [chartOptions, setChartOptions] = useState(
-    getDefaultChartOptions(theme),
-  );
+  const {
+    chart,
+    isLoading,
+    chartOptions,
+    on,
+    addToGroup,
+    setupZoomSelect,
+    addChartSeries,
+    removeChartSeries,
+    updateChartOptions,
+  } = useEchart(chartRef.current);
 
   function handleDataZoom(params: any) {
     if (onZoomSelect) {
@@ -61,138 +63,28 @@ export function useChart({
         );
       }
     }
-
-    /**
-     * If start is not 0% or end is not 100%, the 'dataZoom' event was triggered by a zoom.
-     * We override the zoom to provide new values to the handler.
-     */
-    const isZoomed = params?.start !== 0 || params?.end !== 100;
-
-    if (chart && isZoomed) {
-      chart.dispatchAction({
-        type: 'dataZoom',
-        start: 0, // percentage of starting position
-        end: 100, // percentage of ending position
-      });
-    }
   }
 
-  // Initialize the chart
   useEffect(() => {
-    if (!chart || !core) {
-      return;
-    }
-
-    chart.setOption(chartOptions);
-
-    if (groupId) {
-      addToGroup(groupId);
-    }
-
-    // ECharts does not automatically resize when the window resizes.
-    const resizeHandler = () => {
-      chart.resize();
-    };
-    window.addEventListener('resize', resizeHandler);
-
-    chart.on('rendered', function onInitialRender() {
-      // Enable zooming by default by dispatching the "dataZoomSelect" action.
-      chart.dispatchAction({
-        type: 'takeGlobalCursor',
-        key: 'dataZoomSelect',
-        dataZoomSelectActive: zoomSelect?.xAxis || zoomSelect?.yAxis,
-      });
-
-      chart.on('dataZoom', handleDataZoom);
+    if (chart) {
       onChartReady();
-      chart.off('rendered', onInitialRender);
-    });
-
-    return () => {
-      window.removeEventListener('resize', resizeHandler);
-      chart.dispose();
-    };
-  }, [chart, core]);
-
-  // Set which axis zoom is enabled on
-  useEffect(() => {
-    // `0` index enables zoom on that index, `'none'` disables zoom on that index
-    let xAxisIndex: number | string = 0;
-    let yAxisIndex: number | string = 0;
-    if (!zoomSelect?.xAxis) {
-      xAxisIndex = 'none';
     }
-    if (!zoomSelect?.yAxis) {
-      yAxisIndex = 'none';
-    }
-    updateChartOptions({
-      toolbox: {
-        feature: {
-          dataZoom: {
-            xAxisIndex,
-            yAxisIndex,
-          },
-        },
-      },
-    });
-  }, [zoomSelect]);
-
-  const updateChartRef = useMemo(
-    () =>
-      debounce((chartOptions: Partial<ChartOptions>) => {
-        /**
-         * The second argument is `true` to merge the new options with the existing ones.
-         * This is needed to ensure that series get removed properly.
-         * See issue: https://github.com/apache/echarts/issues/6202
-         * */
-        chart?.setOption(chartOptions, true);
-      }, 50),
-    [],
-  );
-
-  const addChartSeries = useCallback(
-    (data: SeriesOption) => {
-      setChartOptions(currentOptions => {
-        const updatedOptions = addSeries(currentOptions, data);
-        updateChartRef(updatedOptions);
-        return updatedOptions;
-      });
-    },
-    [updateChartRef],
-  );
-
-  const removeChartSeries = useCallback(
-    (name: string) => {
-      setChartOptions(currentOptions => {
-        const updatedOptions = removeSeries(currentOptions, name);
-        updateChartRef(updatedOptions);
-        return updatedOptions;
-      });
-    },
-    [updateChartRef],
-  );
-
-  const updateChartOptions = useCallback(
-    (options: Omit<Partial<ChartOptions>, 'series'>) => {
-      setChartOptions(currentOptions => {
-        const updatedOptions = updateOptions(currentOptions, options);
-        updateChartRef(updatedOptions);
-        return updatedOptions;
-      });
-    },
-    [updateChartRef],
-  );
+  }, [chart]);
 
   useEffect(() => {
-    setChartOptions(currentOptions => {
-      const updatedOptions = {
-        ...currentOptions,
-        color: chartSeriesColors[theme],
-      };
-      updateChartRef(updatedOptions);
-      return updatedOptions;
-    });
-  }, [theme]);
+    if (chart) {
+      if (groupId) {
+        addToGroup(groupId);
+      }
+    }
+  }, [chart, groupId]);
+
+  useEffect(() => {
+    if (chart) {
+      setupZoomSelect({ xAxis: zoomSelect?.xAxis, yAxis: zoomSelect?.yAxis });
+      on('zoomSelect', handleDataZoom);
+    }
+  }, [chart, zoomSelect]);
 
   return {
     chartOptions,
--- charts/core/src/Echarts/useEchart.ts
+++ charts/core/src/Echarts/useEchart.ts
@@ -1,4 +1,16 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import debounce from 'lodash.debounce';
+
+import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
+
+import { getDefaultChartOptions } from '../Chart/config';
+import { ChartOptions, SeriesOption } from '../Chart';
+import {
+  addSeries,
+  removeSeries,
+  updateOptions,
+} from '../Chart/hooks/updateUtils';
+import { chartSeriesColors } from '../Chart/chartSeriesColors';
 
 type EchartsType = any; // has to be any since no types exist until import
 
@@ -63,21 +75,36 @@ async function initializeEcharts() {
 
 export function useEchart(container: HTMLDivElement | null) {
   const [chart, setChart] = useState<EchartsType | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isReady, setisReady] = useState(true);
   const [error, setError] = useState<Error | null>(null);
+  const { theme } = useDarkMode();
+  const [chartOptions, setChartOptions] = useState(
+    getDefaultChartOptions(theme),
+  );
+
+  // ECharts does not automatically resize when the window resizes.
+  const resizeHandler = () => {
+    if (chart) {
+      chart.resize();
+    }
+  };
 
   useEffect(() => {
     (async function setupChart() {
       try {
-        setIsLoading(true);
+        setisReady(true);
         setError(null);
 
         await initializeEcharts();
 
         if (container) {
           const newChart = echartsCore.init(container);
+          newChart.setOption(chartOptions);
+          window.addEventListener('resize', resizeHandler);
           setChart(newChart);
         }
+
+        console.log('is ready');
       } catch (err) {
         console.error(err);
         setError(
@@ -86,22 +113,173 @@ export function useEchart(container: HTMLDivElement | null) {
             : new Error('Failed to initialize ECharts'),
         );
       } finally {
-        setIsLoading(false);
+        setisReady(false);
       }
     })();
 
     // Cleanup function
     return () => {
       if (chart) {
+        window.removeEventListener('resize', resizeHandler);
         chart.dispose();
       }
     };
   }, [container]);
+
+  useEffect(() => {
+    setChartOptions(currentOptions => {
+      const updatedOptions = {
+        ...currentOptions,
+        color: chartSeriesColors[theme],
+      };
+      setEchartOptions(updatedOptions);
+      return updatedOptions;
+    });
+  }, [theme]);
 
   function addToGroup(groupId: string) {
     chart.group = groupId;
     echartsCore.connect(groupId);
   }
 
-  return { chart, addToGroup, core: echartsCore, isLoading, error };
+  function removeFromGroup() {
+    chart.group = null;
+  }
+
+  function setupZoomSelect({
+    xAxis,
+    yAxis,
+  }: {
+    xAxis?: boolean;
+    yAxis?: boolean;
+  }) {
+    if (chart) {
+      // `0` index enables zoom on that index, `'none'` disables zoom on that index
+      let xAxisIndex: number | string = 0;
+      let yAxisIndex: number | string = 0;
+
+      if (!xAxis) {
+        xAxisIndex = 'none';
+      }
+
+      if (!yAxis) {
+        yAxisIndex = 'none';
+      }
+
+      updateChartOptions({
+        toolbox: {
+          feature: {
+            dataZoom: {
+              xAxisIndex,
+              yAxisIndex,
+            },
+          },
+        },
+      });
+
+      chart.on('rendered', () => {
+        chart.dispatchAction({
+          type: 'takeGlobalCursor',
+          key: 'dataZoomSelect',
+          dataZoomSelectActive: xAxis || yAxis,
+        });
+      });
+
+      chart.on('dataZoom', (params: any) => {
+        /**
+         * If start is not 0% or end is not 100%, the 'dataZoom' event was triggered by a zoom.
+         * We override the zoom to prevent it from actually zooming.
+         */
+        const isZoomed = params?.start !== 0 || params?.end !== 100;
+
+        if (chart && isZoomed) {
+          chart.dispatchAction({
+            type: 'dataZoom',
+            start: 0, // percentage of starting position
+            end: 100, // percentage of ending position
+          });
+        }
+      });
+    }
+  }
+
+  // TODO: update type
+  function on(action: 'zoomSelect' | string, callback: any) {
+    switch (action) {
+      case 'zoomSelect': {
+        chart.on('dataZoom', (params: any) => {
+          callback(params); // TODO: Add logic
+        });
+        break;
+      }
+      default: {
+        chart.on(action, callback);
+      }
+    }
+  }
+
+  function off(action: string, callback: any) {
+    chart.off(action, callback);
+  }
+
+  const setEchartOptions = useMemo(
+    () =>
+      debounce((chartOptions: Partial<ChartOptions>) => {
+        /**
+         * The second argument is `true` to merge the new options with the existing ones.
+         * This is needed to ensure that series get removed properly.
+         * See issue: https://github.com/apache/echarts/issues/6202
+         * */
+        chart?.setOption(chartOptions, true);
+      }, 50),
+    [],
+  );
+
+  const addChartSeries = useCallback(
+    (data: SeriesOption) => {
+      setChartOptions(currentOptions => {
+        const updatedOptions = addSeries(currentOptions, data);
+        setEchartOptions(updatedOptions);
+        return updatedOptions;
+      });
+    },
+    [setEchartOptions],
+  );
+
+  const removeChartSeries = useCallback(
+    (name: string) => {
+      setChartOptions(currentOptions => {
+        const updatedOptions = removeSeries(currentOptions, name);
+        setEchartOptions(updatedOptions);
+        return updatedOptions;
+      });
+    },
+    [setEchartOptions],
+  );
+
+  const updateChartOptions = useCallback(
+    (options: Omit<Partial<ChartOptions>, 'series'>) => {
+      setChartOptions(currentOptions => {
+        const updatedOptions = updateOptions(currentOptions, options);
+        setEchartOptions(updatedOptions);
+        return updatedOptions;
+      });
+    },
+    [setEchartOptions],
+  );
+
+  return {
+    chart,
+    chartOptions,
+    addToGroup,
+    removeFromGroup,
+    setupZoomSelect,
+    on,
+    off,
+    addChartSeries,
+    removeChartSeries,
+    updateChartOptions,
+    isReady,
+    error,
+  };
 }
