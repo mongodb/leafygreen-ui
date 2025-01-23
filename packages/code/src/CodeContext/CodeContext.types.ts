a02a75628260a0b8bb540fb59f84edae095ff813
Shaneeza <shaneeza.ali@mongodb.com>
2025-01-23 11:55:27 -0500

    fix build error

--- packages/code/src/CodeContext/CodeContext.types.ts
+++ packages/code/src/CodeContext/CodeContext.types.ts
@@ -1,17 +1,15 @@
 import { DarkModeProps } from '@leafygreen-ui/lib';
-import { PropsWithChildren } from 'react';
 import { LanguageOption } from '../Panel/Panel.types';
 import { Language } from '../types';
 
-export type CodeProviderProps = PropsWithChildren &
-  DarkModeProps & {
-    /**
-     * The contents of the code snippet.
-     */
-    contents: string;
+export type CodeProviderProps = DarkModeProps & {
+  /**
+   * The contents of the code snippet.
+   */
+  contents: string;
 
-    /**
-     * The language of the code snippet.
-     */
-    language: Language | LanguageOption;
-  };
+  /**
+   * The language of the code snippet.
+   */
+  language: Language | LanguageOption;
+};
