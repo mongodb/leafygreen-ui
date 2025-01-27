bd339cfccf8318be8d9d2efde9d7a5e96ebb6821
Shaneeza <shaneeza.ali@mongodb.com>
2025-01-27 08:00:36 -0500

    lint

--- packages/code/src/Code/Code.tsx
+++ packages/code/src/Code/Code.tsx
@@ -10,14 +10,15 @@ import { useBaseFontSize } from '@leafygreen-ui/leafygreen-provider';
 
 import CodeContextProvider from '../CodeContext/CodeContext';
 import { LGIDs, numOfCollapsedLinesOfCode } from '../constants';
+import CopyButton from '../CopyButton/CopyButton';
 import { Panel } from '../Panel';
 import { Syntax } from '../Syntax';
 import { Language } from '../types';
 
 import {
-  getCopyButtonWithoutPanelStyles,
   getCodeStyles,
   getCodeWrapperStyles,
+  getCopyButtonWithoutPanelStyles,
   getExpandedButtonStyles,
   wrapperStyle,
 } from './Code.styles';
@@ -27,7 +28,6 @@ import {
   DetailedElementProps,
   ScrollState,
 } from './Code.types';
-import CopyButton from '../CopyButton/CopyButton';
 
 //TODO: move to utils
 export function hasMultipleLines(string: string): boolean {
