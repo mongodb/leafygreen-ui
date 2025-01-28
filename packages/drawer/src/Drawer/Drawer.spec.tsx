c5f427ffb40b7cd0582cf6bf26fcd3413fc9d691
Stephen Lee <stephen.lee@mongodb.com>
2025-01-28 09:36:16 -0800

    Create @leafygreen-ui/drawer package (#2662)

---
+++ packages/drawer/src/Drawer/Drawer.spec.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Drawer } from '.';
+
+describe('packages/drawer', () => {
+  test('condition', () => {});
+});
