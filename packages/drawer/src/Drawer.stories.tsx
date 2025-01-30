6f943a6b3c4bb21fa1c18899fc7372970ffb4da4
Stephen Lee <stephen.lee@mongodb.com>
2025-01-30 11:40:57 -0800

    LG-4808: basic Drawer component (#2666)
    * Add and install @leafygreen-ui/drawer dependencies
    
    * Implement basic Drawer component
    
    * Init README
    
    * Changeset stub
    
    * Feedback

--- packages/drawer/src/Drawer.stories.tsx
+++ packages/drawer/src/Drawer.stories.tsx
@@ -1,13 +1,70 @@
-import React from 'react';
-import { StoryFn } from '@storybook/react';
+import React, { useState } from 'react';
+import { storybookArgTypes, StoryMetaType } from '@lg-tools/storybook-utils';
+import { StoryObj } from '@storybook/react';
 
-import { Drawer } from '.';
+import Button from '@leafygreen-ui/button';
+
+import { Drawer, DrawerProps } from '.';
 
 export default {
   title: 'Components/Drawer',
   component: Drawer,
+  parameters: {
+    default: 'LiveExample',
+    controls: {
+      exclude: [],
+    },
+    generate: {
+      storyNames: ['LightMode', 'DarkMode'],
+      combineArgs: {
+        title: ['Drawer Title'],
+      },
+    },
+  },
+  args: {
+    title: 'Drawer Title',
+  },
+  argTypes: {
+    darkMode: storybookArgTypes.darkMode,
+    title: {
+      control: 'text',
+    },
+  },
+} satisfies StoryMetaType<typeof Drawer>;
+
+const LiveExampleComponent = args => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setOpen(prevOpen => !prevOpen)}>
+        Open Drawer
+      </Button>
+      <Drawer {...args} open={open} setOpen={setOpen} />
+    </div>
+  );
 };
 
-const Template: StoryFn<typeof Drawer> = props => <Drawer {...props} />;
+export const LiveExample: StoryObj<DrawerProps> = {
+  render: LiveExampleComponent,
+  parameters: {
+    chromatic: {
+      disableSnapshot: true,
+    },
+  },
+};
 
-export const Basic = Template.bind({});
+export const LightMode: StoryObj<DrawerProps> = {
+  render: () => <></>,
+  args: {
+    darkMode: false,
+    open: true,
+  },
+};
+
+export const DarkMode: StoryObj<DrawerProps> = {
+  render: () => <></>,
+  args: {
+    darkMode: true,
+    open: true,
+  },
+};
--- packages/drawer/src/Drawer/Drawer.types.ts
+++ packages/drawer/src/Drawer/Drawer.types.ts
@@ -1,1 +1,23 @@
-export interface DrawerProps {}
+import React from 'react';
+
+import { DarkModeProps, HTMLElementProps } from '@leafygreen-ui/lib';
+
+export interface DrawerProps
+  extends Omit<HTMLElementProps<'div'>, 'title'>,
+    DarkModeProps {
+  /**
+   * Determines if the Drawer is open or closed
+   * @defaultValue false
+   */
+  open?: boolean;
+
+  /**
+   * Callback to change the open state of the Drawer
+   */
+  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+
+  /**
+   * Title of the Drawer
+   */
+  title: React.ReactNode;
+}
