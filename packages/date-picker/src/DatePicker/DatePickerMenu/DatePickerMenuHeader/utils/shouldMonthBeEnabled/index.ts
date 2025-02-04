8e0e8e6ca937b4d3c0c8f8ad5b8bdeb34a142ca4
Terrence Keane <terrence.keane@mongodb.com>
2025-02-04 12:08:49 -0500

    Fix

--- packages/date-picker/src/DatePicker/DatePickerMenu/DatePickerMenuHeader/utils/shouldMonthBeEnabled/index.ts
+++ packages/date-picker/src/DatePicker/DatePickerMenu/DatePickerMenuHeader/utils/shouldMonthBeEnabled/index.ts
@@ -21,22 +21,14 @@ export const shouldMonthBeEnabled = (
 
   const { month, min, max } = context;
 
-  const year = context.month?.getUTCFullYear();
-  const minYear = context.min?.getUTCFullYear();
-  const maxYear = context.max?.getUTCFullYear();
+  const year = month?.getUTCFullYear();
+  const minYear = min?.getUTCFullYear();
+  const maxYear = max?.getUTCFullYear();
 
   if (year && minYear && year < minYear) return false;
   if (year && maxYear && year > maxYear) return false;
-
-  if (month && min && year === minYear) {
-    if (monthIndex < min.getUTCMonth()) return false;
-    return true;
-  }
-
-  if (month && max && year === maxYear) {
-    if (monthIndex > max.getUTCMonth()) return false;
-    return true;
-  }
+  if (min && monthIndex < min.getUTCMonth()) return false;
+  if (max && monthIndex > max.getUTCMonth()) return false;
 
   return true;
 };
