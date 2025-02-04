aa06a16ee3f025946b9e3ed812c6b0f4a56d761b
Stephen Lee <stephen.lee@mongodb.com>
2025-02-04 14:10:09 -0800

    Make Description component polymorphic

--- packages/typography/src/Description/Description.tsx
+++ packages/typography/src/Description/Description.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import { cx } from '@leafygreen-ui/emotion';
 import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
+import {
+  Polymorphic,
+  PolymorphicAs,
+  usePolymorphic,
+} from '@leafygreen-ui/polymorphic';
 
 import { LGIDS_TYPOGRAPHY } from '../constants';
 import { useUpdatedBaseFontSize } from '../utils/useUpdatedBaseFontSize';
@@ -13,35 +18,39 @@ import {
 } from './Description.styles';
 import { DescriptionProps } from './Description.types';
 
-export const Description = ({
-  baseFontSize: baseFontSizeOverride,
-  'data-lgid': dataLgId = LGIDS_TYPOGRAPHY.description,
-  darkMode: darkModeProp,
-  disabled = false,
-  children,
-  className,
-  ...rest
-}: DescriptionProps) => {
-  const { theme } = useDarkMode(darkModeProp);
-  const baseFontSize = useUpdatedBaseFontSize(baseFontSizeOverride);
+export const Description = Polymorphic<DescriptionProps>(
+  ({
+    as = 'p' as PolymorphicAs,
+    baseFontSize: baseFontSizeOverride,
+    children,
+    className,
+    darkMode: darkModeProp,
+    'data-lgid': dataLgId = LGIDS_TYPOGRAPHY.description,
+    disabled = false,
+    ...rest
+  }) => {
+    const { theme } = useDarkMode(darkModeProp);
+    const baseFontSize = useUpdatedBaseFontSize(baseFontSizeOverride);
+    const { Component } = usePolymorphic(as);
 
-  return (
-    <p
-      data-lgid={dataLgId}
-      className={cx(
-        getDescriptionStyle(theme),
-        descriptionTypeScaleStyles[baseFontSize],
-        {
-          [getDisabledDescriptionColorStyle(theme)]: disabled,
-        },
-        className,
-      )}
-      {...rest}
-    >
-      {children}
-    </p>
-  );
-};
+    return (
+      <Component
+        data-lgid={dataLgId}
+        className={cx(
+          getDescriptionStyle(theme),
+          descriptionTypeScaleStyles[baseFontSize],
+          {
+            [getDisabledDescriptionColorStyle(theme)]: disabled,
+          },
+          className,
+        )}
+        {...rest}
+      >
+        {children}
+      </Component>
+    );
+  },
+);
 
 Description.displayName = 'Description';
 
