be37d97b35db3f82805f6a0b2fc057029cb85669
Adam Thompson <adam.thompson@mongodb.com>
2025-02-05 10:43:57 -0500

    comments

--- tools/meta/src/getPackageJson/getPackageJson.spec.ts
+++ tools/meta/src/getPackageJson/getPackageJson.spec.ts
@@ -17,6 +17,7 @@ describe('tools/meta/getPackageJson', () => {
     fsx.rmdirSync('./tmp/');
   });
 
+  // TODO: TBD whether this is the behavior we want, but this is how it's currently implemented
   test('finds the parent package.json by default', () => {
     const packageJson = getPackageJson('./tmp');
     expect(packageJson).toEqual(
@@ -24,7 +25,7 @@ describe('tools/meta/getPackageJson', () => {
     );
   });
 
-  test('returns the package.json data', () => {
+  test('returns the local package.json data', () => {
     const pkgJson = { name: 'test-package', version: '0.0.0' };
     fsx.writeJSONSync('./tmp/package.json', pkgJson);
     const packageJson = getPackageJson('./tmp');
