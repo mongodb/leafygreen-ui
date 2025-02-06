a24e573cad5435a51e199f89a477258c1df28dc6
Shaneeza <shaneeza.ali@mongodb.com>
2025-02-06 13:08:07 -0500

    the correct path

--- packages/code/src/index.ts
+++ packages/code/src/index.ts
@@ -6,8 +6,8 @@ export { variantColors } from './globalStyles';
 export { Panel } from './Panel';
 export type { LanguageOption } from './Panel/Panel.types';
 export type { SyntaxProps } from './Syntax/Syntax.types';
-export { getTestUtils } from './testUtils';
 export type { LineHighlightingDefinition } from './types';
 export { Language } from './types';
+export { getTestUtils } from './utils/getTestUtils/getTestUtils';
 
 export default Code;
