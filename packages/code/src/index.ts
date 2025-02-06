f9102a95b41b551039bb0244077e85799a30efb0
Shaneeza <shaneeza.ali@mongodb.com>
2025-02-06 11:55:18 -0500

    add changetset and export utils

--- packages/code/src/index.ts
+++ packages/code/src/index.ts
@@ -1,10 +1,12 @@
 import { Code, CopyButtonAppearance } from './Code';
 export { Code, CopyButtonAppearance };
 export type { CodeProps } from './Code/Code.types';
+export { LGIDs } from './constants';
 export { variantColors } from './globalStyles';
 export { Panel } from './Panel';
 export type { LanguageOption } from './Panel/Panel.types';
 export type { SyntaxProps } from './Syntax/Syntax.types';
+export { getTestUtils } from './testUtils';
 export type { LineHighlightingDefinition } from './types';
 export { Language } from './types';
 
