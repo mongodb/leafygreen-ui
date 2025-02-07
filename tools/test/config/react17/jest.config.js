000424d75e8328296aa58e8b226405a637c28142
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2025-02-07 17:07:09 -0500

    Update jest.config.js

--- tools/test/config/react17/jest.config.js
+++ tools/test/config/react17/jest.config.js
@@ -7,4 +7,8 @@ module.exports = {
     ...baseConfig.testPathIgnorePatterns,
     'packages/combobox', // Combobox tests are problematic in React 17
   ],
+  setupFiles: [
+    '<rootDir>/node_modules/@lg-tools/test/config/react17/setup.js',
+    'jest-canvas-mock',
+  ],
 };
