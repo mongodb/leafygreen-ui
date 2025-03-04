f3121f2211abe0cefd049bbd42a7b885ac7bb1ce
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-03-04 13:14:35 -0500

    Clean up update-readme.ts

--- scripts/update-readme.ts
+++ scripts/update-readme.ts
@@ -21,25 +21,28 @@ async function buildTable() {
   const packages = getAllPackageNames();
   const lgConfig = getLGConfig();
 
-  const packagesTable = packages.reduce(
-    (md, pkg) => {
-      const [scope, pkgName] = pkg.split('/');
-      const scopePath = lgConfig.scopes[scope];
-      const pkgMd = `[${pkg}](./${scopePath}/${pkgName})`;
-      const versionMd = `[![version](https://img.shields.io/npm/v/${pkg})](https://www.npmjs.com/package/${pkg})`;
-      const downloadsMd = `![downloads](https://img.shields.io/npm/dm/${pkg}?color=white)`;
-      const exampleMd =
-        scope !== '@lg-tools'
-          ? `[Live Example](http://mongodb.design/component/${pkgName}/live-example)`
-          : '';
-      md +=
-        '\n' + `| ${pkgMd} | ${versionMd} | ${downloadsMd} | ${exampleMd} |`;
-      return md;
-    },
-    `## Packages
+  const initialTable = `
+## Packages
 | Package | Latest | Downloads | Live Example |
-| ------- | ------ | --------- | ---- |`,
-  );
+| ------- | ------ | --------- | ---- |`;
+
+  const packagesTable = packages.reduce((md, pkg) => {
+    const [scope, pkgName] = pkg.split('/');
+    const scopePath = lgConfig.scopes[scope];
+    // Link to the package source
+    const pkgMd = `[${pkg}](./${scopePath}/${pkgName})`;
+    // Link to the package on npm, with a status badge
+    const versionMd = `[![version](https://img.shields.io/npm/v/${pkg})](https://www.npmjs.com/package/${pkg})`;
+    // An npm downloads badge
+    const downloadsMd = `![downloads](https://img.shields.io/npm/dm/${pkg}?color=white)`;
+    // Link to the live example on mongodb.design, if it exists
+    const exampleMd =
+      scope !== '@lg-tools'
+        ? `[Live Example](http://mongodb.design/component/${pkgName}/live-example)`
+        : '';
+    const newTableRow = `| ${pkgMd} | ${versionMd} | ${downloadsMd} | ${exampleMd} |`;
+    return md + '\n' + newTableRow;
+  }, initialTable);
 
   const prettierConfig = await prettier.resolveConfig(
     '@lg-tools/lint/config/prettier.config.js',
