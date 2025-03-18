95bf339d242729921bfc71ff1001935a65dbe3b5
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-03-18 13:42:26 -0400

    removes backdrop click mousedown handler

--- packages/hooks/src/useBackdropClick.ts
+++ packages/hooks/src/useBackdropClick.ts
@@ -4,8 +4,9 @@ import useEventListener from './useEventListener';
  * Fires a callback when any element(s)
  * _except_ those passed in as `foreground` is clicked.
  *
- * Note: Disable this hook (with the `enabled` arg)
- * if the `foreground` element(s) are not in view (e.g. menu, tooltip, etc.).
+ * Establishes a click event listener on the document,
+ * firing during the [capture phase](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#capture)
+ * to ensure this check is performed before the event reaches the target.
  */
 export function useBackdropClick(
   /**
@@ -27,39 +28,12 @@ export function useBackdropClick(
    */
   enabled = true,
 ): void {
-  /**
-   * We add two event handlers to the document to handle the backdrop click behavior.
-   * Intended behavior is to fire the callback (usually closing a menu, tooltip, etc.),
-   * and keep focus on the component.
-   *
-   * No other click event handlers should fire on backdrop click
-   *
-   * 1. Mousedown event fires
-   * 2. We prevent `mousedown`'s default behavior, to prevent focus from being applied to the body (or other target)
-   * 3. Click event fires
-   * 4. We handle this event on _capture_, and stop propagation before the `click` event propagates all the way to any other element.
-   *  This ensures that even if we click on a button, that handler is not fired
-   * 5. Then we call the callback (typically fires `closeMenu`, setting `isOpen = false`, and rerender the component)
-   */
-
-  useEventListener(
-    'mousedown',
-    (mousedown: MouseEvent) => {
-      if (!doesComponentContainEventTarget(mousedown)) {
-        mousedown.preventDefault(); // Prevent focus from being applied to body
-        mousedown.stopPropagation(); // Stop any other mousedown events from firing
-      }
-    },
-    {
-      enabled,
-    },
-  );
-
   useEventListener(
     'click',
     (click: MouseEvent) => {
-      if (!doesComponentContainEventTarget(click)) {
-        click.stopPropagation(); // Stop any other click events from firing
+      const isClickInComponent = doesComponentContainEventTarget(click);
+
+      if (!isClickInComponent) {
         callback();
       }
     },
