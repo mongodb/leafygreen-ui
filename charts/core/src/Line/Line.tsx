c454945a70eae0d7fc6c9bc82fe5435eea07544b
Stephen Lee <stephen.lee@mongodb.com>
2025-03-25 15:50:49 -0700

    Style Line color and visibility based on checked status

--- charts/core/src/Line/Line.tsx
+++ charts/core/src/Line/Line.tsx
@@ -1,4 +1,8 @@
 import { useEffect } from 'react';
+import { colors } from '@lg-charts/colors';
+import { useSeriesContext } from '@lg-charts/series-provider';
+
+import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
 
 import { useChartContext } from '../ChartContext';
 
@@ -6,7 +10,14 @@ import { defaultLineOptions } from './config';
 import { LineProps } from './Line.types';
 
 export function Line({ name, data }: LineProps) {
+  const { theme } = useDarkMode();
   const { chart } = useChartContext();
+  const { getSeriesIndex, isChecked } = useSeriesContext();
+
+  const themedColors = colors[theme];
+  const colorIndex = getSeriesIndex(name) % themedColors.length;
+  const color = themedColors[colorIndex];
+  const isVisible = isChecked(name);
 
   useEffect(() => {
     if (!chart.ready) return;
@@ -15,6 +26,11 @@ export function Line({ name, data }: LineProps) {
       ...defaultLineOptions,
       name,
       data,
+      lineStyle: {
+        ...defaultLineOptions.lineStyle,
+        color,
+        opacity: isVisible ? 1 : 0,
+      },
     });
 
     return () => {
@@ -26,7 +42,7 @@ export function Line({ name, data }: LineProps) {
     };
     // FIXME:
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chart.ready, name, data]);
+  }, [chart.ready, name, data, isVisible, theme]);
 
   return null;
 }
