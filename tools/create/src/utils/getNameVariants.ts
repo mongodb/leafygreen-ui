cd9167f74adc1bf18346df07a0edd77b7c192b41
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-04-01 18:06:53 -0400

    update create subcomponents

--- tools/create/src/utils/getNameVariants.ts
+++ tools/create/src/utils/getNameVariants.ts
@@ -3,10 +3,17 @@ import kebabCase from 'lodash/kebabCase';
 import startCase from 'lodash/startCase';
 
 export function getNameVariants(name: string) {
+  // eslint-disable-next-line prefer-const
+  let [_scope, component] = name.split('/');
+
+  if (!component) {
+    component = name;
+  }
+
   // Construct all required parameters
-  const packageNameKebab = kebabCase(name);
-  const packageNameTitle = startCase(name);
-  const packageNamePascal = camelCase(name).replace(/^\w/, c =>
+  const packageNameKebab = kebabCase(component);
+  const packageNameTitle = startCase(component);
+  const packageNamePascal = camelCase(component).replace(/^\w/, c =>
     c.toUpperCase(),
   );
 
