0c1bcba00bd6b5c42f4f78900849cb48537baf69
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-04-02 15:33:12 -0400

    cleanup linter tests

--- tools/lint/src/lint.spec.ts
+++ tools/lint/src/lint.spec.ts
@@ -1,10 +1,9 @@
 /* eslint-disable jest/no-disabled-tests */
 import child_process, { ChildProcess } from 'child_process';
-import path from 'path';
 
 import { runESLint } from './eslint';
 import { npmPkgJsonLint } from './npmPkgJsonLint';
-import { prettier } from './prettier';
+import { runPrettier } from './prettier';
 import { lint } from '.';
 
 const spawnSpy = jest.spyOn(child_process, 'spawn');
@@ -27,89 +26,40 @@ describe.skip('tools/lint', () => {
     beforeEach(() => {
       jest.mock('./eslint');
       jest.mock('./prettier');
-      jest.mock('./eslint');
+      jest.mock('./npmPkgJsonLint');
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
     });
 
     test('runs with no options', () => {
       lint(defaultArgs);
 
       expect(runESLint).toHaveBeenCalled();
-      expect(prettier).toHaveBeenCalled();
+      expect(runPrettier).toHaveBeenCalled();
       expect(npmPkgJsonLint).toHaveBeenCalled();
     });
 
     test('runs with --eslintOnly flag', () => {
       lint({ ...defaultArgs, eslintOnly: true });
       expect(runESLint).toHaveBeenCalled();
-      expect(prettier).not.toHaveBeenCalled();
+      expect(runPrettier).not.toHaveBeenCalled();
       expect(npmPkgJsonLint).not.toHaveBeenCalled();
     });
 
     test('runs with --prettierOnly flag', () => {
       lint({ ...defaultArgs, prettierOnly: true });
       expect(runESLint).not.toHaveBeenCalled();
-      expect(prettier).toHaveBeenCalled();
+      expect(runPrettier).toHaveBeenCalled();
       expect(npmPkgJsonLint).not.toHaveBeenCalled();
     });
 
     test('runs with --pkgJsonOnly flag', () => {
       lint({ ...defaultArgs, pkgJsonOnly: true });
       expect(runESLint).not.toHaveBeenCalled();
-      expect(prettier).not.toHaveBeenCalled();
+      expect(runPrettier).not.toHaveBeenCalled();
       expect(npmPkgJsonLint).toHaveBeenCalled();
     });
   });
-
-  describe.skip('eslint', () => {
-    test('Runs eslint command', () => {
-      runESLint(defaultArgs);
-      const eslintConfigPath = path.resolve(
-        __dirname,
-        '../config/eslint.config.js',
-      );
-
-      expect(spawnSpy).toHaveBeenCalledWith(
-        'eslint',
-        expect.arrayContaining(['--config', eslintConfigPath]),
-      );
-    });
-  });
-
-  describe.skip('prettier', () => {
-    test('Runs prettier command', () => {
-      prettier(defaultArgs);
-      const prettierConfigPath = path.resolve(
-        __dirname,
-        '../config/prettier.config.js',
-      );
-
-      expect(spawnSpy).toHaveBeenCalledWith(
-        'prettier',
-        expect.arrayContaining(['--check', '--config', prettierConfigPath]),
-      );
-    });
-  });
-
-  describe.skip('npmPkgJsonLint', () => {
-    test('Runs npmPkgJson command', () => {
-      npmPkgJsonLint(defaultArgs);
-      const npmPkgLintConfigPath = path.resolve(
-        __dirname,
-        '../config/npmpackagejsonlintrc.config.js',
-      );
-
-      expect(spawnSpy).toHaveBeenCalledWith(
-        'npmPkgJsonLint',
-        expect.arrayContaining([
-          process.cwd(),
-          '--configFile',
-          npmPkgLintConfigPath,
-        ]),
-      );
-    });
-  });
-});
-
-test('it does', () => {
-  expect(true).toBeTruthy();
 });
