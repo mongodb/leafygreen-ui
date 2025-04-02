8e4acd2a1bf5aed7d0f67ef171257f30f4c225e9
Terrence Keane <terrence.keane@mongodb.com>
2025-04-02 16:38:48 -0400

    LG-4661: Custom Charts `Tooltip` (#2769)
    * Working state
    
    * WIP
    
    * Correct styling
    
    * Seperate concerns
    
    * Restructure and rename
    
    * Neaten up tooltip
    
    * More organization
    
    * Improve typing
    
    * Change params to series
    
    * Properly pass formatters
    
    * Add custom formats story
    
    * Small style changes
    
    * Fix sorting
    
    * Create spec file
    
    * Fix dark mode
    
    * Replace sort props with compare fn
    
    * Add spec for CustomTooltip
    
    * Remove sort key and direction
    
    * Fix charts stories
    
    * Fix types
    
    * Fix import
    
    * Changeset
    
    * Undo accidental glyphs changes
    
    * Remove marker from tooltip story
    
    * Fix format test
    
    * CR changes
    
    * Fix test
    
    * Fix story
    
    * Fix line dot color
    
    * Add axisValueFormatter
    
    * Add test
    
    * Update README
    
    * Fix types and deps
    
    * CR changes
    
    * Update changeset

--- charts/core/src/index.ts
+++ charts/core/src/index.ts
@@ -9,12 +9,6 @@ export { Grid, type GridProps } from './Grid';
 export { Header, type HeaderProps } from './Header';
 export { Line, type LineProps } from './Line';
 export { ThresholdLine, type ThresholdLineProps } from './ThresholdLine';
-export {
-  SortDirection,
-  SortKey,
-  SortOrder,
-  Tooltip,
-  type TooltipProps,
-} from './Tooltip';
+export { type SeriesInfo, Tooltip, type TooltipProps } from './Tooltip';
 export { XAxis, type XAxisProps, type XAxisType } from './XAxis';
 export { YAxis, type YAxisProps, type YAxisType } from './YAxis';
