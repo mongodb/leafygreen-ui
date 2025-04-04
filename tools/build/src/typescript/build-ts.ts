58ea13fddf8f9e6e3c4003381edd77d8ccfbcf3c
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-04-04 16:53:15 -0400

    makeTypescriptDiagnosticReporter

--- tools/build/src/typescript/build-ts.ts
+++ tools/build/src/typescript/build-ts.ts
@@ -3,6 +3,7 @@ import chalk from 'chalk';
 import fse from 'fs-extra';
 import path from 'path';
 import ts from 'typescript';
+import { makeTypescriptDiagnosticReporter } from './makeTypescriptDiagnosticReporter';
 import { parsePassThruOptions } from './parsePassThruOptions';
 
 interface BuildTypescriptOptions {
@@ -38,6 +39,7 @@ export function buildTypescript(
 
   // Any additional options passed in via the CLI
   const cliCompilerOptions = parsePassThruOptions(passThru);
+  const reportTypescriptDiagnostic = makeTypescriptDiagnosticReporter(verbose);
 
   // Create a Solution Builder Host to properly handle --build functionality
   const buildHost = ts.createSolutionBuilderHost(
@@ -59,38 +61,4 @@ export function buildTypescript(
 
   // Exit with appropriate code
   process.exit(exitStatus);
-
-  /**
-   * Helper function to report diagnostic messages
-   */
-  function reportTypescriptDiagnostic(diagnostic: ts.Diagnostic): void {
-    const msg = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-
-    const isError = diagnostic.category === ts.DiagnosticCategory.Error;
-    const isWarning = diagnostic.category === ts.DiagnosticCategory.Warning;
-
-    // Only show non-errors in verbose
-    if (!verbose && !(isError || isWarning)) {
-      return;
-    }
-
-    let message = msg;
-
-    if (diagnostic.file && diagnostic.start !== undefined) {
-      const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
-        diagnostic.start,
-      );
-      const l = line + 1;
-      const c = character + 1;
-      message = `${diagnostic.file.fileName} (${l},${c}): ${msg}`;
-    }
-
-    const msgColor = isError
-      ? chalk.red
-      : isWarning
-      ? chalk.yellow
-      : chalk.gray;
-
-    console.log(msgColor(message));
-  }
 }
---
+++ tools/build/src/typescript/makeTypescriptDiagnosticReporter.ts
@@ -0,0 +1,38 @@
+import chalk from 'chalk';
+import ts from 'typescript';
+/**
+ * Helper function to report diagnostic messages
+ */
+
+export function makeTypescriptDiagnosticReporter(verbose?: boolean) {
+  return function reportTypescriptDiagnostic(diagnostic: ts.Diagnostic): void {
+    const msg = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+
+    const isError = diagnostic.category === ts.DiagnosticCategory.Error;
+    const isWarning = diagnostic.category === ts.DiagnosticCategory.Warning;
+
+    // Only show non-errors in verbose
+    if (!verbose && !(isError || isWarning)) {
+      return;
+    }
+
+    let message = msg;
+
+    if (diagnostic.file && diagnostic.start !== undefined) {
+      const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+        diagnostic.start,
+      );
+      const l = line + 1;
+      const c = character + 1;
+      message = `${diagnostic.file.fileName} (${l},${c}): ${msg}`;
+    }
+
+    const msgColor = isError
+      ? chalk.red
+      : isWarning
+      ? chalk.yellow
+      : chalk.gray;
+
+    console.log(msgColor(message));
+  };
+}
