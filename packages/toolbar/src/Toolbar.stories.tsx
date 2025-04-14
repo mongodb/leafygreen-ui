90e627299197f3f989a52adfe8970f595165a9f6
Shaneeza <shaneeza.ali@mongodb.com>
2025-04-14 13:36:32 -0400

    add stories

--- packages/toolbar/src/Toolbar.stories.tsx
+++ packages/toolbar/src/Toolbar.stories.tsx
@@ -1,14 +1,44 @@
 /* eslint-disable no-console */
-import React from 'react';
+import React, { useMemo } from 'react';
+import { faker } from '@faker-js/faker';
 import { StoryFn } from '@storybook/react';
 
-import { Toolbar, ToolbarIconButton } from '.';
+import Button from '@leafygreen-ui/button';
+import { css } from '@leafygreen-ui/emotion';
+import { color, spacing } from '@leafygreen-ui/tokens';
+import { Body } from '@leafygreen-ui/typography';
+
+import { Toolbar, ToolbarIconButton, ToolbarProps } from '.';
+
+const LongContent = () => {
+  const paragraphs = useMemo(() => {
+    return faker.lorem
+      .paragraphs(20, '\n')
+      .split('\n')
+      .map((p, i) => <Body key={i}>{p}</Body>);
+  }, []);
+
+  return (
+    <div
+      className={css`
+        display: inline-flex;
+        flex-direction: column;
+        gap: ${spacing[100]}px;
+        align-items: baseline;
+      `}
+    >
+      <Button>Press Tab</Button>
+      {paragraphs}
+      <Button>Press Tab</Button>
+    </div>
+  );
+};
 
 export default {
   title: 'Components/Toolbar',
   component: Toolbar,
   parameters: {
-    //TODO: default: 'LiveExample',
+    default: 'LiveExample',
     controls: {
       exclude: ['children'],
     },
@@ -49,12 +79,26 @@ export default {
   },
 };
 
-const Template: StoryFn<typeof Toolbar> = props => (
-  <>
-    <button>hey</button>
-    <Toolbar {...props} />
-    <button>hey</button>
-  </>
-);
+const Template: StoryFn<typeof Toolbar> = props => <Toolbar {...props} />;
 
-export const Basic = Template.bind({});
+export const LiveExample = Template.bind({});
+
+export const InLayout: StoryFn<ToolbarProps> = (args: ToolbarProps) => {
+  const theme = args.darkMode ? 'dark' : 'light';
+  return (
+    <div
+      style={{
+        height: '100%',
+        border: `1px solid ${color[theme].border.primary.default}`,
+        display: 'grid',
+        width: '80vw',
+        gridTemplateColumns: 'auto 48px',
+      }}
+    >
+      <main style={{ overflow: 'scroll', padding: `${spacing[500]}px` }}>
+        <LongContent />
+      </main>
+      <Toolbar {...args} />
+    </div>
+  );
+};
