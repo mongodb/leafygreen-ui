0c71d0b4e6493cb43a5c242fdf00d7815c7419be
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-04-22 22:40:06 -0400

    update build scripts

--- tools/build/bin/build-package.js
+++ tools/build/bin/build-package.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
 // Use this bin to build packages that are dependencies of `@lg-tools/cli`
 // and therefore can't use `lg build-package`
-const { buildPackage } = require('../dist/index.js');
+const { buildPackage } = require('../dist/umd/index.js');
 buildPackage({ direct: true });
