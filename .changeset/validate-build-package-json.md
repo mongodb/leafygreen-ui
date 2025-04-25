b07e5472399adf9d473c1cb8445de3847b88e6ef
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-04-25 13:02:36 -0400

    Squashed commit of the following:
    commit d91581d2041b803fb6a8ec6be7004e0db8763d96
    Author: Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
    Date:   Fri Apr 25 12:26:09 2025 -0400
    
        1. Typescript 5.8 [LG-5006] (#2821)
    
        * Bump to TS5.0
    
        * use typescript node API
    
        * handle cli options
    
        * use createSolutionBuilderHost
    
        * add production flag
    
        * makeTypescriptDiagnosticReporter
    
        * creates downlevelDts
    
        * downlevelDts parses package.json
    
        * use --downlevel flag
    
        * add ts version override
    
        * add --downlevel on prepublish
    
        * adds changesets
    
        * Update InlineCode.tsx
    
        * update packagejsons
    
        * fix prepublishOnly
    
        * update logging
    
        * Update build-typescript-5.md
    
        * fix build build scripts
    
        * "tsc": "lg-internal-build-ts"
    
        * add downlevel to prepublish
    
        * reset packagejson
    
        * add runTypescriptDownlevel to bin
    
        * Update pnpm-lock.yaml
    
        * Update package.json
    
        * update typesVersions
    
        * Update package.json
    
        * Update package.json
    
        * lint
    
        * lint console
    
        * Update build-ts.spec.ts
    
        * Update pnpm-lock.yaml
    
        * add missing mock-fs
    
        * @types/mock-fs
    
        * remove TS 3 downleveling
    
        * rm chalk. fix mockFs
    
        * creates updateTypesVersions script
    
        * exclude packages from ts downlevel script
    
        * remove typesVersions
    
        * Create typescript-5-all.md
    
        * updates all tsconfigs
    
        * Update build-package.ts
    
        * updates scripts
    
        * updates downlevel script
    
        * comments
    
        * T 5.8 (#2816)
    
        * upgrade to TS 5.8
    
        * fix or ignore new errors
    
        * Update build-typescript-5.md
    
        * Update tsconfig.json
    
        * lint fix
    
        * Update typescript-5-all.md
    
        * add --update to bin cmd
    
        * ensure empty
    
        * Create validate-build-package-json.md
    
        * Revert "Create validate-build-package-json.md"
    
        This reverts commit 06ec702e08075e1c99003b917677385e5d1f1b47.
    
        * Delete package.json
    
        * revert typesversions
    
        * lockfile
    
        * disable downlevel
    
        * Adds `getPackageRoot` utility (#2826)
    
        * adds getPackageRoot
    
        * Create meta-get-package-root.md
    
        * adds tests
    
        * Squashed commit of the following:
    
        commit ed4332d99892dacf44dedba26b29bd8c9e145c1f
        Author: Adam Michael Thompson <adam.thompson@mongodb.com>
        Date:   Wed Apr 23 18:00:22 2025 -0400
    
            Update index.ts
    
        commit 03c9099036e2aa4e0c99f7d0a2ad1ae6fc086f86
        Author: Adam Michael Thompson <adam.thompson@mongodb.com>
        Date:   Wed Apr 23 17:45:03 2025 -0400
    
            rm validate ignore
    
        commit 511fc407a0821714a6fb2de0b7d2e8063029604c
        Merge: b8c7ecd69 028147fe1
        Author: Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
        Date:   Wed Apr 23 17:34:52 2025 -0400
    
            Merge branch 'main' into a/validate
    
        commit b8c7ecd6990db699a089131ad1196c916b98be56
        Author: Adam Michael Thompson <adam.thompson@mongodb.com>
        Date:   Wed Apr 23 16:11:39 2025 -0400
    
            updates types validation
    
            only validate if it should exist
    
        commit bdb5393c616d6d2e965ef79a3c0cd5c8e404d901
        Author: Adam Michael Thompson <adam.thompson@mongodb.com>
        Date:   Wed Apr 23 12:50:49 2025 -0400
    
            Create validate-build-package-json.md
    
        commit 9d1001af8c8d597a2e8c018bb655737251b4778a
        Author: Adam Michael Thompson <adam.thompson@mongodb.com>
        Date:   Wed Apr 23 12:50:47 2025 -0400
    
            update validate to use package entry files
    
        * Update packagejson exports
    
        * add comment
    
        * use clone deep
    
    commit 6bc396170701e32eea46ac41b5e167726abe93d7
    Author: Adam Michael Thompson <adam.thompson@mongodb.com>
    Date:   Tue Apr 22 17:02:19 2025 -0400
    
        integration

---
+++ .changeset/validate-build-package-json.md
@@ -0,0 +1,5 @@
+---
+'@lg-tools/validate': minor
+---
+
+Updates build validation to use the entry files defined in the package.json
--- tools/build/src/index.ts
+++ tools/build/src/index.ts
@@ -1,6 +1,25 @@
+import spawn from 'cross-spawn';
+
 export { buildAll as build } from './buildAll';
 export { buildPackage } from './rollup/build-package';
 export { buildTSDoc } from './tsdoc/build-tsdoc';
 export { parseTSDoc } from './tsdoc/tsdocParser';
 export { buildTypescript } from './typescript/build-ts';
 export { runTypescriptDownlevel } from './typescript/downlevel';
+
+interface BuildOptions {
+  only: Array<string>;
+  verbose: boolean;
+}
+
+const validSteps = ['build', 'tsc', 'docs'];
+
+export function buildAll(packages: Array<string>, { only }: BuildOptions) {
+  console.log({ packages, only });
+
+  const steps = only ? only.filter(s => validSteps.includes(s)) : validSteps;
+
+  spawn('turbo', ['run', ...steps], {
+    stdio: 'inherit',
+  });
+}
