b33b0ff4e8e74048ec3e3a0a3f98c45970a930b1
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-04-28 18:50:38 -0400

    update preview.js

--- tools/storybook-addon/preset.js
+++ tools/storybook-addon/preset.js
@@ -1,1 +1,9 @@
-module.exports = require('./dist/umd/index');
+// @ts-nocheck
+module.exports = require('./dist/umd/index.js');
+
+/**
+ * We also export named arrays for any files we want to use for default
+ * `preview.js` and `manager.js` settings
+ */
+module.exports.previewAnnotations = [require.resolve('./dist/umd/preview.js')];
+module.exports.managerEntries = [require.resolve('./dist/umd/manager.js')];
