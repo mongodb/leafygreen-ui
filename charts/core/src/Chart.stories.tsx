ded57d4108dd44cd833efce10c1fc632d13dec87
Stephen Lee <stephen.lee@mongodb.com>
2025-05-01 14:34:03 -0700

    Update WithTooltipAndZoom story

--- charts/core/src/Chart.stories.tsx
+++ charts/core/src/Chart.stories.tsx
@@ -834,6 +834,24 @@ export const WithTooltip: StoryObj<{}> = {
   },
 };
 
+export const WithZoomAndTooltip: StoryObj<{}> = {
+  render: () => {
+    return (
+      <Chart
+        zoomSelect={{
+          xAxis: true,
+          yAxis: true,
+        }}
+      >
+        <ChartTooltip />
+        {lineData.map(({ name, data }) => (
+          <Line name={name} data={data} key={name} />
+        ))}
+      </Chart>
+    );
+  },
+};
+
 export const WithXAxis: StoryObj<{}> = {
   render: () => {
     return (
@@ -1056,23 +1074,6 @@ export const WithWarningEventMarkerLine: StoryObj<{}> = {
           message="Event marker line message"
           level="warning"
         />
-        {lineData.map(({ name, data }) => (
-          <Line name={name} data={data} key={name} />
-        ))}
-      </Chart>
-    );
-  },
-};
-
-export const WithZoomAndTooltip: StoryObj<{}> = {
-  render: () => {
-    return (
-      <Chart
-        zoomSelect={{
-          xAxis: true,
-          yAxis: true,
-        }}
-      >
         {lineData.map(({ name, data }) => (
           <Line name={name} data={data} key={name} />
         ))}
