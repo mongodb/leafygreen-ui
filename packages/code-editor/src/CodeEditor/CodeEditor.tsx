26fd5c283b70a7818c28f4079889710060e2920f
Terrence Keane <terrence.keane@mongodb.com>
2025-05-14 15:03:05 -0400

    Start test suite

--- packages/code-editor/src/CodeEditor/CodeEditor.tsx
+++ packages/code-editor/src/CodeEditor/CodeEditor.tsx
@@ -1,80 +1,93 @@
-import React, { useCallback, useMemo, useRef, useState } from 'react';
+import React, {
+  forwardRef,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import { forceParsing } from '@codemirror/language';
 import { hyperLink } from '@uiw/codemirror-extensions-hyper-link';
-import CodeMirror, {
-  EditorView,
-  type Extension,
+import CodeMirror, { EditorView } from '@uiw/react-codemirror';
+
+import { useMergeRefs } from '@leafygreen-ui/hooks';
+
+import {
+  type CodeEditorProps,
+  type CodeMirrorExtension,
   type ReactCodeMirrorRef,
-} from '@uiw/react-codemirror';
+} from './CodeEditor.types';
 
-import { type CodeEditorProps } from './CodeEditor.types';
-
-export function CodeEditor({
-  enableActiveLineHighlighting = true,
-  enableClickableUrls = true,
-  enableCodeFolding = true,
-  enableLineNumbers = true,
-  enableLineWrapping = true,
-  forceParsing: forceParsingProp = false,
-  placeholder,
-  readOnly = false,
-  value: valueProp,
-  ...rest
-}: CodeEditorProps) {
-  const [value, setValue] = useState(valueProp || '');
-  const editorRef = useRef<ReactCodeMirrorRef>(null);
-
-  const onChange = useCallback((val: string) => {
-    setValue(val);
-  }, []);
-
-  const onCreateEditor = useCallback(
-    (editorView: EditorView) => {
-      if (forceParsingProp) {
-        const { state } = editorView;
-
-        if (state.doc.length > 0) {
-          forceParsing(editorView, state.doc.length, 150);
-        }
-      }
+export const CodeEditor = forwardRef<ReactCodeMirrorRef, CodeEditorProps>(
+  (
+    {
+      enableActiveLineHighlighting = true,
+      enableClickableUrls = true,
+      enableCodeFolding = true,
+      enableLineNumbers = true,
+      enableLineWrapping = true,
+      forceParsing: forceParsingProp = false,
+      placeholder,
+      readOnly = false,
+      value: valueProp,
+      ...rest
     },
-    [forceParsingProp],
-  );
+    forwardedRef,
+  ) => {
+    const [value, setValue] = useState(valueProp || '');
+    const editorRef = useRef<ReactCodeMirrorRef>(null);
 
-  const extensions = useMemo(() => {
-    const extensions: Array<Extension> = [];
+    const onChange = useCallback((val: string) => {
+      setValue(val);
+    }, []);
 
-    if (enableClickableUrls) {
-      extensions.push(hyperLink);
-    }
+    const onCreateEditor = useCallback(
+      (editorView: EditorView) => {
+        if (forceParsingProp) {
+          const { state } = editorView;
 
-    if (enableLineWrapping) {
-      extensions.push(EditorView.lineWrapping);
-    }
+          if (state.doc.length > 0) {
+            forceParsing(editorView, state.doc.length, 150);
+          }
+        }
+      },
+      [forceParsingProp],
+    );
 
-    return extensions;
-  }, [enableClickableUrls, enableLineWrapping]);
+    const extensions = useMemo(() => {
+      const extensions: Array<CodeMirrorExtension> = [];
 
-  return (
-    <CodeMirror
-      value={value}
-      height="200px"
-      width="100%"
-      onChange={onChange}
-      onCreateEditor={onCreateEditor}
-      readOnly={readOnly}
-      placeholder={placeholder}
-      extensions={extensions}
-      basicSetup={{
-        allowMultipleSelections: true,
-        foldGutter: enableCodeFolding,
-        highlightActiveLineGutter: enableActiveLineHighlighting,
-        lineNumbers: enableLineNumbers,
-      }}
-      ref={editorRef}
-      {...rest}
-    />
-  );
-}
+      if (enableClickableUrls) {
+        extensions.push(hyperLink);
+      }
+
+      if (enableLineWrapping) {
+        extensions.push(EditorView.lineWrapping);
+      }
+
+      return extensions;
+    }, [enableClickableUrls, enableLineWrapping]);
+
+    return (
+      <CodeMirror
+        value={value}
+        height="200px"
+        width="100%"
+        onChange={onChange}
+        onCreateEditor={onCreateEditor}
+        readOnly={readOnly}
+        placeholder={placeholder}
+        extensions={extensions}
+        basicSetup={{
+          allowMultipleSelections: true,
+          foldGutter: enableCodeFolding,
+          highlightActiveLineGutter: enableActiveLineHighlighting,
+          lineNumbers: enableLineNumbers,
+        }}
+        ref={useMergeRefs([editorRef, forwardedRef])}
+        {...rest}
+      />
+    );
+  },
+);
 
 CodeEditor.displayName = 'CodeEditor';
