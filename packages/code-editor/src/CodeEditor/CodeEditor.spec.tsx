0af8d322eb0e132fbea55808f0a1053c82a02cec
Terrence Keane <terrence.keane@mongodb.com>
2025-05-15 13:24:27 -0400

    feat(code-editor): enable active line highlighting based on prop for improved editor usability

--- packages/code-editor/src/CodeEditor/CodeEditor.spec.tsx
+++ packages/code-editor/src/CodeEditor/CodeEditor.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { act, render } from '@testing-library/react';
-import { ChangeSpec, EditorView } from '@uiw/react-codemirror';
+// import { forceParsing } from '@codemirror/language';
+import { act, render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { EditorView } from '@uiw/react-codemirror';
 
 import { CodeEditor, CodeEditorProps } from '.';
 
@@ -14,8 +16,13 @@ global.MutationObserver = jest.fn().mockImplementation(() => ({
 let editorView: EditorView;
 
 const EditorSelectors = {
+  ActiveLine: '.cm-activeLine',
+  ActiveLineGutter: '.cm-activeLineGutter',
+  Content: '.cm-content',
   FoldGutter: '.cm-foldGutter',
   GutterElement: '.cm-gutterElement',
+  HyperLink: '.cm-hyper-link-icon',
+  LineWrapping: '.cm-lineWrapping',
 } as const;
 type EditorSelectors = (typeof EditorSelectors)[keyof typeof EditorSelectors];
 
@@ -84,22 +91,18 @@ const editor = {
     return elements[0];
   },
 
-  insertText: (text: string, options?: { from?: number; to?: number }) => {
-    if (!editorView) {
-      throw new Error('Editor view is not initialized');
-    }
+  isLineWrappingEnabled: () => {
+    return !!editor.queryBySelector(EditorSelectors.LineWrapping);
+  },
 
-    const changes: ChangeSpec = { insert: text, from: options?.from || 0 };
+  isReadOnly: () => {
+    return editorView.state.readOnly;
+  },
 
-    if (options?.to) {
-      changes.to = options.to;
-    }
-
-    const transaction = editorView.state.update({
-      changes,
-    });
-
-    editorView.dispatch(transaction);
+  type: (text: string) => {
+    const content = editor.getBySelector(EditorSelectors.Content);
+    userEvent.click(content);
+    userEvent.type(content, text);
   },
 };
 
@@ -118,22 +121,31 @@ function renderEditor(props: Partial<CodeEditorProps> = {}) {
   return { container };
 }
 
+// jest.mock('@codemirror/language', () => {
+//   const actualModule = jest.requireActual('@codemirror/language');
+//   return {
+//     ...actualModule,
+//     forceParsing: jest.fn(),
+//   };
+// });
+
 describe('packages/code-editor', () => {
-  test('Renders value in editor', () => {
+  test('Renders default value in editor', () => {
     const { container } = renderEditor({ defaultValue: 'content' });
     expect(container).toHaveTextContent('content');
   });
 
-  test('Updates value on text insertion', async () => {
-    const onChange = jest.fn();
-    const { container } = renderEditor({
-      onChange,
-    });
-    expect(container).not.toHaveTextContent('new content');
+  test('Updates value on when user types', async () => {
+    renderEditor();
+    expect(editor.getBySelector(EditorSelectors.Content)).not.toHaveTextContent(
+      'new content',
+    );
     act(() => {
-      editor.insertText('new content');
+      editor.type('new content');
     });
-    expect(container).toHaveTextContent('new content');
+    expect(editor.getBySelector(EditorSelectors.Content)).toHaveTextContent(
+      'new content',
+    );
   });
 
   test('Fold gutter renders when enabled', () => {
@@ -159,10 +171,91 @@ describe('packages/code-editor', () => {
     ).toBeInTheDocument();
   });
 
-  test('Line numbers does not renders when disabled', () => {
+  test('Line numbers do not renders when disabled', () => {
     renderEditor({ enableLineNumbers: false });
     expect(
       editor.queryBySelector(EditorSelectors.GutterElement, { text: '1' }),
     ).not.toBeInTheDocument();
   });
+
+  test('Active line highlighting renders when enabled', () => {
+    renderEditor({ enableActiveLineHighlighting: true });
+    expect(
+      editor.getBySelector(EditorSelectors.ActiveLine),
+    ).toBeInTheDocument();
+    expect(
+      editor.getBySelector(EditorSelectors.ActiveLineGutter, { text: '1' }),
+    ).toBeInTheDocument();
+  });
+
+  test('Active line highlighting does not render when disabled', () => {
+    renderEditor({ enableActiveLineHighlighting: false });
+    expect(
+      editor.queryBySelector(EditorSelectors.ActiveLine),
+    ).not.toBeInTheDocument();
+    expect(
+      editor.queryBySelector(EditorSelectors.ActiveLineGutter, { text: '1' }),
+    ).not.toBeInTheDocument();
+  });
+
+  test('Clickable URLs render when enabled', () => {
+    renderEditor({
+      defaultValue: 'https://mongodb.design',
+      enableClickableUrls: true,
+    });
+    expect(editor.getBySelector(EditorSelectors.HyperLink)).toBeInTheDocument();
+  });
+
+  test('Clickable URLs do not render when disable', () => {
+    renderEditor({
+      defaultValue: 'https://mongodb.design',
+      enableClickableUrls: false,
+    });
+    expect(
+      editor.queryBySelector(EditorSelectors.HyperLink),
+    ).not.toBeInTheDocument();
+  });
+
+  test('Read-only set on editor state when enabled', () => {
+    renderEditor({ readOnly: true });
+    expect(editor.isReadOnly()).toBe(true);
+  });
+
+  test('Read-only not set on editor state when disabled', () => {
+    renderEditor({ readOnly: false });
+    expect(editor.isReadOnly()).toBe(false);
+  });
+
+  test('Line wrapping enabled when enabled', () => {
+    renderEditor({ enableLineWrapping: true });
+    expect(editor.isLineWrappingEnabled()).toBe(true);
+  });
+
+  test('Line wrapping not enabled when disabled', () => {
+    renderEditor({ enableLineWrapping: false });
+    expect(editor.isLineWrappingEnabled()).toBe(false);
+  });
+
+  test('Editor displays placeholder when empty', () => {
+    renderEditor({ placeholder: 'Type your code here...' });
+    expect(editor.getBySelector(EditorSelectors.Content)).toHaveTextContent(
+      'Type your code here...',
+    );
+  });
+
+  test('Editor displays HTMLElement placeholder when empty', () => {
+    const placeholderElement = document.createElement('div');
+    placeholderElement.textContent = 'Type your code here...';
+    renderEditor({ placeholder: placeholderElement });
+    expect(editor.getBySelector(EditorSelectors.Content)).toHaveTextContent(
+      'Type your code here...',
+    );
+  });
+
+  // test('the forceParsing() method is called when enabled', async () => {
+  //   renderEditor({ forceParsing: true });
+  //   await waitFor(() => {
+  //     expect(forceParsing as jest.Mock).toHaveBeenCalledTimes(1);
+  //   });
+  // });
 });
