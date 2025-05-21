2b8f1e5f337dfa653f5767a465822ce17a7b8a5d
Shaneeza <shaneeza.ali@mongodb.com>
2025-05-21 14:18:26 -0400

    tsdocs for LayoutData

--- packages/drawer/src/DrawerToolbarLayout/DrawerToolbarLayout.types.ts
+++ packages/drawer/src/DrawerToolbarLayout/DrawerToolbarLayout.types.ts
@@ -9,11 +9,36 @@ type PickedRequiredDrawerProps = Required<Pick<DrawerProps, 'displayMode'>>;
 type PickedDrawerProps = Pick<DrawerProps, 'onClose'>;
 
 interface LayoutData {
+  /**
+   * The id of the layout. This is used to open the drawer.
+   */
   id: string;
+
+  /**
+   * The LG Icon that will render in the ToolbarIcon.
+   */
   glyph: GlyphName;
+
+  /**
+   * The content of the drawer.
+   */
   content: React.ReactNode;
+
+  /**
+   * The text that will render in the tooltip on hover.
+   */
   label: React.ReactNode;
-  onClick: () => void;
+
+  /**
+   * Callback function that is called when the button is clicked.
+   * @param event
+   * @returns void
+   */
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+
+  /**
+   * The title of the drawer.
+   */
   title: string;
 }
 
