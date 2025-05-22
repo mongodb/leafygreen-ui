ee06598c1693b854bd5635f2e303ee8ad333f3b9
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-05-22 17:47:45 -0400

    Update Kanopy scripts and permissions

--- apps/chatbot-server/scripts/init.sh
+++ apps/chatbot-server/scripts/init.sh
@@ -2,17 +2,18 @@ echo "Initializing the Kanopy OIDC cluster..."
 echo "This script should open your browser to log in to the Kanopy OIDC cluster."
 echo "If you cannot authenticate, please follow the instructions here: https://kanopy.corp.mongodb.com/docs/"
 
+# Source the .env file to get the PORT and NAMESPACE
+if [ -f ".env" ]; then
+  export $(grep -v '^#' ".env" | xargs)
+else
+  echo "Error: .env file not found"
+  exit 1
+fi
+
+
 CLUSTER=staging
-NAMESPACE=skunkworks
-KOLD=$KUBECONFIG
 export KUBECONFIG=~/.kube/config.$CLUSTER
 mkdir -p $(dirname $KUBECONFIG)
 kanopy-oidc kube setup $CLUSTER > $KUBECONFIG
 kanopy-oidc kube login
 kubectl config set-context $(kubectl config current-context) --namespace=$NAMESPACE
-export KUBECONFIG=$KOLD
-
-# set the staging cluster as the default
-cp ~/.kube/config.$CLUSTER ~/.kube/config
-
-kubectl get pods
