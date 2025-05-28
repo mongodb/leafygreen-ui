935c56dc5c9625e7f553170afe617f65949c485d
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-05-28 15:55:51 -0400

    adds usage guidelines

--- packages/tokens/src/color/scrollbar.ts
+++ packages/tokens/src/color/scrollbar.ts
@@ -68,6 +68,28 @@ const lightModeScrollbarColors = {
   },
 } as const satisfies ScrollbarColorRecord;
 
+/**
+ * Scrollbar colors for dark and light themes.
+ *
+ * Use the `scrollbar-color` CSS property to set the colors of the scrollbar thumb and track.
+ * For Safari, use the `-webkit-scrollbar-thumb` and `-webkit-scrollbar-track` pseudo-elements.
+ *
+ * Usage:
+ * ```tsx
+ * import { scrollbarColor } from '@leafygreen-ui/tokens'
+ *
+ * css`
+ *   scrollbar-color: ${scrollbarColor[theme].thumb.primary.default} ${scrollbarColor[theme].track.primary.default};
+ *   &::-webkit-scrollbar-thumb {
+ *     background-color: ${scrollbarColor[theme].thumb.primary.default};
+ *   }
+ *   &::-webkit-scrollbar-track {
+ *    background-color: ${scrollbarColor[theme].track.primary.default};
+ *   }
+ * `
+ * ;
+ *```
+ */
 export const scrollbarColor = {
   dark: darkModeScrollbarColors,
   light: lightModeScrollbarColors,
