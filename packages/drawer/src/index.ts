150459e75488d40396134a25f97c9a9a3822a310
Shaneeza <shaneeza.ali@mongodb.com>
2025-06-02 10:07:58 -0400

    add exports

--- packages/drawer/src/index.ts
+++ packages/drawer/src/index.ts
@@ -4,3 +4,15 @@ export {
   useDrawerStackContext,
 } from './DrawerStackContext';
 export { getLgIds, getTestUtils, type GetTestUtilsReturnType } from './utils';
+export {
+  DrawerToolbarLayout,
+  type DrawerToolbarLayoutProps,
+} from './DrawerToolbarLayout';
+export {
+  EmbeddedDrawerLayout,
+  type EmbeddedDrawerLayoutProps,
+} from './EmbeddedDrawerLayout';
+export {
+  OverlayDrawerLayout,
+  type OverlayDrawerLayoutProps,
+} from './OverlayDrawerLayout';
