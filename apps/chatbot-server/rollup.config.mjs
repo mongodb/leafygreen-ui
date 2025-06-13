dd53158c42dab218769e7581897881f1f7b27904
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-06-13 12:20:52 -0400

    docker builds ?

--- apps/chatbot-server/rollup.config.mjs
+++ apps/chatbot-server/rollup.config.mjs
@@ -1,8 +1,13 @@
-import { esmConfig, umdConfig } from '@lg-tools/build/config/rollup.config.mjs';
+import { esmConfig } from '@lg-tools/build/config/rollup.config.mjs';
 
 const ingestESMConfig = {
   ...esmConfig,
   input: './src/ingest/ingest.config.ts',
 };
 
-export default [esmConfig, umdConfig, ingestESMConfig];
+export default [
+  {
+    ...esmConfig,
+  },
+  ingestESMConfig,
+];
