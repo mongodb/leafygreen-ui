6adc18d65a367e68f6d30cfa605ab80a6e8db5f0
Alina Yingsi Zeng <alina.yzeng@gmail.com>
2025-06-16 11:04:26 -0400

    base prop defs

--- packages/progress-bar/src/ProgressBar/ProgressBar.tsx
+++ packages/progress-bar/src/ProgressBar/ProgressBar.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import { Size, Variant } from '@leafygreen-ui/tokens';
+
 import { ProgressBarProps } from './ProgressBar.types';
 
-export function ProgressBar({}: ProgressBarProps) {
+export function ProgressBar({
+  type,
+  value,
+  variant = Variant.Info,
+  label,
+  valueType = 'fraction',
+  maxValue = 100,
+  valueUnits,
+  showIcon = false,
+  size = Size.Default,
+  description,
+  darkMode = false,
+  disabled = false,
+  ...rest
+}: ProgressBarProps) {
   return <div>your content here</div>;
 }
 
--- packages/progress-bar/src/ProgressBar/ProgressBar.types.ts
+++ packages/progress-bar/src/ProgressBar/ProgressBar.types.ts
@@ -1,7 +1,61 @@
-interface BaseProgressBarProps {}
+import { Size, Variant } from '@leafygreen-ui/tokens';
 
-interface DeterminateProgressBarProps extends BaseProgressBarProps {}
-interface IndeterminateProgressBarProps extends BaseProgressBarProps {}
+export type ProgressBarVariant =
+  | typeof Variant.Info
+  | typeof Variant.Success
+  | typeof Variant.Warning
+  | typeof Variant.Error;
+
+export type ProgressBarValueType = 'fraction' | 'percentage' | 'number';
+interface BaseProgressBarProps {
+  variant?: ProgressBarVariant;
+
+  /**
+   * The descriptive label for the progress bar.
+   */
+  label?: string;
+
+  /**
+   * The format of the value displayed in the progress bar next to the label.
+   */
+  valueType?: ProgressBarValueType;
+
+  /**
+   * The maximum value of the progress bar.
+   * If not provided, it defaults to 100.
+   */
+  maxValue?: number;
+
+  /**
+   * The units of the value displayed in the progress bar.
+   * If not provided, no units will be displayed.
+   */
+  valueUnits?: string;
+
+  /**
+   * Optional icon displayed next to the value in the progress bar based on the variant.
+   * If not provided, no icon will be displayed.
+   */
+  showIcon?: boolean;
+
+  size?: Size;
+  description?: string;
+  darkMode?: boolean;
+  disabled?: boolean;
+}
+
+interface DeterminateProgressBarProps extends BaseProgressBarProps {
+  type: 'determinate';
+  value: number;
+  /**
+   * Shimmer animation recommended for long-running tasks to provide visual feedback.
+   */
+  enableAnimation?: boolean;
+}
+interface IndeterminateProgressBarProps extends BaseProgressBarProps {
+  type: 'indeterminate';
+  value?: number;
+}
 
 export type ProgressBarProps =
   | DeterminateProgressBarProps
