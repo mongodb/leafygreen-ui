1a01d1d59e7a1d27fb5027bd9f8462a40593a68c
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-06-16 15:46:58 -0400

    hello headers

--- apps/hello-server/index.js
+++ apps/hello-server/index.js
@@ -4,6 +4,15 @@ const app = express();
 const port = 8080;
 
 app.get('/hello', (req, res) => {
+  res.status(200);
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS',
+  );
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
   res.send('Hello, World!');
 });
 
