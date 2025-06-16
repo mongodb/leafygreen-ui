84c5f3575ad1acd0370a398e3dc603ae72578198
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-06-16 16:18:41 -0400

    trailing slashes

--- apps/chatbot-server/src/index.ts
+++ apps/chatbot-server/src/index.ts
@@ -23,11 +23,9 @@ Be concise in your answers.
 
 // Start the server and clean up resources on SIGINT.
 const PORT = process.env.PORT || 3030;
-const allowedOrigins = [
-  'http://localhost:3000/',
-  'https://mongodb.design/',
-  'https://staging.mongodb.design/',
-];
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(',')
+  : [];
 
 const startServer = async () => {
   const {
