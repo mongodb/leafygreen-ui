6b304bdf9338cd7641ab47b4e48eb92bcf0a6cf5
Alina Yingsi Zeng <alina.yzeng@gmail.com>
2025-06-24 13:44:13 -0400

    refactor stories

--- packages/progress-bar/src/ProgressBar.stories.tsx
+++ packages/progress-bar/src/ProgressBar.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { StoryMetaType } from '@lg-tools/storybook-utils';
-import { StoryFn, StoryObj } from '@storybook/react';
+import { StoryObj } from '@storybook/react';
 
 import { ProgressBar, ProgressBarProps } from '.';
 
@@ -31,128 +31,119 @@ const meta: StoryMetaType<typeof ProgressBar> = {
 };
 export default meta;
 
-const Template: StoryFn<typeof ProgressBar> = props => (
-  <ProgressBar {...props} />
-);
+const SimulatedProgressBar = (props: ProgressBarProps) => {
+  const [value, setValue] = useState(props.value ?? 0);
 
-export const LiveExample = Template.bind({});
-LiveExample.args = {
-  value: 90,
-  maxValue: 200,
-  formatValue: 'fraction',
-  showIcon: true,
-  label: <span>Label</span>,
-  description: <span>Helper text</span>,
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setValue(200);
+    }, 1500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [props.value]);
+
+  return <ProgressBar {...props} value={value} />;
+};
+
+export const LiveExample: StoryObj<typeof ProgressBar> = {
+  args: {
+    value: 90,
+    maxValue: 200,
+    formatValue: 'fraction',
+    showIcon: true,
+    label: <span>Label</span>,
+    description: <span>Helper text</span>,
+  },
+  render: SimulatedProgressBar,
 };
 
 export const BasicDeterminate: StoryObj<typeof ProgressBar> = {
   args: {
-    value: 10,
+    value: 80,
     maxValue: 200,
   },
-  render: (props: ProgressBarProps) => {
-    const [value, setValue] = useState(props.value || 0);
-
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setValue(200);
-      }, 500);
-
-      return () => {
-        clearTimeout(timer);
-      };
-    }, [props.value]);
-
-    return <ProgressBar {...props} value={value} />;
-  },
+  render: SimulatedProgressBar,
 };
 
 export const ShimmerDeterminate: StoryObj<typeof ProgressBar> = {
   args: {
-    value: 80,
-    maxValue: 200,
+    ...BasicDeterminate.args,
     enableAnimation: true,
   },
-  render: (props: ProgressBarProps) => {
-    const [value, setValue] = useState(props.value || 0);
+  render: SimulatedProgressBar,
+};
 
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setValue(200);
-      }, 3500);
-
-      return () => {
-        clearTimeout(timer);
-      };
-    }, [props.value]);
-
-    return <ProgressBar {...props} value={value} />;
+export const Indeterminate: StoryObj<typeof ProgressBar> = {
+  args: {
+    isIndeterminate: true,
   },
 };
 
-export const Indeterminate = Template.bind({});
-Indeterminate.args = {
-  isIndeterminate: true,
-};
-
-export const WithLabel = Template.bind({});
-WithLabel.args = {
-  ...BasicDeterminate.args,
-  label: <span>Label</span>,
-};
-
-export const WithValueDisplay = Template.bind({});
-WithValueDisplay.args = {
-  ...BasicDeterminate.args,
-  formatValue: 'percentage',
-};
-
-export const WithDescription = Template.bind({});
-WithDescription.args = {
-  ...BasicDeterminate.args,
-  description: <span>Helper text</span>,
-};
-
-export const DeterminateVariants = Template.bind({});
-DeterminateVariants.parameters = {
-  generate: {
-    combineArgs: {
-      variant: ['info', 'success', 'warning', 'error'],
-      enableAnimation: [false, true],
-      disabled: [false, true],
-    },
-    args: {
-      isIndeterminate: false,
-      value: 47,
-      maxValue: 200,
-      formatValue: (value: number, maxValue?: number) =>
-        `${value} / ${maxValue} GB`,
-      showIcon: true,
-    },
-    excludeCombinations: [
-      {
-        variant: ['warning', 'error'],
-        enableAnimation: [true],
-      },
-      {
-        variant: ['success', 'warning', 'error'],
-        disabled: [true],
-      },
-    ],
+export const WithLabel: StoryObj<typeof ProgressBar> = {
+  args: {
+    ...BasicDeterminate.args,
+    label: <span>Label</span>,
   },
 };
 
-export const IndeterminateVariants = Template.bind({});
-IndeterminateVariants.parameters = {
-  generate: {
-    combineArgs: {
-      variant: ['info', 'success'],
+export const WithValueDisplay: StoryObj<typeof ProgressBar> = {
+  args: {
+    ...BasicDeterminate.args,
+    formatValue: 'percentage',
+  },
+};
+
+export const WithDescription: StoryObj<typeof ProgressBar> = {
+  args: {
+    ...BasicDeterminate.args,
+    description: <span>Helper text</span>,
+  },
+};
+
+export const DeterminateVariants: StoryObj<typeof ProgressBar> = {
+  parameters: {
+    generate: {
+      combineArgs: {
+        variant: ['info', 'success', 'warning', 'error'],
+        enableAnimation: [false, true],
+        disabled: [false, true],
+      },
+      args: {
+        isIndeterminate: false,
+        value: 47,
+        maxValue: 200,
+        formatValue: (value: number, maxValue?: number) =>
+          `${value} / ${maxValue} GB`,
+        showIcon: true,
+      },
+      excludeCombinations: [
+        {
+          variant: ['warning', 'error'],
+          enableAnimation: [true],
+        },
+        {
+          variant: ['success', 'warning', 'error'],
+          disabled: [true],
+        },
+      ],
     },
-    args: {
-      isIndeterminate: true,
-      value: 12,
-      formatValue: (value: number) => `${value} MBs`,
-      showIcon: true,
+  },
+};
+
+export const IndeterminateVariants: StoryObj<typeof ProgressBar> = {
+  parameters: {
+    generate: {
+      combineArgs: {
+        variant: ['info', 'success'],
+      },
+      args: {
+        isIndeterminate: true,
+        value: 12,
+        formatValue: (value: number) => `${value} MBs`,
+        showIcon: true,
+      },
     },
   },
 };
