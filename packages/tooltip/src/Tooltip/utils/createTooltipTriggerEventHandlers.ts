e14515fcc143cbc591ed0462d6f59c9076ea5a89
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-06-26 14:38:57 -0400

    clean up handlers. mv types

--- packages/tooltip/src/Tooltip/utils/createTooltipTriggerEventHandlers.ts
+++ packages/tooltip/src/Tooltip/utils/createTooltipTriggerEventHandlers.ts
@@ -3,7 +3,6 @@ import {
   FocusEventHandler,
   MouseEvent,
   MouseEventHandler,
-  RefObject,
   useCallback,
   useEffect,
   useMemo,
@@ -14,72 +13,12 @@ import debounce from 'lodash/debounce';
 
 import { TriggerEvent } from '../Tooltip.types';
 
-export interface CreateTooltipEventsBaseArgs {
-  /**
-   *  The `useState` dispatch method to toggle the tooltip state
-   */
-  setState: React.Dispatch<React.SetStateAction<boolean>>;
+import type {
+  CreateTooltipEventsArgs,
+  TooltipEventHandlers,
+} from './tooltipHandlers.types';
 
-  /**
-   * Whether the tooltip event handlers should be enabled
-   */
-  isEnabled?: boolean;
-}
-
-export interface CreateTooltipEventsArgsHover {
-  /**
-   * Whether the tooltip will open/close on `hover` or `click` events.
-   * Note: must match the value passed onto `tooltip`
-   */
-  triggerEvent: typeof TriggerEvent.Hover;
-
-  /**
-   * A ref to the Tooltip element.
-   * Optional when the trigger event is `hover`
-   */
-  tooltipRef?: RefObject<HTMLElement>;
-
-  /** Additional side effects to run on this event */
-  onMouseEnter?: MouseEventHandler<HTMLElement>;
-
-  /** Additional side effects to run on this event */
-  onMouseLeave?: MouseEventHandler<HTMLElement>;
-
-  /** Additional side effects to run on this event */
-  onFocus?: FocusEventHandler<HTMLElement>;
-
-  /** Additional side effects to run on this event */
-  onBlur?: FocusEventHandler<HTMLElement>;
-}
-
-export interface CreateTooltipEventsArgsClick {
-  /**
-   * Whether the tooltip will open/close on `hover` or `click` events.
-   * Note: must match the value passed onto `tooltip`
-   */
-  triggerEvent: typeof TriggerEvent.Click;
-
-  /**
-   * A ref to the Tooltip element.
-   * Optional when the trigger event is `hover`
-   */
-  tooltipRef: RefObject<HTMLElement>;
-
-  /**
-   * Additional side effects to run on this event
-   */
-  onClick?: MouseEventHandler<HTMLElement>;
-}
-
-export interface TooltipHoverEvents {
-  onMouseEnter: MouseEventHandler<HTMLElement>;
-  onMouseLeave: MouseEventHandler<HTMLElement>;
-  onFocus: FocusEventHandler<HTMLElement>;
-  onBlur: FocusEventHandler<HTMLElement>;
-}
-export interface TooltipClickEvents {
-  onClick: MouseEventHandler<HTMLElement>;
-}
+export const CALLBACK_DEBOUNCE = 35; // ms
 
 /**
  * Creates the appropriate event handlers
@@ -87,156 +26,123 @@ export interface TooltipClickEvents {
  *
  * Spread the returned object onto the tooltip trigger element
  */
-export function createTooltipTriggerEventHandlers(
-  args: CreateTooltipEventsBaseArgs & CreateTooltipEventsArgsHover,
-): TooltipHoverEvents;
-export function createTooltipTriggerEventHandlers(
-  args: CreateTooltipEventsBaseArgs & CreateTooltipEventsArgsClick,
-): TooltipClickEvents;
-export function createTooltipTriggerEventHandlers(
-  args: CreateTooltipEventsBaseArgs &
-    (CreateTooltipEventsArgsHover | CreateTooltipEventsArgsClick),
-): TooltipHoverEvents | TooltipClickEvents {
+export function createTooltipTriggerEventHandlers<Trigger extends TriggerEvent>(
+  args: CreateTooltipEventsArgs<Trigger>,
+): TooltipEventHandlers<Trigger> {
   const { setState, triggerEvent, tooltipRef, isEnabled = true } = args;
 
-  switch (triggerEvent) {
-    case TriggerEvent.Hover:
-      return {
-        onMouseEnter: debounce((e: MouseEvent<HTMLElement>) => {
-          if (isEnabled) {
-            args.onMouseEnter?.(e);
-            // Without this the tooltip sometimes opens without a transition. flushSync prevents this state update from automatically batching. Instead updates are made synchronously.
-            // https://react.dev/reference/react-dom/flushSync#flushing-updates-for-third-party-integrations
-            flushSync(() => {
-              setState(true);
-            });
-          }
-        }, 35),
-        onMouseLeave: debounce((e: MouseEvent<HTMLElement>) => {
-          if (isEnabled) {
-            args.onMouseLeave?.(e);
-            setState(false);
-          }
-        }, 35),
-        onFocus: (e: FocusEvent<HTMLElement>) => {
-          if (isEnabled) {
-            args.onFocus?.(e);
+  // switch (triggerEvent) {
+  if (triggerEvent === TriggerEvent.Hover) {
+    const onMouseEnter: MouseEventHandler = debounce(
+      (e: MouseEvent<HTMLElement>) => {
+        if (isEnabled) {
+          args.onMouseEnter?.(e);
+          // Without this the tooltip sometimes opens without a transition. flushSync prevents this state update from automatically batching. Instead updates are made synchronously.
+          // https://react.dev/reference/react-dom/flushSync#flushing-updates-for-third-party-integrations
+          flushSync(() => {
             setState(true);
-          }
-        },
-        onBlur: (e: FocusEvent<HTMLElement>) => {
-          if (isEnabled) {
-            args.onBlur?.(e);
-            setState(false);
-          }
-        },
-      };
-    case TriggerEvent.Click:
-    default:
-      return {
-        onClick: (e: MouseEvent<HTMLElement>) => {
-          if (isEnabled) {
-            // ensure that we don't close the tooltip when content inside tooltip is clicked
-            if (e.target !== tooltipRef?.current) {
-              args.onClick?.(e);
-              setState(curr => !curr);
-            }
-          }
-        },
-      };
+          });
+        }
+      },
+      CALLBACK_DEBOUNCE,
+    );
+
+    const onMouseLeave: MouseEventHandler = debounce(
+      (e: MouseEvent<HTMLElement>) => {
+        if (isEnabled) {
+          args.onMouseLeave?.(e);
+          setState(false);
+        }
+      },
+      CALLBACK_DEBOUNCE,
+    );
+
+    const onFocus: FocusEventHandler = (e: FocusEvent<HTMLElement>) => {
+      if (isEnabled) {
+        args.onFocus?.(e);
+        setState(true);
+      }
+    };
+
+    const onBlur: FocusEventHandler = (e: FocusEvent<HTMLElement>) => {
+      if (isEnabled) {
+        args.onBlur?.(e);
+        setState(false);
+      }
+    };
+
+    return {
+      onMouseEnter,
+      onMouseLeave,
+      onFocus,
+      onBlur,
+    } as TooltipEventHandlers<Trigger>;
+  } else {
+    const onClick = (e: MouseEvent<HTMLElement>) => {
+      if (isEnabled) {
+        // ensure that we don't close the tooltip when content inside tooltip is clicked
+        if (e.target !== tooltipRef?.current) {
+          args.onClick?.(e);
+          setState(curr => !curr);
+        }
+      }
+    };
+
+    return {
+      onClick,
+    } as TooltipEventHandlers<Trigger>;
   }
 }
 
-export function useTooltipTriggerEventHandlers(
-  args: CreateTooltipEventsBaseArgs &
-    (CreateTooltipEventsArgsHover | CreateTooltipEventsArgsClick),
-): {
-  onMouseEnter?: MouseEventHandler<HTMLElement>;
-  onMouseLeave?: MouseEventHandler<HTMLElement>;
-  onFocus?: FocusEventHandler<HTMLElement>;
-  onBlur?: FocusEventHandler<HTMLElement>;
-  onClick?: MouseEventHandler<HTMLElement>;
-} {
-  const { setState, triggerEvent, tooltipRef, isEnabled = true } = args;
-  const isHoverTrigger = triggerEvent === TriggerEvent.Hover;
-  const isClickTrigger = triggerEvent === TriggerEvent.Click;
-
+export function useTooltipTriggerEventHandlers<Trigger extends TriggerEvent>(
+  args: CreateTooltipEventsArgs<Trigger>,
+): TooltipEventHandlers<Trigger> {
   // Ensure we have a stable way
   // of checking if the tooltip is enabled from within the callbacks
   // without creating a new callback on each render
-  const isEnabledRef = useRef(isEnabled);
+  const isEnabledRef = useRef(args.isEnabled);
   useEffect(() => {
-    isEnabledRef.current = isEnabled;
-  }, [isEnabled]);
+    isEnabledRef.current = args.isEnabled;
+  }, [args.isEnabled]);
   const getIsEnabled = useCallback(() => isEnabledRef.current, []);
 
-  return {
-    onMouseEnter: useMemo(
-      () =>
-        isHoverTrigger
-          ? debounce((e: MouseEvent<HTMLElement>) => {
-              if (getIsEnabled()) {
-                args.onMouseEnter?.(e);
-                // Without this the tooltip sometimes opens without a transition. flushSync prevents this state update from automatically batching. Instead updates are made synchronously.
-                // https://react.dev/reference/react-dom/flushSync#flushing-updates-for-third-party-integrations
-                flushSync(() => {
-                  setState(true);
-                });
-              }
-            }, 35)
-          : undefined,
-      [args, getIsEnabled, isHoverTrigger, setState],
-    ),
-    onMouseLeave: useMemo(
-      () =>
-        isHoverTrigger
-          ? debounce((e: MouseEvent<HTMLElement>) => {
-              if (getIsEnabled()) {
-                args.onMouseLeave?.(e);
-                setState(false);
-              }
-            }, 35)
-          : undefined,
-      [args, getIsEnabled, isHoverTrigger, setState],
-    ),
-    onFocus: useMemo(
-      () =>
-        isHoverTrigger
-          ? (e: FocusEvent<HTMLElement>) => {
-              if (getIsEnabled()) {
-                args.onFocus?.(e);
-                setState(true);
-              }
-            }
-          : undefined,
-      [args, getIsEnabled, isHoverTrigger, setState],
-    ),
-    onBlur: useMemo(
-      () =>
-        isHoverTrigger
-          ? (e: FocusEvent<HTMLElement>) => {
-              if (getIsEnabled()) {
-                args.onBlur?.(e);
-                setState(false);
-              }
-            }
-          : undefined,
-      [args, getIsEnabled, isHoverTrigger, setState],
-    ),
-    onClick: useMemo(
-      () =>
-        isClickTrigger
-          ? (e: MouseEvent<HTMLElement>) => {
-              if (getIsEnabled()) {
-                // ensure that we don't close the tooltip when content inside tooltip is clicked
-                if (e.target !== tooltipRef?.current) {
-                  args.onClick?.(e);
-                  setState(curr => !curr);
-                }
-              }
-            }
-          : undefined,
-      [args, isClickTrigger, getIsEnabled, setState, tooltipRef],
-    ),
-  };
+  const callbacks = useMemo(
+    () =>
+      createTooltipTriggerEventHandlers({ ...args, isEnabled: getIsEnabled() }),
+    [args, getIsEnabled],
+  );
+
+  return callbacks as TooltipEventHandlers<Trigger>;
+
+  // return {
+  //   onMouseEnter: useMemo(
+  //     () =>
+  //       triggerEvent === TriggerEvent.Hover
+  //         ? callbacks.onMouseEnter
+  //         : undefined,
+  //     [callbacks.onMouseEnter, triggerEvent],
+  //   ),
+  //   onMouseLeave: useMemo(
+  //     () =>
+  //       triggerEvent === TriggerEvent.Hover
+  //         ? callbacks.onMouseLeave
+  //         : undefined,
+  //     [callbacks.onMouseLeave, triggerEvent],
+  //   ),
+  //   onFocus: useMemo(
+  //     () =>
+  //       triggerEvent === TriggerEvent.Hover ? callbacks.onFocus : undefined,
+  //     [callbacks.onFocus, triggerEvent],
+  //   ),
+  //   onBlur: useMemo(
+  //     () =>
+  //       triggerEvent === TriggerEvent.Hover ? callbacks.onBlur : undefined,
+  //     [callbacks.onBlur, triggerEvent],
+  //   ),
+  //   onClick: useMemo(
+  //     () =>
+  //       triggerEvent === TriggerEvent.Click ? callbacks.onClick : undefined,
+  //     [triggerEvent, callbacks.onClick],
+  //   ),
+  // } as TooltipEventHandlers<Trigger>;
 }
---
+++ packages/tooltip/src/Tooltip/utils/tooltipHandlers.types.ts
@@ -0,0 +1,90 @@
+import { FocusEventHandler, MouseEventHandler, RefObject } from 'react';
+
+import type { TriggerEvent } from '../Tooltip.types';
+
+export interface CreateTooltipEventsBaseArgs {
+  /**
+   *  The `useState` dispatch method to toggle the tooltip state
+   */
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+
+  /**
+   * Whether the tooltip event handlers should be enabled
+   */
+  isEnabled?: boolean;
+}
+
+export interface CreateTooltipEventsArgsHover {
+  /**
+   * Whether the tooltip will open/close on `hover` or `click` events.
+   * Note: must match the value passed onto `tooltip`
+   */
+  triggerEvent: typeof TriggerEvent.Hover;
+
+  /**
+   * A ref to the Tooltip element.
+   * Optional when the trigger event is `hover`
+   */
+  tooltipRef?: RefObject<HTMLElement>;
+
+  /** Additional side effects to run on this event */
+  onMouseEnter?: MouseEventHandler<HTMLElement>;
+
+  /** Additional side effects to run on this event */
+  onMouseLeave?: MouseEventHandler<HTMLElement>;
+
+  /** Additional side effects to run on this event */
+  onFocus?: FocusEventHandler<HTMLElement>;
+
+  /** Additional side effects to run on this event */
+  onBlur?: FocusEventHandler<HTMLElement>;
+}
+
+export interface CreateTooltipEventsArgsClick {
+  /**
+   * Whether the tooltip will open/close on `hover` or `click` events.
+   * Note: must match the value passed onto `tooltip`
+   */
+  triggerEvent: typeof TriggerEvent.Click;
+
+  /**
+   * A ref to the Tooltip element.
+   * Optional when the trigger event is `hover`
+   */
+  tooltipRef: RefObject<HTMLElement>;
+
+  /**
+   * Additional side effects to run on this event
+   */
+  onClick?: MouseEventHandler<HTMLElement>;
+}
+
+export type CreateTooltipEventsArgs<Trigger extends TriggerEvent> =
+  CreateTooltipEventsBaseArgs &
+    (Trigger extends 'hover'
+      ? CreateTooltipEventsArgsHover
+      : Trigger extends 'click'
+      ? CreateTooltipEventsArgsClick
+      : never);
+
+export interface TooltipHoverEvents {
+  onMouseEnter: MouseEventHandler<HTMLElement>;
+  onMouseLeave: MouseEventHandler<HTMLElement>;
+  onFocus: FocusEventHandler<HTMLElement>;
+  onBlur: FocusEventHandler<HTMLElement>;
+  onClick: undefined;
+}
+export interface TooltipClickEvents {
+  onClick: MouseEventHandler<HTMLElement>;
+  onMouseEnter: undefined;
+  onMouseLeave: undefined;
+  onFocus: undefined;
+  onBlur: undefined;
+}
+
+export type TooltipEventHandlers<Trigger extends TriggerEvent> =
+  Trigger extends 'hover'
+    ? TooltipHoverEvents
+    : Trigger extends 'click'
+    ? TooltipClickEvents
+    : never;
