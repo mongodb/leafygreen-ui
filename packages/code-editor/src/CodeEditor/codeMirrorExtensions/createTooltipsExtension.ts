6441b620df53cc16b7a974f70a84b3c2cbed139a
Terrence Keane <terrence.keane@mongodb.com>
2025-06-27 14:53:53 -0400

    LG-5101/5102: Updates editor and syntax highlight styling (#2916)
    * First pass
    
    * style(CodeEditor): increase gutter width from 40px to 52px
    
    * feat(CodeEditor): add custom fold gutter functionality and integrate icon support
    
    * style(CodeEditor): adjust padding for line elements and remove redundant padding
    
    * feat(CodeEditor): get height and passed down styling working as expected
    
    * Broken state
    
    * Fix darkmode
    
    * chore(tooltip): remove unused createTooltipExtension file
    
    * refactor(CodeEditor): remove active line highlighting feature and related tests
    
    * docs(CodeMirror): update comments to include styling information reference
    
    * feat(code-editor): add syntax highlighting and tooltip extensions
    
    * changeset
    
    * refactor(code-editor): rename createCodeMirrorLanguageExtensions to createCodeMirrorLanguageExtension and update code snippets formatting
    
    * fix(code-editor): fix broken test
    
    * refactor(code-editor): remove unused styles and enhance editor sizing props
    
    * feat(code-editor): add responsive sizing props for height and width
    
    * refactor(code-editor): reorganize test utilities and update imports
    
    * fix(tsconfig): add missing reference to palette
    
    * refactor(ALL_PACKAGES): remove unused crawler package
    
    * refactor(code-editor): update padding and background color handling in theme extension
    
    * refactor(testUtils): update import path for CodeEditor modules
    
    * refactor(code-editor): update darkMode argType handling in story metadata
    
    * test(code-editor): update line numbers visibility check in spec
    
    * feat(code-editor): add getEditorStyles function for dynamic styling
    
    * refactor(code-editor): simplify styling logic by using getEditorStyles function
    
    * fix(code-editor): apply !important to selection background for correct styling
    
    * feat(package.json): add exports for testing module
    
    * feat(code-editor): move code snippets into testing utilities export and update docs

--- packages/code-editor/src/CodeEditor/codeMirrorExtensions/createTooltipsExtension.ts
+++ packages/code-editor/src/CodeEditor/codeMirrorExtensions/createTooltipsExtension.ts
@@ -13,13 +13,13 @@ import { CodeEditorTooltip, CodeMirrorExtension } from '../CodeEditor.types';
  *
  * @example
  * ```tsx
- * const tooltipExtension = createCodeMirrorTooltipsExtension([
+ * const tooltipExtension = createTooltipsExtension([
  *   { line: 2, column: 5, length: 4, content: <div>Tooltip 1</div> },
  *   { line: 3, column: 2, length: 2, content: <div>Tooltip 2</div> },
  * ]);
  * ```
  */
-export function createCodeMirrorTooltipsExtension(
+export function createTooltipsExtension(
   tooltips: Array<CodeEditorTooltip>,
 ): CodeMirrorExtension {
   return linter(view => {
