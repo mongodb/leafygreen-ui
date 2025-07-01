c1cb077df68f0e36504641d8bb97b054784d840a
Stephen Lee <stephen.lee@mongodb.com>
2025-07-01 14:31:58 -0700

    [LG-5293] feat: ContextDrawer part 2 (#2922)
    * chore(context-drawer): update dependencies
    
    * fix(context-drawer): ContextDrawerButton glyph rotates
    
    * feat(context-drawer): implement ContextDrawer component with stories and specs
    
    * docs(context-drawer): README
    
    * fix(ContextDrawerButton): styling
    
    * refactor(ContextDrawer): hardcode additional spacing for bottomInterceptRef
    
    * style(ContextDrawer): improve visibility transition and collapse reference border
    
    * docs(context-drawer): README format
    
    * refactor(context-drawer): tab navigation with specs
    
    * update spec
    
    * Reduce redundant css selector specificity
    
    * feat(lib): add formatCssSize util (#2930)
    
    * feat(lib): add formatCssSize util
    
    * refactor(ContextDrawer): formatCssSize on expandedHeight
    
    * chore(lib): changeset
    
    * refactor(lib): formatCssSize trims string values before format
    
    * refactor(lib): formatCssSize types

---
+++ packages/context-drawer/src/ContextDrawer/ContextDrawer.spec.tsx
@@ -0,0 +1,230 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { axe } from 'jest-axe';
+
+import { ContextDrawer, ContextDrawerProps } from '.';
+
+const MockContextDrawer = (props: Partial<ContextDrawerProps> = {}) => {
+  return (
+    <ContextDrawer
+      reference={<div>Reference content</div>}
+      content={<div>Drawer content</div>}
+      trigger={<button>Trigger</button>}
+      {...props}
+    />
+  );
+};
+
+const renderContextDrawer = (props: Partial<ContextDrawerProps> = {}) => {
+  return render(<MockContextDrawer {...props} />);
+};
+
+describe('packages/context-drawer', () => {
+  describe('a11y', () => {
+    test('closed drawer does not have basic accessibility issues', async () => {
+      const { container } = renderContextDrawer({ isOpen: false });
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    });
+
+    test('open drawer does not have basic accessibility issues', async () => {
+      const { container } = renderContextDrawer({ isOpen: true });
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    });
+  });
+
+  test('renders its children', () => {
+    renderContextDrawer();
+    expect(screen.getByText('Reference content')).toBeInTheDocument();
+    expect(screen.getByText('Drawer content')).toBeInTheDocument();
+    expect(screen.getByText('Trigger')).toBeInTheDocument();
+  });
+
+  test('renders a trigger function', () => {
+    const triggerFn = jest.fn(({ isOpen }) => (
+      <button>Trigger is {isOpen ? 'open' : 'closed'}</button>
+    ));
+    renderContextDrawer({ trigger: triggerFn });
+
+    expect(triggerFn).toHaveBeenCalledWith({ isOpen: false });
+    expect(screen.getByText('Trigger is closed')).toBeInTheDocument();
+
+    const trigger = screen.getByRole('button');
+    userEvent.click(trigger);
+
+    expect(triggerFn).toHaveBeenCalledWith({ isOpen: true });
+    expect(screen.getByText('Trigger is open')).toBeInTheDocument();
+  });
+
+  test('renders closed by default', () => {
+    renderContextDrawer({
+      reference: <div>Reference content</div>,
+      content: <div>Drawer content</div>,
+      trigger: <button>Trigger</button>,
+    });
+
+    const content = screen.queryByRole('region');
+    expect(content).toBeNull();
+  });
+
+  test('opens when trigger is clicked', () => {
+    renderContextDrawer({
+      reference: <div>Reference content</div>,
+      content: <div>Drawer content</div>,
+      trigger: <button>Trigger</button>,
+    });
+
+    const triggerButton = screen.getByRole('button', { name: 'Trigger' });
+    userEvent.click(triggerButton);
+
+    const content = screen.getByRole('region');
+    expect(content).toBeVisible();
+  });
+
+  test('calls onOpenChange when trigger is clicked', () => {
+    const onOpenChange = jest.fn();
+    renderContextDrawer({
+      reference: <div>Reference content</div>,
+      content: <div>Drawer content</div>,
+      trigger: <button>Trigger</button>,
+      onOpenChange,
+    });
+
+    const triggerButton = screen.getByRole('button', { name: 'Trigger' });
+    userEvent.click(triggerButton);
+
+    expect(onOpenChange).toHaveBeenCalledTimes(1);
+  });
+
+  // JSDOM doesn't support hiding focusable elements when visibility is hidden
+  // this test is used as a stopgap until the below skipped test can be used
+  test('drawer is hidden when closed', () => {
+    renderContextDrawer({
+      reference: <div>Reference content</div>,
+      content: (
+        <>
+          <div>Drawer content</div>
+          <button>Focusable</button>
+        </>
+      ),
+      trigger: <button>Trigger</button>,
+    });
+    const drawer = screen.queryByRole('region');
+    expect(drawer).toBeNull();
+  });
+  // eslint-disable-next-line jest/no-disabled-tests
+  test.skip('focuses the trigger button first when closed', () => {
+    renderContextDrawer({
+      reference: <div>Reference content</div>,
+      content: (
+        <>
+          <div>Drawer content</div>
+          <button>Focusable</button>
+        </>
+      ),
+      trigger: <button>Trigger</button>,
+    });
+
+    userEvent.tab();
+
+    const triggerButton = screen.getByRole('button', { name: 'Trigger' });
+    const focusableButton = screen.getByRole('button', {
+      name: 'Focusable',
+    });
+    expect(triggerButton).toHaveFocus();
+    expect(focusableButton).not.toHaveFocus();
+  });
+
+  test('first focusable element receives focus when opened and tabbing moves focus through drawer and back to the trigger', async () => {
+    renderContextDrawer({
+      reference: <div>Reference content</div>,
+      content: (
+        <>
+          <div>Drawer content</div>
+          <button>Focusable</button>
+        </>
+      ),
+      trigger: <button>Trigger</button>,
+    });
+
+    const triggerButton = screen.getByRole('button', { name: 'Trigger' });
+
+    userEvent.click(triggerButton);
+
+    const focusableButton = await screen.findByRole('button', {
+      name: 'Focusable',
+    });
+    await waitFor(() => expect(focusableButton).toHaveFocus());
+
+    userEvent.tab();
+    expect(triggerButton).toHaveFocus();
+  });
+
+  test('opens and closes when trigger is clicked', async () => {
+    renderContextDrawer();
+    const trigger = screen.getByText('Trigger');
+    let content = screen.queryByRole('region');
+
+    expect(content).toBeNull();
+
+    userEvent.click(trigger);
+    content = screen.getByRole('region');
+    expect(content).toBeVisible();
+
+    userEvent.click(trigger);
+    await waitFor(() => {
+      expect(content).not.toBeVisible();
+    });
+  });
+
+  test('is open by default when `defaultOpen` is true', () => {
+    renderContextDrawer({ defaultOpen: true });
+    const content = screen.getByRole('region');
+    expect(content).toBeVisible();
+  });
+
+  describe('controlled mode', () => {
+    test('`isOpen` and `onOpenChange` props control the state', () => {
+      const onOpenChange = jest.fn();
+      const { rerender } = renderContextDrawer({
+        isOpen: false,
+        onOpenChange,
+      });
+      const trigger = screen.getByText('Trigger');
+      let content = screen.queryByRole('region');
+
+      expect(content).toBeNull();
+
+      userEvent.click(trigger);
+      content = screen.queryByRole('region');
+      expect(onOpenChange).toHaveBeenCalledTimes(1);
+      expect(content).toBeNull();
+
+      rerender(<MockContextDrawer isOpen={true} onOpenChange={onOpenChange} />);
+      content = screen.getByRole('region');
+      expect(content).toBeVisible();
+
+      userEvent.click(trigger);
+      content = screen.queryByRole('region');
+      expect(onOpenChange).toHaveBeenCalledTimes(2);
+      expect(content).toBeVisible();
+    });
+  });
+
+  describe('aria attributes', () => {
+    test('are set correctly', () => {
+      renderContextDrawer();
+      const trigger = screen.getByText('Trigger');
+      const contentWrapper = screen.getByRole('region', { hidden: true });
+
+      expect(trigger).toHaveAttribute('aria-expanded', 'false');
+      expect(trigger).toHaveAttribute('aria-controls', contentWrapper.id);
+
+      userEvent.click(trigger);
+
+      expect(trigger).toHaveAttribute('aria-expanded', 'true');
+    });
+  });
+});
--- packages/context-drawer/src/ContextDrawerButton/ContextDrawerButton.styles.ts
+++ packages/context-drawer/src/ContextDrawerButton/ContextDrawerButton.styles.ts
@@ -1,18 +1,33 @@
 import { css, cx } from '@leafygreen-ui/emotion';
 import { Theme } from '@leafygreen-ui/lib';
 import { palette } from '@leafygreen-ui/palette';
-import { borderRadius, color, spacing } from '@leafygreen-ui/tokens';
+import {
+  borderRadius,
+  color,
+  fontWeights,
+  spacing,
+  typeScales,
+} from '@leafygreen-ui/tokens';
+
+import { TRANSITION_DURATION } from '../constants';
+
+const BUTTON_HEIGHT = 20;
 
 const getTriggerStyles = (theme: Theme) => {
   const backgroundColor = color[theme].background.info.default;
   const textColor = palette.blue[theme === Theme.Dark ? 'light2' : 'dark3'];
 
   return css`
+    height: ${BUTTON_HEIGHT}px;
     border: none;
     background-color: ${backgroundColor};
     color: ${textColor};
     border-radius: 0 0 ${borderRadius[400]}px ${borderRadius[400]}px;
     padding: 0 ${spacing[200]}px;
+    font-size: ${typeScales.disclaimer.fontSize}px;
+    font-weight: ${fontWeights.medium};
+    line-height: ${typeScales.disclaimer.lineHeight}px;
+    text-transform: none;
 
     &:focus-visible {
       background-color: ${backgroundColor};
@@ -29,16 +44,30 @@ const getTriggerStyles = (theme: Theme) => {
   `;
 };
 
-const getGlyphStyles = (theme: Theme) => css`
-  & svg[role='presentation'] {
-    color: ${palette.blue[theme === Theme.Dark ? 'light2' : 'dark2']};
-  }
-`;
+const getGlyphStyles = ({ isOpen, theme }: { isOpen: boolean; theme: Theme }) =>
+  cx(
+    css`
+      & svg {
+        color: ${palette.blue[theme === Theme.Dark ? 'light2' : 'dark2']};
+        transition: transform ${TRANSITION_DURATION}ms ease-in-out;
+        transform: rotate(0deg);
+      }
+    `,
+    {
+      [css`
+        & svg {
+          transform: rotate(180deg);
+        }
+      `]: isOpen,
+    },
+  );
 
 export const getButtonStyles = ({
   className,
+  isOpen,
   theme,
 }: {
   className?: string;
+  isOpen: boolean;
   theme: Theme;
-}) => cx(getTriggerStyles(theme), getGlyphStyles(theme), className);
+}) => cx(getTriggerStyles(theme), getGlyphStyles({ isOpen, theme }), className);
