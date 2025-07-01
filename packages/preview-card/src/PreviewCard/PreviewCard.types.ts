291464f3ae9d306956d4a7ea729b7f42deb13326
Stephen Lee <stephen.lee@mongodb.com>
2025-07-01 08:09:26 -0700

    [LG-5289] feat: PreviewCard (#2929)
    * feat(preview-card): create package and install deps
    
    * feat(preview-card): implement PreviewCard component
    
    * docs(preview-card): README
    
    * update a11y comment and add test spec

---
+++ packages/preview-card/src/PreviewCard/PreviewCard.types.ts
@@ -0,0 +1,41 @@
+import { ChangeEventHandler } from 'react';
+
+import { DarkModeProps, HTMLElementProps } from '@leafygreen-ui/lib';
+
+export interface PreviewCardProps
+  extends DarkModeProps,
+    HTMLElementProps<'div'> {
+  /**
+   * Height of the card when collapsed, can be a number (in pixels) or a string (e.g., '50%').
+   * @default 140
+   */
+  collapsedHeight?: number | string;
+
+  /**
+   * The default open state of the card when it is uncontrolled.
+   * @default false
+   */
+  defaultOpen?: boolean;
+
+  /**
+   * The open state of the card. Providing this prop will switch the component to controlled mode.
+   */
+  isOpen?: boolean;
+
+  /**
+   * Event handler called when the open state of the card changes.
+   */
+  onOpenChange?: ChangeEventHandler<boolean>;
+
+  /**
+   * Button text when the card is expanded.
+   * @default "View less"
+   */
+  viewLessText?: React.ReactNode;
+
+  /**
+   * Button text when the card is collapsed.
+   * @default "View more"
+   */
+  viewMoreText?: React.ReactNode;
+}
