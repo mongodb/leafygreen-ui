ec90bda5c361617220de2b3dce2c4c6633fc8e5d
Stephen Lee <stephen.lee@mongodb.com>
2025-07-10 07:54:30 -0700

    fix(context-drawer): ContextDrawerButton styles (#2949)
    * fix(context-drawer): svg styling
    
    * chore(context-drawer): changeset

--- packages/context-drawer/src/ButtonCorner/ButtonCorner.tsx
+++ packages/context-drawer/src/ButtonCorner/ButtonCorner.tsx
@@ -16,6 +16,7 @@ export const ButtonCorner = ({ side }: { side: Side }) => {
   return (
     <div className={getWrapperStyles(side)}>
       <svg
+        aria-hidden={true}
         width="8"
         height="8"
         viewBox="0 0 8 8"
