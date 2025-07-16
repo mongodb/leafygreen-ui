6a0f37665ad740cf350864a711643aca15b858a9
Stephen Lee <stephen.lee@mongodb.com>
2025-07-16 08:20:54 -0700

    feat(avatar): add AssistantAvatar

---
+++ packages/avatar/src/AssistantAvatar/AssistantAvatar.stories.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { storybookArgTypes, StoryMetaType } from '@lg-tools/storybook-utils';
+import { StoryObj } from '@storybook/react';
+
+import { AvatarSize } from '../Avatar/Avatar.types';
+
+import { AssistantAvatar } from '.';
+
+export default {
+  title: 'Components/Avatar/AssistantAvatar',
+  component: AssistantAvatar,
+  parameters: {
+    default: 'LiveExample',
+    controls: {
+      exclude: [],
+    },
+    generate: {
+      combineArgs: {
+        darkMode: [false, true],
+        size: Object.values(AvatarSize),
+      },
+    },
+  },
+  args: {
+    size: AvatarSize.Default,
+  },
+  argTypes: {
+    darkMode: storybookArgTypes.darkMode,
+    size: {
+      control: 'select',
+      options: Object.values(AvatarSize),
+    },
+  },
+} satisfies StoryMetaType<typeof AssistantAvatar>;
+
+export const LiveExample: StoryObj<typeof AssistantAvatar> = {
+  render: args => {
+    return <AssistantAvatar {...args} />;
+  },
+  parameters: {
+    chromatic: {
+      disableSnapshot: true,
+    },
+  },
+};
+
+export const Generated: StoryObj<typeof AssistantAvatar> = {
+  render: () => <></>,
+  parameters: {
+    controls: {
+      exclude: ['size', 'darkMode'],
+    },
+  },
+};
---
+++ packages/avatar/src/AssistantAvatar/index.ts
@@ -0,0 +1,1 @@
+export { AssistantAvatar } from './AssistantAvatar';
