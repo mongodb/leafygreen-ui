244fab657c21daabaf10b6999207057ea8a59fe5
Joy Wang <joy.wang@mongodb.com>
2025-07-21 16:34:29 -0400

    story updates

--- chat/suggestion-card/src/SuggestionCard.stories.tsx
+++ chat/suggestion-card/src/SuggestionCard.stories.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import { storybookArgTypes } from '@lg-tools/storybook-utils';
+import {
+  storybookArgTypes,
+  storybookExcludedControlParams,
+  StoryMetaType,
+} from '@lg-tools/storybook-utils';
 import { StoryFn } from '@storybook/react';
 
 import SuggestionCard, { Status, SuggestionCardProps } from '.';
 
-export default {
+const meta: StoryMetaType<typeof SuggestionCard> = {
   title: 'Chat/SuggestionCard',
   component: SuggestionCard,
+  parameters: {
+    default: 'LiveExample',
+    generate: {
+      combineArgs: {
+        darkMode: [false, true],
+        status: Object.values(Status),
+      },
+    },
+    controls: {
+      exclude: [...storybookExcludedControlParams],
+    },
+    docs: {
+      description: {
+        component:
+          'A card that displays a suggestion for a MongoDB cluster configuration and allows the user to apply the suggestion.',
+      },
+    },
+  },
   args: {
     status: Status.Unset,
     suggestedConfigurationParameters: {
@@ -17,21 +39,31 @@ export default {
       ram: '256 GB',
       vCPUs: '64 vCPUs',
     },
+    handleApply: () => {},
+    darkMode: false,
   },
   argTypes: {
     darkMode: storybookArgTypes.darkMode,
     status: {
-      control: 'select',
       options: Object.values(Status),
+      control: { type: 'select' },
     },
     suggestedConfigurationParameters: {
       control: 'object',
     },
+    handleApply: {
+      action: 'handleApply',
+    },
   },
 };
+
+export default meta;
 
 export const LiveExample: StoryFn<SuggestionCardProps> = props => (
   <SuggestionCard {...props} />
 );
+LiveExample.parameters = {
+  chromatic: { disableSnapshot: true },
+};
 
 export const Generated = () => {};
--- chat/suggestion-card/src/SuggestionCard/SuggestionCard.tsx
+++ chat/suggestion-card/src/SuggestionCard/SuggestionCard.tsx
@@ -2,7 +2,9 @@ import React, { forwardRef } from 'react';
 
 import Button from '@leafygreen-ui/button';
 import ArrowLeftIcon from '@leafygreen-ui/icon/dist/ArrowLeft';
-import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
+import LeafyGreenProvider, {
+  useDarkMode,
+} from '@leafygreen-ui/leafygreen-provider';
 
 import { StatusBanner } from '../StatusBanner/StatusBanner';
 
@@ -18,61 +20,72 @@ import { Status, SuggestionCardProps } from './SuggestionCard.types';
 
 const SuggestionCard = forwardRef<HTMLDivElement, SuggestionCardProps>(
   (props, fwdRef) => {
-    const { status, suggestedConfigurationParameters, handleApply } = props;
+    const {
+      status,
+      suggestedConfigurationParameters,
+      handleApply,
+      darkMode: darkModeProp,
+    } = props;
     const { clusterTier, price, cloudProvider, storage, ram, vCPUs } =
       suggestedConfigurationParameters;
-    const { theme } = useDarkMode();
+    const { theme, darkMode } = useDarkMode(darkModeProp);
 
     return (
-      <div ref={fwdRef}>
-        <div className={getSuggetionCardWrapperStyles(theme)}>
-          <div className={boldedTextStyle}>
-            Apply configuration to your cluster?
+      <LeafyGreenProvider darkMode={darkMode}>
+        <div ref={fwdRef}>
+          <div className={getSuggetionCardWrapperStyles(theme)}>
+            <div className={boldedTextStyle}>
+              Apply configuration to your cluster?
+            </div>
+            <table className={tableStyles}>
+              <tr>
+                <th className={tableHeaderStyles}>Cluster Tier</th>
+                <td className={tableCellStyles}>
+                  {clusterTier} ({price})
+                </td>
+              </tr>
+              <tr>
+                <th className={tableHeaderStyles}>Provider</th>
+                <td className={tableCellStyles}>{cloudProvider}</td>
+              </tr>
+              <tr>
+                <th className={tableHeaderStyles}>Storage</th>
+                <td className={tableCellStyles}>{storage}</td>
+              </tr>
+              <tr>
+                <th className={tableHeaderStyles}>RAM</th>
+                <td className={tableCellStyles}>{ram}</td>
+              </tr>
+              <tr>
+                <th className={tableHeaderStyles}>vCPUs</th>
+                <td className={tableCellStyles}>{vCPUs}</td>
+              </tr>
+            </table>
+            {status === Status.Apply && (
+              <Button
+                className={applyButtonStyles}
+                variant="primary"
+                size="small"
+                leftGlyph={<ArrowLeftIcon />}
+                onClick={handleApply}
+              >
+                Apply these suggestions
+              </Button>
+            )}
           </div>
-          <table className={tableStyles}>
-            <tr>
-              <th className={tableHeaderStyles}>Cluster Tier</th>
-              <td className={tableCellStyles}>
-                {clusterTier} ({price})
-              </td>
-            </tr>
-            <tr>
-              <th className={tableHeaderStyles}>Provider</th>
-              <td className={tableCellStyles}>{cloudProvider}</td>
-            </tr>
-            <tr>
-              <th className={tableHeaderStyles}>Storage</th>
-              <td className={tableCellStyles}>{storage}</td>
-            </tr>
-            <tr>
-              <th className={tableHeaderStyles}>RAM</th>
-              <td className={tableCellStyles}>{ram}</td>
-            </tr>
-            <tr>
-              <th className={tableHeaderStyles}>vCPUs</th>
-              <td className={tableCellStyles}>{vCPUs}</td>
-            </tr>
-          </table>
-          {status === Status.Apply && (
-            <Button
-              className={applyButtonStyles}
-              variant="primary"
-              size="small"
-              leftGlyph={<ArrowLeftIcon />}
-              onClick={handleApply}
-            >
-              Apply these suggestions
-            </Button>
+
+          {([Status.Success, Status.Error] as Array<Status>).includes(
+            status,
+          ) && (
+            <StatusBanner
+              status={status}
+              suggestedConfigurationParameters={
+                suggestedConfigurationParameters
+              }
+            />
           )}
         </div>
-
-        {([Status.Success, Status.Error] as Array<Status>).includes(status) && (
-          <StatusBanner
-            status={status}
-            suggestedConfigurationParameters={suggestedConfigurationParameters}
-          />
-        )}
-      </div>
+      </LeafyGreenProvider>
     );
   },
 );
