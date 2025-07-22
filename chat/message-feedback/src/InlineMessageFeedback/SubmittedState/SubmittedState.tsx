377cef0835e7d4ade52f6ddbe1ee408324fbf577
Stephen Lee <stephen.lee@mongodb.com>
2025-07-22 16:03:03 -0700

    fix(message-feedback): SubmittedState styles

--- chat/message-feedback/src/InlineMessageFeedback/SubmittedState/SubmittedState.tsx
+++ chat/message-feedback/src/InlineMessageFeedback/SubmittedState/SubmittedState.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 
 import CheckmarkWithCircle from '@leafygreen-ui/icon/dist/CheckmarkWithCircle';
-import { palette } from '@leafygreen-ui/palette';
+import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
 import { Body } from '@leafygreen-ui/typography';
 
 import { InlineMessageFeedbackProps } from '..';
 
-import { baseStyles, bodyStyles } from './SubmittedState.styles';
+import {
+  containerStyles,
+  getIconFill,
+  getTextStyles,
+} from './SubmittedState.styles';
 
 export const SubmittedState = ({
   submittedMessage,
 }: Pick<InlineMessageFeedbackProps, 'submittedMessage'>) => {
+  const { theme } = useDarkMode();
+
   return (
-    <div className={baseStyles}>
-      <CheckmarkWithCircle color={palette.green.dark1} />
-      <Body className={bodyStyles}>{submittedMessage}</Body>
+    <div className={containerStyles}>
+      <CheckmarkWithCircle fill={getIconFill(theme)} />
+      <Body className={getTextStyles(theme)}>{submittedMessage}</Body>
     </div>
   );
 };
