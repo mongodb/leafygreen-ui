65140a4bc523711e9cf292deb1e7af4e19fc7e30
Terrence Keane <terrence.keane@mongodb.com>
2025-07-23 14:39:05 -0400

    Revert "feat(useModuleLoaders): implement dynamic loading of language extensions based on specified language"
    This reverts commit 41793f294e1e5c18c68a3a1f12c0814e2593e898.

--- packages/code-editor/src/CodeEditor/util/useModuleLoaders.ts
+++ packages/code-editor/src/CodeEditor/util/useModuleLoaders.ts
@@ -8,20 +8,6 @@ interface CodeEditorModules {
   '@uiw/codemirror-extensions-hyper-link': typeof import('@uiw/codemirror-extensions-hyper-link');
   '@codemirror/language': typeof import('@codemirror/language');
   '@codemirror/lint': typeof import('@codemirror/lint');
-  '@lezer/highlight': typeof import('@lezer/highlight');
-  '@codemirror/lang-cpp': typeof import('@codemirror/lang-cpp');
-  '@replit/codemirror-lang-csharp': typeof import('@replit/codemirror-lang-csharp');
-  '@codemirror/lang-css': typeof import('@codemirror/lang-css');
-  '@codemirror/lang-go': typeof import('@codemirror/lang-go');
-  '@codemirror/lang-html': typeof import('@codemirror/lang-html');
-  '@codemirror/lang-java': typeof import('@codemirror/lang-java');
-  '@codemirror/lang-javascript': typeof import('@codemirror/lang-javascript');
-  '@codemirror/lang-json': typeof import('@codemirror/lang-json');
-  '@codemirror/legacy-modes/mode/clike': typeof import('@codemirror/legacy-modes/mode/clike');
-  '@codemirror/lang-php': typeof import('@codemirror/lang-php');
-  '@codemirror/lang-python': typeof import('@codemirror/lang-python');
-  '@codemirror/legacy-modes/mode/ruby': typeof import('@codemirror/legacy-modes/mode/ruby');
-  '@codemirror/lang-rust': typeof import('@codemirror/lang-rust');
 }
 
 export const useModuleLoaders = ({
@@ -30,7 +16,6 @@ export const useModuleLoaders = ({
   forceParsing,
   indentUnit,
   tooltips,
-  language,
 }: // language,
 CodeEditorProps) => {
   const loaders = useMemo(() => {
@@ -41,73 +26,9 @@ CodeEditorProps) => {
         import('@uiw/codemirror-extensions-hyper-link');
     }
 
-    if (enableCodeFolding || forceParsing || !!indentUnit || language) {
+    if (enableCodeFolding || forceParsing || !!indentUnit) {
       neededLoaders['@codemirror/language'] = () =>
         import('@codemirror/language');
-    }
-
-    if (language) {
-      /* If no language is specified, syntax highlighting isn't needed */
-      neededLoaders['@lezer/highlight'] = () => import(`@lezer/highlight`);
-
-      // Load the language extension dynamically based on the specified language
-      switch (language) {
-        case 'cpp':
-          neededLoaders['@codemirror/lang-cpp'] = () =>
-            import('@codemirror/lang-cpp');
-          break;
-        case 'csharp':
-          neededLoaders['@replit/codemirror-lang-csharp'] = () =>
-            import('@replit/codemirror-lang-csharp');
-          break;
-        case 'css':
-          neededLoaders['@codemirror/lang-css'] = () =>
-            import('@codemirror/lang-css');
-          break;
-        case 'go':
-          neededLoaders['@codemirror/lang-go'] = () =>
-            import('@codemirror/lang-go');
-          break;
-        case 'html':
-          neededLoaders['@codemirror/lang-html'] = () =>
-            import('@codemirror/lang-html');
-          break;
-        case 'java':
-          neededLoaders['@codemirror/lang-java'] = () =>
-            import('@codemirror/lang-java');
-          break;
-        case 'javascript':
-        case 'jsx':
-        case 'typescript':
-        case 'tsx':
-          neededLoaders['@codemirror/lang-javascript'] = () =>
-            import('@codemirror/lang-javascript');
-          break;
-        case 'json':
-          neededLoaders['@codemirror/lang-json'] = () =>
-            import('@codemirror/lang-json');
-          break;
-        case 'kotlin':
-          neededLoaders['@codemirror/legacy-modes/mode/clike'] = () =>
-            import('@codemirror/legacy-modes/mode/clike');
-          break;
-        case 'php':
-          neededLoaders['@codemirror/lang-php'] = () =>
-            import('@codemirror/lang-php');
-          break;
-        case 'python':
-          neededLoaders['@codemirror/lang-python'] = () =>
-            import('@codemirror/lang-python');
-          break;
-        case 'ruby':
-          neededLoaders['@codemirror/legacy-modes/mode/ruby'] = () =>
-            import('@codemirror/legacy-modes/mode/ruby');
-          break;
-        case 'rust':
-          neededLoaders['@codemirror/lang-rust'] = () =>
-            import('@codemirror/lang-rust');
-          break;
-      }
     }
 
     if (tooltips && tooltips.length > 0) {
@@ -121,7 +42,7 @@ CodeEditorProps) => {
     forceParsing,
     indentUnit,
     tooltips,
-    language,
+    // language,
   ]);
 
   return loaders;
