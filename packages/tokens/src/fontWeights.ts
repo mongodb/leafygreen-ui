08c3f7d929b83f9c57f4c209e63776d16f717af4
Terrence Keane <terrence.keane@mongodb.com>
2025-07-24 09:24:06 -0400

    fix(fontWeights): update deprecation notice for bold weight to clarify usage

--- packages/tokens/src/fontWeights.ts
+++ packages/tokens/src/fontWeights.ts
@@ -2,7 +2,12 @@ export const fontWeights = {
   regular: 400,
   medium: 500,
   semiBold: 600,
-  /** @deprecated - Prefer {@link semiBold} */
+  /**
+   * @deprecated The font face used has always been a semibold font. Even
+   * though a bold weight was offered, it still was rendering a semibold font.
+   * This has been deprecated in favor of {@link semiBold} to more accurately
+   * reflect the font face being used.
+   */
   bold: 700, // semi-bold inherent to font file is locally defined as bold
 } as const;
 
