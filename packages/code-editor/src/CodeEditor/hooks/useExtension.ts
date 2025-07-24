57bc607caded4da9adf621c6131f3832476cff24
Terrence Keane <terrence.keane@mongodb.com>
2025-07-24 09:44:51 -0400

    docs(useExtension): update documentation for clarity and remove redundant comments

--- packages/code-editor/src/CodeEditor/hooks/useExtension.ts
+++ packages/code-editor/src/CodeEditor/hooks/useExtension.ts
@@ -6,6 +6,12 @@ import { Compartment, EditorView, Extension } from '@uiw/react-codemirror';
  * This hook simplifies using extensions that need to change based on props
  * by handling the compartment creation and reconfiguration internally.
  *
+ * CodeMirror state is immutable. Once configuration is set, the
+ * entire state would need to be updated to update one facet. Compartments allow
+ * us to dynamically change parts of the configuration after
+ * initialization, without needing to recreate the entire editor state.
+ * See https://codemirror.net/examples/config/#dynamic-configuration
+ *
  * @param view The CodeMirror EditorView instance.
  * @param value The dynamic value that the extension depends on. When this value
  * changes, the extension will be reconfigured.
@@ -13,12 +19,6 @@ import { Compartment, EditorView, Extension } from '@uiw/react-codemirror';
  * `Extension`. This function should be stable (e.g., defined outside the
  * component or memoized with useCallback).
  * @returns An `Extension` to be included in the editor's configuration.
- *
- * BACKGROUND: CodeMirror state is immutable. Once configuration is set, the
- * entire state would need to be updated to update one facet. Compartments allow
- * us to dynamically change parts of the configuration after
- * initialization, without needing to recreate the entire editor state.
- * See https://codemirror.net/examples/config/#dynamic-configuration
  */
 export const useExtension = <T>(
   view: EditorView | null,
