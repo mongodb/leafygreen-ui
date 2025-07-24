5abbfbe99fd51a6d0961c5defb3f6ec269499a19
Alina Yingsi Zeng <alina.yzeng@gmail.com>
2025-07-24 18:18:59 -0400

    properly process batches

--- packages/icon/rollup.icon.batch.config.mjs
+++ packages/icon/rollup.icon.batch.config.mjs
@@ -1,21 +1,18 @@
 import { esmConfig, umdConfig } from '@lg-tools/build/config/rollup.config.mjs';
-import path from 'path';
 
 const iconsEnv = process.env.ICONS || '';
 
-const iconNames = iconsEnv.split(',').filter(Boolean);
+const iconNames = iconsEnv.split('|');
 
-export function getBatchIconConfig(names) {
-  const inputs = names.map(name => path.resolve(`src/generated/${name}.tsx`));
-
-  return [esmConfig, umdConfig].map(config => ({
+const iconConfigs = [esmConfig, umdConfig].flatMap(config =>
+  iconNames.map(name => ({
     ...config,
-    input: inputs,
+    input: `src/generated/${name}.tsx`,
     output: {
       ...config.output,
-      //   dir: config.output.dir || 'dist/',
+      name: `${name}.js`,
     },
-  }));
-}
+  })),
+);
 
-export default iconNames.length ? getBatchIconConfig(iconNames) : [];
+export default iconNames.length ? iconConfigs : [];
