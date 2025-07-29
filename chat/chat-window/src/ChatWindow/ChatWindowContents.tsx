dce454d0ea7723046d31e88c3dcfd9b40685c3ff
Stephen Lee <stephen.lee@mongodb.com>
2025-07-29 16:37:08 -0700

    feat(chat-window): ChatWindow compact variant

--- chat/chat-window/src/ChatWindow/ChatWindowContents.tsx
+++ chat/chat-window/src/ChatWindow/ChatWindowContents.tsx
@@ -1,20 +1,20 @@
 import React, { ForwardedRef, forwardRef, ReactChild } from 'react';
 import flattenChildren from 'react-keyed-flatten-children';
-import { useLeafyGreenChatContext } from '@lg-chat/leafygreen-chat-provider';
+import {
+  useLeafyGreenChatContext,
+  Variant,
+} from '@lg-chat/leafygreen-chat-provider';
 import { TitleBar } from '@lg-chat/title-bar';
 
-import { cx } from '@leafygreen-ui/emotion';
 import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
 import { isComponentType } from '@leafygreen-ui/lib';
 import { breakpoints } from '@leafygreen-ui/tokens';
 
 import {
-  baseStyles,
   contentContainerStyles,
-  lgInputBarContainerStyles,
-  lgInputBarStyles,
-  mobileInputBarContainerStyles,
-  themeStyles,
+  getContainerStyles,
+  getInputBarStyles,
+  getInputBarWrapperStyles,
 } from './ChatWindow.styles';
 import { ChatWindowProps } from '.';
 
@@ -33,19 +33,22 @@ export const ChatWindowContents = forwardRef(
     ref: ForwardedRef<HTMLDivElement>,
   ) => {
     const { theme } = useDarkMode(darkModeProp);
-    const { containerWidth } = useLeafyGreenChatContext();
+    const { containerWidth, variant } = useLeafyGreenChatContext();
+
+    const isCompact = variant === Variant.Compact;
+    const isMobile = !!containerWidth && containerWidth < breakpoints.Tablet;
     const flattenedChildren = flattenChildren(children) as Array<ReactChild>;
     const renderedChildren = flattenedChildren.map(child => {
       if (isComponentType(child, 'InputBar')) {
         return (
           <div
-            className={cx(lgInputBarContainerStyles, {
-              [mobileInputBarContainerStyles]:
-                !!containerWidth && containerWidth < breakpoints.Tablet,
+            className={getInputBarWrapperStyles({
+              isCompact,
+              isMobile,
             })}
             key="input-bar-container"
           >
-            <div className={lgInputBarStyles}>{child}</div>
+            <div className={getInputBarStyles(isCompact)}>{child}</div>
           </div>
         );
       } else {
@@ -55,16 +58,22 @@ export const ChatWindowContents = forwardRef(
 
     return (
       <div
-        className={cx(baseStyles, themeStyles[theme], className)}
+        className={getContainerStyles({
+          className,
+          isCompact,
+          theme,
+        })}
         ref={ref}
         {...rest}
       >
-        <TitleBar
-          title={title}
-          badgeText={badgeText}
-          onClose={onClose}
-          iconSlot={iconSlot}
-        />
+        {!isCompact && (
+          <TitleBar
+            title={title}
+            badgeText={badgeText}
+            onClose={onClose}
+            iconSlot={iconSlot}
+          />
+        )}
         <div className={contentContainerStyles}>{renderedChildren}</div>
       </div>
     );
