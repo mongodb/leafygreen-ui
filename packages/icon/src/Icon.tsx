fc83448d3e2d3a18fab74c2636163843d249ae3d
Alina Yingsi Zeng <alina.yzeng@gmail.com>
2025-07-30 15:42:59 -0400

    trigger build

--- packages/icon/src/Icon.tsx
+++ packages/icon/src/Icon.tsx
@@ -5,3 +5,4 @@ import { glyphs } from './glyphs';
  * The LeafyGreen system icons are a clear and concise set of glyphs designed to be minimal in form and express a variety of use-cases for all digital products.
  */
 export const Icon = createIconComponent(glyphs);
+// test comment to invalidate build cache
