ab818d64bf95c3e047e64f7e5bc16deefcd0c73b
bruugey <brooke.yalof@mongodb.com>
2025-08-01 17:29:46 -0400

    update generated

--- packages/icon/src/generated/AIModel.tsx
+++ packages/icon/src/generated/AIModel.tsx
@@ -2,7 +2,7 @@
 * This is a generated file. Do not modify it manually.
 *
 * @script packages/icon/scripts/prebuild.ts
-* @checksum b09c5c5f099bd14d46362ffd2a34685d
+* @checksum a48c17f2c48df0b38a9d9985cfddd889
 */
 import * as React from "react";
 import { css, cx } from '@leafygreen-ui/emotion';
@@ -32,7 +32,7 @@ const AIModel = ({
   });
   return <svg className={cx({
     [fillStyle]: fill != null
-  }, noFlexShrink, className)} height={typeof size === 'number' ? size : sizeMap[size]} width={typeof size === 'number' ? size : sizeMap[size]} role={role} {...accessibleProps} {...props} viewBox="0 0 16 16"><path d="M13.5625 5.30957V10.6895L8 13.4414L2.4375 10.6895V5.30957L8 2.55762L13.5625 5.30957Z" stroke="#fff" /><path d="M8 2L8 14" stroke="#fff" strokeWidth={0.5} /><path d="M3.09998 10.8594L12.5324 5.19998" stroke="#fff" strokeWidth={0.5} strokeLinecap="round" /><path d="M3.19995 5.20001L12.6324 10.8595" stroke="#fff" strokeWidth={0.5} strokeLinecap="round" /><circle cx={8} cy={2.75} r={1.75} fill="#fff" /><circle cx={8} cy={2.75} r={1.75} stroke="fff" /><circle cx={8} cy={13.25} r={1.75} fill="#fff" /><circle cx={8} cy={13.25} r={1.75} stroke="fff" /><circle cx={8} cy={8} r={1.75} fill="#fff" /><circle cx={8} cy={8} r={1.75} stroke="fff" /><circle cx={13.25} cy={5} r={1.75} fill="#fff" /><circle cx={13.25} cy={5} r={1.75} stroke="fff" /><circle cx={13.25} cy={10.75} r={1.75} fill="#fff" /><circle cx={13.25} cy={10.75} r={1.75} stroke="fff" /><circle cx={2.75} cy={10.75} r={1.75} fill="#fff" /><circle cx={2.75} cy={10.75} r={1.75} stroke="fff" /><circle cx={2.75} cy={5} r={1.75} fill="#fff" /><circle cx={2.75} cy={5} r={1.75} stroke="fff" /></svg>;
+  }, noFlexShrink, className)} height={typeof size === 'number' ? size : sizeMap[size]} width={typeof size === 'number' ? size : sizeMap[size]} role={role} {...accessibleProps} {...props} viewBox="0 0 16 16"><path d="M13.5625 5.30957V10.6895L8 13.4414L2.4375 10.6895V5.30957L8 2.55762L13.5625 5.30957Z" stroke={'currentColor'} /><path d="M8 2L8 14" stroke={'currentColor'} strokeWidth={0.5} /><path d="M3.09998 10.8594L12.5324 5.19998" stroke={'currentColor'} strokeWidth={0.5} strokeLinecap="round" /><path d="M3.19995 5.20001L12.6324 10.8595" stroke={'currentColor'} strokeWidth={0.5} strokeLinecap="round" /><circle cx={8} cy={2.75} r={1.75} fill={'currentColor'} /><circle cx={8} cy={2.75} r={1.75} stroke={'currentColor'} /><circle cx={8} cy={13.25} r={1.75} fill={'currentColor'} /><circle cx={8} cy={13.25} r={1.75} stroke={'currentColor'} /><circle cx={8} cy={8} r={1.75} fill={'currentColor'} /><circle cx={8} cy={8} r={1.75} stroke={'currentColor'} /><circle cx={13.25} cy={5} r={1.75} fill={'currentColor'} /><circle cx={13.25} cy={5} r={1.75} stroke={'currentColor'} /><circle cx={13.25} cy={10.75} r={1.75} fill={'currentColor'} /><circle cx={13.25} cy={10.75} r={1.75} stroke={'currentColor'} /><circle cx={2.75} cy={10.75} r={1.75} fill={'currentColor'} /><circle cx={2.75} cy={10.75} r={1.75} stroke={'currentColor'} /><circle cx={2.75} cy={5} r={1.75} fill={'currentColor'} /><circle cx={2.75} cy={5} r={1.75} stroke={'currentColor'} /></svg>;
 };
 AIModel.displayName = 'AIModel';
 AIModel.isGlyph = true;
