2225976ec63bd7bf13ff9194454b458a16be7673
Shaneeza <shaneeza.ali@mongodb.com>
2025-08-04 10:35:41 -0400

    remove type from Size export

--- packages/drawer/src/Drawer/index.ts
+++ packages/drawer/src/Drawer/index.ts
@@ -1,4 +1,4 @@
 export { Drawer } from './Drawer';
 export { MOBILE_BREAKPOINT } from './Drawer.constants';
 export { drawerClassName } from './Drawer.styles';
-export { DisplayMode, type DrawerProps, type Size } from './Drawer.types';
+export { DisplayMode, type DrawerProps, Size } from './Drawer.types';
--- packages/drawer/src/index.ts
+++ packages/drawer/src/index.ts
@@ -3,7 +3,7 @@ export {
   Drawer,
   drawerClassName,
   type DrawerProps,
-  type Size,
+  Size,
 } from './Drawer';
 export { DrawerLayout, type DrawerLayoutProps } from './DrawerLayout';
 export {
