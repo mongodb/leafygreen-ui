d3c0f1f40d478a4a9bf900e1141ea3bca76cb644
Stephen Lee <stephen.lee@mongodb.com>
2025-08-15 08:07:50 -0700

    [LG-5431] fix(button): add constants subpath export for PRIMARY_BUTTON_INTERACTIVE_GREEN (#3035)
    * fix: button constant export and transpilation
    
    * chore: changeset
    
    * fix(button): build
    
    * fix(button): rollup includes /testing export
    
    * fix(button): rollup config

---
+++ packages/button/src/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Off-palette value specific to primary button instances
+ * @see TODO: https://jira.mongodb.org/browse/LG-5388
+ */
+export const PRIMARY_BUTTON_INTERACTIVE_GREEN = '#00593F';
