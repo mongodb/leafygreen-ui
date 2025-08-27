40b2a94d131c64116383e9721236e5b45787e1d8
Terrence Keane <terrence.keane@mongodb.com>
2025-08-27 15:33:07 -0400

    feat(Panel): Add shortcut menu functionality

---
+++ packages/code-editor/src/CodeEditor/ShortcutMenu/ShortcutMenu.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import Badge from '@leafygreen-ui/badge';
+import { Disclaimer, H3 } from '@leafygreen-ui/typography';
+
+import {
+  HeadingStyles,
+  PlusSignStyles,
+  TableContainerStyles,
+  TableStyles,
+} from './ShortcutMenu.styles';
+
+export function ShortcutMenu({ className }: { className?: string }) {
+  return (
+    <div className={className}>
+      <H3 className={HeadingStyles}>Code Editor Shortcuts</H3>
+      <div className={TableContainerStyles}>
+        <table className={TableStyles}>
+          <tr>
+            <td>
+              <Badge variant="lightgray">TAB</Badge>
+            </td>
+            <td>
+              <Disclaimer>to indent</Disclaimer>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Badge variant="lightgray">ESC</Badge>
+              <Disclaimer className={PlusSignStyles}>+</Disclaimer>
+              <Badge variant="lightgray">TAB</Badge>
+            </td>
+            <td>
+              <Disclaimer>to change focus</Disclaimer>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Badge variant="lightgray">⌘/CTRL</Badge>
+              <Disclaimer className={PlusSignStyles}>+</Disclaimer>
+              <Badge variant="lightgray">F</Badge>
+            </td>
+            <td>
+              <Disclaimer>to find</Disclaimer>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Badge variant="lightgray">⌘/CTRL</Badge>
+              <Disclaimer className={PlusSignStyles}>+</Disclaimer>
+              <Badge variant="lightgray">Z</Badge>
+            </td>
+            <td>
+              <Disclaimer>to undo</Disclaimer>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <Badge variant="lightgray">⌘/CTRL</Badge>
+              <Disclaimer className={PlusSignStyles}>+</Disclaimer>
+              <Badge variant="lightgray">SHIFT</Badge>
+              <Disclaimer className={PlusSignStyles}>+</Disclaimer>
+              <Badge variant="lightgray">Z</Badge>
+            </td>
+            <td>
+              <Disclaimer>to redo</Disclaimer>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
