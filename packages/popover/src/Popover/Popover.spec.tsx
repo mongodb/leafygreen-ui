0ba1a611828251bcadb653be171be42840fd5f07
Adam Thompson <adam.thompson@mongodb.com>
2025-09-03 14:08:17 -0400

    packages/popover/src/Popover/Popover.spec.tsx

--- packages/popover/src/Popover/Popover.spec.tsx
+++ packages/popover/src/Popover/Popover.spec.tsx
@@ -536,7 +536,7 @@ describe('packages/popover', () => {
     test(`does not allow specifying portal props, when renderMode is not ${RenderMode.Portal}`, () => {
       const scrollContainer = document.createElement('div');
 
-      // @ts-expect-error
+      // @ts-ignore React18-expect-error
       <Popover
         active
         renderMode={RenderMode.Inline}
@@ -546,7 +546,7 @@ describe('packages/popover', () => {
         Popover Content
       </Popover>;
 
-      //@ts-expect-error
+      // @ts-ignore React18-expect-error
       <Popover
         active
         renderMode={RenderMode.Inline}
@@ -556,7 +556,7 @@ describe('packages/popover', () => {
         Popover Content
       </Popover>;
 
-      // @ts-expect-error
+      //  @ts-ignore React18-expect-error
       <Popover
         active
         renderMode={RenderMode.TopLayer}
@@ -566,7 +566,7 @@ describe('packages/popover', () => {
         Popover Content
       </Popover>;
 
-      //@ts-expect-error
+      // @ts-ignore React18-expect-error
       <Popover
         active
         renderMode={RenderMode.TopLayer}
@@ -578,7 +578,7 @@ describe('packages/popover', () => {
     });
 
     test(`does not allow specifying dismissMode or onToggle, when renderMode is not ${RenderMode.TopLayer}`, () => {
-      // @ts-expect-error
+      //  @ts-ignore React18-expect-error
       <Popover
         active
         renderMode={RenderMode.Inline}
@@ -588,7 +588,7 @@ describe('packages/popover', () => {
         Popover Content
       </Popover>;
 
-      // @ts-expect-error
+      //  @ts-ignore React18-expect-error
       <Popover
         active
         renderMode={RenderMode.Portal}
