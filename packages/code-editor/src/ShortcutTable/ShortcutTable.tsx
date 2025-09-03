3ecfc5a43e5c44ba1c6d1e48797416b98564e3bc
Terrence Keane <terrence.keane@mongodb.com>
2025-09-03 12:01:14 -0400

    fix(ShortcutTable): wrap table rows in tbody for proper HTML structure

--- packages/code-editor/src/ShortcutTable/ShortcutTable.tsx
+++ packages/code-editor/src/ShortcutTable/ShortcutTable.tsx
@@ -16,54 +16,56 @@ export function ShortcutTable({ className }: { className?: string }) {
       <H3 className={HeadingStyles}>Code Editor Shortcuts</H3>
       <div className={TableContainerStyles}>
         <table className={TableStyles}>
-          <tr>
-            <td>
-              <Badge variant="lightgray">TAB</Badge>
-            </td>
-            <td>
-              <Disclaimer>to indent</Disclaimer>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Badge variant="lightgray">ESC</Badge>
-            </td>
-            <td>
-              <Disclaimer>to unfocus editor</Disclaimer>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Badge variant="lightgray">⌘/CTRL</Badge>
-              <Disclaimer className={PlusSignStyles}>+</Disclaimer>
-              <Badge variant="lightgray">F</Badge>
-            </td>
-            <td>
-              <Disclaimer>to find</Disclaimer>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Badge variant="lightgray">⌘/CTRL</Badge>
-              <Disclaimer className={PlusSignStyles}>+</Disclaimer>
-              <Badge variant="lightgray">Z</Badge>
-            </td>
-            <td>
-              <Disclaimer>to undo</Disclaimer>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Badge variant="lightgray">⌘/CTRL</Badge>
-              <Disclaimer className={PlusSignStyles}>+</Disclaimer>
-              <Badge variant="lightgray">SHIFT</Badge>
-              <Disclaimer className={PlusSignStyles}>+</Disclaimer>
-              <Badge variant="lightgray">Z</Badge>
-            </td>
-            <td>
-              <Disclaimer>to redo</Disclaimer>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>
+                <Badge variant="lightgray">TAB</Badge>
+              </td>
+              <td>
+                <Disclaimer>to indent</Disclaimer>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Badge variant="lightgray">ESC</Badge>
+              </td>
+              <td>
+                <Disclaimer>to unfocus editor</Disclaimer>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Badge variant="lightgray">⌘/CTRL</Badge>
+                <Disclaimer className={PlusSignStyles}>+</Disclaimer>
+                <Badge variant="lightgray">F</Badge>
+              </td>
+              <td>
+                <Disclaimer>to find</Disclaimer>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Badge variant="lightgray">⌘/CTRL</Badge>
+                <Disclaimer className={PlusSignStyles}>+</Disclaimer>
+                <Badge variant="lightgray">Z</Badge>
+              </td>
+              <td>
+                <Disclaimer>to undo</Disclaimer>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Badge variant="lightgray">⌘/CTRL</Badge>
+                <Disclaimer className={PlusSignStyles}>+</Disclaimer>
+                <Badge variant="lightgray">SHIFT</Badge>
+                <Disclaimer className={PlusSignStyles}>+</Disclaimer>
+                <Badge variant="lightgray">Z</Badge>
+              </td>
+              <td>
+                <Disclaimer>to redo</Disclaimer>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
