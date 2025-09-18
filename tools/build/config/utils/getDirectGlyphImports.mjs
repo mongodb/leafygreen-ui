b02593058e82cd1caf7edf86427c63eba823a0d3
Adam Thompson <adam.thompson@mongodb.com>
2025-09-18 12:17:35 -0400

    ensure lg packages have umd global name

---
+++ tools/build/config/utils/getDirectGlyphImports.mjs
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * @returns An array of all glyph import paths
+ */
+export function getDirectGlyphImports(allDependencies) {
+  const pkgHasIconDependency = allDependencies['@leafygreen-ui/icon'];
+  const glyphsDir = path.resolve(process.cwd(), '../icon/src/glyphs');
+
+  if (pkgHasIconDependency && fs.existsSync(glyphsDir)) {
+    return fs
+      .readdirSync(glyphsDir)
+      .filter(path => /.svg/.test(path))
+      .map(
+        fileName =>
+          `@leafygreen-ui/icon/dist/${path.basename(fileName, '.svg')}`,
+      );
+  }
+
+  return [];
+}
