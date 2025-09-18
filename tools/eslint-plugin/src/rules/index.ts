ba279c4fd1da64884273c4da60faf39d1821703c
Adam Thompson <adam.thompson@mongodb.com>
2025-09-18 17:06:17 -0400

    Update index.ts

--- tools/eslint-plugin/src/rules/index.ts
+++ tools/eslint-plugin/src/rules/index.ts
@@ -2,12 +2,8 @@
  * DO NOT MODIFY THIS FILE
  * ANY CHANGES WILL BE REMOVED ON THE NEXT BUILD
  */
-import { booleanVerbPrefixRule } from './boolean-verb-prefix';
 import { noIndirectImportsRule } from './no-indirect-imports';
-import { standardTestidRule } from './standard-testid';
 
 export const rules = {
-  'boolean-verb-prefix': booleanVerbPrefixRule,
   'no-indirect-imports': noIndirectImportsRule,
-  'standard-testid': standardTestidRule,
 };
