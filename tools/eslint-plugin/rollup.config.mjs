965bdc6c4ece2f5ada3868394485c0cd1cbf30d1
Adam Thompson <adam.thompson@mongodb.com>
2025-09-19 12:10:36 -0400

    Delete rollup.config.mjs

--- tools/eslint-plugin/rollup.config.mjs
+++
@@ -1,8 +0,0 @@
-import { umdConfig } from '@lg-tools/build/config/rollup.config.mjs';
-
-const config = {
-  ...umdConfig,
-  external: [...umdConfig.external, /^@typescript-eslint\//],
-};
-
-export default [config];
