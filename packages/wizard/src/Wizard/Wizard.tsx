360e1ad8da7ae200829320b1336c9cbfe4a1e23d
Adam Thompson <adam.thompson@mongodb.com>
2025-09-24 13:58:45 -0400

    scaffolds wizard package

---
+++ packages/wizard/src/Wizard/Wizard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { WizardProps } from './Wizard.types';
+
+export function Wizard({}: WizardProps) {
+  return <div>your content here</div>;
+}
+
+Wizard.displayName = 'Wizard';
