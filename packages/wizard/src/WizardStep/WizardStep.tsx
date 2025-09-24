2096173b574760b2c1fe7d1ec5048830b5ae2560
Adam Thompson <adam.thompson@mongodb.com>
2025-09-24 14:57:19 -0400

    scaffold wizard step

---
+++ packages/wizard/src/WizardStep/WizardStep.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { WizardStepProps } from './WizardStep.types';
+
+export function WizardStep({}: WizardStepProps) {
+  return <div>your content here</div>;
+}
+
+WizardStep.displayName = 'WizardStep';
