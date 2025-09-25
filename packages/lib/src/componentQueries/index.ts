5fe4f9d58f70e03a0c83ae3fe49d7963b22c9ceb
Adam Thompson <adam.thompson@mongodb.com>
2025-09-25 12:59:35 -0400

    update index files

---
+++ packages/lib/src/componentQueries/index.ts
@@ -0,0 +1,5 @@
+export { filterChildren } from './filterChildren';
+export { findChild } from './findChild';
+export { findChildren } from './findChildren';
+export { hasAnyStaticProperty, hasStaticProperty } from './hasStaticProperty';
+export { isChildWithProperty } from './isChildWithProperty';
