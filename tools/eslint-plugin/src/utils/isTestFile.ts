0af63e3fc93a99df3570857198d49d602c198f8f
Adam Thompson <adam.thompson@mongodb.com>
2025-09-25 15:41:45 -0400

    Delete isTestFile.ts

--- tools/eslint-plugin/src/utils/isTestFile.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Returns whether the string is a spec, story or testutils file
- */
-export const isTestFile = (filename: string): boolean => {
-  const testFileRegex = /.+\.((spec)|(story)|(testutils))\.tsx?$/;
-  const isTestFile = testFileRegex.test(filename);
-  return isTestFile;
-};
