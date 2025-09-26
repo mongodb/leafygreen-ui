f5d32af71782e572f93a88496913e1a6dfc4afa0
Adam Thompson <adam.thompson@mongodb.com>
2025-09-26 16:33:33 -0400

    use typography in Step

--- packages/wizard/src/WizardStep/WizardStep.styles.ts
+++ packages/wizard/src/WizardStep/WizardStep.styles.ts
@@ -1,20 +1,5 @@
 import { css } from '@leafygreen-ui/emotion';
 
-export const stepTitleStyles = css`
-  font-size: 24px;
-  font-weight: 600;
-  line-height: 1.33;
-  margin-bottom: 8px;
-  color: inherit;
-`;
-
-export const stepDescriptionStyles = css`
-  font-size: 16px;
-  line-height: 1.5;
-  margin-bottom: 24px;
-  color: inherit;
-`;
-
 export const stepContentStyles = css`
   /* Content styles */
 `;
