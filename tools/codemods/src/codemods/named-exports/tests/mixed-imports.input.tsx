1923bfcfa6f8b9b3a277c4bca730a6b6d444cf58
Stephen Lee <stephen.lee@mongodb.com>
2025-09-30 07:42:48 -0700

    feat(codemods): add named-exports codemod

---
+++ tools/codemods/src/codemods/named-exports/tests/mixed-imports.input.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Button, { Size } from '@leafygreen-ui/button';
+import Popover, { Align } from '@leafygreen-ui/popover';
+
+const App = () => {
+  return (
+    <div>
+      <Button size={Size.Small}>Click me</Button>
+      <Popover align={Align.Top}>Popover content</Popover>
+    </div>
+  );
+};
+
+export default App;
