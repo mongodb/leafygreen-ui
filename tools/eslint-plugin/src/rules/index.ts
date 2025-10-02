90a296ae8e6d7c9dae4e71f19b3c26b39fcde934
Kræn Hansen <kraen.hansen@mongodb.com>
2025-10-02 14:49:14 +0200

    Create a rule avoid duplicate data-lgid and data-testid attributes

--- tools/eslint-plugin/src/rules/index.ts
+++ tools/eslint-plugin/src/rules/index.ts
@@ -1,11 +1,14 @@
 /**
  * DO NOT MODIFY THIS FILE
  * ANY CHANGES WILL BE REMOVED ON THE NEXT BUILD
+ * USE THE "create-rule" SCRIPT TO ADD NEW RULES INSTEAD
  */
+import { noDuplicateIdsRule } from './no-duplicate-ids';
 import { noIndirectImportsRule } from './no-indirect-imports';
 import { noRenderHookFromRtlRule } from './no-render-hook-from-rtl';
 
 export const rules = {
+  'no-duplicate-ids': noDuplicateIdsRule,
   'no-indirect-imports': noIndirectImportsRule,
   'no-render-hook-from-rtl': noRenderHookFromRtlRule,
 };
