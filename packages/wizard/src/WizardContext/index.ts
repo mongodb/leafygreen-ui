e1077dba270f6051e67de7e50e84cd69d7921544
Adam Thompson <adam.thompson@mongodb.com>
2025-10-06 12:11:56 -0400

    fix exports

--- packages/wizard/src/WizardContext/index.ts
+++ packages/wizard/src/WizardContext/index.ts
@@ -1,5 +1,5 @@
 export {
+  useWizardContext,
   WizardContext,
   type WizardContextData,
-  useWizardContext,
 } from './WizardContext';
--- packages/wizard/src/index.ts
+++ packages/wizard/src/index.ts
@@ -1,8 +1,8 @@
 export { Wizard, type WizardProps } from './Wizard';
 export {
-  WizardContext,
-  WizardContextData,
   useWizardContext,
+  WizardContext,
+  type WizardContextData,
 } from './WizardContext';
 export { type WizardFooterProps } from './WizardFooter';
 export { type WizardStepProps } from './WizardStep';
