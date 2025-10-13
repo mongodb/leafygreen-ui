a4f71153a34b7cb350e3b14ca51c5c1977b467b8
Adam Thompson <adam.thompson@mongodb.com>
2025-10-13 16:45:26 -0400

    deprecate Spinner from loading-indicator

--- packages/loading-indicator/src/index.ts
+++ packages/loading-indicator/src/index.ts
@@ -1,3 +1,12 @@
 import PageLoader, { type PageLoaderProps } from './PageLoader';
 import Spinner, { type SpinnerProps } from './Spinner';
-export { PageLoader, type PageLoaderProps, Spinner, type SpinnerProps };
+export {
+  PageLoader,
+  type PageLoaderProps,
+  /**
+   * @deprecated - This package installs and bundles Lottie player, a heavy run-time animation library.
+   * For simple loading spinners, prefer using the `@leafygreen-ui/loading-spinner` package
+   */
+  Spinner,
+  type SpinnerProps,
+};
