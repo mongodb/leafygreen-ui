11840b5380e4321d206266007051754e6d81638c
Adam Thompson <adam.thompson@mongodb.com>
2025-10-13 13:26:10 -0400

    codemod tests

---
+++ tools/codemods/src/codemods/named-exports/tests/banner.input.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Banner from '@leafygreen-ui/banner';
+
+const App = () => {
+  return (
+    <div>
+      <Banner>Banner content</Banner>
+    </div>
+  );
+};
+
+export default App;
