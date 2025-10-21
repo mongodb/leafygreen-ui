b48488b3658a5075f52180759f6960e46a0d9006
Adam Thompson <adam.thompson@mongodb.com>
2025-10-21 17:32:51 -0400

    refactor(WizardFooter): simplify props by extending FormFooterProps

--- packages/wizard/src/WizardFooter/WizardFooter.types.ts
+++ packages/wizard/src/WizardFooter/WizardFooter.types.ts
@@ -1,18 +1,5 @@
 import { FormFooterProps } from '@leafygreen-ui/form-footer';
 
-export interface WizardFooterProps extends React.ComponentProps<'footer'> {
-  /**
-   * Props for the back button (left-most button)
-   */
-  backButtonProps?: FormFooterProps['backButtonProps'];
-
-  /**
-   * Props for the cancel button (center button)
-   */
-  cancelButtonProps?: FormFooterProps['cancelButtonProps'];
-
-  /**
-   * Props for the primary button (right-most button)
-   */
-  primaryButtonProps: FormFooterProps['primaryButtonProps'];
-}
+export interface WizardFooterProps
+  extends React.ComponentProps<'footer'>,
+    FormFooterProps {}
