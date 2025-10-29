2f600d94d4b5202ce7503920c8e44fadad05d05e
Shaneeza <shaneeza.ali@mongodb.com>
2025-10-29 16:02:18 -0400

    refactor(input-box): consolidate InputBox stories into a single file and enhance control parameters

--- packages/input-box/src/InputBox.stories.tsx
+++ packages/input-box/src/InputBox.stories.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
+import {
+  storybookExcludedControlParams,
+  StoryMetaType,
+} from '@lg-tools/storybook-utils';
 import { StoryFn } from '@storybook/react';
 
-import { InputBox } from '.';
+import { css } from '@leafygreen-ui/emotion';
+import { palette } from '@leafygreen-ui/palette';
 
-export default {
-  title: 'Components/InputBox',
+import { InputBoxWithState } from './testutils';
+
+import { InputBox } from './InputBox';
+
+const meta: StoryMetaType<typeof InputBox> = {
+  title: 'Components/Inputs/InputBox',
   component: InputBox,
+  decorators: [
+    StoryFn => (
+      <div
+        className={css`
+          border: 1px solid ${palette.gray.base};
+        `}
+      >
+        <StoryFn />
+      </div>
+    ),
+  ],
+  parameters: {
+    default: 'LiveExample',
+    controls: {
+      exclude: [
+        ...storybookExcludedControlParams,
+        'segments',
+        'segmentObj',
+        'segmentRefs',
+        'setSegment',
+        'charsPerSegment',
+        'formatParts',
+        'segmentRules',
+        'labelledBy',
+        'onSegmentChange',
+        'renderSegment',
+      ],
+    },
+  },
 };
+export default meta;
 
-const Template: StoryFn<typeof InputBox> = props => <InputBox {...props} />;
-
-export const Basic = Template.bind({});
+export const LiveExample: StoryFn<typeof InputBox> = props => {
+  return <InputBoxWithState {...props} />;
+};
