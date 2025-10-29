fe07756db5499c951ec01a373ff977112d607a90
Stephen Lee <stephen.lee@mongodb.com>
2025-10-29 13:17:23 -0700

    [LG-5645] feat(chat-layout): implement ChatMain component (#3252)
    * feat(chat-layout): implement ChatMain component
    
    * refactor(chat-layout): remove PropsWithChildren

--- chat/chat-layout/src/ChatLayout/ChatLayout.types.ts
+++ chat/chat-layout/src/ChatLayout/ChatLayout.types.ts
@@ -1,19 +1,19 @@
-import { ComponentPropsWithRef, PropsWithChildren } from 'react';
+import { ComponentPropsWithRef } from 'react';
 
 import { DarkModeProps } from '@leafygreen-ui/lib';
 
-export type ChatLayoutProps = ComponentPropsWithRef<'div'> &
-  DarkModeProps &
-  PropsWithChildren<{
-    /**
-     * Initial state for whether the side nav is pinned (expanded).
-     * @default true
-     */
-    initialIsPinned?: boolean;
+export interface ChatLayoutProps
+  extends ComponentPropsWithRef<'div'>,
+    DarkModeProps {
+  /**
+   * Initial state for whether the side nav is pinned (expanded).
+   * @default true
+   */
+  initialIsPinned?: boolean;
 
-    /**
-     * Callback fired when the side nav is toggled (pinned/unpinned).
-     * Receives the new `isPinned` state as an argument.
-     */
-    onTogglePinned?: (isPinned: boolean) => void;
-  }>;
+  /**
+   * Callback fired when the side nav is toggled (pinned/unpinned).
+   * Receives the new `isPinned` state as an argument.
+   */
+  onTogglePinned?: (isPinned: boolean) => void;
+}
