01e06af1ccc78d3f8106dda7a5f9fb4b21806b01
Adam Rasheed <adam.rasheed@mongodb.com>
2025-10-28 18:07:06 -0700

    rm 100 value, updated changeset

---
+++ .changeset/cold-rivers-drive.md
@@ -0,0 +1,22 @@
+---
+'@leafygreen-ui/tokens': major
+'@leafygreen-ui/confirmation-modal': minor
+'@leafygreen-ui/expandable-card': minor
+'@leafygreen-ui/search-input': minor
+'@leafygreen-ui/code-editor': minor
+'@leafygreen-ui/date-picker': minor
+'@leafygreen-ui/form-footer': minor
+'@leafygreen-ui/combobox': minor
+'@lg-chat/chat-window': minor
+'@leafygreen-ui/emotion': minor
+'@leafygreen-ui/select': minor
+'@lg-chat/title-bar': minor
+'@leafygreen-ui/modal': minor
+'@leafygreen-ui/table': minor
+'@leafygreen-ui/toast': minor
+'@leafygreen-ui/card': minor
+'@leafygreen-ui/menu': minor
+'@lg-chat/message': minor
+---
+
+Updated major version to Shadow tokens. `shadow` object and its key/values have been changed. Other packages utilizing the shadow values have had a minor update to accommodate the changes.
--- packages/tokens/src/shadows/shadows.ts
+++ packages/tokens/src/shadows/shadows.ts
@@ -1,7 +1,7 @@
 import { Theme } from '@leafygreen-ui/lib';
 import { palette } from '@leafygreen-ui/palette';
 
-export type ShadowKey = 100 | 1 | 2 | 3 | 'overflow';
+export type ShadowKey = 1 | 2 | 3 | 'overflow';
 
 type Value = {
   [key in ShadowKey]: string;
@@ -9,14 +9,12 @@ type Value = {
 
 export const shadows: Record<Theme, Value> = {
   [Theme.Light]: {
-    100: `color-mix(in srgb, ${palette.black} 15%, transparent)`,
     1: `color-mix(in srgb, ${palette.black} 15%, transparent)`,
     2: `color-mix(in srgb, ${palette.black} 20%, transparent)`,
     3: `color-mix(in srgb, ${palette.black} 60%, transparent)`,
     overflow: `color-mix(in srgb, ${palette.gray.dark1} 30%, transparent)`,
   },
   [Theme.Dark]: {
-    100: `unset`, // no shadow in dark mode
     1: 'transparent',
     2: `color-mix(in srgb, #000000 45%, transparent)`,
     3: `color-mix(in srgb, #000000 60%, transparent)`,
@@ -26,16 +24,12 @@ export const shadows: Record<Theme, Value> = {
 
 export const boxShadows: Record<Theme, Value> = {
   [Theme.Light]: {
-    /* @deprecated use shadows[Theme.Light][1], shadows[Theme.Light][2], shadows[Theme.Light][3] instead */
-    100: `0px 2px 4px 1px ${shadows[Theme.Light][100]}`,
     1: `0px 2px 4px 1px ${shadows[Theme.Light][1]}`,
     2: `0px 18px 18px -15px ${shadows[Theme.Light][2]}`,
     3: `0px 8px 20px -8px ${shadows[Theme.Light][3]}`,
     overflow: `0px 2px 4px 1px ${shadows[Theme.Light]['overflow']}`,
   },
   [Theme.Dark]: {
-    /* @deprecated use shadows[Theme.Dark][1], shadows[Theme.Dark][2], shadows[Theme.Dark][3] instead */
-    100: `unset`, // no shadow in dark mode
     1: `unset`,
     2: `0 18px 18px -15px ${shadows[Theme.Dark][2]}`,
     3: `0 8px 20px -8px ${shadows[Theme.Dark][3]}`,
