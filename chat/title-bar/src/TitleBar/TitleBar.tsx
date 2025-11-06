af60a2db56ed429832dc3b1ac48c79dda46e5861
Stephen Lee <stephen.lee@mongodb.com>
2025-11-06 07:58:22 -0800

    [LG-5664] feat(title-bar): add compact variant support (#3275)
    * refactor(title-bar): reorg file structure and export Align
    
    * refactor(title-bar): styles and install lg chat provider peer dep
    
    * feat(title-bar): compact variant
    
    * test(title-bar): update stories
    
    * docs(title-bar): readme
    
    * chore(title-bar): changeset
    
    * chore(title-bar): copilot feedback
    
    * feat(chat-window): enable compact TitleBar in ChatWindow
    
    * fix: chat builds

--- chat/title-bar/src/TitleBar/TitleBar.tsx
+++ chat/title-bar/src/TitleBar/TitleBar.tsx
@@ -1,5 +1,9 @@
 import React, { ForwardedRef, forwardRef } from 'react';
 import { Avatar, Variant as ChatAvatarVariant } from '@lg-chat/avatar';
+import {
+  useLeafyGreenChatContext,
+  Variant,
+} from '@lg-chat/leafygreen-chat-provider';
 
 import { Badge } from '@leafygreen-ui/badge';
 import XIcon from '@leafygreen-ui/icon/dist/X';
@@ -7,29 +11,41 @@ import { IconButton } from '@leafygreen-ui/icon-button';
 import LeafyGreenProvider, {
   useDarkMode,
 } from '@leafygreen-ui/leafygreen-provider';
+import { consoleOnce } from '@leafygreen-ui/lib';
 import { Body } from '@leafygreen-ui/typography';
 
 import {
   getContentContainerStyles,
   getTitleBarStyles,
 } from './TitleBar.styles';
-import { Align, TitleBarProps } from '.';
+import { Align, type TitleBarProps } from './TitleBar.types';
 
 export const TitleBar = forwardRef(
   (
     {
-      title,
-      className,
-      align = Align.Center,
-      onClose,
+      align: alignProp = Align.Center,
       badgeText,
+      className,
       darkMode: darkModeProp,
       iconSlot,
+      onClose,
+      title,
       ...rest
     }: TitleBarProps,
     ref: ForwardedRef<HTMLDivElement>,
   ) => {
     const { darkMode, theme } = useDarkMode(darkModeProp);
+    const { variant } = useLeafyGreenChatContext();
+    const isCompact = variant === Variant.Compact;
+
+    if (isCompact && (alignProp !== Align.Center || iconSlot || onClose)) {
+      consoleOnce.warn(
+        `@lg-chat/title-bar: The TitleBar component's props 'align', 'iconSlot', and 'onClose' are only used in the 'spacious' variant. They will not be rendered in the 'compact' variant set by the provider.`,
+      );
+    }
+
+    const align = isCompact ? Align.Center : alignProp;
+
     return (
       <LeafyGreenProvider darkMode={darkMode}>
         <div
@@ -38,13 +54,15 @@ export const TitleBar = forwardRef(
           ref={ref}
         >
           <div className={getContentContainerStyles({ align })}>
-            <Avatar variant={ChatAvatarVariant.Mongo} sizeOverride={24} />
+            {!isCompact && (
+              <Avatar variant={ChatAvatarVariant.Mongo} sizeOverride={24} />
+            )}
             <Body>
               <strong>{title}</strong>
             </Body>
             {badgeText && <Badge variant="blue">{badgeText}</Badge>}
           </div>
-          {!!onClose && (
+          {!isCompact && !!onClose && (
             <IconButton aria-label="Close chat" onClick={onClose}>
               {iconSlot ? iconSlot : <XIcon />}
             </IconButton>
--- chat/title-bar/src/TitleBar/TitleBar.types.ts
+++ chat/title-bar/src/TitleBar/TitleBar.types.ts
@@ -13,25 +13,34 @@ export interface TitleBarProps
   extends Omit<React.ComponentPropsWithoutRef<'div'>, 'children'>,
     DarkModeProps {
   /**
-   * Title text
-   */
-  title: string;
-  /**
    * Alignment of the title text and badge
    * @default Align.Left
+   * @remarks This prop is only considered when the parent `LeafyGreenChatProvider` has `variant="spacious"`.
+   * @deprecated The spacious variant will be removed by EOY 2025. Instead, use the compact variant.
    */
   align?: Align;
+
   /**
    * Badge text rendered to indicate 'Beta' or 'Experimental' flags
    */
   badgeText?: string;
+
+  /**
+   * Slot for custom close icon
+   * @remarks This prop is only considered when the parent `LeafyGreenChatProvider` has `variant="spacious"`.
+   * @deprecated The spacious variant will be removed by EOY 2025. Instead, use the compact variant.
+   */
+  iconSlot?: React.ReactNode;
+
   /**
    * Event handler called when the close button is clicked
+   * @remarks This prop is only considered when the parent `LeafyGreenChatProvider` has `variant="spacious"`.
+   * @deprecated The spacious variant will be removed by EOY 2025. Instead, use the compact variant.
    */
   onClose?: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
 
   /**
-   * Slot for custom close icon
+   * Title text
    */
-  iconSlot?: React.ReactNode;
+  title: string;
 }
