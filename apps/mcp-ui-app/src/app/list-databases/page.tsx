d8485a96ac2c52d5c3e6877d607fb67677b40d05
Adam Rasheed <adam.rasheed@mongodb.com>
2025-11-17 14:16:13 -0500

    [LG-5666] MCP-UI App Scaffolding (#3280)
    * [LG-5666] MCP-UI App Scaffolding
    
    * Added component, sdk scaffolding, package json updates
    
    * cleanup
    
    * fixed linting
    
    * updated workspace
    
    * added tsconfig
    
    * updated build
    
    * r17 updates
    
    * updated turbo settings
    
    * added devDeps
    
    * rm sdk, changed directory structure, updated listdb styles
    
    * fix: r17 scripts (#3305)
    
    * update r17 scripts
    
    * filter apps from r17 build workflow
    
    * add mcpui to caches
    
    ---------
    
    Co-authored-by: Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>

---
+++ apps/mcp-ui-app/src/app/list-databases/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import { ListDatabases } from '@lg-mcp-ui/list-databases';
+
+export default function ListDatabasesPage() {
+  return <ListDatabases databases={[]} />;
+}
