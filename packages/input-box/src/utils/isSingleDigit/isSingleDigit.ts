f476dde18ef43806117401f926ac697b8f16c7d2
Shaneeza <shaneeza.ali@mongodb.com>
2025-11-19 21:22:52 -0500

    remove isSingleDigit utility and its associated tests

--- packages/input-box/src/utils/isSingleDigit/isSingleDigit.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Checks if the key is a single digit.
- *
- * @param key - The key to check.
- * @returns True if the key is a single digit, false otherwise.
- */
-export const isSingleDigit = (key: string): boolean => /^[0-9]$/.test(key);
