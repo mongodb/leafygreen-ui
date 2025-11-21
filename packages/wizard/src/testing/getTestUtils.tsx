e6b5dfaca6e88672f20b2df5d2dbb99397ef1f07
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-11-21 16:50:53 -0500

    adds test utils

--- packages/wizard/src/testing/getTestUtils.tsx
+++ packages/wizard/src/testing/getTestUtils.tsx
@@ -1,3 +1,5 @@
+import { screen } from '@testing-library/react';
+
 import { findByLgId, getByLgId, queryByLgId } from '@lg-tools/test-harnesses';
 
 import { LgIdString } from '@leafygreen-ui/lib';
@@ -11,5 +13,120 @@ export const getTestUtils = (
 ): TestUtilsReturnType => {
   const lgIds = getLgIds(lgId);
 
-  return {};
+  /**
+   * @returns a promise that resolves to the current WizardStep element using the `data-lgid` data attribute.
+   * The promise is rejected if no elements match or if more than one match is found.
+   */
+  const findCurrentStep = () => findByLgId!<HTMLDivElement>(lgIds.step);
+
+  /**
+   * @returns the current WizardStep element using the `data-lgid` data attribute.
+   * Will throw if no elements match or if more than one match is found.
+   */
+  const getCurrentStep = () => getByLgId!<HTMLDivElement>(lgIds.step);
+
+  /**
+   * @returns the current WizardStep element using the `data-lgid` data attribute or `null` if no elements match.
+   * Will throw if more than one match is found.
+   */
+  const queryCurrentStep = () => queryByLgId!<HTMLDivElement>(lgIds.step);
+
+  /**
+   * @returns a promise that resolves to the WizardFooter element using the `data-testid` data attribute.
+   * The promise is rejected if no elements match or if more than one match is found.
+   */
+  const findFooter = () => screen.findByTestId<HTMLElement>(lgIds.footer);
+
+  /**
+   * @returns the WizardFooter element using the `data-testid` data attribute.
+   * Will throw if no elements match or if more than one match is found.
+   */
+  const getFooter = () => screen.getByTestId<HTMLElement>(lgIds.footer);
+
+  /**
+   * @returns the WizardFooter element using the `data-testid` data attribute or `null` if no elements match.
+   * Will throw if more than one match is found.
+   */
+  const queryFooter = () => screen.queryByTestId<HTMLElement>(lgIds.footer);
+
+  /**
+   * @returns the primary button element using the `data-testid` data attribute.
+   * Will throw if no elements match or if more than one match is found.
+   */
+  const getPrimaryButton = () =>
+    screen.getByTestId<HTMLButtonElement>(lgIds.footerPrimaryButton);
+
+  /**
+   * @returns the primary button element using the `data-testid` data attribute or `null` if no elements match.
+   * Will throw if more than one match is found.
+   */
+  const queryPrimaryButton = () =>
+    screen.queryByTestId<HTMLButtonElement>(lgIds.footerPrimaryButton);
+
+  /**
+   * @returns a promise that resolves to the primary button element using the `data-testid` data attribute.
+   * The promise is rejected if no elements match or if more than one match is found.
+   */
+  const findPrimaryButton = () =>
+    screen.findByTestId<HTMLButtonElement>(lgIds.footerPrimaryButton);
+
+  /**
+   * @returns the back button element using the `data-testid` data attribute.
+   * Will throw if no elements match or if more than one match is found.
+   */
+  const getBackButton = () =>
+    screen.getByTestId<HTMLButtonElement>(lgIds.footerBackButton);
+
+  /**
+   * @returns the back button element using the `data-testid` data attribute or `null` if no elements match.
+   * Will throw if more than one match is found.
+   */
+  const queryBackButton = () =>
+    screen.queryByTestId<HTMLButtonElement>(lgIds.footerBackButton);
+
+  /**
+   * @returns a promise that resolves to the back button element using the `data-testid` data attribute.
+   * The promise is rejected if no elements match or if more than one match is found.
+   */
+  const findBackButton = () =>
+    screen.findByTestId<HTMLButtonElement>(lgIds.footerBackButton);
+
+  /**
+   * @returns the cancel button element using the `data-testid` data attribute.
+   * Will throw if no elements match or if more than one match is found.
+   */
+  const getCancelButton = () =>
+    screen.getByTestId<HTMLButtonElement>(lgIds.footerCancelButton);
+
+  /**
+   * @returns the cancel button element using the `data-testid` data attribute or `null` if no elements match.
+   * Will throw if more than one match is found.
+   */
+  const queryCancelButton = () =>
+    screen.queryByTestId<HTMLButtonElement>(lgIds.footerCancelButton);
+
+  /**
+   * @returns a promise that resolves to the cancel button element using the `data-testid` data attribute.
+   * The promise is rejected if no elements match or if more than one match is found.
+   */
+  const findCancelButton = () =>
+    screen.findByTestId<HTMLButtonElement>(lgIds.footerCancelButton);
+
+  return {
+    findCurrentStep,
+    getCurrentStep,
+    queryCurrentStep,
+    findFooter,
+    getFooter,
+    queryFooter,
+    getPrimaryButton,
+    queryPrimaryButton,
+    findPrimaryButton,
+    getBackButton,
+    queryBackButton,
+    findBackButton,
+    getCancelButton,
+    queryCancelButton,
+    findCancelButton,
+  };
 };
