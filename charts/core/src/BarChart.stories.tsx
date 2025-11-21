d19f711477c2407f77b446b9bf7853f18042634c
Nima Taheri <nima.taheri@mongodb.com>
2025-11-21 15:30:37 -0800

    cloudp-335379 Pass data index to header formatter (#3332)
    * feat: pass data-index to the header-formatter
    
    * refactor: move bar-charts stories to a separate file
    
    * docs: add changeset

---
+++ charts/core/src/BarChart.stories.tsx
@@ -0,0 +1,182 @@
+import React from 'react';
+import type { StoryObj } from '@storybook/react';
+import { getByTestId, waitFor } from '@storybook/test';
+// @ts-ignore
+import { getInstanceByDom } from 'echarts';
+
+import { ChartTooltipProps } from './ChartTooltip/ChartTooltip.types';
+import { BarHoverBehavior } from './Series/Bar';
+import { Bar } from './Series';
+import { makeSeriesData } from './testUtils';
+import { Chart, ChartTooltip, XAxis, YAxis } from '.';
+
+const numOfLineColors = 15;
+const seriesData = makeSeriesData(numOfLineColors);
+const lowDensitySeriesData = seriesData
+  .filter((_, i) => i < 3)
+  .map(series => ({
+    ...series,
+    data: series.data.filter((d, i) => i % 4 === 0),
+  }));
+
+export default {
+  title: 'Composition/Charts/Core/Bar',
+  component: Chart,
+};
+
+export const _Bar: StoryObj<{}> = {
+  render: () => {
+    return (
+      <Chart>
+        <ChartTooltip />
+        {lowDensitySeriesData.map(({ name, data }) => (
+          <Bar name={name} data={data} key={name} />
+        ))}
+      </Chart>
+    );
+  },
+};
+
+export const BarStacked: StoryObj<{}> = {
+  render: () => {
+    return (
+      <Chart>
+        <ChartTooltip />
+        {lowDensitySeriesData.map(({ name, data }, index) => (
+          <Bar
+            name={name}
+            data={data}
+            key={name}
+            stack={index < 2 ? 'same' : undefined}
+          />
+        ))}
+      </Chart>
+    );
+  },
+};
+
+export const BarWithOnHoverDimOthersBehavior: StoryObj<{
+  hoverBehavior: BarHoverBehavior;
+}> = {
+  args: {
+    hoverBehavior: BarHoverBehavior.DimOthers,
+  },
+  argTypes: {
+    hoverBehavior: {
+      control: 'select',
+      options: [BarHoverBehavior.DimOthers, BarHoverBehavior.None],
+      defaultValue: BarHoverBehavior.DimOthers,
+    },
+  },
+  render: ({ hoverBehavior }) => {
+    return (
+      <Chart data-testid="chart-component">
+        <ChartTooltip />
+        {lowDensitySeriesData.map(({ name, data }) => (
+          <Bar
+            name={name}
+            data={data}
+            key={name}
+            hoverBehavior={hoverBehavior}
+          />
+        ))}
+      </Chart>
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const chartDom = getByTestId(canvasElement, 'chart-component');
+
+    const echartsInstance = await waitFor(function getEChartsInstance() {
+      const instance = getInstanceByDom(chartDom);
+      if (!instance) throw new Error('ECharts instance not found');
+      return instance;
+    });
+
+    echartsInstance.dispatchAction({
+      type: 'highlight',
+      seriesIndex: 1,
+      dataIndex: 0,
+    });
+  },
+};
+
+export const BarWithCustomAxisPointer: StoryObj<{
+  axisPointer: ChartTooltipProps['axisPointer'];
+}> = {
+  args: {
+    axisPointer: 'shadow',
+  },
+  argTypes: {
+    axisPointer: {
+      control: 'select',
+      options: ['line', 'shadow', 'none'],
+      defaultValue: 'shadow',
+    },
+  },
+  render: ({ axisPointer }) => {
+    return (
+      <Chart>
+        <XAxis type="time" />
+        <YAxis type="value" />
+        <ChartTooltip axisPointer={axisPointer} />
+        {lowDensitySeriesData.map(({ name, data }) => (
+          <Bar name={name} data={data} key={name} />
+        ))}
+      </Chart>
+    );
+  },
+};
+
+export const BarWithCategoryAxisLabel: StoryObj<{
+  axisPointer: ChartTooltipProps['axisPointer'];
+}> = {
+  args: {
+    axisPointer: 'shadow',
+  },
+  argTypes: {
+    axisPointer: {
+      control: 'select',
+      options: ['line', 'shadow', 'none'],
+      defaultValue: 'shadow',
+    },
+  },
+  render: ({ axisPointer }) => {
+    const xAxisData = lowDensitySeriesData[0].data.map(([x, _]) =>
+      // Format as "mm:ss" instead of slicing the ISO string
+      (() => {
+        const date = new Date(x as number);
+        const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+        const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+        return `⏱️ ${minutes}:${seconds}`;
+      })(),
+    );
+
+    const yAxisData = [
+      'Low',
+      'Medium-Low',
+      'Medium',
+      'Medium-High',
+      'High',
+      'Very High',
+      'Extreme',
+    ];
+
+    return (
+      <Chart>
+        <XAxis type="category" labels={xAxisData} />
+        <YAxis type="category" labels={yAxisData} />
+        <ChartTooltip axisPointer={axisPointer} />
+        {lowDensitySeriesData.map(({ name, data }) => (
+          <Bar
+            key={name}
+            name={name}
+            data={data.map(([_, value], i) => [
+              i,
+              (value as number) % yAxisData.length,
+            ])}
+          />
+        ))}
+      </Chart>
+    );
+  },
+};
