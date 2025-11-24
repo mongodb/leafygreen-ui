b2c8178b074a29e4218286e8c4252b9b07df5db8
Adam Michael Thompson <adam.thompson@mongodb.com>
2025-11-24 14:21:17 -0500

    Update WizardFooter.types.ts

--- packages/wizard/src/WizardFooter/WizardFooter.types.ts
+++ packages/wizard/src/WizardFooter/WizardFooter.types.ts
@@ -1,5 +1,14 @@
-import { FormFooterProps } from '@leafygreen-ui/form-footer';
+import type {
+  BackStandardButtonProps,
+  CancelStandardButtonProps,
+  FormFooterProps,
+  PrimaryStandardButtonProps,
+} from '@leafygreen-ui/form-footer';
 
 export interface WizardFooterProps
   extends React.ComponentProps<'footer'>,
-    FormFooterProps {}
+    FormFooterProps {
+  backButtonProps?: BackStandardButtonProps;
+  cancelButtonProps?: CancelStandardButtonProps;
+  primaryButtonProps: PrimaryStandardButtonProps;
+}
