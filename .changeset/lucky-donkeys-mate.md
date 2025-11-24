fdab2f91d5098f4381ddb5ad5b66796af50ecbc7
Stephen Lee <stephen.lee@mongodb.com>
2025-11-24 14:28:30 -0800

    [LG-5736] chore(input-bar): update disclaimer text (#3343)
    * feat(input-bar): update disclaimer text
    
    * chore(input-bar): changeset

---
+++ .changeset/lucky-donkeys-mate.md
@@ -0,0 +1,5 @@
+---
+'@lg-chat/input-bar': minor
+---
+
+[LG-5736](https://jira.mongodb.org/browse/LG-5736): update disclaimer text
--- chat/input-bar/src/DisclaimerText/DisclaimerText.tsx
+++ chat/input-bar/src/DisclaimerText/DisclaimerText.tsx
@@ -5,13 +5,13 @@ import { Disclaimer, Link } from '@leafygreen-ui/typography';
 import { linkStyles } from './DisclaimerText.styles';
 import { type DisclaimerTextProps } from './DisclaimerText.types';
 
-const DISCLAIMER_TEXT = 'AI can make mistakes, so review for accuracy.';
-const LEARN_MORE_TEXT = 'Learn more';
+const DISCLAIMER_TEXT = 'Review answers for accuracy. See our ';
+const LEARN_MORE_TEXT = 'AI and data usage FAQ';
 const LEARN_MORE_URL = 'https://www.mongodb.com/docs/generative-ai-faq/';
 
 export const DisclaimerText = ({ className, ...rest }: DisclaimerTextProps) => (
   <Disclaimer className={className} {...rest}>
-    {DISCLAIMER_TEXT}{' '}
+    {DISCLAIMER_TEXT}
     <Link className={linkStyles} href={LEARN_MORE_URL}>
       {LEARN_MORE_TEXT}
     </Link>
