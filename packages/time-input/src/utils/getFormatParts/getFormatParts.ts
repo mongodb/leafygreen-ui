971540758f0ada66b2b310496d5556f862b1b739
Shaneeza <shaneeza.ali@mongodb.com>
2025-12-02 17:52:53 -0500

    refactor(time-input): simplify getFormatParts function by removing locale dependency and enhancing formatting logic

--- packages/time-input/src/utils/getFormatParts/getFormatParts.ts
+++ packages/time-input/src/utils/getFormatParts/getFormatParts.ts
@@ -1,60 +1,18 @@
-import { SupportedLocales } from '@leafygreen-ui/date-utils';
-import { getFormatter } from '..';
-
-/** A sample date to use for formatting */
-const now = new Date();
-
-/**
- * Returns the ISO format parts for the given locale
- * @param showSeconds - Whether to show seconds
- * @returns The ISO format parts
- *
- * @example
- * ```js
- * getIsoFormatParts(true);
- *
- * // [
- * //   { type: 'hour', value: '' },
- * //   { type: 'literal', value: ':' },
- * //   { type: 'minute', value: '' },
- * //   { type: 'literal', value: ':' },
- * //   { type: 'second', value: '' },
- * // ]
- * ```
- */
-// const getIsoFormatParts = (
-//   showSeconds: boolean,
-// ): Array<Intl.DateTimeFormatPart> => {
-//   const formatParts: Array<Intl.DateTimeFormatPart> = [
-//     { type: 'hour', value: '' },
-//     { type: 'literal', value: ':' },
-//     { type: 'minute', value: '' },
-//     ...(showSeconds
-//       ? ([
-//           { type: 'literal', value: ':' },
-//           { type: 'second', value: '' },
-//         ] as Array<Intl.DateTimeFormatPart>)
-//       : []),
-//   ];
-
-//   return formatParts;
-// };
-
-// TODO: confirm with Sooa if we want to change the presentation value based on the locale. If that is the case then we only need to return a predefined format with seconds or not.
 /**
  * Returns an array of {@link Intl.DateTimeFormatPart} for the provided locale.
  *
  * Filters out the dayPeriod and the empty literal before it
- * since they are not part of the time format parts
+ * since they are not part of the time format parts.
  *
- * @param locale - The locale to get the format parts for
+ * This will return `:` for every literal part regardless of the locale.
+ *
  * @param showSeconds - Whether to show seconds
  * @returns The format parts
  *
  * @example
  *
  * ```js
- * getFormatParts({ locale: 'en-US', showSeconds: true });
+ * getFormatParts({ showSeconds: true });
  *
  * // [
  * //   { type: 'hour', value: '' },
@@ -65,22 +23,22 @@ const now = new Date();
  * // ]
  */
 export const getFormatParts = ({
-  locale,
   showSeconds = false,
 }: {
   locale: string;
   showSeconds?: boolean;
 }): Array<Intl.DateTimeFormatPart> | undefined => {
-  const isIsoLocale = locale === SupportedLocales.ISO_8601;
-
-  const formatter = getFormatter({
-    locale: locale,
-    isIsoLocale,
-    showSeconds,
-    options: { hourCycle: 'h23' },
-  });
-
-  const formatParts = formatter?.formatToParts(now);
+  const formatParts: Array<Intl.DateTimeFormatPart> = [
+    { type: 'hour', value: '' },
+    { type: 'literal', value: ':' },
+    { type: 'minute', value: '' },
+    ...(showSeconds
+      ? ([
+          { type: 'literal', value: ':' },
+          { type: 'second', value: '' },
+        ] as Array<Intl.DateTimeFormatPart>)
+      : []),
+  ];
 
   return formatParts;
 };
