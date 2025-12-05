f8c89ff5e4fa36f790ba867f409ae407e058e262
Stephen Lee <stephen.lee@mongodb.com>
2025-12-05 14:56:46 -0800

    fix(chat-button): test

--- chat/chat-button/src/ChatButton/ChatButton.spec.tsx
+++ chat/chat-button/src/ChatButton/ChatButton.spec.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { axe } from 'jest-axe';
 
-import { ChatButton } from '.';
+import { ChatButton, type ChatButtonProps } from '.';
+
+const buttonText = 'Button text';
+
+function renderChatButton(props: ChatButtonProps = {}) {
+  const utils = render(
+    <ChatButton {...props} data-testid="chat-button">
+      {buttonText}
+    </ChatButton>,
+  );
+  const button = screen.getByTestId('chat-button');
+  return { ...utils, button };
+}
 
 describe('chat/chat-button', () => {
   describe('a11y', () => {
     test('does not have basic accessibility issues', async () => {
-      const { container } = render(<ChatButton />);
+      const { container } = renderChatButton();
       const results = await axe(container);
       expect(results).toHaveNoViolations();
     });
