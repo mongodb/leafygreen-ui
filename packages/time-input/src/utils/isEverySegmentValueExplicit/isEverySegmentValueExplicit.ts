2a4ffd3443ba3077b52a0cb8c1b220e0f273de53
Shaneeza <shaneeza.ali@mongodb.com>
2025-12-23 16:52:27 -0500

    refactor(time-input): rename isExplicitSegmentValue to createExplicitTimeSegmentValidator for clarity and update its usage in isEverySegmentValueExplicit

--- packages/time-input/src/utils/isEverySegmentValueExplicit/isEverySegmentValueExplicit.ts
+++ packages/time-input/src/utils/isEverySegmentValueExplicit/isEverySegmentValueExplicit.ts
@@ -3,7 +3,7 @@ import { createExplicitSegmentValidator } from '@leafygreen-ui/input-box';
 import { TimeSegment, TimeSegmentsState } from '../../shared.types';
 import { getTimeSegmentRules } from '../getTimeSegmentRules';
 
-export const isExplicitSegmentValue = (is12HourFormat: boolean) =>
+export const createExplicitTimeSegmentValidator = (is12HourFormat: boolean) =>
   createExplicitSegmentValidator({
     segmentEnum: TimeSegment,
     rules: getTimeSegmentRules({ is12HourFormat }),
@@ -20,9 +20,12 @@ export const isEverySegmentValueExplicit = ({
   segments: TimeSegmentsState;
   is12HourFormat: boolean;
 }): boolean => {
+  const isExplicitSegmentValue =
+    createExplicitTimeSegmentValidator(is12HourFormat);
+
   return (Object.entries(segments) as Array<[TimeSegment, string]>).every(
     ([segment, value]) => {
-      const isExplicit = isExplicitSegmentValue(is12HourFormat)({
+      const isExplicit = isExplicitSegmentValue({
         segment: segment,
         value,
         allowZero: segment === TimeSegment.Hour ? !is12HourFormat : true,
