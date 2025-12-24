df25f3cf2d9cbdd587cad6db61ba7fe3b950b06e
Adam Rasheed <adam.rasheed@mongodb.com>
2025-12-24 02:45:15 -0800

    [LG-844] feat(collection-toolbar): Title component

--- packages/collection-toolbar/src/CollectionToolbar.stories.tsx
+++ packages/collection-toolbar/src/CollectionToolbar.stories.tsx
@@ -1,17 +1,43 @@
-
 import React from 'react';
+import { storybookArgTypes, StoryMetaType } from '@lg-tools/storybook-utils';
 import { StoryFn } from '@storybook/react';
 
-import { CollectionToolbar } from '.';
+import { CollectionToolbar, Size, Variant } from '.';
 
-export default {
+const meta: StoryMetaType<typeof CollectionToolbar> = {
   title: 'Components/CollectionToolbar',
   component: CollectionToolbar,
-}
+  parameters: {
+    default: 'LiveExample',
+    generate: {
+      combineArgs: {
+        darkMode: [false, true],
+        variant: Object.values(Variant),
+        size: Object.values(Size),
+      },
+    },
+  },
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: Object.values(Variant),
+    },
+    size: {
+      control: 'select',
+      options: Object.values(Size),
+    },
+    darkMode: storybookArgTypes.darkMode,
+  },
+};
 
-const Template: StoryFn<typeof CollectionToolbar> = (props) => (
+export default meta;
+
+export const LiveExample: StoryFn<typeof CollectionToolbar> = props => (
   <CollectionToolbar {...props} />
 );
 
-export const Basic = Template.bind({});
-
+export const Title: StoryFn<typeof CollectionToolbar> = props => (
+  <CollectionToolbar {...props}>
+    <CollectionToolbar.Title>Title</CollectionToolbar.Title>
+  </CollectionToolbar>
+);
--- packages/collection-toolbar/src/CollectionToolbar/CollectionToolbar.tsx
+++ packages/collection-toolbar/src/CollectionToolbar/CollectionToolbar.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
 
+import {
+  CompoundComponent,
+  findChild,
+} from '@leafygreen-ui/compound-component';
+
 import { getCollectionToolbarStyles } from './CollectionToolbar.styles';
 import {
   CollectionToolbarProps,
+  CollectionToolbarSubComponentProperty,
   Size,
   Variant,
 } from './CollectionToolbar.types';
+import { CollectionToolbarTitle } from './CollectionToolbarTitle';
 
-export function CollectionToolbar({
-  size = Size.Default,
-  variant = Variant.Default,
-  className,
-}: CollectionToolbarProps) {
-  return (
-    <div className={getCollectionToolbarStyles({ size, variant, className })}>
-      CollectionToolbar
-    </div>
-  );
-}
+export const CollectionToolbar = CompoundComponent(
+  ({
+    size = Size.Default,
+    variant = Variant.Default,
+    className,
+    children,
+  }: CollectionToolbarProps) => {
+    const title = findChild(
+      children,
+      CollectionToolbarSubComponentProperty.Title,
+    );
+    return (
+      <div className={getCollectionToolbarStyles({ size, variant, className })}>
+        {title}
+        CollectionToolbar
+      </div>
+    );
+  },
+  {
+    displayName: 'CollectionToolbar',
+    Title: CollectionToolbarTitle,
+  },
+);
 
 CollectionToolbar.displayName = 'CollectionToolbar';
