94fafbf24f11f8c6e0b5dfce08378906ea5d9c10
Adam Rasheed <adam.rasheed@mongodb.com>
2025-12-29 13:05:42 -0800

    updated tests, linitng cleanup

--- packages/collection-toolbar/src/CollectionToolbar/CollectionToolbar.spec.tsx
+++ packages/collection-toolbar/src/CollectionToolbar/CollectionToolbar.spec.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
+<<<<<<< HEAD
 import { Variant } from '../shared.types';
 import { getTestUtils } from '../testing/getTestUtils';
 
 import { CollectionToolbar } from '.';
+=======
+import { CollectionToolbar, Variant } from '.';
+>>>>>>> d84c9e296 (updated tests, linitng cleanup)
 
 describe('packages/collection-toolbar', () => {
   test('renders correctly', () => {
     render(<CollectionToolbar />);
+<<<<<<< HEAD
     const utils = getTestUtils();
     expect(utils.getCollectionToolbar()).toBeInTheDocument();
+=======
+    expect(screen.getByTestId('lg-collection_toolbar')).toBeInTheDocument();
+>>>>>>> d84c9e296 (updated tests, linitng cleanup)
   });
 
   test('applies className to the root element', () => {
     render(<CollectionToolbar className="test-class" />);
+<<<<<<< HEAD
     const utils = getTestUtils();
     expect(utils.getCollectionToolbar()?.className).toContain('test-class');
+=======
+    expect(screen.getByTestId('lg-collection_toolbar')).toHaveClass(
+      'test-class',
+    );
+>>>>>>> d84c9e296 (updated tests, linitng cleanup)
   });
 
   describe('variant: collapsible', () => {
