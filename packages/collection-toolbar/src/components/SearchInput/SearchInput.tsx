59fc2b2bcc39ed561de194c01f2b70bb2075e454
Adam Rasheed <adam.rasheed@mongodb.com>
2026-01-21 11:31:29 -0800

    cleaned up prop types

--- packages/collection-toolbar/src/components/SearchInput/SearchInput.tsx
+++ packages/collection-toolbar/src/components/SearchInput/SearchInput.tsx
@@ -1,5 +1,6 @@
 import React, { forwardRef } from 'react';
 
+import { validateAriaLabelProps } from '@leafygreen-ui/a11y';
 import { CompoundSubComponent } from '@leafygreen-ui/compound-component';
 import { SearchInput as LGSearchInput } from '@leafygreen-ui/search-input';
 
@@ -12,24 +13,17 @@ import { SearchInputProps } from './SearchInput.types';
 export const SearchInput = CompoundSubComponent(
   // eslint-disable-next-line react/display-name
   forwardRef<HTMLInputElement, SearchInputProps>(
-    (
-      {
-        className,
-        'aria-label': ariaLabel = '',
-        'aria-labelledby': ariaLabelledby,
-        ...props
-      },
-      fwdRef,
-    ) => {
+    ({ className, ...props }, fwdRef) => {
       const { size, variant, darkMode, lgIds } = useCollectionToolbarContext();
+
+      validateAriaLabelProps(props, 'SearchInput');
+
       return (
         <LGSearchInput
           size={size}
           darkMode={darkMode}
           ref={fwdRef}
           data-lgid={lgIds.searchInput}
-          aria-label={ariaLabel}
-          aria-labelledby={ariaLabelledby}
           className={getSearchInputStyles({ variant, size, className })}
           {...props}
         />
