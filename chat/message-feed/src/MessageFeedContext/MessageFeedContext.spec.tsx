958d58ac39d50b8c32d2a340f9e67c6566c62069
Shaneeza <shaneeza.ali@mongodb.com>
2026-01-27 14:17:39 -0500

    feat(MessageFeed): add MessageFeedContext

---
+++ chat/message-feed/src/MessageFeedContext/MessageFeedContext.spec.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { renderHook } from '@leafygreen-ui/testing-lib';
+
+import {
+  MessageFeedProvider,
+  useMessageFeedContext,
+} from './MessageFeedContext';
+import { MessageFeedProviderProps } from './MessageFeedContext.types';
+
+const renderMessageFeedProvider = (
+  props?: Partial<MessageFeedProviderProps>,
+) => {
+  const defaultProps: MessageFeedProviderProps = {
+    shouldHideInitialMessage: false,
+  };
+
+  const { result } = renderHook(() => useMessageFeedContext(), {
+    wrapper: ({ children }: { children?: React.ReactNode }) => (
+      <MessageFeedProvider {...defaultProps} {...props}>
+        {children}
+      </MessageFeedProvider>
+    ),
+  });
+
+  return { result };
+};
+describe('useMessageFeedContext', () => {
+  test('should return the correct context', () => {
+    const { result } = renderMessageFeedProvider();
+    expect(result.current.shouldHideInitialMessage).toBe(false);
+  });
+
+  test('should return the correct context when props are provided', () => {
+    const { result } = renderMessageFeedProvider({
+      shouldHideInitialMessage: true,
+    });
+    expect(result.current.shouldHideInitialMessage).toBe(true);
+  });
+
+  test('should throw an error when used outside the provider', () => {
+    expect(() => {
+      renderHook(() => useMessageFeedContext());
+    }).toThrow(
+      'useMessageFeedContext must be used within a MessageFeedContextProvider',
+    );
+  });
+});
---
+++ chat/message-feed/src/MessageFeedContext/MessageFeedContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext } from 'react';
+
+import {
+  MessageFeedContextType,
+  MessageFeedProviderProps,
+} from './MessageFeedContext.types';
+
+export const MessageFeedContext = createContext<MessageFeedContextType | null>(
+  null,
+);
+
+export const MessageFeedProvider = ({
+  children,
+  shouldHideInitialMessage = false,
+}: MessageFeedProviderProps) => {
+  return (
+    <MessageFeedContext.Provider value={{ shouldHideInitialMessage }}>
+      {children}
+    </MessageFeedContext.Provider>
+  );
+};
+
+export const useMessageFeedContext = () => {
+  const context = useContext(MessageFeedContext);
+
+  if (!context) {
+    throw new Error(
+      'useMessageFeedContext must be used within a MessageFeedContextProvider',
+    );
+  }
+
+  return context;
+};
