81a414d71c6ee6468a6b8f864ea67079bc72a05f
Shaneeza <shaneeza.ali@mongodb.com>
2026-01-30 10:33:27 -0500

    fix(ResourceListItem): update prop documentation for glyph and children to clarify usage

--- chat/message-feed/src/Components/ResourceListItem/ResourceListItem.types.ts
+++ chat/message-feed/src/Components/ResourceListItem/ResourceListItem.types.ts
@@ -5,12 +5,12 @@ import { GlyphName } from '@leafygreen-ui/icon';
 export interface ResourceListItemProps
   extends Omit<ComponentPropsWithRef<'li'>, 'children'> {
   /**
-   * The title of the list item.
+   * The LeafyGreen icon glyph name to render in the list item
    */
   glyph: GlyphName;
 
   /**
-   * The content of the list item.
+   * The content of the list item. This will render after the glyph
    */
   children: React.ReactNode;
 }
